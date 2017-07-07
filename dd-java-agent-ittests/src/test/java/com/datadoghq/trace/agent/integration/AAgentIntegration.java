2d5f22b6daf37ff064483f5a3e87f7a2b51b8d5c
Guillaume Polaert <gpolaert@gmail.com>
2017-07-07 12:04:17 +0200

    Debugging CircleCI

--- dd-java-agent-ittests/src/test/java/com/datadoghq/trace/agent/integration/AAgentIntegration.java
+++ dd-java-agent-ittests/src/test/java/com/datadoghq/trace/agent/integration/AAgentIntegration.java
@@ -16,9 +16,6 @@ public class AAgentIntegration {
 	@Before
 	public void beforeTest() throws Exception {
 
-
-		GlobalTracer.register(new DDTracer());
-
 		try{
 			GlobalTracer.register(tracer);
 		}catch(Exception e){
