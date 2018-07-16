8dc84de0e506f7ef8f5262c028f6f2ecece0a5b1
Gary Huang <gary.huang@datadoghq.com>
2018-07-16 16:09:56 -0400

    POC sun JDK http urlconnection instrumentation, currently creates one span.

--- dd-java-agent/instrumentation/http-url-connection/src/test/groovy/HttpUrlConnectionTest.groovy
+++ dd-java-agent/instrumentation/http-url-connection/src/test/groovy/HttpUrlConnectionTest.groovy
@@ -7,6 +7,7 @@ import io.opentracing.propagation.Format
 import io.opentracing.propagation.TextMap
 import io.opentracing.tag.Tags
 import io.opentracing.util.GlobalTracer
+import org.springframework.web.client.RestTemplate
 import ratpack.handling.Context
 import spock.lang.Shared
 
@@ -18,7 +19,8 @@ import static ratpack.http.HttpMethod.POST
 
 class HttpUrlConnectionTest extends AgentTestRunner {
   static {
-    System.setProperty("dd.integration.httpurlconnection.enabled", "true")
+    System.setProperty("dd.integration.httpurlconnection.enabled", "false")
+    System.setProperty("dd.integration.sun-httpurlconnection.enabled", "true")
   }
 
   static final RESPONSE = "<html><body><h1>Hello test.</h1>"
@@ -441,6 +443,17 @@ class HttpUrlConnectionTest extends AgentTestRunner {
         }
       }
     }
+  }
+
+  def "rest template"() {
+    when:
+    RestTemplate restTemplate = new RestTemplate()
+    String res = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", String.class)
+
+    then:
+    println(res)
+    println(TEST_WRITER.size())
+    println(TEST_WRITER[0].size())
   }
 
   private static class RatpackResponseAdapter implements TextMap {
