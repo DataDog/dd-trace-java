ee0ef7633ea44c8b94d06bedc5d76b3c55ef4cac
gary.huang <gary.huang@datadoghq.com>
2018-08-09 16:53:32 -0400

    Spring Boot Webflux Netty tests
    Tests out spring boot webflux 2 with Netty.

---
+++ dd-java-agent/instrumentation/netty-4.1/src/test/groovy/SpringWebFluxTestApplication.groovy
@@ -0,0 +1,136 @@
+package datadog.trace.instrumentation.netty41.server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.WebApplicationType
+import org.springframework.boot.autoconfigure.AutoConfigureBefore
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.web.reactive.ReactiveWebServerFactoryAutoConfiguration
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.server.HandlerFunction
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET
+import static org.springframework.web.reactive.function.server.RouterFunctions.route
+
+@SpringBootApplication
+class SpringWebFluxTestApplication {
+
+  static void main(String[] args) {
+    SpringApplication app = new SpringApplication(SpringWebFluxTestApplication)
+    app.setWebApplicationType(WebApplicationType.REACTIVE)
+    app.run(args)
+  }
+
+  @TestConfiguration
+  @AutoConfigureBefore(ReactiveWebServerFactoryAutoConfiguration)
+  class ForceNettyAutoConfiguration {
+    @Bean
+    NettyReactiveWebServerFactory nettyFactory() {
+      return new NettyReactiveWebServerFactory()
+    }
+  }
+
+  @Bean
+  RouterFunction<ServerResponse> redirectRouterFunction() {
+    return route(GET("/double-greet-redirect"), {
+      req -> ServerResponse.temporaryRedirect(URI.create("/double-greet")).build()
+    })
+  }
+
+  @Bean
+  RouterFunction<ServerResponse> echoRouterFunction(EchoHandler echoHandler) {
+    return route(POST("/echo"), new HandlerFunction<ServerResponse>() {
+      @Override
+      Mono<ServerResponse> handle(ServerRequest request) {
+        return echoHandler.echo(request)
+      }
+    }).andRoute(POST("/fail-echo"), new HandlerFunction<ServerResponse>() {
+      @Override
+      Mono<ServerResponse> handle(ServerRequest request) {
+        return echoHandler.echo(null)
+      }
+    })
+  }
+
+  @Bean
+  RouterFunction<ServerResponse> greetRouterFunction(GreetingHandler greetingHandler) {
+    return route(GET("/greet"), new HandlerFunction<ServerResponse>() {
+      @Override
+      Mono<ServerResponse> handle(ServerRequest request) {
+        return greetingHandler.defaultGreet()
+      }
+    }).andRoute(GET("/greet/{name}"), new HandlerFunction<ServerResponse>() {
+      @Override
+      Mono<ServerResponse> handle(ServerRequest request) {
+        return greetingHandler.customGreet(request)
+      }
+    }).andRoute(GET("/double-greet"), new HandlerFunction<ServerResponse>() {
+      @Override
+      Mono<ServerResponse> handle(ServerRequest request) {
+        return greetingHandler.doubleGreet()
+      }
+    }).andRoute(GET("/greet-counter/{count}"), new HandlerFunction<ServerResponse>() {
+      @Override
+      Mono<ServerResponse> handle(ServerRequest request) {
+        return greetingHandler.counterGreet(request)
+      }
+    })
+  }
+
+  @Component
+  class EchoHandler {
+    Mono<ServerResponse> echo(ServerRequest request) {
+      return ServerResponse.accepted().contentType(MediaType.TEXT_PLAIN)
+        .body(request.bodyToMono(String), String)
+    }
+  }
+
+  @Component
+  class GreetingHandler {
+    static final String DEFAULT_RESPONSE = "HELLO"
+
+    static String[] createGreetJsonStringArr(int count) {
+      String[] jsonStrArr = new String[count]
+      for (int i = 0; i < count; ++i) {
+        jsonStrArr[i] = createGreetJsonString(i)
+      }
+      return jsonStrArr
+    }
+
+    static String createGreetJsonString(int id) {
+      return new String("{\"hello\":" + id + "}")
+    }
+
+    Mono<ServerResponse> defaultGreet() {
+      return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+        .body(BodyInserters.fromObject(DEFAULT_RESPONSE))
+    }
+
+    Mono<ServerResponse> doubleGreet() {
+      return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+        .body(BodyInserters.fromObject(DEFAULT_RESPONSE + DEFAULT_RESPONSE))
+    }
+
+    Mono<ServerResponse> customGreet(ServerRequest request) {
+      return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+        .body(BodyInserters.fromObject(DEFAULT_RESPONSE + " " + request.pathVariable("name")))
+    }
+
+    Mono<ServerResponse> counterGreet(ServerRequest request) {
+      final int countTo = Integer.valueOf(request.pathVariable("count"))
+      String[] jsonStrArr = createGreetJsonStringArr(countTo)
+      return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+        .body(Flux.fromArray(jsonStrArr), String)
+    }
+  }
+}
---
+++ dd-java-agent/instrumentation/netty-4.1/src/test/groovy/SpringWebfluxTest.groovy
@@ -0,0 +1,278 @@
+import datadog.trace.agent.test.AgentTestRunner
+import datadog.trace.agent.test.OkHttpUtils
+import datadog.trace.api.DDSpanTypes
+import datadog.trace.api.DDTags
+import datadog.trace.instrumentation.netty41.server.SpringWebFluxTestApplication
+import io.opentracing.tag.Tags
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.WebApplicationType
+import org.springframework.context.ConfigurableApplicationContext
+import spock.lang.Shared
+import spock.lang.Unroll
+
+import static datadog.trace.agent.test.ListWriterAssert.assertTraces
+
+class SpringWebfluxTest extends AgentTestRunner {
+
+  static {
+    System.setProperty("dd.integration.netty.enabled", "true")
+  }
+
+  static final okhttp3.MediaType PLAIN_TYPE = okhttp3.MediaType.parse("text/plain; charset=utf-8")
+
+  @Shared
+  SpringApplication app
+
+  @Shared
+  ConfigurableApplicationContext context
+
+  @Shared
+  String randomServerPort = -1
+
+  @Shared
+  String baseUrl = "http://localhost:"
+
+  OkHttpClient client = OkHttpUtils.client()
+
+  def setupSpec() {
+    app = new SpringApplication(SpringWebFluxTestApplication)
+    app.setDefaultProperties([
+      "server.port" : 0
+    ])
+    app.setWebApplicationType(WebApplicationType.REACTIVE)
+    context = app.run("")
+    randomServerPort = context.getEnvironment().getProperty("local.server.port")
+    baseUrl += randomServerPort
+    println("Server running at " + baseUrl)
+  }
+
+  @Unroll
+  def "Basic GET test #testName"() {
+    setup:
+    String url = baseUrl + "/greet" + urlSuffix
+    def request = new Request.Builder().url(url).get().build()
+
+    when:
+    def response = client.newCall(request).execute()
+
+    then:
+    response.code == 200
+    response.body().string() == expectedResponseBody
+    assertTraces(TEST_WRITER, 1) {
+      trace(0, 1) {
+        span(0) {
+          operationName "netty.request"
+          spanType DDSpanTypes.HTTP_SERVER
+          tags {
+            "$Tags.COMPONENT.key" "netty"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_SERVER
+            "$DDTags.SPAN_TYPE" DDSpanTypes.HTTP_SERVER
+            "$Tags.PEER_HOSTNAME.key" "localhost"
+            "$Tags.PEER_PORT.key" Integer
+            "$Tags.HTTP_METHOD.key" "GET"
+            "$Tags.HTTP_STATUS.key" 200
+            "$Tags.HTTP_URL.key" url
+            defaultTags()
+          }
+        }
+      }
+    }
+
+    where:
+    testName | urlSuffix | expectedResponseBody
+    "without paramaters" | "" | SpringWebFluxTestApplication.GreetingHandler.DEFAULT_RESPONSE
+    "with parameter" | "/WORLD" | SpringWebFluxTestApplication.GreetingHandler.DEFAULT_RESPONSE + " WORLD"
+  }
+
+  def "404 GET test"() {
+    setup:
+    String url = baseUrl + "/notfoundgreet"
+    def request = new Request.Builder().url(url).get().build()
+
+    when:
+    def response = client.newCall(request).execute()
+
+    then:
+    response.code == 404
+    assertTraces(TEST_WRITER, 1) {
+      trace(0, 1) {
+        span(0) {
+          operationName "netty.request"
+          spanType DDSpanTypes.HTTP_SERVER
+          tags {
+            "$Tags.COMPONENT.key" "netty"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_SERVER
+            "$DDTags.SPAN_TYPE" DDSpanTypes.HTTP_SERVER
+            "$Tags.PEER_HOSTNAME.key" "localhost"
+            "$Tags.PEER_PORT.key" Integer
+            "$Tags.HTTP_METHOD.key" "GET"
+            "$Tags.HTTP_STATUS.key" 404
+            "$Tags.HTTP_URL.key" url
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "Basic POST test"() {
+    setup:
+    String echoString = "TEST"
+    String url = "http://localhost:$randomServerPort/echo"
+    RequestBody body = RequestBody.create(PLAIN_TYPE, echoString)
+    def request = new Request.Builder().url(url).post(body).build()
+
+    when:
+    def response = client.newCall(request).execute()
+
+    then:
+    response.code() == 202
+    response.body().string() == echoString
+    assertTraces(TEST_WRITER, 1) {
+      trace(0, 1) {
+        span(0) {
+          operationName "netty.request"
+          spanType DDSpanTypes.HTTP_SERVER
+          tags {
+            "$Tags.COMPONENT.key" "netty"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_SERVER
+            "$DDTags.SPAN_TYPE" DDSpanTypes.HTTP_SERVER
+            "$Tags.PEER_HOSTNAME.key" "localhost"
+            "$Tags.PEER_PORT.key" Integer
+            "$Tags.HTTP_METHOD.key" "POST"
+            "$Tags.HTTP_STATUS.key" 202
+            "$Tags.HTTP_URL.key" url
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "POST to bad endpoint"() {
+    setup:
+    String echoString = "TEST"
+    RequestBody body = RequestBody.create(PLAIN_TYPE, echoString)
+    String url = "http://localhost:$randomServerPort/fail-echo"
+    def request = new Request.Builder().url(url).post(body).build()
+
+    when:
+    def response = client.newCall(request).execute()
+
+    then:
+    response.code() == 500
+    assertTraces(TEST_WRITER, 1) {
+      trace(0, 1) {
+        span(0) {
+          operationName "netty.request"
+          spanType DDSpanTypes.HTTP_SERVER
+          errored true
+          tags {
+            "$Tags.COMPONENT.key" "netty"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_SERVER
+            "$DDTags.SPAN_TYPE" DDSpanTypes.HTTP_SERVER
+            "$Tags.PEER_HOSTNAME.key" "localhost"
+            "$Tags.PEER_PORT.key" Integer
+            "$Tags.HTTP_METHOD.key" "POST"
+            "$Tags.HTTP_STATUS.key" 500
+            "$Tags.HTTP_URL.key" url
+            "error" true
+            // errorTags(NullPointerException, String)
+            // ideally this should be here with the stacktrace, but for now this doesn't seem
+            // like a low hanging fruit/requires further instrumentation at spring webflux level
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "Redirect test"() {
+    setup:
+    String url = baseUrl + "/double-greet-redirect"
+    def request = new Request.Builder().url(url).get().build()
+
+    when:
+    def response = client.newCall(request).execute()
+
+    then:
+    response.code == 200
+    assertTraces(TEST_WRITER, 2) {
+      trace(0, 1) {
+        span(0) {
+          operationName "netty.request"
+          spanType DDSpanTypes.HTTP_SERVER
+          tags {
+            "$Tags.COMPONENT.key" "netty"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_SERVER
+            "$DDTags.SPAN_TYPE" DDSpanTypes.HTTP_SERVER
+            "$Tags.PEER_HOSTNAME.key" "localhost"
+            "$Tags.PEER_PORT.key" Integer
+            "$Tags.HTTP_METHOD.key" "GET"
+            "$Tags.HTTP_STATUS.key" 307
+            "$Tags.HTTP_URL.key" url
+            defaultTags()
+          }
+        }
+      }
+      trace(1, 1) {
+        span(0) {
+          operationName "netty.request"
+          spanType DDSpanTypes.HTTP_SERVER
+          tags {
+            "$Tags.COMPONENT.key" "netty"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_SERVER
+            "$DDTags.SPAN_TYPE" DDSpanTypes.HTTP_SERVER
+            "$Tags.PEER_HOSTNAME.key" "localhost"
+            "$Tags.PEER_PORT.key" Integer
+            "$Tags.HTTP_METHOD.key" "GET"
+            "$Tags.HTTP_STATUS.key" 200
+            "$Tags.HTTP_URL.key" baseUrl + "/double-greet"
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "Flux GET test"() {
+    setup:
+    def count = 10
+    String[] expectedJsonStringArr = SpringWebFluxTestApplication.GreetingHandler.createGreetJsonStringArr(10)
+    String expectedResponseBodyStr = ""
+    for (int i = 0; i < count; ++i) {
+      expectedResponseBodyStr += expectedJsonStringArr[i]
+    }
+    String url = baseUrl + "/greet-counter/$count"
+    def request = new Request.Builder().url(url).get().build()
+
+    when:
+    def response = client.newCall(request).execute()
+
+    then:
+    response.code() == 200
+    expectedResponseBodyStr == response.body().string()
+    assertTraces(TEST_WRITER, 1) {
+      trace(0, 1) {
+        span(0) {
+          operationName "netty.request"
+          spanType DDSpanTypes.HTTP_SERVER
+          tags {
+            "$Tags.COMPONENT.key" "netty"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_SERVER
+            "$DDTags.SPAN_TYPE" DDSpanTypes.HTTP_SERVER
+            "$Tags.PEER_HOSTNAME.key" "localhost"
+            "$Tags.PEER_PORT.key" Integer
+            "$Tags.HTTP_METHOD.key" "GET"
+            "$Tags.HTTP_STATUS.key" 200
+            "$Tags.HTTP_URL.key" url
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+}
