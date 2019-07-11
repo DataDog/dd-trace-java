89ee512bdf6fa9bc952ae3911a4173832badfb04
Nikolay Martynov <mar.kolya@gmail.com>
2019-07-11 13:44:05 -0400

    Disable JFR events on java 9-10
    Using them in premain causes segfault on macos on java9 and 10.
    Since these versions are not LTS we can just disable those events to
    avoid crashes.

--- dd-trace-ot/jfr-openjdk/src/main/java/datadog/opentracing/jfr/openjdk/SpanEventFactory.java
+++ dd-trace-ot/jfr-openjdk/src/main/java/datadog/opentracing/jfr/openjdk/SpanEventFactory.java
@@ -7,10 +7,15 @@ import datadog.opentracing.jfr.DDSpanEventFactory;
 /** Event factory for {@link SpanEvent} */
 public class SpanEventFactory implements DDSpanEventFactory {
 
-  // This is needed to ensure SpanEven class is loaded when SpanEventFactory is loaded
-  // Loading SpanEven is important because it also loads JFR classes - which may not be present on
+  // This is needed to ensure SpanEvent class is loaded when SpanEventFactory is loaded
+  // Loading SpanEvent is important because it also loads JFR classes - which may not be present on
   // some JVMs
-  private static final Class<?> EVENT_CLASS = SpanEvent.class;
+  private final Class<?> eventClass;
+
+  public SpanEventFactory() throws ClassNotFoundException {
+    BlackList.checkBlackList();
+    eventClass = Class.forName("datadog.opentracing.jfr.openjdk.SpanEvent");
+  }
 
   @Override
   public DDSpanEvent create(final DDSpanContext context) {
