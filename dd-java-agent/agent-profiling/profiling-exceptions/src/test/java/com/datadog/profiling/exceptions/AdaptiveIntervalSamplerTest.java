47ec13cc935aa67fe06f6b33a1d52706701fca16
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-03-10 19:01:38 +0100

    Disable AdaptiveIntervalSamplerTest in CI

--- dd-java-agent/agent-profiling/profiling-exceptions/src/test/java/com/datadog/profiling/exceptions/AdaptiveIntervalSamplerTest.java
+++ dd-java-agent/agent-profiling/profiling-exceptions/src/test/java/com/datadog/profiling/exceptions/AdaptiveIntervalSamplerTest.java
@@ -5,10 +5,13 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.LockSupport;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+// disabled unless a system property is specified due to being timing sensitive and unstable in CI
+@EnabledIfSystemProperty(named = "enable.timeSensitive", matches = "true")
 public class AdaptiveIntervalSamplerTest {
   @ParameterizedTest
   @MethodSource("samplerParams")
