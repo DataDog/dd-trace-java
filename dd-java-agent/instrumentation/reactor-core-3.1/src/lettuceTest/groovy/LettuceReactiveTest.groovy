67e7cd3892ace0ee63e2eb85cf4e0609255a8f3b
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2020-03-10 10:57:21 -0400

    use embedded redis instead of locally running one

--- dd-java-agent/instrumentation/reactor-core-3.1/src/lettuceTest/groovy/LettuceReactiveTest.groovy
+++ dd-java-agent/instrumentation/reactor-core-3.1/src/lettuceTest/groovy/LettuceReactiveTest.groovy
@@ -1,18 +1,61 @@
 import datadog.trace.agent.test.AgentTestRunner
+import datadog.trace.agent.test.utils.PortUtils
 import datadog.trace.agent.test.utils.TraceUtils
+import io.lettuce.core.ClientOptions
 import io.lettuce.core.RedisClient
-import io.lettuce.core.api.StatefulRedisConnection
-import io.lettuce.core.api.reactive.RedisStringReactiveCommands
+import io.lettuce.core.api.StatefulConnection
+import io.lettuce.core.api.reactive.RedisReactiveCommands
 import reactor.core.scheduler.Schedulers
+import redis.embedded.RedisServer
+import spock.lang.Shared
 
 class LettuceReactiveTest extends AgentTestRunner {
+  public static final String HOST = "127.0.0.1"
+  public static final int DB_INDEX = 0
+  // Disable autoreconnect so we do not get stray traces popping up on server shutdown
+  public static final ClientOptions CLIENT_OPTIONS = ClientOptions.builder().autoReconnect(false).build()
 
-  RedisStringReactiveCommands<String, String> reactive
+  @Shared
+  String embeddedDbUri
+
+  @Shared
+  RedisServer redisServer
+
+  RedisClient redisClient
+  StatefulConnection connection
+  RedisReactiveCommands<String, ?> reactive
+
+  def setupSpec() {
+    int port = PortUtils.randomOpenPort()
+    String dbAddr = HOST + ":" + port + "/" + DB_INDEX
+    embeddedDbUri = "redis://" + dbAddr
+
+    redisServer = RedisServer.builder()
+    // bind to localhost to avoid firewall popup
+      .setting("bind " + HOST)
+    // set max memory to avoid problems in CI
+      .setting("maxmemory 128M")
+      .port(port).build()
+  }
 
   def setup() {
-    RedisClient client = RedisClient.create("redis://localhost:6379")
-    StatefulRedisConnection<String, String> connection = client.connect()
+    redisClient = RedisClient.create(embeddedDbUri)
+
+    println "Using redis: $redisServer.args"
+    redisServer.start()
+    redisClient.setOptions(CLIENT_OPTIONS)
+    connection = redisClient.connect()
+
     reactive = connection.reactive()
+    reactive.set("test", "test").block()
+
+    TEST_WRITER.waitForTraces(2)
+    TEST_WRITER.clear()
+  }
+
+  def cleanup() {
+    connection.close()
+    redisServer.stop()
   }
 
   def "blocking subscriber"() {
@@ -22,12 +65,9 @@ class LettuceReactiveTest extends AgentTestRunner {
         .then(reactive.get("a")) // The get here is ending up in another trace
         .block()
     }
-    TEST_WRITER.waitForTraces(2)
+    TEST_WRITER.waitForTraces(1)
 
     def traces = TEST_WRITER.collect()
-    traces.removeAll {
-      it[0].resourceName.startsWith("CONNECT")
-    }
 
     then:
     traces.size() == 1
@@ -41,12 +81,9 @@ class LettuceReactiveTest extends AgentTestRunner {
         .then(reactive.get("a")) // The get here is ending up in another trace
         .subscribe()
     }
-    TEST_WRITER.waitForTraces(2)
+    TEST_WRITER.waitForTraces(1)
 
     def traces = TEST_WRITER.collect()
-    traces.removeAll {
-      it[0].resourceName.startsWith("CONNECT")
-    }
 
     then:
     traces.size() == 1
@@ -61,12 +98,9 @@ class LettuceReactiveTest extends AgentTestRunner {
         .subscribeOn(Schedulers.elastic())
         .subscribe()
     }
-    TEST_WRITER.waitForTraces(2)
+    TEST_WRITER.waitForTraces(1)
 
     def traces = TEST_WRITER.collect()
-    traces.removeAll {
-      it[0].resourceName.startsWith("CONNECT")
-    }
 
     then:
     traces.size() == 1
