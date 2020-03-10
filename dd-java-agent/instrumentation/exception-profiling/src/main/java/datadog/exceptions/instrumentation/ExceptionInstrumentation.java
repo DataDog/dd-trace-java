048bcb514bd72f8dc2927fe6026cfc18a343e061
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-03-10 18:36:27 +0100

    Formatting changes

--- dd-java-agent/instrumentation/exception-profiling/src/main/java/datadog/exceptions/instrumentation/ExceptionInstrumentation.java
+++ dd-java-agent/instrumentation/exception-profiling/src/main/java/datadog/exceptions/instrumentation/ExceptionInstrumentation.java
@@ -15,10 +15,10 @@ import net.bytebuddy.matcher.ElementMatchers;
 @AutoService(Instrumenter.class)
 @Slf4j
 /**
- * Provides instrumentation of {@linkplain Exception} constructor.
- * <br>
- * {@linkplain Exception}, as opposed to {@linkplain Throwable} was deliberately chosen such that we don't instrument
- * {@linkplain Error} class/subclasses since they are tracked by a native JFR event already.
+ * Provides instrumentation of {@linkplain Exception} constructor. <br>
+ * {@linkplain Exception}, as opposed to {@linkplain Throwable} was deliberately chosen such that we
+ * don't instrument {@linkplain Error} class/subclasses since they are tracked by a native JFR event
+ * already.
  */
 public class ExceptionInstrumentation extends Instrumenter.Default {
   private final boolean hasJfr;
@@ -27,8 +27,10 @@ public class ExceptionInstrumentation extends Instrumenter.Default {
     super("exceptions");
     boolean jfr = false;
     try {
-      // check only for the open-sources JFR implementation
-      // if it is ever needed to support also the closed sourced JDK 8 version the check should be enhanced
+      /* Check only for the open-sources JFR implementation.
+       * If it is ever needed to support also the closed sourced JDK 8 version the check should be
+       * enhanced
+       */
       Class.forName("jdk.jfr.Event");
       jfr = true;
     } catch (ClassNotFoundException ignored) {
@@ -47,18 +49,20 @@ public class ExceptionInstrumentation extends Instrumenter.Default {
      * Since the only instrumentation target is java.lang.Exception which is loaded by bootstrap classloader
      * it is ok to use helper classes instead of hacking around a Java 8 specific bootstrap.
      */
-    return hasJfr ? new String[] {
-      "com.datadog.profiling.exceptions.AdaptiveIntervalSampler",
-      "com.datadog.profiling.exceptions.ExceptionCountEvent",
-      "com.datadog.profiling.exceptions.ExceptionHistogram",
-      "com.datadog.profiling.exceptions.ExceptionHistogram$1",
-      "com.datadog.profiling.exceptions.ExceptionHistogram$ValueVisitor",
-      "com.datadog.profiling.exceptions.ExceptionProfiling",
-      "com.datadog.profiling.exceptions.ExceptionProfiling$1",
-      "com.datadog.profiling.exceptions.ExceptionProfiling$Singleton",
-      "com.datadog.profiling.exceptions.ExceptionSampleEvent",
-      "com.datadog.profiling.exceptions.ExceptionSampler"
-    } : new String[0];
+    return hasJfr
+        ? new String[] {
+          "com.datadog.profiling.exceptions.AdaptiveIntervalSampler",
+          "com.datadog.profiling.exceptions.ExceptionCountEvent",
+          "com.datadog.profiling.exceptions.ExceptionHistogram",
+          "com.datadog.profiling.exceptions.ExceptionHistogram$1",
+          "com.datadog.profiling.exceptions.ExceptionHistogram$ValueVisitor",
+          "com.datadog.profiling.exceptions.ExceptionProfiling",
+          "com.datadog.profiling.exceptions.ExceptionProfiling$1",
+          "com.datadog.profiling.exceptions.ExceptionProfiling$Singleton",
+          "com.datadog.profiling.exceptions.ExceptionSampleEvent",
+          "com.datadog.profiling.exceptions.ExceptionSampler"
+        }
+        : new String[0];
   }
 
   @Override
@@ -74,7 +78,7 @@ public class ExceptionInstrumentation extends Instrumenter.Default {
   public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
     if (hasJfr) {
       return Collections.singletonMap(
-        isConstructor(), "datadog.exceptions.instrumentation.ExceptionAdvice");
+          isConstructor(), "datadog.exceptions.instrumentation.ExceptionAdvice");
     }
     return Collections.emptyMap();
   }
