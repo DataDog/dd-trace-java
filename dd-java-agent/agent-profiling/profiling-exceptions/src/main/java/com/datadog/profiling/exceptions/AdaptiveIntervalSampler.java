4f1e3a8996bbd2200b16700c56bd8dc39da1833c
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-03-11 10:34:53 +0100

    Impose hard limit on number of samples

--- dd-java-agent/agent-profiling/profiling-exceptions/src/main/java/com/datadog/profiling/exceptions/AdaptiveIntervalSampler.java
+++ dd-java-agent/agent-profiling/profiling-exceptions/src/main/java/com/datadog/profiling/exceptions/AdaptiveIntervalSampler.java
@@ -1,9 +1,10 @@
 package com.datadog.profiling.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public final class AdaptiveIntervalSampler {
@@ -13,67 +14,80 @@ public final class AdaptiveIntervalSampler {
   private long interval;
   private final long timeWindowNs;
   private final long maxSamples;
-  private volatile long ts = System.nanoTime();
+  private final long targetSamples;
   private volatile long counterTop = 0L;
   private final AtomicLong counter;
+  private long ts = System.nanoTime();
+  private long sampleCounter = 0L;
   private final int dummyMultiplier = 1;
 
-  public AdaptiveIntervalSampler(String id, int minInterval, long timeWindowMs, long maxSamples) {
+  public AdaptiveIntervalSampler(final String id, final int minInterval, final long timeWindowMs, final long maxSamples) {
     this(id, minInterval, 50, timeWindowMs, maxSamples);
   }
 
   public AdaptiveIntervalSampler(
-      String id, int minInterval, int stdDevPercent, long timeWindowMs, long maxSamples) {
+    final String id, final int minInterval, final int stdDevPercent, final long timeWindowMs, final long maxSamples) {
     this.id = id;
     this.minInterval = minInterval;
-    this.stdDev = Math.max(Math.min(stdDevPercent, 100), 1) / 100d;
-    this.interval = minInterval;
-    this.timeWindowNs = TimeUnit.NANOSECONDS.convert(timeWindowMs, TimeUnit.MILLISECONDS);
+    stdDev = Math.max(Math.min(stdDevPercent, 100), 1) / 100d;
+    interval = minInterval;
+    timeWindowNs = TimeUnit.NANOSECONDS.convert(timeWindowMs, TimeUnit.MILLISECONDS);
+    this.maxSamples = maxSamples;
     // for small minimal intervals the sampler can overshoot significantly; trying to compensate for
     // that here
-    this.maxSamples = Math.round(maxSamples / (Math.max(1, 4 - Math.log10(minInterval))));
-    this.counterTop = expectedHits(interval);
-    this.counter = new AtomicLong(counterTop);
+    targetSamples = Math.round(maxSamples / (Math.max(1, 3 - Math.log10(minInterval))));
+    counterTop = expectedHits(interval);
+    counter = new AtomicLong(counterTop);
   }
 
-  private long expectedHits(long interval) {
+  private long expectedHits(final long interval) {
     return Math.max(
-        Math.round(ThreadLocalRandom.current().nextGaussian() * interval * stdDev) + interval, 1);
+      Math.round(ThreadLocalRandom.current().nextGaussian() * interval * stdDev) + interval, 1);
   }
 
   public void reset() {
     log.debug("Sampler reset [id: {}]", id);
     interval = minInterval;
-    // write to volatile 'ts' to force the visibility of change to 'interval'
-    ts = ts * dummyMultiplier;
+    sampleCounter = 0L;
+    // write to volatile 'counterTop' to force the visibility of change to 'interval'
+    counterTop = counterTop * dummyMultiplier;
   }
 
   public boolean sample() {
-    long currentCnt = counter.decrementAndGet();
+    /*
+     * All changes done by other threads before writing to 'counterTop' will be visible after this
+     * line
+     */
+    long currentTop = counterTop;
+    if (sampleCounter >= maxSamples) {
+      // hit the hard limit; no more samples
+      return false;
+    }
+    final long currentCnt = counter.decrementAndGet();
 
-    // all changes done by other threads before writing to 'counterTop' will be visible after this
-    // line
-    if (currentCnt % counterTop == 0L) {
-      long ts1 = System.nanoTime();
+    if (currentCnt % currentTop == 0L) {
+      final long ts1 = System.nanoTime();
       // all changes done by other threads before writing to 'ts' will be visible after this line
-      long tDiff = ts1 - ts;
-      double projectedSamplesCnt = timeWindowNs / (double) tDiff;
-      double intervalScale = projectedSamplesCnt / maxSamples;
-      long newInterval = Math.max(Math.round(interval * intervalScale), minInterval);
+      final long tDiff = ts1 - ts;
+      final double projectedSamplesCnt = timeWindowNs / (double) tDiff;
+      final double intervalScale = projectedSamplesCnt / maxSamples;
+      final long newInterval = Math.max(Math.round(interval * intervalScale), minInterval);
       // calculate interval diff in a way it does not swing wildly
-      long intervalDiff =
-          newInterval == interval
-              ? 0
-              : Math.round(
-                  Math.log(Math.abs(newInterval - interval)) * (newInterval < interval ? -1 : 1));
+      final long intervalDiff =
+        newInterval == interval
+          ? 0
+          : Math.round(
+          Math.log(Math.abs(newInterval - interval)) * (newInterval < interval ? -1 : 1));
       interval = interval + intervalDiff;
       // write to counterTop makes all previous changes to shared data visible after counterTop is
       // read by other threads
-      counterTop = expectedHits(interval);
+      currentTop = expectedHits(interval);
       counter.addAndGet(counterTop);
-      // write to ts makes all previous changes to shared data visible after ts is read by other
-      // threads
+      sampleCounter++;
       ts = ts1;
+      // write to 'counterTop' makes all previous changes to shared data visible after ts is read by other
+      // threads
+      counterTop = currentTop;
 
       return true;
     }
