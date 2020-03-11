33359432ff42293c7aea81ea1bafba993bf021f2
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-03-11 10:34:53 +0100

    Impose hard limit on number of samples

--- dd-java-agent/agent-profiling/profiling-exceptions/src/test/java/com/datadog/profiling/exceptions/AdaptiveIntervalSamplerTest.java
+++ dd-java-agent/agent-profiling/profiling-exceptions/src/test/java/com/datadog/profiling/exceptions/AdaptiveIntervalSamplerTest.java
@@ -1,77 +1,86 @@
 package com.datadog.profiling.exceptions;
 
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.locks.LockSupport;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.LockSupport;
+import java.util.stream.Stream;
+
 // disabled unless a system property is specified due to being timing sensitive and unstable in CI
-@EnabledIfSystemProperty(named = "enable.timeSensitive", matches = "true")
+//@EnabledIfSystemProperty(named = "enable.timeSensitive", matches = "true")
 public class AdaptiveIntervalSamplerTest {
   @ParameterizedTest
   @MethodSource("samplerParams")
-  public void sampleConcurrent(int threadCnt, int minInterval, long timeWindowMs, long maxSamples)
-      throws Exception {
-    AdaptiveIntervalSampler instance =
-        new AdaptiveIntervalSampler("test", minInterval, timeWindowMs, maxSamples);
+  public void sampleConcurrent(final int threadCnt, final int minInterval, final long timeWindowMs, final long maxSamples)
+    throws Exception {
+    final AdaptiveIntervalSampler instance =
+      new AdaptiveIntervalSampler("test", minInterval, timeWindowMs, maxSamples);
 
-    AtomicLong allCnt = new AtomicLong(0);
-    long latency = 1_000L;
-    Thread[] threads = new Thread[threadCnt];
-    long hits = (timeWindowMs * (1_000_000L / latency));
+    final AtomicLong allCnt = new AtomicLong(0);
+    final long latency = 1_000L;
+    final Thread[] threads = new Thread[threadCnt];
+    final long hits = (timeWindowMs * (1_000_000L / latency));
     for (int j = 0; j < threads.length; j++) {
       threads[j] =
-          new Thread(
-              () -> {
-                Random rnd = new Random();
-                int cnt = 0;
-                for (long i = 0; i < hits / threads.length; i++) {
-                  cnt += instance.sample() ? 1 : 0;
-                  LockSupport.parkNanos(rnd.nextInt((int) latency) + 100);
-                }
+        new Thread(
+          () -> {
+            final Random rnd = new Random();
+            int cnt = 0;
+            for (long i = 0; i < hits / threads.length; i++) {
+              cnt += instance.sample() ? 1 : 0;
+              LockSupport.parkNanos(rnd.nextInt((int) latency) + 100);
+            }
 
-                allCnt.addAndGet(cnt);
-              });
+            allCnt.addAndGet(cnt);
+          });
       threads[j].start();
     }
     for (int j = 0; j < threads.length; j++) {
       threads[j].join();
     }
-    long cnt = allCnt.get();
+    final long cnt = allCnt.get();
+    final long diff = cnt - maxSamples;
+    System.out.println("threads: " + threadCnt + ", max samples: " + maxSamples + ", samples: " + cnt);
+    // diff not larger than 10%
     Assertions.assertTrue(
-        cnt <= maxSamples * 2, "[" + threadCnt + "]: " + cnt + " <= " + (maxSamples * 2));
+      diff <= maxSamples * 0.1, "[" + threadCnt + "]: " + cnt + " <= " + maxSamples);
   }
 
   private static Stream<Arguments> samplerParams() {
     return Stream.of(
-        Arguments.of(1, 1, 100L, 500L),
-        Arguments.of(1, 1, 100L, 2000L),
-        Arguments.of(1, 10, 100L, 500L),
-        Arguments.of(1, 10, 100L, 2000L),
-        Arguments.of(1, 100, 100L, 500L),
-        Arguments.of(1, 100, 100L, 2000L),
-        Arguments.of(2, 1, 100L, 500L),
-        Arguments.of(2, 1, 100L, 2000L),
-        Arguments.of(2, 10, 100L, 500L),
-        Arguments.of(2, 10, 100L, 2000L),
-        Arguments.of(2, 100, 100L, 500L),
-        Arguments.of(2, 100, 100L, 2000L),
-        Arguments.of(4, 1, 100L, 500L),
-        Arguments.of(4, 1, 100L, 2000L),
-        Arguments.of(4, 10, 100L, 500L),
-        Arguments.of(4, 10, 100L, 2000L),
-        Arguments.of(4, 100, 100L, 500L),
-        Arguments.of(4, 100, 100L, 2000L),
-        Arguments.of(16, 1, 100L, 500L),
-        Arguments.of(16, 1, 100L, 2000L),
-        Arguments.of(16, 10, 100L, 500L),
-        Arguments.of(16, 10, 100L, 2000L),
-        Arguments.of(16, 100, 100L, 500L),
-        Arguments.of(16, 100, 100L, 2000L));
+      Arguments.of(1, 1, 100L, 500L),
+      Arguments.of(1, 1, 100L, 2000L),
+      Arguments.of(1, 10, 100L, 500L),
+      Arguments.of(1, 10, 100L, 2000L),
+      Arguments.of(1, 100, 100L, 500L),
+      Arguments.of(1, 100, 100L, 2000L),
+      Arguments.of(2, 1, 100L, 500L),
+      Arguments.of(2, 1, 100L, 2000L),
+      Arguments.of(2, 10, 100L, 500L),
+      Arguments.of(2, 10, 100L, 2000L),
+      Arguments.of(2, 100, 100L, 500L),
+      Arguments.of(2, 100, 100L, 2000L),
+      Arguments.of(4, 1, 100L, 500L),
+      Arguments.of(4, 1, 100L, 2000L),
+      Arguments.of(4, 10, 100L, 500L),
+      Arguments.of(4, 10, 100L, 2000L),
+      Arguments.of(4, 100, 100L, 500L),
+      Arguments.of(4, 100, 100L, 2000L),
+      Arguments.of(16, 1, 100L, 500L),
+      Arguments.of(16, 1, 100L, 2000L),
+      Arguments.of(16, 10, 100L, 500L),
+      Arguments.of(16, 10, 100L, 2000L),
+      Arguments.of(16, 100, 100L, 500L),
+      Arguments.of(16, 100, 100L, 2000L),
+      Arguments.of(128, 1, 100L, 500L),
+      Arguments.of(128, 1, 100L, 2000L),
+      Arguments.of(128, 10, 100L, 500L),
+      Arguments.of(128, 10, 100L, 2000L),
+      Arguments.of(128, 100, 100L, 500L),
+      Arguments.of(128, 100, 100L, 2000L));
   }
 }
