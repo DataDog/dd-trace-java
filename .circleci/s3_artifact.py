302fffdc6819bdb7803f295761546dd644d00ef2
Amine.Mesbahi <amine.mesbahi@datadoghq.com>
2020-03-23 14:08:45 +0100

    test copy s3

--- .circleci/s3_artifact.py
+++ .circleci/s3_artifact.py
@@ -1,5 +1,7 @@
 import os
+import boto3
 
-for path, subdirs, files in os.walk('./workspace'):
+for path, subdirs, files in os.walk('./workspace/dd-java-agent/build/libs'):
     for name in files:
+      if 'dd-java-agent-' in name
         print(os.path.join(path, name))
