69071a3b00f74aaf1430ea2ab64baec4761b3a67
Amine.Mesbahi <amine.mesbahi@datadoghq.com>
2020-03-23 14:53:05 +0100

    test copy s3

--- .circleci/s3_artifact.py
+++ .circleci/s3_artifact.py
@@ -1,7 +1,10 @@
 import os
 import boto3
 
+client = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+transfer = boto3.s3.transfer.S3Transfer(client)
+
 for path, subdirs, files in os.walk('./workspace/dd-java-agent/build/libs'):
     for name in files:
       if 'dd-java-agent-' in name:
-        print(os.path.join(path, name))
+        transfer.upload_file(os.path.join(path, name), 'datadog-reliability-env', f'test/{name}')
