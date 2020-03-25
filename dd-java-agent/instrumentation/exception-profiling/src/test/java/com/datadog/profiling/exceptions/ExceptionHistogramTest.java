b9e03f5335d737f95c7504170f66f6e927b87df8
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-03-25 18:58:07 +0100

    Try to fix test dependencies

--- dd-java-agent/instrumentation/exception-profiling/src/test/java/com/datadog/profiling/exceptions/ExceptionHistogramTest.java
+++ dd-java-agent/instrumentation/exception-profiling/src/test/java/com/datadog/profiling/exceptions/ExceptionHistogramTest.java
@@ -1,15 +1,11 @@
 package com.datadog.profiling.exceptions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSortedMap;
 import datadog.trace.api.Config;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Properties;
 import jdk.jfr.FlightRecorder;
 import jdk.jfr.Recording;
 import org.junit.jupiter.api.AfterEach;
@@ -25,25 +21,31 @@ import org.openjdk.jmc.common.unit.UnitLookup;
 import org.openjdk.jmc.flightrecorder.CouldNotLoadRecordingException;
 import org.openjdk.jmc.flightrecorder.JfrLoaderToolkit;
 
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Properties;
+
 public class ExceptionHistogramTest {
 
   private static final IAttribute<String> TYPE =
-      Attribute.attr("type", "type", "Exception type", UnitLookup.PLAIN_TEXT);
+    Attribute.attr("type", "type", "Exception type", UnitLookup.PLAIN_TEXT);
   private static final IAttribute<IQuantity> COUNT =
-      Attribute.attr("count", "count", "Exception count", UnitLookup.NUMBER);
+    Attribute.attr("count", "count", "Exception count", UnitLookup.NUMBER);
 
   private static final Comparator<Exception> EXCEPTION_COMPARATOR =
-      new Comparator<Exception>() {
-        @Override
-        public int compare(final Exception e1, final Exception e2) {
-          return e1.getClass().getCanonicalName().compareTo(e2.getClass().getCanonicalName());
-        }
+    new Comparator<Exception>() {
+      @Override
+      public int compare(final Exception e1, final Exception e2) {
+        return e1.getClass().getCanonicalName().compareTo(e2.getClass().getCanonicalName());
+      }
 
-        @Override
-        public boolean equals(final Object obj) {
-          return this == obj;
-        }
-      };
+      @Override
+      public boolean equals(final Object obj) {
+        return this == obj;
+      }
+    };
 
   private static final int MAX_ITEMS = 2;
   private static final int MAX_SIZE = 2;
@@ -60,7 +62,7 @@ public class ExceptionHistogramTest {
 
     final Properties properties = new Properties();
     properties.setProperty(
-        Config.PROFILING_EXCEPTION_HISTOGRAM_TOP_ITEMS, Integer.toString(MAX_ITEMS));
+      Config.PROFILING_EXCEPTION_HISTOGRAM_TOP_ITEMS, Integer.toString(MAX_ITEMS));
 
     instance = new ExceptionHistogram(Config.get(properties));
   }
@@ -76,15 +78,15 @@ public class ExceptionHistogramTest {
 
   @Test
   public void testExceptionsRecorded()
-      throws IOException, CouldNotLoadRecordingException, InterruptedException {
+    throws IOException, CouldNotLoadRecordingException, InterruptedException {
     writeExceptions(
-        ImmutableMap.of(
-            new NullPointerException(),
-            8,
-            new IllegalArgumentException(),
-            5,
-            new RuntimeException(),
-            1));
+      ImmutableMap.of(
+        new NullPointerException(),
+        8,
+        new IllegalArgumentException(),
+        5,
+        new RuntimeException(),
+        1));
 
     final Instant firstRecordingNow = Instant.now();
     snapshot = FlightRecorder.getFlightRecorder().takeSnapshot();
@@ -92,74 +94,74 @@ public class ExceptionHistogramTest {
 
     assertEquals(MAX_ITEMS, firstRecording.getAggregate(Aggregators.count()).longValue());
     assertEquals(
-        8,
-        firstRecording
-            .apply(ItemFilters.equals(TYPE, NullPointerException.class.getCanonicalName()))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      8,
+      firstRecording
+        .apply(ItemFilters.equals(TYPE, NullPointerException.class.getCanonicalName()))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     assertEquals(
-        5,
-        firstRecording
-            .apply(ItemFilters.equals(TYPE, IllegalArgumentException.class.getCanonicalName()))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      5,
+      firstRecording
+        .apply(ItemFilters.equals(TYPE, IllegalArgumentException.class.getCanonicalName()))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     snapshot.close();
 
     // Sleep to make sure we get new batch of exceptions only
     Thread.sleep(1000);
 
     writeExceptions(
-        ImmutableMap.of(
-            new RuntimeException(),
-            8,
-            new NullPointerException(),
-            5,
-            new IllegalArgumentException(),
-            1));
+      ImmutableMap.of(
+        new RuntimeException(),
+        8,
+        new NullPointerException(),
+        5,
+        new IllegalArgumentException(),
+        1));
 
     snapshot = FlightRecorder.getFlightRecorder().takeSnapshot();
     final IItemCollection secondRecording =
-        getEvents(snapshot, firstRecordingNow.plusMillis(1000), Instant.MAX);
+      getEvents(snapshot, firstRecordingNow.plusMillis(1000), Instant.MAX);
 
     assertEquals(MAX_ITEMS, secondRecording.getAggregate(Aggregators.count()).longValue());
     assertEquals(
-        8,
-        secondRecording
-            .apply(ItemFilters.equals(TYPE, RuntimeException.class.getCanonicalName()))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      8,
+      secondRecording
+        .apply(ItemFilters.equals(TYPE, RuntimeException.class.getCanonicalName()))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     assertEquals(
-        5,
-        secondRecording
-            .apply(ItemFilters.equals(TYPE, NullPointerException.class.getCanonicalName()))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      5,
+      secondRecording
+        .apply(ItemFilters.equals(TYPE, NullPointerException.class.getCanonicalName()))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     snapshot.close();
   }
 
   @Test
   public void testHistogramSizeIsLimited()
-      throws IOException, CouldNotLoadRecordingException, InterruptedException {
+    throws IOException, CouldNotLoadRecordingException, InterruptedException {
     instance.deregister();
     final Properties properties = new Properties();
     properties.setProperty(
-        Config.PROFILING_EXCEPTION_HISTOGRAM_MAX_COLLECTION_SIZE, Integer.toString(MAX_SIZE));
+      Config.PROFILING_EXCEPTION_HISTOGRAM_MAX_COLLECTION_SIZE, Integer.toString(MAX_SIZE));
 
     instance = new ExceptionHistogram(Config.get(properties));
 
     // Exceptions are written in alphabetical order
     writeExceptions(
-        ImmutableSortedMap.copyOf(
-            ImmutableMap.of(
-                new Exception(),
-                5,
-                new IllegalArgumentException(),
-                8,
-                new NegativeArraySizeException(),
-                10,
-                new NullPointerException(),
-                11),
-            EXCEPTION_COMPARATOR));
+      ImmutableSortedMap.copyOf(
+        ImmutableMap.of(
+          new Exception(),
+          5,
+          new IllegalArgumentException(),
+          8,
+          new NegativeArraySizeException(),
+          10,
+          new NullPointerException(),
+          11),
+        EXCEPTION_COMPARATOR));
 
     final Instant firstRecordingNow = Instant.now();
     snapshot = FlightRecorder.getFlightRecorder().takeSnapshot();
@@ -167,23 +169,23 @@ public class ExceptionHistogramTest {
 
     assertEquals(MAX_ITEMS + 1, firstRecording.getAggregate(Aggregators.count()).longValue());
     assertEquals(
-        5,
-        firstRecording
-            .apply(ItemFilters.equals(TYPE, Exception.class.getCanonicalName()))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      5,
+      firstRecording
+        .apply(ItemFilters.equals(TYPE, Exception.class.getCanonicalName()))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     assertEquals(
-        8,
-        firstRecording
-            .apply(ItemFilters.equals(TYPE, IllegalArgumentException.class.getCanonicalName()))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      8,
+      firstRecording
+        .apply(ItemFilters.equals(TYPE, IllegalArgumentException.class.getCanonicalName()))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     assertEquals(
-        21,
-        firstRecording
-            .apply(ItemFilters.equals(TYPE, ExceptionHistogram.CLIPPED_ENTRY_TYPE_NAME))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      21,
+      firstRecording
+        .apply(ItemFilters.equals(TYPE, ExceptionHistogram.CLIPPED_ENTRY_TYPE_NAME))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     snapshot.close();
 
     // Sleep to make sure we get new batch of exceptions only
@@ -191,41 +193,41 @@ public class ExceptionHistogramTest {
 
     // Exceptions are written in 'code' order
     writeExceptions(
-        ImmutableSortedMap.copyOf(
-            ImmutableMap.of(
-                new IllegalArgumentException(),
-                5,
-                new NegativeArraySizeException(),
-                8,
-                new NullPointerException(),
-                10,
-                new RuntimeException(),
-                11),
-            EXCEPTION_COMPARATOR));
+      ImmutableSortedMap.copyOf(
+        ImmutableMap.of(
+          new IllegalArgumentException(),
+          5,
+          new NegativeArraySizeException(),
+          8,
+          new NullPointerException(),
+          10,
+          new RuntimeException(),
+          11),
+        EXCEPTION_COMPARATOR));
 
     snapshot = FlightRecorder.getFlightRecorder().takeSnapshot();
     final IItemCollection secondRecording =
-        getEvents(snapshot, firstRecordingNow.plusMillis(1000), Instant.MAX);
+      getEvents(snapshot, firstRecordingNow.plusMillis(1000), Instant.MAX);
 
     assertEquals(MAX_ITEMS + 1, secondRecording.getAggregate(Aggregators.count()).longValue());
     assertEquals(
-        5,
-        secondRecording
-            .apply(ItemFilters.equals(TYPE, IllegalArgumentException.class.getCanonicalName()))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      5,
+      secondRecording
+        .apply(ItemFilters.equals(TYPE, IllegalArgumentException.class.getCanonicalName()))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     assertEquals(
-        8,
-        secondRecording
-            .apply(ItemFilters.equals(TYPE, NegativeArraySizeException.class.getCanonicalName()))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      8,
+      secondRecording
+        .apply(ItemFilters.equals(TYPE, NegativeArraySizeException.class.getCanonicalName()))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     assertEquals(
-        21,
-        firstRecording
-            .apply(ItemFilters.equals(TYPE, ExceptionHistogram.CLIPPED_ENTRY_TYPE_NAME))
-            .getAggregate(Aggregators.sum(COUNT))
-            .longValue());
+      21,
+      firstRecording
+        .apply(ItemFilters.equals(TYPE, ExceptionHistogram.CLIPPED_ENTRY_TYPE_NAME))
+        .getAggregate(Aggregators.sum(COUNT))
+        .longValue());
     snapshot.close();
   }
 
@@ -233,13 +235,13 @@ public class ExceptionHistogramTest {
   public void testDisabled() throws IOException, CouldNotLoadRecordingException {
     recording.disable("datadog.ExceptionCount");
     final Map<Exception, Integer> exceptions =
-        ImmutableMap.of(
-            new NullPointerException(),
-            8,
-            new IllegalArgumentException(),
-            5,
-            new RuntimeException(),
-            1);
+      ImmutableMap.of(
+        new NullPointerException(),
+        8,
+        new IllegalArgumentException(),
+        5,
+        new RuntimeException(),
+        1);
 
     for (final Map.Entry<Exception, Integer> entry : exceptions.entrySet()) {
       for (int i = 0; i < entry.getValue(); i++) {
@@ -256,10 +258,10 @@ public class ExceptionHistogramTest {
   }
 
   private IItemCollection getEvents(
-      final Recording secondSnapshot, final Instant start, final Instant end)
-      throws IOException, CouldNotLoadRecordingException {
+    final Recording secondSnapshot, final Instant start, final Instant end)
+    throws IOException, CouldNotLoadRecordingException {
     return JfrLoaderToolkit.loadEvents(secondSnapshot.getStream(start, end))
-        .apply(ItemFilters.type("datadog.ExceptionCount"));
+      .apply(ItemFilters.type("datadog.ExceptionCount"));
   }
 
   private void writeExceptions(final Map<Exception, Integer> exceptions) {
