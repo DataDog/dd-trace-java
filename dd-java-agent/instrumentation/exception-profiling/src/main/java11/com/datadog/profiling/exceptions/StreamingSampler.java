350d054bc52b591003a477e7236c3174e4ac7f30
Nikolay Martynov <nikolay.martynov@datadoghq.com>
2020-04-03 21:22:11 -0400

    Fix some problems with exception sampler
    * Using EMA for running budget is wrong since we need sum, not average
    * Check window duration after locking to avoid 'double close'
    * Sample after window close to avoid sampling 'late'

--- dd-java-agent/instrumentation/exception-profiling/src/main/java11/com/datadog/profiling/exceptions/StreamingSampler.java
+++ dd-java-agent/instrumentation/exception-profiling/src/main/java11/com/datadog/profiling/exceptions/StreamingSampler.java
@@ -1,7 +1,8 @@
 package com.datadog.profiling.exceptions;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static java.lang.Double.isNaN;
+import static java.lang.Integer.max;
+import static java.lang.Math.min;
 
 import java.time.Duration;
 import java.util.concurrent.ThreadLocalRandom;
@@ -34,52 +35,46 @@ import java.util.function.Supplier;
  * </p>
  */
 class StreamingSampler {
-  private static final Logger LOGGER = LoggerFactory.getLogger(StreamingSampler.class);
+
+  /*
+   * We keep a 'budget' by counting number of unused samples form few last windows.
+   * Exact value here should be on order of (but probably less than) size of the JFR chunk duration.
+   * Another consideration to choosing this value is that we have to do linear number of operations
+   * on resulting array every window so this value should not be too large.
+   *
+   * Note: we want sum of unused samples in previous windows - this is why we are using array for this and not EMA.
+   * With EMA we would need to come up with some multiplier for and average and it's unclear how to do that.
+   */
+  private static final int CARRIED_OVER_ARRAY_SIZE = 16;
 
   /*
    * Exponential Moving Average (EMA) last element weight.
    * Check out papers about using EMA for streaming data - eg.
    * https://nestedsoftware.com/2018/04/04/exponential-moving-average-on-streaming-data-4hhl.24876.html
    *
-   * Corresponds to 'lookback' of N values:
-   * With T being the index of the most recent value the lookback of N values means that for all values with index
+   * Corresponds to 'lookback' of N windows:
+   * With T being the index of the most recent value the lookback of N windows means that for all values with index
    * T-K, where K > N, the relative weight of that value computed as (1 - alpha)^K is less or equal than the
    * weight assigned by a plain arithmetic average (= 1/N).
    */
-  private final double emaAlpha;
-  private final Supplier<Long> tsProvider;
-  private final long windowDurationNs;
+  private final double eventsPerWindowEmaAlpha;
+  private final Supplier<Long> timestampProvider;
+  private final long windowDuration;
   private final int samplesPerWindow;
 
   private final LongAdder testCounter = new LongAdder();
   private final AtomicLong sampledCounter = new AtomicLong(0L);
   private final ReentrantLock endOfWindowLock = new ReentrantLock();
-  private final AtomicLong extraBudget = new AtomicLong(0);
 
   // these attributes need to be volatile since they are accessed outside of the 'endOfWindowLock' guarded block
   private volatile double probability = 1d;
-  private volatile long nextWindowTs = 0L;
+  private volatile long samplesBudget = 0;
+  private volatile long nextWindowTimestamp = 0L;
 
   // these attributes are accessed solely under `endOfWindowLock`
-  private double totalCountRunningAverage = 0d;
-  private double avgBudget = Double.NaN;
-
-  /**
-   * Create a new sampler instance
-   *
-   * @param windowDuration   the sampling window duration
-   * @param samplesPerWindow the maximum number of samples in the sampling window
-   * @param lookback         the number of windows to consider in averaging the sampling rate
-   * @param tsProvider       timestamp provider
-   */
-  StreamingSampler(
-    final Duration windowDuration, final int samplesPerWindow, final int lookback, final Supplier<Long> tsProvider) {
-    this.tsProvider = tsProvider;
-    windowDurationNs = windowDuration.toNanos();
-    nextWindowTs = getNextWindowTs(tsProvider.get());
-    this.samplesPerWindow = samplesPerWindow;
-    this.emaAlpha = computeLookbackAlpha(lookback);
-  }
+  private double totalCountRunningAverage = Double.NaN;
+  private final long[] carriedOverSamples;
+  private int carriedOverSampleIndex = 0;
 
   /**
    * Create a new sampler instance
@@ -93,64 +88,96 @@ class StreamingSampler {
   }
 
   /**
+   * Create a new sampler instance
+   *
+   * @param windowDuration    the sampling window duration
+   * @param samplesPerWindow  the maximum number of samples in the sampling window
+   * @param lookback          the number of windows making significant contribution in averaging the sampling rate
+   * @param timestampProvider timestamp provider
+   */
+  StreamingSampler(
+    final Duration windowDuration, final int samplesPerWindow, final int lookback, final Supplier<Long> timestampProvider) {
+    this.timestampProvider = timestampProvider;
+    this.windowDuration = windowDuration.toNanos();
+    nextWindowTimestamp = getNextWindowTimestamp(timestampProvider.get());
+    this.samplesPerWindow = samplesPerWindow;
+    samplesBudget = samplesPerWindow;
+    eventsPerWindowEmaAlpha = computeLookbackAlpha(lookback);
+
+    carriedOverSamples = new long[max(min(CARRIED_OVER_ARRAY_SIZE, lookback), 1)];
+  }
+
+  /**
    * Provides binary answer whether the current event is to be sampled
    *
    * @return {@literal true} if the event should be sampled
    */
   final boolean sample() {
-    boolean sampled = false;
-    testCounter.increment();
-    if (sampledCounter.get() <= samplesPerWindow + extraBudget.get()) {
-      if (ThreadLocalRandom.current().nextDouble() < probability) {
-        sampledCounter.incrementAndGet();
-        sampled = true;
-      }
-    }
-
-    final long ts = tsProvider.get();
-    final long extraDuration = ts - nextWindowTs;
-    final boolean isExpired = extraDuration >= 0;
-
-    if (isExpired) {
+    final long now = timestampProvider.get();
+    if (now >= nextWindowTimestamp) {
       if (endOfWindowLock.tryLock()) {
         try {
-          final long totalCount = testCounter.sumThenReset();
-          final long sampledCount = sampledCounter.getAndSet(0);
+          if (now >= nextWindowTimestamp) {
+            final long totalCount = testCounter.sumThenReset();
+            final long sampledCount = sampledCounter.getAndSet(0);
 
-          long sampleBudget = samplesPerWindow - sampledCount;
-          avgBudget = Double.isNaN(avgBudget) ? sampleBudget : avgBudget + emaAlpha * (sampleBudget - avgBudget);
-          extraBudget.set(Math.round(Math.max(avgBudget, 0) * 1));
+            // Integer division rounds down, so this is number of windows that have passed
+            final long passedWindows = (now - nextWindowTimestamp) / windowDuration;
 
-          if (totalCountRunningAverage == 0) {
-            totalCountRunningAverage = totalCount;
-          } else {
-            totalCountRunningAverage = totalCountRunningAverage + emaAlpha * (totalCount - totalCountRunningAverage);
+            final long unusedSamplesCount = samplesPerWindow - sampledCount;
+            carriedOverSampleIndex = (carriedOverSampleIndex + 1) % carriedOverSamples.length;
+            carriedOverSamples[carriedOverSampleIndex] = unusedSamplesCount;
+
+            // Budget includes events in next window
+            // FIXME: Adding extra samples budget to calculate probability is questionable because
+            // we essentially make probability swing widely instead of being a stable value based upon long running average.
+            // But empirically this seems to produce better results.
+            long newSamplesBudget = samplesPerWindow;
+            for (int i = 0; i < carriedOverSamples.length; i++) {
+              newSamplesBudget += carriedOverSamples[i];
+            }
+            newSamplesBudget = Math.max(newSamplesBudget, 0);
+            samplesBudget = newSamplesBudget;
+
+            if (isNaN(totalCountRunningAverage)) {
+              totalCountRunningAverage = totalCount;
+            } else {
+              totalCountRunningAverage += eventsPerWindowEmaAlpha * (totalCount - totalCountRunningAverage);
+            }
+
+            if (totalCountRunningAverage <= 0) {
+              probability = 1;
+            } else {
+              probability = min(newSamplesBudget / totalCountRunningAverage, 1d);
+            }
+
+            // FIXME: handle skipped windows
+            nextWindowTimestamp = getNextWindowTimestamp(nextWindowTimestamp);
           }
-
-          if (totalCountRunningAverage <= 0) {
-            probability = 1;
-          } else {
-            probability =
-              Math.min(
-                (samplesPerWindow + avgBudget)
-                  / totalCountRunningAverage,
-                1d);
-          }
-          nextWindowTs = getNextWindowTs(nextWindowTs);
         } finally {
           endOfWindowLock.unlock();
         }
       }
     }
 
+    // Sample after window has been updated: this makes us relatively sure that window is not over yet
+    boolean sampled = false;
+    testCounter.increment();
+    if (sampledCounter.get() <= samplesBudget) {
+      if (ThreadLocalRandom.current().nextDouble() < probability) {
+        sampledCounter.incrementAndGet();
+        sampled = true;
+      }
+    }
+
     return sampled;
   }
 
-  private long getNextWindowTs(long currentWindowTs) {
-    return currentWindowTs + windowDurationNs;
+  private long getNextWindowTimestamp(final long currentWindowTimestamp) {
+    return currentWindowTimestamp + windowDuration;
   }
 
-  private static double computeLookbackAlpha(int lookback) {
+  private static double computeLookbackAlpha(final int lookback) {
     return 1 - Math.pow(lookback, -1d / lookback);
   }
 }
