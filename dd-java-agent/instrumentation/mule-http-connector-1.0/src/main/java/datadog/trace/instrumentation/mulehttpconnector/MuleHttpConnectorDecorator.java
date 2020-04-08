b940c76810ef0be348aa298f21febf71e8ce1737
heathkd <heather.dsouza@datadoghq.com>
2020-04-08 10:12:39 -0400

    update decorate with request and response objects

--- dd-java-agent/instrumentation/mule-http-connector-1.0/src/main/java/datadog/trace/instrumentation/mulehttpconnector/MuleHttpConnectorDecorator.java
+++ dd-java-agent/instrumentation/mule-http-connector-1.0/src/main/java/datadog/trace/instrumentation/mulehttpconnector/MuleHttpConnectorDecorator.java
@@ -1,4 +1,38 @@
 package datadog.trace.instrumentation.mulehttpconnector;
 
-public class MuleHttpConnectorDecorator {
+import com.ning.http.client.Request;
+import com.ning.http.client.Response;
+import datadog.trace.bootstrap.instrumentation.decorator.HttpClientDecorator;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MuleHttpConnectorDecorator extends HttpClientDecorator<Request, Response> {
+
+  public static final MuleHttpConnectorDecorator DECORATE = new MuleHttpConnectorDecorator();
+
+  @Override
+  protected String[] instrumentationNames() {
+    return new String[] {"mule-http-connector"};
+  }
+
+  @Override
+  protected String component() {
+    return "http-listener";
+  }
+
+  @Override
+  protected String method(final Request request) {
+    return request.getMethod();
+  }
+
+  @Override
+  protected URI url(final Request request) throws URISyntaxException {
+    return request.getUri().toJavaNetURI();
+  }
+
+  @Override
+  protected Integer status(final Response response) {
+    return response.getStatusCode();
+  }
 }
