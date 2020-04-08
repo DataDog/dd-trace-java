38fdffaa8f7073dc8ad443c01db854786022a54f
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-04-08 12:07:09 +0200

    Update instrumentation helper classes

--- dd-java-agent/instrumentation/exception-profiling/src/main/java/datadog/exceptions/instrumentation/ExceptionInstrumentation.java
+++ dd-java-agent/instrumentation/exception-profiling/src/main/java/datadog/exceptions/instrumentation/ExceptionInstrumentation.java
@@ -41,6 +41,7 @@ public final class ExceptionInstrumentation extends Instrumenter.Default {
     return hasJfr
         ? new String[] {
           "com.datadog.profiling.exceptions.StreamingSampler",
+          "com.datadog.profiling.exceptions.StreamingSampler$Counts",
           "com.datadog.profiling.exceptions.ExceptionCountEvent",
           "com.datadog.profiling.exceptions.ExceptionHistogram",
           "com.datadog.profiling.exceptions.ExceptionHistogram$Pair",
