25155204fa5fddbeac5d1d60c7d59a4f2cfadedf
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-04-17 17:01:15 +0200

    Change log level

--- dd-trace-ot/src/main/java/datadog/trace/common/util/ThreadCpuTimeAccess.java
+++ dd-trace-ot/src/main/java/datadog/trace/common/util/ThreadCpuTimeAccess.java
@@ -23,7 +23,7 @@ public final class ThreadCpuTimeAccess {
   /** Enable JMX based thread CPU time */
   public static void enableJmx() {
     if (!Config.get().isProfilingEnabled()) {
-      log.info("Will not enable thread CPU time access. Profiling is disabled.");
+      log.debug("Will not enable thread CPU time access. Profiling is disabled.");
       return;
     }
     try {
