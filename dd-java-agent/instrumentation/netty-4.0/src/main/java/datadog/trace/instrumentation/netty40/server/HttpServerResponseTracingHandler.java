047727347b74e7ae6d6e9d11c490dcfa9a102758
Nikolay Martynov <nikolay.martynov@datadoghq.com>
2020-04-29 11:55:01 -0400

    Add scopes to http server response

--- dd-java-agent/instrumentation/netty-4.0/src/main/java/datadog/trace/instrumentation/netty40/server/HttpServerResponseTracingHandler.java
+++ dd-java-agent/instrumentation/netty-4.0/src/main/java/datadog/trace/instrumentation/netty40/server/HttpServerResponseTracingHandler.java
@@ -1,7 +1,9 @@
 package datadog.trace.instrumentation.netty40.server;
 
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
 import static datadog.trace.instrumentation.netty40.server.NettyHttpServerDecorator.DECORATE;
 
+import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.Tags;
 import datadog.trace.instrumentation.netty40.AttributeKeys;
@@ -20,18 +22,20 @@ public class HttpServerResponseTracingHandler extends ChannelOutboundHandlerAdap
       return;
     }
 
-    final HttpResponse response = (HttpResponse) msg;
+    try (final AgentScope scope = activateSpan(span, false)) {
+      final HttpResponse response = (HttpResponse) msg;
 
-    try {
-      ctx.write(msg, prm);
-    } catch (final Throwable throwable) {
-      DECORATE.onError(span, throwable);
-      span.setTag(Tags.HTTP_STATUS, 500);
+      try {
+        ctx.write(msg, prm);
+      } catch (final Throwable throwable) {
+        DECORATE.onError(span, throwable);
+        span.setTag(Tags.HTTP_STATUS, 500);
+        span.finish(); // Finish the span manually since finishSpanOnClose was false
+        throw throwable;
+      }
+      DECORATE.onResponse(span, response);
+      DECORATE.beforeFinish(span);
       span.finish(); // Finish the span manually since finishSpanOnClose was false
-      throw throwable;
     }
-    DECORATE.onResponse(span, response);
-    DECORATE.beforeFinish(span);
-    span.finish(); // Finish the span manually since finishSpanOnClose was false
   }
 }
