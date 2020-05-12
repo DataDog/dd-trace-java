9b82831c0ee6e4181c06b00c97c831738a32c715
Scott Shields <sshields@wayfair.com>
2020-05-12 08:37:36 -0400

    Fixing checkstyle error

--- dd-java-agent/instrumentation/kafka-clients-0.11/src/main/java/datadog/trace/instrumentation/kafka_clients/KafkaProducerInstrumentation.java
+++ dd-java-agent/instrumentation/kafka-clients-0.11/src/main/java/datadog/trace/instrumentation/kafka_clients/KafkaProducerInstrumentation.java
@@ -78,9 +78,11 @@ public final class KafkaProducerInstrumentation extends Instrumenter.Default {
       // This is how similar check is being done in Kafka client itself:
       // https://github.com/apache/kafka/blob/05fcfde8f69b0349216553f711fdfc3f0259c601/clients/src/main/java/org/apache/kafka/common/record/MemoryRecordsBuilder.java#L411-L412
       // Also, do not inject headers if specified by JVM option or environment variable
-      // This can help in mixed client environments where clients < 0.11 that do not support headers attempt to read
+      // This can help in mixed client environments where clients < 0.11 that do not support headers
+      // attempt to read
       // messages that were produced by clients > 0.11 and the magic value of the broker(s) is >= 2
-      if (apiVersions.maxUsableProduceMagic() >= RecordBatch.MAGIC_VALUE_V2 && Config.get().isKafkaHeadersEnabled()) {
+      if (apiVersions.maxUsableProduceMagic() >= RecordBatch.MAGIC_VALUE_V2
+          && Config.get().isKafkaHeadersEnabled()) {
         try {
           propagate().inject(span, record.headers(), SETTER);
         } catch (final IllegalStateException e) {
