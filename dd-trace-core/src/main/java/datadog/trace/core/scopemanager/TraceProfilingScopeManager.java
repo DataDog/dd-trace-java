2d7498561947a169c6c02ea5ca8660aaff48e7b1
Tyler Benson <tyler.benson@datadoghq.com>
2020-05-13 17:19:38 -0400

    Add additional heuristics.

--- dd-trace-core/src/main/java/datadog/trace/core/scopemanager/TraceProfilingScopeManager.java
+++ dd-trace-core/src/main/java/datadog/trace/core/scopemanager/TraceProfilingScopeManager.java
@@ -1,15 +1,29 @@
 package datadog.trace.core.scopemanager;
 
+import com.google.common.util.concurrent.RateLimiter;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.context.TraceScope;
 import datadog.trace.core.DDSpan;
 import datadog.trace.core.interceptor.TraceStatsCollector;
+import java.util.concurrent.TimeUnit;
 import org.HdrHistogram.Histogram;
 
 public class TraceProfilingScopeManager implements DDScopeManager {
+  private static final long MAX_NANOSECONDS_BETWEEN_ACTIVATIONS = TimeUnit.SECONDS.toNanos(1);
+  private static final double ACTIVATIONS_PER_SECOND = 5;
+  private static final ThreadLocal<Boolean> IS_THREAD_PROFILING =
+      new ThreadLocal<Boolean>() {
+        @Override
+        protected Boolean initialValue() {
+          return false;
+        }
+      };
+
   private final DDScopeManager delegate;
   private final TraceStatsCollector statsCollector;
+  private final RateLimiter rateLimiter = RateLimiter.create(ACTIVATIONS_PER_SECOND);
+  private volatile long lastProfileTimestamp = System.nanoTime();
 
   public TraceProfilingScopeManager(
       final DDScopeManager delegate, final TraceStatsCollector statsCollector) {
@@ -33,23 +47,51 @@ public class TraceProfilingScopeManager implements DDScopeManager {
   }
 
   private AgentScope considerProfiling(final AgentScope scope, final AgentSpan span) {
-    if (!maybeInteresting(span) || scope instanceof TraceProfilingScope) {
+    if (scope instanceof TraceProfilingScope
+        || IS_THREAD_PROFILING.get() // don't need to waste a permit if so.
+        || !(span instanceof DDSpan)
+        || !maybeInteresting((DDSpan) span)
+        || !acquireProfilePermit()) {
+      // We don't want to wrap the scope for profiling.
       return scope;
     }
+    lastProfileTimestamp = System.nanoTime();
     return new TraceProfilingScope(scope);
   }
 
-  private boolean maybeInteresting(final AgentSpan span) {
-    if (span instanceof DDSpan) {
-      final Histogram overallStats = statsCollector.getOverallStats();
-      final Histogram traceStats = statsCollector.getTraceStats((DDSpan) span);
+  private boolean maybeInteresting(final DDSpan span) {
+    final Histogram traceStats = statsCollector.getTraceStats(span);
+    if (traceStats == null) {
+      // No historical data for this trace yet.
+      return false;
+    }
+    final Histogram overallStats = statsCollector.getOverallStats();
 
-      final long overall90 = overallStats.getValueAtPercentile(90);
-      final long traceAverage = traceStats.getValueAtPercentile(50);
-      return overall90 < traceAverage;
-      // TODO: keep track of how often this triggers and capture a minimum threshold.
+    final long traceAverage = traceStats.getValueAtPercentile(50);
+    final long overall80 = overallStats.getValueAtPercentile(80);
+    if (overall80 < traceAverage) {
+      // This trace is likely to be slower than most, so lets profile it.
+      return true;
+    }
+
+    final long traceCount = traceStats.getTotalCount();
+    final long overallCount = overallStats.getTotalCount();
+    if (3 < traceCount && traceCount < (overallCount * .9)) {
+      // This is an uncommon trace (but not unique), so lets profile it.
+      return true;
+    }
+
+    if (lastProfileTimestamp + MAX_NANOSECONDS_BETWEEN_ACTIVATIONS < System.nanoTime()) {
+      // It's been a while since we last profiled, so lets take one now.
+      // Due to the multi-threaded nature here, we will likely have multiple threads enter here
+      // but they will still be subject to the rate limiter following.
+      return true;
     }
     return false;
+  }
+
+  private boolean acquireProfilePermit() {
+    return rateLimiter.tryAcquire();
   }
 
   private static class TraceProfilingScope implements AgentScope {
@@ -57,6 +99,7 @@ public class TraceProfilingScopeManager implements DDScopeManager {
     private final AgentScope delegate;
 
     private TraceProfilingScope(final AgentScope delegate) {
+      IS_THREAD_PROFILING.set(true);
       // Start profiling
       this.delegate = delegate;
     }
@@ -73,6 +116,7 @@ public class TraceProfilingScopeManager implements DDScopeManager {
 
     @Override
     public void close() {
+      IS_THREAD_PROFILING.set(false);
       // Stop profiling
       delegate.close();
     }
