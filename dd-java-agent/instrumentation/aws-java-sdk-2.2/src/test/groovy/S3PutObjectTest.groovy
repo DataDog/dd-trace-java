2e1d0364e950260818ca953fa2a9409d6fa41ba5
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2020-05-19 15:11:17 -0400

    More consistent reproducer

--- dd-java-agent/instrumentation/aws-java-sdk-2.2/src/test/groovy/S3PutObjectTest.groovy
+++ dd-java-agent/instrumentation/aws-java-sdk-2.2/src/test/groovy/S3PutObjectTest.groovy
@@ -10,10 +10,12 @@ import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest
 import software.amazon.awssdk.services.s3.model.ListObjectsResponse
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
-import software.amazon.awssdk.services.s3.model.PutObjectResponse
 import spock.lang.Shared
 
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadPoolExecutor
 
 @Testcontainers
 class S3PutObjectTest extends AgentTestRunner {
@@ -44,33 +46,40 @@ class S3PutObjectTest extends AgentTestRunner {
 
   def "Send large file"() {
     given:
-    byte[] bytes = new byte[1024 * 1024 * 100]
+    byte[] bytes = new byte[1024 * 1024 * 50]
     random.nextBytes(bytes)
     File file = File.createTempFile("test", "data")
     FileOutputStream out = new FileOutputStream(file)
     out.write(bytes)
     out.close()
 
+    Executor executor = Executors.newFixedThreadPool(5)
+
     when:
-    PutObjectResponse putResponse
-    TraceUtils.runUnderTrace("put") {
-      putResponse = s3Client.putObject(PutObjectRequest.builder().bucket("test").key("test").build() as PutObjectRequest,
-        AsyncRequestBody.fromFile(file)).join()
+    for (int i = 0; i < 50; i++) {
+      executor.submit {
+        TraceUtils.runUnderTrace("put-$i") {
+          s3Client.putObject(PutObjectRequest.builder().bucket("test").key("test-$i").build() as PutObjectRequest,
+            AsyncRequestBody.fromFile(file)).join()
+        }
+      }
     }
 
-    then:
-    putResponse.sdkHttpResponse().successful
+    and:
+    while (((ThreadPoolExecutor) executor).queue.size() != 0) {
+      sleep(10)
+    }
 
-    when:
+    and:
     CompletableFuture<ListObjectsResponse> response
     TraceUtils.runUnderTrace("list") {
       response = s3Client.listObjects(ListObjectsRequest.builder().bucket("test").build() as ListObjectsRequest)
     }
 
     then:
-    response.get().contents().any({ it.key() == "test" })
+    response.get().contents().any({ it.key() == "test-50" })
 
     and:
-    TEST_WRITER.size() == 2
+    TEST_WRITER.size() == 51
   }
 }
