89d71eec14bc13667f71f385610a68f9d2cf6bbd
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2020-05-20 20:04:46 +0200

    increase test coverage

--- dd-java-agent/agent-profiling/profiling-controller/src/test/java/com/datadog/profiling/mlt/JMXSamplerTest.java
+++ dd-java-agent/agent-profiling/profiling-controller/src/test/java/com/datadog/profiling/mlt/JMXSamplerTest.java
@@ -7,11 +7,14 @@ import org.junit.jupiter.api.Test;
 public class JMXSamplerTest {
 
   @Test
-  public void sampler() {
+  public void sampler() throws InterruptedException {
     ThreadStackAccess.enableJmx();
     StackTraceSink sink = new JFRStackTraceSink();
     JMXSampler sampler = new JMXSampler(sink);
     sampler.addThreadId(Thread.currentThread().getId());
+    sampler.addThreadId(1);
+    Thread.sleep(100);
+    sampler.removeThread(1);
     sampler.removeThread(Thread.currentThread().getId());
     sampler.shutdown();
     byte[] buffer = sink.flush();
