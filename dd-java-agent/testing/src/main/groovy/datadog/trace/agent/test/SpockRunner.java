0e3a028b4cf8f3e241174be1a70330ecc28e4a93
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2020-06-04 15:11:06 +0200

    fix some packages

--- dd-java-agent/testing/src/main/groovy/datadog/trace/agent/test/SpockRunner.java
+++ dd-java-agent/testing/src/main/groovy/datadog/trace/agent/test/SpockRunner.java
@@ -38,7 +38,7 @@ public class SpockRunner extends Sputnik {
     "datadog.trace.bootstrap",
     "datadog.trace.context",
     "datadog.trace.instrumentation.api",
-    "datadog.trace.profiling",
+    "datadog.trace.mlt",
   };
 
   private static final String[] TEST_BOOTSTRAP_PREFIXES;
--- dd-trace-core/src/test/groovy/datadog/trace/core/scopemanager/TraceProfilingScopeInterceptorTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/core/scopemanager/TraceProfilingScopeInterceptorTest.groovy
@@ -3,9 +3,9 @@ package datadog.trace.core.scopemanager
 import datadog.trace.api.DDId
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan
 import datadog.trace.core.interceptor.TraceStatsCollector
-import datadog.trace.profiling.MethodLevelTracer
-import datadog.trace.profiling.Session
-import datadog.trace.profiling.SessionFactory
+import datadog.trace.mlt.MethodLevelTracer
+import datadog.trace.mlt.Session
+import datadog.trace.mlt.SessionFactory
 import datadog.trace.util.test.DDSpecification
 import org.HdrHistogram.Histogram
 
