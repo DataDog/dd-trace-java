8ad8e34e5575184cf49b84bed02d109329320ebc
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-06-11 15:38:53 +0200

    Provide a ByteBuffer backed implementation of LEB128Writer

---
+++ utils/mlt-support/src/main/java/com/datadog/mlt/io/LEB128ByteBufferWriter.java
@@ -0,0 +1,135 @@
+package com.datadog.mlt.io;
+
+import java.nio.ByteBuffer;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+final class LEB128ByteBufferWriter extends AbstractLEB128Writer {
+  private ByteBuffer buffer;
+
+  LEB128ByteBufferWriter(int initialCapacity) {
+    this.buffer = ByteBuffer.allocateDirect(initialCapacity);
+  }
+
+  @Override
+  public void reset() {
+    buffer.clear();
+  }
+
+  @Override
+  public long writeFloat(long offset, float data) {
+    ensureCapacity((int) offset, 4);
+    int originalPosition = buffer.position();
+    buffer.position((int) offset);
+    buffer.putFloat(data);
+    if (originalPosition > buffer.position()) {
+      buffer.position(originalPosition);
+    }
+    return buffer.position();
+  }
+
+  @Override
+  public long writeDouble(long offset, double data) {
+    ensureCapacity((int) offset, 8);
+    int originalPosition = buffer.position();
+    buffer.position((int) offset);
+    buffer.putDouble(data);
+    if (originalPosition > buffer.position()) {
+      buffer.position(originalPosition);
+    }
+    return buffer.position();
+  }
+
+  @Override
+  public long writeByte(long offset, byte data) {
+    ensureCapacity((int) offset, 1);
+    int originalPosition = buffer.position();
+    buffer.position((int) offset);
+    buffer.put(data);
+    if (originalPosition > buffer.position()) {
+      buffer.position(originalPosition);
+    }
+    return buffer.position();
+  }
+
+  @Override
+  public long writeBytes(long offset, byte... data) {
+    ensureCapacity((int) offset, data.length);
+    int originalPosition = buffer.position();
+    buffer.position((int) offset);
+    buffer.put(data, 0, data.length);
+    if (originalPosition > buffer.position()) {
+      buffer.position(originalPosition);
+    }
+    return buffer.position();
+  }
+
+  @Override
+  public long writeShortRaw(long offset, short data) {
+    ensureCapacity((int) offset, 2);
+    int originalPosition = buffer.position();
+    buffer.position((int) offset);
+    buffer.putShort(data);
+    if (originalPosition > buffer.position()) {
+      buffer.position(originalPosition);
+    }
+    return buffer.position();
+  }
+
+  @Override
+  public long writeIntRaw(long offset, int data) {
+    ensureCapacity((int) offset, 4);
+    int originalPosition = buffer.position();
+    buffer.position((int) offset);
+    buffer.putInt(data);
+    if (originalPosition > buffer.position()) {
+      buffer.position(originalPosition);
+    }
+    return buffer.position();
+  }
+
+  @Override
+  public long writeLongRaw(long offset, long data) {
+    ensureCapacity((int) offset, 8);
+    int originalPosition = buffer.position();
+    buffer.position((int) offset);
+    buffer.putLong(data);
+    if (originalPosition > buffer.position()) {
+      buffer.position(originalPosition);
+    }
+    return buffer.position();
+  }
+
+  @Override
+  public byte[] toByteArray() {
+    buffer.flip();
+    byte[] data = new byte[buffer.remaining()];
+    buffer.get(data, 0, data.length);
+    return data;
+  }
+
+  @Override
+  public int position() {
+    return buffer.position();
+  }
+
+  @Override
+  public int capacity() {
+    return buffer.capacity();
+  }
+
+  private void ensureCapacity(int offset, int dataLength) {
+    if (offset + dataLength > buffer.capacity()) {
+      int newCapacity = buffer.capacity() * 2;
+      log.warn(
+          "{} capacity ({} bytes) exceeded. Reallocating internal buffer with new capacity {} bytes",
+          this.getClass().getName(),
+          buffer.capacity(),
+          newCapacity);
+      ByteBuffer newBuffer = ByteBuffer.allocateDirect(newCapacity);
+      buffer.flip();
+      newBuffer.put(buffer);
+      buffer = newBuffer;
+    }
+  }
+}
--- utils/mlt-support/src/test/java/com/datadog/mlt/io/AbstractLEB128WriterTest.java
+++ utils/mlt-support/src/test/java/com/datadog/mlt/io/AbstractLEB128WriterTest.java
@@ -4,17 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class LEB128ByteArrayWriterTest {
+class AbstractLEB128WriterTest {
   /** Make sure the packed integer length is correctly calculated */
   @Test
   void testGetPackedIntLen() {
-    assertEquals(1, LEB128ByteArrayWriter.getPackedIntLen(0));
+    assertEquals(1, AbstractLEB128Writer.getPackedIntLen(0));
 
     long val = 1L;
 
     for (int i = 1; i < 10; i++) {
-      assertEquals(Math.max(i - 1, 1), LEB128ByteArrayWriter.getPackedIntLen(val - 1));
-      assertEquals(i, LEB128ByteArrayWriter.getPackedIntLen(val));
+      assertEquals(Math.max(i - 1, 1), AbstractLEB128Writer.getPackedIntLen(val - 1));
+      assertEquals(i, AbstractLEB128Writer.getPackedIntLen(val));
       val = val << 7;
     }
   }
@@ -24,7 +24,7 @@ class LEB128ByteArrayWriterTest {
     // numeric overflow over Integer.MAX_VALUE will cause negative number and we want stop right at
     // that moment
     for (int i = 1; i > 0; i = i * 2) {
-      int estimatedLength = i + LEB128ByteArrayWriter.getPackedIntLen(i);
+      int estimatedLength = i + AbstractLEB128Writer.getPackedIntLen(i);
       assertTrue(estimatedLength <= LEB128ByteArrayWriter.adjustLength(i));
     }
   }
