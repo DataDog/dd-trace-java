f9c9ccc5807a219a40e5f9aab2d3f9d1d28f1df4
Richard Startin <richard.startin@datadoghq.com>
2020-06-15 12:30:25 +0100

    add some tests for NameMatchers

---
+++ dd-java-agent/agent-tooling/src/test/groovy/datadog/trace/agent/tooling/bytebuddy/matcher/NameMatchersTest.groovy
@@ -0,0 +1,46 @@
+package datadog.trace.agent.tooling.bytebuddy.matcher
+
+import datadog.trace.util.test.DDSpecification
+import net.bytebuddy.description.NamedElement
+
+class NameMatchersTest extends DDSpecification {
+
+  def "test namedOneOf"() {
+    setup:
+    def named = Mock(NamedElement)
+    named.getActualName() >> { name }
+    def matcher = NameMatchers.namedOneOf("foo", "bar")
+
+    when:
+    def result = matcher.matches(named)
+
+    then:
+    result == expected
+
+    where:
+    name         | expected
+    "foo"        | true
+    "bar"        | true
+    "missing"    | false
+  }
+
+
+  def "test namedNoneOf"() {
+    setup:
+    def named = Mock(NamedElement)
+    named.getActualName() >> { name }
+    def matcher = NameMatchers.namedNoneOf("foo", "bar")
+
+    when:
+    def result = matcher.matches(named)
+
+    then:
+    result == expected
+
+    where:
+    name         | expected
+    "foo"        | false
+    "bar"        | false
+    "missing"    | true
+  }
+}
