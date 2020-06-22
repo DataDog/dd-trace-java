d56c563db0218d10743644aba53f2326df5a9acb
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2020-06-22 13:23:23 +0200

    Cache Strings in DBTypeTagInterceptor

--- dd-trace-core/src/main/java/datadog/trace/core/taginterceptor/DBTypeTagInterceptor.java
+++ dd-trace-core/src/main/java/datadog/trace/core/taginterceptor/DBTypeTagInterceptor.java
@@ -3,6 +3,7 @@ package datadog.trace.core.taginterceptor;
 import datadog.trace.api.DDSpanTypes;
 import datadog.trace.bootstrap.instrumentation.api.Tags;
 import datadog.trace.core.DDSpanContext;
+import datadog.trace.core.util.FixedSizeCache;
 
 /**
  * This span decorator leverages DB tags. It allows the dev to define a custom service name and
@@ -15,29 +16,46 @@ class DBTypeTagInterceptor extends AbstractTagInterceptor {
     super(Tags.DB_TYPE);
   }
 
+  private static final String OPERATION_SUFFIX = ".query";
+
+  // The total number of entries in the cache will normally be less than 4, since
+  // most applications only have one or two DBs, and "jdbc" itself is also used as
+  // one DB_TYPE, but set the cache size to 16 to help avoid collisions.
+  private final FixedSizeCache<String, String> cache = new FixedSizeCache<>(16);
+  private final FixedSizeCache.Creator<String, String> appendOperationSuffix =
+      new FixedSizeCache.Creator<String, String>() {
+        @Override
+        public String create(String key) {
+          return key + OPERATION_SUFFIX;
+        }
+      };
+
   @Override
   public boolean shouldSetTag(final DDSpanContext context, final String tag, final Object value) {
-    context.setServiceName(String.valueOf(value));
+    final String serviceName = String.valueOf(value);
 
     // Assign service name
-    if ("couchbase".equals(value) || "elasticsearch".equals(value)) {
+    context.setServiceName(serviceName);
+
+    if ("couchbase".equals(serviceName) || "elasticsearch".equals(serviceName)) {
       // these instrumentation have different behavior.
       return true;
     }
     // Assign span type to DB
     // Special case: Mongo, set to mongodb
-    if ("mongo".equals(value)) {
+    if ("mongo".equals(serviceName)) {
       // Todo: not sure it's used cos already in the agent mongo helper
       context.setSpanType(DDSpanTypes.MONGO);
-    } else if ("cassandra".equals(value)) {
+    } else if ("cassandra".equals(serviceName)) {
       context.setSpanType(DDSpanTypes.CASSANDRA);
-    } else if ("memcached".equals(value)) {
+    } else if ("memcached".equals(serviceName)) {
       context.setSpanType(DDSpanTypes.MEMCACHED);
     } else {
       context.setSpanType(DDSpanTypes.SQL);
     }
     // Works for: mongo, cassandra, jdbc
-    context.setOperationName(String.valueOf(value) + ".query");
+    String operationName = cache.computeIfAbsent(serviceName, appendOperationSuffix);
+    context.setOperationName(operationName);
 
     return true;
   }
