cc6c5d5be22d018ce2add142166a4e2324d46239
Tyler Benson <tyler.benson@datadoghq.com>
2020-07-27 15:02:29 -0400

    Move Spring Scheduling RunnableWrapper to separate class
    This works around an edge case where Spring Sleuth tries to load the RunnableWrapper's enclosing class:
    https://github.com/spring-projects/spring-framework/blob/v5.2.2.RELEASE/spring-core/src/main/java/org/springframework/core/annotation/AnnotationsScanner.java#L233
    
    Since the enclosing class is on a separate class loader, this fails and results in an exception.

---
+++ dd-java-agent/instrumentation/spring-scheduling-3.1/src/main/java/datadog/trace/instrumentation/springscheduling/SpringSchedulingRunnableWrapper.java
@@ -0,0 +1,46 @@
+package datadog.trace.instrumentation.springscheduling;
+
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
+import static datadog.trace.instrumentation.springscheduling.SpringSchedulingDecorator.DECORATE;
+
+import datadog.trace.bootstrap.instrumentation.api.AgentScope;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+
+public class SpringSchedulingRunnableWrapper implements Runnable {
+  private final Runnable runnable;
+
+  private SpringSchedulingRunnableWrapper(final Runnable runnable) {
+    this.runnable = runnable;
+  }
+
+  @Override
+  public void run() {
+    final AgentSpan span = startSpan("scheduled.call");
+    DECORATE.afterStart(span);
+
+    try (final AgentScope scope = activateSpan(span)) {
+      DECORATE.onRun(span, runnable);
+      scope.setAsyncPropagation(true);
+
+      try {
+        runnable.run();
+      } catch (final Throwable throwable) {
+        DECORATE.onError(span, throwable);
+        throw throwable;
+      }
+    } finally {
+      DECORATE.beforeFinish(span);
+      span.finish();
+    }
+  }
+
+  public static Runnable wrapIfNeeded(final Runnable task) {
+    // We wrap only lambdas' anonymous classes and if given object has not already been wrapped.
+    // Anonymous classes have '/' in class name which is not allowed in 'normal' classes.
+    if (task instanceof SpringSchedulingRunnableWrapper) {
+      return task;
+    }
+    return new SpringSchedulingRunnableWrapper(task);
+  }
+}
