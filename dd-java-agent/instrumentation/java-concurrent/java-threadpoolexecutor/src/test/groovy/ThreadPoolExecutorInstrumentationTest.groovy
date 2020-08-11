6ae4927494387d386fdd01f9903d1b8f06aa0bcd
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2020-08-11 10:37:30 -0400

    I think I got it working......

--- dd-java-agent/instrumentation/java-concurrent/java-threadpoolexecutor/src/test/groovy/ThreadPoolExecutorInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/java-concurrent/java-threadpoolexecutor/src/test/groovy/ThreadPoolExecutorInstrumentationTest.groovy
@@ -1,21 +1,20 @@
 import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.agent.test.utils.ConfigUtils
 import datadog.trace.api.Trace
-import datadog.trace.core.DDSpan
+import datadog.trace.bootstrap.instrumentation.java.concurrent.CallableWrapper
+import datadog.trace.bootstrap.instrumentation.java.concurrent.RunnableWrapper
 import spock.lang.Shared
 
-import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.Callable
-import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.ScheduledThreadPoolExecutor
 import java.util.concurrent.TimeUnit
 
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeScope
-import static org.junit.Assume.assumeTrue
 
 class ThreadPoolExecutorInstrumentationTest extends AgentTestRunner {
   static {
     ConfigUtils.updateConfig {
-      System.setProperty("dd.integration.thread-pool-executor.enabled", "false")
+      System.setProperty("dd.trace.thread_pool_executor_wrapped_tasks.enabled", "false")
     }
   }
 
@@ -26,57 +25,51 @@ class ThreadPoolExecutorInstrumentationTest extends AgentTestRunner {
   @Shared
   def submitCallable = { e, c -> e.submit((Callable) c) }
   @Shared
-  def invokeAll = { e, c -> e.invokeAll([(Callable) c]) }
+  def scheduleRunnable = { e, c -> e.schedule((Runnable) c, 10, TimeUnit.MILLISECONDS) }
   @Shared
-  def invokeAllTimeout = { e, c -> e.invokeAll([(Callable) c], 10, TimeUnit.SECONDS) }
-  @Shared
-  def invokeAny = { e, c -> e.invokeAny([(Callable) c]) }
-  @Shared
-  def invokeAnyTimeout = { e, c -> e.invokeAny([(Callable) c], 10, TimeUnit.SECONDS) }
+  def scheduleCallable = { e, c -> e.schedule((Callable) c, 10, TimeUnit.MILLISECONDS) }
 
   def "#poolImpl '#name' disabled"() {
     setup:
-    assumeTrue(poolImpl != null) // skip for Java 7 CompletableFuture
     def pool = poolImpl
     def m = method
+    def w = wrap
 
+    JavaAsyncChild child = new JavaAsyncChild(true, true)
     new Runnable() {
       @Override
       @Trace(operationName = "parent")
       void run() {
         activeScope().setAsyncPropagation(true)
-        // this child will have a span
-        m(pool, new JavaAsyncChild())
-        // this child won't
-        m(pool, new JavaAsyncChild(false, false))
-        blockUntilChildSpansFinished(1)
+        m(pool, w(child))
       }
     }.run()
+    // We block in child to make sure spans close in predictable order
+    child.unblock()
 
-    TEST_WRITER.waitForTraces(1)
-    List<DDSpan> trace = TEST_WRITER.get(0)
+    // Expect two traces because async propagation gets effectively disabled
+    TEST_WRITER.waitForTraces(2)
 
     expect:
-    TEST_WRITER.size() == 1
-    trace.size() == 2
-    trace.get(0).operationName == "parent"
-    trace.get(1).operationName == "asyncChild"
-    trace.get(1).parentId == trace.get(0).spanId
+    TEST_WRITER.size() == 2
+    TEST_WRITER.get(0).size() == 1
+    TEST_WRITER.get(0).get(0).operationName == "parent"
+    TEST_WRITER.get(1).size() == 1
+    TEST_WRITER.get(1).get(0).operationName == "asyncChild"
 
     cleanup:
-    if (pool?.hasProperty("shutdown")) {
-      pool?.shutdown()
-    }
+    pool?.shutdown()
 
-    // Unfortunately, there's no simple way to test the cross product of methods/pools.
     where:
-    name                     | method           | poolImpl
-    "execute Runnable"       | executeRunnable  | new ThreadPoolExecutor(1, 1, 1000, TimeUnit.NANOSECONDS, new ArrayBlockingQueue<Runnable>(1))
-    "submit Runnable"        | submitRunnable   | new ThreadPoolExecutor(1, 1, 1000, TimeUnit.NANOSECONDS, new ArrayBlockingQueue<Runnable>(1))
-    "submit Callable"        | submitCallable   | new ThreadPoolExecutor(1, 1, 1000, TimeUnit.NANOSECONDS, new ArrayBlockingQueue<Runnable>(1))
-    "invokeAll"              | invokeAll        | new ThreadPoolExecutor(1, 1, 1000, TimeUnit.NANOSECONDS, new ArrayBlockingQueue<Runnable>(1))
-    "invokeAll with timeout" | invokeAllTimeout | new ThreadPoolExecutor(1, 1, 1000, TimeUnit.NANOSECONDS, new ArrayBlockingQueue<Runnable>(1))
-    "invokeAny"              | invokeAny        | new ThreadPoolExecutor(1, 1, 1000, TimeUnit.NANOSECONDS, new ArrayBlockingQueue<Runnable>(1))
-    "invokeAny with timeout" | invokeAnyTimeout | new ThreadPoolExecutor(1, 1, 1000, TimeUnit.NANOSECONDS, new ArrayBlockingQueue<Runnable>(1))
+    // Scheduled executor cannot accept wrapped tasks
+    // TODO: we should have a test that passes lambda, but this is hard
+    // because this requires tests to be run in java8+ only.
+    // Instead we 'hand-wrap' tasks in this test.
+    name                | method           | wrap                        | poolImpl
+    "execute Runnable"  | executeRunnable  | { new RunnableWrapper(it) } | new ScheduledThreadPoolExecutor(1)
+    "submit Runnable"   | submitRunnable   | { new RunnableWrapper(it) } | new ScheduledThreadPoolExecutor(1)
+    "submit Callable"   | submitCallable   | { new CallableWrapper(it) } | new ScheduledThreadPoolExecutor(1)
+    "schedule Runnable" | scheduleRunnable | { new RunnableWrapper(it) } | new ScheduledThreadPoolExecutor(1)
+    "schedule Callable" | scheduleCallable | { new CallableWrapper(it) } | new ScheduledThreadPoolExecutor(1)
   }
 }
