35607b6dfaa014085f03dd1e1f18f1d487e4fa3a
orekyuu <orekyuu@gmail.com>
2020-08-16 19:47:35 +0900

    Add scalikejdbc test.

---
+++ dd-java-agent/instrumentation/jdbc/src/test/scala/scalike/ScalikeSqlExecutor.scala
@@ -0,0 +1,22 @@
+package scalike
+
+import scalikejdbc._
+
+object ScalikeSqlExecutor {
+
+  def execute(
+      driverClass: String,
+      jdbcUrl: String,
+      user: String,
+      password: String,
+      query: String
+  ): Unit = {
+    Class.forName(driverClass)
+
+    ConnectionPool.singleton(jdbcUrl, user, password)
+
+    implicit val session = AutoSession
+
+    SQL(query).execute().apply()
+  }
+}
