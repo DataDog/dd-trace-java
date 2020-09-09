0a3088db92a4ae5210467cb2a0596999db1cc2a2
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2020-09-09 11:45:05 -0400

    Add java8 tests

---
+++ dd-java-agent/instrumentation/java-concurrent/lambda-testing/src/test/groovy/ExecutorLambdaTest.groovy
@@ -0,0 +1,58 @@
+import datadog.trace.agent.test.AgentTestRunner
+import spock.lang.Shared
+
+import java.util.concurrent.Callable
+import java.util.concurrent.ScheduledThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+
+import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
+
+class ExecutorLambdaTest extends AgentTestRunner {
+  @Shared
+  def executeRunnable = { e, c -> e.execute((Runnable) c) }
+  @Shared
+  def submitRunnable = { e, c -> e.submit((Runnable) c) }
+  @Shared
+  def submitCallable = { e, c -> e.submit((Callable) c) }
+  @Shared
+  def scheduleRunnable = { e, c -> e.schedule((Runnable) c, 10, TimeUnit.MILLISECONDS) }
+  @Shared
+  def scheduleCallable = { e, c -> e.schedule((Callable) c, 10, TimeUnit.MILLISECONDS) }
+
+  def "#poolImpl '#name' wrap lambdas"() {
+    setup:
+    def pool = poolImpl
+    def m = method
+    def w = wrap
+    JavaAsyncChild child = new JavaAsyncChild(true, true)
+    new Runnable() {
+      @Override
+      void run() {
+        runUnderTrace("parent") {
+          m(pool, w(child))
+        }
+      }
+    }.run()
+    child.unblock()
+
+    TEST_WRITER.waitForTraces(1)
+
+    expect:
+    TEST_WRITER.size() == 1
+    TEST_WRITER.get(0).size() == 2
+    TEST_WRITER.get(0).get(1).operationName == "parent"
+    TEST_WRITER.get(0).get(0).operationName == "asyncChild"
+    TEST_WRITER.get(0).get(0).parentId == TEST_WRITER.get(0).get(1).spanId
+
+    cleanup:
+    pool?.shutdown()
+
+    where:
+    name                | method           | wrap                                 | poolImpl
+    "execute Runnable"  | executeRunnable  | { LambdaGenerator.wrapRunnable(it) } | new ScheduledThreadPoolExecutor(1)
+    "submit Runnable"   | submitRunnable   | { LambdaGenerator.wrapRunnable(it) } | new ScheduledThreadPoolExecutor(1)
+    "submit Callable"   | submitCallable   | { LambdaGenerator.wrapCallable(it) } | new ScheduledThreadPoolExecutor(1)
+    "schedule Runnable" | scheduleRunnable | { LambdaGenerator.wrapRunnable(it) } | new ScheduledThreadPoolExecutor(1)
+    "schedule Callable" | scheduleCallable | { LambdaGenerator.wrapCallable(it) } | new ScheduledThreadPoolExecutor(1)
+  }
+}
