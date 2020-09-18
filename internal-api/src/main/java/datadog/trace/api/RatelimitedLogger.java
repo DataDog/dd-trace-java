e710b5d316214aa7e2cce4c0e055c012eba35a2c
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2020-09-18 16:25:02 +0200

    Move RateLimitedLogger to internal-api
    Replace existing rate limited in DDAgentApi by this new class
    Migrate & Improve testing

--- internal-api/src/main/java/datadog/trace/api/RatelimitedLogger.java
+++ internal-api/src/main/java/datadog/trace/api/RatelimitedLogger.java
@@ -1,7 +1,6 @@
-package com.datadog.profiling.uploader.util;
+package datadog.trace.api;
 
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Supplier;
 import org.slf4j.Logger;
 
 /**
@@ -11,36 +10,51 @@ import org.slf4j.Logger;
 // FIXME: move this to a place where it can be reused
 public class RatelimitedLogger {
 
+  public interface TimeSourceSupplier {
+    TimeSourceSupplier DEFAULT =
+        new TimeSourceSupplier() {
+          @Override
+          public long get() {
+            return System.nanoTime();
+          }
+        };
+
+    long get();
+  }
+
   private final Logger log;
   private final long delay;
-  private final Supplier<Long> timeSource;
+  private final TimeSourceSupplier timeSource;
 
   private final AtomicLong previousErrorLogNanos = new AtomicLong();
 
   public RatelimitedLogger(final Logger log, final long delay) {
-    this(log, delay, System::nanoTime);
+    this(log, delay, TimeSourceSupplier.DEFAULT);
   }
 
   // Visible for testing
-  RatelimitedLogger(final Logger log, final long delay, final Supplier<Long> timeSource) {
+  RatelimitedLogger(final Logger log, final long delay, final TimeSourceSupplier timeSource) {
     this.log = log;
     this.delay = delay;
     this.timeSource = timeSource;
   }
 
-  public void warn(final String format, final Object... arguments) {
+  /** @return true if actually logged the message, false otherwise */
+  public boolean warn(final String format, final Object... arguments) {
     if (log.isDebugEnabled()) {
       log.warn(format, arguments);
-      return;
+      return true;
     }
     if (log.isWarnEnabled()) {
       final long previous = previousErrorLogNanos.get();
       final long now = timeSource.get();
       if (now - previous >= delay) {
         if (previousErrorLogNanos.compareAndSet(previous, now)) {
-          log.warn(format + " {} (Will not log errors for 5 minutes)", arguments);
+          log.warn(format + " (Will not log errors for 5 minutes)", arguments);
+          return true;
         }
       }
     }
+    return false;
   }
 }
