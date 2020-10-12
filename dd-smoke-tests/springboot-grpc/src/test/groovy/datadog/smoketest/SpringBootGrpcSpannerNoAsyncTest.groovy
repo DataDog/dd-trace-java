56d1b90861ff9c5e13619334554bb0828b0baa8b
Richard Startin <richard.startin@datadoghq.com>
2020-10-12 16:31:53 +0100

    enable spanner tests now they are deterministic

--- dd-smoke-tests/springboot-grpc/src/test/groovy/datadog/smoketest/SpringBootGrpcSpannerNoAsyncTest.groovy
+++ dd-smoke-tests/springboot-grpc/src/test/groovy/datadog/smoketest/SpringBootGrpcSpannerNoAsyncTest.groovy
@@ -1,19 +1,25 @@
 package datadog.smoketest
 
-import spock.lang.Ignore
 
 import java.util.concurrent.atomic.AtomicInteger
 
-@Ignore("can unignore when strict continuation reference counting is dropped")
 class SpringBootGrpcSpannerNoAsyncTest extends SpringBootWithGRPCTest {
 
   @Override
   boolean isAcceptable(Map<String, AtomicInteger> traceCounts) {
-    // currently spring @Async instrumentation doesn't hold the trace back
-    // if spans are created after the @Async span finishes - compensate for this
-    for (Map.Entry<String, AtomicInteger> entry : traceCounts.entrySet()) {
-      if (entry.getKey().startsWith("[servlet.request[spring.handler[grpc.client]") && entry.getValue().get() > 0) {
-        return true
+    // because of the way spanner's session creation works, without authentication,
+    // we can't guarantee a deterministic flush with grpc.client spans attached to
+    // their parents, but orphaned root grpc client spans (those without a parent id
+    // to link to) are unacceptable because they can't be stitched together by the
+    // backend. The writer will only report traces as [grpc.client] if the parent id
+    // is zero. 
+    if (!traceCounts.containsKey("[grpc.client]")) {
+      for (Map.Entry<String, AtomicInteger> entry : traceCounts.entrySet()) {
+        if (entry.getKey().startsWith("[servlet.request[spring.handler[grpc.client]")
+          || entry.getKey().startsWith("[servlet.request[spring.handler[http.request]")
+          && entry.getValue().get() > 0) {
+          return true
+        }
       }
     }
     return false
