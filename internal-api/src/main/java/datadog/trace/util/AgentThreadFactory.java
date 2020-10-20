99b85a665ae9359ffe68ed79d4a393e5df39c263
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2020-10-20 13:27:14 +0100

    Remove unused feature to simplify task creation

--- internal-api/src/main/java/datadog/trace/util/AgentThreadFactory.java
+++ internal-api/src/main/java/datadog/trace/util/AgentThreadFactory.java
@@ -13,7 +13,6 @@ public final class AgentThreadFactory implements ThreadFactory {
       new AgentThreadFactory("dd-task-scheduler");
 
   private final String threadName;
-  private final Runnable initializer;
 
   /**
    * Constructs a new {@code AgentThreadFactory} with a null ContextClassLoader.
@@ -21,46 +20,11 @@ public final class AgentThreadFactory implements ThreadFactory {
    * @param threadName used to prefix all thread names.
    */
   public AgentThreadFactory(final String threadName) {
-    this(threadName, null);
-  }
-
-  /**
-   * Constructs a new {@code AgentThreadFactory} with a null ContextClassLoader.
-   *
-   * @param threadName used to prefix all thread names.
-   * @param initializer initializer runnable that will be executed for all created threads.
-   * @note The initializer runnable can be executed by multiple threads if more than one thread is
-   *     created by the factory, and should take that into account to avoid race conditions.
-   */
-  private AgentThreadFactory(final String threadName, Runnable initializer) {
     this.threadName = threadName;
-    this.initializer = initializer;
-  }
-
-  /**
-   * Constructs a new {@code AgentThreadFactory} with a null ContextClassLoader.
-   *
-   * @param initializer initializer runnable that will be executed for all created threads.
-   * @note The initializer runnable can be executed by multiple threads if more than one thread is
-   *     created by the factory, and should take that into account to avoid race conditions.
-   */
-  public ThreadFactory withInitializer(Runnable initializer) {
-    return new AgentThreadFactory(this.threadName, initializer);
   }
 
   @Override
-  public Thread newThread(final Runnable r) {
-    Runnable runnable = r;
-    if (this.initializer != null) {
-      runnable =
-          new Runnable() {
-            @Override
-            public void run() {
-              initializer.run();
-              r.run();
-            }
-          };
-    }
+  public Thread newThread(final Runnable runnable) {
     final Thread thread = new Thread(AGENT_THREAD_GROUP, runnable, threadName);
     thread.setDaemon(true);
     thread.setContextClassLoader(null);
