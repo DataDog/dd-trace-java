57c7341fe9e5295760cdf02c1d04fe4377fb3dd2
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2020-10-22 12:16:47 +0100

    Elasticsearch 7 instrumentation

---
+++ dd-java-agent/instrumentation/elasticsearch/transport-7.3/src/main/java/datadog/trace/instrumentation/elasticsearch7_3/Elasticsearch73TransportClientInstrumentation.java
@@ -0,0 +1,95 @@
+package datadog.trace.instrumentation.elasticsearch7_3;
+
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
+import static datadog.trace.instrumentation.elasticsearch.ElasticsearchTransportClientDecorator.DECORATE;
+import static datadog.trace.instrumentation.elasticsearch.ElasticsearchTransportClientDecorator.ELASTICSEARCH_QUERY;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.bootstrap.instrumentation.api.AgentScope;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.ActionRequest;
+import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.action.ActionType;
+
+/**
+ * Most of this class is identical to version 5's instrumentation, but they changed an interface to
+ * an abstract class, so the bytecode isn't directly compatible.
+ */
+@AutoService(Instrumenter.class)
+public class Elasticsearch73TransportClientInstrumentation extends Instrumenter.Default {
+
+  public Elasticsearch73TransportClientInstrumentation() {
+    super("elasticsearch", "elasticsearch-transport", "elasticsearch-transport-7");
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    // If we want to be more generic, we could instrument the interface instead:
+    // .and(safeHasSuperType(named("org.elasticsearch.client.ElasticsearchClient"))))
+    return named("org.elasticsearch.client.support.AbstractClient");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      "datadog.trace.instrumentation.elasticsearch.ElasticsearchTransportClientDecorator",
+      packageName + ".TransportActionListener",
+    };
+  }
+
+  @Override
+  public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
+    return singletonMap(
+        isMethod()
+            .and(named("execute"))
+            .and(takesArgument(0, named("org.elasticsearch.action.ActionType")))
+            .and(takesArgument(1, named("org.elasticsearch.action.ActionRequest")))
+            .and(takesArgument(2, named("org.elasticsearch.action.ActionListener"))),
+        Elasticsearch73TransportClientInstrumentation.class.getName()
+            + "$Elasticsearch73TransportClientAdvice");
+  }
+
+  public static class Elasticsearch73TransportClientAdvice {
+
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static AgentScope onEnter(
+        @Advice.Argument(0) final ActionType action,
+        @Advice.Argument(1) final ActionRequest actionRequest,
+        @Advice.Argument(value = 2, readOnly = false)
+            ActionListener<ActionResponse> actionListener) {
+
+      final AgentSpan span = startSpan(ELASTICSEARCH_QUERY);
+      DECORATE.afterStart(span);
+      DECORATE.onRequest(span, action.getClass(), actionRequest.getClass());
+
+      actionListener = new TransportActionListener<>(actionRequest, actionListener, span);
+
+      return activateSpan(span);
+    }
+
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void stopSpan(
+        @Advice.Enter final AgentScope scope, @Advice.Thrown final Throwable throwable) {
+      if (throwable != null) {
+        final AgentSpan span = scope.span();
+        DECORATE.onError(span, throwable);
+        DECORATE.beforeFinish(span);
+        span.finish();
+      }
+      scope.close();
+      // span finished by TransportActionListener
+    }
+  }
+}
---
+++ dd-java-agent/instrumentation/elasticsearch/transport-7.3/src/test/groovy/Elasticsearch73TransportClientTest.groovy
@@ -0,0 +1,303 @@
+import datadog.trace.agent.test.AgentTestRunner
+import datadog.trace.api.DDSpanTypes
+import datadog.trace.bootstrap.instrumentation.api.Tags
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest
+import org.elasticsearch.client.transport.TransportClient
+import org.elasticsearch.common.io.FileSystemUtils
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.transport.TransportAddress
+import org.elasticsearch.index.IndexNotFoundException
+import org.elasticsearch.node.InternalSettingsPreparer
+import org.elasticsearch.node.Node
+import org.elasticsearch.transport.Netty4Plugin
+import org.elasticsearch.transport.RemoteTransportException
+import org.elasticsearch.transport.TransportService
+import org.elasticsearch.transport.client.PreBuiltTransportClient
+import spock.lang.Retry
+import spock.lang.Shared
+
+import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
+import static org.elasticsearch.cluster.ClusterName.CLUSTER_NAME_SETTING
+
+@Retry(count = 3, delay = 1000, mode = Retry.Mode.SETUP_FEATURE_CLEANUP)
+class Elasticsearch73TransportClientTest extends AgentTestRunner {
+  public static final long TIMEOUT = 10000; // 10 seconds
+
+  @Shared
+  TransportAddress tcpPublishAddress
+  @Shared
+  Node testNode
+  @Shared
+  File esWorkingDir
+  @Shared
+  String clusterName = UUID.randomUUID().toString()
+
+  @Shared
+  TransportClient client
+
+  def setupSpec() {
+    esWorkingDir = File.createTempDir("test-es-working-dir-", "")
+    esWorkingDir.deleteOnExit()
+    println "ES work dir: $esWorkingDir"
+
+    def settings = Settings.builder()
+      .put("path.home", esWorkingDir.path)
+      .put(CLUSTER_NAME_SETTING.getKey(), clusterName)
+      .put("node.name", "test-node")
+      .put("transport.type", "netty4")
+      .build()
+    testNode = new Node(InternalSettingsPreparer.prepareEnvironment(
+        settings, [:], null, null), [Netty4Plugin], false) {}
+    testNode.start()
+    tcpPublishAddress = testNode.injector().getInstance(TransportService).boundAddress().publishAddress()
+
+    client = new PreBuiltTransportClient(
+      Settings.builder()
+      // Since we use listeners to close spans this should make our span closing deterministic which is good for tests
+        .put("thread_pool.listener.size", 1)
+        .put(CLUSTER_NAME_SETTING.getKey(), clusterName)
+        .build()
+    )
+    client.addTransportAddress(tcpPublishAddress)
+    runUnderTrace("setup") {
+      // this may potentially create multiple requests and therefore multiple spans, so we wrap this call
+      // into a top level trace to get exactly one trace in the result.
+      client.admin().cluster().prepareHealth().setWaitForYellowStatus().execute().actionGet(TIMEOUT)
+    }
+    TEST_WRITER.waitForTraces(1)
+  }
+
+  def cleanupSpec() {
+    testNode?.close()
+    if (esWorkingDir != null) {
+      FileSystemUtils.deleteSubDirectories(esWorkingDir.toPath())
+      esWorkingDir.delete()
+    }
+  }
+
+  def "test elasticsearch status"() {
+    setup:
+    def result = client.admin().cluster().health(new ClusterHealthRequest())
+
+    def status = result.get().status
+
+    expect:
+    status.name() == "GREEN"
+
+    assertTraces(1) {
+      trace(1) {
+        span {
+          serviceName "elasticsearch"
+          resourceName "ClusterHealthAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          tags {
+            "$Tags.COMPONENT" "elasticsearch-java"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.PEER_HOSTNAME" "localhost"
+            "$Tags.PEER_HOST_IPV4" tcpPublishAddress.address
+            "$Tags.PEER_PORT" tcpPublishAddress.port
+            "$Tags.DB_TYPE" "elasticsearch"
+            "elasticsearch.action" "ClusterHealthAction"
+            "elasticsearch.request" "ClusterHealthRequest"
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "test elasticsearch error"() {
+    when:
+    client.prepareGet(indexName, indexType, id).get()
+
+    then:
+    thrown IndexNotFoundException
+
+    and:
+    assertTraces(1) {
+      trace(1) {
+        span {
+          serviceName "elasticsearch"
+          resourceName "GetAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          errored true
+          tags {
+            "$Tags.COMPONENT" "elasticsearch-java"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.DB_TYPE" "elasticsearch"
+            "elasticsearch.action" "GetAction"
+            "elasticsearch.request" "GetRequest"
+            "elasticsearch.request.indices" indexName
+            errorTags RemoteTransportException, String
+            defaultTags()
+          }
+        }
+      }
+    }
+
+    where:
+    indexName = "invalid-index"
+    indexType = "test-type"
+    id = "1"
+  }
+
+  def "test elasticsearch get"() {
+    setup:
+    assert TEST_WRITER == []
+    def indexResult = client.admin().indices().prepareCreate(indexName).get()
+    TEST_WRITER.waitForTraces(1)
+
+    expect:
+    indexResult.index() == indexName
+    TEST_WRITER.size() == 1
+
+    when:
+    def emptyResult = client.prepareGet(indexName, indexType, id).get()
+
+    then:
+    !emptyResult.isExists()
+    emptyResult.id == id
+    emptyResult.type == indexType
+    emptyResult.index == indexName
+
+    when:
+    def createResult = client.prepareIndex(indexName, indexType, id).setSource([:]).get()
+
+    then:
+    createResult.id == id
+    createResult.type == indexType
+    createResult.index == indexName
+    createResult.status().status == 201
+
+    when:
+    def result = client.prepareGet(indexName, indexType, id).get()
+
+    then:
+    result.isExists()
+    result.id == id
+    result.type == indexType
+    result.index == indexName
+
+    and:
+    assertTraces(5) {
+      trace(1) {
+        span {
+          serviceName "elasticsearch"
+          resourceName "CreateIndexAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          tags {
+            "$Tags.COMPONENT" "elasticsearch-java"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.PEER_HOSTNAME" "localhost"
+            "$Tags.PEER_HOST_IPV4" tcpPublishAddress.address
+            "$Tags.PEER_PORT" tcpPublishAddress.port
+            "$Tags.DB_TYPE" "elasticsearch"
+            "elasticsearch.action" "CreateIndexAction"
+            "elasticsearch.request" "CreateIndexRequest"
+            "elasticsearch.request.indices" indexName
+            defaultTags()
+          }
+        }
+      }
+      trace(1) {
+        span {
+          serviceName "elasticsearch"
+          resourceName "GetAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          tags {
+            "$Tags.COMPONENT" "elasticsearch-java"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.PEER_HOSTNAME" "localhost"
+            "$Tags.PEER_HOST_IPV4" tcpPublishAddress.address
+            "$Tags.PEER_PORT" tcpPublishAddress.port
+            "$Tags.DB_TYPE" "elasticsearch"
+            "elasticsearch.action" "GetAction"
+            "elasticsearch.request" "GetRequest"
+            "elasticsearch.request.indices" indexName
+            "elasticsearch.type" indexType
+            "elasticsearch.id" "1"
+            "elasticsearch.version"(-1)
+            defaultTags()
+          }
+        }
+      }
+      trace(1) {
+        span {
+          serviceName "elasticsearch"
+          resourceName "IndexAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          tags {
+            "$Tags.COMPONENT" "elasticsearch-java"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.PEER_HOSTNAME" "localhost"
+            "$Tags.PEER_HOST_IPV4" tcpPublishAddress.address
+            "$Tags.PEER_PORT" tcpPublishAddress.port
+            "$Tags.DB_TYPE" "elasticsearch"
+            "elasticsearch.action" "IndexAction"
+            "elasticsearch.request" "IndexRequest"
+            "elasticsearch.request.indices" indexName
+            "elasticsearch.request.write.type" indexType
+            "elasticsearch.request.write.version"(-3)
+            "elasticsearch.response.status" 201
+            "elasticsearch.shard.replication.total" 2
+            "elasticsearch.shard.replication.successful" 1
+            "elasticsearch.shard.replication.failed" 0
+            defaultTags()
+          }
+        }
+      }
+      trace(1) {
+        span {
+          serviceName "elasticsearch"
+          resourceName "PutMappingAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          tags {
+            "$Tags.COMPONENT" "elasticsearch-java"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.DB_TYPE" "elasticsearch"
+            "elasticsearch.action" "PutMappingAction"
+            "elasticsearch.request" "PutMappingRequest"
+            defaultTags()
+          }
+        }
+      }
+      trace(1) {
+        span {
+          serviceName "elasticsearch"
+          resourceName "GetAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          tags {
+            "$Tags.COMPONENT" "elasticsearch-java"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.PEER_HOSTNAME" "localhost"
+            "$Tags.PEER_HOST_IPV4" tcpPublishAddress.address
+            "$Tags.PEER_PORT" tcpPublishAddress.port
+            "$Tags.DB_TYPE" "elasticsearch"
+            "elasticsearch.action" "GetAction"
+            "elasticsearch.request" "GetRequest"
+            "elasticsearch.request.indices" indexName
+            "elasticsearch.type" indexType
+            "elasticsearch.id" "1"
+            "elasticsearch.version" 1
+            defaultTags()
+          }
+        }
+      }
+    }
+
+    cleanup:
+    client.admin().indices().prepareDelete(indexName).get()
+
+    where:
+    indexName = "test-index"
+    indexType = "test-type"
+    id = "1"
+  }
+}
