a0db32e071f6bfb11af9dcabdf4a9827e5835b49
Laplie Anderson <laplie.anderson@datadoghq.com>
2020-10-26 13:04:23 -0400

    Updating ConfigReset test

--- dd-java-agent/testing/src/test/groovy/ConfigResetTest.groovy
+++ dd-java-agent/testing/src/test/groovy/ConfigResetTest.groovy
@@ -5,27 +5,25 @@ import spock.lang.Shared
 class ConfigResetTest extends AgentTestRunner {
 
   @Shared
-  def sharedInstance = new ShouldBeConstructedWithoutConfig()
+  def sharedInstance = checkStaticAssertions()
 
-  @Shared
-  def sharedInstanceCreatedInSetup
-
-  def notSharedInstance = new ShouldBeConstructedWithoutConfig()
-
-  def notSharedInstanceCreatedInSetup
+  def notSharedInstance = checkStaticAssertions()
 
   def setupSpec() {
-    sharedInstanceCreatedInSetup = new ShouldBeConstructedWithoutConfig()
+    checkStaticAssertions()
   }
 
   def setup() {
-    notSharedInstanceCreatedInSetup = new ShouldBeConstructedWithoutConfig()
+    checkStaticAssertions()
   }
 
-  static void checkStaticAssertions() {
+  static Object checkStaticAssertions() {
     assert System.getProperty("dd.trace.enabled") == null
     assert System.getenv("DD_TRACE_ENABLED") == null
     assert Config.get().isTraceEnabled()
+
+    // Returning a new object so this can be used in field initializations
+    return new Object()
   }
 
   void checkAssertions() {
@@ -33,11 +31,7 @@ class ConfigResetTest extends AgentTestRunner {
 
     // These assertions are to satisfy codeNarc
     assert sharedInstance != null
-    assert sharedInstanceCreatedInSetup != null
     assert notSharedInstance != null
-    assert notSharedInstanceCreatedInSetup != null
-    assert sharedInstanceCreatedInSetup != null
-    assert notSharedInstanceCreatedInSetup != null
   }
 
   void injectAllConfigs() {
@@ -45,10 +39,9 @@ class ConfigResetTest extends AgentTestRunner {
     injectEnvConfig("DD_TRACE_ENABLED", "false")
   }
 
-  def "first test"() {
+  def "first test has clean config"() {
     setup:
     checkAssertions()
-    def preInstance = new ShouldBeConstructedWithoutConfig()
 
     injectAllConfigs()
 
@@ -56,7 +49,6 @@ class ConfigResetTest extends AgentTestRunner {
     injectAllConfigs()
 
     then:
-    assert preInstance != null
     injectAllConfigs()
     noExceptionThrown()
 
@@ -64,10 +56,9 @@ class ConfigResetTest extends AgentTestRunner {
     injectAllConfigs()
   }
 
-  def "second test"() {
+  def "second test has clean config"() {
     setup:
     checkAssertions()
-    def preInstance = new ShouldBeConstructedWithoutConfig()
 
     injectAllConfigs()
 
@@ -75,17 +66,155 @@ class ConfigResetTest extends AgentTestRunner {
     injectAllConfigs()
 
     then:
-    assert preInstance != null
     injectAllConfigs()
     noExceptionThrown()
 
     cleanup:
     injectAllConfigs()
   }
-}
 
-class ShouldBeConstructedWithoutConfig {
-  ShouldBeConstructedWithoutConfig() {
-    ConfigResetTest.checkStaticAssertions()
+  def "injecting system config"() {
+    setup:
+    checkAssertions()
+
+    when:
+    injectSysConfig("dd.trace.enabled", "true")
+
+    then:
+    System.getProperty("dd.trace.enabled") == "true"
+    Config.get().isTraceEnabled()
+
+    when:
+    injectSysConfig("dd.trace.enabled", "false")
+
+    then:
+    System.getProperty("dd.trace.enabled") == "false"
+    !Config.get().isTraceEnabled()
+  }
+
+  def "injecting system config without prefix"() {
+    setup:
+    checkAssertions()
+
+    when:
+    injectSysConfig("trace.enabled", "true")
+
+    then:
+    System.getProperty("dd.trace.enabled") == "true"
+    Config.get().isTraceEnabled()
+
+    when:
+    injectSysConfig("trace.enabled", "false")
+
+    then:
+    System.getProperty("dd.trace.enabled") == "false"
+    !Config.get().isTraceEnabled()
+
+  }
+
+  def "removing sys config"() {
+    setup:
+    checkAssertions()
+
+    when:
+    injectSysConfig("dd.trace.enabled", "false")
+
+    then:
+    System.getProperty("dd.trace.enabled") == "false"
+    !Config.get().isTraceEnabled()
+
+    when:
+    removeSysConfig("dd.trace.enabled")
+
+    then:
+    System.getProperty("dd.trace.enabled") == null
+    Config.get().isTraceEnabled()
+
+    when:
+    injectSysConfig("dd.trace.enabled", "false")
+
+    then:
+    System.getProperty("dd.trace.enabled") == "false"
+    !Config.get().isTraceEnabled()
+
+    when:
+    removeSysConfig("trace.enabled")
+
+    then:
+    System.getProperty("dd.trace.enabled") == null
+    Config.get().isTraceEnabled()
+  }
+
+  def "injecting env config"() {
+    setup:
+    checkAssertions()
+
+    when:
+    injectEnvConfig("DD_TRACE_ENABLED", "true")
+
+    then:
+    System.getenv("DD_TRACE_ENABLED") == "true"
+    Config.get().isTraceEnabled()
+
+    when:
+    injectEnvConfig("DD_TRACE_ENABLED", "false")
+
+    then:
+    System.getenv("DD_TRACE_ENABLED") == "false"
+    !Config.get().isTraceEnabled()
+  }
+
+  def "injecting env config without prefix"() {
+    setup:
+    checkAssertions()
+
+    when:
+    injectEnvConfig("TRACE_ENABLED", "true")
+
+    then:
+    System.getenv("DD_TRACE_ENABLED") == "true"
+    Config.get().isTraceEnabled()
+
+    when:
+    injectEnvConfig("TRACE_ENABLED", "false")
+
+    then:
+    System.getenv("DD_TRACE_ENABLED") == "false"
+    !Config.get().isTraceEnabled()
+
+  }
+
+  def "removing env config"() {
+    setup:
+    checkAssertions()
+
+    when:
+    injectEnvConfig("DD_TRACE_ENABLED", "false")
+
+    then:
+    System.getenv("DD_TRACE_ENABLED") == "false"
+    !Config.get().isTraceEnabled()
+
+    when:
+    removeEnvConfig("DD_TRACE_ENABLED")
+
+    then:
+    System.getenv("DD_TRACE_ENABLED") == null
+    Config.get().isTraceEnabled()
+
+    when:
+    injectEnvConfig("DD_TRACE_ENABLED", "false")
+
+    then:
+    System.getenv("DD_TRACE_ENABLED") == "false"
+    !Config.get().isTraceEnabled()
+
+    when:
+    removeEnvConfig("TRACE_ENABLED")
+
+    then:
+    System.getenv("DD_TRACE_ENABLED") == null
+    Config.get().isTraceEnabled()
   }
 }
+
