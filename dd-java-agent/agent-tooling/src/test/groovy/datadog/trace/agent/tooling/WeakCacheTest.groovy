e583356b8372f331557e01d5a5f33197e9a5c4b0
Laplie Anderson <laplie.anderson@datadoghq.com>
2020-10-29 19:36:00 -0400

    Update test

--- dd-java-agent/agent-tooling/src/test/groovy/datadog/trace/agent/tooling/WeakCacheTest.groovy
+++ dd-java-agent/agent-tooling/src/test/groovy/datadog/trace/agent/tooling/WeakCacheTest.groovy
@@ -7,64 +7,68 @@ class WeakCacheTest extends Specification {
   def supplier = new CounterSupplier()
 
   def weakCache = AgentTooling.newWeakCache()
-  def weakCacheFor1elem = AgentTooling.newWeakCache(1)
 
-  def "getOrCreate a value"() {
+  def "computeIfAbsent a value"() {
     when:
-    def count = weakCache.get('key', supplier)
+    def count = weakCache.computeIfAbsent('key', supplier)
 
     then:
     count == 1
     supplier.counter == 1
-    weakCache.cache.size() == 1
+    weakCache.getIfPresent('key') == 1
   }
 
-  def "getOrCreate a value multiple times same class loader same key"() {
+  def "computeIfAbsent a value multiple times same class loader same key"() {
     when:
-    def count1 = weakCache.get('key', supplier)
-    def count2 = weakCache.get('key', supplier)
+    def count1 = weakCache.computeIfAbsent('key', supplier)
+    def count2 = weakCache.computeIfAbsent('key', supplier)
 
     then:
     count1 == 1
     count2 == 1
     supplier.counter == 1
-    weakCache.cache.size() == 1
+    weakCache.getIfPresent('key') == 1
   }
 
-  def "getOrCreate a value multiple times same class loader different keys"() {
+  def "computeIfAbsent a value multiple times same class loader different keys"() {
     when:
-    def count1 = weakCache.get('key1', supplier)
-    def count2 = weakCache.get('key2', supplier)
+    def count1 = weakCache.computeIfAbsent('key1', supplier)
+    def count2 = weakCache.computeIfAbsent('key2', supplier)
 
     then:
     count1 == 1
     count2 == 2
     supplier.counter == 2
-    weakCache.cache.size() == 2
+    weakCache.getIfPresent('key1') == 1
+    weakCache.getIfPresent('key2') == 2
   }
 
   def "max size check"() {
+    setup:
+    def weakCacheFor1elem = AgentTooling.newWeakCache(1)
+
     when:
-    def sizeBefore = weakCacheFor1elem.cache.size()
     def valBefore = weakCacheFor1elem.getIfPresent("key1")
-    def sizeAfter = weakCacheFor1elem.cache.size()
-    def valAfterGet = weakCacheFor1elem.getIfPresentOrCompute("key1", supplier)
-    def sizeAfterCompute = weakCacheFor1elem.cache.size()
-    weakCacheFor1elem.put("key1", 42)
-    def valAfterPut = weakCacheFor1elem.getIfPresentOrCompute("key1", supplier)
-    def valByKey2 = weakCacheFor1elem.getIfPresentOrCompute("key2", supplier)
-    def valAfterReplace = weakCacheFor1elem.getIfPresent("key1")
+    def valAfterGet = weakCacheFor1elem.computeIfAbsent("key1", supplier)
 
     then:
     valBefore == null
     valAfterGet == 1
-    sizeBefore == 0
-    sizeAfter == 0
-    sizeAfterCompute == 1
-    valAfterPut == 42
+
+    when:
+    weakCacheFor1elem.put("key1", 42)
+
+    then:
+    weakCacheFor1elem.computeIfAbsent("key1", supplier) == 42
+
+    when:
+    def valByKey2 = weakCacheFor1elem.computeIfAbsent("key2", supplier)
+
+    then:
     valByKey2 == 2
-    valAfterReplace == null
-    weakCacheFor1elem.cache.size() == 1
+
+    // The following check doesn't work because caches are allowed to temporarily exceed the max size
+    //    weakCacheFor1elem.getIfPresent("key1") == null
   }
 
   class CounterSupplier implements Function<String, Integer> {
