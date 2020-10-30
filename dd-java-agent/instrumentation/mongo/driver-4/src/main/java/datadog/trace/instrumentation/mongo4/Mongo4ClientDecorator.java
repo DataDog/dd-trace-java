0f4f768654ad23eaf6ebd5bb448440d1f47e5007
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2020-10-30 11:50:28 +0000

    MongoDB 4 driver instrumentation
    Covers both sync and reactivestreams clients

---
+++ dd-java-agent/instrumentation/mongo/driver-4/src/main/java/datadog/trace/instrumentation/mongo4/Mongo4ClientDecorator.java
@@ -0,0 +1,122 @@
+package datadog.trace.instrumentation.mongo4;
+
+import com.mongodb.connection.ConnectionDescription;
+import com.mongodb.event.CommandStartedEvent;
+import datadog.trace.api.DDSpanTypes;
+import datadog.trace.api.DDTags;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
+import datadog.trace.bootstrap.instrumentation.decorator.DBTypeProcessingDatabaseClientDecorator;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.bson.BsonArray;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
+import org.bson.BsonValue;
+
+public class Mongo4ClientDecorator
+    extends DBTypeProcessingDatabaseClientDecorator<CommandStartedEvent> {
+
+  public static final UTF8BytesString MONGO_QUERY = UTF8BytesString.createConstant("mongo.query");
+
+  public static final Mongo4ClientDecorator DECORATE = new Mongo4ClientDecorator();
+
+  @Override
+  protected String[] instrumentationNames() {
+    return new String[] {"mongo"};
+  }
+
+  @Override
+  protected String service() {
+    return "mongo";
+  }
+
+  @Override
+  protected CharSequence component() {
+    return "java-mongo";
+  }
+
+  @Override
+  protected CharSequence spanType() {
+    return DDSpanTypes.MONGO;
+  }
+
+  @Override
+  protected String dbType() {
+    return "mongo";
+  }
+
+  @Override
+  protected String dbUser(final CommandStartedEvent event) {
+    return null;
+  }
+
+  @Override
+  protected String dbInstance(final CommandStartedEvent event) {
+    return event.getDatabaseName();
+  }
+
+  @Override
+  protected String dbHostname(CommandStartedEvent event) {
+    final ConnectionDescription connectionDescription = event.getConnectionDescription();
+    if (connectionDescription != null) {
+      return connectionDescription.getServerAddress().getHost();
+    }
+
+    return null;
+  }
+
+  public AgentSpan onStatement(final AgentSpan span, final BsonDocument statement) {
+
+    // scrub the Mongo command so that parameters are removed from the string
+    final BsonDocument scrubbed = scrub(statement);
+    final String mongoCmd = scrubbed.toString();
+
+    span.setTag(DDTags.RESOURCE_NAME, mongoCmd);
+    return onStatement(span, mongoCmd);
+  }
+
+  /**
+   * The values of these mongo fields will not be scrubbed out. This allows the non-sensitive
+   * collection names to be captured.
+   */
+  private static final List<String> UNSCRUBBED_FIELDS =
+      Arrays.asList("ordered", "insert", "count", "find", "create");
+
+  private static final BsonValue HIDDEN_CHAR = new BsonString("?");
+
+  private static BsonDocument scrub(final BsonDocument origin) {
+    final BsonDocument scrub = new BsonDocument();
+    for (final Map.Entry<String, BsonValue> entry : origin.entrySet()) {
+      if (UNSCRUBBED_FIELDS.contains(entry.getKey()) && entry.getValue().isString()) {
+        scrub.put(entry.getKey(), entry.getValue());
+      } else {
+        final BsonValue child = scrub(entry.getValue());
+        scrub.put(entry.getKey(), child);
+      }
+    }
+    return scrub;
+  }
+
+  private static BsonValue scrub(final BsonArray origin) {
+    final BsonArray scrub = new BsonArray();
+    for (final BsonValue value : origin) {
+      final BsonValue child = scrub(value);
+      scrub.add(child);
+    }
+    return scrub;
+  }
+
+  private static BsonValue scrub(final BsonValue origin) {
+    final BsonValue scrubbed;
+    if (origin.isDocument()) {
+      scrubbed = scrub(origin.asDocument());
+    } else if (origin.isArray()) {
+      scrubbed = scrub(origin.asArray());
+    } else {
+      scrubbed = HIDDEN_CHAR;
+    }
+    return scrubbed;
+  }
+}
---
+++ dd-java-agent/instrumentation/mongo/driver-4/src/test/groovy/Mongo4ReactiveClientTest.groovy
@@ -0,0 +1,302 @@
+import com.mongodb.client.result.DeleteResult
+import com.mongodb.client.result.UpdateResult
+import com.mongodb.reactivestreams.client.MongoClient
+import com.mongodb.reactivestreams.client.MongoClients
+import com.mongodb.reactivestreams.client.MongoCollection
+import com.mongodb.reactivestreams.client.MongoDatabase
+import datadog.trace.agent.test.asserts.TraceAssert
+import datadog.trace.api.DDSpanTypes
+import datadog.trace.bootstrap.instrumentation.api.Tags
+import datadog.trace.core.DDSpan
+import org.bson.BsonDocument
+import org.bson.BsonString
+import org.bson.Document
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
+import spock.lang.Shared
+import spock.lang.Timeout
+
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CountDownLatch
+
+import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
+
+@Timeout(10)
+class Mongo4ReactiveClientTest extends MongoBaseTest {
+
+  @Shared
+  MongoClient client
+
+  def setup() throws Exception {
+    client = MongoClients.create("mongodb://localhost:$port/?appname=some-instance")
+  }
+
+  def cleanup() throws Exception {
+    client?.close()
+    client = null
+  }
+
+  def "test create collection"() {
+    setup:
+    MongoDatabase db = client.getDatabase(dbName)
+
+    when:
+    db.createCollection(collectionName).subscribe(toSubscriber {})
+
+    then:
+    assertTraces(1) {
+      trace(1) {
+        mongoSpan(it, 0) {
+          assert it.replaceAll(" ", "") == "{\"create\":\"$collectionName\",\"capped\":\"?\"}" ||
+            it == "{\"create\": \"$collectionName\", \"capped\": \"?\", \"\$db\": \"?\", \"\$readPreference\": {\"mode\": \"?\"}}"
+          true
+        }
+      }
+    }
+
+    where:
+    dbName = "test_db"
+    collectionName = "testCollection"
+  }
+
+  def "test create collection no description"() {
+    setup:
+    MongoDatabase db = MongoClients.create("mongodb://localhost:$port").getDatabase(dbName)
+
+    when:
+    db.createCollection(collectionName).subscribe(toSubscriber {})
+
+    then:
+    assertTraces(1) {
+      trace(1) {
+        mongoSpan(it, 0, {
+          assert it.replaceAll(" ", "") == "{\"create\":\"$collectionName\",\"capped\":\"?\"}" ||
+            it == "{\"create\": \"$collectionName\", \"capped\": \"?\", \"\$db\": \"?\", \"\$readPreference\": {\"mode\": \"?\"}}"
+          true
+        }, dbName)
+      }
+    }
+
+    where:
+    dbName = "test_db"
+    collectionName = "testCollection"
+  }
+
+  def "test get collection"() {
+    setup:
+    MongoDatabase db = client.getDatabase(dbName)
+
+    when:
+    def count = new CompletableFuture()
+    db.getCollection(collectionName).estimatedDocumentCount().subscribe(toSubscriber { count.complete(it) })
+
+    then:
+    count.get() == 0
+    assertTraces(1) {
+      trace(1) {
+        mongoSpan(it, 0) {
+          assert it.replaceAll(" ", "") == "{\"count\":\"$collectionName\",\"query\":{}}" ||
+            it == "{\"count\": \"$collectionName\", \"query\": {}, \"\$db\": \"?\", \"\$readPreference\": {\"mode\": \"?\"}}"
+          true
+        }
+      }
+    }
+
+    where:
+    dbName = "test_db"
+    collectionName = "testCollection"
+  }
+
+  def "test insert"() {
+    setup:
+    MongoCollection<Document> collection = runUnderTrace("setup") {
+      MongoDatabase db = client.getDatabase(dbName)
+      def latch1 = new CountDownLatch(1)
+      // This creates a trace that isn't linked to the parent... using NIO internally that we don't handle.
+      db.createCollection(collectionName).subscribe(toSubscriber { latch1.countDown() })
+      latch1.await()
+      return db.getCollection(collectionName)
+    }
+    TEST_WRITER.waitForTraces(2)
+    TEST_WRITER.clear()
+
+    when:
+    def count = new CompletableFuture()
+    collection.insertOne(new Document("password", "SECRET")).subscribe(toSubscriber {
+      collection.estimatedDocumentCount().subscribe(toSubscriber { count.complete(it) })
+    })
+
+    then:
+    count.get() == 1
+    assertTraces(2) {
+      trace(1) {
+        mongoSpan(it, 0) {
+          assert it.replaceAll(" ", "") == "{\"insert\":\"$collectionName\",\"ordered\":\"?\",\"documents\":[{\"_id\":\"?\",\"password\":\"?\"}]}" ||
+            it == "{\"insert\": \"$collectionName\", \"ordered\": \"?\", \"\$db\": \"?\", \"documents\": [{\"_id\": \"?\", \"password\": \"?\"}]}"
+          true
+        }
+      }
+      trace(1) {
+        mongoSpan(it, 0) {
+          assert it.replaceAll(" ", "") == "{\"count\":\"$collectionName\",\"query\":{}}" ||
+            it == "{\"count\": \"$collectionName\", \"query\": {}, \"\$db\": \"?\", \"\$readPreference\": {\"mode\": \"?\"}}"
+          true
+        }
+      }
+    }
+
+    where:
+    dbName = "test_db"
+    collectionName = "testCollection"
+  }
+
+  def "test update"() {
+    setup:
+    MongoCollection<Document> collection = runUnderTrace("setup") {
+      MongoDatabase db = client.getDatabase(dbName)
+      def latch1 = new CountDownLatch(1)
+      db.createCollection(collectionName).subscribe(toSubscriber { latch1.countDown() })
+      latch1.await()
+      def coll = db.getCollection(collectionName)
+      def latch2 = new CountDownLatch(1)
+      coll.insertOne(new Document("password", "OLDPW")).subscribe(toSubscriber { latch2.countDown() })
+      latch2.await()
+      return coll
+    }
+    TEST_WRITER.waitForTraces(1)
+    TEST_WRITER.clear()
+
+    when:
+    def result = new CompletableFuture<UpdateResult>()
+    def count = new CompletableFuture()
+    collection.updateOne(
+      new BsonDocument("password", new BsonString("OLDPW")),
+      new BsonDocument('$set', new BsonDocument("password", new BsonString("NEWPW")))).subscribe(toSubscriber {
+      result.complete(it)
+      collection.estimatedDocumentCount().subscribe(toSubscriber { count.complete(it) })
+    })
+
+    then:
+    result.get().modifiedCount == 1
+    count.get() == 1
+    assertTraces(2) {
+      trace(1) {
+        mongoSpan(it, 0) {
+          assert it.replaceAll(" ", "") == "{\"update\":\"?\",\"ordered\":\"?\",\"updates\":[{\"q\":{\"password\":\"?\"},\"u\":{\"\$set\":{\"password\":\"?\"}}}]}" ||
+            it == "{\"update\": \"?\", \"ordered\": \"?\", \"\$db\": \"?\", \"updates\": [{\"q\": {\"password\": \"?\"}, \"u\": {\"\$set\": {\"password\": \"?\"}}}]}"
+          true
+        }
+      }
+      trace(1) {
+        mongoSpan(it, 0) {
+          assert it.replaceAll(" ", "") == "{\"count\":\"$collectionName\",\"query\":{}}" ||
+            it == "{\"count\": \"$collectionName\", \"query\": {}, \"\$db\": \"?\", \"\$readPreference\": {\"mode\": \"?\"}}"
+          true
+        }
+      }
+    }
+
+    where:
+    dbName = "test_db"
+    collectionName = "testCollection"
+  }
+
+  def "test delete"() {
+    setup:
+    MongoCollection<Document> collection = runUnderTrace("setup") {
+      MongoDatabase db = client.getDatabase(dbName)
+      def latch1 = new CountDownLatch(1)
+      db.createCollection(collectionName).subscribe(toSubscriber { latch1.countDown() })
+      latch1.await()
+      def coll = db.getCollection(collectionName)
+      def latch2 = new CountDownLatch(1)
+      coll.insertOne(new Document("password", "SECRET")).subscribe(toSubscriber { latch2.countDown() })
+      latch2.await()
+      return coll
+    }
+    TEST_WRITER.waitForTraces(1)
+    TEST_WRITER.clear()
+
+    when:
+    def result = new CompletableFuture<DeleteResult>()
+    def count = new CompletableFuture()
+    collection.deleteOne(new BsonDocument("password", new BsonString("SECRET"))).subscribe(toSubscriber {
+      result.complete(it)
+      collection.estimatedDocumentCount().subscribe(toSubscriber { count.complete(it) })
+    })
+
+    then:
+    result.get().deletedCount == 1
+    count.get() == 0
+    assertTraces(2) {
+      trace(1) {
+        mongoSpan(it, 0) {
+          assert it.replaceAll(" ", "") == "{\"delete\":\"?\",\"ordered\":\"?\",\"deletes\":[{\"q\":{\"password\":\"?\"},\"limit\":\"?\"}]}" ||
+            it == "{\"delete\": \"?\", \"ordered\": \"?\", \"\$db\": \"?\", \"deletes\": [{\"q\": {\"password\": \"?\"}, \"limit\": \"?\"}]}"
+          true
+        }
+      }
+      trace(1) {
+        mongoSpan(it, 0) {
+          assert it.replaceAll(" ", "") == "{\"count\":\"$collectionName\",\"query\":{}}" ||
+            it == "{\"count\": \"$collectionName\", \"query\": {}, \"\$db\": \"?\", \"\$readPreference\": {\"mode\": \"?\"}}"
+          true
+        }
+      }
+    }
+
+    where:
+    dbName = "test_db"
+    collectionName = "testCollection"
+  }
+
+  def Subscriber<?> toSubscriber(Closure closure) {
+    return new Subscriber() {
+      boolean hasResult
+
+      @Override
+      void onSubscribe(Subscription s) {
+        s.request(1) // must request 1 value to trigger async call
+      }
+
+      @Override
+      void onNext(Object o) { hasResult = true; closure.call(o) }
+
+      @Override
+      void onError(Throwable t) { hasResult = true; closure.call(t) }
+
+      @Override
+      void onComplete() {
+        if (!hasResult) {
+          hasResult = true
+          closure.call()
+        }
+      }
+    }
+  }
+
+  def mongoSpan(TraceAssert trace, int index, Closure<Boolean> statementEval, String instance = "some-instance", Object parentSpan = null, Throwable exception = null) {
+    trace.span {
+      serviceName "mongo"
+      operationName "mongo.query"
+      resourceName statementEval
+      spanType DDSpanTypes.MONGO
+      if (parentSpan == null) {
+        parent()
+      } else {
+        childOf((DDSpan) parentSpan)
+      }
+      tags {
+        "$Tags.COMPONENT" "java-mongo"
+        "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+        "$Tags.PEER_HOSTNAME" "localhost"
+        "$Tags.PEER_HOST_IPV4" "127.0.0.1"
+        "$Tags.PEER_PORT" port
+        "$Tags.DB_STATEMENT" statementEval
+        "$Tags.DB_TYPE" "mongo"
+        "$Tags.DB_INSTANCE" instance
+        defaultTags()
+      }
+    }
+  }
+}
