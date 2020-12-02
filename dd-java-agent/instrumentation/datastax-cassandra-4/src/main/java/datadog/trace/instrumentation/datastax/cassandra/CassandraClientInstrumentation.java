c488deef87196fc3b847121ea25cb018a29b4dfd
Laplie Anderson <laplie.anderson@datadoghq.com>
2020-12-02 16:13:39 -0500

    Datastax Cassandra 4 instrumentation

---
+++ dd-java-agent/instrumentation/datastax-cassandra-4/src/main/java/datadog/trace/instrumentation/datastax/cassandra/CassandraClientInstrumentation.java
@@ -0,0 +1,41 @@
+package datadog.trace.instrumentation.datastax.cassandra;
+
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isStatic;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import java.util.Map;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+@AutoService(Instrumenter.class)
+public class CassandraClientInstrumentation extends Instrumenter.Default {
+
+  public CassandraClientInstrumentation() {
+    super("cassandra");
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return named("com.datastax.oss.driver.internal.core.session.DefaultSession");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      packageName + ".CassandraClientDecorator", packageName + ".TracingSession"
+    };
+  }
+
+  @Override
+  public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
+    return singletonMap(
+        isMethod().and(named("init")).and(isStatic()).and(takesArguments(3)),
+        packageName + ".CassandraClientAdvice");
+  }
+}
