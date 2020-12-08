dbcfa79f4649b43a0538d7a364ae53963c6f031f
Richard Startin <richard.startin@datadoghq.com>
2020-12-08 10:38:36 +0000

    implement a buffering mechanism for requests when the agent is slow to respond

--- dd-trace-core/src/test/groovy/datadog/trace/common/metrics/OkHttpSinkTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/common/metrics/OkHttpSinkTest.groovy
@@ -11,6 +11,10 @@ import okhttp3.ResponseBody
 import spock.lang.Requires
 
 import java.nio.ByteBuffer
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
 
 import static datadog.trace.api.Platform.isJavaVersionAtLeast
 import static datadog.trace.common.metrics.EventListener.EventType.BAD_PAYLOAD
@@ -27,7 +31,6 @@ class OkHttpSinkTest extends DDSpecification {
     String path = "v0.5/stats"
     EventListener listener = Mock(EventListener)
     OkHttpClient client = Mock(OkHttpClient)
-
     OkHttpSink sink = new OkHttpSink(client, agentUrl, path)
     sink.register(listener)
 
@@ -48,6 +51,53 @@ class OkHttpSinkTest extends DDSpecification {
     OK          | 201
   }
 
+  def "degrade to async mode when agent slow to respond"() {
+    // metrics payloads are relatively large and we don't want to copy them,
+    // and we typically expect the agent to respond well within the aggregation
+    // window, so will send synchronously whenever possible to avoid allocating
+    // a copy of the payload. When the agent is slow to respond, we degrade to
+    // an asynchronous mode where up to 100 seconds of requests are copied and
+    // enqueued for sending in the background, because we don't want to lose
+    // them if it's possible not to.
+    setup:
+    String agentUrl = "http://localhost:8126"
+    String path = "v0.5/stats"
+    CountDownLatch latch = new CountDownLatch(2)
+    EventListener listener = new BlockingListener(latch)
+    OkHttpClient client = Mock(OkHttpClient)
+    OkHttpSink sink = new OkHttpSink(client, agentUrl, path,
+      TimeUnit.MILLISECONDS.toNanos(100))
+    sink.register(listener)
+    AtomicBoolean first = new AtomicBoolean(true)
+
+    when: "one slow response followed by a request"
+    sink.accept(1, ByteBuffer.allocate(0))
+    sink.accept(1, ByteBuffer.allocate(0))
+    latch.await()
+    then: "the second request degrades to async mode"
+    2 * client.newCall(_) >> { Request request ->
+      if (first.compareAndSet(true, false)) {
+        Thread.sleep(101)
+      } else {
+        assert sink.isInDegradedMode()
+      }
+      respond(request, 200)
+    }
+    listener.events.size() == 2
+    for (EventListener.EventType eventType : listener.events) {
+      assert eventType == OK
+    }
+    long asyncRequests = sink.asyncRequestCount()
+    asyncRequests == 1
+    sink.isInDegradedMode()
+    when: "the agent has recovered and has responded quickly once"
+    sink.accept(1, ByteBuffer.allocate(0))
+    then: "the request was sent synchronously"
+    1 * client.newCall(_) >> { Request request -> respond(request, 200) }
+    asyncRequests == sink.asyncRequestCount()
+    !sink.isInDegradedMode()
+  }
+
   def respond(Request request, int code) {
     if (0 == code) {
       return error(request)
@@ -66,6 +116,22 @@ class OkHttpSinkTest extends DDSpecification {
   def error(Request request) {
     return Mock(Call) {
       it.execute() >> { throw new IOException("thrown by test") }
+    }
+  }
+
+  class BlockingListener implements EventListener {
+
+    private final CountDownLatch latch
+    private List<EventType> events = new CopyOnWriteArrayList<>()
+
+    BlockingListener(CountDownLatch latch) {
+      this.latch = latch
+    }
+
+    @Override
+    void onEvent(EventType eventType, String message) {
+      events.add(eventType)
+      latch.countDown()
     }
   }
 
