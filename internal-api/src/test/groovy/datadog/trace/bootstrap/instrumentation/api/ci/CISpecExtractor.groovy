de54f9b428ae874a2347e841455fb7683a58a8f1
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2020-12-17 16:24:19 +0100

    Fix codenarc

--- internal-api/src/test/groovy/datadog/trace/bootstrap/instrumentation/api/ci/CISpecExtractor.groovy
+++ internal-api/src/test/groovy/datadog/trace/bootstrap/instrumentation/api/ci/CISpecExtractor.groovy
@@ -4,13 +4,13 @@ import groovy.json.JsonSlurper
 
 class CISpecExtractor {
 
-  static def extract(String ciProviderName) {
+  static extract(String ciProviderName) {
     if ("noop" == ciProviderName) {
       return Arrays.asList(new CISpec(Collections.EMPTY_MAP, Collections.EMPTY_MAP))
     }
 
     def jsonSlurper = new JsonSlurper()
-    List<List<Map<String, String>>> spec = jsonSlurper.parse(CISpecExtractor.class.getClassLoader().getResourceAsStream(String.format("ci/%s.json", ciProviderName)))
+    List<List<Map<String, String>>> spec = jsonSlurper.parse(CISpecExtractor.getClassLoader().getResourceAsStream(String.format("ci/%s.json", ciProviderName)))
 
     def ciSpecs = new ArrayList<CISpec>()
     spec.each {
