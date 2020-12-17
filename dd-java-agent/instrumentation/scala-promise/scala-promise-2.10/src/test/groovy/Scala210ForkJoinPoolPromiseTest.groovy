fc1981acb9239376748e66e1ebdbeced0e004794
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2020-12-17 15:41:00 +0100

    Make Scala Promise 2.10 behave the same as 2.13

--- dd-java-agent/instrumentation/scala-promise/scala-promise-2.10/src/test/groovy/Scala210ForkJoinPoolPromiseTest.groovy
+++ dd-java-agent/instrumentation/scala-promise/scala-promise-2.10/src/test/groovy/Scala210ForkJoinPoolPromiseTest.groovy
@@ -1,10 +1,11 @@
 // Can't import both 'scala.concurrent.ExecutionContext' and '...ExecutionContext$', since CodeNarc
 // is broken and complains about duplicate import statements even though it isn't.
+import java.util.concurrent.ForkJoinPool
 import scala.concurrent.ExecutionContext$
 
-class Scala210PromiseGlobalTest extends ScalaPromiseTestBase {
+class Scala210ForkJoinPoolPromiseTest extends ScalaPromiseTestBase {
   @Override
   protected scala.concurrent.ExecutionContext getExecutionContext() {
-    return ExecutionContext$.MODULE$.global()
+    return ExecutionContext$.MODULE$.fromExecutor(ForkJoinPool.commonPool())
   }
 }
--- dd-java-agent/instrumentation/scala-promise/scala-promise-2.10/src/test/groovy/Scala210PromiseThreadPoolTest.groovy
+++ dd-java-agent/instrumentation/scala-promise/scala-promise-2.10/src/test/groovy/Scala210PromiseThreadPoolTest.groovy
@@ -1,10 +1,11 @@
 // Can't import both 'scala.concurrent.ExecutionContext' and '...ExecutionContext$', since CodeNarc
 // is broken and complains about duplicate import statements even though it isn't.
+import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext$
 
-class Scala210PromiseGlobalTest extends ScalaPromiseTestBase {
+class Scala210PromiseThreadPoolTest extends ScalaPromiseTestBase {
   @Override
   protected scala.concurrent.ExecutionContext getExecutionContext() {
-    return ExecutionContext$.MODULE$.global()
+    return ExecutionContext$.MODULE$.fromExecutorService(Executors.newCachedThreadPool())
   }
 }
