e88c4ff61944cfd96d6a329e7386f714df07b43b
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2020-12-21 11:37:40 -0500

    FFix the test compile for boot 2.4

--- dd-java-agent/instrumentation/spring-webflux-5/src/boot24Test/groovy/SingleThreadedSpringWebfluxTest.groovy
+++ dd-java-agent/instrumentation/spring-webflux-5/src/boot24Test/groovy/SingleThreadedSpringWebfluxTest.groovy
@@ -4,7 +4,8 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory
 import org.springframework.boot.web.embedded.netty.NettyServerCustomizer
 import org.springframework.context.annotation.Bean
-import reactor.ipc.netty.resources.LoopResources
+import reactor.netty.http.server.HttpServer
+import reactor.netty.resources.LoopResources
 
 /**
  * Run all Webflux tests under netty event loop having only 1 thread.
@@ -19,7 +20,7 @@ class SingleThreadedSpringWebfluxTest extends SpringWebfluxTest {
     @Bean
     NettyReactiveWebServerFactory nettyFactory() {
       def factory = new NettyReactiveWebServerFactory()
-      NettyServerCustomizer customizer = { builder -> builder.loopResources(LoopResources.create("my-http", 1, true)) }
+      NettyServerCustomizer customizer = { HttpServer httpServer -> httpServer.runOn(LoopResources.create("my-http", 1, true)) }
       factory.addServerCustomizers(customizer)
       return factory
     }
