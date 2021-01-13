347f52e0fea1d7bd9889cc5733c3928cec4a77b1
kevin.feng <kevin.feng@datadoghq.com>
2021-01-13 16:34:56 -0500

    [JAVAAPM-468] Rollback wrong changes

--- dd-java-agent/instrumentation/lettuce-4/src/test/groovy/Lettuce4SyncClientTest.groovy
+++ dd-java-agent/instrumentation/lettuce-4/src/test/groovy/Lettuce4SyncClientTest.groovy
@@ -180,6 +180,7 @@ class Lettuce4SyncClientTest extends AgentTestRunner {
     setup:
     String res = syncCommands.get("TESTKEY")
 
+    expect:
     res == "TESTVAL"
     assertTraces(1) {
       trace(1) {
