96695248bbcfc5b981e074d9e7de14ad02fb099e
Richard Startin <richard.startin@datadoghq.com>
2021-01-18 16:13:09 +0000

    add callable statement classes and limit the amount of class hierarchy traversal in matching

--- dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/ConnectionInstrumentation.java
+++ dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/ConnectionInstrumentation.java
@@ -13,6 +13,7 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers;
 import datadog.trace.api.Config;
 import datadog.trace.bootstrap.ContextStore;
 import datadog.trace.bootstrap.InstrumentationContext;
@@ -26,6 +27,57 @@ import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(Instrumenter.class)
 public final class ConnectionInstrumentation extends Instrumenter.Tracing {
+
+  private static final String[] CONCRETE_TYPES = {
+    // jt400
+    "com.ibm.as400.access.AS400JDBCConnection",
+    // possibly need more coverage
+    "com.microsoft.sqlserver.jdbc.SQLServerConnection",
+    // should cover mysql
+    "com.mysql.jdbc.Connection",
+    "com.mysql.jdbc.jdbc1.Connection",
+    "com.mysql.jdbc.jdbc2.Connection",
+    "com.mysql.jdbc.ConnectionImpl",
+    "com.mysql.jdbc.JDBC4Connection",
+    "com.mysql.cj.jdbc.JdbcConnection",
+    // should cover derby
+    "org.apache.derby.impl.jdbc.EmbedConnection",
+    // complete
+    "org.h2.jdbc.JdbcConnection",
+    // complete
+    "org.hsqldb.jdbc.JDBCConnection",
+    "org.hsqldb.jdbc.jdbcConnection",
+    "org.apache.hive.jdbc.HiveConnection",
+    // complete
+    "org.mariadb.jdbc.MariaDbConnection",
+    "org.mariadb.jdbc.MySQLConnection",
+
+    // postgresql seems to be complete
+    "org.postgresql.jdbc.PgConnection",
+    "org.postgresql.jdbc1.Connection",
+    "org.postgresql.jdbc1.Jdbc1Connection",
+    "org.postgresql.jdbc2.Connection",
+    "org.postgresql.jdbc2.Jdbc2Connection",
+    "org.postgresql.jdbc3.Jdbc3Connection",
+    "org.postgresql.jdbc3g.Jdbc3gConnection",
+    "org.postgresql.jdbc4.Jdbc4Connection",
+    "postgresql.Connection",
+    // sqlite seems to be complete
+    "org.sqlite.Conn",
+    "org.sqlite.jdbc3.JDBC3Connection",
+    "org.sqlite.jdbc4.JDBC4Connection",
+    // this won't match any class unless the property is set
+    Config.get().getJdbcConnectionClassName()
+  };
+
+  private static final String[] ABSTRACT_TYPES = {
+    // this should cover DB2
+    "com.ibm.db2.jcc.DB2Connection",
+    // this should cover Oracle
+    "oracle.jdbc.OracleConnection",
+    // this won't match any class unless the property is set
+    Config.get().getJdbcConnectionClassName()
+  };
 
   public ConnectionInstrumentation() {
     super("jdbc");
@@ -43,52 +95,8 @@ public final class ConnectionInstrumentation extends Instrumenter.Tracing {
 
   @Override
   public ElementMatcher<TypeDescription> typeMatcher() {
-
-    return safeHasSuperType(
-        namedOneOf(
-            // jt400
-            "com.ibm.as400.access.AS400JDBCConnection",
-            // this should cover DB2
-            "com.ibm.db2.jcc.DB2Connection",
-            // this should cover Oracle
-            "oracle.jdbc.OracleConnection",
-            // possibly need more coverage
-            "com.microsoft.sqlserver.jdbc.SQLServerConnection",
-            // should cover mysql
-            "com.mysql.jdbc.Connection",
-            "com.mysql.jdbc.jdbc1.Connection",
-            "com.mysql.jdbc.jdbc2.Connection",
-            "com.mysql.jdbc.ConnectionImpl",
-            "com.mysql.jdbc.JDBC4Connection",
-            "com.mysql.cj.jdbc.JdbcConnection",
-            // should cover derby
-            "org.apache.derby.impl.jdbc.EmbedConnection",
-            // complete
-            "org.h2.jdbc.JdbcConnection",
-            // complete
-            "org.hsqldb.jdbc.JDBCConnection",
-            "org.hsqldb.jdbc.jdbcConnection",
-            "org.apache.hive.jdbc.HiveConnection",
-            // complete
-            "org.mariadb.jdbc.MariaDbConnection",
-            "org.mariadb.jdbc.MySQLConnection",
-
-            // postgresql seems to be complete
-            "org.postgresql.jdbc.PgConnection",
-            "org.postgresql.jdbc1.Connection",
-            "org.postgresql.jdbc1.Jdbc1Connection",
-            "org.postgresql.jdbc2.Connection",
-            "org.postgresql.jdbc2.Jdbc2Connection",
-            "org.postgresql.jdbc3.Jdbc3Connection",
-            "org.postgresql.jdbc3g.Jdbc3gConnection",
-            "org.postgresql.jdbc4.Jdbc4Connection",
-            "postgresql.Connection",
-            // sqlite seems to be complete
-            "org.sqlite.Conn",
-            "org.sqlite.jdbc3.JDBC3Connection",
-            "org.sqlite.jdbc4.JDBC4Connection",
-            // this won't match any class unless the property is set
-            Config.get().getJdbcConnectionClassName()));
+    return namedOneOf(CONCRETE_TYPES)
+        .or(safeHasSuperType(NameMatchers.<TypeDescription>namedOneOf(ABSTRACT_TYPES)));
   }
 
   @Override
--- dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/PreparedStatementInstrumentation.java
+++ dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/PreparedStatementInstrumentation.java
@@ -16,6 +16,7 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers;
 import datadog.trace.api.Config;
 import datadog.trace.bootstrap.CallDepthThreadLocalMap;
 import datadog.trace.bootstrap.InstrumentationContext;
@@ -32,6 +33,85 @@ import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(Instrumenter.class)
 public final class PreparedStatementInstrumentation extends Instrumenter.Tracing {
+
+  private static final String[] CONCRETE_TYPES = {
+    // jt400
+    "com.ibm.as400.access.AS400JDBCPreparedStatement",
+    // probably patchy cover
+    "com.microsoft.sqlserver.jdbc.SQLServerPreparedStatement",
+    "com.microsoft.sqlserver.jdbc.SQLServerCallableStatement",
+    // should cover mysql
+    "com.mysql.cj.JdbcPreparedStatement",
+    "com.mysql.jdbc.PreparedStatement",
+    "com.mysql.jdbc.jdbc1.PreparedStatement",
+    "com.mysql.jdbc.jdbc2.PreparedStatement",
+    "com.mysql.jdbc.ServerPreparedStatement",
+    "com.mysql.cj.jdbc.PreparedStatement",
+    "com.mysql.cj.jdbc.ServerPreparedStatement",
+    "com.mysql.cj.JdbcCallableStatement",
+    "com.mysql.jdbc.CallableStatement",
+    "com.mysql.jdbc.jdbc1.CallableStatement",
+    "com.mysql.jdbc.jdbc2.CallableStatement",
+    "com.mysql.cj.jdbc.CallableStatement",
+    // covers hsqldb
+    "org.hsqldb.jdbc.JDBCPreparedStatement",
+    "org.hsqldb.jdbc.jdbcPreparedStatement",
+    "org.hsqldb.jdbc.JDBCCallableStatement",
+    "org.hsqldb.jdbc.jdbcCallableStatement",
+    // should cover derby
+    "org.apache.derby.impl.jdbc.EmbedPreparedStatement",
+    "org.apache.derby.impl.jdbc.EmbedCallableStatement",
+    // hive
+    "org.apache.hive.jdbc.HivePreparedStatement",
+    "org.apache.hive.jdbc.HiveCallableStatement",
+    // covers h2
+    "org.h2.jdbc.JdbcPreparedStatement",
+    "org.h2.jdbc.JdbcCallableStatement",
+    // covers mariadb
+    "org.mariadb.jdbc.JdbcPreparedStatement",
+    "org.mariadb.jdbc.JdbcCallableStatement",
+    "org.mariadb.jdbc.ServerSidePreparedStatement",
+    "org.mariadb.jdbc.ClientSidePreparedStatement",
+    "org.mariadb.jdbc.MariaDbServerPreparedStatement",
+    "org.mariadb.jdbc.MariaDbClientPreparedStatement",
+    "org.mariadb.jdbc.MySQLPreparedStatement",
+    "org.mariadb.jdbc.MySQLCallableStatement",
+    "org.mariadb.jdbc.MySQLServerSidePreparedStatement",
+    // should completely cover postgresql
+    "org.postgresql.jdbc1.PreparedStatement",
+    "org.postgresql.jdbc1.CallableStatement",
+    "org.postgresql.jdbc1.Jdbc1PreparedStatement",
+    "org.postgresql.jdbc1.Jdbc1CallableStatement",
+    "org.postgresql.jdbc2.PreparedStatement",
+    "org.postgresql.jdbc2.CallableStatement",
+    "org.postgresql.jdbc2.Jdbc2PreparedStatement",
+    "org.postgresql.jdbc2.Jdbc2CallableStatement",
+    "org.postgresql.jdbc3.Jdbc3PreparedStatement",
+    "org.postgresql.jdbc3.Jdbc3CallableStatement",
+    "org.postgresql.jdbc3g.Jdbc3gPreparedStatement",
+    "org.postgresql.jdbc3g.Jdbc3gCallableStatement",
+    "org.postgresql.jdbc4.Jdbc4PreparedStatement",
+    "org.postgresql.jdbc4.Jdbc4CallableStatement",
+    "org.postgresql.jdbc.PgPreparedStatement",
+    "org.postgresql.jdbc.PgCallableStatement",
+    "postgresql.PreparedStatement",
+    "postgresql.CallableStatement",
+    // should completely cover sqlite
+    "org.sqlite.jdbc3.JDBC3PreparedStatement",
+    "org.sqlite.jdbc4.JDBC4PreparedStatement",
+    "org.sqlite.PrepStmt",
+    // this won't match any classes unless set
+    Config.get().getJdbcPreparedStatementClassName()
+  };
+
+  private static final String[] ABSTRACT_TYPES = {
+    // should cover DB2
+    "com.ibm.db2.jcc.DB2PreparedStatement",
+    // should cover Oracle
+    "oracle.jdbc.OraclePreparedStatement",
+    // this won't match any classes unless set
+    Config.get().getJdbcPreparedStatementClassName()
+  };
 
   public PreparedStatementInstrumentation() {
     super("jdbc");
@@ -52,56 +132,8 @@ public final class PreparedStatementInstrumentation extends Instrumenter.Tracing
 
   @Override
   public ElementMatcher<TypeDescription> typeMatcher() {
-    return safeHasSuperType(
-        namedOneOf(
-            // jt400
-            "com.ibm.as400.access.AS400JDBCPreparedStatement",
-            // should cover DB2
-            "com.ibm.db2.jcc.DB2PreparedStatement",
-            // should cover Oracle
-            "oracle.jdbc.OraclePreparedStatement",
-            // probably patchy cover
-            "com.microsoft.sqlserver.jdbc.SQLServerPreparedStatement",
-            // should cover mysql
-            "com.mysql.cj.JdbcPreparedStatement",
-            "com.mysql.jdbc.PreparedStatement",
-            "com.mysql.jdbc.jdbc1.PreparedStatement",
-            "com.mysql.jdbc.jdbc2.PreparedStatement",
-            "com.mysql.jdbc.ServerPreparedStatement",
-            "com.mysql.cj.jdbc.PreparedStatement",
-            "com.mysql.cj.jdbc.ServerPreparedStatement",
-            // covers hsqldb
-            "org.hsqldb.jdbc.JDBCPreparedStatement",
-            "org.hsqldb.jdbc.jdbcPreparedStatement",
-            // should cover derby
-            "org.apache.derby.impl.jdbc.EmbedPreparedStatement",
-            "org.apache.hive.jdbc.HivePreparedStatement",
-            // covers h2
-            "org.h2.jdbc.JdbcPreparedStatement",
-            // covers mariadb
-            "org.mariadb.jdbc.JdbcPreparedStatement",
-            "org.mariadb.jdbc.ServerSidePreparedStatement",
-            "org.mariadb.jdbc.ClientSidePreparedStatement",
-            "org.mariadb.jdbc.MariaDbServerPreparedStatement",
-            "org.mariadb.jdbc.MariaDbClientPreparedStatement",
-            "org.mariadb.jdbc.MySQLPreparedStatement",
-            "org.mariadb.jdbc.MySQLServerSidePreparedStatement",
-            // should completely cover postgresql
-            "org.postgresql.jdbc1.PreparedStatement",
-            "org.postgresql.jdbc1.Jdbc1PreparedStatement",
-            "org.postgresql.jdbc2.PreparedStatement",
-            "org.postgresql.jdbc2.Jdbc2PreparedStatement",
-            "org.postgresql.jdbc3.Jdbc3PreparedStatement",
-            "org.postgresql.jdbc3g.Jdbc3gPreparedStatement",
-            "org.postgresql.jdbc4.Jdbc4PreparedStatement",
-            "org.postgresql.jdbc.PgPreparedStatement",
-            "postgresql.PreparedStatement",
-            // should completely cover sqlite
-            "org.sqlite.jdbc3.JDBC3PreparedStatement",
-            "org.sqlite.jdbc4.JDBC4PreparedStatement",
-            "org.sqlite.PrepStmt",
-            // this won't match any classes unless set
-            Config.get().getJdbcPreparedStatementClassName()));
+    return namedOneOf(CONCRETE_TYPES)
+        .or(safeHasSuperType(NameMatchers.<TypeDescription>namedOneOf(ABSTRACT_TYPES)));
   }
 
   @Override
