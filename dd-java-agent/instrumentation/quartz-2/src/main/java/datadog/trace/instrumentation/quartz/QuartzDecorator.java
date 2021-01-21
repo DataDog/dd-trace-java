dc41aef62c99f47ce201aeb71bf101ded974bd50
kevin.feng <kevin.feng@datadoghq.com>
2021-01-20 20:54:19 -0500

    applied spotlessApply

--- dd-java-agent/instrumentation/quartz-2/src/main/java/datadog/trace/instrumentation/quartz/QuartzDecorator.java
+++ dd-java-agent/instrumentation/quartz-2/src/main/java/datadog/trace/instrumentation/quartz/QuartzDecorator.java
@@ -2,9 +2,6 @@ package datadog.trace.instrumentation.quartz;
 
 import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.QUARTZ_JOB_GROUP;
 import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.QUARTZ_JOB_NAME;
-import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.QUARTZ_SCHEDULER;
-import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.QUARTZ_SCHEDULER_ACTUAL_TIME;
-import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.QUARTZ_SCHEDULER_FIRED_TIME;
 import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.QUARTZ_TRIGGER_GROUP;
 import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.QUARTZ_TRIGGER_NAME;
 
@@ -14,11 +11,11 @@ import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
 import datadog.trace.bootstrap.instrumentation.decorator.BaseDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionContext;
-import org.quartz.SchedulerException;
 
 @Slf4j
 public class QuartzDecorator extends BaseDecorator {
-  public static final CharSequence SCHEDULED_CALL = UTF8BytesString.createConstant("scheduled.call");
+  public static final CharSequence SCHEDULED_CALL =
+      UTF8BytesString.createConstant("scheduled.call");
   public static final QuartzDecorator DECORATE = new QuartzDecorator();
 
   private QuartzDecorator() {}
@@ -41,16 +38,16 @@ public class QuartzDecorator extends BaseDecorator {
   public AgentSpan onExecute(final AgentSpan span, JobExecutionContext context) {
     if (context != null) {
       span.setTag(DDTags.RESOURCE_NAME, context.getJobInstance().getClass()).toString();
-//      try {
-//        span.setTag(QUARTZ_SCHEDULER, context.getScheduler().getSchedulerName());
-//      } catch (SchedulerException ignore) {
-//      }
+      //      try {
+      //        span.setTag(QUARTZ_SCHEDULER, context.getScheduler().getSchedulerName());
+      //      } catch (SchedulerException ignore) {
+      //      }
       span.setTag(QUARTZ_TRIGGER_NAME, context.getTrigger().getKey().getName());
       span.setTag(QUARTZ_TRIGGER_GROUP, context.getTrigger().getKey().getGroup());
       span.setTag(QUARTZ_JOB_NAME, context.getTrigger().getJobKey().getName());
       span.setTag(QUARTZ_JOB_GROUP, context.getTrigger().getJobKey().getGroup());
-//      span.setTag(QUARTZ_SCHEDULER_FIRED_TIME, context.getScheduledFireTime().toString());
-//      span.setTag(QUARTZ_SCHEDULER_ACTUAL_TIME, context.getFireTime().toString());
+      //      span.setTag(QUARTZ_SCHEDULER_FIRED_TIME, context.getScheduledFireTime().toString());
+      //      span.setTag(QUARTZ_SCHEDULER_ACTUAL_TIME, context.getFireTime().toString());
     }
     return span;
   }
