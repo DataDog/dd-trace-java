bb6f814277d1e87dfcb5f152d1fcfcb211ca1c5f
Richard Startin <richard.startin@datadoghq.com>
2021-02-03 12:34:57 +0000

    update byte-buddy to 1.10.20

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/bytebuddy/DDCachingPoolStrategy.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/bytebuddy/DDCachingPoolStrategy.java
@@ -109,6 +109,13 @@ public class DDCachingPoolStrategy implements PoolStrategy {
     return createCachingTypePool(loaderHash, loaderRef, classFileLocator);
   }
 
+  public final TypePool typePool(
+      final ClassFileLocator classFileLocator, final ClassLoader classLoader, final String name) {
+    // FIXME satisfy interface constraint that the currently instrumented type is not used in the
+    // cache
+    return typePool(classFileLocator, classLoader);
+  }
+
   private TypePool.CacheProvider createCacheProvider(
       final int loaderHash, final WeakReference<ClassLoader> loaderRef) {
     return new SharedResolutionCacheAdapter(
