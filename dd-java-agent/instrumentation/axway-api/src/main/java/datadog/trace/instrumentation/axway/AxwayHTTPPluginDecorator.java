71b6ce4669518664839358414c2dad830606527b
Lev Priima <1118651+lpriima@users.noreply.github.com>
2021-02-05 11:41:34 -0800

    Access fields via method handles

--- dd-java-agent/instrumentation/axway-api/src/main/java/datadog/trace/instrumentation/axway/AxwayHTTPPluginDecorator.java
+++ dd-java-agent/instrumentation/axway-api/src/main/java/datadog/trace/instrumentation/axway/AxwayHTTPPluginDecorator.java
@@ -4,6 +4,7 @@ import static java.lang.invoke.MethodType.methodType;
 
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.DefaultURIDataAdapter;
+import datadog.trace.bootstrap.instrumentation.api.Tags;
 import datadog.trace.bootstrap.instrumentation.api.URIDataAdapter;
 import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
 import datadog.trace.bootstrap.instrumentation.decorator.HttpServerDecorator;
@@ -18,35 +19,92 @@ import lombok.extern.slf4j.Slf4j;
 // request = is com.vordel.circuit.net.State,  connection = com.vordel.dwe.http.ServerTransaction
 @Slf4j
 public class AxwayHTTPPluginDecorator extends HttpServerDecorator<Object, Object, Object> {
-  public static final String HOST = "host";
-  public static final String PORT = "port";
   public static final CharSequence AXWAY_REQUEST = UTF8BytesString.createConstant("axway.request");
-  public static final CharSequence AXWAY_TRY_TRAMSACTION =
+  public static final CharSequence AXWAY_TRY_TRANSACTION =
       UTF8BytesString.createConstant("axway.trytransaction");
 
   public static final AxwayHTTPPluginDecorator DECORATE = new AxwayHTTPPluginDecorator();
 
   private static final MethodHandles.Lookup lookup = MethodHandles.lookup();
-  private static MethodHandle getRemoteAddr_mh;
-  private static MethodHandle getMethod_mh;
-  private static MethodHandle getURI_mh;
+
+  private static final String SERVERTRANSACTION_CLASSNAME = "com.vordel.dwe.http.ServerTransaction";
+  private static final Class<?> classServerTransaction;
+  private static final MethodHandle getRemoteAddr_mh;
+  private static final MethodHandle getMethod_mh;
+  private static final MethodHandle getURI_mh;
+
+  private static final String STATE_CLASSNAME = "com.vordel.circuit.net.State";
+  private static final Class<?> classState;
+  private static final MethodHandle hostField_mh;
+  private static final MethodHandle portField_mh;
+  private static final MethodHandle methodField_mh;
+  private static final MethodHandle uriField_mh;
 
   static {
-    try {
-      Class<?> classServerTransaction = Class.forName("com.vordel.dwe.http.ServerTransaction");
-      getRemoteAddr_mh =
-          lookup.findVirtual(
-              classServerTransaction, "getRemoteAddr", methodType(InetSocketAddress.class));
-      getMethod_mh =
-          lookup.findVirtual(classServerTransaction, "getMethod", methodType(String.class));
+    classServerTransaction = initClass(SERVERTRANSACTION_CLASSNAME);
+    getRemoteAddr_mh =
+        initNoArgServerTransactionMethodHandle("getRemoteAddr", InetSocketAddress.class);
+    getMethod_mh = initNoArgServerTransactionMethodHandle("getMethod", String.class);
+    getURI_mh = initGetURI();
 
-      Method m = classServerTransaction.getDeclaredMethod("getURI"); // private method
-      m.setAccessible(true);
-      getURI_mh = lookup.unreflect(m);
-    } catch (Throwable e) {
-      log.debug("Can't find 'com.vordel.dwe.http.ServerTransaction::getRemoteAddr' method: ", e);
-    }
+    classState = initClass(STATE_CLASSNAME);
+    hostField_mh = initStateFieldGetter("host");
+    portField_mh = initStateFieldGetter("port");
+    methodField_mh = initStateFieldGetter("verb");
+    uriField_mh = initStateFieldGetter("uri");
   }
+
+  private static Class<?> initClass(final String name) {
+    try {
+      return Class.forName(name);
+    } catch (ClassNotFoundException e) {
+      log.debug(
+          "Can't find class '{}': Axaway integration failed. ", SERVERTRANSACTION_CLASSNAME, e);
+    }
+    return null;
+  }
+
+  private static MethodHandle initNoArgServerTransactionMethodHandle(String name, Class<?> rtype) {
+    try {
+      return lookup.findVirtual(classServerTransaction, name, methodType(rtype));
+    } catch (NoSuchMethodException | IllegalAccessException e) {
+      log.debug("Can't find method handler '{}' ", name, e);
+    }
+    return null;
+  }
+
+  private static MethodHandle initGetURI() {
+    Method m = null;
+    try {
+      m = classServerTransaction.getDeclaredMethod("getURI"); // private method
+      m.setAccessible(true);
+      return lookup.unreflect(m);
+    } catch (Throwable e) {
+      log.debug("Can't unreflect method '{}': ", m, e);
+    }
+    return null;
+  }
+
+  private static MethodHandle initStateFieldGetter(String fieldName) {
+    MethodHandle mh = null;
+    try {
+      Field field = classState.getDeclaredField(fieldName);
+      field.setAccessible(true);
+      mh = lookup.unreflectGetter(field);
+      log.debug(
+          "Initialized field '{}' of class '{}' unreflected to {}", fieldName, classState, mh);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      log.debug(
+          "Can't find and unreflect declared field '{}' for class '{}' to mh: '{}'",
+          fieldName,
+          classState,
+          mh,
+          e);
+    }
+    return mh;
+  }
+
+  //
 
   @Override
   protected String[] instrumentationNames() {
@@ -61,16 +119,26 @@ public class AxwayHTTPPluginDecorator extends HttpServerDecorator<Object, Object
   @Override
   protected String method(final Object serverTransaction) {
     try {
-      return getMethod_mh.invoke(serverTransaction).toString();
+      return (String) getMethod_mh.invoke(serverTransaction);
     } catch (Throwable throwable) {
-      log.debug("Can't invoke com.vordel.dwe.http.ServerTransaction::getMethod(): ", throwable);
+      log.debug(
+          "Can't invoke invoke '{}' on instance '{}' of class '{}'",
+          getMethod_mh,
+          serverTransaction,
+          serverTransaction.getClass(),
+          throwable);
     }
     return "UNKNOWN";
   }
 
   @Override
   protected URIDataAdapter url(final Object serverTransaction) {
-    return new DefaultURIDataAdapter(getUri(serverTransaction));
+    try {
+      return new DefaultURIDataAdapter((URI) getURI_mh.invoke(serverTransaction));
+    } catch (Throwable e) {
+      log.debug("Can't find invoke '{}}' on '{}': ", getURI_mh, serverTransaction, e);
+    }
+    return new DefaultURIDataAdapter(URI.create(""));
   }
 
   @Override
@@ -83,44 +151,76 @@ public class AxwayHTTPPluginDecorator extends HttpServerDecorator<Object, Object
     return getRemoteAddr(serverTransaction).getPort();
   }
 
+  /** @param serverTransaction instance of {@value #SERVERTRANSACTION_CLASSNAME} */
   @Override
   protected int status(final Object serverTransaction) {
     // TODO will be done manually
     return 0;
   }
 
-  public static URI getUri(Object obj) {
-    try {
-      return (URI) getURI_mh.invoke(obj);
-    } catch (Throwable e) {
-      log.debug("Can't find invoke 'getUri' in object " + obj, e);
+  /** @param stateInstance type com.vordel.circuit.net.State */
+  public AgentSpan onTransaction(AgentSpan span, Object stateInstance) {
+    if (span != null) {
+      setStringTagFromStateField(span, Tags.PEER_HOSTNAME, stateInstance, hostField_mh);
+      setStringTagFromStateField(span, Tags.PEER_PORT, stateInstance, portField_mh);
+      setStringTagFromStateField(span, Tags.HTTP_METHOD, stateInstance, methodField_mh);
+      setURLTagFromUriStateField(span, stateInstance);
     }
-    return URI.create("");
+    return span;
   }
 
+  /**
+   *
+   *
+   * <pre>
+   * <code>public class Transaction implements IMetricsTransaction {
+   *    public native InetSocketAddress getLocalAddr();
+   *    public native InetSocketAddress getRemoteAddr();
+   * }
+   * public abstract class HTTPTransaction extends Transaction {
+   *
+   * }
+   * public class ServerTransaction extends HTTPTransaction {
+   *
+   * }
+   *
+   * </code>
+   * </pre>
+   *
+   * @param obj instance of {@value #SERVERTRANSACTION_CLASSNAME}
+   * @return result of com.vordel.dwe.http.ServerTransaction::getRemoteAddr()
+   */
   private static InetSocketAddress getRemoteAddr(Object obj) {
     try {
       return (InetSocketAddress) getRemoteAddr_mh.invoke(obj);
     } catch (Throwable throwable) {
-      log.debug("Can't com.vordel.dwe.http.ServerTransaction::getRemoteAddr() : ", throwable);
+      log.debug("Can't invoke '{}' on instance '{}': ", getRemoteAddr_mh, obj, throwable);
     }
     return new InetSocketAddress(0);
   }
 
-  public static void setTagFromField(AgentSpan span, String tag, Object obj, String field) {
-    span.setTag(tag, getFieldValue(obj, field).toString());
+  private static void setStringTagFromStateField(
+      AgentSpan span, String tag, Object stateInstance, MethodHandle mh) {
+    String v = "";
+    try {
+      v = (String) mh.invoke(stateInstance);
+    } catch (Throwable e) {
+      log.debug(
+          "Can't invoke '{}' on instance '{}'; ; Tag '{}' not set.", mh, stateInstance, tag, e);
+    }
+    span.setTag(tag, v);
   }
 
-  public static Object getFieldValue(Object obj, String fieldName) {
+  private static void setURLTagFromUriStateField(AgentSpan span, Object stateInstance) {
     try {
-      Field field = obj.getClass().getDeclaredField(fieldName);
-      field.setAccessible(true);
-      Object v = field.get(obj);
-      log.debug("field '{}': {}", fieldName, v);
-      return v;
-    } catch (NoSuchFieldException | IllegalAccessException e) {
-      log.debug("Can't find field '" + fieldName + "': ", e);
+      span.setTag(Tags.HTTP_URL, uriField_mh.invoke(stateInstance).toString());
+    } catch (Throwable e) {
+      log.debug(
+          "Can't invoke '{}' on instance '{}'; Tag '{}' not set.",
+          uriField_mh,
+          stateInstance,
+          Tags.HTTP_URL,
+          e);
     }
-    return "null";
   }
 }
--- dd-java-agent/instrumentation/axway-api/src/main/java/datadog/trace/instrumentation/axway/StateAdvice.java
+++ dd-java-agent/instrumentation/axway-api/src/main/java/datadog/trace/instrumentation/axway/StateAdvice.java
@@ -2,15 +2,11 @@ package datadog.trace.instrumentation.axway;
 
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
-import static datadog.trace.instrumentation.axway.AxwayHTTPPluginDecorator.AXWAY_TRY_TRAMSACTION;
+import static datadog.trace.instrumentation.axway.AxwayHTTPPluginDecorator.AXWAY_TRY_TRANSACTION;
 import static datadog.trace.instrumentation.axway.AxwayHTTPPluginDecorator.DECORATE;
-import static datadog.trace.instrumentation.axway.AxwayHTTPPluginDecorator.HOST;
-import static datadog.trace.instrumentation.axway.AxwayHTTPPluginDecorator.PORT;
-import static datadog.trace.instrumentation.axway.AxwayHTTPPluginDecorator.setTagFromField;
 
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
-import datadog.trace.bootstrap.instrumentation.api.Tags;
 import net.bytebuddy.asm.Advice;
 
 /**
@@ -23,14 +19,10 @@ import net.bytebuddy.asm.Advice;
 public class StateAdvice {
   @Advice.OnMethodEnter(suppress = Throwable.class)
   public static AgentScope onEnter(@Advice.This final Object stateInstance) {
-    final AgentSpan span = startSpan(AXWAY_TRY_TRAMSACTION);
+    final AgentSpan span = startSpan(AXWAY_TRY_TRANSACTION);
     final AgentScope scope = activateSpan(span);
     span.setMeasured(true);
-    // manually DECORATE.onRequest(span, stateInstance) :
-    setTagFromField(span, Tags.HTTP_METHOD, stateInstance, "verb");
-    setTagFromField(span, Tags.HTTP_URL, stateInstance, "uri");
-    setTagFromField(span, Tags.PEER_HOSTNAME, stateInstance, HOST);
-    setTagFromField(span, Tags.PEER_PORT, stateInstance, PORT);
+    DECORATE.onTransaction(span, stateInstance);
     DECORATE.afterStart(span);
     return scope;
   }
