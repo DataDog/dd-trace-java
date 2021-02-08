13e6602d28f3809546d33937d8bad31982d6f2e6
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2021-02-08 12:08:44 +0000

    Initial support for detecting certain libraries at boot to help tune agent behaviour

---
+++ dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/Library.java
@@ -0,0 +1,29 @@
+package datadog.trace.bootstrap;
+
+import java.util.EnumSet;
+import org.slf4j.Logger;
+
+/** Tracks third-party libraries that may need special handling during agent startup. */
+public enum Library {
+  WILDFLY;
+
+  /**
+   * Best-effort detection of libraries potentially used by the application. This is called at boot
+   * so we need to be very careful how many checks happen here. Some library use may not be visible
+   * to the agent at this point.
+   */
+  public static EnumSet<Library> detectLibraries(final Logger log) {
+    final EnumSet<Library> libraries = EnumSet.noneOf(Library.class);
+
+    final String jbossHome = System.getenv("JBOSS_HOME");
+    if (jbossHome != null) {
+      log.debug("Env - jboss: {}", jbossHome);
+      libraries.add(WILDFLY);
+    }
+
+    if (!libraries.isEmpty()) {
+      log.debug("Detected {}", libraries);
+    }
+    return libraries;
+  }
+}
