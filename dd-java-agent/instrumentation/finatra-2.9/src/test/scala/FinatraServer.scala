8aaa42c96368efbc0c42cbb753ecf62f43876d44
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2021-02-09 07:50:36 +0100

    Don't spin wait for test FinatraServer to start

--- dd-java-agent/instrumentation/finatra-2.9/src/test/scala/FinatraServer.scala
+++ dd-java-agent/instrumentation/finatra-2.9/src/test/scala/FinatraServer.scala
@@ -3,11 +3,25 @@ import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.ExceptionMappingFilter
 import com.twitter.finatra.http.routing.HttpRouter
 
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
 class FinatraServer extends HttpServer {
+
+  private val latch = new CountDownLatch(1)
+
   override protected def configureHttp(router: HttpRouter): Unit = {
     router
       .filter[ExceptionMappingFilter[Request]]
       .add[FinatraController]
       .exceptionMapper[ResponseSettingExceptionMapper]
+  }
+
+  override protected def postWarmup(): Unit = {
+    super.postWarmup()
+    latch.countDown()
+  }
+
+  def awaitStart(timeout: Long, unit: TimeUnit): Unit = {
+    latch.await(timeout, unit)
   }
 }
