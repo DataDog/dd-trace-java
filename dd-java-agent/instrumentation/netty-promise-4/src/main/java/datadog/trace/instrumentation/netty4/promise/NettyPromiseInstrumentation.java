48a1c3b1e9601a9c35a795ee8bda103aae9be846
Tyler Benson <tyler.benson@datadoghq.com>
2021-02-11 12:32:39 -0500

    Avoid double instrumentation
    This change avoids an extra instanceof check that would result when `addListener` called `addListener0`.

--- dd-java-agent/instrumentation/netty-promise-4/src/main/java/datadog/trace/instrumentation/netty4/promise/NettyPromiseInstrumentation.java
+++ dd-java-agent/instrumentation/netty-promise-4/src/main/java/datadog/trace/instrumentation/netty4/promise/NettyPromiseInstrumentation.java
@@ -1,14 +1,13 @@
 package datadog.trace.instrumentation.netty4.promise;
 
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
-import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.namedOneOf;
-import static java.util.Collections.singletonMap;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
+import java.util.HashMap;
 import java.util.Map;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.method.MethodDescription;
@@ -38,10 +37,16 @@ public class NettyPromiseInstrumentation extends Instrumenter.Tracing {
 
   @Override
   public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
-    return singletonMap(
-        namedOneOf("addListener", "addListener0")
+    Map<ElementMatcher<MethodDescription>, String> transformers = new HashMap<>(2);
+    transformers.put(
+        named("addListener")
             .and(takesArgument(0, named("io.netty.util.concurrent.GenericFutureListener"))),
         NettyPromiseInstrumentation.class.getName() + "$WrapListenerAdvice");
+    transformers.put(
+        named("addListeners")
+            .and(takesArgument(0, named("io.netty.util.concurrent.GenericFutureListener[]"))),
+        NettyPromiseInstrumentation.class.getName() + "$WrapListenersAdvice");
+    return transformers;
   }
 
   public static class WrapListenerAdvice {
@@ -50,6 +55,17 @@ public class NettyPromiseInstrumentation extends Instrumenter.Tracing {
         @Advice.Argument(value = 0, readOnly = false)
             GenericFutureListener<? extends Future<?>> listener) {
       listener = ListenerWrapper.wrapIfNeeded(listener);
+    }
+  }
+
+  public static class WrapListenersAdvice {
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void wrapListener(
+        @Advice.Argument(value = 0, readOnly = false)
+            GenericFutureListener<? extends Future<?>>[] listeners) {
+      for (int i = 0; i < listeners.length; i++) {
+        listeners[i] = ListenerWrapper.wrapIfNeeded(listeners[i]);
+      }
     }
   }
 }
