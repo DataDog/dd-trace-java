b78e7b9af4d8535c65cfd58d269bce28a3de5559
Richard Startin <richard.startin@datadoghq.com>
2021-02-16 07:14:32 +0000

    DDAgent features discovery

---
+++ dd-trace-core/src/main/java/datadog/trace/common/writer/ddagent/DDAgentFeaturesDiscovery.java
@@ -0,0 +1,163 @@
+package datadog.trace.common.writer.ddagent;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+import java.io.IOException;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@Slf4j
+public class DDAgentFeaturesDiscovery {
+
+  private static final JsonAdapter<Map<String, Object>> RESPONSE_ADAPTER =
+      new Moshi.Builder()
+          .build()
+          .adapter(Types.newParameterizedType(Map.class, String.class, Object.class));
+
+  private static final String V3_ENDPOINT = "v0.3/traces";
+  private static final String V4_ENDPOINT = "v0.4/traces";
+  private static final String V5_ENDPOINT = "v0.5/traces";
+
+  private static final String V5_METRICS_ENDPOINT = "v0.5/stats";
+
+  private final OkHttpClient client;
+  private final HttpUrl agentBaseUrl;
+
+  private volatile String traceEndpoint;
+  private volatile String metricsEndpoint;
+  private volatile boolean supportsDropping;
+
+  private final String[] traceEndpoints;
+  private final String[] metricsEndpoints = {V5_METRICS_ENDPOINT};
+
+  public DDAgentFeaturesDiscovery(OkHttpClient client, String agentUrl, boolean enableV05Traces) {
+    this.client = client;
+    this.agentBaseUrl = HttpUrl.get(agentUrl);
+    this.traceEndpoints =
+        enableV05Traces
+            ? new String[] {V5_ENDPOINT, V4_ENDPOINT, V3_ENDPOINT}
+            : new String[] {V4_ENDPOINT, V3_ENDPOINT};
+  }
+
+  public void discover() {
+    // 1. try to fetch info about the agent, if the endpoint is there
+    // 2. try to parse the response, if it can be parsed, finish
+    // 3. fallback if the endpoint couldn't be found or the response couldn't be parsed
+    boolean fallback = true;
+    try (Response response =
+        client
+            .newCall(new Request.Builder().url(agentBaseUrl.resolve("info").url()).build())
+            .execute()) {
+      if (response.isSuccessful()) {
+        fallback = !processInfoResponse(response.body().string());
+      }
+    } catch (Throwable error) {
+      errorQueryingEndpoint("info", error);
+    }
+    if (fallback) {
+      log.debug("Falling back to probing, client dropping will be disabled");
+      this.metricsEndpoint = probeTracerMetricsEndpoint();
+      this.traceEndpoint = probeTracesEndpoint();
+    }
+  }
+
+  private String probeTracerMetricsEndpoint() {
+    String candidate = "v0.5/stats";
+    try (Response response =
+        client
+            .newCall(new Request.Builder().url(agentBaseUrl.resolve(candidate).url()).build())
+            .execute()) {
+      if (response.code() != 404) {
+        return candidate;
+      }
+    } catch (IOException e) {
+      errorQueryingEndpoint(candidate, e);
+    }
+    log.debug("No metrics endpoint found, metrics will be disabled");
+    return null;
+  }
+
+  private String probeTracesEndpoint() {
+    for (String candidate : traceEndpoints) {
+      try (Response response =
+          client
+              .newCall(new Request.Builder().url(agentBaseUrl.resolve(candidate).url()).build())
+              .execute()) {
+        if (response.code() != 404) {
+          return candidate;
+        }
+      } catch (IOException e) {
+        errorQueryingEndpoint(candidate, e);
+      }
+    }
+    return V3_ENDPOINT;
+  }
+
+  @SuppressWarnings("unchecked")
+  private boolean processInfoResponse(String response) {
+    try {
+      Map<String, Object> map = RESPONSE_ADAPTER.fromJson(response);
+      List<String> endpoints = ((List<String>) map.get("endpoints"));
+      ListIterator<String> traceAgentSupportedEndpoints = endpoints.listIterator(endpoints.size());
+      boolean traceEndpointFound = false;
+      boolean metricsEndpointFound = false;
+      while ((!traceEndpointFound || !metricsEndpointFound)
+          && traceAgentSupportedEndpoints.hasPrevious()) {
+        String traceAgentSupportedEndpoint = traceAgentSupportedEndpoints.previous();
+        if (traceAgentSupportedEndpoint.startsWith("/")
+            && traceAgentSupportedEndpoint.length() > 1) {
+          traceAgentSupportedEndpoint = traceAgentSupportedEndpoint.substring(1);
+        }
+        if (!metricsEndpointFound) {
+          for (int i = metricsEndpoints.length - 1; i >= 0; --i) {
+            if (metricsEndpoints[i].equalsIgnoreCase(traceAgentSupportedEndpoint)) {
+              this.metricsEndpoint = traceAgentSupportedEndpoint;
+              metricsEndpointFound = true;
+              break;
+            }
+          }
+        }
+        if (!traceEndpointFound) {
+          for (int i = traceEndpoints.length - 1; i >= 0; --i) {
+            if (traceEndpoints[i].equalsIgnoreCase(traceAgentSupportedEndpoint)) {
+              this.traceEndpoint = traceAgentSupportedEndpoint;
+              traceEndpointFound = true;
+              break;
+            }
+          }
+        }
+      }
+      Object canDrop = map.get("client_drop_p0s");
+      this.supportsDropping =
+          null != canDrop
+              && ("true".equalsIgnoreCase(String.valueOf(canDrop)) || Boolean.TRUE.equals(canDrop));
+      return true;
+    } catch (Throwable error) {
+      log.debug("Error parsing trace agent /info response", error);
+    }
+    return false;
+  }
+
+  public boolean supportsDropping() {
+    return supportsDropping;
+  }
+
+  public String getMetricsEndpoint() {
+    return metricsEndpoint;
+  }
+
+  public String getTraceEndpoint() {
+    return traceEndpoint;
+  }
+
+  private void errorQueryingEndpoint(String endpoint, Throwable t) {
+    log.debug("Error querying {} at {}", endpoint, agentBaseUrl, t);
+  }
+}
---
+++ dd-trace-core/src/test/groovy/datadog/trace/common/writer/ddagent/DDAgentFeaturesDiscoveryTest.groovy
@@ -0,0 +1,209 @@
+package datadog.trace.common.writer.ddagent
+
+
+import datadog.trace.test.util.DDSpecification
+import okhttp3.Call
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.ResponseBody
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class DDAgentFeaturesDiscoveryTest extends DDSpecification {
+
+  static final String INFO_RESPONSE = loadJsonFile("agent-info.json")
+  static final String INFO_WITH_CLIENT_DROPPING_RESPONSE = loadJsonFile("agent-info-with-client-dropping.json")
+
+  def "test parse /info response"() {
+    setup:
+    OkHttpClient client = Mock(OkHttpClient)
+    DDAgentFeaturesDiscovery features = new DDAgentFeaturesDiscovery(client, "http://localhost:8125", true)
+
+    when: "/info available"
+    features.discover()
+
+    then:
+    1 * client.newCall(_) >> { Request request -> infoResponse(request, INFO_RESPONSE) }
+    features.getMetricsEndpoint() == "v0.5/stats"
+    features.getTraceEndpoint() == "v0.5/traces"
+    !features.supportsDropping()
+  }
+
+  def "test parse /info response with client dropping"() {
+    setup:
+    OkHttpClient client = Mock(OkHttpClient)
+    DDAgentFeaturesDiscovery features = new DDAgentFeaturesDiscovery(client, "http://localhost:8125", true)
+
+    when: "/info available"
+    features.discover()
+
+    then:
+    1 * client.newCall(_) >> { Request request -> infoResponse(request, INFO_WITH_CLIENT_DROPPING_RESPONSE) }
+    features.getMetricsEndpoint() == "v0.5/stats"
+    features.getTraceEndpoint() == "v0.5/traces"
+    features.supportsDropping()
+  }
+
+  def "test fallback when /info not found" () {
+    setup:
+    OkHttpClient client = Mock(OkHttpClient)
+    DDAgentFeaturesDiscovery features = new DDAgentFeaturesDiscovery(client, "http://localhost:8125", true)
+
+    when: "/info unavailable"
+    features.discover()
+
+    then:
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/info" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/stats" }) >> { Request request -> clientError(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/traces" }) >> { Request request -> clientError(request) }
+    features.getMetricsEndpoint() == "v0.5/stats"
+    features.getTraceEndpoint() == "v0.5/traces"
+    !features.supportsDropping()
+  }
+
+  def "test fallback when /info not found and agent returns ok" () {
+    setup:
+    OkHttpClient client = Mock(OkHttpClient)
+    DDAgentFeaturesDiscovery features = new DDAgentFeaturesDiscovery(client, "http://localhost:8125", true)
+
+    when: "/info unavailable"
+    features.discover()
+
+    then:
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/info" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/stats" }) >> { Request request -> success(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/traces" }) >> { Request request -> success(request) }
+    features.getMetricsEndpoint() == "v0.5/stats"
+    features.getTraceEndpoint() == "v0.5/traces"
+    !features.supportsDropping()
+  }
+
+  def "test fallback when /info not found and v0.5 disabled" () {
+    setup:
+    OkHttpClient client = Mock(OkHttpClient)
+    DDAgentFeaturesDiscovery features = new DDAgentFeaturesDiscovery(client, "http://localhost:8125", false)
+
+    when: "/info unavailable"
+    features.discover()
+
+    then:
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/info" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/stats" }) >> { Request request -> clientError(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.4/traces" }) >> { Request request -> clientError(request) }
+    features.getMetricsEndpoint() == "v0.5/stats"
+    features.getTraceEndpoint() == "v0.4/traces"
+    !features.supportsDropping()
+  }
+
+  def "test fallback when /info not found and v0.5 unavailable agent side" () {
+    setup:
+    OkHttpClient client = Mock(OkHttpClient)
+    DDAgentFeaturesDiscovery features = new DDAgentFeaturesDiscovery(client, "http://localhost:8125", true)
+
+    when: "/info unavailable"
+    features.discover()
+
+    then:
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/info" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/stats" }) >> { Request request -> clientError(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/traces" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.4/traces" }) >> { Request request -> clientError(request) }
+    features.getMetricsEndpoint() == "v0.5/stats"
+    features.getTraceEndpoint() == "v0.4/traces"
+    !features.supportsDropping()
+  }
+
+  def "test fallback on old agent" () {
+    setup:
+    OkHttpClient client = Mock(OkHttpClient)
+    DDAgentFeaturesDiscovery features = new DDAgentFeaturesDiscovery(client, "http://localhost:8125", true)
+
+    when: "/info unavailable"
+    features.discover()
+
+    then:
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/info" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/stats" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/traces" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.4/traces" }) >> { Request request -> clientError(request) }
+    features.getMetricsEndpoint() == null
+    features.getTraceEndpoint() == "v0.4/traces"
+    !features.supportsDropping()
+  }
+
+  def "test fallback on very old agent" () {
+    setup:
+    OkHttpClient client = Mock(OkHttpClient)
+    DDAgentFeaturesDiscovery features = new DDAgentFeaturesDiscovery(client, "http://localhost:8125", true)
+
+    when: "/info unavailable"
+    features.discover()
+
+    then:
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/info" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/stats" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.5/traces" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.4/traces" }) >> { Request request -> notFound(request) }
+    1 * client.newCall({ Request request -> request.url().toString() == "http://localhost:8125/v0.3/traces" }) >> { Request request -> clientError(request) }
+    features.getMetricsEndpoint() == null
+    features.getTraceEndpoint() == "v0.3/traces"
+    !features.supportsDropping()
+  }
+
+
+  def infoResponse(Request request, String json) {
+    return Mock(Call) {
+      it.execute() >> new Response.Builder()
+        .code(200)
+        .request(request)
+        .protocol(Protocol.HTTP_1_1)
+        .message("")
+        .body(ResponseBody.create(MediaType.get("application/json"), json))
+        .build()
+    }
+  }
+
+  def notFound(Request request) {
+    return Mock(Call) {
+      it.execute() >> new Response.Builder()
+        .code(404)
+        .request(request)
+        .protocol(Protocol.HTTP_1_1)
+        .message("")
+        .body(ResponseBody.create(MediaType.get("application/json"), ""))
+        .build()
+    }
+  }
+
+  def clientError(Request request) {
+    return Mock(Call) {
+      it.execute() >> new Response.Builder()
+        .code(400)
+        .request(request)
+        .protocol(Protocol.HTTP_1_1)
+        .message("")
+        .body(ResponseBody.create(MediaType.get("application/msgpack"), ""))
+        .build()
+    }
+  }
+
+  def success(Request request) {
+    return Mock(Call) {
+      it.execute() >> new Response.Builder()
+        .code(200)
+        .request(request)
+        .protocol(Protocol.HTTP_1_1)
+        .message("")
+        .body(ResponseBody.create(MediaType.get("application/msgpack"), ""))
+        .build()
+    }
+  }
+
+  private static String loadJsonFile(String name) {
+    return new String(Files.readAllBytes(Paths.get("src/test/resources/agent-features").resolve(name)))
+  }
+}
