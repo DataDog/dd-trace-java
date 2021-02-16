10d09c94d3eb1d2fe1d2c34f310734fdcb1a6b6f
Richard Startin <richard.startin@datadoghq.com>
2021-02-16 19:55:46 +0000

    change removal from common keys to try to preserve recently seen keys

--- dd-trace-core/src/main/java/datadog/trace/common/metrics/Aggregator.java
+++ dd-trace-core/src/main/java/datadog/trace/common/metrics/Aggregator.java
@@ -21,7 +21,7 @@ final class Aggregator implements Runnable {
   private final BlockingQueue<Batch> inbox;
   private final LRUCache<MetricKey, AggregateMetric> aggregates;
   private final ConcurrentHashMap<MetricKey, Batch> pending;
-  private final Set<MetricKey> newKeysInInterval;
+  private final Set<MetricKey> commonKeys;
   private final MetricWriter writer;
   // the reporting interval controls how much history will be buffered
   // when the agent is unresponsive (only 10 pending requests will be
@@ -35,14 +35,14 @@ final class Aggregator implements Runnable {
       Queue<Batch> batchPool,
       BlockingQueue<Batch> inbox,
       ConcurrentHashMap<MetricKey, Batch> pending,
-      Set<MetricKey> newKeysInInterval,
+      Set<MetricKey> commonKeys,
       int maxAggregates,
       long reportingInterval,
       TimeUnit reportingIntervalTimeUnit) {
     this.writer = writer;
     this.batchPool = batchPool;
     this.inbox = inbox;
-    this.newKeysInInterval = newKeysInInterval;
+    this.commonKeys = commonKeys;
     this.aggregates = new LRUCache<>(maxAggregates * 4 / 3, 0.75f, maxAggregates);
     this.pending = pending;
     this.reportingIntervalNanos = reportingIntervalTimeUnit.toNanos(reportingInterval);
@@ -90,10 +90,8 @@ final class Aggregator implements Runnable {
         if (!aggregates.isEmpty()) {
           writer.startBucket(aggregates.size(), when, reportingIntervalNanos);
           for (Map.Entry<MetricKey, AggregateMetric> aggregate : aggregates.entrySet()) {
-            if (aggregate.getValue().getHitCount() > 0) {
-              writer.add(aggregate.getKey(), aggregate.getValue());
-              aggregate.getValue().clear();
-            }
+            writer.add(aggregate.getKey(), aggregate.getValue());
+            aggregate.getValue().clear();
           }
           // note that this may do IO and block
           writer.finishBucket();
@@ -109,9 +107,11 @@ final class Aggregator implements Runnable {
   private void expungeStaleAggregates() {
     Iterator<Map.Entry<MetricKey, AggregateMetric>> it = aggregates.entrySet().iterator();
     while (it.hasNext()) {
-      AggregateMetric metric = it.next().getValue();
+      Map.Entry<MetricKey, AggregateMetric> pair = it.next();
+      AggregateMetric metric = pair.getValue();
       if (metric.getHitCount() == 0) {
         it.remove();
+        commonKeys.remove(pair.getKey());
       }
     }
   }
