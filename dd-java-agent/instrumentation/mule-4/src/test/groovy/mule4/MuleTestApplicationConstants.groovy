fe6225ec4d799428f918a6370333e96d3f3906c1
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2021-03-17 10:52:56 +0100

    Add Mule 4 http client and parallel for each tests

---
+++ dd-java-agent/instrumentation/mule-4/src/test/groovy/mule4/MuleTestApplicationConstants.groovy
@@ -0,0 +1,6 @@
+package mule4
+
+class MuleTestApplicationConstants {
+  static final String TEST_APPLICATION_JAR = MuleTestApplicationConstants.simpleName + ".jar"
+  static final String TEST_APPLICATION_NAME = MuleTestApplicationConstants.simpleName + ".name"
+}
