d0cec3215f737bb9550c70c11d6885728896de4f
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2021-04-15 11:37:21 +0200

    Try out simpler weak cache on top of CLM

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/CLHMWeakCache.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/CLHMWeakCache.java
@@ -1,12 +1,14 @@
 package datadog.trace.agent.tooling;
 
-import com.blogspot.mydailyjava.weaklockfree.WeakConcurrentMap;
 import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
+import com.googlecode.concurrentlinkedhashmap.EvictionListener;
 import datadog.trace.api.Function;
 import datadog.trace.bootstrap.WeakCache;
-import java.util.concurrent.ConcurrentMap;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
 
-public class CLHMWeakCache<K, V> implements WeakCache<K, V> {
+public class CLHMWeakCache<K, V> extends ReferenceQueue<K> implements WeakCache<K, V> {
   public static final class Provider implements WeakCache.Provider {
     @Override
     public <K, V> WeakCache<K, V> newWeakCache(long maxSize) {
@@ -16,38 +18,44 @@ public class CLHMWeakCache<K, V> implements WeakCache<K, V> {
 
   private static final int CACHE_CONCURRENCY =
       Math.max(8, Runtime.getRuntime().availableProcessors());
-  private final WeakConcurrentMap<K, V> weakMap;
-  private final long maxSize;
+  private final ConcurrentLinkedHashMap<WeakKey<K>, V> concurrentMap;
 
   public CLHMWeakCache(long maxSize) {
-    // No parameterization because WeakKey isn't visible
-    ConcurrentMap linkedMap =
-        new ConcurrentLinkedHashMap.Builder()
+    concurrentMap =
+        new ConcurrentLinkedHashMap.Builder<WeakKey<K>, V>()
             .maximumWeightedCapacity(maxSize)
+            .listener(
+                new EvictionListener<WeakKey<K>, V>() {
+                  @Override
+                  public void onEviction(WeakKey<K> weakKey, V value) {
+                    CLHMWeakCache.this.expungeStaleEntries();
+                  }
+                })
             .concurrencyLevel(CACHE_CONCURRENCY)
             .build();
-
-    weakMap = new WeakConcurrentMap<>(false, true, linkedMap);
-
-    this.maxSize = maxSize;
   }
 
   @Override
   public V getIfPresent(K key) {
-    return weakMap.getIfPresent(key);
+    WeakKey<K> weakKey = new WeakKey<>(key, null);
+    V value = concurrentMap.get(weakKey);
+    weakKey.clear();
+    return value;
   }
 
   @Override
   public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
-    V value = weakMap.getIfPresent(key);
+    WeakKey<K> weakKey = new WeakKey<>(key, this);
+    V value = concurrentMap.get(weakKey);
     if (value == null) {
       value = mappingFunction.apply(key);
-
-      expungeIfNecessary();
-      V oldValue = weakMap.putIfProbablyAbsent(key, value);
+      V oldValue = concurrentMap.putIfAbsent(weakKey, value);
       if (oldValue != null) {
         value = oldValue;
+        weakKey.clear();
       }
+    } else {
+      weakKey.clear();
     }
 
     return value;
@@ -55,14 +63,66 @@ public class CLHMWeakCache<K, V> implements WeakCache<K, V> {
 
   @Override
   public void put(K key, V value) {
-    expungeIfNecessary();
-
-    weakMap.put(key, value);
+    concurrentMap.put(new WeakKey<>(key, this), value);
   }
 
-  private void expungeIfNecessary() {
-    if (weakMap.approximateSize() >= maxSize) {
-      weakMap.expungeStaleEntries();
+  private void expungeStaleEntries() {
+    Reference<?> reference;
+    while ((reference = poll()) != null) {
+      concurrentMap.remove(reference);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "CLHMWeakCache{" + concurrentMap + '}';
+  }
+
+  private static final class WeakKey<T> extends WeakReference<T> {
+
+    private final int hashCode;
+
+    WeakKey(T key, ReferenceQueue<? super T> queue) {
+      super(key, queue);
+      hashCode = key.hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+      return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+      if (other == this) {
+        return true;
+      }
+      if (null == other) {
+        return false;
+      }
+      T key = get();
+      if (null == key) {
+        return false;
+      }
+      Object otherKey;
+      if (other instanceof WeakKey) {
+        WeakKey<?> otherWeakKey = (WeakKey<?>) other;
+        if (hashCode != otherWeakKey.hashCode) {
+          return false;
+        }
+        otherKey = otherWeakKey.get();
+        if (null == otherKey) {
+          return false;
+        }
+      } else {
+        otherKey = other;
+      }
+      return key.equals(otherKey);
+    }
+
+    @Override
+    public String toString() {
+      return "WeakKey{" + get() + "}";
     }
   }
 }
