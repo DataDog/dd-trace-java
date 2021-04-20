1da5f1f9a6c5bf7423cd1ba3d22eaf05025ce4ff
Richard Startin <richard.startin@datadoghq.com>
2021-04-20 16:54:20 +0100

    add shortcut matchers for gRPC

---
+++ dd-java-agent/instrumentation/grpc-1.5/src/test/groovy/HierarchyMatcherGrpcTest.groovy
@@ -0,0 +1,7 @@
+class HierarchyMatcherGrpcTest extends GrpcTest {
+  @Override
+  protected void configurePreAgent() {
+    super.configurePreAgent()
+    injectSysConfig("dd.integration.grpc.matching.shortcut.enabled", "false")
+  }
+}
