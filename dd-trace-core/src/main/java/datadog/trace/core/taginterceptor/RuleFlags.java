0b59b4063d011e21ebd5928d4d7f1a6b91d8ec9a
Tyler Benson <tyler.benson@datadoghq.com>
2021-05-04 12:13:53 -0400

    Add RuleFlag.Feature constructor per review

--- dd-trace-core/src/main/java/datadog/trace/core/taginterceptor/RuleFlags.java
+++ dd-trace-core/src/main/java/datadog/trace/core/taginterceptor/RuleFlags.java
@@ -7,17 +7,22 @@ public class RuleFlags {
   public enum Feature {
     // These names all derive from the simple class names which
     // were exposed as config at some point in the past.
-    RESOURCE_NAME("ResourceNameRule", true),
-    DB_STATEMENT("DBStatementRule", true),
-    FORCE_MANUAL_DROP("ForceManualDropTagInterceptor", true),
-    FORCE_MANUAL_KEEP("ForceManualKeepTagInterceptor", true),
+    RESOURCE_NAME("ResourceNameRule"),
+    DB_STATEMENT("DBStatementRule"),
+    FORCE_MANUAL_DROP("ForceManualDropTagInterceptor"),
+    FORCE_MANUAL_KEEP("ForceManualKeepTagInterceptor"),
     PEER_SERVICE("PeerServiceTagInterceptor", false),
-    SERVICE_NAME("ServiceNameTagInterceptor", true),
-    SERVLET_CONTEXT("ServletContextTagInterceptor", true);
+    SERVICE_NAME("ServiceNameTagInterceptor"),
+    SERVLET_CONTEXT("ServletContextTagInterceptor");
 
     private final String name;
 
     private final boolean defaultEnabled;
+
+    Feature(String name) {
+      this.name = name;
+      this.defaultEnabled = true;
+    }
 
     Feature(String name, boolean defaultEnabled) {
       this.name = name;
