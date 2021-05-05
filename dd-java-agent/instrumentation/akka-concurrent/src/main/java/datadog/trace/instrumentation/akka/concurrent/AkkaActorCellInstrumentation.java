701be8d3732474ad4a68db6a66ebe669bdd4ab97
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2021-05-05 15:01:56 +0200

    Don't create noop scopes for akka messaging unless necessary

--- dd-java-agent/instrumentation/akka-concurrent/src/main/java/datadog/trace/instrumentation/akka/concurrent/AkkaActorCellInstrumentation.java
+++ dd-java-agent/instrumentation/akka-concurrent/src/main/java/datadog/trace/instrumentation/akka/concurrent/AkkaActorCellInstrumentation.java
@@ -3,6 +3,7 @@ package datadog.trace.instrumentation.akka.concurrent;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeScope;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeSpan;
 import static java.util.Collections.singletonMap;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 
@@ -10,7 +11,6 @@ import akka.dispatch.Envelope;
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.bootstrap.InstrumentationContext;
-import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
 import datadog.trace.bootstrap.instrumentation.java.concurrent.AdviceUtils;
 import datadog.trace.bootstrap.instrumentation.java.concurrent.State;
@@ -56,29 +56,40 @@ public class AkkaActorCellInstrumentation extends Instrumenter.Tracing {
    */
   public static class InvokeAdvice {
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static TraceScope enter(@Advice.Argument(value = 0) Envelope envelope) {
-      TraceScope scope =
+    public static TraceScope enter(
+        @Advice.Argument(value = 0) Envelope envelope,
+        @Advice.Local(value = "localScope") TraceScope localScope) {
+      TraceScope activeScope = activeScope();
+      localScope =
           AdviceUtils.startTaskScope(
               InstrumentationContext.get(Envelope.class, State.class), envelope);
-      if (scope != null) {
-        return scope;
+      // There was a scope created from the envelop, so use that
+      if (localScope != null) {
+        return activeScope;
       }
-      // If there is no scope created from the envelope, we create our own noopSpan to make sure
-      // that we can close all scopes up until this position after exit.
-      AgentSpan span = new AgentTracer.NoopAgentSpan();
-      return activateSpan(span, false);
+      // If there is no active scope, we can clean all the way to the bottom
+      if (null == activeScope) {
+        return null;
+      }
+      // If there is a noop span in the active scope, we can clean all the way to this scope
+      if (activeSpan() instanceof AgentTracer.NoopAgentSpan) {
+        return activeScope;
+      }
+      // Create an active scope with a noop span, and clean all the way to the previous scope
+      localScope = activateSpan(AgentTracer.NoopAgentSpan.INSTANCE, false);
+      return activeScope;
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
-    public static void exit(@Advice.Enter TraceScope scope) {
+    public static void exit(
+        @Advice.Enter TraceScope scope, @Advice.Local(value = "localScope") TraceScope localScope) {
+      if (localScope != null) {
+        localScope.close();
+      }
       // Clean up any leaking scopes from akka-streams/akka-http et.c.
       TraceScope activeScope = activeScope();
       while (activeScope != null && activeScope != scope) {
         activeScope.close();
-        activeScope = activeScope();
-      }
-      while (activeScope == scope) {
-        scope.close();
         activeScope = activeScope();
       }
     }
