d43589a27e7a5bdda6d1876aa2a56bb890ea419b
Valentin Zakharov <valentin.zakharov1@gmail.com>
2021-05-27 18:45:12 +0300

    Callback addresses are always unique

--- dd-java-agent/appsec/src/main/java/com/datadog/appsec/Callback.java
+++ dd-java-agent/appsec/src/main/java/com/datadog/appsec/Callback.java
@@ -3,12 +3,12 @@ package com.datadog.appsec;
 import datadog.trace.api.gateway.Flow;
 import datadog.trace.api.gateway.RequestContext;
 
-import java.util.List;
+import java.util.Set;
 
 @SuppressWarnings("rawtypes")
 public interface Callback {
 
-  List<Address> getRequiredAddresses();
+  Set<Address> getRequiredAddresses();
   Flow.ResultFlow onDataAvailable(RequestContext ctx, DataBundle dataBundle, boolean transientData);
 
 }
