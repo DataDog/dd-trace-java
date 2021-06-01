591c0f5a87811eac0865f21587c911ac14093c37
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2021-06-01 12:59:58 +0200

    Add tests

--- dd-trace-core/src/traceAgentTest/groovy/TraceGenerator.groovy
+++ dd-trace-core/src/traceAgentTest/groovy/TraceGenerator.groovy
@@ -129,7 +129,7 @@ class TraceGenerator {
       this.type = type
       this.measured = measured
       this.metadata = new Metadata(Thread.currentThread().getId(),
-        UTF8BytesString.create(Thread.currentThread().getName()), tags, baggage, UNSET, measured, topLevel, null)
+        UTF8BytesString.create(Thread.currentThread().getName()), tags, baggage, UNSET, measured, topLevel, null, null)
     }
 
     @Override
