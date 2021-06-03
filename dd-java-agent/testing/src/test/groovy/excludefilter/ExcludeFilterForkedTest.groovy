1602d13f7ecf916e992e42269e9a3f6db752454d
Richard Startin <richard.startin@datadoghq.com>
2021-06-03 22:17:53 +0100

    clean up exclude filters

--- dd-java-agent/testing/src/test/groovy/excludefilter/ExcludeFilterForkedTest.groovy
+++ dd-java-agent/testing/src/test/groovy/excludefilter/ExcludeFilterForkedTest.groovy
@@ -5,16 +5,16 @@ import datadog.trace.bootstrap.FieldBackedContextStores
 import datadog.trace.bootstrap.instrumentation.java.concurrent.ExcludeFilter
 
 import java.lang.reflect.Field
-import java.util.concurrent.Callable
+import java.util.concurrent.Executor
 
 import static datadog.trace.bootstrap.instrumentation.java.concurrent.ExcludeFilter.ExcludeType.RUNNABLE
-import static excludefilter.ExcludeFilterTestInstrumentation.CallableExcludedRunnable
-import static excludefilter.ExcludeFilterTestInstrumentation.CallableRunnable
-import static excludefilter.ExcludeFilterTestInstrumentation.ExcludedCallable
+import static excludefilter.ExcludeFilterTestInstrumentation.ExecutorExcludedRunnable
+import static excludefilter.ExcludeFilterTestInstrumentation.ExecutorRunnable
+import static excludefilter.ExcludeFilterTestInstrumentation.ExcludedExecutor
 import static excludefilter.ExcludeFilterTestInstrumentation.ExcludedRunnable
-import static excludefilter.ExcludeFilterTestInstrumentation.NormalCallable
+import static excludefilter.ExcludeFilterTestInstrumentation.NormalExecutor
 import static excludefilter.ExcludeFilterTestInstrumentation.NormalRunnable
-import static excludefilter.ExcludeFilterTestInstrumentation.RunnableExcludedCallable
+import static excludefilter.ExcludeFilterTestInstrumentation.RunnableExcludedExecutor
 
 class ExcludeFilterForkedTest extends AgentTestRunner {
 
@@ -28,31 +28,31 @@ class ExcludeFilterForkedTest extends AgentTestRunner {
     new NormalRunnable()   | false
   }
 
-  def "test field injection exclusion"() {
+  def "test field injection exclusion #clazz"() {
     setup:
     def runnableCheck = new InjectionCheck(clazz, Runnable, Object)
-    def callableCheck = new InjectionCheck(clazz, Callable, Object)
+    def executorCheck = new InjectionCheck(clazz, Executor, Object)
 
     expect:
     runnableCheck.hasField() == hasRunnable
     runnableCheck.hasAccessorInterface() == hasAccessor
-    callableCheck.hasField() == hasCallable
-    callableCheck.hasAccessorInterface() == hasAccessor
+    executorCheck.hasField() == hasExecutor
+    executorCheck.hasAccessorInterface() == hasAccessor
 
     where:
-    clazz                    | hasRunnable | hasCallable | hasAccessor
+    clazz                    | hasRunnable | hasExecutor | hasAccessor
     ExcludedRunnable         | false       | false       | false
     NormalRunnable           | true        | false       | true
-    RunnableExcludedCallable | true        | false       | true
-    ExcludedCallable         | false       | false       | false
-    NormalCallable           | false       | true        | true
-    CallableExcludedRunnable | false       | true        | true
-    CallableRunnable         | true        | true        | true
+    RunnableExcludedExecutor | true        | false       | true
+    ExcludedExecutor         | false       | false       | false
+    NormalExecutor           | false       | true        | true
+    ExecutorExcludedRunnable | false       | true        | true
+    ExecutorRunnable         | true        | true        | true
   }
 
   static class InjectionCheck {
     private final boolean hasField
-    private final boolean  hasAccessorInterface
+    private final boolean hasAccessorInterface
 
     InjectionCheck(Class<?> clazz, Class<?> key, Class<?> value) {
       int storeId = FieldBackedContextStores.getContextStoreId(key.name, value.name)
