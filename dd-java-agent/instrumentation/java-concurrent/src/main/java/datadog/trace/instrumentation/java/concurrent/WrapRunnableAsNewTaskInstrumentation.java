011d18d93139ae53da0e7cd331dbf30352ed12d5
Richard Startin <richard.startin@datadoghq.com>
2021-06-04 11:09:45 +0100

    move jetty executor instrumentations into dedicated module with more specific handling for dispatch method

--- dd-java-agent/instrumentation/java-concurrent/src/main/java/datadog/trace/instrumentation/java/concurrent/WrapRunnableAsNewTaskInstrumentation.java
+++ dd-java-agent/instrumentation/java-concurrent/src/main/java/datadog/trace/instrumentation/java/concurrent/WrapRunnableAsNewTaskInstrumentation.java
@@ -42,9 +42,7 @@ public final class WrapRunnableAsNewTaskInstrumentation extends Instrumenter.Tra
         "io.netty.util.concurrent.SingleThreadEventExecutor",
         "java.util.concurrent.AbstractExecutorService",
         "java.util.concurrent.CompletableFuture$ThreadPerTaskExecutor",
-        "org.glassfish.grizzly.threadpool.GrizzlyExecutorService",
-        "org.eclipse.jetty.util.thread.QueuedThreadPool",
-        "org.eclipse.jetty.util.thread.ReservedThreadExecutor");
+        "org.glassfish.grizzly.threadpool.GrizzlyExecutorService");
   }
 
   @Override
