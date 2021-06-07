d38ba4b95e5c38ecd6cab92d891ac824a3b07ca5
Adrian Lopez <adrian.lopezcalvo@datadoghq.com>
2021-06-07 17:40:35 +0200

    Rewrite tags based on java properties

--- .circleci/upload_ciapp.sh
+++ .circleci/upload_ciapp.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 SERVICE_NAME="dd-trace-java"
-TEST_BUNDLE=$1
-JDK_STR=$2
 
-TAGS="test.bundle:${TEST_BUNDLE}"
-if [[ $JDK_STR =~ ([^0-9]*)([0-9]+) ]]; then
-    JDK_NAME=${BASH_REMATCH[1]:-"adoptOpenJDK"}
-    JDK_VERSION=${BASH_REMATCH[2]}
-    TAGS="${TAGS},runtime.name:${JDK_NAME},runtime.version:${JDK_VERSION}"
+# JAVA_???_HOME are set in the base image for each used JDK https://github.com/DataDog/dd-trace-java-docker-build/blob/master/Dockerfile#L86
+java_bin="\$JAVA_$2_HOME/bin/java"
+if [ ! -x $java_bin ]; then
+    java_bin=$(which java)
 fi
+java_props=$($java_bin -XshowSettings:properties -version 2>&1)
+java_prop () {
+    echo "$(echo "$java_props" | grep $1 | head -n1 | cut -d'=' -f2 | xargs)"
+}
 
-datadog-ci junit upload --service $SERVICE --tags $TAGS ./results
+# based on tracer implementation: https://github.com/DataDog/dd-trace-java/blob/master/dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/decorator/TestDecorator.java#L55-L77
+TAGS="test.bundle:$1,\
+runtime.name:$(java_prop java.runtime.name),runtime.vendor:$(java_prop java.vendor),runtime.version:$(java_prop java.version),\
+os.arch:$(java_prop os.arch),os.name:$(java_prop os.name),os.version:$(java_prop os.version)\
+"
+
+datadog-ci junit upload --service $SERVICE_NAME --tags "${TAGS}" ./results
