919b1ca703d177e8774cbe19cd6f0bd838357360
Stuart McCulloch <mcculls@gmail.com>
2021-06-10 23:38:06 +0100

    Add command-line option to list integration names

--- dd-java-agent/src/main/java/datadog/trace/bootstrap/AgentJar.java
+++ dd-java-agent/src/main/java/datadog/trace/bootstrap/AgentJar.java
@@ -3,13 +3,69 @@ package datadog.trace.bootstrap;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.security.CodeSource;
 
 /** Entry point when running the agent as a sample application with -jar. */
 public final class AgentJar {
   private static final Class<?> thisClass = AgentJar.class;
 
   public static void main(final String[] args) {
+    if (args.length == 0) {
+      printAgentVersion();
+    } else {
+      try {
+        switch (args[0]) {
+          case "--list-integrations":
+          case "-li":
+            printIntegrationNames();
+            break;
+          case "--help":
+          case "-h":
+            printUsage();
+            break;
+          case "--version":
+          case "-v":
+            printAgentVersion();
+            break;
+          default:
+            throw new IllegalArgumentException("unknown option: " + args[0]);
+        }
+      } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+        printUsage();
+      } catch (Throwable e) {
+        System.out.println("Failed to process agent option");
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private static void printUsage() {
+    System.out.println("usage: ");
+    System.out.println("       [-li | --list-integrations]");
+    System.out.println("       [-h  | --help]");
+    System.out.println("       [-v  | --version]");
+  }
+
+  private static void printIntegrationNames() throws Exception {
+    CodeSource codeSource = thisClass.getProtectionDomain().getCodeSource();
+    if (codeSource == null || codeSource.getLocation() == null) {
+      throw new MalformedURLException("Could not get jar location from code source");
+    }
+
+    Class<?> agentClass =
+        ClassLoader.getSystemClassLoader().loadClass("datadog.trace.bootstrap.Agent");
+    Method listMethod = agentClass.getMethod("listIntegrationNames", URL.class);
+    for (String name : (Iterable<String>) listMethod.invoke(null, codeSource.getLocation())) {
+      System.out.println(name);
+    }
+  }
+
+  private static void printAgentVersion() {
     try {
       System.out.println(getAgentVersion());
     } catch (final Exception e) {
