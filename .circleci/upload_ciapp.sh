66ee70cd41256ab514db14e36a2612d7a96c2249
Adrian Lopez <adrian.lopezcalvo@datadoghq.com>
2021-06-15 12:39:36 +0200

    Debug JAVA_BIN in ciapp script

--- .circleci/upload_ciapp.sh
+++ .circleci/upload_ciapp.sh
@@ -6,13 +6,15 @@ java_bin="\$JAVA_$2_HOME/bin/java"
 if [ ! -x $java_bin ]; then
     java_bin=$(which java)
 fi
+echo "JAVA_BIN: $java_bin"
+
 java_props=$($java_bin -XshowSettings:properties -version 2>&1)
 java_prop () {
     echo "$(echo "$java_props" | grep $1 | head -n1 | cut -d'=' -f2 | xargs)"
 }
 
 # based on tracer implementation: https://github.com/DataDog/dd-trace-java/blob/master/dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/decorator/TestDecorator.java#L55-L77
-TAGS="test.bundle:$1,\
+TAGS="test.traits:{\"marker\":[\"$1\"]},\
 runtime.name:$(java_prop java.runtime.name),runtime.vendor:$(java_prop java.vendor),runtime.version:$(java_prop java.version),\
 os.architecture:$(java_prop os.arch),os.platform:$(java_prop os.name),os.version:$(java_prop os.version)\
 "
