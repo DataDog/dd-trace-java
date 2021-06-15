c932002a50ae2a384ec447d22a8e14a9fd5897e1
Ludovic Henry <git@ludovic.dev>
2021-06-15 17:38:21 +0200

    Add specific JDK 11, 15, and 16 for OldObjectSampleEvent (#2827)

--- dd-java-agent/agent-profiling/profiling-controller-openjdk/src/test/java/com/datadog/profiling/controller/openjdk/OpenJdkControllerTest.java
+++ dd-java-agent/agent-profiling/profiling-controller-openjdk/src/test/java/com/datadog/profiling/controller/openjdk/OpenJdkControllerTest.java
@@ -1,5 +1,6 @@
 package com.datadog.profiling.controller.openjdk;
 
+import static datadog.trace.api.Platform.isJavaVersion;
 import static datadog.trace.api.Platform.isJavaVersionAtLeast;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -40,7 +41,10 @@ public class OpenJdkControllerTest {
         .thenReturn(JfpUtilsTest.OVERRIDES_OLD_OBJECT_SAMPLE);
     OpenJdkController controller = new OpenJdkController(config);
     try (final Recording recording = controller.createRecording(TEST_NAME).stop().getRecording()) {
-      if (!isJavaVersionAtLeast(17)) {
+      if (!((isJavaVersion(11) && isJavaVersionAtLeast(11, 0, 12))
+          || (isJavaVersion(15) && isJavaVersionAtLeast(15, 0, 4))
+          || (isJavaVersion(16) && isJavaVersionAtLeast(16, 0, 2))
+          || isJavaVersionAtLeast(17))) {
         assertEquals(
             Boolean.parseBoolean(recording.getSettings().get("jdk.OldObjectSample#enabled")),
             false);
