cddcf83f79fea4e1b71013ef7f31111d8011c47d
Adrián López Calvo <adrian.lopezcalvo@datadoghq.com>
2021-06-17 12:31:42 +0200

    [CIAPP] Fix configuration tags for JDK in CI-App script (#2826)

--- .circleci/upload_ciapp.sh
+++ .circleci/upload_ciapp.sh
@@ -2,19 +2,24 @@
 SERVICE_NAME="dd-trace-java"
 
 # JAVA_???_HOME are set in the base image for each used JDK https://github.com/DataDog/dd-trace-java-docker-build/blob/master/Dockerfile#L86
-java_bin="\$JAVA_$2_HOME/bin/java"
+java_home="JAVA_$2_HOME"
+java_bin="${!java_home}/bin/java"
 if [ ! -x $java_bin ]; then
     java_bin=$(which java)
 fi
+
 java_props=$($java_bin -XshowSettings:properties -version 2>&1)
 java_prop () {
     echo "$(echo "$java_props" | grep $1 | head -n1 | cut -d'=' -f2 | xargs)"
 }
 
 # based on tracer implementation: https://github.com/DataDog/dd-trace-java/blob/master/dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/decorator/TestDecorator.java#L55-L77
-TAGS="test.bundle:$1,\
-runtime.name:$(java_prop java.runtime.name),runtime.vendor:$(java_prop java.vendor),runtime.version:$(java_prop java.version),\
-os.architecture:$(java_prop os.arch),os.platform:$(java_prop os.name),os.version:$(java_prop os.version)\
-"
-
-DD_TAGS="${TAGS}" datadog-ci junit upload --service $SERVICE_NAME ./results
+datadog-ci junit upload --service $SERVICE_NAME \
+    --tags "test.traits:{\"marker\":[\"$1\"]}" \
+    --tags "runtime.name:$(java_prop java.runtime.name)" \
+    --tags "runtime.vendor:$(java_prop java.vendor)" \
+    --tags "runtime.version:$(java_prop java.version)" \
+    --tags "os.architecture:$(java_prop os.arch)" \
+    --tags "os.platform:$(java_prop os.name)" \
+    --tags "os.version:$(java_prop os.version)" \
+./results
