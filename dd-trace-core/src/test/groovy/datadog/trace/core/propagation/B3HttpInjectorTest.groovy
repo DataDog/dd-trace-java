6e7819f7910da788f2b8a3d432741e32934e1ba1
Liza Pressman <liza.pressman@datadoghq.com>
2021-06-21 19:30:18 -0400

    wildcard

--- dd-trace-core/src/test/groovy/datadog/trace/core/propagation/B3HttpInjectorTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/core/propagation/B3HttpInjectorTest.groovy
@@ -8,7 +8,10 @@ import datadog.trace.core.DDSpanContext
 import datadog.trace.core.test.DDCoreSpecification
 
 import static datadog.trace.core.CoreTracer.TRACE_ID_MAX
-import static datadog.trace.core.propagation.B3HttpCodec.*
+import static datadog.trace.core.propagation.B3HttpCodec.B3_KEY
+import static datadog.trace.core.propagation.B3HttpCodec.SAMPLING_PRIORITY_KEY
+import static datadog.trace.core.propagation.B3HttpCodec.SPAN_ID_KEY
+import static datadog.trace.core.propagation.B3HttpCodec.TRACE_ID_KEY
 
 class B3HttpInjectorTest extends DDCoreSpecification {
 
@@ -20,20 +23,20 @@ class B3HttpInjectorTest extends DDCoreSpecification {
     def tracer = tracerBuilder().writer(writer).build()
     final DDSpanContext mockedContext =
       new DDSpanContext(
-      DDId.from("$traceId"),
-      DDId.from("$spanId"),
-      DDId.ZERO,
-      null,
-      "fakeService",
-      "fakeOperation",
-      "fakeResource",
-      samplingPriority,
-      "fakeOrigin",
-      ["k1": "v1", "k2": "v2"],
-      false,
-      "fakeType",
-      0,
-      tracer.pendingTraceFactory.create(DDId.ONE))
+        DDId.from("$traceId"),
+        DDId.from("$spanId"),
+        DDId.ZERO,
+        null,
+        "fakeService",
+        "fakeOperation",
+        "fakeResource",
+        samplingPriority,
+        "fakeOrigin",
+        ["k1": "v1", "k2": "v2"],
+        false,
+        "fakeType",
+        0,
+        tracer.pendingTraceFactory.create(DDId.ONE))
 
     final Map<String, String> carrier = Mock()
 
@@ -77,20 +80,20 @@ class B3HttpInjectorTest extends DDCoreSpecification {
     final TagContext context = extractor.extract(headers, ContextVisitors.stringValuesMap())
     final DDSpanContext mockedContext =
       new DDSpanContext(
-      context.traceId,
-      context.spanId,
-      DDId.ZERO,
-      null,
-      "fakeService",
-      "fakeOperation",
-      "fakeResource",
-      PrioritySampling.UNSET,
-      "fakeOrigin",
-      ["k1": "v1", "k2": "v2"],
-      false,
-      "fakeType",
-      0,
-      tracer.pendingTraceFactory.create(DDId.ONE))
+        context.traceId,
+        context.spanId,
+        DDId.ZERO,
+        null,
+        "fakeService",
+        "fakeOperation",
+        "fakeResource",
+        PrioritySampling.UNSET,
+        "fakeOrigin",
+        ["k1": "v1", "k2": "v2"],
+        false,
+        "fakeType",
+        0,
+        tracer.pendingTraceFactory.create(DDId.ONE))
     final Map<String, String> carrier = Mock()
 
     when:
