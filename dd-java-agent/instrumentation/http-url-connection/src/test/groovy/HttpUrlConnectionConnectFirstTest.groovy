38e77eaffcecc405829991ef825c965cad3ec870
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2021-06-29 12:39:49 +0100

    Ignore 'connect first' test on IBM Java because it uses different HTTPS connection types that we don't currently instrument.

--- dd-java-agent/instrumentation/http-url-connection/src/test/groovy/HttpUrlConnectionConnectFirstTest.groovy
+++ dd-java-agent/instrumentation/http-url-connection/src/test/groovy/HttpUrlConnectionConnectFirstTest.groovy
@@ -1,9 +1,12 @@
 import datadog.trace.agent.test.base.HttpClientTest
 import datadog.trace.bootstrap.instrumentation.httpurlconnection.HttpUrlConnectionDecorator
+import spock.lang.Requires
 import spock.lang.Timeout
 
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeScope
 
+// This fails on IBM JVM because it uses different HTTPS connection types
+@Requires({ !System.getProperty("java.vm.name").contains("IBM J9 VM") })
 @Timeout(5)
 class HttpUrlConnectionConnectFirstTest extends HttpClientTest {
 
