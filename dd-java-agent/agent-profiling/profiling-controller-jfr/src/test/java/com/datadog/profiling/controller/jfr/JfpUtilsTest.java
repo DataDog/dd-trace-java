4b5b4cb623407d202ce920859ceea6d8acb1138c
Ludovic Henry <git@ludovic.dev>
2021-06-30 10:43:26 +0200

    Rework how JFR events are disabled based on JVM version (#2858)
    * Include all bin/ folder to .gitignore
    
    * Rework how JFR events are disabled based on JVM version
    
    We want to disable events enabled by default in dd.jfp based on the
    version. For example, OldObjectSample or NativeMethodSample have poor
    performance on older versions. We don't want to penalize customers who
    are on these older versions and just use the default profile.
    
    However, we still want to allow users to override any settings they
    want as they may have perfectly valid reasons to do so and to accept to
    pay the cost for them.

--- dd-java-agent/agent-profiling/profiling-controller-jfr/src/test/java/com/datadog/profiling/controller/jfr/JfpUtilsTest.java
+++ dd-java-agent/agent-profiling/profiling-controller-jfr/src/test/java/com/datadog/profiling/controller/jfr/JfpUtilsTest.java
@@ -18,27 +18,29 @@ public class JfpUtilsTest {
       JfpUtilsTest.class.getClassLoader().getResource("overrides.jfp").getFile();
   public static final String OVERRIDES_OLD_OBJECT_SAMPLE =
       JfpUtilsTest.class.getClassLoader().getResource("overrides-oldobjectsample.jfp").getFile();
+  public static final String OVERRIDES_OBJECT_ALLOCATION =
+      JfpUtilsTest.class.getClassLoader().getResource("overrides-objectallocation.jfp").getFile();
+  public static final String OVERRIDES_NATIVE_METHOD_SAMPLE =
+      JfpUtilsTest.class.getClassLoader().getResource("overrides-nativemethodsample.jfp").getFile();
 
   @Test
   public void testLoadingInvalidOverride() throws IOException {
     final String INVALID_OVERRIDE = "really_non_existent_file.jfp";
 
     assertThrows(
-        IOException.class,
-        () -> JfpUtils.readNamedJfpResource(JfpUtils.DEFAULT_JFP, INVALID_OVERRIDE));
+        IOException.class, () -> JfpUtils.readJfpResources(JfpUtils.DEFAULT_JFP, INVALID_OVERRIDE));
   }
 
   @Test
   public void testLoadingContinuousConfig() throws IOException {
-    final Map<String, String> config = JfpUtils.readNamedJfpResource(JfpUtils.DEFAULT_JFP, null);
+    final Map<String, String> config = JfpUtils.readJfpResources(JfpUtils.DEFAULT_JFP, null);
     assertEquals("true", config.get(CONFIG_ENTRY));
     assertNull(config.get(CONFIG_OVERRIDE_ENTRY));
   }
 
   @Test
   public void testLoadingContinuousConfigWithOverride() throws IOException {
-    final Map<String, String> config =
-        JfpUtils.readNamedJfpResource(JfpUtils.DEFAULT_JFP, OVERRIDES);
+    final Map<String, String> config = JfpUtils.readJfpResources(JfpUtils.DEFAULT_JFP, OVERRIDES);
     assertEquals("true", config.get(CONFIG_ENTRY));
     assertEquals("200", config.get(CONFIG_OVERRIDE_ENTRY));
   }
@@ -46,7 +48,7 @@ public class JfpUtilsTest {
   @ParameterizedTest
   @ValueSource(strings = {"minimal", "minimal.jfp"})
   public void testLoadingConfigMinimal(String override) throws IOException {
-    Map<String, String> config = JfpUtils.readNamedJfpResource(JfpUtils.DEFAULT_JFP, override);
+    Map<String, String> config = JfpUtils.readJfpResources(JfpUtils.DEFAULT_JFP, override);
     assertEquals("500 ms", config.get("jdk.ThreadSleep#threshold"));
     assertEquals("false", config.get("jdk.OldObjectSample#enabled"));
     assertEquals("false", config.get("jdk.ObjectAllocationInNewTLAB#enabled"));
@@ -55,7 +57,7 @@ public class JfpUtilsTest {
   @ParameterizedTest
   @ValueSource(strings = {"comprehensive", "comprehensive.jfp"})
   public void testLoadingConfigComprehensive(String override) throws IOException {
-    Map<String, String> config = JfpUtils.readNamedJfpResource(JfpUtils.DEFAULT_JFP, override);
+    Map<String, String> config = JfpUtils.readJfpResources(JfpUtils.DEFAULT_JFP, override);
     assertEquals("10 ms", config.get("jdk.ThreadSleep#threshold"));
     assertEquals("true", config.get("jdk.OldObjectSample#enabled"));
     assertEquals("true", config.get("jdk.ObjectAllocationInNewTLAB#enabled"));
