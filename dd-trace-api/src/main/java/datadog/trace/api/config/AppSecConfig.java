25e63069f8f6ecdf60be83cbc4cd7093b2e9ee45
Valentin Zakharov <valentin.zakharov1@gmail.com>
2021-07-02 00:55:41 +0300

    Changed AppSec Enable variable name to follow convention

--- dd-trace-api/src/main/java/datadog/trace/api/config/AppSecConfig.java
+++ dd-trace-api/src/main/java/datadog/trace/api/config/AppSecConfig.java
@@ -2,7 +2,7 @@ package datadog.trace.api.config;
 
 public final class AppSecConfig {
 
-  public static final String APPSEC_ENABLED = "appsec.beta";
+  public static final String APPSEC_ENABLED = "appsec.enabled";
 
   private AppSecConfig() {}
 }
