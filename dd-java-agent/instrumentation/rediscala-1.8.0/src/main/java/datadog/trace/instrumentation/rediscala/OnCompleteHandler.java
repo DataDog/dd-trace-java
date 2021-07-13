4de8fe8bc6a9c45e2252430e16d96d4291b85db1
Richard Startin <richard.startin@datadoghq.com>
2021-07-13 17:56:54 +0100

    make rediscala instrumentation scala-promise aware

--- dd-java-agent/instrumentation/rediscala-1.8.0/src/main/java/datadog/trace/instrumentation/rediscala/OnCompleteHandler.java
+++ dd-java-agent/instrumentation/rediscala-1.8.0/src/main/java/datadog/trace/instrumentation/rediscala/OnCompleteHandler.java
@@ -3,31 +3,30 @@ package datadog.trace.instrumentation.rediscala;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeScope;
 import static datadog.trace.instrumentation.rediscala.RediscalaClientDecorator.DECORATE;
 
+import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.context.TraceScope;
 import scala.runtime.AbstractFunction1;
 import scala.util.Try;
 
-public class OnCompleteHandler extends AbstractFunction1<Try<Object>, Void> {
-  private final AgentSpan span;
+public final class OnCompleteHandler extends AbstractFunction1<Try<Object>, Void> {
 
-  public OnCompleteHandler(final AgentSpan span) {
-    this.span = span;
-  }
+  public static final OnCompleteHandler INSTANCE = new OnCompleteHandler();
 
   @Override
   public Void apply(final Try<Object> result) {
-    try {
-      if (result.isFailure()) {
-        DECORATE.onError(span, result.failed().get());
+    // propagation handled by scala promise instrumentation
+    TraceScope activeScope = activeScope();
+    if (activeScope instanceof AgentScope) {
+      AgentSpan span = ((AgentScope) activeScope).span();
+      try {
+        if (result.isFailure()) {
+          DECORATE.onError(span, result.failed().get());
+        }
+        DECORATE.beforeFinish(span);
+      } finally {
+        span.finish();
       }
-      DECORATE.beforeFinish(span);
-      final TraceScope scope = activeScope();
-      if (scope != null) {
-        scope.setAsyncPropagation(false);
-      }
-    } finally {
-      span.finish();
     }
     return null;
   }
