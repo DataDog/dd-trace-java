24660551a8689a5e51168cc4cdb246a1a2866505
Richard Startin <richard.startin@datadoghq.com>
2021-07-15 12:54:46 +0100

    prevent double wrpping in identified executors

--- dd-java-agent/instrumentation/java-concurrent/src/main/java/datadog/trace/instrumentation/java/concurrent/ThreadPoolExecutorInstrumentation.java
+++ dd-java-agent/instrumentation/java-concurrent/src/main/java/datadog/trace/instrumentation/java/concurrent/ThreadPoolExecutorInstrumentation.java
@@ -2,9 +2,12 @@ package datadog.trace.instrumentation.java.concurrent;
 
 import static datadog.trace.agent.tooling.bytebuddy.matcher.DDElementMatchers.extendsClass;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.namedOneOf;
 import static datadog.trace.bootstrap.instrumentation.java.concurrent.ExcludeFilter.ExcludeType.RUNNABLE;
 import static datadog.trace.instrumentation.java.concurrent.AbstractExecutorInstrumentation.EXEC_NAME;
+import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.not;
 import static net.bytebuddy.matcher.ElementMatchers.returns;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
@@ -18,6 +21,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 import net.bytebuddy.matcher.ElementMatchers;
@@ -25,6 +29,14 @@ import net.bytebuddy.matcher.ElementMatchers;
 @AutoService(Instrumenter.class)
 public final class ThreadPoolExecutorInstrumentation extends Instrumenter.Tracing
     implements ExcludeFilterProvider {
+
+  // executors which do their own wrapping before calling super,
+  // leading to double wrapping, once at the child level and once
+  // in ThreadPoolExecutor
+  private static final ElementMatcher<MethodDescription> NO_WRAPPING_BEFORE_DELEGATION =
+      not(
+          isDeclaredBy(
+              namedOneOf("org.elasticsearch.common.util.concurrent.EsThreadPoolExecutor")));
 
   public ThreadPoolExecutorInstrumentation() {
     super(EXEC_NAME);
@@ -39,7 +51,9 @@ public final class ThreadPoolExecutorInstrumentation extends Instrumenter.Tracin
 
   @Override
   public void adviceTransformations(AdviceTransformation transformation) {
-    transformation.applyAdvice(named("execute").and(isMethod()), getClass().getName() + "$Execute");
+    transformation.applyAdvice(
+        named("execute").and(isMethod()).and(NO_WRAPPING_BEFORE_DELEGATION),
+        getClass().getName() + "$Execute");
     transformation.applyAdvice(
         named("beforeExecute")
             .and(isMethod())
