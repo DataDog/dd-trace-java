ac6641c43fb432ead093ec605ca272e2601b3623
Ludovic Henry <git@ludovic.dev>
2021-07-30 13:47:22 +0000

    Revert merge issue

--- dd-java-agent/testing/src/test/groovy/checkpoints/IntervalValidatorTest.groovy
+++ dd-java-agent/testing/src/test/groovy/checkpoints/IntervalValidatorTest.groovy
@@ -15,91 +15,99 @@ class IntervalValidatorTest extends Specification {
 
   def "start_end span"() {
     expect:
-    tracker.startSpan(1)
-    tracker.endSpan(1)
+    tracker.startSpan(1L)
+    tracker.endSpan(1L)
     tracker.endSequence()
   }
 
   def "end span on other thread"() {
     expect:
-    tracker.startSpan(1)
-    tracker.startSpan(2)
-    tracker.suspendSpan(2)
-    tracker.endSpan(1)
+    tracker.startSpan(1L)
+    tracker.startSpan(2L)
+    tracker.suspendSpan(2L)
+    tracker.endSpan(1L)
     tracker.endSequence()
   }
 
   def "overlapping spans"() {
     expect:
-    tracker.startSpan(1)
-    tracker.startSpan(2)
-    !tracker.endSpan(1)
-    !tracker.endSpan(2)
+    tracker.startSpan(1L)
+    tracker.startSpan(2L)
+    !tracker.endSpan(1L)
+    !tracker.endSpan(2L)
   }
 
   def "overlapping spans complex"() {
     expect:
-    tracker.startSpan(1)
-    tracker.startSpan(2)
+    tracker.startSpan(1L)
+    tracker.startSpan(2L)
     tracker.startSpan(3)
     tracker.endSpan(3)
-    !tracker.endSpan(1)
-    !tracker.endSpan(2)
+    !tracker.endSpan(1L)
+    !tracker.endSpan(2L)
   }
 
   def "suspend resume same thread"() {
     expect:
-    tracker.startSpan(1)
-    tracker.suspendSpan(1)
-    tracker.suspendSpan(1)
-    tracker.endTask(1)
-    tracker.resumeSpan(1)
-    tracker.resumeSpan(1)
-    tracker.endTask(1)
-    tracker.endSpan(1)
+    tracker.startSpan(1L)
+    tracker.suspendSpan(1L)
+    tracker.suspendSpan(1L)
+    tracker.endTask(1L)
+    tracker.resumeSpan(1L)
+    tracker.resumeSpan(1L)
+    tracker.endTask(1L)
+    tracker.endSpan(1L)
     tracker.endSequence()
   }
 
   def "end task after end span"() {
     expect:
-    tracker.resumeSpan(1)
-    tracker.endSpan(1)
-    tracker.endTask(1)
+    tracker.resumeSpan(1L)
+    tracker.endSpan(1L)
+    tracker.endTask(1L)
     tracker.endSequence()
   }
 
   def "end task before end span"() {
     expect:
-    tracker.resumeSpan(1)
-    tracker.endTask(1)
-    tracker.endSpan(1)
+    tracker.resumeSpan(1L)
+    tracker.endTask(1L)
+    tracker.endSpan(1L)
     tracker.endSequence()
   }
 
   def "included spans"() {
     expect:
-    tracker.startSpan(1)
-    tracker.suspendSpan(1)
-    tracker.startSpan(2)
-    tracker.endSpan(2)
+    tracker.startSpan(1L)
+    tracker.suspendSpan(1L)
+    tracker.startSpan(2L)
+    tracker.endSpan(2L)
     tracker.startSpan(3)
     tracker.endSpan(3)
-    tracker.resumeSpan(1)
-    tracker.endSpan(1)
+    tracker.resumeSpan(1L)
+    tracker.endSpan(1L)
     tracker.endSequence()
   }
 
   def "dangling spans"() {
     expect:
-    tracker.startSpan(1)
-    tracker.startSpan(2)
+    tracker.startSpan(1L)
+    tracker.startSpan(2L)
     !tracker.endSequence()
   }
 
   def "dangling suspends"() {
     expect:
-    tracker.startSpan(1)
-    tracker.suspendSpan(1)
-    !tracker.endSequence()
+    tracker.startSpan(1L)
+    tracker.suspendSpan(1L)
+    tracker.endSequence()
+  }
+
+  def "dangling suspend after end span"() {
+    expect:
+    tracker.startSpan(1L)
+    tracker.endSpan(1L)
+    tracker.suspendSpan(1L)
+    tracker.endSequence()
   }
 }
