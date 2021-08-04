eef2b1de61f13b0a97f733534992f660db914280
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2021-08-04 18:29:48 +0100

    Support disabling JMS propagation for topics/queues when using DatadogMessageListener

--- dd-java-agent/instrumentation/jms/src/main/java/datadog/trace/instrumentation/jms/DatadogMessageListener.java
+++ dd-java-agent/instrumentation/jms/src/main/java/datadog/trace/instrumentation/jms/DatadogMessageListener.java
@@ -7,6 +7,7 @@ import static datadog.trace.instrumentation.jms.JMSDecorator.CONSUMER_DECORATE;
 import static datadog.trace.instrumentation.jms.JMSDecorator.JMS_CONSUME;
 import static datadog.trace.instrumentation.jms.MessageExtractAdapter.GETTER;
 
+import datadog.trace.api.Config;
 import datadog.trace.bootstrap.ContextStore;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
@@ -16,23 +17,29 @@ import javax.jms.MessageListener;
 
 public class DatadogMessageListener implements MessageListener {
 
-  private final ContextStore<Message, AgentSpan> messageSpanStore;
+  private final ContextStore<Message, AgentSpan> messageAckStore;
   private final MessageConsumerState messageConsumerState;
   private final MessageListener messageListener;
 
   public DatadogMessageListener(
-      ContextStore<Message, AgentSpan> messageSpanStore,
+      ContextStore<Message, AgentSpan> messageAckStore,
       MessageListener messageListener,
       MessageConsumerState messageConsumerState) {
-    this.messageSpanStore = messageSpanStore;
+    this.messageAckStore = messageAckStore;
     this.messageConsumerState = messageConsumerState;
     this.messageListener = messageListener;
   }
 
   @Override
   public void onMessage(Message message) {
-    AgentSpan.Context extractedContext = propagate().extract(message, GETTER);
-    AgentSpan span = startSpan(JMS_CONSUME, extractedContext);
+    AgentSpan span;
+    String destinationName = messageConsumerState.getDestinationName();
+    if (!Config.get().isJMSPropagationDisabledForDestination(destinationName)) {
+      AgentSpan.Context extractedContext = propagate().extract(message, GETTER);
+      span = startSpan(JMS_CONSUME, extractedContext);
+    } else {
+      span = startSpan(JMS_CONSUME, null);
+    }
     CONSUMER_DECORATE.afterStart(span);
     CONSUMER_DECORATE.onConsume(span, message, messageConsumerState.getResourceName());
     try (AgentScope scope = activateSpan(span)) {
@@ -42,10 +49,12 @@ public class DatadogMessageListener implements MessageListener {
       throw thrown;
     } finally {
       if (messageConsumerState.isClientAcknowledge()) {
-        messageSpanStore.put(message, span);
+        // span will be finished by a call to Message.acknowledge
+        messageAckStore.put(message, span);
       } else if (messageConsumerState.isAutoAcknowledge()) {
         span.finish();
       } else if (messageConsumerState.isTransactedSession()) {
+        // span will be finished by Session.commit/rollback/close
         messageConsumerState.getSessionState().finishOnCommit(span);
       }
     }
