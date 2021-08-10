7e6a0426ad1f242df8a8791c051fa69e406bd218
Ludovic Henry <git@ludovic.dev>
2021-08-10 11:25:47 +0000

    Fix failures on CI

--- dd-java-agent/instrumentation/play-2.4/src/test/groovy/server/PlayAsyncServerTest.groovy
+++ dd-java-agent/instrumentation/play-2.4/src/test/groovy/server/PlayAsyncServerTest.groovy
@@ -1,6 +1,8 @@
 package server
 
 import datadog.trace.agent.test.base.HttpServer
+import datadog.trace.agent.test.checkpoints.CheckpointValidator
+import datadog.trace.agent.test.checkpoints.CheckpointValidationMode
 import play.libs.concurrent.HttpExecution
 import play.mvc.Results
 import play.routing.RoutingDsl
@@ -18,6 +20,12 @@ import static datadog.trace.agent.test.base.HttpServerTest.ServerEndpoint.REDIRE
 import static datadog.trace.agent.test.base.HttpServerTest.ServerEndpoint.SUCCESS
 
 class PlayAsyncServerTest extends PlayServerTest {
+
+  @Override
+  def setup() {
+    CheckpointValidator.excludeValidations_DONOTUSE_I_REPEAT_DO_NOT_USE(
+      CheckpointValidationMode.INTERVALS)
+  }
 
   @Override
   HttpServer server() {
