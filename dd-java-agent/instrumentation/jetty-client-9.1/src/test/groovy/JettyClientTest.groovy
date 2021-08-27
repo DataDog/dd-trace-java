c16b9c47e356eb7c1d1fab73f7213cf99b5aa551
Tyler Benson <tyler.benson@datadoghq.com>
2021-08-27 15:29:31 -0400

    Fix merge conflict with JettyClientTest

--- dd-java-agent/instrumentation/jetty-client-9.1/src/test/groovy/JettyClientTest.groovy
+++ dd-java-agent/instrumentation/jetty-client-9.1/src/test/groovy/JettyClientTest.groovy
@@ -97,7 +97,6 @@ class JettyClientTest extends HttpClientTest {
   def setup() {
     CheckpointValidator.excludeValidations_DONOTUSE_I_REPEAT_DO_NOT_USE(
       CheckpointValidationMode.INTERVALS,
-      CheckpointValidationMode.SUSPEND_RESUME,
       CheckpointValidationMode.THREAD_SEQUENCE)
   }
 }
