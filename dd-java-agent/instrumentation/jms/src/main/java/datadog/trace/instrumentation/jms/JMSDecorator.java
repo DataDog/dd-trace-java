fae71f9efdc7c861babcc387559ba49a08be71ad
Stuart McCulloch <mcculls@gmail.com>
2021-09-01 11:47:01 +0100

    Cache JMS resource names (#3044)

--- dd-java-agent/instrumentation/jms/src/main/java/datadog/trace/instrumentation/jms/JMSDecorator.java
+++ dd-java-agent/instrumentation/jms/src/main/java/datadog/trace/instrumentation/jms/JMSDecorator.java
@@ -3,6 +3,11 @@ package datadog.trace.instrumentation.jms;
 import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.RECORD_QUEUE_TIME_MS;
 
 import datadog.trace.api.DDSpanTypes;
+import datadog.trace.api.Function;
+import datadog.trace.api.Functions.Join;
+import datadog.trace.api.Functions.PrefixJoin;
+import datadog.trace.api.cache.DDCache;
+import datadog.trace.api.cache.DDCaches;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.Tags;
 import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
@@ -21,15 +26,38 @@ public final class JMSDecorator extends ClientDecorator {
   public static final CharSequence JMS_CONSUME = UTF8BytesString.create("jms.consume");
   public static final CharSequence JMS_PRODUCE = UTF8BytesString.create("jms.produce");
 
+  private static final Join QUEUE_JOINER = PrefixJoin.of("Queue ");
+  private static final Join TOPIC_JOINER = PrefixJoin.of("Topic ");
+
+  private final DDCache<CharSequence, CharSequence> resourceNameCache =
+      DDCaches.newFixedSizeCache(32);
+
+  private final String resourcePrefix;
+
+  private final UTF8BytesString queueTempResourceName;
+  private final UTF8BytesString topicTempResourceName;
+
+  private final Function<CharSequence, CharSequence> queueResourceJoiner;
+  private final Function<CharSequence, CharSequence> topicResourceJoiner;
+
   private final String spanKind;
   private final String spanType;
+
   public static final JMSDecorator PRODUCER_DECORATE =
-      new JMSDecorator(Tags.SPAN_KIND_PRODUCER, DDSpanTypes.MESSAGE_PRODUCER);
+      new JMSDecorator("Produced for ", Tags.SPAN_KIND_PRODUCER, DDSpanTypes.MESSAGE_PRODUCER);
 
   public static final JMSDecorator CONSUMER_DECORATE =
-      new JMSDecorator(Tags.SPAN_KIND_CONSUMER, DDSpanTypes.MESSAGE_CONSUMER);
+      new JMSDecorator("Consumed from ", Tags.SPAN_KIND_CONSUMER, DDSpanTypes.MESSAGE_CONSUMER);
 
-  public JMSDecorator(String spanKind, String spanType) {
+  public JMSDecorator(String resourcePrefix, String spanKind, String spanType) {
+    this.resourcePrefix = resourcePrefix;
+
+    this.queueTempResourceName = UTF8BytesString.create(resourcePrefix + "Temporary Queue");
+    this.topicTempResourceName = UTF8BytesString.create(resourcePrefix + "Temporary Topic");
+
+    this.queueResourceJoiner = QUEUE_JOINER.curry(resourcePrefix);
+    this.topicResourceJoiner = TOPIC_JOINER.curry(resourcePrefix);
+
     this.spanKind = spanKind;
     this.spanType = spanType;
   }
@@ -59,7 +87,7 @@ public final class JMSDecorator extends ClientDecorator {
     return spanKind;
   }
 
-  public void onConsume(final AgentSpan span, final Message message, UTF8BytesString resourceName) {
+  public void onConsume(AgentSpan span, Message message, CharSequence resourceName) {
     if (null != resourceName) {
       span.setResourceName(resourceName);
     }
@@ -74,41 +102,45 @@ public final class JMSDecorator extends ClientDecorator {
     }
   }
 
-  public void onProduce(
-      final AgentSpan span, final Message message, final Destination destination) {
-    span.setResourceName("Produced for " + toResourceName(message, destination));
+  public void onProduce(AgentSpan span, CharSequence resourceName) {
+    if (null != resourceName) {
+      span.setResourceName(resourceName);
+    }
   }
 
   private static final String TIBCO_TMP_PREFIX = "$TMP$";
 
-  public static String toResourceName(final Message message, final Destination destination) {
-    Destination jmsDestination = null;
-    try {
-      jmsDestination = message.getJMSDestination();
-    } catch (Exception ignored) {
+  public CharSequence toResourceName(String destinationName, boolean isQueue) {
+    if (null == destinationName) {
+      return isQueue ? queueTempResourceName : topicTempResourceName;
     }
-    if (jmsDestination == null) {
-      jmsDestination = destination;
+    Function<CharSequence, CharSequence> joiner =
+        isQueue ? queueResourceJoiner : topicResourceJoiner;
+    // some systems may have queues and topics with the same name - since we won't know which was
+    // cached first we check the character after the initial prefix to see if it's Q (for Queue) -
+    // if that's what we expect we can use the cached value, otherwise generate the correct name
+    CharSequence resourceName = resourceNameCache.computeIfAbsent(destinationName, joiner);
+    if ((resourceName.charAt(resourcePrefix.length()) == 'Q') == isQueue) {
+      return resourceName;
     }
+    return joiner.apply(destinationName);
+  }
+
+  public String getDestinationName(Destination destination) {
+    String name = null;
     try {
-      if (jmsDestination instanceof Queue) {
-        final String queueName = ((Queue) jmsDestination).getQueueName();
-        if (jmsDestination instanceof TemporaryQueue || queueName.startsWith(TIBCO_TMP_PREFIX)) {
-          return "Temporary Queue";
-        } else {
-          return "Queue " + queueName;
+      if (destination instanceof Queue) {
+        if (!(destination instanceof TemporaryQueue)) {
+          name = ((Queue) destination).getQueueName();
         }
       }
-      if (jmsDestination instanceof Topic) {
-        final String topicName = ((Topic) jmsDestination).getTopicName();
-        if (jmsDestination instanceof TemporaryTopic || topicName.startsWith(TIBCO_TMP_PREFIX)) {
-          return "Temporary Topic";
-        } else {
-          return "Topic " + topicName;
+      if (destination instanceof Topic) {
+        if (!(destination instanceof TemporaryTopic)) {
+          name = ((Topic) destination).getTopicName();
         }
       }
     } catch (Exception ignored) {
     }
-    return "Destination";
+    return null != name && !name.startsWith(TIBCO_TMP_PREFIX) ? name : null;
   }
 }
