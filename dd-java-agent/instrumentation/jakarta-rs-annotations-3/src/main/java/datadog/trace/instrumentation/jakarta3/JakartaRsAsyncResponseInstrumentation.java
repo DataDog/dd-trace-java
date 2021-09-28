4c12d4ad96df2434c734ce5b9cdf2d29f98c7485
Tuan Nguyen <tnguyen@pason.com>
2021-09-28 12:40:07 -0600

    HFLL-3789: Patch datadog to work with jetty 11 and jakarta

---
+++ dd-java-agent/instrumentation/jakarta-rs-annotations-3/src/main/java/datadog/trace/instrumentation/jakarta3/JakartaRsAsyncResponseInstrumentation.java
@@ -0,0 +1,125 @@
+package datadog.trace.instrumentation.jakarta3;
+
+import static datadog.trace.agent.tooling.ClassLoaderMatcher.hasClassesNamed;
+import static datadog.trace.agent.tooling.bytebuddy.matcher.DDElementMatchers.implementsInterface;
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static datadog.trace.instrumentation.jakarta3.JakartaRsAnnotationsDecorator.DECORATE;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.bootstrap.ContextStore;
+import datadog.trace.bootstrap.InstrumentationContext;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import java.util.Collections;
+import java.util.Map;
+import jakarta.ws.rs.container.AsyncResponse;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+@AutoService(Instrumenter.class)
+public final class JakartaRsAsyncResponseInstrumentation extends Instrumenter.Tracing {
+
+  public JakartaRsAsyncResponseInstrumentation() {
+    super("jakarta-rs", "jakartars", "jakarta-rs-annotations");
+  }
+
+  static final ElementMatcher<ClassLoader> CLASS_LOADER_MATCHER =
+      hasClassesNamed("jakarta.ws.rs.container.AsyncResponse");
+
+  @Override
+  public Map<String, String> contextStore() {
+    return Collections.singletonMap(
+        "jakarta.ws.rs.container.AsyncResponse", AgentSpan.class.getName());
+  }
+
+  @Override
+  public ElementMatcher<ClassLoader> classLoaderMatcher() {
+    // Optimization for expensive typeMatcher.
+    return CLASS_LOADER_MATCHER;
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return implementsInterface(named("jakarta.ws.rs.container.AsyncResponse"));
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      "datadog.trace.agent.tooling.ClassHierarchyIterable",
+      "datadog.trace.agent.tooling.ClassHierarchyIterable$ClassIterator",
+      packageName + ".JakartaRsAnnotationsDecorator",
+    };
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    transformation.applyAdvice(
+        named("resume").and(takesArgument(0, Object.class)).and(isPublic()),
+        JakartaRsAsyncResponseInstrumentation.class.getName() + "$AsyncResponseAdvice");
+    transformation.applyAdvice(
+        named("resume").and(takesArgument(0, Throwable.class)).and(isPublic()),
+        JakartaRsAsyncResponseInstrumentation.class.getName() + "$AsyncResponseThrowableAdvice");
+    transformation.applyAdvice(
+        named("cancel"),
+        JakartaRsAsyncResponseInstrumentation.class.getName() + "$AsyncResponseCancelAdvice");
+  }
+
+  public static class AsyncResponseAdvice {
+
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void stopSpan(@Advice.This final AsyncResponse asyncResponse) {
+
+      final ContextStore<AsyncResponse, AgentSpan> contextStore =
+          InstrumentationContext.get(AsyncResponse.class, AgentSpan.class);
+
+      final AgentSpan span = contextStore.get(asyncResponse);
+      if (span != null) {
+        contextStore.put(asyncResponse, null);
+        DECORATE.beforeFinish(span);
+        span.finish();
+      }
+    }
+  }
+
+  public static class AsyncResponseThrowableAdvice {
+
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void stopSpan(
+        @Advice.This final AsyncResponse asyncResponse,
+        @Advice.Argument(0) final Throwable throwable) {
+
+      final ContextStore<AsyncResponse, AgentSpan> contextStore =
+          InstrumentationContext.get(AsyncResponse.class, AgentSpan.class);
+
+      final AgentSpan span = contextStore.get(asyncResponse);
+      if (span != null) {
+        contextStore.put(asyncResponse, null);
+        DECORATE.onError(span, throwable);
+        DECORATE.beforeFinish(span);
+        span.finish();
+      }
+    }
+  }
+
+  public static class AsyncResponseCancelAdvice {
+
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void stopSpan(@Advice.This final AsyncResponse asyncResponse) {
+
+      final ContextStore<AsyncResponse, AgentSpan> contextStore =
+          InstrumentationContext.get(AsyncResponse.class, AgentSpan.class);
+
+      final AgentSpan span = contextStore.get(asyncResponse);
+      if (span != null) {
+        contextStore.put(asyncResponse, null);
+        span.setTag("canceled", true);
+        DECORATE.beforeFinish(span);
+        span.finish();
+      }
+    }
+  }
+}
---
+++ dd-java-agent/instrumentation/jetty-11/src/main/java/datadog/trace/instrumentation/jetty11/JettyDecorator.java
@@ -0,0 +1,72 @@
+package datadog.trace.instrumentation.jetty11;
+
+import datadog.trace.api.Config;
+import datadog.trace.api.DDTags;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.URIDataAdapter;
+import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
+import datadog.trace.bootstrap.instrumentation.decorator.HttpServerDecorator;
+import jakarta.servlet.ServletException;
+import org.eclipse.jetty.server.HttpChannel;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+
+public class JettyDecorator extends HttpServerDecorator<Request, Request, Response> {
+  public static final CharSequence SERVLET_REQUEST = UTF8BytesString.create("servlet.request");
+  public static final CharSequence JETTY_SERVER = UTF8BytesString.create("jetty-server");
+  public static final JettyDecorator DECORATE = new JettyDecorator();
+  public static final String DD_CONTEXT_PATH_ATTRIBUTE = "datadog.context.path";
+  public static final String DD_SERVLET_PATH_ATTRIBUTE = "datadog.servlet.path";
+
+  @Override
+  protected String[] instrumentationNames() {
+    return new String[] {"jetty"};
+  }
+
+  @Override
+  protected CharSequence component() {
+    return JETTY_SERVER;
+  }
+
+  @Override
+  protected String method(final Request request) {
+    return request.getMethod();
+  }
+
+  @Override
+  protected URIDataAdapter url(final Request request) {
+    return new RequestURIDataAdapter(request);
+  }
+
+  @Override
+  protected String peerHostIP(final Request request) {
+    return request.getRemoteAddr();
+  }
+
+  @Override
+  protected int peerPort(final Request request) {
+    return request.getRemotePort();
+  }
+
+  @Override
+  protected int status(final Response response) {
+    return response.getStatus();
+  }
+
+  public AgentSpan onResponse(AgentSpan span, HttpChannel channel) {
+    Request request = channel.getRequest();
+    Response response = channel.getResponse();
+    if (Config.get().isServletPrincipalEnabled() && request.getUserPrincipal() != null) {
+      span.setTag(DDTags.USER_NAME, request.getUserPrincipal().getName());
+    }
+    Object ex = request.getAttribute("javax.servlet.error.exception");
+    if (ex instanceof Throwable) {
+      Throwable throwable = (Throwable) ex;
+      if (throwable instanceof ServletException) {
+        throwable = ((ServletException) throwable).getRootCause();
+      }
+      onError(span, throwable);
+    }
+    return super.onResponse(span, response);
+  }
+}
