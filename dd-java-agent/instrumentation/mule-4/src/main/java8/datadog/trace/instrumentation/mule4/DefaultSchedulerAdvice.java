412d9ce65e520cb51b1b81ee0949f57a5f2e99bb
Ludovic Henry <git@ludovic.dev>
2021-09-30 08:33:28 +0000

    Fix muzzle

--- dd-java-agent/instrumentation/mule-4/src/main/java8/datadog/trace/instrumentation/mule4/DefaultSchedulerAdvice.java
+++ dd-java-agent/instrumentation/mule-4/src/main/java8/datadog/trace/instrumentation/mule4/DefaultSchedulerAdvice.java
@@ -4,6 +4,8 @@ import datadog.trace.bootstrap.CallDepthThreadLocalMap;
 import java.util.concurrent.RunnableFuture;
 import net.bytebuddy.asm.Advice;
 
+import org.mule.runtime.core.internal.connector.SchedulerController;
+
 public class DefaultSchedulerAdvice {
   @Advice.OnMethodEnter(suppress = Throwable.class)
   public static void onEnter() {
@@ -13,5 +15,11 @@ public class DefaultSchedulerAdvice {
   @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
   public static void onExit() {
     CallDepthThreadLocalMap.decrementCallDepth(RunnableFuture.class);
+  }
+
+  public static void muzzleCheck(
+      // Moved in 4.0
+      SchedulerController controller) {
+    controller.isPrimarySchedulingInstance();
   }
 }
--- dd-java-agent/instrumentation/mule-4/src/main/java8/datadog/trace/instrumentation/mule4/HttpRequesterDoRequestAdvice.java
+++ dd-java-agent/instrumentation/mule-4/src/main/java8/datadog/trace/instrumentation/mule4/HttpRequesterDoRequestAdvice.java
@@ -4,6 +4,8 @@ import datadog.trace.bootstrap.CallDepthThreadLocalMap;
 import java.util.concurrent.CompletableFuture;
 import net.bytebuddy.asm.Advice;
 
+import org.mule.runtime.core.internal.connector.SchedulerController;
+
 public class HttpRequesterDoRequestAdvice {
 
   @Advice.OnMethodEnter(suppress = Throwable.class)
@@ -14,5 +16,11 @@ public class HttpRequesterDoRequestAdvice {
   @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
   public static void onExit() {
     CallDepthThreadLocalMap.decrementCallDepth(CompletableFuture.class);
+  }
+
+  public static void muzzleCheck(
+      // Moved in 4.0
+      SchedulerController controller) {
+    controller.isPrimarySchedulingInstance();
   }
 }
