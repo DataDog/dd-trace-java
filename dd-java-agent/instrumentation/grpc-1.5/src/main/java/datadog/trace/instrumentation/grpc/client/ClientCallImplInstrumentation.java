b8e532cbc5b7037fdd556f4beea124f55c7cbf17
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2021-10-15 15:17:29 +0200

    Fix the outstanding context related span suspends/resumes (#3149)

--- dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/client/ClientCallImplInstrumentation.java
+++ dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/client/ClientCallImplInstrumentation.java
@@ -14,7 +14,6 @@ import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.bootstrap.InstrumentationContext;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
-import datadog.trace.context.TraceScope;
 import io.grpc.ClientCall;
 import io.grpc.Metadata;
 import io.grpc.MethodDescriptor;
@@ -70,19 +69,23 @@ public final class ClientCallImplInstrumentation extends Instrumenter.Tracing {
       AgentSpan span = DECORATE.startCall(method);
       if (null != span) {
         InstrumentationContext.get(ClientCall.class, AgentSpan.class).put(call, span);
+        // span is escaping via context - can be picked up by any thread
+        span.startThreadMigration();
       }
     }
   }
 
   public static final class Start {
     @Advice.OnMethodEnter
-    public static TraceScope before(
+    public static AgentScope before(
         @Advice.This ClientCall<?, ?> call,
         @Advice.Argument(1) Metadata headers,
         @Advice.Local("$$ddSpan") AgentSpan span) {
       span = InstrumentationContext.get(ClientCall.class, AgentSpan.class).get(call);
       if (null != span) {
         propagate().inject(span, headers, SETTER);
+        // span has been retrieved from the context - resume
+        span.finishThreadMigration();
         return activateSpan(span);
       }
       return null;
@@ -90,11 +93,13 @@ public final class ClientCallImplInstrumentation extends Instrumenter.Tracing {
 
     @Advice.OnMethodExit(onThrowable = Throwable.class)
     public static void after(
-        @Advice.Enter TraceScope scope,
+        @Advice.Enter AgentScope scope,
         @Advice.Thrown Throwable error,
         @Advice.Local("$$ddSpan") AgentSpan span)
         throws Throwable {
       if (null != scope) {
+        // the span is deactivated
+        scope.span().finishWork();
         scope.close();
       }
       if (null != error && null != span) {
@@ -111,6 +116,8 @@ public final class ClientCallImplInstrumentation extends Instrumenter.Tracing {
     public static void before(@Advice.This ClientCall<?, ?> call) {
       AgentSpan span = InstrumentationContext.get(ClientCall.class, AgentSpan.class).remove(call);
       if (null != span) {
+        // span has been retrieved from the context - resume
+        span.finishThreadMigration();
         span.finish();
       }
     }
@@ -122,6 +129,8 @@ public final class ClientCallImplInstrumentation extends Instrumenter.Tracing {
       // could create a message span here for the request
       AgentSpan span = InstrumentationContext.get(ClientCall.class, AgentSpan.class).get(call);
       if (span != null) {
+        // span has been retrieved from the context - resume
+        span.finishThreadMigration();
         return activateSpan(span);
       }
       return null;
@@ -130,6 +139,8 @@ public final class ClientCallImplInstrumentation extends Instrumenter.Tracing {
     @Advice.OnMethodExit(onThrowable = Throwable.class)
     public static void after(@Advice.Enter AgentScope scope) {
       if (null != scope) {
+        // the span is deactivated
+        scope.span().finishWork();
         scope.close();
       }
     }
@@ -141,6 +152,8 @@ public final class ClientCallImplInstrumentation extends Instrumenter.Tracing {
         @Advice.This ClientCall<?, ?> call, @Advice.Argument(1) Status status) {
       AgentSpan span = InstrumentationContext.get(ClientCall.class, AgentSpan.class).remove(call);
       if (null != span) {
+        // span has been retrieved from the context - resume
+        span.finishThreadMigration();
         DECORATE.onClose(span, status);
         span.finish();
       }
