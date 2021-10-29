1807faffb292642e0c7f223069b2558719a9bfbe
Laplie Anderson <laplie.anderson@datadoghq.com>
2021-10-28 22:17:04 -0400

    Accept "1" as true for boolean properties

---
+++ internal-api/src/test/groovy/datadog/trace/bootstrap/config/provider/ConfigConverterTest.groovy
@@ -0,0 +1,25 @@
+package datadog.trace.bootstrap.config.provider
+
+import datadog.trace.test.util.DDSpecification
+
+class ConfigConverterTest extends DDSpecification {
+
+  def "Convert boolean properties"() {
+    when:
+    def value = ConfigConverter.valueOf(stringValue, Boolean.class)
+
+    then:
+    value == expectedConvertedValue
+
+    where:
+    stringValue | expectedConvertedValue
+    "true"      | true
+    "TRUE"      | true
+    "True"      | true
+    "1"         | true
+    "false"     | false
+    null        | null
+    ""          | null
+    "0"         | false
+  }
+}
