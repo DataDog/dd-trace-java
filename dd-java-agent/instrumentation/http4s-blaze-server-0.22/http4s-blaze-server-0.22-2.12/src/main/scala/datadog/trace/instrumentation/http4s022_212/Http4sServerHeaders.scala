79b12a72ea1192479663b56cf4519dcb77662700
Charlie Gracie <charlie.gracie@datadoghq.com>
2021-10-31 21:06:58 -0400

    Add blaze server 0.22 support

---
+++ dd-java-agent/instrumentation/http4s-blaze-server-0.22/http4s-blaze-server-0.22-2.12/src/main/scala/datadog/trace/instrumentation/http4s022_212/Http4sServerHeaders.scala
@@ -0,0 +1,23 @@
+package datadog.trace.instrumentation.http4s022_212
+
+import datadog.trace.bootstrap.instrumentation.api.AgentPropagation
+import org.http4s.Request
+
+object Http4sServerHeaders {
+  private val Getter = new Http4sServerHeaders()
+
+  def getter[F[_]]: Http4sServerHeaders[F] =
+    Getter.asInstanceOf[Http4sServerHeaders[F]]
+}
+
+class Http4sServerHeaders[F[_]] extends AgentPropagation.ContextVisitor[Request[F]] {
+  override def forEachKey(
+      carrier: Request[F],
+      classifier: AgentPropagation.KeyClassifier
+  ): Unit = {
+    for (header <- carrier.headers) {
+      if (!classifier.accept(header.name.toString.toLowerCase, header.value))
+        return
+    }
+  }
+}
