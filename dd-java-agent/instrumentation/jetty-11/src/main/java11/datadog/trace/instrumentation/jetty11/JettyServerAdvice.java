7cd8aa945fd7c721674e2d64b4e0fcbcb690c90f
Phil Denis <phillip.denis@pason.com>
2021-12-06 00:11:00 -0700

    fixing tests for jetty11

--- dd-java-agent/instrumentation/jetty-11/src/main/java11/datadog/trace/instrumentation/jetty11/JettyServerAdvice.java
+++ dd-java-agent/instrumentation/jetty-11/src/main/java11/datadog/trace/instrumentation/jetty11/JettyServerAdvice.java
@@ -1,12 +1,8 @@
 package datadog.trace.instrumentation.jetty11;
 
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
-import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.propagate;
-import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
 import static datadog.trace.bootstrap.instrumentation.decorator.HttpServerDecorator.DD_SPAN_ATTRIBUTE;
 import static datadog.trace.instrumentation.jetty11.JettyDecorator.DECORATE;
-import static datadog.trace.instrumentation.jetty11.JettyDecorator.SERVLET_REQUEST;
-import static datadog.trace.instrumentation.jetty11.RequestExtractAdapter.GETTER;
 
 import datadog.trace.api.CorrelationIdentifier;
 import datadog.trace.api.GlobalTracer;
@@ -26,12 +22,12 @@ public class JettyServerAdvice {
       Object existingSpan = req.getAttribute(DD_SPAN_ATTRIBUTE);
       if (existingSpan instanceof AgentSpan) {
         // Request already gone through initial processing, so just activate the span.
+        ((AgentSpan) existingSpan).finishThreadMigration();
         return activateSpan((AgentSpan) existingSpan);
       }
 
-      final AgentSpan.Context.Extracted extractedContext = propagate().extract(req, GETTER);
-
-      final AgentSpan span = startSpan(SERVLET_REQUEST, extractedContext).setMeasured(true);
+      final AgentSpan.Context.Extracted extractedContext = DECORATE.extract(req);
+      final AgentSpan span = DECORATE.startSpan(req, extractedContext).setMeasured(true);
       DECORATE.afterStart(span);
       DECORATE.onRequest(span, req, req, extractedContext);
 
@@ -40,11 +36,14 @@ public class JettyServerAdvice {
       req.setAttribute(DD_SPAN_ATTRIBUTE, span);
       req.setAttribute(CorrelationIdentifier.getTraceIdKey(), GlobalTracer.get().getTraceId());
       req.setAttribute(CorrelationIdentifier.getSpanIdKey(), GlobalTracer.get().getSpanId());
+      // request may be processed on any thread; signal thread migration
+      span.startThreadMigration();
       return scope;
     }
 
     @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
     public static void closeScope(@Advice.Enter final AgentScope scope) {
+      scope.span().finishWork();
       scope.close();
     }
   }
@@ -62,6 +61,8 @@ public class JettyServerAdvice {
         final AgentSpan span = (AgentSpan) spanObj;
         DECORATE.onResponse(span, channel);
         DECORATE.beforeFinish(span);
+        // span could have been originated on a different thread and migrated
+        span.finishThreadMigration();
         span.finish();
       }
     }
