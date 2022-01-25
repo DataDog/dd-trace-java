df0bd425b20dbb839a4823c46be27e32f12f3ca7
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2022-01-25 12:40:29 +0000

    Add CACHE to InternalSpanTypes

--- internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/InternalSpanTypes.java
+++ internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/InternalSpanTypes.java
@@ -6,6 +6,7 @@ public class InternalSpanTypes {
   public static final CharSequence HTTP_CLIENT = UTF8BytesString.create(DDSpanTypes.HTTP_CLIENT);
   public static final CharSequence HTTP_SERVER = UTF8BytesString.create(DDSpanTypes.HTTP_SERVER);
   public static final CharSequence RPC = UTF8BytesString.create(DDSpanTypes.RPC);
+  public static final CharSequence CACHE = UTF8BytesString.create(DDSpanTypes.CACHE);
   public static final CharSequence SOAP = UTF8BytesString.create(DDSpanTypes.SOAP);
 
   public static final CharSequence SQL = UTF8BytesString.create(DDSpanTypes.SQL);
