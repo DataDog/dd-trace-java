5ef4cb4a33ab595b895103f965a6eada40e9c4c6
Ludovic Henry <git@ludovic.dev>
2022-02-07 12:13:33 +0100

    Bump async-profiler + Make use of memleakcap setting

--- dd-trace-api/src/main/java/datadog/trace/api/config/ProfilingConfig.java
+++ dd-trace-api/src/main/java/datadog/trace/api/config/ProfilingConfig.java
@@ -77,6 +77,8 @@ public final class ProfilingConfig {
   public static final boolean PROFILING_ASYNC_MEMLEAK_ENABLED_DEFAULT = false;
   public static final String PROFILING_ASYNC_MEMLEAK_INTERVAL = "profiling.async.memleak.interval";
   public static final int PROFILING_ASYNC_MEMLEAK_INTERVAL_DEFAULT = 256 * 1024;
+  public static final String PROFILING_ASYNC_MEMLEAK_CAPACITY = "profiling.async.memleak.capacity";
+  public static final int PROFILING_ASYNC_MEMLEAK_CAPACITY_DEFAULT = 2048;
 
   public static final String PROFILING_LEGACY_TRACING_INTEGRATION =
       "profiling.legacy.tracing.integration";
