f4845177a4f2a3474c16505ed0a2da40f84e27c2
owendavisco <owendavisco@gmail.com>
2022-02-12 17:52:54 -0700

    Use earliest possible redisson version

--- dd-java-agent/instrumentation/redisson-2.0.0/src/test/groovy/RedissonClientTest.groovy
+++ dd-java-agent/instrumentation/redisson-2.0.0/src/test/groovy/RedissonClientTest.groovy
@@ -3,10 +3,9 @@ import datadog.trace.agent.test.utils.PortUtils
 import datadog.trace.api.DDSpanTypes
 import datadog.trace.bootstrap.instrumentation.api.Tags
 import org.redisson.Redisson
-import org.redisson.api.BatchOptions
-import org.redisson.api.RedissonClient
-import org.redisson.config.Config
-import org.redisson.config.SingleServerConfig
+import org.redisson.RedissonClient
+import org.redisson.Config
+import org.redisson.SingleServerConfig
 import redis.embedded.RedisServer
 import spock.lang.Shared
 
@@ -22,16 +21,16 @@ class RedissonClientTest extends AgentTestRunner {
   @Shared
   RedisServer redisServer = RedisServer.builder()
   // bind to localhost to avoid firewall popup
-    .setting("bind 127.0.0.1")
+  .setting("bind 127.0.0.1")
   // set max memory to avoid problems in CI
-    .setting("maxmemory 128M")
-    .port(port).build()
+  .setting("maxmemory 128M")
+  .port(port).build()
 
   @Shared
   Config config = new Config()
 
   @Shared
-  SingleServerConfig singleServerConfig = config.useSingleServer().setAddress("redis://127.0.0.1:${port}")
+  SingleServerConfig singleServerConfig = config.useSingleServer().setAddress("127.0.0.1:${port}")
 
   @Shared
   RedissonClient redissonClient
@@ -64,7 +63,7 @@ class RedissonClientTest extends AgentTestRunner {
   }
 
   def cleanup() {
-    redissonClient.getKeys().flushdb()
+    redissonClient.flushdb()
   }
 
   def "bucket set command"() {
@@ -132,203 +131,6 @@ class RedissonClientTest extends AgentTestRunner {
     }
   }
 
-  def "bucket get and set command"() {
-    when:
-    redissonClient.getBucket("foo").set("bar")
-    def value = redissonClient.getBucket("foo").getAndSet("barTwo")
-
-    then:
-    value == "bar"
-
-    assertTraces(2) {
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "SET"
-          spanType DDSpanTypes.REDIS
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "GETSET"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-    }
-  }
-
-  def "binary stream set command"() {
-    when:
-    redissonClient.getBinaryStream("foo").set("bar".getBytes())
-
-    then:
-    assertTraces(1) {
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "SET"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-    }
-  }
-
-  def "binary stream get command"() {
-    when:
-    redissonClient.getBinaryStream("foo").set("bar".getBytes())
-    def value = redissonClient.getBinaryStream("foo").get()
-
-    then:
-    value == "bar".getBytes()
-
-    assertTraces(2) {
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "SET"
-          spanType DDSpanTypes.REDIS
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "GET"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-    }
-  }
-
-// Not supported by 'embedded-redis'
-//  def "geospatial add command"() {
-//    when:
-//    redissonClient.getGeo("foo").add(
-//      new GeoEntry(40.58921356383488, -105.06320045888452, "bar"))
-//
-//    then:
-//    assertTraces(1) {
-//      trace(1) {
-//        span {
-//          serviceName "redis"
-//          operationName "redis.query"
-//          resourceName "GEOADD"
-//          spanType DDSpanTypes.REDIS
-//          topLevel true
-//          tags {
-//            "$Tags.COMPONENT" "redis-command"
-//            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-//            "$Tags.DB_TYPE" "redis"
-//            defaultTags()
-//          }
-//        }
-//      }
-//    }
-//  }
-
-  def "bitset set command"() {
-    when:
-    redissonClient.getBitSet("foo").set(0, true)
-
-    then:
-    assertTraces(1) {
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "SETBIT"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-    }
-  }
-
-  def "bitset get command"() {
-    when:
-    redissonClient.getBitSet("foo").set(0, true)
-    def value = redissonClient.getBitSet("foo").get(0)
-
-    then:
-    value
-
-    assertTraces(2) {
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "SETBIT"
-          spanType DDSpanTypes.REDIS
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "GETBIT"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-    }
-  }
-
   def "atomic long set command"() {
     when:
     redissonClient.getAtomicLong("foo").set(0)
@@ -380,7 +182,7 @@ class RedissonClientTest extends AgentTestRunner {
         span {
           serviceName "redis"
           operationName "redis.query"
-          resourceName "GET"
+          resourceName "INCRBY"
           spanType DDSpanTypes.REDIS
           topLevel true
           tags {
@@ -458,55 +260,6 @@ class RedissonClientTest extends AgentTestRunner {
       }
     }
   }
-
-//  Issue with SUBSCRIBE resourceName not coming through
-//  def "topic pub sub command"() {
-//    when:
-//    def value = new CompletableFuture<String>()
-//    def topic = redissonClient.getTopic("foo")
-//    topic.addListener(String.class, new MessageListener<String>() {
-//      @Override
-//      void onMessage(CharSequence channel, String msg) {
-//        value.complete(msg)
-//      }
-//    })
-//    topic.publish("bar")
-//
-//    then:
-//    value.get() == "bar"
-//
-//    assertTraces(2) {
-//      trace(1) {
-//        span {
-//          serviceName "redis"
-//          operationName "redis.query"
-//          resourceName "SUBSCRIBE"
-//          spanType DDSpanTypes.REDIS
-//          topLevel true
-//          tags {
-//            "$Tags.COMPONENT" "redis-command"
-//            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-//            "$Tags.DB_TYPE" "redis"
-//            defaultTags()
-//          }
-//        }
-//      }
-//      trace(1) {
-//        span {
-//          serviceName "redis"
-//          operationName "redis.query"
-//          resourceName "PUBLISH"
-//          spanType DDSpanTypes.REDIS
-//          tags {
-//            "$Tags.COMPONENT" "redis-command"
-//            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-//            "$Tags.DB_TYPE" "redis"
-//            defaultTags()
-//          }
-//        }
-//      }
-//    }
-//  }
 
   def "hyperloglog add command"() {
     when:
@@ -664,114 +417,6 @@ class RedissonClientTest extends AgentTestRunner {
     }
   }
 
-  def "set remove random command"() {
-    when:
-    redissonClient.getSet("foo").add("bar")
-    def value = redissonClient.getSet("foo").removeRandom()
-
-    then:
-    value == "bar"
-
-    assertTraces(2) {
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "SADD"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "SPOP"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-    }
-  }
-
-  def "scored sorted add command"() {
-    when:
-    redissonClient.getScoredSortedSet("foo").add(0, "bar")
-
-    then:
-    assertTraces(1) {
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "ZADD"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-    }
-  }
-
-  def "scored sorted get command"() {
-    when:
-    redissonClient.getScoredSortedSet("foo").add(0, "bar")
-    def value = redissonClient.getScoredSortedSet("foo")[0]
-
-    then:
-    value == "bar"
-
-    assertTraces(2) {
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "ZADD"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-      trace(1) {
-        span {
-          serviceName "redis"
-          operationName "redis.query"
-          resourceName "ZSCAN"
-          spanType DDSpanTypes.REDIS
-          topLevel true
-          tags {
-            "$Tags.COMPONENT" "redis-command"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-            "$Tags.DB_TYPE" "redis"
-            defaultTags()
-          }
-        }
-      }
-    }
-  }
-
   def "list add command"() {
     when:
     redissonClient.getList("foo").add("bar")
@@ -804,12 +449,27 @@ class RedissonClientTest extends AgentTestRunner {
     then:
     value == "bar"
 
-    assertTraces(2) {
+    assertTraces(3) {
       trace(1) {
         span {
           serviceName "redis"
           operationName "redis.query"
           resourceName "RPUSH"
+          spanType DDSpanTypes.REDIS
+          topLevel true
+          tags {
+            "$Tags.COMPONENT" "redis-command"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.DB_TYPE" "redis"
+            defaultTags()
+          }
+        }
+      }
+      trace(1) {
+        span {
+          serviceName "redis"
+          operationName "redis.query"
+          resourceName "LLEN"
           spanType DDSpanTypes.REDIS
           topLevel true
           tags {
@@ -838,34 +498,9 @@ class RedissonClientTest extends AgentTestRunner {
     }
   }
 
-// Not supported by 'embedded-redis'
-//  def "stream add command"() {
-//    when:
-//    redissonClient.getStream("foo").add("fooMeOnce", "bar")
-//
-//    then:
-//    assertTraces(1) {
-//      trace(1) {
-//        span {
-//          serviceName "redis"
-//          operationName "redis.query"
-//          resourceName "XADD"
-//          spanType DDSpanTypes.REDIS
-//          topLevel true
-//          tags {
-//            "$Tags.COMPONENT" "redis-command"
-//            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
-//            "$Tags.DB_TYPE" "redis"
-//            defaultTags()
-//          }
-//        }
-//      }
-//    }
-//  }
-
   def "simple pipelining command"() {
     when:
-    def batch = redissonClient.createBatch(BatchOptions.defaults())
+    def batch = redissonClient.createBatch()
     batch.getBucket("foo").setAsync("bar")
     batch.execute()
 
@@ -891,7 +526,7 @@ class RedissonClientTest extends AgentTestRunner {
 
   def "bucket set and get pipelining commands"() {
     when:
-    def batch = redissonClient.createBatch(BatchOptions.defaults())
+    def batch = redissonClient.createBatch()
     batch.getBucket("foo").setAsync("bar")
     def future = batch.getBucket("foo").getAsync()
     batch.execute()
@@ -921,7 +556,7 @@ class RedissonClientTest extends AgentTestRunner {
 
   def "complex pipelining commands"() {
     when:
-    def batch = redissonClient.createBatch(BatchOptions.defaults())
+    def batch = redissonClient.createBatch()
 
     batch.getBucket("bucket").setAsync("barOne")
     batch.getBucket("bucket").setAsync("barTwo")
