e60483617f4970f5ab6c757d5841939b48531856
Eric Ming <eric.ming@datadoghq.com>
2022-02-23 15:17:01 -0500

    Fix indentation

--- dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/ConnectionInstrumentation.java
+++ dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/ConnectionInstrumentation.java
@@ -9,71 +9,71 @@ public class ConnectionInstrumentation extends AbstractConnectionInstrumentation
     implements Instrumenter.ForKnownTypes {
 
   private static final String[] CONCRETE_TYPES = {
-      // redshift
-      "com.amazon.redshift.jdbc.RedshiftConnectionImpl",
-      // jt400
-      "com.ibm.as400.access.AS400JDBCConnection",
-      // possibly need more coverage
-      "com.microsoft.sqlserver.jdbc.SQLServerConnection",
-      // should cover mysql
-      "com.mysql.jdbc.Connection",
-      "com.mysql.jdbc.jdbc1.Connection",
-      "com.mysql.jdbc.jdbc2.Connection",
-      "com.mysql.jdbc.ConnectionImpl",
-      "com.mysql.jdbc.JDBC4Connection",
-      "com.mysql.cj.jdbc.ConnectionImpl",
-      // should cover Oracle
-      "oracle.jdbc.driver.PhysicalConnection",
-      // should cover derby
-      "org.apache.derby.impl.jdbc.EmbedConnection",
-      "org.apache.hive.jdbc.HiveConnection",
-      "org.apache.phoenix.jdbc.PhoenixConnection",
-      "org.apache.pinot.client.PinotConnection",
-      // complete
-      "org.h2.jdbc.JdbcConnection",
-      // GridGain's fork of H2
-      "org.gridgain.internal.h2.jdbc.JdbcConnection",
-      // complete
-      "org.hsqldb.jdbc.JDBCConnection",
-      "org.hsqldb.jdbc.jdbcConnection",
-      // complete
-      "org.mariadb.jdbc.MariaDbConnection",
-      "org.mariadb.jdbc.MySQLConnection",
+    // redshift
+    "com.amazon.redshift.jdbc.RedshiftConnectionImpl",
+    // jt400
+    "com.ibm.as400.access.AS400JDBCConnection",
+    // possibly need more coverage
+    "com.microsoft.sqlserver.jdbc.SQLServerConnection",
+    // should cover mysql
+    "com.mysql.jdbc.Connection",
+    "com.mysql.jdbc.jdbc1.Connection",
+    "com.mysql.jdbc.jdbc2.Connection",
+    "com.mysql.jdbc.ConnectionImpl",
+    "com.mysql.jdbc.JDBC4Connection",
+    "com.mysql.cj.jdbc.ConnectionImpl",
+    // should cover Oracle
+    "oracle.jdbc.driver.PhysicalConnection",
+    // should cover derby
+    "org.apache.derby.impl.jdbc.EmbedConnection",
+    "org.apache.hive.jdbc.HiveConnection",
+    "org.apache.phoenix.jdbc.PhoenixConnection",
+    "org.apache.pinot.client.PinotConnection",
+    // complete
+    "org.h2.jdbc.JdbcConnection",
+    // GridGain's fork of H2
+    "org.gridgain.internal.h2.jdbc.JdbcConnection",
+    // complete
+    "org.hsqldb.jdbc.JDBCConnection",
+    "org.hsqldb.jdbc.jdbcConnection",
+    // complete
+    "org.mariadb.jdbc.MariaDbConnection",
+    "org.mariadb.jdbc.MySQLConnection",
 
-      // postgresql seems to be complete
-      "org.postgresql.jdbc.PgConnection",
-      "org.postgresql.jdbc1.Connection",
-      "org.postgresql.jdbc1.Jdbc1Connection",
-      "org.postgresql.jdbc2.Connection",
-      "org.postgresql.jdbc2.Jdbc2Connection",
-      "org.postgresql.jdbc3.Jdbc3Connection",
-      "org.postgresql.jdbc3g.Jdbc3gConnection",
-      "org.postgresql.jdbc4.Jdbc4Connection",
-      "postgresql.Connection",
-      // EDB version of postgresql
-      "com.edb.jdbc.PgConnection",
-      // sqlite seems to be complete
-      "org.sqlite.Conn",
-      "org.sqlite.jdbc3.JDBC3Connection",
-      "org.sqlite.jdbc4.JDBC4Connection",
-      // covers snowflake
-      "net.snowflake.client.jdbc.SnowflakeConnectionV1",
-      // vertica
-      "com.vertica.jdbc.VerticaConnection",
-      "com.vertica.jdbc.common.SConnection",
-      "com.vertica.jdbc.common.SPooledConnection", // Not sure if should include: implements
-      // javax.sql.PooledConnection
-      "com.vertica.jdbc.kv.RoutableConnection",
-      // apache drill
-      "org.apache.drill.jdbc.DrillConnection",
-      // jtds (for SQL Server and Sybase)
-      "net.sourceforge.jtds.jdbc.JtdsConnection",
-      // SAP HANA in-memory DB
-      "com.sap.db.jdbc.ConnectionSapDB",
-      // for testing purposes
-      "test.TestConnection",
-      // this won't match any class unless the property is set
-      Config.get().getJdbcConnectionClassName()
+    // postgresql seems to be complete
+    "org.postgresql.jdbc.PgConnection",
+    "org.postgresql.jdbc1.Connection",
+    "org.postgresql.jdbc1.Jdbc1Connection",
+    "org.postgresql.jdbc2.Connection",
+    "org.postgresql.jdbc2.Jdbc2Connection",
+    "org.postgresql.jdbc3.Jdbc3Connection",
+    "org.postgresql.jdbc3g.Jdbc3gConnection",
+    "org.postgresql.jdbc4.Jdbc4Connection",
+    "postgresql.Connection",
+    // EDB version of postgresql
+    "com.edb.jdbc.PgConnection",
+    // sqlite seems to be complete
+    "org.sqlite.Conn",
+    "org.sqlite.jdbc3.JDBC3Connection",
+    "org.sqlite.jdbc4.JDBC4Connection",
+    // covers snowflake
+    "net.snowflake.client.jdbc.SnowflakeConnectionV1",
+    // vertica
+    "com.vertica.jdbc.VerticaConnection",
+    "com.vertica.jdbc.common.SConnection",
+    "com.vertica.jdbc.common.SPooledConnection", // Not sure if should include: implements
+    // javax.sql.PooledConnection
+    "com.vertica.jdbc.kv.RoutableConnection",
+    // apache drill
+    "org.apache.drill.jdbc.DrillConnection",
+    // jtds (for SQL Server and Sybase)
+    "net.sourceforge.jtds.jdbc.JtdsConnection",
+    // SAP HANA in-memory DB
+    "com.sap.db.jdbc.ConnectionSapDB",
+    // for testing purposes
+    "test.TestConnection",
+    // this won't match any class unless the property is set
+    Config.get().getJdbcConnectionClassName()
   };
 
   public ConnectionInstrumentation() {
