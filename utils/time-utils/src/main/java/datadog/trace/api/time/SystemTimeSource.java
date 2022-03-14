e2a82985e8b847b3a383199af968aca21e24e058
Laplie Anderson <laplie.anderson@datadoghq.com>
2022-03-14 13:11:38 -0400

    Remove higher precision until performance testing can be done

--- utils/time-utils/src/main/java/datadog/trace/api/time/SystemTimeSource.java
+++ utils/time-utils/src/main/java/datadog/trace/api/time/SystemTimeSource.java
@@ -1,51 +1,31 @@
 package datadog.trace.api.time;
 
-import java.lang.reflect.InvocationTargetException;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+import java.time.Instant;
 
 public class SystemTimeSource implements TimeSource {
   public static final TimeSource INSTANCE = new SystemTimeSource();
 
-  private final TimeSource delegate;
-
-  private SystemTimeSource() {
-    TimeSource target;
-    try {
-      // Platform is in internal-api so we can't use it to check for Java 8+
-      // Reflection to avoid loading the class on Java  7
-      target =
-          (TimeSource)
-              Class.forName("datadog.trace.api.time.Java8TimeSource")
-                  .getDeclaredConstructor()
-                  .newInstance();
-    } catch (InstantiationException
-        | ClassNotFoundException
-        | NoSuchMethodException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoClassDefFoundError e) {
-      target = new Java7TimeSource();
-    }
-
-    delegate = target;
-  }
+  private SystemTimeSource() {}
 
   @Override
   public long getNanoTicks() {
-    return delegate.getNanoTicks();
+    return System.nanoTime();
   }
 
   @Override
   public long getCurrentTimeMillis() {
-    return delegate.getCurrentTimeMillis();
+    return System.currentTimeMillis();
   }
 
   @Override
   public long getCurrentTimeMicros() {
-    return delegate.getCurrentTimeMicros();
+    return MILLISECONDS.toMicros(getCurrentTimeMillis());
   }
 
   @Override
   public long getCurrentTimeNanos() {
-    return delegate.getCurrentTimeNanos();
+    return MILLISECONDS.toNanos(getCurrentTimeMillis());
   }
 }
