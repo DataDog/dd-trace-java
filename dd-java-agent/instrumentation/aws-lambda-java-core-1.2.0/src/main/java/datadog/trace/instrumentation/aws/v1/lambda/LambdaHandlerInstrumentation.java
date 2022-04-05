50df9c7088f28e6495131cd3ecc3651ee0dff3a9
maxday <maxime.david@datadoghq.com>
2022-04-05 17:24:02 -0400

    first draft

--- dd-java-agent/instrumentation/aws-lambda-java-core-1.2.0/src/main/java/datadog/trace/instrumentation/aws/v1/lambda/LambdaHandlerInstrumentation.java
+++ dd-java-agent/instrumentation/aws-lambda-java-core-1.2.0/src/main/java/datadog/trace/instrumentation/aws/v1/lambda/LambdaHandlerInstrumentation.java
@@ -1,24 +1,40 @@
-package datadog.trace.instrumentation.aws.v1.sqs;
+package datadog.trace.instrumentation.aws.v1.lambda;
 
-import static com.amazon.sqs.javamessaging.SQSMessagingClientConstants.STRING;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.isConstructor;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.asm.Advice.AllArguments;
+import static net.bytebuddy.asm.Advice.Enter;
+import static net.bytebuddy.asm.Advice.OnMethodEnter;
+import static net.bytebuddy.asm.Advice.OnMethodExit;
+import static net.bytebuddy.asm.Advice.Origin;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 
-import com.amazon.sqs.javamessaging.message.SQSMessage;
-import com.amazonaws.services.sqs.model.Message;
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
-import datadog.trace.api.Config;
-import java.util.Map;
-import javax.jms.JMSException;
+
 import net.bytebuddy.asm.Advice;
 
 @AutoService(Instrumenter.class)
 public class LambdaHandlerInstrumentation extends Instrumenter.Tracing
     implements Instrumenter.ForSingleType {
+
+  private String instrumentedType;
+  private String methodName;
+
   public LambdaHandlerInstrumentation() {
-    super("aws-sdk");
+    super("aws-lambda");
+    final String handler = System.getenv("_HANDLER");
+    if (null != handler) {
+      final String[] tokens = handler.split("::");
+      if (tokens.length == 1) {
+        this.instrumentedType = handler;
+        this.methodName = "handleRequest";
+      } else if (tokens.length == 2) {
+        this.instrumentedType = tokens[0];
+        this.methodName = tokens[1];
+      }
+      System.out.println("instrumentedType=" + this.instrumentedType);
+      System.out.println("methodName=" + this.methodName);
+    }
   }
 
   @Override
@@ -28,26 +44,57 @@ public class LambdaHandlerInstrumentation extends Instrumenter.Tracing
 
   @Override
   public String instrumentedType() {
-    return "com.amazonaws.services.lambda.runtime.RequestHandler";
+    return this.instrumentedType;
   }
 
   @Override
   public void adviceTransformations(AdviceTransformation transformation) {
-    transformation.applyAdvice(
-        method(ElementMatchers.nameContains("handleRequest")),
-        getClass().getName() + "$ExtensionCommunicationAdvice");
+    if (null != this.instrumentedType && null != this.methodName) {
+      transformation.applyAdvice(
+          isMethod().and(named(this.methodName)),
+          getClass().getName() + "$ExtensionCommunicationAdvice");
+    }
   }
 
   public static class ExtensionCommunicationAdvice {
+
+//    static void post(String url) {
+//      URL urlToOpen = new URL(url);
+//      URLConnection con = urlToOpen.openConnection();
+//      con.setDoOutput(true);
+//      HttpURLConnection http = (HttpURLConnection) con;
+//      http.setRequestMethod("POST");
+//
+//      byte[] out =
+//          "{\"username\":\"root\",\"password\":\"password\"}".getBytes(StandardCharsets.UTF_8);
+//      int length = out.length;
+//      http.setFixedLengthStreamingMode(length);
+//      http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+//      http.connect();
+//      try (OutputStream os = http.getOutputStream()) {
+//        os.write(out);
+//      }
+//    }
+
     @OnMethodEnter
     static long enter() {
       System.out.println("[maxday-poc-java-no-code] - Enter the function");
+//      try {
+//        post("\"http://127.0.0.1:8124/lambda/start-invocation");
+//      } catch (Exception e){
+//        System.out.println("ooopsy = " + e);
+//      }
       return System.currentTimeMillis();
     }
 
     @OnMethodExit
     static void exit(@Origin String method, @Enter long start, @AllArguments Object[] args) {
-      System.out.println("[maxday-poc-java-no-code] - Exit the function");
+      System.out.println("[maxday-poc-java-no-code] - Exit the function" + method);
+//      try {
+//        post("\"http://127.0.0.1:8124/lambda/end-invocation");
+//      } catch (Exception e){
+//        System.out.println("ooopsy = " + e);
+//      }
       System.out.println("[maxday-poc-java-no-code] - Took " + (System.currentTimeMillis() - start) + " milliseconds ");
     }
   }
