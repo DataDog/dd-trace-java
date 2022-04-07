33e4dd5bee1520f9d398e55bf4d586145b9769e9
Gustavo Lopes <mail@geleia.net>
2022-04-07 17:45:08 +0100

    Fix vertx path instr on newer versions (3.7+)

--- dd-java-agent/instrumentation/vertx-web-3.4/src/main/java8/datadog/trace/instrumentation/vertx_3_4/server/PathParameterPublishingHelper.java
+++ dd-java-agent/instrumentation/vertx-web-3.4/src/main/java8/datadog/trace/instrumentation/vertx_3_4/server/PathParameterPublishingHelper.java
@@ -9,14 +9,10 @@ import datadog.trace.api.gateway.Flow;
 import datadog.trace.api.gateway.RequestContext;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
-import io.vertx.ext.web.impl.Utils;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
 
 public class PathParameterPublishingHelper {
-  public static void publishParams(Matcher m, List<String> groups) {
+  public static void publishParams(Map<String, String> params) {
     AgentSpan agentSpan = activeSpan();
     if (agentSpan == null) {
       return;
@@ -28,25 +24,6 @@ public class PathParameterPublishingHelper {
     RequestContext<Object> requestContext = agentSpan.getRequestContext();
     if (requestContext == null || callback == null) {
       return;
-    }
-
-    final int groupCount = m.groupCount();
-    final Map<String, String> params = new HashMap<>(groupCount + (groupCount + 2) / 3);
-    if (groups != null) {
-      for (int i = 0; i < groups.size(); i++) {
-        final String k = groups.get(i);
-        final String value = Utils.urlDecode(m.group("p" + i), false);
-        params.put(k, value);
-      }
-    } else {
-      for (int i = 0; i < groupCount; i++) {
-        String group = m.group(i + 1);
-        if (group != null) {
-          final String k = "param" + i;
-          final String value = Utils.urlDecode(group, false);
-          params.put(k, value);
-        }
-      }
     }
 
     callback.apply(requestContext, params);
