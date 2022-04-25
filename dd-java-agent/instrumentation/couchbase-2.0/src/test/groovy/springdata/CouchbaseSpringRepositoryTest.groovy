ab8b0002d0382a77f4a78be2198f36b90febfa2f
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2022-04-25 10:10:08 +0200

    Add Java 17 to CI

--- dd-java-agent/instrumentation/couchbase-2.0/src/test/groovy/springdata/CouchbaseSpringRepositoryTest.groovy
+++ dd-java-agent/instrumentation/couchbase-2.0/src/test/groovy/springdata/CouchbaseSpringRepositoryTest.groovy
@@ -10,6 +10,7 @@ import com.couchbase.client.java.view.DesignDocument
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.data.repository.CrudRepository
+import spock.lang.IgnoreIf
 import spock.lang.Shared
 import spock.lang.Unroll
 import util.AbstractCouchbaseTest
@@ -18,6 +19,10 @@ import static datadog.trace.agent.test.utils.TraceUtils.basicSpan
 import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeSpan
 
+@IgnoreIf({
+  // TODO Java 17: This version of spring-data doesn't support Java 17
+  new BigDecimal(System.getProperty("java.specification.version")).isAtLeast(17.0)
+})
 @Unroll
 class CouchbaseSpringRepositoryTest extends AbstractCouchbaseTest {
   static final Closure<Doc> FIND
