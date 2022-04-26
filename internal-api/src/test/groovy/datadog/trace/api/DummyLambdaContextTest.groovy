4648fd822ac614b6cb2993243f7f23f4fec28eb0
maxday <maxime.david@datadoghq.com>
2022-04-25 21:43:27 -0400

    add tests

---
+++ internal-api/src/test/groovy/datadog/trace/api/DummyLambdaContextTest.groovy
@@ -0,0 +1,15 @@
+package datadog.trace.api
+
+import datadog.trace.bootstrap.instrumentation.api.DummyLambdaContext
+import spock.lang.Specification
+
+class DummyLambdaContextTest extends Specification {
+
+  def "test constructor and getters"(){
+    when:
+    def dummyLambdaContext = new DummyLambdaContext("1234", "5678")
+    then:
+    dummyLambdaContext.getTraceId().toString() == "1234"
+    dummyLambdaContext.getSpanId().toString() == "5678"
+  }
+}
