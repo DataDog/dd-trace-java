be87497b5dca8986b5e2cb14776b070fffa18cae
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2022-05-03 17:34:36 +0200

    WriterFactoryTest

---
+++ dd-trace-core/src/test/groovy/datadog/trace/common/writer/WriterFactoryTest.groovy
@@ -0,0 +1,30 @@
+package datadog.trace.common.writer
+
+import datadog.communication.ddagent.SharedCommunicationObjects
+import datadog.trace.api.Config
+import datadog.trace.api.StatsDClient
+import datadog.trace.common.sampling.Sampler
+import datadog.trace.test.util.DDSpecification
+
+class WriterFactoryTest extends DDSpecification {
+
+  def "test"() {
+    setup:
+    def config = Mock(Config)
+    def sharedComm = Mock(SharedCommunicationObjects)
+    def sampler = Mock(Sampler)
+    def statsd = Mock(StatsDClient)
+
+    when:
+    def writer = WriterFactory.createWriter(config, sharedComm, sampler, statsd, configuredType)
+
+    then:
+    writer.class == expectedClass
+
+    where:
+    configuredType | expectedClass
+    "LoggingWriter"| LoggingWriter
+    "PrintingWriter" | PrintingWriter
+    "TraceStructureWriter" | TraceStructureWriter
+  }
+}
