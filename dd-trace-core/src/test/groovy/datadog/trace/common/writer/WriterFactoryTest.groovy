45fd7cc304629670deaffaf7c72b04e66b4d8125
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2022-05-05 11:22:38 +0200

    WriterFactoryTest

--- dd-trace-core/src/test/groovy/datadog/trace/common/writer/WriterFactoryTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/common/writer/WriterFactoryTest.groovy
@@ -4,14 +4,24 @@ import datadog.communication.ddagent.SharedCommunicationObjects
 import datadog.trace.api.Config
 import datadog.trace.api.StatsDClient
 import datadog.trace.common.sampling.Sampler
+import datadog.trace.common.writer.ddagent.Prioritization
 import datadog.trace.test.util.DDSpecification
+
+import static datadog.trace.api.config.TracerConfig.PRIORITIZATION_TYPE
 
 class WriterFactoryTest extends DDSpecification {
 
-  def "test direct writer creation"() {
+  def "test writer creation for #configuredType"() {
     setup:
     def config = Mock(Config)
-    def sharedComm = Mock(SharedCommunicationObjects)
+    config.apiKey >> "my-api-key"
+    config.agentUrl >> "http://my-agent.url"
+    config.getEnumValue(PRIORITIZATION_TYPE, _, _) >> Prioritization.FAST_LANE
+    config.tracerMetricsEnabled >> true
+
+    def sharedComm = new SharedCommunicationObjects()
+    sharedComm.createRemaining(config)
+
     def sampler = Mock(Sampler)
     def statsd = Mock(StatsDClient)
 
@@ -27,5 +37,8 @@ class WriterFactoryTest extends DDSpecification {
     "PrintingWriter" | PrintingWriter
     "TraceStructureWriter" | TraceStructureWriter
     "MultiWriter:LoggingWriter,PrintingWriter" | MultiWriter
+    "DDIntakeWriter" | DDIntakeWriter
+    "DDAgentWriter" | DDAgentWriter
+    "not-found" | DDAgentWriter
   }
 }
