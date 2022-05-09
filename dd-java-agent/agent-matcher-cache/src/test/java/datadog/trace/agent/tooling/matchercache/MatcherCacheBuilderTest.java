a354793c05a410562970a4c0a0ff6ba58b842aa6
Yury Gribkov <yury.gribkov@gmail.com>
2022-05-09 11:40:02 -0700

    Fix test coverage

--- dd-java-agent/agent-matcher-cache/src/test/java/datadog/trace/agent/tooling/matchercache/MatcherCacheBuilderTest.java
+++ dd-java-agent/agent-matcher-cache/src/test/java/datadog/trace/agent/tooling/matchercache/MatcherCacheBuilderTest.java
@@ -77,6 +77,8 @@ public class MatcherCacheBuilderTest {
 
     assertEquals("Ignore: 2; Skip: 2; Transform: 3; Fail: 1", stats.toString());
 
+    matcherCacheBuilder.optimize();
+
     // serialize MatcherCache and load as MatcherCache and check the result
     MatcherCache matcherCache = serializeAndLoadCacheData(matcherCacheBuilder);
 
