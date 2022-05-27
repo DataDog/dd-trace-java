efd59492253786e7f864b5bd622b23b745d99046
Gustavo Lopes <mail@geleia.net>
2022-05-27 15:13:36 +0100

    Condition test on uname executable existing

--- telemetry/src/test/groovy/datadog/telemetry/UnameTest.groovy
+++ telemetry/src/test/groovy/datadog/telemetry/UnameTest.groovy
@@ -1,9 +1,12 @@
 package datadog.telemetry
 
+import org.junit.Assume
 import spock.lang.Specification
 
 class UnameTest extends Specification {
   void 'can obtain the data via libc'() {
+    Assume.assumeTrue('uname -a'.execute().waitFor() == 0)
+
     expect:
     Uname.UTS_NAME != null
     Uname.UTS_NAME.machine() == 'uname -m'.execute().text.trim()
