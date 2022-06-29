63d6c64cc1ec0a70cca1167f701ff220241e4bc1
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2022-06-29 08:24:14 -0400

    don't use local registry

--- lib-injection/build.sh
+++ lib-injection/build.sh
@@ -4,6 +4,8 @@
 SCRIPT_PATH=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+IMAGE_NAME=${1}
+IMAGE_TAG=${2}
 
 if [ -z ${CI} ] ; then
   echo "Running manually"
@@ -14,10 +16,59 @@ else
   mv ${SCRIPT_DIR}/*.jar ${SCRIPT_DIR}/dd-java-agent.jar
 fi
 
+cat <<EOF > ${SCRIPT_DIR}/app-config.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    app: my-app
+  name: my-app
+spec:
+  initContainers:
+    - image: ${IMAGE_NAME}:${IMAGE_TAG}
+      command: ["sh", "copy-javaagent.sh", "/datadog"]
+      name: copy-sdk
+      volumeMounts:
+        - name: apm-sdk-volume
+          mountPath: /datadog
+  containers:
+    - image: docker.io/bdevinssureshatddog/k8s-lib-injection-app:latest
+      env:
+        - name: SERVER_PORT
+          value: "18080"
+        - name: JAVA_TOOL_OPTIONS
+          value: "-javaagent:/datadog/dd-java-agent.jar"
+        - name: DD_AGENT_HOST
+          valueFrom:
+            fieldRef:
+              fieldPath: status.hostIP
+        - name: DD_AGENT_PORT
+          value: "18126"
+      name: my-app
+      readinessProbe:
+        initialDelaySeconds: 1
+        periodSeconds: 2
+        timeoutSeconds: 1
+        successThreshold: 1
+        failureThreshold: 1
+        httpGet:
+          host:
+          scheme: HTTP
+          path: /
+          port: 18080
+        initialDelaySeconds: 5
+        periodSeconds: 5
+      ports:
+        - containerPort: 18080
+          hostPort: 18080
+          protocol: TCP
+      volumeMounts:
+        - name: apm-sdk-volume
+          mountPath: /datadog
+  volumes:
+    - name: apm-sdk-volume
+      emptyDir: {}
+EOF
+
 cd ${SCRIPT_DIR}
-IMAGE_NAME=${1}
-IMAGE_TAG=${2}
-docker build . -t "${IMAGE_NAME}"
-docker tag "${IMAGE_NAME}" "${IMAGE_NAME}:${IMAGE_TAG}"
-docker push "${IMAGE_NAME}"
-docker push "${IMAGE_NAME}:${IMAGE_TAG}"
+docker buildx build --platform ${BUILDX_PLATFORMS} -t "${IMAGE_NAME}:${IMAGE_TAG}" --push .
