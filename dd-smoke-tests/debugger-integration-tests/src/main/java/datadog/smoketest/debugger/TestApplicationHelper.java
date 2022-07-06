b3106e73a8b7b2369d387c1e5af04a4bfa64848d
Gustavo Lopes <mail@geleia.net>
2022-07-06 09:46:21 +0100

    Moved configuration poller tasks to their own thread

--- dd-smoke-tests/debugger-integration-tests/src/main/java/datadog/smoketest/debugger/TestApplicationHelper.java
+++ dd-smoke-tests/debugger-integration-tests/src/main/java/datadog/smoketest/debugger/TestApplicationHelper.java
@@ -16,9 +16,9 @@ public class TestApplicationHelper {
   private static final String INSTRUMENTATION_DONE_MAIN_THREAD =
       "[main] DEBUG com.datadog.debugger.agent.DebuggerTransformer - Generating bytecode for class: %s";
   private static final String INSTRUMENTATION_DONE_BCKG_THREAD =
-      "[dd-task-scheduler] DEBUG com.datadog.debugger.agent.DebuggerTransformer - Generating bytecode for class: %s";
+      "[dd-remote-config] DEBUG com.datadog.debugger.agent.DebuggerTransformer - Generating bytecode for class: %s";
   private static final String RENTRANSFORMATION_CLASS =
-      "[dd-task-scheduler] INFO com.datadog.debugger.agent.ConfigurationUpdater - re-transforming %s";
+      "[dd-remote-config] INFO com.datadog.debugger.agent.ConfigurationUpdater - re-transforming %s";
   private static final String RETRANSFORMATION_DONE =
       "com.datadog.debugger.agent.ConfigurationUpdater - Re-transformation done.";
   private static final long SLEEP_MS = 100;
