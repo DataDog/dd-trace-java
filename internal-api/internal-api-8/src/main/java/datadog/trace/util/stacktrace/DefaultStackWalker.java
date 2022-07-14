c2f283409286aa6361fcbd992278f64466696e10
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2022-07-14 09:42:26 +0200

    Replace isNotDDTraceClass method for a predicate

--- internal-api/internal-api-8/src/main/java/datadog/trace/util/stacktrace/DefaultStackWalker.java
+++ internal-api/internal-api-8/src/main/java/datadog/trace/util/stacktrace/DefaultStackWalker.java
@@ -1,18 +1,17 @@
 package datadog.trace.util.stacktrace;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class DefaultStackWalker implements StackWalker {
 
   DefaultStackWalker() {}
 
-  protected static Set<String> filteredPackages =
-      Stream.of("datadog.trace.", "com.datadog.appsec.")
-          .collect(Collectors.toCollection(HashSet::new));
+  protected static Predicate<StackTraceElement> isNotDDTraceClass =
+      (stackTraceElement) ->
+          !stackTraceElement.getClassName().startsWith("datadog.trace.")
+              && !stackTraceElement.getClassName().startsWith("com.datadog.appsec.");
 
   @Override
   public boolean isEnabled() {
@@ -21,11 +20,7 @@ public class DefaultStackWalker implements StackWalker {
 
   @Override
   public Stream<StackTraceElement> walk() {
-    return doGetStack().filter(e -> isNotDDTraceClass(e.getClassName()));
-  }
-
-  static boolean isNotDDTraceClass(final String className) {
-    return filteredPackages.stream().noneMatch(className::startsWith);
+    return doGetStack().filter(isNotDDTraceClass);
   }
 
   /**
