cd05347c5ddd7f78d316b3f257d76288ea2740a6
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2022-07-14 14:07:21 +0200

    Exclude datadog.trace.util.stacktrace.StackWalkerFactory from coverage test

--- internal-api/internal-api-8/src/main/java/datadog/trace/util/stacktrace/StackWalkerFactory.java
+++ internal-api/internal-api-8/src/main/java/datadog/trace/util/stacktrace/StackWalkerFactory.java
@@ -2,7 +2,7 @@ package datadog.trace.util.stacktrace;
 
 public class StackWalkerFactory {
 
-  public static StackWalker INSTANCE = StackWalkerFactory.getInstance();
+  public static StackWalker INSTANCE = getInstance();
 
   private static StackWalker getInstance() {
     // New StackWalker implementations must be added in the future
