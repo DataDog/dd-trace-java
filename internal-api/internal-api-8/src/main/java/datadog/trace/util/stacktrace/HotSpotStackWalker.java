68703105bb9cc7045c8c01af4910c0a7efa80008
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2022-07-15 13:53:49 +0200

    first HotSpotStackWalker implementation

---
+++ internal-api/internal-api-8/src/main/java/datadog/trace/util/stacktrace/HotSpotStackWalker.java
@@ -0,0 +1,63 @@
+package datadog.trace.util.stacktrace;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class HotSpotStackWalker extends AbstractStackWalker {
+
+  private final sun.misc.JavaLangAccess access = sun.misc.SharedSecrets.getJavaLangAccess();
+
+  @Override
+  public boolean isEnabled() {
+    try {
+      String str = System.getProperty("java.version");
+      if (str.length() > 3
+          && str.startsWith("1.")
+          && Integer.parseInt(Character.toString(str.charAt(2))) >= 7) {
+        if (access != null) {
+          access.getStackTraceElement(new Throwable(), 0);
+          return true;
+        }
+      }
+    } catch (Throwable localThrowable) {
+    }
+    return false;
+  }
+
+  @Override
+  Stream<StackTraceElement> doGetStack() {
+
+    Throwable throwable = new Throwable();
+
+    Iterable<StackTraceElement> iterable =
+        () ->
+            new Iterator<StackTraceElement>() {
+
+              int index = 0;
+
+              final int size = access.getStackTraceDepth(throwable);
+
+              @Override
+              public boolean hasNext() {
+                return index < size;
+              }
+
+              @Override
+              public StackTraceElement next() {
+                if (index >= size) {
+                  throw new NoSuchElementException();
+                }
+                return access.getStackTraceElement(throwable, index++);
+              }
+
+              @Override
+              public void remove() {
+                throw new UnsupportedOperationException();
+              }
+            };
+
+    return StreamSupport.stream(iterable.spliterator(), false);
+  }
+}
--- internal-api/internal-api-8/src/main/java/datadog/trace/util/stacktrace/StackWalkerFactory.java
+++ internal-api/internal-api-8/src/main/java/datadog/trace/util/stacktrace/StackWalkerFactory.java
@@ -1,11 +1,18 @@
 package datadog.trace.util.stacktrace;
 
+import java.util.stream.Stream;
+
 public class StackWalkerFactory {
 
   public static StackWalker INSTANCE = getInstance();
 
   private static StackWalker getInstance() {
-    // New StackWalker implementations must be added in the future
-    return new DefaultStackWalker();
+
+    // New StackTraceGenerator must be added to the list
+    Stream<StackWalker> implementations = Stream.of(new HotSpotStackWalker());
+    return implementations
+        .filter(StackWalker::isEnabled)
+        .findFirst()
+        .orElse(new DefaultStackWalker());
   }
 }
