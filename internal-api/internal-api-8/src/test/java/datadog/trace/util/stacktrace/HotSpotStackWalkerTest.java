b430ca3b96141c8bc95e0e2a21c0bbcef76a4de5
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2022-07-19 11:57:02 +0200

    fix HotSpotStackWalkerTest

--- internal-api/internal-api-8/src/test/java/datadog/trace/util/stacktrace/HotSpotStackWalkerTest.java
+++ internal-api/internal-api-8/src/test/java/datadog/trace/util/stacktrace/HotSpotStackWalkerTest.java
@@ -1,25 +1,41 @@
 package datadog.trace.util.stacktrace;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import datadog.trace.api.Platform;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import sun.misc.JavaLangAccess;
+import sun.misc.SharedSecrets;
 
 public class HotSpotStackWalkerTest {
+
+  private final boolean isJDK8WithHotSpot = isRunningJDK8WithHotSpot();
 
   private final HotSpotStackWalker hotSpotStackWalker = new HotSpotStackWalker();
 
   @Test
   public void hotSpotStackWalker_must_be_enabled_for_JDK8_with_hotspot() {
-    assertTrue(hotSpotStackWalker.isEnabled());
+    assertEquals(isJDK8WithHotSpot, hotSpotStackWalker.isEnabled());
   }
 
   @Test
   public void get_stack_trace() {
+    assumeTrue(isJDK8WithHotSpot);
     // When
     Stream<StackTraceElement> stream = hotSpotStackWalker.doGetStack();
     // Then
     assertNotEquals(stream.count(), 0);
+  }
+
+  private boolean isRunningJDK8WithHotSpot() {
+    try {
+      JavaLangAccess access = SharedSecrets.getJavaLangAccess();
+      return Platform.isJavaVersion(8) && access != null;
+    } catch (Throwable throwable) {
+      return false;
+    }
   }
 }
