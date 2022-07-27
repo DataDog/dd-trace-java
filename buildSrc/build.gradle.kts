917b55173c77a430a7bd8159dc2481f4c96e3b5e
Yury Gribkov <yury.gribkov@gmail.com>
2022-07-27 12:07:04 -0700

    Revert "Revert "Upgrade gradle to 7.5 (#3655)""
    This reverts commit 5d3677cb28adb32029c32555f5c653c07f4fabab.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
   implementation("org.ow2.asm", "asm-tree", "9.0")
 
   testImplementation("org.spockframework", "spock-core", "2.0-groovy-3.0")
-  testImplementation("org.codehaus.groovy", "groovy-all", "3.0.9")
+  testImplementation("org.codehaus.groovy", "groovy-all", "3.0.10")
 }
 
 tasks.test {
