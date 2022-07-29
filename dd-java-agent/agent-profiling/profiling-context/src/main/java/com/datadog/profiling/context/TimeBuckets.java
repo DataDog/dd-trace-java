8abd0b6d9f8b2f81bda42d95160c4213e1be3b1d
jbachorik <jaroslav.bachorik@datadoghq.com>
2022-07-29 19:54:41 +0200

    WIP 3

--- dd-java-agent/agent-profiling/profiling-context/src/main/java/com/datadog/profiling/context/TimeBuckets.java
+++ dd-java-agent/agent-profiling/profiling-context/src/main/java/com/datadog/profiling/context/TimeBuckets.java
@@ -5,6 +5,8 @@ import datadog.trace.util.AgentTaskScheduler;
 import org.jctools.queues.MpscArrayQueue;
 
 import java.io.Closeable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -37,7 +39,7 @@ final class TimeBuckets implements Closeable {
 
     void touch(long ts) {
       long target = ts + expiration;
-      EXPIRATION_TS_UPDATER.updateAndGet(this, prev -> Math.max(prev, target));
+      EXPIRATION_TS_UPDATER.updateAndGet(this, prev -> prev > -1 ? Math.max(prev, target) : -1);
     }
 
     void expire() {
@@ -47,8 +49,19 @@ final class TimeBuckets implements Closeable {
       try {
         onExpired.accept(this);
       } catch (Throwable ignored) {}
+      EXPIRATION_TS_UPDATER.updateAndGet(this, prev -> -1);
+    }
+
+    boolean isExpired() {
+      return expirationTs == -1;
+    }
+
+    boolean isExpiring(long ts) {
+      long targetTs = expirationTs;
+      return targetTs > -1 && targetTs <= ts;
     }
   }
+
   static final class Bucket {
     interface Callback {
       Callback EMPTY = new Callback() {
@@ -69,10 +82,6 @@ final class TimeBuckets implements Closeable {
 
     private boolean fillupTriggered = false;
 
-    Bucket(int capacity) {
-      this(capacity, Callback.EMPTY);
-    }
-
     Bucket(int capacity, Callback fillupCallback) {
       data = new Expiring[capacity];
       this.fillupCallback = fillupCallback;
@@ -80,6 +89,9 @@ final class TimeBuckets implements Closeable {
 
     Expiring add(long ts, long expiration, Consumer<Expiring> onExpired) {
       if (size == data.length) {
+        return Expiring.NOOP;
+      }
+      if (size == data.length - 1) {
         if (!fillupTriggered) {
           try {
             fillupCallback.onBucketFull();
@@ -88,7 +100,6 @@ final class TimeBuckets implements Closeable {
             fillupTriggered = true;
           }
         }
-        return Expiring.NOOP;
       }
       int pos = size;
       Expiring e = new Expiring(this, pos, ts, expiration, onExpired);
@@ -112,22 +123,32 @@ final class TimeBuckets implements Closeable {
       }
     }
 
-    void clean(long ts) {
+    boolean clean(long ts) {
+      int reinsertionIdx = 0;
       for (int i =0; i < data.length; i++) {
         if (data[i] == null) {
-          continue;
+          // the data is auto-compacted so first 'null' element means all the followup ones are null also
+          break;
         }
         Expiring e = data[i];
-        if (data[i].expirationTs <= ts) {
+        if (e.isExpiring(ts)) {
           try {
             e.onExpired.accept(e);
           } catch (Throwable ignored) {
+          } finally {
+            data[i] = null;
           }
+        } else {
+          if (reinsertionIdx != i) {
+            data[reinsertionIdx] = data[i];
+            data[i] = null;
+          }
+          // if reinsertionIdx == i the data stay in its place so data[i] is not be nulled
+          reinsertionIdx++;
         }
-        data[i] = null;
       }
-      size = 0;
-      if (fillupTriggered) {
+      size = reinsertionIdx;
+      if (fillupTriggered && size < data.length) {
         try {
           fillupCallback.onBucketAvailable();
         } catch (Throwable ignored) {
@@ -135,6 +156,9 @@ final class TimeBuckets implements Closeable {
           fillupTriggered = false;
         }
       }
+      // set the tainted status if there were any reinsertions
+      int tainted = reinsertionIdx;
+      return tainted > 0;
     }
   }
 
@@ -167,14 +191,15 @@ final class TimeBuckets implements Closeable {
     private final Bucket[] buckets;
 
     private final int numStripes;
-    private final int bucketCapacity;
 
     private final BucketAvailabilityTracker availabilityTracker;
+
+    private long lastCleanupTs = -1;
 
     StripedBucket(int parallelism, int capacity) {
       this.numStripes = parallelism * 2;
       buckets = new Bucket[numStripes];
-      bucketCapacity = (int)Math.ceil((double)capacity / numStripes);
+      int bucketCapacity = (int) Math.ceil((double) capacity / numStripes);
       availabilityTracker = new BucketAvailabilityTracker(numStripes);
       for (int i = 0; i < numStripes; i++) {
         buckets[i] = new Bucket(bucketCapacity, availabilityTracker);
@@ -206,15 +231,30 @@ final class TimeBuckets implements Closeable {
       return Expiring.NOOP;
     }
 
-    void clean(long ts) {
+    /**
+     * Cleans up the bucket by expiring all eligible elements.<br>
+     * This method should always be called from the same thread and from only thread at a time -
+     * otherwise the performancy may suffer because the same bucket could be cleaned up several times
+     * @param ts the operation timestamp
+     * @return {@literal true} if the bucket was completely cleaned
+     */
+    boolean clean(long ts) {
+      if (lastCleanupTs ==  ts) {
+        return true; // already processed in this epoch
+      }
+      boolean tainted = false;
       for (Bucket bucket : buckets) {
         synchronized (bucket) {
-          bucket.clean(ts);
+          tainted = bucket.clean(ts) || tainted;
         }
       }
+      lastCleanupTs = ts;
+      return !tainted;
     }
   }
 
+  private final int capacity;
+  private final int tickCapacity;
   private final StripedBucket[] buckets;
   private final int numBuckets;
   private final TimeUnit timeUnit;
@@ -223,6 +263,7 @@ final class TimeBuckets implements Closeable {
 
   private static final AtomicLongFieldUpdater<TimeBuckets> LAST_ACCESS_TS_UPDATER = AtomicLongFieldUpdater.newUpdater(TimeBuckets.class, "lastAccessTs");
   private volatile long lastAccessTs = -1;
+  private volatile boolean closed = false;
 
   private final MpscArrayQueue<StripedBucket> cleanupQueue;
   private final AgentTaskScheduler.Scheduled<TimeBuckets> scheduledTask;
@@ -242,60 +283,116 @@ final class TimeBuckets implements Closeable {
 
     numBuckets = (int)(this.expiration / this.granularity) + 1;
     buckets = new StripedBucket[numBuckets];
+    tickCapacity = (int)Math.ceil(capacity / (double)numBuckets);
+
+    this.capacity = numBuckets * tickCapacity;
     for (int i = 0; i < numBuckets; i++) {
-      buckets[i] = new StripedBucket(parallelism, (int)Math.ceil(capacity / (double)numBuckets));
+      buckets[i] = new StripedBucket(parallelism, tickCapacity);
     }
+    cleanupQueue = new MpscArrayQueue<>(numBuckets * 2);
     if (scheduleCleanup) {
-      cleanupQueue = new MpscArrayQueue<>(numBuckets);
       int schedulePeriod = Math.max((int)(granularity / 2), 1);
       scheduledTask = AgentTaskScheduler.INSTANCE.scheduleAtFixedRate(TimeBuckets::processCleanup, this, schedulePeriod, schedulePeriod, timeUnit);
     } else {
-      cleanupQueue = null;
       scheduledTask = null;
     }
   }
 
+  final List<StripedBucket> survivors = new ArrayList<>();
+  private long lastCleanupTs = -1;
+
   private void processCleanup() {
-    long ts = TimeUnit.NANOSECONDS.convert(System.nanoTime(), timeUnit);
-    // first process the expired queue
-    cleanupQueue.drain(b -> b.clean(ts));
-    // also, expire the bucket related to the current timestamp
-    int toWriteIdx = getBucketIndex(ts);
-    buckets[toWriteIdx < numBuckets - 1 ? toWriteIdx + 1 : 0].clean(ts);
+    processCleanup(timeUnit.convert(System.nanoTime(), TimeUnit.NANOSECONDS));
+  }
+
+  void processCleanup(long ts) {
+    try {
+      if (ts > lastCleanupTs) {
+        // first process the expired queue
+        int processed = cleanupQueue.drain(b -> {
+          if (!b.clean(ts)) {
+            survivors.add(b);
+          }
+        });
+        // re-add the surviving buckets
+        cleanupQueue.addAll(survivors);
+
+        if (processed == 0) {
+          // if not scheduled externally expire the bucket related to the current timestamp
+          int toCleanIdx = getBucketIndex(ts);
+          StripedBucket toClean = buckets[toCleanIdx];
+          if (!toClean.clean(ts)) {
+            // re-add the surviving bucket
+            cleanupQueue.add(toClean);
+          }
+        }
+      }
+    } finally {
+      lastCleanupTs = ts;
+      survivors.clear();
+    }
   }
 
   Expiring add(long ts, Runnable onExpired) {
+    if (closed) {
+      return Expiring.NOOP;
+    }
     long previousTs = LAST_ACCESS_TS_UPDATER.getAndSet(this, ts);
     int toWriteIdx = getBucketIndex(ts);
     if (previousTs != ts) {
-      expireBucket(toWriteIdx);
+      if (!expireBucketAt(ts, toWriteIdx)) {
+        return Expiring.NOOP;
+      }
     }
     return buckets[toWriteIdx].add(ts, expiration, onExpired);
   }
 
   void expireBucket(long ts) {
-    int toWriteIdx = getBucketIndex(ts);
-    StripedBucket toClean = buckets[toWriteIdx < numBuckets - 1 ? toWriteIdx + 1 : 0];
-    toClean.clean(ts);
+    expireBucketAt(ts, getBucketIndex(ts));
   }
 
-  private void expireBucket(int toWriteIdx) {
+  private boolean expireBucketAt(long ts, int toWriteIdx) {
     StripedBucket toClean = buckets[toWriteIdx < numBuckets - 1 ? toWriteIdx + 1 : 0];
     if (cleanupQueue != null) {
-      while (!cleanupQueue.relaxedOffer(toClean)) {
-        Thread.yield();
+      if (!cleanupQueue.relaxedOffer(toClean)) {
+        int cnt = 0;
+        long start = System.nanoTime();
+        while (!cleanupQueue.relaxedOffer(toClean)) {
+          Thread.yield();
+          if ((cnt = (cnt + 1) % 1000) == 0) {
+            if (closed) {
+              return false;
+            }
+            if (System.nanoTime() - start > 500_000_000L) {
+              // something went terribly wrong and the cleanup thread is most probably dead
+              close();
+              return false;
+            }
+          }
+        }
       }
     }
+    return true;
   }
 
   @Override
   public void close() {
     if (scheduledTask != null) {
+      cleanupQueue.clear();
       scheduledTask.cancel();
     }
+    closed = true;
   }
 
   int getBucketIndex(long tsSecs) {
     return (int)Math.ceil(tsSecs / (double)granularity) %  numBuckets;
+  }
+
+  int capacity() {
+    return capacity;
+  }
+
+  int tickCapacity() {
+    return tickCapacity;
   }
 }
--- dd-java-agent/agent-profiling/profiling-context/src/test/java/com/datadog/profiling/context/TimeBucketsTest.java
+++ dd-java-agent/agent-profiling/profiling-context/src/test/java/com/datadog/profiling/context/TimeBucketsTest.java
@@ -6,16 +6,19 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TimeBucketsTest {
+  private static long expiration = 5;
+  private static long granularity = 1;
+  private static int capacity = 20;
+
   private TimeBuckets instance;
 
   @BeforeEach
   void setup() throws Exception {
-    instance = new TimeBuckets(5, 1, TimeUnit.MILLISECONDS, 2, 20);
+    instance = new TimeBuckets(expiration, granularity, TimeUnit.MILLISECONDS, 2, capacity, false);
   }
 
   @AfterEach
@@ -24,21 +27,54 @@ class TimeBucketsTest {
   }
 
   @Test
-  void test() throws Exception {
-    TimeBuckets.Expiring[] expirings = new TimeBuckets.Expiring[10];
-    boolean check[] = new boolean[expirings.length + 4];
+  void checkCapacity() throws Exception {
+    for (int i = 0; i < capacity; i++) {
+      TimeBuckets.Expiring e = instance.add(i, () -> {});
+      assertNotNull(e);
+      assertNotEquals(TimeBuckets.Expiring.NOOP, e);
 
-    for (int i = 0; i < check.length + 4; i++) {
-      if (i < 10) {
+      // cleaner thread 'tick'
+      instance.processCleanup(i);
+    }
+  }
+
+  @Test
+  void checkBrokenCleanup() {
+    // here we are not going to move forward the cleaner ticks so we should start geting NOOPs after the 'expiration' number of adds
+
+    for (int i = 0; i < 16; i++) {
+      TimeBuckets.Expiring e = instance.add(i, () -> {});
+      assertNotNull(e);
+      assertNotEquals(TimeBuckets.Expiring.NOOP, e);
+    }
+
+    TimeBuckets.Expiring e = instance.add(17, () -> {});
+    assertEquals(TimeBuckets.Expiring.NOOP, e);
+  }
+
+  @Test
+  void test() throws Exception {
+    int limit = 4;
+    int iterations = limit * 10;
+    int backIdx = limit / 2;
+        TimeBuckets.Expiring[] expirings = new TimeBuckets.Expiring[limit];
+    boolean check[] = new boolean[iterations - limit];
+
+    for (int i = 0; i < iterations; i++) {
+      if (i < (iterations - limit)) {
         int target = i;
         expirings[i % expirings.length] = instance.add(i, () -> check[target] = true);
+      } else {
+        instance.expireBucket(i);
       }
-      if (i > 5) {
-        expirings[(i - 5) % expirings.length].touch(i);
-        instance.expireBucket(i - 6);
+
+      if (i > backIdx) {
+        expirings[(i - backIdx) % expirings.length].touch(i);
       }
+      instance.processCleanup(i);
     }
-    System.out.println(Arrays.toString(expirings));
+    instance.processCleanup(iterations);
+    System.out.println(Arrays.toString(check));
   }
 
 }
