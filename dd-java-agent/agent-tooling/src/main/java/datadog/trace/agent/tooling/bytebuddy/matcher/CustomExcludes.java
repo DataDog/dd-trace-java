bf8410fb5d8a37aaf73cb1d50288850e504cf4e9
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2022-08-03 14:12:25 +0100

    Avoid loading trie build if no custom excludes

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/bytebuddy/matcher/CustomExcludes.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/bytebuddy/matcher/CustomExcludes.java
@@ -3,6 +3,7 @@ package datadog.trace.agent.tooling.bytebuddy.matcher;
 import datadog.trace.api.Config;
 import datadog.trace.util.ClassNameTrie;
 import java.nio.file.Paths;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,19 +16,24 @@ public class CustomExcludes {
   private static final ClassNameTrie excludes;
 
   static {
-    ClassNameTrie.Builder builder = new ClassNameTrie.Builder();
-    for (String name : Config.get().getExcludedClasses()) {
-      builder.put(name, 1);
-    }
-    String excludedClassesFile = Config.get().getExcludedClassesFile();
-    if (null != excludedClassesFile) {
-      try {
-        builder.readClassNameMapping(Paths.get(excludedClassesFile));
-      } catch (Exception e) {
-        log.warn("Problem reading class excludes from {}", excludedClassesFile, e);
+    List<String> excludedClasses = Config.get().getExcludedClasses();
+    if (excludedClasses.isEmpty()) {
+      excludes = null;
+    } else {
+      ClassNameTrie.Builder builder = new ClassNameTrie.Builder();
+      for (String name : excludedClasses) {
+        builder.put(name, 1);
       }
+      String excludedClassesFile = Config.get().getExcludedClassesFile();
+      if (null != excludedClassesFile) {
+        try {
+          builder.readClassNameMapping(Paths.get(excludedClassesFile));
+        } catch (Exception e) {
+          log.warn("Problem reading class excludes from {}", excludedClassesFile, e);
+        }
+      }
+      excludes = builder.buildTrie();
     }
-    excludes = !builder.isEmpty() ? builder.buildTrie() : null;
   }
 
   public static boolean isExcluded(String name) {
