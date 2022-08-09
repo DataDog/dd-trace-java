343ea46faa8c7c9b9638a80373bc6f9df4702c0e
Ludovic Henry <git@ludovic.dev>
2022-08-09 13:31:37 +0000

    Fix async tests

--- dd-java-agent/agent-profiling/profiling-async/src/test/java/com/datadog/profiling/async/AsyncProfilerTest.java
+++ dd-java-agent/agent-profiling/profiling-async/src/test/java/com/datadog/profiling/async/AsyncProfilerTest.java
@@ -61,9 +61,10 @@ class AsyncProfilerTest {
 
   @ParameterizedTest
   @MethodSource("profilingModes")
-  void testStartCmd(boolean cpu, boolean alloc, boolean memleak) throws Exception {
+  void testStartCmd(boolean cpu, boolean wall, boolean alloc, boolean memleak) throws Exception {
     Properties props = new Properties();
     props.put(ProfilingConfig.PROFILING_ASYNC_CPU_ENABLED, Boolean.toString(cpu));
+    props.put(ProfilingConfig.PROFILING_ASYNC_WALL_ENABLED, Boolean.toString(wall));
     props.put(ProfilingConfig.PROFILING_ASYNC_ALLOC_ENABLED, Boolean.toString(alloc));
     props.put(ProfilingConfig.PROFILING_ASYNC_MEMLEAK_ENABLED, Boolean.toString(memleak));
 
@@ -73,7 +74,10 @@ class AsyncProfilerTest {
     String cmd = profiler.cmdStartProfiling(targetFile);
 
     if (profiler.enabledModes().contains(ProfilingMode.CPU)) {
-      assertTrue(cmd.contains("event=cpu"));
+      assertTrue(cmd.contains("cpu="));
+    }
+    if (profiler.enabledModes().contains(ProfilingMode.WALL)) {
+      assertTrue(cmd.contains("wall="));
     }
     if (profiler.enabledModes().contains(ProfilingMode.ALLOCATION)) {
       assertTrue(cmd.contains("alloc="));
@@ -85,13 +89,21 @@ class AsyncProfilerTest {
 
   private static Stream<Arguments> profilingModes() {
     return Stream.of(
-        Arguments.of(false, false, false),
-        Arguments.of(false, false, true),
-        Arguments.of(false, true, false),
-        Arguments.of(false, true, true),
-        Arguments.of(true, false, false),
-        Arguments.of(true, false, true),
-        Arguments.of(true, true, false),
-        Arguments.of(true, true, true));
+        Arguments.of(false, false, false, false),
+        Arguments.of(false, false, false, true),
+        Arguments.of(false, false, true, false),
+        Arguments.of(false, false, true, true),
+        Arguments.of(false, true, false, false),
+        Arguments.of(false, true, false, true),
+        Arguments.of(false, true, true, false),
+        Arguments.of(false, true, true, true),
+        Arguments.of(true, false, false, false),
+        Arguments.of(true, false, false, true),
+        Arguments.of(true, false, true, false),
+        Arguments.of(true, false, true, true),
+        Arguments.of(true, true, false, false),
+        Arguments.of(true, true, false, true),
+        Arguments.of(true, true, true, false),
+        Arguments.of(true, true, true, true));
   }
 }
