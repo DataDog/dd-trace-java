c30c8faf80c59db4000aa638f82c0cc4770bb00f
jbachorik <jaroslav.bachorik@datadoghq.com>
2022-08-12 16:46:52 +0200

    Use the span sampling decision in context tracking

--- dd-trace-core/src/main/java/datadog/trace/core/DDSpan.java
+++ dd-trace-core/src/main/java/datadog/trace/core/DDSpan.java
@@ -57,7 +57,10 @@ public class DDSpan
     final DDSpan span = new DDSpan(timestampMicro, context, emitCheckpoints);
     log.debug("Started span: {}", span);
     context.getTrace().registerSpan(span);
-    span.tracingContextTracker = TracingContextTrackerFactory.instance(span);
+    span.tracingContextTracker =
+        span.eligibleForDropping()
+            ? TracingContextTracker.EMPTY
+            : TracingContextTrackerFactory.instance(span);
     return span;
   }
 
