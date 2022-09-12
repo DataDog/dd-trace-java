80308cbbd78ffad276d61b68206637cc17fd5511
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2022-09-12 12:55:20 +0200

    Align plugins, formatting and use application main class name in config

--- dd-smoke-tests/debugger-integration-tests/latest-jdk-app/src/main/java/App.java
+++ dd-smoke-tests/debugger-integration-tests/latest-jdk-app/src/main/java/App.java
@@ -12,72 +12,72 @@ public class App {
   private static final String LOG_FILENAME = System.getenv().get("DD_LOG_FILE");
 
   public String getGreeting() {
-        return "Hello World!";
-    }
+    return "Hello World!";
+  }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Waiting for instrumentation...");
-        waitForInstrumentation();
-        System.out.println("Executing method getGreeting");
-        System.out.println(new App().getGreeting());
-        System.out.println("Executed");
-        waitForSnapshotUpload(1);
-        System.out.println("Exiting...");
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("Waiting for instrumentation...");
+    waitForInstrumentation();
+    System.out.println("Executing method getGreeting");
+    System.out.println(new App().getGreeting());
+    System.out.println("Executed");
+    waitForSnapshotUpload(1);
+    System.out.println("Exiting...");
+  }
 
   private static void waitForInstrumentation() throws IOException {
     AtomicBoolean generatingByteCode = new AtomicBoolean();
     waitForSpecificLogLine(
-      Paths.get(LOG_FILENAME),
-      line -> {
-        // when instrumentation is done by main thread, we are good to go
-        if (line.contains(
-          "[main] DEBUG com.datadog.debugger.agent.DebuggerTransformer - Generating bytecode for class: App")) {
-          return true;
-        }
-        if (!generatingByteCode.get()) {
-          // instrumentation is done by background thread, need to wait for end of
-          // re-transformation
+        Paths.get(LOG_FILENAME),
+        line -> {
+          // when instrumentation is done by main thread, we are good to go
           if (line.contains(
-            "[dd-task-scheduler] DEBUG com.datadog.debugger.agent.DebuggerTransformer - Generating bytecode for class: App")) {
-            generatingByteCode.set(true);
-          }
-        } else {
-          if (line.contains(
-            "com.datadog.debugger.agent.DebuggerProbeRedefinition - Re-transformation done.")) {
+              "[main] DEBUG com.datadog.debugger.agent.DebuggerTransformer - Generating bytecode for class: App")) {
             return true;
           }
-        }
-        return false;
-      },
-      () -> {},
-      Duration.ofMillis(100),
-      Duration.ofSeconds(10));
+          if (!generatingByteCode.get()) {
+            // instrumentation is done by background thread, need to wait for end of
+            // re-transformation
+            if (line.contains(
+                "[dd-task-scheduler] DEBUG com.datadog.debugger.agent.DebuggerTransformer - Generating bytecode for class: App")) {
+              generatingByteCode.set(true);
+            }
+          } else {
+            if (line.contains(
+                "com.datadog.debugger.agent.DebuggerProbeRedefinition - Re-transformation done.")) {
+              return true;
+            }
+          }
+          return false;
+        },
+        () -> {},
+        Duration.ofMillis(100),
+        Duration.ofSeconds(10));
   }
 
   private static void waitForSnapshotUpload(int expectedUploads) throws IOException {
     AtomicInteger uploadCount = new AtomicInteger(0);
     waitForSpecificLogLine(
-      Paths.get(LOG_FILENAME),
-      line -> {
-        if (line.contains("DEBUG com.datadog.debugger.uploader.SnapshotUploader - Upload done")) {
-          int currentUploads = uploadCount.incrementAndGet();
-          return currentUploads == expectedUploads;
-        }
-        return false;
-      },
-      () -> uploadCount.set(0),
-      Duration.ofMillis(100),
-      Duration.ofSeconds(10));
+        Paths.get(LOG_FILENAME),
+        line -> {
+          if (line.contains("DEBUG com.datadog.debugger.uploader.SnapshotUploader - Upload done")) {
+            int currentUploads = uploadCount.incrementAndGet();
+            return currentUploads == expectedUploads;
+          }
+          return false;
+        },
+        () -> uploadCount.set(0),
+        Duration.ofMillis(100),
+        Duration.ofSeconds(10));
   }
 
   private static void waitForSpecificLogLine(
-    Path logFilePath,
-    Predicate<String> lineMatcher,
-    Runnable init,
-    Duration sleep,
-    Duration timeout)
-    throws IOException {
+      Path logFilePath,
+      Predicate<String> lineMatcher,
+      Runnable init,
+      Duration sleep,
+      Duration timeout)
+      throws IOException {
     boolean[] result = new boolean[] {false};
     long total = sleep.toNanos() == 0 ? 0 : timeout.toNanos() / sleep.toNanos();
     int i = 0;
@@ -86,12 +86,12 @@ public class App {
       System.err.flush();
       init.run();
       Files.lines(logFilePath)
-        .forEach(
-          it -> {
-            if (lineMatcher.test(it)) {
-              result[0] = true;
-            }
-          });
+          .forEach(
+              it -> {
+                if (lineMatcher.test(it)) {
+                  result[0] = true;
+                }
+              });
       LockSupport.parkNanos(sleep.toNanos());
       i++;
     }
@@ -99,5 +99,4 @@ public class App {
       System.out.println("waitForSpecificLogLine timed out!");
     }
   }
-
 }
