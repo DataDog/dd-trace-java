7cb148203f6580c9a16ab67c83e3e6f10021452d
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2022-09-19 15:46:37 +0200

    Collect error logs and heap dumps during CI build

--- .circleci/collect_libs.sh
+++ .circleci/collect_libs.sh
@@ -9,6 +9,9 @@ set -e
 LIBS_DIR=./libs/
 mkdir -p $LIBS_DIR >/dev/null 2>&1
 
+cp /tmp/hs_err_pid*.log $LIBS_DIR || true
+cp /tmp/java_pid*.hprof $LIBS_DIR || true
+
 for lib_path in workspace/*/build/libs; do
     echo "saving libs in $lib_path"
     cp $lib_path/*.jar $LIBS_DIR/
