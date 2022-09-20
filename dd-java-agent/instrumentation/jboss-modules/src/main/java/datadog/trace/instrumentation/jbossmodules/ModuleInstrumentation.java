e16ac44f962e8af230e6648cb9850ecfce8fd9cc
Stuart McCulloch <mcculls@gmail.com>
2022-09-20 16:40:36 +0100

    Instrument JBoss Modules to relax visibility rules during transform requests

---
+++ dd-java-agent/instrumentation/jboss-modules/src/main/java/datadog/trace/instrumentation/jbossmodules/ModuleInstrumentation.java
@@ -0,0 +1,156 @@
+package datadog.trace.instrumentation.jbossmodules;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.bootstrap.AgentClassLoading;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import net.bytebuddy.asm.Advice;
+import org.jboss.modules.Module;
+import org.jboss.modules.ModuleLinkageHelper;
+
+@AutoService(Instrumenter.class)
+public final class ModuleInstrumentation extends Instrumenter.Tracing
+    implements Instrumenter.ForSingleType {
+  public ModuleInstrumentation() {
+    super("classloading", "jboss-modules");
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "org.jboss.modules.Module";
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      "org.jboss.modules.ModuleLinkageHelper",
+      "org.jboss.modules.ModuleLinkageHelper$1",
+      "org.jboss.modules.ModuleLinkageHelper$2"
+    };
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    transformation.applyAdvice(
+        isMethod()
+            .and(named("getResource"))
+            .and(takesArguments(1).and(takesArgument(0, String.class))),
+        ModuleInstrumentation.class.getName() + "$WidenGetResourceAdvice");
+    transformation.applyAdvice(
+        isMethod()
+            .and(named("getResourceAsStream"))
+            .and(takesArguments(1).and(takesArgument(0, String.class))),
+        ModuleInstrumentation.class.getName() + "$WidenGetResourceAsStreamAdvice");
+    transformation.applyAdvice(
+        isMethod()
+            .and(named("loadModuleClass"))
+            .and(
+                takesArguments(1)
+                    .and(takesArgument(0, String.class))
+                    .or(
+                        takesArguments(2)
+                            .and(takesArgument(0, String.class))
+                            .and(takesArgument(1, boolean.class)))),
+        ModuleInstrumentation.class.getName() + "$WidenLoadClassAdvice");
+  }
+
+  /**
+   * Bypass local visibility rules by repeating failed requests from module linked as dependencies.
+   *
+   * <p>We only do this for agent requests that require this additional visibility.
+   */
+  public static class WidenGetResourceAdvice {
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void onExit(
+        @Advice.This final Module module,
+        @Advice.Argument(0) final String name,
+        @Advice.Return(readOnly = false) URL result,
+        @Advice.Thrown(readOnly = false) Throwable error) {
+      if (null == result) {
+        AgentClassLoading requestType = AgentClassLoading.type();
+        if (null != requestType) {
+          requestType.end(); // avoid looping back into our advice
+          try {
+            // widen search by peeking inside module linkage
+            result = ModuleLinkageHelper.getResource(module, name);
+            if (null != result) {
+              error = null; // clear any error from original call
+            }
+          } finally {
+            requestType.begin();
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Bypass local visibility rules by repeating failed requests from modules linked as dependencies.
+   *
+   * <p>We only do this for agent requests that require this additional visibility.
+   */
+  public static class WidenGetResourceAsStreamAdvice {
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void onExit(
+        @Advice.This final Module module,
+        @Advice.Argument(0) final String name,
+        @Advice.Return(readOnly = false) InputStream result,
+        @Advice.Thrown(readOnly = false) Throwable error) {
+      if (null == result) {
+        AgentClassLoading requestType = AgentClassLoading.type();
+        if (null != requestType) {
+          requestType.end(); // avoid looping back into our advice
+          try {
+            // widen search by peeking inside module linkage
+            URL resource = ModuleLinkageHelper.getResource(module, name);
+            if (null != resource) {
+              result = resource.openStream();
+              error = null; // clear any error from original call
+            }
+          } catch (IOException e) {
+            // ignore missing resource
+          } finally {
+            requestType.begin();
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Bypass local visibility rules by repeating failed requests from modules linked as dependencies.
+   *
+   * <p>We only do this for agent requests that require this additional visibility.
+   */
+  public static class WidenLoadClassAdvice {
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void onExit(
+        @Advice.This final Module module,
+        @Advice.Argument(0) final String name,
+        @Advice.Return(readOnly = false) Class<?> result,
+        @Advice.Thrown(readOnly = false) Throwable error) {
+      if (null == result) {
+        AgentClassLoading requestType = AgentClassLoading.type();
+        if (null != requestType) {
+          requestType.end(); // avoid looping back into our advice
+          try {
+            // widen search by peeking inside module linkage
+            result = ModuleLinkageHelper.loadClass(module, name);
+            if (null != result) {
+              error = null; // clear any error from original call
+            }
+          } finally {
+            requestType.begin();
+          }
+        }
+      }
+    }
+  }
+}
