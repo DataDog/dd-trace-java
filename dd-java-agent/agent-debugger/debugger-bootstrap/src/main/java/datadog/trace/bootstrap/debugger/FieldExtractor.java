84098cbfafe0203b14ee493a9170df637e23a38b
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2022-09-27 10:29:10 +0200

    move basic extract field code to test

--- dd-java-agent/agent-debugger/debugger-bootstrap/src/main/java/datadog/trace/bootstrap/debugger/FieldExtractor.java
+++ dd-java-agent/agent-debugger/debugger-bootstrap/src/main/java/datadog/trace/bootstrap/debugger/FieldExtractor.java
@@ -2,9 +2,6 @@ package datadog.trace.bootstrap.debugger;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.ObjIntConsumer;
 import org.slf4j.Logger;
@@ -24,65 +21,6 @@ public class FieldExtractor {
       return false;
     }
     return true;
-  }
-
-  private static void extractField(
-      Field field, Object value, Map<String, Snapshot.CapturedValue> results, Limits limits) {
-    Map<String, Snapshot.CapturedValue> subFields =
-        extract(
-            value,
-            new Limits(
-                limits.maxReferenceDepth - 1,
-                limits.maxCollectionSize,
-                limits.maxLength,
-                limits.maxFieldCount));
-    Snapshot.CapturedValue capturedValue =
-        Snapshot.CapturedValue.raw(field.getType().getName(), value, limits, subFields, null);
-    results.put(field.getName(), capturedValue);
-  }
-
-  private static void handleExtractException(
-      Exception ex, Field field, String className, Map<String, Snapshot.CapturedValue> results) {
-    String fieldName = field.getName();
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Cannot extract field[{}] from class[{}]", fieldName, className, ex);
-    }
-    Snapshot.CapturedValue notCapturedReason =
-        Snapshot.CapturedValue.notCapturedReason(
-            fieldName, field.getType().getName(), ex.toString());
-    results.put(fieldName, notCapturedReason);
-  }
-
-  private static void onMaxFieldCount(
-      Field f, Map<String, Snapshot.CapturedValue> results, int maxFieldCount, int totalFields) {
-    String msg =
-        String.format(
-            "Max %d fields reached, %d fields were not captured",
-            maxFieldCount, totalFields - maxFieldCount);
-    results.put("@status", Snapshot.CapturedValue.notCapturedReason("@status", "", msg));
-  }
-
-  public static Map<String, Snapshot.CapturedValue> extract(Object obj, Limits limits) {
-    if (obj == null) {
-      return Collections.emptyMap();
-    }
-    if (Fields.isPrimitiveClass(obj)) {
-      return Collections.emptyMap();
-    }
-    Field[] declaredFields = obj.getClass().getDeclaredFields();
-    if (declaredFields.length == 0) {
-      return Collections.emptyMap();
-    }
-    Map<String, Snapshot.CapturedValue> results = new HashMap<>();
-    Fields.processFields(
-        obj,
-        FieldExtractor::filterIn,
-        (f, value, maxDepth) -> extractField(f, value, results, limits),
-        (ex, f) -> handleExtractException(ex, f, obj.getClass().getName(), results),
-        (f, total) -> onMaxFieldCount(f, results, limits.maxFieldCount, total),
-        limits.maxFieldCount,
-        limits.maxReferenceDepth);
-    return results;
   }
 
   public static void extract(
