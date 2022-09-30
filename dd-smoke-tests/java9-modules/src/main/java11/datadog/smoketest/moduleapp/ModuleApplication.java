2ed150d491f481bcb5c6e86b0e1b1d73f8c4246e
roberto montero <roberto.montero@datadoghq.com>
2022-09-30 08:38:21 +0200

    simplify the test

--- dd-smoke-tests/java9-modules/src/main/java11/datadog/smoketest/moduleapp/ModuleApplication.java
+++ dd-smoke-tests/java9-modules/src/main/java11/datadog/smoketest/moduleapp/ModuleApplication.java
@@ -3,6 +3,24 @@ package datadog.smoketest.moduleapp;
 public class ModuleApplication {
   public static void main(final String[] args) throws InterruptedException {
     Thread.sleep(600);
-    new BasicModuleTest().doTests();
+    ModuleApplication.testBaeModuleRead();
+  }
+  public static void testBaeModuleRead() {
+    Module base = Object.class.getModule();
+    Module thisModule = ModuleApplication.class.getModule();
+
+    if(base.canRead(base)) {
+      System.out.println("Good. Base can read Base");
+    }else {
+      throw new RuntimeException("Fail! Base cannot read base");
+    }
+
+    if(!base.canRead(thisModule)) {
+      System.out.println("Good. Base cannot read thisModule");
+    }else {
+      throw new RuntimeException("Fail! Base can read thisModule");
+    }
+
+    System.out.println("Finish!!!");
   }
 }
