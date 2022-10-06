2b4f0915e2fb24b13bbdd4b86faa519681564f30
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2022-10-06 13:32:49 +0200

    Add spanId to vulnerabilities

--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/model/Location.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/model/Location.java
@@ -1,18 +1,27 @@
 package com.datadog.iast.model;
 
+import datadog.trace.api.DDId;
+
 public final class Location {
 
   private final String path;
 
   private final int line;
 
-  private Location(final String path, final int line) {
+  private final DDId spanId;
+
+  private Location(final DDId spanId, final String path, final int line) {
+    this.spanId = spanId;
     this.path = path;
     this.line = line;
   }
 
-  public static Location forStack(final StackTraceElement stack) {
-    return new Location(stack.getClassName(), stack.getLineNumber());
+  public static Location forSpanAndStack(final DDId spanId, final StackTraceElement stack) {
+    return new Location(spanId, stack.getClassName(), stack.getLineNumber());
+  }
+
+  public DDId getSpanId() {
+    return spanId;
   }
 
   public String getPath() {
