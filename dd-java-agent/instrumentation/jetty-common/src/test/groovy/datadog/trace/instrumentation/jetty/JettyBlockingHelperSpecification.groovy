52b7484646cdbc5bcd9f3e4d06c63e5a60ed9c5e
Gustavo Lopes <mail@geleia.net>
2022-10-11 14:47:25 +0100

    Add test for JettyBlockingHelper

---
+++ dd-java-agent/instrumentation/jetty-common/src/test/groovy/datadog/trace/instrumentation/jetty/JettyBlockingHelperSpecification.groovy
@@ -0,0 +1,37 @@
+package datadog.trace.instrumentation.jetty
+
+import datadog.trace.api.gateway.Flow
+import datadog.trace.test.util.DDSpecification
+import org.eclipse.jetty.server.Request
+import org.eclipse.jetty.server.Response
+
+import javax.servlet.ServletOutputStream
+
+import static datadog.trace.api.gateway.Flow.Action.BlockingContentType.AUTO
+
+class JettyBlockingHelperSpecification extends DDSpecification {
+  void 'block completes successfully'() {
+    setup:
+    Request req = Mock()
+    Response resp = Mock()
+    ServletOutputStream os = Mock()
+
+    when:
+    JettyBlockingHelper.block(req, resp, new Flow.Action.RequestBlockingAction(402, AUTO))
+
+    then:
+    1 * resp.isCommitted() >> false
+    1 * resp.setStatus(402)
+    1 * req.getHeader('Accept') >> 'text/html'
+    1 * resp.setHeader('Content-type', 'text/html;charset=utf-8')
+    1 * resp.setHeader('Content-length', _)
+    1 * resp.getOutputStream() >> os
+    1 * os.write(_)
+    1 * os.close()
+    if (resp.respondsTo('complete')) {
+      1 * resp.complete()
+    } else {
+      1 * resp.closeOutput()
+    }
+  }
+}
