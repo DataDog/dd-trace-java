89a5c20f163ba241a2323d7437c41687b82e439c
Gustavo Lopes <mail@geleia.net>
2022-10-13 16:34:44 +0100

    Add benchmark comparing old iastmodule strategy with new one

---
+++ internal-api/src/jmh/java/datadog/trace/api/iast/CallPseudoAdviceDump.java
@@ -0,0 +1,91 @@
+package datadog.trace.api.iast;
+
+import net.bytebuddy.jar.asm.ClassWriter;
+import net.bytebuddy.jar.asm.Handle;
+import net.bytebuddy.jar.asm.MethodVisitor;
+import net.bytebuddy.jar.asm.Opcodes;
+
+public class CallPseudoAdviceDump implements Opcodes {
+  public static byte[] dump() throws Exception {
+    ClassWriter cw = new ClassWriter(0);
+    MethodVisitor mv;
+
+    cw.visit(
+        V1_7,
+        ACC_PUBLIC | ACC_SUPER,
+        "datadog/trace/api/iast/CallPseudoAdvice",
+        null,
+        "java/lang/Object",
+        new String[] {"datadog/trace/api/iast/HelperInvocationBenchmark$CallPseudoAdviceI"});
+
+    cw.visitInnerClass(
+        "datadog/trace/api/iast/HelperInvocationBenchmark$CallPseudoAdviceI",
+        "datadog/trace/api/iast/HelperInvocationBenchmark",
+        "CallPseudoAdviceI",
+        ACC_PUBLIC | ACC_STATIC | ACC_ABSTRACT | ACC_INTERFACE);
+
+    {
+      mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+      mv.visitCode();
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(1, 1);
+      mv.visitEnd();
+    }
+    {
+      mv =
+          cw.visitMethod(
+              ACC_PUBLIC,
+              "callStatic",
+              "(Lorg/openjdk/jmh/infra/Blackhole;Ljava/lang/String;)V",
+              null,
+              null);
+      mv.visitCode();
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitVarInsn(ALOAD, 2);
+      mv.visitMethodInsn(
+          INVOKESTATIC,
+          "datadog/trace/api/iast/HelperInvocationBenchmark$StaticBridge",
+          "callHelper",
+          "(Lorg/openjdk/jmh/infra/Blackhole;Ljava/lang/String;)V",
+          false);
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(2, 3);
+      mv.visitEnd();
+    }
+    {
+      mv =
+          cw.visitMethod(
+              ACC_PUBLIC,
+              "callDynamic",
+              "(Lorg/openjdk/jmh/infra/Blackhole;Ljava/lang/String;)V",
+              null,
+              null);
+      mv.visitCode();
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitVarInsn(ALOAD, 2);
+
+      Handle handle =
+          new Handle(
+              Opcodes.H_INVOKESTATIC,
+              "datadog/trace/api/iast/CallSiteHelperRegistry",
+              "bootstrap",
+              "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/String;)Ljava/lang/invoke/CallSite;",
+              false);
+      String helperKey =
+          HelperInvocationBenchmark.HelperContainer.class.getName() + "\0" + "helperMeth";
+      mv.visitInvokeDynamicInsn(
+          "callHelper",
+          "(Lorg/openjdk/jmh/infra/Blackhole;Ljava/lang/String;)V",
+          handle,
+          helperKey);
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(2, 3);
+      mv.visitEnd();
+    }
+    cw.visitEnd();
+
+    return cw.toByteArray();
+  }
+}
---
+++ internal-api/src/jmh/java/datadog/trace/api/iast/HelperInvocationBenchmark.java
@@ -0,0 +1,105 @@
+package datadog.trace.api.iast;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+import net.bytebuddy.dynamic.loading.ByteArrayClassLoader;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@Fork(value = 3)
+@Measurement(iterations = 4, time = 5)
+@Warmup(iterations = 3, time = 3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class HelperInvocationBenchmark {
+
+  interface HelpersI {
+    void helperMeth(Blackhole bh, String s);
+  }
+
+  public static class HelpersImpl implements HelpersI {
+    @Override
+    public void helperMeth(Blackhole bh, String s) {
+      bh.consume(s);
+    }
+  }
+
+  public static class StaticBridge {
+    static HelpersI helpers;
+
+    public static void callHelper(Blackhole bh, String s) {
+      if (helpers != null) {
+        try {
+          helpers.helperMeth(bh, s);
+        } catch (Throwable t) {
+          System.out.println("Helper threw");
+        }
+      }
+    }
+  }
+
+  public static class HelperContainer implements CallSiteHelperContainer {
+    @CallSiteHelper
+    public static void helperMeth(Blackhole bh, String s) {
+      bh.consume(s);
+    }
+  }
+
+  @State(Scope.Benchmark)
+  public static class StateClass {
+    private final CallPseudoAdviceI callPseudoAdvice;
+
+    public StateClass() {
+      StaticBridge.helpers = new HelpersImpl();
+      CallSiteHelperRegistry.reset();
+      CallSiteHelperRegistry.registerHelperContainer(MethodHandles.lookup(), HelperContainer.class);
+      String cpaClassName =
+          HelperInvocationBenchmark.class.getPackage().getName() + ".CallPseudoAdvice";
+      try {
+        ByteArrayClassLoader cl =
+            new ByteArrayClassLoader(
+                StateClass.class.getClassLoader(),
+                Collections.singletonMap(cpaClassName, CallPseudoAdviceDump.dump()));
+        Class<?> cpaCls = cl.loadClass(cpaClassName);
+        callPseudoAdvice = (CallPseudoAdviceI) cpaCls.newInstance();
+      } catch (Exception e) {
+        throw new UndeclaredThrowableException(e);
+      }
+    }
+  }
+
+  public interface CallPseudoAdviceI {
+    void callStatic(Blackhole bh, String s);
+
+    void callDynamic(Blackhole bh, String s);
+  }
+
+  @Benchmark
+  public void callHelperStatic(StateClass cls, Blackhole bh) {
+    cls.callPseudoAdvice.callStatic(bh, "my string");
+  }
+
+  @Benchmark
+  public void callHelperDynamic(StateClass cls, Blackhole bh) {
+    cls.callPseudoAdvice.callDynamic(bh, "my string");
+  }
+
+  public static void main(String[] args) throws Exception {
+    StateClass stateClass = new StateClass();
+    Blackhole bh =
+        new Blackhole(
+            "Today's password is swordfish. I understand instantiating Blackholes directly is dangerous.");
+    stateClass.callPseudoAdvice.callStatic(bh, "my string");
+    stateClass.callPseudoAdvice.callDynamic(bh, "my string");
+  }
+}
