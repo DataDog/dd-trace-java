13c954891e17a93644aa02a4db870c4f1b24b514
nayeem-kamal <nayeem.kamal@datadoghq.com>
2022-10-14 16:55:37 -0400

    WIP-adding extraction logic boilerplate

--- dd-trace-core/src/main/java/datadog/trace/core/propagation/W3CHttpCodec.java
+++ dd-trace-core/src/main/java/datadog/trace/core/propagation/W3CHttpCodec.java
@@ -5,6 +5,8 @@ import datadog.trace.core.DDSpanContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
 /**
  * A Codec designed for HTTP Context propagation using W3C Trace Context Propagation
  *
@@ -18,7 +20,7 @@ public class W3CHttpCodec {
   static final String VERSION = "00";
 
 
-  private W3CHttpCodec(){
+  private W3CHttpCodec() {
     // This class should not be created. This also makes code coverage checks happy.
   }
 
@@ -38,12 +40,45 @@ public class W3CHttpCodec {
       //W3C traceparent format: version - trace-id - parent-id - trace_flags
       injectedTraceparent.append(VERSION).append('-').append(injectedTraceId).append('-')
           .append(injectedSpanId).append('-').append("00");
-      setter.set(carrier,TRACE_PARENT,injectedTraceparent.toString());
+      setter.set(carrier, TRACE_PARENT, injectedTraceparent.toString());
 
       final StringBuilder injectedTracestate = new StringBuilder();
-
+      injectedTracestate.append("dd.context").append(contextList(context));
     }
   }
 
+  public static HttpCodec.Extractor newExtractor(final Map<String, String> tagMapping) {
+    return new TagContextExtractor(
+        tagMapping,
+        new ContextInterpreter.Factory() {
+          @Override
+          protected ContextInterpreter construct(final Map<String, String> mapping) {
+            return new B3HttpCodec.W3CContextInterpreter(mapping);
+          }
+        });
+  }
 
+  private static String contextList(final DDSpanContext context) {
+    return "";
+  }
+
+  private static class W3CContextInterpreter extends ContextInterpreter {
+
+
+    private W3CContextInterpreter(Map<String, String> taggedHeaders) {
+      super(taggedHeaders);
+    }
+
+    @Override
+    public boolean accept(String key, String value) {
+      if (null == key || key.isEmpty()) {
+        return true;
+      }
+      if (LOG_EXTRACT_HEADER_NAMES) {
+        log.debug("Header: {}", key);
+      }
+      return false;
+    }
+
+  }
 }
