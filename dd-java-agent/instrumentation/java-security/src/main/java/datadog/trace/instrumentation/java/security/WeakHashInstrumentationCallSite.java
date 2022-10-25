f071c5904a70bfa6d721c67554dedbad7e0f9131
Javier Santos <javier.santos@datadoghq.com>
2022-10-25 12:19:49 +0200

    Moving to after instead of before

--- dd-java-agent/instrumentation/java-security/src/main/java/datadog/trace/instrumentation/java/security/WeakHashInstrumentationCallSite.java
+++ dd-java-agent/instrumentation/java-security/src/main/java/datadog/trace/instrumentation/java/security/WeakHashInstrumentationCallSite.java
@@ -3,13 +3,24 @@ package datadog.trace.instrumentation.java.security;
 import datadog.trace.agent.tooling.csi.CallSite;
 import datadog.trace.api.iast.IastAdvice;
 import datadog.trace.api.iast.InstrumentationBridge;
+import java.security.MessageDigest;
 
 @CallSite(spi = IastAdvice.class)
 public class WeakHashInstrumentationCallSite {
 
+  /*
   @CallSite.Before(
       "java.security.MessageDigest java.security.MessageDigest.getInstance(java.lang.String)")
   public static void beforeGetInstance(@CallSite.Argument final String algo) {
     InstrumentationBridge.onMessageDigestGetInstance(algo);
+  }
+   */
+
+  @CallSite.After(
+      "java.security.MessageDigest java.security.MessageDigest.getInstance(java.lang.String)")
+  public static MessageDigest afterGetInstance(
+      @CallSite.Argument final String algo, @CallSite.Return final MessageDigest retValue) {
+    InstrumentationBridge.onMessageDigestGetInstance(algo);
+    return retValue;
   }
 }
