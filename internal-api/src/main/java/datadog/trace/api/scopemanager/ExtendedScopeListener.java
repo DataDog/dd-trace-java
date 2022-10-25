f14c7133b6762bbad9987720cd77e0740b3b9464
Richard Startin <richard.startin@datadoghq.com>
2022-10-25 21:44:13 +0100

    add local root span id to ExtendedScopeListener

--- internal-api/src/main/java/datadog/trace/api/scopemanager/ExtendedScopeListener.java
+++ internal-api/src/main/java/datadog/trace/api/scopemanager/ExtendedScopeListener.java
@@ -4,7 +4,7 @@ import datadog.trace.api.DDId;
 import datadog.trace.context.ScopeListener;
 
 public interface ExtendedScopeListener extends ScopeListener {
-  void afterScopeActivated(DDId traceId, DDId spanId);
+  void afterScopeActivated(DDId traceId, DDId localRootSpanId, DDId spanId);
 
   /** Called just after a scope is closed. */
   @Override
