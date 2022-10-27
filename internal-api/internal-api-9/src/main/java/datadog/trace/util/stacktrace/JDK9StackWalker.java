bc2b1abe62a73c330119893f0384064152efcbc3
Manuel Álvarez Álvarez <manuel-alvarez-alvarez@users.noreply.github.com>
2022-10-27 18:06:40 +0200

    Prevent segmentation fault in IBM J9 11 (#4108)

--- internal-api/internal-api-9/src/main/java/datadog/trace/util/stacktrace/JDK9StackWalker.java
+++ internal-api/internal-api-9/src/main/java/datadog/trace/util/stacktrace/JDK9StackWalker.java
@@ -1,5 +1,6 @@
 package datadog.trace.util.stacktrace;
 
+import datadog.trace.api.Platform;
 import de.thetaphi.forbiddenapis.SuppressForbidden;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -7,25 +8,53 @@ import java.util.stream.Stream;
 @SuppressForbidden
 public class JDK9StackWalker extends AbstractStackWalker {
 
-  static java.lang.StackWalker walker;
+  private static final java.lang.StackWalker walker;
+  private static final StackMapper mapper;
 
   static {
-    try {
-      walker = java.lang.StackWalker.getInstance();
-
-    } catch (Throwable e) {
-      // Nothing to do
-    }
+    walker = newStackWalker();
+    mapper = findMapper();
   }
 
   @Override
   public boolean isEnabled() {
-    return walker != null;
+    return walker != null && mapper != null;
   }
 
   @Override
   <T> T doGetStack(final Function<Stream<StackTraceElement>, T> consumer) {
-    return walker.walk(
-        stack -> consumer.apply(stack.map(java.lang.StackWalker.StackFrame::toStackTraceElement)));
+    return walker.walk(stack -> consumer.apply(stack.map(mapper)));
   }
+
+  /**
+   * IBM J9 v.0.26.0-release was segfaulting when calling StackFrame::toStackTraceElement(), newer
+   * versions are OK
+   */
+  private static StackTraceElement mapFrameForJ9(final java.lang.StackWalker.StackFrame frame) {
+    return new StackTraceElement(
+        frame.getClassName(), frame.getMethodName(), frame.getFileName(), frame.getLineNumber());
+  }
+
+  private static java.lang.StackWalker newStackWalker() {
+    try {
+      return java.lang.StackWalker.getInstance();
+    } catch (final Throwable e) {
+      // Nothing to do
+      return null;
+    }
+  }
+
+  private static StackMapper findMapper() {
+    try {
+      return Platform.isJ9()
+          ? JDK9StackWalker::mapFrameForJ9
+          : java.lang.StackWalker.StackFrame::toStackTraceElement;
+    } catch (final Throwable e) {
+      // Nothing to do
+      return null;
+    }
+  }
+
+  private interface StackMapper
+      extends Function<java.lang.StackWalker.StackFrame, StackTraceElement> {}
 }
