7710f4d4e179d21682639d80e12183c5563613f8
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2022-11-15 16:53:17 +0100

    Create spans for vuls outside requests

--- dd-smoke-tests/springboot/src/main/java/ddtest/client/sources/Hasher.java
+++ dd-smoke-tests/springboot/src/main/java/ddtest/client/sources/Hasher.java
@@ -4,7 +4,27 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Hasher {
-  public void executeHash() throws NoSuchAlgorithmException {
-    MessageDigest.getInstance("MD5");
+  public MessageDigest sha1() {
+    try {
+      return MessageDigest.getInstance("SHA1");
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public MessageDigest md4() {
+    try {
+      return MessageDigest.getInstance("MD4");
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public MessageDigest md5() {
+    try {
+      return MessageDigest.getInstance("MD5");
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
