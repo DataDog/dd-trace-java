cac8f4e97ebb005f5ada24074e7752b892565c35
Andrea Marziali <andrea.marziali@datadoghq.com>
2022-11-22 15:36:04 +0100

    jetty11: fix servletpath and context path instrumentations

--- dd-java-agent/instrumentation/jetty-11/src/main/java11/datadog/trace/instrumentation/jetty11/SetContextPathAdvice.java
+++ dd-java-agent/instrumentation/jetty-11/src/main/java11/datadog/trace/instrumentation/jetty11/SetContextPathAdvice.java
@@ -1,9 +1,11 @@
 package datadog.trace.instrumentation.jetty11;
 
 import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.SERVLET_CONTEXT;
+import static datadog.trace.bootstrap.instrumentation.api.InstrumentationTags.SERVLET_PATH;
 import static datadog.trace.bootstrap.instrumentation.decorator.HttpServerDecorator.DD_DISPATCH_SPAN_ATTRIBUTE;
 import static datadog.trace.bootstrap.instrumentation.decorator.HttpServerDecorator.DD_SPAN_ATTRIBUTE;
 import static datadog.trace.instrumentation.jetty11.JettyDecorator.DD_CONTEXT_PATH_ATTRIBUTE;
+import static datadog.trace.instrumentation.jetty11.JettyDecorator.DD_SERVLET_PATH_ATTRIBUTE;
 
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import net.bytebuddy.asm.Advice;
@@ -24,8 +26,19 @@ public class SetContextPathAdvice {
       Object span = req.getAttribute(DD_SPAN_ATTRIBUTE);
       // Don't want to update while being dispatched to new servlet
       if (span instanceof AgentSpan && req.getAttribute(DD_DISPATCH_SPAN_ATTRIBUTE) == null) {
-        ((AgentSpan) span).setTag(SERVLET_CONTEXT, contextPath);
-        req.setAttribute(DD_CONTEXT_PATH_ATTRIBUTE, contextPath);
+        if (context != null && context.getContextPath() != null) {
+          final String servletContext = context.getContextPath();
+          ((AgentSpan) span).setTag(SERVLET_CONTEXT, servletContext);
+          req.setAttribute(DD_CONTEXT_PATH_ATTRIBUTE, servletContext);
+          if (contextPath != null) {
+            final String relativePath =
+                contextPath.startsWith(servletContext)
+                    ? contextPath.substring(servletContext.length())
+                    : contextPath;
+            ((AgentSpan) span).setTag(SERVLET_PATH, relativePath);
+            req.setAttribute(DD_SERVLET_PATH_ATTRIBUTE, relativePath);
+          }
+        }
       }
     }
   }
