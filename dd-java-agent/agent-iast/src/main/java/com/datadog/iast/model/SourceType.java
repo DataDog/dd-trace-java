37a2d219f46f8f41399c3472fba95fe32d445616
Manuel Álvarez Álvarez <manuel-alvarez-alvarez@users.noreply.github.com>
2022-11-23 12:31:06 +0100

    Align source types with the spec (#4299)

--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/model/SourceType.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/model/SourceType.java
@@ -6,21 +6,13 @@ public final class SourceType {
 
   public static final byte NONE = 0;
 
-  public static final byte REQUEST_QUERY_PARAMETER = 1;
-  static final String REQUEST_QUERY_PARAMETER_STRING = "http.url_details.queryString";
-  public static final byte REQUEST_PATH = 2;
-  static final String REQUEST_PATH_STRING = "http.url_details.path";
-  public static final byte REQUEST_PARAMETER_NAME = 3;
-  static final String REQUEST_PARAMETER_NAME_STRING = "http.param.name";
-  public static final byte REQUEST_PARAMETER_VALUE = 4;
-  static final String REQUEST_PARAMETER_VALUE_STRING = "http.param.value";
+  public static final byte REQUEST_PARAMETER_NAME = 1;
+  static final String REQUEST_PARAMETER_NAME_STRING = "http.request.parameter.name";
+  public static final byte REQUEST_PARAMETER_VALUE = 2;
+  static final String REQUEST_PARAMETER_VALUE_STRING = "http.request.parameter";
 
   public static String toString(final byte sourceType) {
     switch (sourceType) {
-      case REQUEST_QUERY_PARAMETER:
-        return REQUEST_QUERY_PARAMETER_STRING;
-      case REQUEST_PATH:
-        return REQUEST_PATH_STRING;
       case REQUEST_PARAMETER_NAME:
         return REQUEST_PARAMETER_NAME_STRING;
       case REQUEST_PARAMETER_VALUE:
