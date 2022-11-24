a147d134559e6acd5710d7585a53f22edc970b4c
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2022-11-24 14:24:13 +0100

    Add missing call sites for weak hash an cipher

--- dd-java-agent/instrumentation/java-security/src/test/groovy/test/WeakCipherTest.groovy
+++ dd-java-agent/instrumentation/java-security/src/test/groovy/test/WeakCipherTest.groovy
@@ -5,7 +5,9 @@ import datadog.trace.api.iast.IastModule
 import datadog.trace.api.iast.InstrumentationBridge
 import foo.bar.TestSuite
 
+import javax.crypto.Cipher
 import java.security.NoSuchAlgorithmException
+import java.security.Provider
 
 import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
 
@@ -34,6 +36,32 @@ class WeakCipherTest extends AgentTestRunner {
     1 * module.onCipherAlgorithm(_)
   }
 
+  def "test weak cipher instrumentation with provider"() {
+    setup:
+    IastModule module = Mock(IastModule)
+    InstrumentationBridge.registerIastModule(module)
+    final provider = providerFor('DES')
+
+    when:
+    new TestSuite().getCipherInstance("DES", provider)
+
+    then:
+    1 * module.onCipherAlgorithm(_)
+  }
+
+  def "test weak cipher instrumentation with provider string"() {
+    setup:
+    IastModule module = Mock(IastModule)
+    InstrumentationBridge.registerIastModule(module)
+    final provider = providerFor('DES')
+
+    when:
+    new TestSuite().getCipherInstance('DES', provider.getName())
+
+    then:
+    1 * module.onCipherAlgorithm(_)
+  }
+
   def "weak cipher instrumentation with null argument"() {
     setup:
     IastModule module = Mock(IastModule)
@@ -44,5 +72,10 @@ class WeakCipherTest extends AgentTestRunner {
 
     then:
     thrown NoSuchAlgorithmException
+  }
+
+  private static Provider providerFor(final String algo) {
+    final instance = Cipher.getInstance(algo)
+    return instance.getProvider()
   }
 }
