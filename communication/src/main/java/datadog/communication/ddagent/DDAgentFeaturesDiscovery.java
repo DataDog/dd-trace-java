40392fd109728402d8a7e468e928a06ed2283851
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2022-11-28 15:13:29 +0100

    Support for EVP Proxy in DDAgentFeaturesDiscovery

--- communication/src/main/java/datadog/communication/ddagent/DDAgentFeaturesDiscovery.java
+++ communication/src/main/java/datadog/communication/ddagent/DDAgentFeaturesDiscovery.java
@@ -41,6 +41,8 @@ public class DDAgentFeaturesDiscovery implements DroppingPolicy {
 
   public static final String V01_DATASTREAMS_ENDPOINT = "v0.1/pipeline_stats";
 
+  public static final String V2_EVP_PROXY_ENDPOINT = "evp_proxy/v2/";
+
   public static final String DATADOG_AGENT_STATE = "Datadog-Agent-State";
 
   public static final String DEBUGGER_ENDPOINT = "debugger/v1/input";
@@ -55,6 +57,7 @@ public class DDAgentFeaturesDiscovery implements DroppingPolicy {
   private final String[] configEndpoints = {V7_CONFIG_ENDPOINT};
   private final boolean metricsEnabled;
   private final String[] dataStreamsEndpoints = {V01_DATASTREAMS_ENDPOINT};
+  private final String[] evpProxyEndpoints = {V2_EVP_PROXY_ENDPOINT};
 
   private volatile String traceEndpoint;
   private volatile String metricsEndpoint;
@@ -63,6 +66,7 @@ public class DDAgentFeaturesDiscovery implements DroppingPolicy {
   private volatile String state;
   private volatile String configEndpoint;
   private volatile String debuggerEndpoint;
+  private volatile String evpProxyEndpoint;
   private volatile String version;
 
   private long lastTimeDiscovered;
@@ -90,6 +94,8 @@ public class DDAgentFeaturesDiscovery implements DroppingPolicy {
     state = null;
     configEndpoint = null;
     debuggerEndpoint = null;
+    dataStreamsEndpoint = null;
+    evpProxyEndpoint = null;
     version = null;
     lastTimeDiscovered = 0;
   }
@@ -149,11 +155,12 @@ public class DDAgentFeaturesDiscovery implements DroppingPolicy {
 
     if (log.isDebugEnabled()) {
       log.debug(
-          "discovered traceEndpoint={}, metricsEndpoint={}, supportsDropping={}, dataStreamsEndpoint={}, configEndpoint={}",
+          "discovered traceEndpoint={}, metricsEndpoint={}, supportsDropping={}, dataStreamsEndpoint={}, configEndpoint={}, evpProxyEndpoint={}",
           traceEndpoint,
           metricsEndpoint,
           supportsDropping,
           dataStreamsEndpoint,
+          evpProxyEndpoint,
           configEndpoint);
     }
   }
@@ -218,16 +225,20 @@ public class DDAgentFeaturesDiscovery implements DroppingPolicy {
         debuggerEndpoint = DEBUGGER_ENDPOINT;
       }
 
-      String foundDatastreamsEndpoint = null;
       for (String endpoint : dataStreamsEndpoints) {
         if (endpoints.contains(endpoint) || endpoints.contains("/" + endpoint)) {
-          foundDatastreamsEndpoint = endpoint;
+          dataStreamsEndpoint = endpoint;
           break;
         }
       }
 
-      // This is done outside of the loop to set dataStreamsEndpoint to null if not found
-      dataStreamsEndpoint = foundDatastreamsEndpoint;
+      for (String endpoint : evpProxyEndpoints) {
+        if (endpoints.contains(endpoint) || endpoints.contains("/" + endpoint)) {
+          evpProxyEndpoint = endpoint;
+          break;
+        }
+      }
+
 
       if (metricsEnabled) {
         Object canDrop = map.get("client_drop_p0s");
@@ -288,12 +299,20 @@ public class DDAgentFeaturesDiscovery implements DroppingPolicy {
     return dataStreamsEndpoint;
   }
 
+  public String getEvpProxyEndpoint() {
+    return evpProxyEndpoint;
+  }
+
   public HttpUrl buildUrl(String endpoint) {
     return agentBaseUrl.resolve(endpoint);
   }
 
   public boolean supportsDataStreams() {
     return dataStreamsEndpoint != null;
+  }
+
+  public boolean supportsEvpProxy() {
+    return evpProxyEndpoint != null;
   }
 
   public String getConfigEndpoint() {
