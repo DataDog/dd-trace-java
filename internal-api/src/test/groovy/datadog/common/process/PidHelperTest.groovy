a640d4570ec0d1331c0233a6f189c7072bc94807
Zarir Hamza <zarir.hamza@datadoghq.com>
2022-12-05 14:54:06 -0500

    Changes PidHelper test
    Creates PidHelper test as a spock test
    Removes old junit test
    Removes exclusion from code coverage

---
+++ internal-api/src/test/groovy/datadog/common/process/PidHelperTest.groovy
@@ -0,0 +1,10 @@
+package datadog.common.process
+
+import spock.lang.Specification
+
+class PidHelperTest extends Specification {
+  def "Expect PID to be present since we run tests in systems where we can load it"() {
+    expect:
+    PidHelper.PID > 0
+  }
+}
