214755784d770aada3284d843ad21517d8dd5fd0
Dylan Yang <dylan.yang@datadoghq.com>
2022-12-06 17:03:24 -0500

    capture lambda response (#4380)

--- dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
+++ dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
@@ -785,8 +785,8 @@ public class CoreTracer implements AgentTracer.TracerAPI {
   }
 
   @Override
-  public void notifyExtensionEnd(AgentSpan span, boolean isError) {
-    LambdaHandler.notifyEndInvocation(span, isError);
+  public void notifyExtensionEnd(AgentSpan span, Object result, boolean isError) {
+    LambdaHandler.notifyEndInvocation(span, result, isError);
   }
 
   private final RatelimitedLogger rlLog = new RatelimitedLogger(log, 1, MINUTES);
