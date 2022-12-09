31ce550dd207198e53d45204f608270b5b613a3f
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2022-12-09 01:18:30 +0000

    coverage

--- internal-api/src/test/groovy/datadog/trace/api/InstrumenterConfigTest.groovy
+++ internal-api/src/test/groovy/datadog/trace/api/InstrumenterConfigTest.groovy
@@ -14,8 +14,17 @@ class InstrumenterConfigTest extends DDSpecification {
     System.setProperty("dd.integration.test-prop.enabled", "true")
     System.setProperty("dd.integration.disabled-prop.enabled", "false")
 
+    environmentVariables.set("DD_INTEGRATION_ORDER_MATCHING_SHORTCUT_ENABLED", "false")
+    environmentVariables.set("DD_INTEGRATION_TEST_ENV_MATCHING_SHORTCUT_ENABLED", "true")
+    environmentVariables.set("DD_INTEGRATION_DISABLED_ENV_MATCHING_SHORTCUT_ENABLED", "false")
+
+    System.setProperty("dd.integration.order.matching.shortcut.enabled", "true")
+    System.setProperty("dd.integration.test-prop.matching.shortcut.enabled", "true")
+    System.setProperty("dd.integration.disabled-prop.matching.shortcut.enabled", "false")
+
     expect:
     InstrumenterConfig.get().isIntegrationEnabled(integrationNames, defaultEnabled) == expected
+    InstrumenterConfig.get().isIntegrationShortcutMatchingEnabled(integrationNames, defaultEnabled) == expected
 
     where:
     // spotless:off
