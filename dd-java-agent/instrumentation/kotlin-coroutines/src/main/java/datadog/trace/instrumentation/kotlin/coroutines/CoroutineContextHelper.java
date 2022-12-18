f285a1e72db25c4b780fd6706c68c0f0fd257fbc
monosoul <Kloz.Klaud@gmail.com>
2022-12-18 17:47:04 +0100

    Refactor CoroutineContextHelper a bit and add a comment
    Signed-off-by: monosoul <Kloz.Klaud@gmail.com>

--- dd-java-agent/instrumentation/kotlin-coroutines/src/main/java/datadog/trace/instrumentation/kotlin/coroutines/CoroutineContextHelper.java
+++ dd-java-agent/instrumentation/kotlin-coroutines/src/main/java/datadog/trace/instrumentation/kotlin/coroutines/CoroutineContextHelper.java
@@ -1,13 +1,24 @@
 package datadog.trace.instrumentation.kotlin.coroutines;
 
 import kotlin.coroutines.CoroutineContext;
+import kotlinx.coroutines.CoroutineName;
 import kotlinx.coroutines.Job;
 import org.jetbrains.annotations.Nullable;
 
 public class CoroutineContextHelper {
+  /*
+  IntelliJ shows a warning here for Job and CoroutineName being out of bounds, but that's not true, the class compiles.
+   */
+
   @Nullable
-  @SuppressWarnings({"rawtypes", "unchecked"})
+  @SuppressWarnings("unchecked")
   public static Job getJob(final CoroutineContext context) {
-    return (Job) context.get((CoroutineContext.Key) Job.Key);
+    return context.get((CoroutineContext.Key<Job>) Job.Key);
+  }
+
+  @Nullable
+  @SuppressWarnings("unchecked")
+  public static CoroutineName getName(final CoroutineContext context) {
+    return context.get((CoroutineContext.Key<CoroutineName>) CoroutineName.Key);
   }
 }
