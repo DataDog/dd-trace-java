be78939b9059e13db3af160d4f95a59b6b29641c
monosoul <Kloz.Klaud@gmail.com>
2022-12-19 14:55:19 +0100

    Remove unused method
    Signed-off-by: monosoul <Kloz.Klaud@gmail.com>

--- dd-java-agent/instrumentation/kotlin-coroutines/src/main/java/datadog/trace/instrumentation/kotlin/coroutines/CoroutineContextHelper.java
+++ dd-java-agent/instrumentation/kotlin-coroutines/src/main/java/datadog/trace/instrumentation/kotlin/coroutines/CoroutineContextHelper.java
@@ -1,24 +1,17 @@
 package datadog.trace.instrumentation.kotlin.coroutines;
 
 import kotlin.coroutines.CoroutineContext;
-import kotlinx.coroutines.CoroutineName;
 import kotlinx.coroutines.Job;
 import org.jetbrains.annotations.Nullable;
 
 public class CoroutineContextHelper {
   /*
-  IntelliJ shows a warning here for Job and CoroutineName being out of bounds, but that's not true, the class compiles.
+  IntelliJ shows a warning here for Job being out of bounds, but that's not true, the class compiles.
    */
 
   @Nullable
   @SuppressWarnings("unchecked")
   public static Job getJob(final CoroutineContext context) {
     return context.get((CoroutineContext.Key<Job>) Job.Key);
-  }
-
-  @Nullable
-  @SuppressWarnings("unchecked")
-  public static CoroutineName getName(final CoroutineContext context) {
-    return context.get((CoroutineContext.Key<CoroutineName>) CoroutineName.Key);
   }
 }
