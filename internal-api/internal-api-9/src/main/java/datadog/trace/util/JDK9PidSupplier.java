70875ff9c58b08372b7498121088428c99bee81c
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2022-12-22 09:31:46 +0000

    Make PidHelper fallback lazy (#4467)

--- internal-api/internal-api-9/src/main/java/datadog/trace/util/JDK9PidSupplier.java
+++ internal-api/internal-api-9/src/main/java/datadog/trace/util/JDK9PidSupplier.java
@@ -5,16 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /** Use standard API to retrieve PID on Java9+. */
-public final class JDK9PidSupplier implements Supplier<Long> {
+public final class JDK9PidSupplier implements Supplier<String> {
   private static final Logger log = LoggerFactory.getLogger(JDK9PidSupplier.class);
 
   @Override
-  public Long get() {
+  public String get() {
     try {
-      return ProcessHandle.current().pid();
+      return Long.toString(ProcessHandle.current().pid());
     } catch (Throwable e) {
       log.debug("Cannot get PID through JVM API", e);
-      return null;
+      return "";
     }
   }
 }
--- internal-api/src/test/java/datadog/trace/util/PidHelperForkedTest.java
+++ internal-api/src/test/java/datadog/trace/util/PidHelperForkedTest.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class PidHelperTest {
+public class PidHelperForkedTest {
   @Test
   public void testPidCanBeSupplied() {
-    PidHelper.supplyIfAbsent(() -> 12345L);
+    PidHelper.Fallback.set(() -> "12345");
     assertEquals("12345", PidHelper.getPid(), "Expect PID to match supplied value");
-    PidHelper.supplyIfAbsent(() -> 67890L);
+    PidHelper.Fallback.set(() -> "67890");
     assertEquals("12345", PidHelper.getPid(), "Expect PID to not change once set");
   }
 }
