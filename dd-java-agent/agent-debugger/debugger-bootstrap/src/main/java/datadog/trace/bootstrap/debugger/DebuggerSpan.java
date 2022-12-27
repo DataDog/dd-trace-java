75204840f8267554171b4e6d29456d0e257eca1f
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2022-12-27 17:17:41 +0100

    Set error in instrumented span on exception
    When exception is thrown during the instrumented span, we store the
    throwable inside the span for reporting it as error span.

--- dd-java-agent/agent-debugger/debugger-bootstrap/src/main/java/datadog/trace/bootstrap/debugger/DebuggerSpan.java
+++ dd-java-agent/agent-debugger/debugger-bootstrap/src/main/java/datadog/trace/bootstrap/debugger/DebuggerSpan.java
@@ -3,11 +3,16 @@ package datadog.trace.bootstrap.debugger;
 public interface DebuggerSpan {
   void finish();
 
+  void setError(Throwable t);
+
   DebuggerSpan NOOP_SPAN = new NoopSpan();
 
   class NoopSpan implements DebuggerSpan {
 
     @Override
     public void finish() {}
+
+    @Override
+    public void setError(Throwable t) {}
   }
 }
