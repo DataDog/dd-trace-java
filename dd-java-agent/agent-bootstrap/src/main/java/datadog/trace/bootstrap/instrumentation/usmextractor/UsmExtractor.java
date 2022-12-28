e663854f029d8ed8bd0a1fb2abf0c41f7f1b0047
Val Pliskin <valeri.pliskin@datadoghq.com>
2022-12-28 15:10:54 +0200

    - replaced println with log

--- dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/usmextractor/UsmExtractor.java
+++ dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/usmextractor/UsmExtractor.java
@@ -1,16 +1,14 @@
 package datadog.trace.bootstrap.instrumentation.usmextractor;
 
 import sun.security.ssl.SSLSocketImpl;
-import java.util.function.Function;
 import com.sun.jna.Library;
 import com.sun.jna.NativeLong;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 
 
+//TODO: maybe move to a separate module: "agent-usm" (similar to agent-iast, agent-debugger, etc... under the dd-java-agent)
 public class UsmExtractor {
-
-  static final NativeLong REQUEST_CODE = new NativeLong(0xda7ad09);
   public interface CLibrary extends Library {
     CLibrary Instance = (CLibrary) Native.load("c", CLibrary.class);
 
@@ -18,12 +16,9 @@ public class UsmExtractor {
   }
 
   public static void send(UsmMessage message) {
-    System.out.println("inside foo: socket");
-    if (!message.validate()){
-      System.out.println("invalid message");
+    if (message.validate()){
+      NativeLong res = CLibrary.Instance.ioctl(new NativeLong(0), UsmMessage.USM_IOCTL_ID, Pointer.nativeValue(message.getBufferPtr()));
     }
-    NativeLong res = CLibrary.Instance.ioctl(new NativeLong(0), UsmMessage.USM_IOCTL_ID, Pointer.nativeValue(message.getBufferPtr()));
-    System.out.println("ioctl result: " + res.toString());
   }
 
 }
