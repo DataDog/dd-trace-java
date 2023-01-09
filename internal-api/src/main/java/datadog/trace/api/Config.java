6068ea3c5349eb1909902e5ac954e0fb8bfc4e91
jmeunier28 <jojo.meunier@datadoghq.com>
2023-01-09 17:10:32 -0500

    Implement DBM-APM feature for JDBC instrumentation

--- internal-api/src/main/java/datadog/trace/api/Config.java
+++ internal-api/src/main/java/datadog/trace/api/Config.java
@@ -17,6 +17,7 @@ import static datadog.trace.api.ConfigDefaults.DEFAULT_CWS_TLS_REFRESH;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_DATA_STREAMS_ENABLED;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_DB_CLIENT_HOST_SPLIT_BY_INSTANCE;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_DB_CLIENT_HOST_SPLIT_BY_INSTANCE_TYPE_SUFFIX;
+import static datadog.trace.api.ConfigDefaults.DEFAULT_DB_CLIENT_SQL_COMMENT_INJECTION_MODE;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_DEBUGGER_CLASSFILE_DUMP_ENABLED;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_DEBUGGER_DIAGNOSTICS_INTERVAL;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_DEBUGGER_ENABLED;
@@ -208,6 +209,7 @@ import static datadog.trace.api.config.RemoteConfigConfig.REMOTE_CONFIG_TARGETS_
 import static datadog.trace.api.config.RemoteConfigConfig.REMOTE_CONFIG_URL;
 import static datadog.trace.api.config.TraceInstrumentationConfig.DB_CLIENT_HOST_SPLIT_BY_INSTANCE;
 import static datadog.trace.api.config.TraceInstrumentationConfig.DB_CLIENT_HOST_SPLIT_BY_INSTANCE_TYPE_SUFFIX;
+import static datadog.trace.api.config.TraceInstrumentationConfig.DB_CLIENT_SQL_COMMENT_INJECTION_MODE;
 import static datadog.trace.api.config.TraceInstrumentationConfig.GRPC_CLIENT_ERROR_STATUSES;
 import static datadog.trace.api.config.TraceInstrumentationConfig.GRPC_IGNORED_INBOUND_METHODS;
 import static datadog.trace.api.config.TraceInstrumentationConfig.GRPC_IGNORED_OUTBOUND_METHODS;
@@ -543,6 +545,8 @@ public class Config {
   private final String remoteConfigTargetsKeyId;
   private final String remoteConfigTargetsKey;
 
+  private final String sqlCommenterInjectionMode;
+
   private final boolean debuggerEnabled;
   private final int debuggerUploadTimeout;
   private final int debuggerUploadFlushInterval;
@@ -875,6 +879,10 @@ public class Config {
         configProvider.getBoolean(
             DB_CLIENT_HOST_SPLIT_BY_INSTANCE_TYPE_SUFFIX,
             DEFAULT_DB_CLIENT_HOST_SPLIT_BY_INSTANCE_TYPE_SUFFIX);
+
+    sqlCommenterInjectionMode =
+        configProvider.getString(
+            DB_CLIENT_SQL_COMMENT_INJECTION_MODE, DEFAULT_DB_CLIENT_SQL_COMMENT_INJECTION_MODE);
 
     splitByTags = tryMakeImmutableSet(configProvider.getList(SPLIT_BY_TAGS));
 
@@ -2568,6 +2576,10 @@ public class Config {
         Collections.singletonList(settingName), "", settingSuffix, defaultEnabled);
   }
 
+  public String getSqlCommentInjectionMode() {
+    return sqlCommenterInjectionMode;
+  }
+
   private void logIgnoredSettingWarning(
       String setting, String overridingSetting, String overridingSuffix) {
     log.warn(
@@ -2977,6 +2989,8 @@ public class Config {
         + dbClientSplitByInstance
         + ", dbClientSplitByInstanceTypeSuffix="
         + dbClientSplitByInstanceTypeSuffix
+        + ", sqlCommenterInjectionMode="
+        + sqlCommenterInjectionMode
         + ", splitByTags="
         + splitByTags
         + ", scopeDepthLimit="
