243db7081ad01e86dc8aa4d9307e15318c00cc70
Alejandro González García <alejandro.gonzalez@datadoghq.com>
2023-01-11 09:33:39 +0100

    String taint tracking - repeat method (#3997)
    What Does This Do
    Adds all the instrumentation needed to perform taint tacking in repeat operation for String objects
    
    Motivation
    IAST requires to track all modifications that happen to strings in the code, this PR implements the repeat operation for String class
    
    Additional Notes
    The String repeat method is available since JAVA 11

---
+++ dd-java-agent/instrumentation/java-lang/java-lang-11/src/main/java/datadog/trace/instrumentation/java/lang/jdk11/StringCallSite.java
@@ -0,0 +1,25 @@
+package datadog.trace.instrumentation.java.lang.jdk11;
+
+import datadog.trace.agent.tooling.csi.CallSite;
+import datadog.trace.api.iast.IastAdvice;
+import datadog.trace.api.iast.InstrumentationBridge;
+import datadog.trace.api.iast.propagation.StringModule;
+
+@CallSite(spi = IastAdvice.class, minJavaVersion = 11)
+public class StringCallSite {
+  @CallSite.After("java.lang.String java.lang.String.repeat(int)")
+  public static String afterRepeat(
+      @CallSite.This final String self,
+      @CallSite.Argument final int count,
+      @CallSite.Return final String result) {
+    final StringModule module = InstrumentationBridge.STRING;
+    try {
+      if (module != null) {
+        module.onStringRepeat(self, count, result);
+      }
+    } catch (final Throwable e) {
+      module.onUnexpectedException("afterRepeat threw", e);
+    }
+    return result;
+  }
+}
