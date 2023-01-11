0dad8d45068a3ef92ccd9b5ece1f7bc9125c1146
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2023-01-11 12:00:50 +0100

    update internal logic to use compiler plugin client

--- internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/ci/source/CompilerAidedSourcePathResolver.java
+++ internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/ci/source/CompilerAidedSourcePathResolver.java
@@ -1,33 +1,23 @@
 package datadog.trace.bootstrap.instrumentation.ci.source;
 
-import java.lang.reflect.Field;
+import datadog.compiler.CompilerUtils;
 import javax.annotation.Nullable;
 
-// FIXME add documentation
 public class CompilerAidedSourcePathResolver implements SourcePathResolver {
-
-  public static final String SOURCE_PATH_FIELD_NAME =
-      "__datadog_sourcePath"; // FIXME possible to make a reference to compiler plugin?
 
   @Nullable
   @Override
   public String getSourcePath(Class<?> c) {
     try {
-      return tryGetSourcePath(c);
+      return CompilerUtils.getSourcePath(c);
     } catch (Exception e) {
       return null;
     }
   }
 
-  private String tryGetSourcePath(Class<?> c) throws NoSuchFieldException, IllegalAccessException {
-    Field sourceFilePath = c.getDeclaredField(SOURCE_PATH_FIELD_NAME);
-    sourceFilePath.setAccessible(true);
-    return (String) sourceFilePath.get(c);
-  }
-
   public boolean isSourcePathInfoAvailable(Class<?> c) {
     try {
-      return tryGetSourcePath(c) != null;
+      return CompilerUtils.getSourcePath(c) != null;
     } catch (Exception e) {
       return false;
     }
