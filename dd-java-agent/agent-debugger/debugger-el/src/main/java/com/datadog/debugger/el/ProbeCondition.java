0f95511bd1e3e81d7c71d7911531bcd107349afc
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2023-01-12 15:30:16 +0100

    Add smoke tests for conditions & logs
    validate that conditions and log templates are working

--- dd-java-agent/agent-debugger/debugger-el/src/main/java/com/datadog/debugger/el/ProbeCondition.java
+++ dd-java-agent/agent-debugger/debugger-el/src/main/java/com/datadog/debugger/el/ProbeCondition.java
@@ -30,6 +30,10 @@ public final class ProbeCondition implements DebuggerScript<Boolean> {
     return dslExpression;
   }
 
+  public WhenExpression getWhen() {
+    return when;
+  }
+
   public static class ProbeConditionJsonAdapter extends JsonAdapter<ProbeCondition> {
     @Override
     public ProbeCondition fromJson(JsonReader reader) throws IOException {
