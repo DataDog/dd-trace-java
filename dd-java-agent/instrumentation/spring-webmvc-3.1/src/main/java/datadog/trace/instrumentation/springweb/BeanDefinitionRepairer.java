87ed737415bd05a5f18d92ad169821065d8c5784
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2023-01-16 14:15:25 +0000

    Use ConcurrentHashMap to track registered bean classes; remove prefix check

--- dd-java-agent/instrumentation/spring-webmvc-3.1/src/main/java/datadog/trace/instrumentation/springweb/BeanDefinitionRepairer.java
+++ dd-java-agent/instrumentation/spring-webmvc-3.1/src/main/java/datadog/trace/instrumentation/springweb/BeanDefinitionRepairer.java
@@ -1,16 +1,14 @@
 package datadog.trace.instrumentation.springweb;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 
 /**
  * Repairs Datadog injected bean definitions by restoring missing class references at resolve time.
  */
 public final class BeanDefinitionRepairer {
-  private static final Map<String, Class<?>> ddBeanClasses =
-      Collections.synchronizedMap(new HashMap<>());
+  private static final Map<String, Class<?>> ddBeanClasses = new ConcurrentHashMap<>();
 
   public static void register(Class<?> beanClass) {
     ddBeanClasses.put(beanClass.getName(), beanClass);
@@ -19,7 +17,7 @@ public final class BeanDefinitionRepairer {
   public static void repair(RootBeanDefinition beanDefinition) {
     if (!beanDefinition.hasBeanClass()) {
       String className = beanDefinition.getBeanClassName();
-      if (null != className && className.startsWith("datadog.trace.instrumentation.")) {
+      if (null != className) {
         Class<?> beanClass = ddBeanClasses.get(className);
         if (null != beanClass) {
           beanDefinition.setBeanClass(beanClass);
