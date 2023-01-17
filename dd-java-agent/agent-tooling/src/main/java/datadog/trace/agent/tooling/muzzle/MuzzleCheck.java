952cfc63d62eab6d3657fdf22a729d84e1ffe033
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2023-01-17 11:59:06 +0000

    Miscellaneous matcher cleanup (#4551)
    * Allow use of MuzzleCheck as a plain class-loader matcher
    * Refactor InjectContextFieldMatcher into separate re-usable pieces:
      the activation matcher and the matcher to decide when it is safe to
      inject the context field in a type. This also lets us reduce that
      matcher's parameters down to just what it uses, which means we can
      avoid passing in null's when we don't have the unused arguments.

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/muzzle/MuzzleCheck.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/muzzle/MuzzleCheck.java
@@ -3,10 +3,11 @@ package datadog.trace.agent.tooling.muzzle;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterState;
 import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MuzzleCheck {
+public class MuzzleCheck implements ElementMatcher<ClassLoader> {
   private static final Logger log = LoggerFactory.getLogger(MuzzleCheck.class);
 
   private final int instrumentationId;
@@ -21,7 +22,7 @@ public class MuzzleCheck {
     this.runtimeMuzzleReferences = instrumenter.runtimeMuzzleReferences();
   }
 
-  public boolean allow(ClassLoader classLoader) {
+  public boolean matches(ClassLoader classLoader) {
     Boolean applicable = InstrumenterState.isApplicable(classLoader, instrumentationId);
     if (null != applicable) {
       return applicable;
