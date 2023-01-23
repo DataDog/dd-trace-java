84f8c74ceefb75bcd8df25b5fb5a1292bf2fa17b
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2023-01-23 12:44:11 +0100

    add more test cases

--- internal-api/src/test/groovy/datadog/trace/bootstrap/instrumentation/ci/source/RepoIndexSourcePathResolverTest.groovy
+++ internal-api/src/test/groovy/datadog/trace/bootstrap/instrumentation/ci/source/RepoIndexSourcePathResolverTest.groovy
@@ -6,6 +6,7 @@ import groovy.transform.PackageScope
 import spock.lang.Specification
 
 import java.nio.file.Files
+import java.nio.file.Path
 
 class RepoIndexSourcePathResolverTest extends Specification {
 
@@ -71,14 +72,69 @@ class RepoIndexSourcePathResolverTest extends Specification {
     sourcePathResolver.getSourcePath(PackagePrivateClass) == expectedSourcePath
   }
 
-  private String givenSourceFile(Class c, String sourceRoot) {
-    def classPath = fileSystem.getPath(generateSourceFileName(c, sourceRoot))
-    sourceRootResolver.getSourceRoot(classPath) >> fileSystem.getPath(sourceRoot)
+  def "test source path for non-indexed class"() {
+    setup:
+
+    when:
+    def sourcePathResolver = new RepoIndexSourcePathResolver(repoRoot, sourceRootResolver, fileSystem)
+
+    then:
+    sourcePathResolver.getSourcePath(RepoIndexSourcePathResolver) == null
+  }
+
+  def "test source path for non-indexed package-private class"() {
+    setup:
+
+    when:
+    def sourcePathResolver = new RepoIndexSourcePathResolver(repoRoot, sourceRootResolver, fileSystem)
+
+    then:
+    sourcePathResolver.getSourcePath(PackagePrivateClass) == null
+  }
+
+  def "test file-indexing failure"() {
+    setup:
+    def classPath = fileSystem.getPath(generateSourceFileName(RepoIndexSourcePathResolverTest, repoRoot))
+    sourceRootResolver.getSourceRoot(classPath) >> { throw new IOException() }
 
     Files.createDirectories(classPath.getParent())
     Files.write(classPath, "STUB CLASS BODY".getBytes())
 
+    when:
+    def sourcePathResolver = new RepoIndexSourcePathResolver(repoRoot, sourceRootResolver, fileSystem)
+
+    then:
+    sourcePathResolver.getSourcePath(RepoIndexSourcePathResolverTest) == null
+  }
+
+  def "test source path resolution for repo with multiple files"() {
+    setup:
+    def expectedSourcePathOne = givenSourceFile(RepoIndexSourcePathResolverTest, repoRoot + "/src")
+    def expectedSourcePathTwo = givenSourceFile(RepoIndexSourcePathResolver, repoRoot + "/src")
+
+    givenRepoFile(fileSystem.getPath(repoRoot, "README.md"))
+
+    when:
+    def sourcePathResolver = new RepoIndexSourcePathResolver(repoRoot, sourceRootResolver, fileSystem)
+
+    then:
+    sourcePathResolver.getSourcePath(RepoIndexSourcePathResolverTest) == expectedSourcePathOne
+    sourcePathResolver.getSourcePath(InnerClass) == expectedSourcePathOne
+    sourcePathResolver.getSourcePath(RepoIndexSourcePathResolver) == expectedSourcePathTwo
+  }
+
+  private String givenSourceFile(Class c, String sourceRoot) {
+    def classPath = fileSystem.getPath(generateSourceFileName(c, sourceRoot))
+    sourceRootResolver.getSourceRoot(classPath) >> fileSystem.getPath(sourceRoot)
+
+    givenRepoFile(classPath)
+
     return fileSystem.getPath(repoRoot).relativize(classPath).toString()
+  }
+
+  private static void givenRepoFile(Path file) {
+    Files.createDirectories(file.getParent())
+    Files.write(file, "STUB FILE BODY".getBytes())
   }
 
   private static String generateSourceFileName(Class c, String sourceRoot) {
