0459137c0adb20b756b51db6092030e9faa89515
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2023-01-25 11:31:20 +0100

    fix tests

--- dd-java-agent/instrumentation/junit-4.10/src/test/groovy/JUnit4DecoratorTest.groovy
+++ dd-java-agent/instrumentation/junit-4.10/src/test/groovy/JUnit4DecoratorTest.groovy
@@ -1,5 +1,9 @@
 import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.api.DisableTestTrace
+import datadog.trace.api.ci.InstrumentationBridge
+import datadog.trace.bootstrap.instrumentation.ci.codeowners.Codeowners
+import datadog.trace.bootstrap.instrumentation.ci.source.MethodLinesResolver
+import datadog.trace.bootstrap.instrumentation.ci.source.SourcePathResolver
 import datadog.trace.instrumentation.junit4.JUnit4Decorator
 import org.example.TestDisableTestTrace
 import org.example.TestSucceed
@@ -8,6 +12,12 @@ import spock.lang.Shared
 
 @DisableTestTrace(reason = "avoid self-tracing")
 class JUnit4DecoratorTest extends AgentTestRunner {
+
+  def setupSpec() {
+    InstrumentationBridge.setCodeownersFactory { repoRoot -> Stub(Codeowners) }
+    InstrumentationBridge.setSourcePathResolverFactory { repoRoot -> Stub(SourcePathResolver) }
+    InstrumentationBridge.setMethodLinesResolverFactory { -> Stub(MethodLinesResolver) }
+  }
 
   @Shared
   def decorator = new JUnit4Decorator()
