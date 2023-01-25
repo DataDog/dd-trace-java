pvishwa3/cito-trace-java

implementation of Telemetry API modification to enable Exception reporting

internal-api/src/main/java/datadog/trace/api/telemetry/LogCollector.java
@@ -1,0 +1,115 @@
+package datadog.trace.api.telemetry;
+
+import datadog.trace.util.stacktrace.StackUtils;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import org.slf4j.helpers.MessageFormatter;
+
+public class LogCollector {
+  public static final int MAX_ENTRIES = 10000;
+  private static int overflowedEntryCount = 0;
+  private static boolean enabled = false;
+  private static LinkedHashSet<TelemetryLogEntry> logEntries = new LinkedHashSet<>();
+  private static String[] packageList = {"datadog.", "com.datadog.", "java.", "javax."};
+  private static final String RET = "\r\n";
+
+  public static class Holder {
+    public static final LogCollector INSTANCE = new LogCollector();
+  }
+
+  public static LogCollector get() {
+    return Holder.INSTANCE;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public boolean getEnabled() {
+    return enabled;
+  }
+
+  public synchronized void addLogEntry(
+      String msg,
+      String level,
+      Throwable t,
+      String format,
+      Object arg1,
+      Object arg2,
+      Object[] args) {
+    if (null == msg && null == format) {
+      return;
+    }
+    if (logEntries.size() >= MAX_ENTRIES) {
+      overflowedEntryCount++;
+      return;
+    }
+
+    boolean isDDCode = false;
+    TelemetryLogEntry log = new TelemetryLogEntry();
+    if (null != format) {
+      t = MessageFormatter.getThrowableCandidate(args);
+    }
+    if (null != t) {
+      isDDCode = isDataDogCode(t);
+      log.setStackTrace(renderStackTrace(t, isDDCode));
+    }
+    if (null != msg) {
+      log.setMessage(msg);
+    } else if (isDDCode && null != format) {
+      log.setMessage(MessageFormatter.arrayFormat(format, args).getMessage());
+    }
+    log.setLevel(level);
+    logEntries.add(log);
+  }
+
+  private String renderStackTrace(Throwable t, boolean isDataDogCode) {
+    StringBuilder stackTrace = new StringBuilder();
+    stackTrace.append(t.getClass().getCanonicalName());
+
+    String firstStackElementClassName = t.getStackTrace()[0].getClassName();
+    if (isDataDogCode) {
+      stackTrace.append(": ");
+      stackTrace.append(t.getMessage());
+    }
+    stackTrace.append(RET);
+
+    StackUtils.filterPackagesIn(t, packageList);
+    for (StackTraceElement stackTraceElement : t.getStackTrace()) {
+      stackTrace.append("  at ");
+      stackTrace.append(stackTraceElement.toString());
+      stackTrace.append(RET);
+    }
+    return stackTrace.toString();
+  }
+
+  boolean isDataDogCode(Throwable t) {
+    String firstStackElementClassName = t.getStackTrace()[0].getClassName();
+    return firstStackElementClassName.startsWith("datadog.")
+        || firstStackElementClassName.startsWith("com.datadog.");
+  }
+
+  public synchronized List<TelemetryLogEntry> drain() {
+    if (logEntries.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    ArrayList<TelemetryLogEntry> drainedEntries = new ArrayList<>();
+
+    drainedEntries.addAll(logEntries);
+
+    if (overflowedEntryCount > 0) {
+      TelemetryLogEntry overflowErrorEntry = new TelemetryLogEntry();
+      overflowErrorEntry.setMessage(
+          "Omitted " + overflowedEntryCount + " entries due to overflowing");
+      overflowErrorEntry.setLevel("ERROR");
+      drainedEntries.add(overflowErrorEntry);
+    }
+
+    overflowedEntryCount = 0;
+    logEntries.clear();
+    return drainedEntries;
+  }
+}

telemetry/src/test/groovy/datadog/telemetry/log/TelemetryLogTest.groovy
@@ -1,0 +1,99 @@
+package datadog.telemetry.log
+
+import datadog.slf4j.impl.StaticLoggerBinder
+import datadog.telemetry.TelemetryService
+import datadog.trace.api.telemetry.LogCollector
+import datadog.trace.api.telemetry.TelemetryLogEntry
+import datadog.trace.logging.ddlogger.DDLogger
+import datadog.trace.test.util.DDSpecification
+import org.slf4j.Logger
+
+class TelemetryLogTest extends DDSpecification {
+  LogPeriodicAction periodicAction = new LogPeriodicAction()
+  TelemetryService telemetryService = Mock()
+  Logger logger
+
+  @Override
+  void setup(){
+    injectSysConfig("dd.instrumentation.telemetry.debug", "true")
+    logger = StaticLoggerBinder.getSingleton().getLoggerFactory().getLogger("pepe")
+    LogCollector.get().setEnabled(true)
+    LogCollector.get().drain()
+  }
+
+  @Override
+  void cleanup(){
+    LogCollector.get().drain()
+  }
+
+  void 'test static binder'(){
+    when:
+    logger.debug("Exception Message", new Exception("Exception Message"))
+    periodicAction.doIteration(telemetryService)
+
+    then:
+    1 * telemetryService.addLogEntry( { TelemetryLogEntry logEntry ->
+      logEntry.message == 'Exception Message'
+    } )
+    0 * _._
+  }
+
+
+  void 'test marker debugging'(){
+    when:
+
+    logger.debug(DDLogger.SEND_TELEMETRY, "Debug Message")
+    periodicAction.doIteration(telemetryService)
+
+    then:
+    1 * telemetryService.addLogEntry( { TelemetryLogEntry exception ->
+      exception.message == 'Debug Message'
+    } )
+    0 * _._
+  }
+
+  void 'test debugging with no marker'(){
+    when:
+    logger.debug("Debug Message")
+    periodicAction.doIteration(telemetryService)
+
+    then:
+    0 * _._
+  }
+
+  void 'test overflowed entries'(){
+    setup:
+    List<TelemetryLogEntry> entries
+
+    when:
+    for (int i=0; i< LogCollector.MAX_ENTRIES + 10; i++){
+      logger.debug(DDLogger.SEND_TELEMETRY, "Debug Message " + i)
+    }
+    entries = LogCollector.get().drain()
+
+    then:
+    entries.size() == LogCollector.MAX_ENTRIES + 1
+    entries.get(LogCollector.MAX_ENTRIES).getMessage().contains("Omitted")
+    0 * _._
+  }
+
+  void 'test rate limiting of repeated log messages'(){
+    setup:
+    List<TelemetryLogEntry> entries
+
+    when:
+    for (int i=0; i< LogCollector.MAX_ENTRIES + 10; i++){
+      logger.debug(DDLogger.SEND_TELEMETRY, "Debug Message 1")
+    }
+    for (int i=0; i< LogCollector.MAX_ENTRIES + 10; i++){
+      logger.debug(DDLogger.SEND_TELEMETRY, "Debug Message 2")
+    }
+    entries = LogCollector.get().drain()
+
+    then:
+    entries.size() == 2
+    entries.get(0).getMessage().equals("Debug Message 1")
+    entries.get(1).getMessage().equals("Debug Message 2")
+    0 * _._
+  }
+}

DataDog/dd-trace-java

internal-api/src/main/java/datadog/trace/api/telemetry/LogCollector.java
@@ -1,0 +1,130 @@
+package datadog.trace.api.telemetry;
+
+import datadog.trace.util.stacktrace.StackUtils;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import org.slf4j.helpers.MessageFormatter;
+
+public class LogCollector {
+  public static final int MAX_ENTRIES = 10000;
+  private static int overflowedEntryCount = 0;
+  private static boolean enabled = false;
+  public static boolean debugEnabled = false;
+  private static LinkedHashSet<TelemetryLogEntry> logEntries = new LinkedHashSet<>();
+  private static String[] packageList = {"datadog.", "com.datadog.", "java.", "javax."};
+  private static final String RET = "\r\n";
+
+  public static class Holder {
+    public static final LogCollector INSTANCE = new LogCollector();
+  }
+
+  public static LogCollector get() {
+    return Holder.INSTANCE;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public void setDebugEnabled(boolean enabled) {
+    debugEnabled = enabled;
+  }
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public boolean isDebugEnabled() {
+    return debugEnabled;
+  }
+
+  public synchronized void addLogEntry(
+      String msg,
+      String level,
+      Throwable t,
+      String format,
+      Object arg1,
+      Object arg2,
+      Object[] args) {
+    if (null == msg && null == format) {
+      return;
+    }
+    if (logEntries.size() >= MAX_ENTRIES) {
+      overflowedEntryCount++;
+      return;
+    }
+
+    boolean isDDCode = false;
+    TelemetryLogEntry log = new TelemetryLogEntry();
+    if (null != format) {
+      t = MessageFormatter.getThrowableCandidate(args);
+    }
+    if (null != t) {
+      isDDCode = isDataDogCode(t);
+      log.setStackTrace(renderStackTrace(t, isDDCode));
+    }
+    if (null != msg) {
+      log.setMessage(msg);
+    } else {
+      if (null != format) {
+        if (isDDCode) {
+          log.setMessage(MessageFormatter.arrayFormat(format, args).getMessage());
+        } else {
+          log.setMessage(format);
+        }
+      }
+    }
+    log.setLevel(level);
+    logEntries.add(log);
+  }
+
+  private String renderStackTrace(Throwable t, boolean isDataDogCode) {
+    StringBuilder stackTrace = new StringBuilder();
+    stackTrace.append(t.getClass().getCanonicalName());
+
+    String firstStackElementClassName = t.getStackTrace()[0].getClassName();
+    if (isDataDogCode) {
+      stackTrace.append(": ");
+      stackTrace.append(t.getMessage());
+    }
+    stackTrace.append(RET);
+
+    StackUtils.filterPackagesIn(t, packageList);
+    for (StackTraceElement stackTraceElement : t.getStackTrace()) {
+      stackTrace.append("  at ");
+      stackTrace.append(stackTraceElement.toString());
+      stackTrace.append(RET);
+    }
+    return stackTrace.toString();
+  }
+
+  boolean isDataDogCode(Throwable t) {
+    String firstStackElementClassName = t.getStackTrace()[0].getClassName();
+    return firstStackElementClassName.startsWith("datadog.")
+        || firstStackElementClassName.startsWith("com.datadog.");
+  }
+
+  public synchronized List<TelemetryLogEntry> drain() {
+    if (logEntries.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    ArrayList<TelemetryLogEntry> drainedEntries = new ArrayList<>();
+
+    drainedEntries.addAll(logEntries);
+
+    if (overflowedEntryCount > 0) {
+      TelemetryLogEntry overflowErrorEntry = new TelemetryLogEntry();
+      overflowErrorEntry.setMessage(
+          "Omitted " + overflowedEntryCount + " entries due to overflowing");
+      overflowErrorEntry.setLevel("ERROR");
+      drainedEntries.add(overflowErrorEntry);
+    }
+
+    overflowedEntryCount = 0;
+    logEntries.clear();
+    return drainedEntries;
+  }
+}

telemetry/src/test/groovy/datadog/telemetry/log/TelemetryLogTest.groovy
@@ -1,0 +1,99 @@
+package datadog.telemetry.log
+
+import datadog.slf4j.impl.StaticLoggerBinder
+import datadog.telemetry.TelemetryService
+import datadog.trace.api.telemetry.LogCollector
+import datadog.trace.api.telemetry.TelemetryLogEntry
+import datadog.trace.logging.ddlogger.DDLogger
+import datadog.trace.test.util.DDSpecification
+import org.slf4j.Logger
+
+class TelemetryLogTest extends DDSpecification {
+  LogPeriodicAction periodicAction = new LogPeriodicAction()
+  TelemetryService telemetryService = Mock()
+  Logger logger
+
+  @Override
+  void setup(){
+    injectSysConfig("dd.instrumentation.telemetry.debug", "true")
+    logger = StaticLoggerBinder.getSingleton().getLoggerFactory().getLogger("pepe")
+    LogCollector.get().setEnabled(true)
+    LogCollector.get().drain()
+  }
+
+  @Override
+  void cleanup(){
+    LogCollector.get().drain()
+  }
+
+  void 'test static binder'(){
+    when:
+    logger.error("Exception Message", new Exception("Exception Message"))
+    periodicAction.doIteration(telemetryService)
+
+    then:
+    1 * telemetryService.addLogEntry( { TelemetryLogEntry logEntry ->
+      logEntry.message == 'Exception Message'
+    } )
+    0 * _._
+  }
+
+
+  void 'test marker debugging'(){
+    when:
+
+    logger.warn(DDLogger.SEND_TELEMETRY, "Debug Message")
+    periodicAction.doIteration(telemetryService)
+
+    then:
+    1 * telemetryService.addLogEntry( { TelemetryLogEntry exception ->
+      exception.message == 'Debug Message'
+    } )
+    0 * _._
+  }
+
+  void 'test debugging with no marker'(){
+    when:
+    logger.warn("Debug Message")
+    periodicAction.doIteration(telemetryService)
+
+    then:
+    0 * _._
+  }
+
+  void 'test overflowed entries'(){
+    setup:
+    List<TelemetryLogEntry> entries
+
+    when:
+    for (int i=0; i< LogCollector.MAX_ENTRIES + 10; i++){
+      logger.warn(DDLogger.SEND_TELEMETRY, "Debug Message " + i)
+    }
+    entries = LogCollector.get().drain()
+
+    then:
+    entries.size() == LogCollector.MAX_ENTRIES + 1
+    entries.get(LogCollector.MAX_ENTRIES).getMessage().contains("Omitted")
+    0 * _._
+  }
+
+  void 'test rate limiting of repeated log messages'(){
+    setup:
+    List<TelemetryLogEntry> entries
+
+    when:
+    for (int i=0; i< LogCollector.MAX_ENTRIES + 10; i++){
+      logger.warn(DDLogger.SEND_TELEMETRY, "Debug Message 1")
+    }
+    for (int i=0; i< LogCollector.MAX_ENTRIES + 10; i++){
+      logger.warn(DDLogger.SEND_TELEMETRY, "Debug Message 2")
+    }
+    entries = LogCollector.get().drain()
+
+    then:
+    entries.size() == 2
+    entries.get(0).getMessage().equals("Debug Message 1")
+    entries.get(1).getMessage().equals("Debug Message 2")
+    0 * _._
+  }
+}