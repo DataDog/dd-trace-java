34768f0e1962a75e04493c7ef726cb4e306ca6e4
Richard Startin <richard.startin@datadoghq.com>
2023-02-03 10:30:37 +0000

    rework instrumentation to handle pending calls

--- dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/client/ClientCallImplInstrumentation.java
+++ dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/client/ClientCallImplInstrumentation.java
@@ -5,6 +5,7 @@ import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSp
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.propagate;
 import static datadog.trace.instrumentation.grpc.client.GrpcClientDecorator.CLIENT_PATHWAY_EDGE_TAGS;
 import static datadog.trace.instrumentation.grpc.client.GrpcClientDecorator.DECORATE;
+import static datadog.trace.instrumentation.grpc.client.GrpcClientDecorator.GRPC_CLIENT;
 import static datadog.trace.instrumentation.grpc.client.GrpcInjectAdapter.SETTER;
 import static net.bytebuddy.matcher.ElementMatchers.isConstructor;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
@@ -15,9 +16,9 @@ import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.bootstrap.InstrumentationContext;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
 import io.grpc.ClientCall;
 import io.grpc.Metadata;
-import io.grpc.MethodDescriptor;
 import io.grpc.Status;
 import io.grpc.StatusException;
 import java.util.Collections;
@@ -70,11 +71,10 @@ public final class ClientCallImplInstrumentation extends Instrumenter.Tracing
 
   public static final class Capture {
     @Advice.OnMethodExit
-    public static void capture(
-        @Advice.This io.grpc.ClientCall<?, ?> call,
-        @Advice.Argument(0) MethodDescriptor<?, ?> method) {
-      AgentSpan span = DECORATE.startCall(method);
-      if (null != span) {
+    public static void capture(@Advice.This io.grpc.ClientCall<?, ?> call) {
+      AgentSpan span = AgentTracer.activeSpan();
+      // embed the span in the call only if a grpc.client span is active
+      if (null != span && GRPC_CLIENT.equals(span.getOperationName())) {
         InstrumentationContext.get(ClientCall.class, AgentSpan.class).put(call, span);
       }
     }
