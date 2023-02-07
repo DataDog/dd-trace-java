00ebab15975bf888b633ffce9842dfecaa196f2a
Alejandro González García <alejandro.gonzalez@datadoghq.com>
2023-02-07 15:23:00 +0100

    Add iast smoke test to springboot-2.6-webmvc
    #What Does This Do
    Improve code reusability
    Add iast smoke test to springboot-2.6-webmvc
    
    #Motivation
    Run IAST test with more spring versions
    
    #Additional Notes
    first iteration, we must add IAST tests to springboot-3.0-webmvc

--- dd-smoke-tests/spring-boot-2.6-webmvc/src/main/java/datadog/smoketest/springboot/controller/IastWebController.java
+++ dd-smoke-tests/spring-boot-2.6-webmvc/src/main/java/datadog/smoketest/springboot/controller/IastWebController.java
@@ -46,12 +46,6 @@ public class IastWebController {
 
   @RequestMapping("/getparameter")
   public String getParameter(@RequestParam String param, HttpServletRequest request) {
-    // StringWriter sw = new StringWriter();
-    // PrintWriter pw = new PrintWriter(sw);
-    // new Throwable().printStackTrace(pw);
-    // return sw.toString();
-    // TestSuite testSuite = new TestSuite(new HttpServletRequestWrapper(request));
-    // testSuite.getParameterMap();
     return "Param is: " + param;
   }
 
