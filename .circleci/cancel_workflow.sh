42a61ce444538f91d214e31a506ec10c2476ed2a
Santiago Mola <santiago.mola@datadoghq.com>
2023-02-08 12:54:12 +0100

    Typo

--- .circleci/cancel_workflow.sh
+++ .circleci/cancel_workflow.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
-echo "Canceliing workflow ${CIRCLE_WORKFLOW_ID}"
+echo "Cancelling workflow ${CIRCLE_WORKFLOW_ID}"
 curl --request POST --url https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/cancel --header "Circle-Token: ${CIRCLE_TOKEN}"
