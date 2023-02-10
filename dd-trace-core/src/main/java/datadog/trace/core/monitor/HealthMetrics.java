5da7f823346ff1e561603956a1006f670ad1c2e1
Bruce Bujon <bruce.bujon@datadoghq.com>
2023-02-10 11:13:43 +0100

    chore: Clean up HealthMetrics
    Prevent referencing subclass from parent class.

--- dd-trace-core/src/main/java/datadog/trace/core/monitor/HealthMetrics.java
+++ dd-trace-core/src/main/java/datadog/trace/core/monitor/HealthMetrics.java
@@ -17,46 +17,46 @@ import java.util.List;
  * </ul>
  */
 public abstract class HealthMetrics implements AutoCloseable {
-  public static HealthMetrics NO_OP = new NoOpHealthMetrics();
+  public static HealthMetrics NO_OP = new HealthMetrics() {};
 
-  public void start() {};
+  public void start() {}
 
-  public void onStart(final int queueCapacity) {};
+  public void onStart(final int queueCapacity) {}
 
-  public void onShutdown(final boolean flushSuccess) {};
+  public void onShutdown(final boolean flushSuccess) {}
 
-  public void onPublish(final List<DDSpan> trace, final int samplingPriority) {};
+  public void onPublish(final List<DDSpan> trace, final int samplingPriority) {}
 
-  public void onFailedPublish(final int samplingPriority) {};
+  public void onFailedPublish(final int samplingPriority) {}
 
-  public void onPartialPublish(final int numberOfDroppedSpans) {};
+  public void onPartialPublish(final int numberOfDroppedSpans) {}
 
-  public void onScheduleFlush(final boolean previousIncomplete) {};
+  public void onScheduleFlush(final boolean previousIncomplete) {}
 
-  public void onFlush(final boolean early) {};
+  public void onFlush(final boolean early) {}
 
-  public void onPartialFlush(final int sizeInBytes) {};
+  public void onPartialFlush(final int sizeInBytes) {}
 
-  public void onSerialize(final int serializedSizeInBytes) {};
+  public void onSerialize(final int serializedSizeInBytes) {}
 
-  public void onFailedSerialize(final List<DDSpan> trace, final Throwable optionalCause) {};
+  public void onFailedSerialize(final List<DDSpan> trace, final Throwable optionalCause) {}
 
-  public void onCreateSpan() {};
+  public void onCreateSpan() {}
 
-  public void onCreateTrace() {};
+  public void onCreateTrace() {}
 
-  public void onCreateManualTrace() {};
+  public void onCreateManualTrace() {}
 
-  public void onCancelContinuation() {};
+  public void onCancelContinuation() {}
 
-  public void onFinishContinuation() {};
+  public void onFinishContinuation() {}
 
   public void onSend(
-      final int traceCount, final int sizeInBytes, final RemoteApi.Response response) {};
+      final int traceCount, final int sizeInBytes, final RemoteApi.Response response) {}
 
   public void onFailedSend(
-      final int traceCount, final int sizeInBytes, final RemoteApi.Response response) {};
+      final int traceCount, final int sizeInBytes, final RemoteApi.Response response) {}
 
   @Override
-  public void close() {};
+  public void close() {}
 }
