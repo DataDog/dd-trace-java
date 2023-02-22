pvishwa3/cito-trace-java

Implementation of IAST telemetry

dd-java-agent/instrumentation/java-lang/java-lang-9/src/main/java/datadog/trace/instrumentation/java/lang/invoke/StringConcatFactoryCallSite.java
@@ -7,6 +7,7 @@ import datadog.trace.agent.tooling.csi.CallSite;
 import datadog.trace.api.iast.IastAdvice;
 import datadog.trace.api.iast.InstrumentationBridge;
 import datadog.trace.api.iast.propagation.StringModule;
+import datadog.trace.api.iast.telemetry.Propagation;
 import de.thetaphi.forbiddenapis.SuppressForbidden;
 import java.lang.invoke.ConstantCallSite;
 import java.lang.invoke.MethodHandle;
@@ -20,6 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SuppressForbidden
+@Propagation
 @CallSite(spi = IastAdvice.class, minJavaVersion = 9)
 public class StringConcatFactoryCallSite {
 

DataDog/dd-trace-java

dd-java-agent/instrumentation/java-lang/java-lang-9/src/main/java/datadog/trace/instrumentation/java/lang/invoke/StringConcatFactoryCallSite.java
@@ -5,7 +5,9 @@ import static java.lang.invoke.StringConcatFactory.makeConcatWithConstants;
 
 import datadog.trace.agent.tooling.csi.CallSite;
 import datadog.trace.api.iast.IastAdvice;
+import datadog.trace.api.iast.IastAdvice.Propagation;
 import datadog.trace.api.iast.InstrumentationBridge;
+import datadog.trace.api.iast.model.PropagationTypes;
 import datadog.trace.api.iast.propagation.StringModule;
 import de.thetaphi.forbiddenapis.SuppressForbidden;
 import java.lang.invoke.ConstantCallSite;
@@ -20,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SuppressForbidden
+@Propagation(PropagationTypes.STRING)
 @CallSite(spi = IastAdvice.class, minJavaVersion = 9)
 public class StringConcatFactoryCallSite {
 