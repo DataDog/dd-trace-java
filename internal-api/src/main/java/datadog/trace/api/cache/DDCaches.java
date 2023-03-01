2511959b10e868ea528779c41f0c307a7ab78605
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2023-03-01 10:11:36 +0000

    Support fixed-sized weak-key caches (#4819)

--- internal-api/src/main/java/datadog/trace/api/cache/DDCaches.java
+++ internal-api/src/main/java/datadog/trace/api/cache/DDCaches.java
@@ -40,6 +40,16 @@ public final class DDCaches {
   }
 
   /**
+   * Specialized fixed-size cache whose keys are weakly referenced. Uses {@link
+   * System#identityHashCode} for key hashing and equality.
+   *
+   * @see #newFixedSizeCache(int)
+   */
+  public static <K, V> DDCache<K, V> newFixedSizeWeakKeyCache(final int capacity) {
+    return new FixedSizeWeakKeyCache<>(capacity);
+  }
+
+  /**
    * Creates a memoization of an association. Useful for creating an association between an
    * implicitly bounded set of keys and values, where the nature of the keys prevents unbounded
    * space usage.
