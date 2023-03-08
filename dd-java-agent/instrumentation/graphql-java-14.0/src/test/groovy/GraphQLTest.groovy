e8a9dc8662cda40ecabda96f6ee5e6eaf1281c8d
Jon Christiansen <467023+theJC@users.noreply.github.com>
2023-03-08 13:13:55 -0600

    Blend previous use of an enum, with some form of type indicator

--- dd-java-agent/instrumentation/graphql-java-14.0/src/test/groovy/GraphQLTest.groovy
+++ dd-java-agent/instrumentation/graphql-java-14.0/src/test/groovy/GraphQLTest.groovy
@@ -206,7 +206,7 @@ abstract class GraphQLTest extends VersionedNamingTestBase {
       '  }\n' +
       '}'
     def expectedQuery = 'query findBookById {\n' +
-      '  bookById(id: "") {\n' +
+      '  bookById(id: {String}) {\n' +
       '    id\n' +
       '    title\n' +
       '    color\n' +
@@ -322,7 +322,7 @@ abstract class GraphQLTest extends VersionedNamingTestBase {
       '  }\n' +
       '}'
     def expectedQuery = 'query findBookById {\n' +
-      '  bookById(id: "") {\n' +
+      '  bookById(id: {String}) {\n' +
       '    id\n' +
       '    cover\n' +
       '  }\n' +
