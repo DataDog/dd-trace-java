c031d754cb75f920ef13ef6d138a73c8c2946ef6
nayeem-kamal <nayeem.kamal@datadoghq.com>
2023-03-16 15:11:12 -0400

    added comment pointing to tracer docs for config format

--- dd-trace-api/src/main/java/datadog/trace/api/config/TraceInstrumentationConfig.java
+++ dd-trace-api/src/main/java/datadog/trace/api/config/TraceInstrumentationConfig.java
@@ -19,6 +19,10 @@ public final class TraceInstrumentationConfig {
   public static final String TRACE_EXECUTORS_ALL = "trace.executors.all";
   public static final String TRACE_EXECUTORS = "trace.executors";
   public static final String TRACE_METHODS = "trace.methods";
+  /*
+  format for measure.methods is the same as for trace.methods:
+  https://docs.datadoghq.com/tracing/trace_collection/custom_instrumentation/java/
+   */
   public static final String MEASURE_METHODS = "measure.methods";
   public static final String TRACE_CLASSES_EXCLUDE = "trace.classes.exclude";
   public static final String TRACE_CLASSES_EXCLUDE_FILE = "trace.classes.exclude.file";
