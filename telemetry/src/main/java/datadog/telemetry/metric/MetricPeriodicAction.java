416801221ea9a240ec3818b67ec00f043457a0d5
Valentin Zakharov <valentin.zakharov1@gmail.com>
2023-03-17 19:15:26 +0100

    SpotlessApply

--- telemetry/src/main/java/datadog/telemetry/metric/MetricPeriodicAction.java
+++ telemetry/src/main/java/datadog/telemetry/metric/MetricPeriodicAction.java
@@ -3,16 +3,15 @@ package datadog.telemetry.metric;
 import datadog.telemetry.TelemetryRunnable;
 import datadog.telemetry.TelemetryService;
 import datadog.telemetry.api.Metric;
-
 import java.util.Collection;
 
 public class MetricPeriodicAction implements TelemetryRunnable.TelemetryPeriodicAction {
-    @Override
-    public void doIteration(TelemetryService service) {
-        Collection<Metric> metrics = MetricCollector.get().drain();
+  @Override
+  public void doIteration(TelemetryService service) {
+    Collection<Metric> metrics = MetricCollector.get().drain();
 
-        for (Metric metric: metrics) {
-            service.addMetric(metric);
-        }
+    for (Metric metric : metrics) {
+      service.addMetric(metric);
     }
+  }
 }
