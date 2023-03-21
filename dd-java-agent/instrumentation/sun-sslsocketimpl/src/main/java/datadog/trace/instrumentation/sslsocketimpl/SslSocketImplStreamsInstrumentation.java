c742e296a23f371621d5dabd4c4a54df9edc3399
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2023-03-21 16:07:23 +0100

    usm: instrument SSLSocket type hierarchy
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>

--- dd-java-agent/instrumentation/sun-sslsocketimpl/src/main/java/datadog/trace/instrumentation/sslsocketimpl/SslSocketImplStreamsInstrumentation.java
+++ dd-java-agent/instrumentation/sun-sslsocketimpl/src/main/java/datadog/trace/instrumentation/sslsocketimpl/SslSocketImplStreamsInstrumentation.java
@@ -7,19 +7,29 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers;
 import datadog.trace.bootstrap.instrumentation.api.UsmConnection;
 import datadog.trace.bootstrap.instrumentation.api.UsmExtractor;
 import datadog.trace.bootstrap.instrumentation.api.UsmMessage;
 import datadog.trace.bootstrap.instrumentation.api.UsmMessageFactory;
+
+import java.io.FilterInputStream;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Inet6Address;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
+import net.bytebuddy.matcher.ElementMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.security.ssl.SSLSocketImpl;
+import javax.net.ssl.SSLSocket;
 
 @AutoService(Instrumenter.class)
 public class SslSocketImplStreamsInstrumentation extends Instrumenter.Usm
-    implements Instrumenter.ForBootstrap, Instrumenter.ForKnownTypes {
+    implements Instrumenter.ForBootstrap, Instrumenter.ForTypeHierarchy {
 
   private static final Logger log =
       LoggerFactory.getLogger(SslSocketImplStreamsInstrumentation.class);
@@ -29,80 +39,73 @@ public class SslSocketImplStreamsInstrumentation extends Instrumenter.Usm
   }
 
   @Override
-  public String[] knownMatchingTypes() {
+  public String hierarchyMarkerType() {
     // we instrument both input and output streams which are inner classes of the
     // SSLSocketImpl
-    return new String[] {
-      "sun.security.ssl.SSLSocketImpl$AppInputStream",
-      "sun.security.ssl.SSLSocketImpl$AppOutputStream"
-    };
+    return "javax.net.ssl.SSLSocket";
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> hierarchyMatcher() {
+    return HierarchyMatchers.extendsClass(named("javax.net.ssl.SSLSocket"));
   }
 
   @Override
   public void adviceTransformations(AdviceTransformation transformation) {
-    transformation.applyAdvice(
-        isMethod()
-            .and(
-                named("write")
-                    .and(takesArguments(3))
-                    .and(takesArgument(0, byte[].class))
-                    .and(takesArgument(1, int.class))
-                    .and(takesArgument(2, int.class))),
-        SslSocketImplStreamsInstrumentation.class.getName() + "$WriteAdvice");
-    transformation.applyAdvice(
-        isMethod()
-            .and(
-                named("read")
-                    .and(takesArguments(3))
-                    .and(takesArgument(0, byte[].class))
-                    .and(takesArgument(1, int.class))
-                    .and(takesArgument(2, int.class))),
-        SslSocketImplStreamsInstrumentation.class.getName() + "$ReadAdvice");
+        transformation.applyAdvice(isMethod().and(named("getInputStream")),
+            SslSocketImplStreamsInstrumentation.class.getName() + "$GetInputStreamAdvice");
+    transformation.applyAdvice(isMethod().and(named("getOutputStream")),
+        SslSocketImplStreamsInstrumentation.class.getName() + "$GetOutputStreamAdvice");
   }
 
-  public static class WriteAdvice {
+  public static class GetOutputStreamAdvice {
 
-    @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void write(
-        @Advice.FieldValue("this$0") SSLSocketImpl socket,
-        @Advice.Argument(0) final byte[] buffer,
-        @Advice.Argument(1) int offset,
-        @Advice.Argument(2) int len) {
-
-      boolean isIPv6 = socket.getLocalAddress() instanceof Inet6Address;
-      UsmConnection connection =
-          new UsmConnection(
-              socket.getLocalAddress(),
-              socket.getLocalPort(),
-              socket.getInetAddress(),
-              socket.getPeerPort(),
-              isIPv6);
-      UsmMessage message =
-          UsmMessageFactory.Supplier.getRequestMessage(connection, buffer, offset, len);
-      UsmExtractor.Supplier.send(message);
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void getOutputStream(
+        @Advice.This final SSLSocket socket,
+        @Advice.Return(readOnly = false) OutputStream retValue) {
+        retValue = new FilterOutputStream(retValue) {
+          @Override
+          public void write(byte[] b, int off, int len) throws IOException {
+            boolean isIPv6 = socket.getLocalAddress() instanceof Inet6Address;
+            UsmConnection connection =
+                new UsmConnection(
+                    socket.getLocalAddress(),
+                    socket.getLocalPort(),
+                    socket.getInetAddress(),
+                    socket.getPort(),
+                    isIPv6);
+            UsmMessage message =
+                UsmMessageFactory.Supplier.getRequestMessage(connection, b, off, len);
+            UsmExtractor.Supplier.send(message);
+            super.write(b, off, len);
+          }
+        };
     }
   }
 
-  public static class ReadAdvice {
-
+  public static class GetInputStreamAdvice {
     @Advice.OnMethodExit(suppress = Throwable.class)
-    public static void read(
-        @Advice.FieldValue("this$0") SSLSocketImpl socket,
-        @Advice.Argument(0) final byte[] buffer,
-        @Advice.Argument(1) int offset,
-        @Advice.Argument(2) int len) {
-
-      boolean isIPv6 = socket.getLocalAddress() instanceof Inet6Address;
-      UsmConnection connection =
-          new UsmConnection(
-              socket.getLocalAddress(),
-              socket.getLocalPort(),
-              socket.getInetAddress(),
-              socket.getPeerPort(),
-              isIPv6);
-      UsmMessage message =
-          UsmMessageFactory.Supplier.getRequestMessage(connection, buffer, offset, len);
-      UsmExtractor.Supplier.send(message);
+    public static void getInputStream(
+        @Advice.This final SSLSocket socket,
+        @Advice.Return(readOnly = false) InputStream retValue) {
+        retValue = new FilterInputStream(retValue) {
+          @Override
+          public int read(byte[] b, int off, int len) throws IOException {
+            boolean isIPv6 = socket.getLocalAddress() instanceof Inet6Address;
+            UsmConnection connection =
+                new UsmConnection(
+                    socket.getLocalAddress(),
+                    socket.getLocalPort(),
+                    socket.getInetAddress(),
+                    socket.getPort(),
+                    isIPv6);
+            UsmMessage message =
+                UsmMessageFactory.Supplier.getRequestMessage(connection, b, off, len);
+            UsmExtractor.Supplier.send(message);
+            return super.read(b, off, len);
+          }
+        };
     }
   }
 }
