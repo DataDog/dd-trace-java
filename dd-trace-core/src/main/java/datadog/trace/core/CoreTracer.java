65f68b9b093db8814396e2098eb518a8fb96d456
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2023-03-21 19:46:21 -0400

    Adding RequestContext slot for CI Visibility (#4900)
    * Adding RequestContext slot for CI Visibility
    
    * Avoid map allocation
    
    * Add another test

--- dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
+++ dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
@@ -1143,6 +1143,9 @@ public class CoreTracer implements AgentTracer.TracerAPI {
     private boolean errorFlag;
     private CharSequence spanType;
     private boolean ignoreScope = false;
+    private Object builderRequestContextDataAppSec;
+    private Object builderRequestContextDataIast;
+    private Object builderCiVisibilityContextData;
 
     CoreSpanBuilder(final CharSequence operationName, CoreTracer tracer) {
       this.operationName = operationName;
@@ -1241,6 +1244,22 @@ public class CoreTracer implements AgentTracer.TracerAPI {
       return this;
     }
 
+    @Override
+    public <T> AgentTracer.SpanBuilder withRequestContextData(RequestContextSlot slot, T data) {
+      switch (slot) {
+        case APPSEC:
+          builderRequestContextDataAppSec = data;
+          break;
+        case CI_VISIBILITY:
+          builderCiVisibilityContextData = data;
+          break;
+        case IAST:
+          builderRequestContextDataIast = data;
+          break;
+      }
+      return this;
+    }
+
     /**
      * Build the SpanContext, if the actual span has a parent, the following attributes must be
      * propagated: - ServiceName - Baggage - Trace (a list of all spans related) - SpanType
@@ -1261,6 +1280,7 @@ public class CoreTracer implements AgentTracer.TracerAPI {
       final DDSpanContext context;
       Object requestContextDataAppSec;
       Object requestContextDataIast;
+      Object ciVisibilityContextData;
       final PathwayContext pathwayContext;
       final PropagationTags propagationTags;
 
@@ -1298,9 +1318,11 @@ public class CoreTracer implements AgentTracer.TracerAPI {
         if (requestContext != null) {
           requestContextDataAppSec = requestContext.getData(RequestContextSlot.APPSEC);
           requestContextDataIast = requestContext.getData(RequestContextSlot.IAST);
+          ciVisibilityContextData = requestContext.getData(RequestContextSlot.CI_VISIBILITY);
         } else {
           requestContextDataAppSec = null;
           requestContextDataIast = null;
+          ciVisibilityContextData = null;
         }
         pathwayContext =
             ddsc.getPathwayContext().isStarted()
@@ -1335,12 +1357,14 @@ public class CoreTracer implements AgentTracer.TracerAPI {
           baggage = tc.getBaggage();
           requestContextDataAppSec = tc.getRequestContextDataAppSec();
           requestContextDataIast = tc.getRequestContextDataIast();
+          ciVisibilityContextData = tc.getCiVisibilityContextData();
         } else {
           coreTags = null;
           origin = null;
           baggage = null;
           requestContextDataAppSec = null;
           requestContextDataIast = null;
+          ciVisibilityContextData = null;
         }
 
         rootSpanTags = localRootSpanTags;
@@ -1366,6 +1390,17 @@ public class CoreTracer implements AgentTracer.TracerAPI {
               + defaultSpanTags.size()
               + (null == coreTags ? 0 : coreTags.size())
               + (null == rootSpanTags ? 0 : rootSpanTags.size());
+
+      if (builderRequestContextDataAppSec != null) {
+        requestContextDataAppSec = builderRequestContextDataAppSec;
+      }
+      if (builderCiVisibilityContextData != null) {
+        ciVisibilityContextData = builderCiVisibilityContextData;
+      }
+      if (builderRequestContextDataIast != null) {
+        requestContextDataIast = builderRequestContextDataIast;
+      }
+
       // some attributes are inherited from the parent
       context =
           new DDSpanContext(
@@ -1385,6 +1420,7 @@ public class CoreTracer implements AgentTracer.TracerAPI {
               parentTrace,
               requestContextDataAppSec,
               requestContextDataIast,
+              ciVisibilityContextData,
               pathwayContext,
               disableSamplingMechanismValidation,
               propagationTags,
