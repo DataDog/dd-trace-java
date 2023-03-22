dd118b40bd43c1416b3ed3edc76b9cc726c34d67
nayeem-kamal <nayeem.kamal@datadoghq.com>
2023-03-21 22:34:13 -0400

    fixed tests

--- dd-java-agent/instrumentation/trace-annotation/src/test/groovy/MeasuredConfigTest.groovy
+++ dd-java-agent/instrumentation/trace-annotation/src/test/groovy/MeasuredConfigTest.groovy
@@ -7,67 +7,97 @@ import java.util.concurrent.Callable
 
 class MeasuredConfigTest extends AgentTestRunner {
 
+  @Override
+  void configurePreAgent() {
+    super.configurePreAgent()
+    injectSysConfig("dd.trace.methods", "package.ClassName[method1,method2];${ConfigTracedCallable.name}[call];${ConfigTracedCallable2.name}[*];")
+    injectSysConfig("dd.measure.methods", "package.ClassName[method1,method2];${ConfigTracedCallable.name}[call];${ConfigTracedCallable2.name}[*];")
 
-  def "test configuration \"#value\""() {
-    //
-    setup:
-    if (value) {
-      injectSysConfig("dd.trace.methods", value)
-      injectSysConfig("dd.measure.methods", value)
-    } else {
-      removeSysConfig("dd.trace.methods")
-      removeSysConfig("dd.measure.methods")
+  }
+
+  class ConfigTracedCallable implements Callable<String> {
+    @Override
+    String call() throws Exception {
+      return "Hello!"
+    }
+  }
+  class ConfigTracedCallable2 implements Callable<String> {
+    int g
+
+    ConfigTracedCallable2(){
+      g = 4
+    }
+    @Override
+    String call() throws Exception {
+      return call_helper()
     }
 
-    expect:
-    new TraceConfigInstrumentation().classMethodsToTrace == expected
-    new MeasuredMethodFilter().methodsToMeasure == expected
+    String call_helper() throws Exception {
+      return "Hello2!"
+    }
 
-    where:
-    value                                                           | expected
-    null                                                            | [:]
-    " "                                                             | [:]
-    "*"                                                             | [:]
-    "some.package.ClassName"                                        | [:]
-    "some.package.ClassName[ , ]"                                   | [:]
-    "some.package.ClassName[ , method]"                             | [:]
-    "some.package.ClassName[ method1,  , method2]"                  | [:]
-    "some.package.Class\$Name[ method , ]"                          | ["some.package.Class\$Name": ["method"].toSet()]
-    "some.package.Class\$Name[ method , , ]"                        | [:]
-    "ClassName[ method1,]"                                          | ["ClassName": ["method1"].toSet()]
-    "ClassName[method1 , method2]"                                  | ["ClassName": ["method1", "method2"].toSet()]
-    "Class\$1[method1 ] ; Class\$2[ method2];"                      | ["Class\$1": ["method1"].toSet(), "Class\$2": ["method2"].toSet()]
-    "Duplicate[method1] ; Duplicate[method2]  ;Duplicate[method3];" | ["Duplicate": ["method1", "method2", "method3"].toSet()]
-    "Duplicate[method1] ; Duplicate[method2]  ;Duplicate[*];"       | ["Duplicate": ["method1", "method2", "*"].toSet()]
-    "ClassName[*]"                                                  | ["ClassName": ["*"].toSet()]
-    "ClassName[asdfg*]"                                             | [:]
-    "ClassName[*,asdfg]"                                            | [:]
-    "ClassName[asdfg,*]"                                            | [:]
-    "Class[*] ; Class\$2[ method2];"                                | ["Class": ["*"].toSet(), "Class\$2": ["method2"].toSet()]
-    "Class[*] ; Class\$2[ method2];      "                          | ["Class": ["*"].toSet(), "Class\$2": ["method2"].toSet()]
-    "     Class[*] ; Class\$2[ method2];      "                     | ["Class": ["*"].toSet(), "Class\$2": ["method2"].toSet()]
-    "     Class[*] ; Class\$2[ method2];"                           | ["Class": ["*"].toSet(), "Class\$2": ["method2"].toSet()]
-    "c1[m1,m2];c2[m1,m2]"                                           | ["c1": ["m1", "m2"].toSet(), "c2": ["m1", "m2"].toSet()]
-    "c1;[m1,m2]"                                                    | [:]
-    "c1[m1;m2]"                                                     | [:]
-    "c1[m1],"                                                       | [:]
-    "c1[m1];asdfg"                                                  | [:]
-    "asdfg;c1[m1]"                                                  | [:]
+    String getValue() {
+      return "hello"
+    }
+    void setValue(int value) {
+      g = value
+    }
   }
 
-  def "survive very long list"() {
-    setup:
-    def mset = ["m1", "m2"].toSet()
-    Map<String, Set<String>> expected = [:]
-    def methods = (1..600).collect {
-      expected.put("c$it".toString(), mset)
-      return "c$it[${mset.join(",")}]"
-    }.join(";")
-    injectSysConfig("dd.measure.methods", methods)
-    injectSysConfig("dd.trace.methods", methods)
-
-    expect:
-    new TraceConfigInstrumentation().classMethodsToTrace == expected
-    new MeasuredMethodFilter().methodsToMeasure == expected
+  def "test configuration based trace"() {
+    when:
+    new ConfigTracedCallable().call() == "Hello!"
+    then:
+    assertTraces(1) {
+      trace(1) {
+        span {
+          resourceName "ConfigTracedCallable.call"
+          operationName "trace.annotation"
+          measured true
+          tags {
+            "$Tags.COMPONENT" "trace"
+            defaultTags()
+          }
+        }
+      }
+    }
   }
+  def "test wildcard configuration"() {
+    when:
+    ConfigTracedCallable2 object = new ConfigTracedCallable2()
+    object.call()
+    object.hashCode()
+    object == new ConfigTracedCallable2()
+    object.toString()
+    object.finalize()
+    object.getValue()
+    object.setValue(5)
+
+
+    then:
+    assertTraces(1) {
+      trace(2) {
+        span {
+          resourceName "ConfigTracedCallable2.call"
+          operationName "trace.annotation"
+          measured true
+          tags {
+            "$Tags.COMPONENT" "trace"
+            defaultTags()
+          }
+        }
+        span {
+          resourceName "ConfigTracedCallable2.call_helper"
+          operationName "trace.annotation"
+          measured true
+          tags {
+            "$Tags.COMPONENT" "trace"
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+
 }
