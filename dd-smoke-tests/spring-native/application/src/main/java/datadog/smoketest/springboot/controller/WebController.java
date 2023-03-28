327d08aa13f2d45db0791a5491e87ea8f984d249
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2023-03-28 16:07:16 +0100

    Excercise @Trace annotation and DD_TRACE_METHODS in spring-native smoke test

--- dd-smoke-tests/spring-native/application/src/main/java/datadog/smoketest/springboot/controller/WebController.java
+++ dd-smoke-tests/spring-native/application/src/main/java/datadog/smoketest/springboot/controller/WebController.java
@@ -1,5 +1,6 @@
 package datadog.smoketest.springboot.controller;
 
+import datadog.trace.api.Trace;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,16 @@ public class WebController {
 
   @RequestMapping("/hello")
   public String hello() {
+    return doHello();
+  }
+
+  @Trace
+  private String doHello() {
+    return sayHello();
+  }
+
+  /** DD_TRACE_METHOD="datadog.smoketest.springboot.controller.WebController[sayHello]" */
+  private String sayHello() {
     return "Hello world";
   }
 }
