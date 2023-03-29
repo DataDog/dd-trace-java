d21ccbb1af2922ec7b29ff8d151a9d12b23febdd
Valentin Zakharov <valentin.zakharov1@gmail.com>
2023-03-29 16:40:59 +0200

    Fix: blocked requests are also triggered

--- internal-api/src/test/groovy/datadog/trace/api/TelemetryCollectorsTest.groovy
+++ internal-api/src/test/groovy/datadog/trace/api/TelemetryCollectorsTest.groovy
@@ -40,6 +40,90 @@ class TelemetryCollectorsTest extends DDSpecification {
     ConfigCollector.get() == [key1: 'replaced', key2: 'value2']
   }
 
+  def "put-get waf metrics"() {
+    when:
+    MetricCollector.get().wafInit('waf_ver1', 'rules.1')
+    MetricCollector.get().wafUpdates('rules.2')
+    MetricCollector.get().wafUpdates('rules.3')
+    MetricCollector.get().wafRequest()
+    MetricCollector.get().wafRequest()
+    MetricCollector.get().wafRequest()
+    MetricCollector.get().wafRequestTriggered()
+    MetricCollector.get().wafRequestBlocked()
+
+    MetricCollector.get().prepareRequestMetrics()
+
+    then:
+    def metrics = MetricCollector.get().drain()
+
+    def initMetric = (MetricCollector.WafInitRawMetric)metrics[0]
+    initMetric.counter == 1
+    initMetric.namespace == 'appsec'
+    initMetric.metricName == 'waf.init'
+    initMetric.wafVersion == 'waf_ver1'
+    initMetric.rulesVersion == 'rules.1'
+
+    def updateMetric1 = (MetricCollector.WafUpdatesRawMetric)metrics[1]
+    updateMetric1.counter == 1
+    updateMetric1.namespace == 'appsec'
+    updateMetric1.metricName == 'waf.updates'
+    updateMetric1.rulesVersion == 'rules.2'
+
+    def updateMetric2 = (MetricCollector.WafUpdatesRawMetric)metrics[2]
+    updateMetric2.counter == 2
+    updateMetric2.namespace == 'appsec'
+    updateMetric2.metricName == 'waf.updates'
+    updateMetric2.rulesVersion == 'rules.3'
+
+    def requestMetric = (MetricCollector.WafRequestsRawMetric)metrics[3]
+    requestMetric.namespace == 'appsec'
+    requestMetric.metricName == 'waf.requests'
+    requestMetric.counter == 3
+    !requestMetric.triggered        // false
+    !requestMetric.blocked          // false
+
+    def requestTriggeredMetric = (MetricCollector.WafRequestsRawMetric)metrics[4]
+    requestTriggeredMetric.namespace == 'appsec'
+    requestTriggeredMetric.metricName == 'waf.requests'
+    requestTriggeredMetric.counter == 2
+    requestTriggeredMetric.triggered      // true
+    !requestTriggeredMetric.blocked       // false
+
+    def requestBlockedMetric = (MetricCollector.WafRequestsRawMetric)metrics[5]
+    requestBlockedMetric.namespace == 'appsec'
+    requestBlockedMetric.metricName == 'waf.requests'
+    requestBlockedMetric.counter == 1
+    requestBlockedMetric.triggered        // true
+    requestBlockedMetric.blocked          // true
+
+
+    //    def init = (MetricCollector.WafInitRawMetric)metrics[0]
+    //    init.counter == 1
+    //    init.wafVersion == 'waf_ver1'
+    //    init.rulesVersion == 'rules.1'
+
+    //metrics[0] == new MetricCollector.WafInitRawMetric(1, 'waf_ver1', 'rules.1')
+    //metrics[0] == new MetricCollector.WafInitRawMetric(counter: 1, wafVersion: 'waf_ver1', rulesVersion: 'rules.1')
+
+
+    //            == [MetricCollector.WafInitRawMetric
+    //                                      [
+    //                    'metricName': 'waf.init',
+    //                    'namespace': 'appsec',
+    //                    'wafVersion': 'waf_ver1',
+    //                    'rulesVersion': 'rules.1',
+    //                    'counter': 1
+    //            ]
+    //    ]
+
+    //    wafVersion = "waf_ver1"
+    //    rulesVersion = "rules.1"
+    //    metricName = "waf.init"
+    //    timestamp = 1680081130462
+    //    counter = 1
+    //    namespace = "appsec"
+  }
+
   def "hide pii configuration data"() {
     setup:
     ConfigCollector.get().clear()
