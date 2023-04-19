a2fd6a63d153d8d89f5da3d2f55e6386b259497e
Darcy Rayner <darcy.rayner@datadoghq.com>
2023-04-19 17:41:51 -0400

    Add missing import

--- dd-java-agent/instrumentation/aws-lambda-handler/src/test/groovy/HandlerStreaming.java
+++ dd-java-agent/instrumentation/aws-lambda-handler/src/test/groovy/HandlerStreaming.java
@@ -2,6 +2,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.IOException
 
 public class HandlerStreaming implements RequestStreamHandler {
   @Override
