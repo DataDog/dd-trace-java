705ef70ba052c6ff9c49328e6d5ced03dad78956
Darcy Rayner <darcy.rayner@datadoghq.com>
2023-04-20 10:11:40 -0400

    Fix typo

--- dd-java-agent/instrumentation/aws-lambda-handler/src/test/groovy/HandlerStreaming.java
+++ dd-java-agent/instrumentation/aws-lambda-handler/src/test/groovy/HandlerStreaming.java
@@ -1,8 +1,8 @@
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.IOException
 
 public class HandlerStreaming implements RequestStreamHandler {
   @Override
