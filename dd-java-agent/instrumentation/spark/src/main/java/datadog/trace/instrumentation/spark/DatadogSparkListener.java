63a23564b2f487a74478f84dfd50de537504074f
Paul Laffon <paul.laffon@datadoghq.com>
2023-05-04 12:06:28 +0200

    Add databricks parent context

--- dd-java-agent/instrumentation/spark/src/main/java/datadog/trace/instrumentation/spark/DatadogSparkListener.java
+++ dd-java-agent/instrumentation/spark/src/main/java/datadog/trace/instrumentation/spark/DatadogSparkListener.java
@@ -121,15 +121,21 @@ public class DatadogSparkListener extends SparkListener {
       }
 
       if (jobStart.properties() != null) {
-        // ids to link those spans to databricks job/task traces
-        jobSpanBuilder.withTag(
-            "databricks_job_id", jobStart.properties().get("spark.databricks.job.id"));
-        jobSpanBuilder.withTag(
-            "databricks_job_run_id", getDatabricksJobRunId(jobStart.properties()));
+        String databricksJobId = (String) jobStart.properties().get("spark.databricks.job.id");
+        String databricksJobRunId = getDatabricksJobRunId(jobStart.properties());
 
         // spark.databricks.job.runId is the runId of the task, not of the Job
-        jobSpanBuilder.withTag(
-            "databricks_task_run_id", jobStart.properties().get("spark.databricks.job.runId"));
+        String databricksTaskRunId =
+            (String) jobStart.properties().get("spark.databricks.job.runId");
+
+        // ids to link those spans to databricks job/task traces
+        jobSpanBuilder.withTag("databricks_job_id", databricksJobId);
+        jobSpanBuilder.withTag("databricks_job_run_id", databricksJobRunId);
+        jobSpanBuilder.withTag("databricks_task_run_id", databricksTaskRunId);
+
+        AgentSpan.Context parentContext =
+            new DatabricksParentContext(databricksJobId, databricksJobRunId, databricksTaskRunId);
+        jobSpanBuilder.asChildOf(parentContext);
       }
     } else {
       // In non-databricks env, the spark application is the local root spans
--- dd-java-agent/instrumentation/spark/src/test/groovy/SparkTest.groovy
+++ dd-java-agent/instrumentation/spark/src/test/groovy/SparkTest.groovy
@@ -1,4 +1,6 @@
 import datadog.trace.agent.test.AgentTestRunner
+import datadog.trace.api.DDTraceId
+import datadog.trace.instrumentation.spark.DatabricksParentContext
 import org.apache.spark.sql.SparkSession
 
 class SparkTest extends AgentTestRunner {
@@ -55,5 +57,69 @@ class SparkTest extends AgentTestRunner {
         }
       }
     }
+  }
+
+  def "generate databricks spans"() {
+    setup:
+    def sparkSession = SparkSession.builder()
+      .config("spark.master", "local")
+      .config("spark.default.parallelism", "2") // Small parallelism to speed up tests
+      .config("spark.sql.shuffle.partitions", "2")
+      .config("spark.databricks.sparkContextId", "some_id")
+      .getOrCreate()
+
+    sparkSession.sparkContext().setLocalProperty("spark.databricks.job.id", "1234")
+    sparkSession.sparkContext().setLocalProperty("spark.databricks.job.runId", "9012")
+    sparkSession.sparkContext().setLocalProperty("spark.databricks.clusterUsageTags.clusterName", "job-1234-run-5678-Job_cluster>")
+    TestSparkComputation.generateTestSparkComputation(sparkSession)
+
+    expect:
+    assertTraces(1) {
+      trace(3) {
+        span {
+          operationName "spark.job"
+          resourceName "count at TestSparkComputation.java:12"
+          spanType "spark"
+          errored false
+          traceId new BigInteger("8944764253919609482")
+          parentSpanId new BigInteger("15104224823446433673")
+        }
+        span {
+          operationName "spark.stage"
+          resourceName "count at TestSparkComputation.java:12"
+          spanType "spark"
+          errored false
+          childOf(span(0))
+        }
+        span {
+          operationName "spark.stage"
+          resourceName "distinct at TestSparkComputation.java:12"
+          spanType "spark"
+          errored false
+          childOf(span(0))
+        }
+      }
+    }
+  }
+
+  def "compute the databricks parent context"() {
+    setup:
+    def contextWithJobRunId = new DatabricksParentContext("1234", "5678", "9012")
+    def contextWithoutJobRunId = new DatabricksParentContext("1234", null, "9012")
+    def contextWithoutJobId = new DatabricksParentContext(null, "5678", "9012")
+    def contextWithoutTaskRunId = new DatabricksParentContext(null, "5678", null)
+
+    expect:
+    contextWithJobRunId.getTraceId() == DDTraceId.from("8944764253919609482")
+    contextWithJobRunId.getSpanId() == -3342519250263117943L
+
+    contextWithoutJobRunId.getTraceId() == DDTraceId.from("15104224823446433673")
+    contextWithoutJobRunId.getSpanId() == -3342519250263117943L
+
+    contextWithoutJobId.getTraceId() == DDTraceId.ZERO
+    contextWithoutJobId.getSpanId() == 0
+
+    contextWithoutTaskRunId.getTraceId() == DDTraceId.ZERO
+    contextWithoutTaskRunId.getSpanId() == 0
   }
 }
