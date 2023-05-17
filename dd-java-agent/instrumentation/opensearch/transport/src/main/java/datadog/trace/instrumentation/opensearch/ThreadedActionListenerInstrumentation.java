e329aec9fe573a8f3fa10d06ba53c3276991ac0c
Ahmad Al-Masry <33571825+AhmadMasry@users.noreply.github.com>
2023-05-17 22:30:27 +0300

    Opensearch support (#1)
    Add support for Amazon Opensearch instrumentation.

---
+++ dd-java-agent/instrumentation/opensearch/transport/src/main/java/datadog/trace/instrumentation/opensearch/ThreadedActionListenerInstrumentation.java
@@ -0,0 +1,74 @@
+package datadog.trace.instrumentation.opensearch;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.namedOneOf;
+import static datadog.trace.bootstrap.instrumentation.java.concurrent.AdviceUtils.capture;
+import static datadog.trace.bootstrap.instrumentation.java.concurrent.AdviceUtils.endTaskScope;
+import static datadog.trace.bootstrap.instrumentation.java.concurrent.AdviceUtils.startTaskScope;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isConstructor;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.bootstrap.InstrumentationContext;
+import datadog.trace.bootstrap.instrumentation.api.AgentScope;
+import datadog.trace.bootstrap.instrumentation.java.concurrent.State;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import org.opensearch.action.support.ThreadedActionListener;
+
+/**
+ * Captures context at the point a request is made, and ensures it propagates into asynchronous
+ * actions.
+ */
+@AutoService(Instrumenter.class)
+public final class ThreadedActionListenerInstrumentation extends Instrumenter.Tracing
+    implements Instrumenter.ForSingleType {
+
+  public ThreadedActionListenerInstrumentation() {
+    super("opensearch", "opensearch-transport");
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "org.opensearch.action.support.ThreadedActionListener";
+  }
+
+  @Override
+  public Map<String, String> contextStore() {
+    return singletonMap(
+        "org.opensearch.action.support.ThreadedActionListener", State.class.getName());
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    // only one constructor
+    transformation.applyAdvice(isConstructor(), getClass().getName() + "$Construct");
+    transformation.applyAdvice(
+        namedOneOf("onResponse", "onFailure").and(takesArguments(1)),
+        getClass().getName() + "$OnResponse");
+  }
+
+  @SuppressWarnings("rawtypes")
+  public static final class Construct {
+    @Advice.OnMethodExit
+    public static void after(@Advice.This ThreadedActionListener listener) {
+      capture(
+          InstrumentationContext.get(ThreadedActionListener.class, State.class), listener, true);
+    }
+  }
+
+  @SuppressWarnings("rawtypes")
+  public static final class OnResponse {
+    @Advice.OnMethodEnter
+    public static AgentScope before(@Advice.This ThreadedActionListener listener) {
+      return startTaskScope(
+          InstrumentationContext.get(ThreadedActionListener.class, State.class), listener);
+    }
+
+    @Advice.OnMethodExit(onThrowable = Throwable.class)
+    public static void after(@Advice.Enter AgentScope scope) {
+      endTaskScope(scope);
+    }
+  }
+}
