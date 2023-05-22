200f3684e79178ae7a534d69d6ffbed05d07d17a
Val Pliskin <valeri.pliskin@datadoghq.com>
2023-05-22 12:47:22 +0300

    spotless fixes

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/usm/UsmExtractorImpl.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/usm/UsmExtractorImpl.java
@@ -5,10 +5,9 @@ import com.sun.jna.Native;
 import com.sun.jna.NativeLong;
 import com.sun.jna.Pointer;
 import datadog.trace.bootstrap.instrumentation.usm.Extractor;
+import java.nio.Buffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.nio.Buffer;
 
 public class UsmExtractorImpl implements Extractor {
   private static final Logger log = LoggerFactory.getLogger(UsmExtractorImpl.class);
@@ -22,19 +21,22 @@ public class UsmExtractorImpl implements Extractor {
   }
 
   public void send(Buffer buffer) {
-    if (!buffer.isDirect()){
+    if (!buffer.isDirect()) {
       log.error("message buffer is not direct ");
       return;
     }
 
-    log.debug("sending ioctl: " + String.format("%08x", USM_IOCTL_ID.intValue()) + " with buffer of size: " + buffer.position());
+    log.debug(
+        "sending ioctl: "
+            + String.format("%08x", USM_IOCTL_ID.intValue())
+            + " with buffer of size: "
+            + buffer.position());
     NativeLong res =
         CLibrary.Instance.ioctl(
             new NativeLong(0),
             USM_IOCTL_ID,
             Pointer.nativeValue(Native.getDirectBufferPointer(buffer)));
     log.debug("ioctl result: " + String.format("%08x", res.intValue()));
-
   }
 
   public static void registerAsSupplier() {
