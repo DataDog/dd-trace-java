f1a98c6f3a8405cced479109c48b9010631f6b3d
Gustavo Lopes <mail@geleia.net>
2023-05-26 15:09:49 +0100

    Multipart support on undertow
    Non-blocking parsing not yet supported.

---
+++ dd-java-agent/instrumentation/undertow/undertow-2.2/src/test/groovy/BodyMultipartServlet.groovy
@@ -0,0 +1,22 @@
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+
+import static datadog.trace.agent.test.base.HttpServerTest.ServerEndpoint.BODY_MULTIPART
+import static datadog.trace.agent.test.base.HttpServerTest.controller
+
+class BodyMultipartServlet extends HttpServlet {
+  @Override
+  protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+    controller(BODY_MULTIPART) {
+      resp.status = BODY_MULTIPART.status
+      resp.writer.print(
+        req.parameterMap
+        .findAll{
+          it.key != 'ignore'
+        }
+        .collectEntries {[it.key, it.value as List]} as String)
+    }
+  }
+}
