b7479afda78f8c1fe14b327148e43652d5858e06
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2023-06-05 18:40:08 +0200

    Fix TestNG onBeforeClass/onAfterClass notifications for cases when only a portion of class' test methods is executed

--- dd-java-agent/instrumentation/testng/src/testFixtures/groovy/datadog/trace/instrumentation/testng/TestNGTest.groovy
+++ dd-java-agent/instrumentation/testng/src/testFixtures/groovy/datadog/trace/instrumentation/testng/TestNGTest.groovy
@@ -577,6 +577,59 @@ abstract class TestNGTest extends CiVisibilityTest {
     })
   }
 
+  def "test successful test cases executed in parallel with TESTS parallel mode and not all test methods included"() {
+    setup:
+    def suiteXml = """
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd" >
+<suite name="API Test Suite" parallel="tests" configfailurepolicy="continue">
+    <test name="Test A">
+        <classes>
+            <class name="org.example.TestSucceedThreeCases">
+                <methods>
+                    <include name="test_succeed_a"/>
+                </methods>
+            </class>
+        </classes>
+    </test>
+
+    <test name="Test B">
+        <classes>
+            <class name="org.example.TestSucceedThreeCases">
+                <methods>
+                    <include name="test_succeed_b"/>
+                </methods>
+            </class>
+        </classes>
+    </test>
+</suite>
+    """
+
+    def parser = new SuiteXmlParser()
+    def xmlSuite = parser.parse("testng.xml", new ByteArrayInputStream(suiteXml.bytes), true)
+
+    def testNG = new TestNG()
+    testNG.setOutputDirectory(testOutputDir)
+    testNG.setParallel("tests")
+    testNG.setXmlSuites(Collections.singletonList(xmlSuite))
+    testNG.run()
+
+    expect:
+    ListWriterAssert.assertTraces(TEST_WRITER, 3, false, SORT_TRACES_BY_DESC_SIZE_THEN_BY_NAMES, {
+      long testModuleId
+      long testSuiteId
+      trace(2, true) {
+        testModuleId = testModuleSpan(it, 0, CIConstants.TEST_PASS)
+        testSuiteId = testSuiteSpan(it, 1, testModuleId, "org.example.TestSucceedThreeCases", CIConstants.TEST_PASS)
+      }
+      trace(1) {
+        testSpan(it, 0, testModuleId, testSuiteId, "org.example.TestSucceedThreeCases", "test_succeed_a", CIConstants.TEST_PASS)
+      }
+      trace(1) {
+        testSpan(it, 0, testModuleId, testSuiteId, "org.example.TestSucceedThreeCases", "test_succeed_b", CIConstants.TEST_PASS)
+      }
+    })
+  }
+
   @Override
   String expectedOperationPrefix() {
     return "testng"
---
+++ dd-java-agent/instrumentation/testng/testng-7.5/src/main/java/datadog/trace/instrumentation/testng75/TestNGTestRunnerInstrumentation.java
@@ -0,0 +1,63 @@
+package datadog.trace.instrumentation.testng75;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesNoArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.instrumentation.testng.TestNGClassListener;
+import datadog.trace.instrumentation.testng.TestNGUtils;
+import datadog.trace.util.Strings;
+import net.bytebuddy.asm.Advice;
+import org.testng.ITestNGMethod;
+import org.testng.TestRunner;
+import org.testng.annotations.DataProvider;
+
+@AutoService(Instrumenter.class)
+public class TestNGTestRunnerInstrumentation extends Instrumenter.CiVisibility
+    implements Instrumenter.ForSingleType {
+
+  private final String commonPackageName = Strings.getPackageName(TestNGUtils.class.getName());
+
+  public TestNGTestRunnerInstrumentation() {
+    super("testng-75-test-runner");
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "org.testng.TestRunner";
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    transformation.applyAdvice(
+        named("run").and(takesNoArguments()),
+        TestNGTestRunnerInstrumentation.class.getName() + "$InvokeRunAdvice");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      commonPackageName + ".TestNGUtils",
+      commonPackageName + ".TestNGClassListener",
+      commonPackageName + ".TracingListener"
+    };
+  }
+
+  public static class InvokeRunAdvice {
+    @Advice.OnMethodEnter
+    public static void invokeRun(@Advice.This final TestRunner testRunner) {
+      ITestNGMethod[] testMethods = testRunner.getAllTestMethods();
+      TestNGClassListener listener = TestNGUtils.getTestNGClassListener(testRunner);
+      listener.registerTestMethods(testMethods);
+    }
+
+    // TestNG 6.4 and above
+    public static void muzzleCheck(
+        final DataProvider dataProvider,
+        final org.testng.internal.invokers.TestMethodWorker testMethodWorker) {
+      testMethodWorker.getTestResults();
+      dataProvider.name();
+    }
+  }
+}
