726811c620d6899db5da000f28f91275bfb6fdaa
Javier Santos <javier.santos@datadoghq.com>
2023-06-09 09:38:39 +0200

    clean up

--- dd-java-agent/instrumentation/vertx-web-3.9/src/main/java/datadog/trace/instrumentation/vertx_3_9/Vertx39HttpServertResponseInstrumentation.java
+++ dd-java-agent/instrumentation/vertx-web-3.9/src/main/java/datadog/trace/instrumentation/vertx_3_9/Vertx39HttpServertResponseInstrumentation.java
@@ -1,24 +1,23 @@
 package datadog.trace.instrumentation.vertx_3_9;
 
-import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
-import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.isPublic;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
-
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.api.iast.InstrumentationBridge;
-import datadog.trace.api.iast.sink.InsecureCookieModule;
 import io.vertx.core.http.Cookie;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
+import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+
 @AutoService(Instrumenter.class)
 public class Vertx39HttpServertResponseInstrumentation extends Instrumenter.Iast
     implements Instrumenter.ForTypeHierarchy {
   public Vertx39HttpServertResponseInstrumentation() {
-    super("vertx", "vertx-4.0", "response");
+    super("vertx", "vertx-3.9", "response");
   }
 
   @Override
@@ -43,10 +42,7 @@ public class Vertx39HttpServertResponseInstrumentation extends Instrumenter.Iast
   public static class InstrumenterAdvice {
     @Advice.OnMethodEnter(suppress = Throwable.class)
     public static void onEnter(@Advice.Argument(0) final Cookie cookie) {
-      final InsecureCookieModule module = InstrumentationBridge.INSECURE_COOKIE;
-      if (module != null) {
-        InstrumentationBridge.onHeader("Set-Cookie", cookie.encode());
-      }
+      InstrumentationBridge.onHeader("Set-Cookie", cookie.encode());
     }
   }
 }
