a835012c07a6c860c28b25ea62c66d7ad02023d7
Igor Kravchenko <21974069+kr-igor@users.noreply.github.com>
2023-06-30 12:04:36 -0500

    Added javadoc for setDataStreamCheckpoint

--- internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/AgentTracer.java
+++ internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/AgentTracer.java
@@ -194,6 +194,16 @@ public class AgentTracer {
 
     CallbackProvider getUniversalCallbackProvider();
 
+    /**
+     * Sets data streams checkpoint, used for both produce and consume operations.
+     *
+     * @param span active span
+     * @param sortedTags alphabetically sorted tags for the checkpoint (direction, queue type etc)
+     * @param defaultTimestamp unix timestamp to use as a start of the pathway if this is the first checkpoint in the chain.
+     *                         Zero should be passed if we can't extract the timestamp from the message / payload itself (for instance:
+     *                         produce operations; http produce / consume etc). Value will be ignored for checkpoints happening not at the start
+     *                         of the pipeline.
+     */
     void setDataStreamCheckpoint(
         AgentSpan span, LinkedHashMap<String, String> sortedTags, long defaultTimestamp);
 
