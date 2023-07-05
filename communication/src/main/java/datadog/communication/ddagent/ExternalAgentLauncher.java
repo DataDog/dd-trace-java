ad0f8d8d8b01a3db7a968825a728d80527e1db97
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2023-07-05 12:00:14 +0100

    Discard output from spawned datadog-trace-agent and dogstatsd processes to avoid blocking

--- communication/src/main/java/datadog/communication/ddagent/ExternalAgentLauncher.java
+++ communication/src/main/java/datadog/communication/ddagent/ExternalAgentLauncher.java
@@ -1,13 +1,18 @@
 package datadog.communication.ddagent;
 
 import datadog.trace.api.Config;
+import datadog.trace.api.Platform;
 import datadog.trace.util.ProcessSupervisor;
 import java.io.Closeable;
+import java.io.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExternalAgentLauncher implements Closeable {
   private static final Logger log = LoggerFactory.getLogger(ExternalAgentLauncher.class);
+
+  private static final ProcessBuilder.Redirect DISCARD =
+      ProcessBuilder.Redirect.to(new File((Platform.isWindows() ? "NUL" : "/dev/null")));
 
   private ProcessSupervisor traceProcessSupervisor;
   private ProcessSupervisor dogStatsDProcessSupervisor;
@@ -16,6 +21,8 @@ public class ExternalAgentLauncher implements Closeable {
     if (config.isAzureAppServices()) {
       if (config.getTraceAgentPath() != null) {
         ProcessBuilder traceProcessBuilder = new ProcessBuilder(config.getTraceAgentPath());
+        traceProcessBuilder.redirectOutput(DISCARD);
+        traceProcessBuilder.redirectError(DISCARD);
         traceProcessBuilder.command().addAll(config.getTraceAgentArgs());
 
         traceProcessSupervisor = new ProcessSupervisor("datadog-trace-agent", traceProcessBuilder);
@@ -25,6 +32,8 @@ public class ExternalAgentLauncher implements Closeable {
 
       if (config.getDogStatsDPath() != null) {
         ProcessBuilder dogStatsDProcessBuilder = new ProcessBuilder(config.getDogStatsDPath());
+        dogStatsDProcessBuilder.redirectOutput(DISCARD);
+        dogStatsDProcessBuilder.redirectError(DISCARD);
         dogStatsDProcessBuilder.command().addAll(config.getDogStatsDArgs());
 
         dogStatsDProcessSupervisor = new ProcessSupervisor("dogstatsd", dogStatsDProcessBuilder);
