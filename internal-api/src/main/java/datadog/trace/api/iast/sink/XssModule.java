39796d4bea07ad4232288e3b927cd02f519de98d
Alejandro González García <alejandro.gonzalez@datadoghq.com>
2023-07-19 22:13:42 +0200

    Add support to IAST XSS vulnerability (#5589)
    ##What Does This Do
    New XSS Module
    Add Call Site Instrumentation to java.io.PrintWriter methods used in OWASP Benchmark
    Add spring-boot smoke tests
    
    ##Motivation
    Pass OWASP Benchmark related to XSS vulnerability
    
    ##Additional Notes
    Full XSS topic is not covered, only the necessary for OWASP Benchmark

---
+++ internal-api/src/main/java/datadog/trace/api/iast/sink/XssModule.java
@@ -0,0 +1,14 @@
+package datadog.trace.api.iast.sink;
+
+import datadog.trace.api.iast.IastModule;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public interface XssModule extends IastModule {
+
+  void onXss(@Nonnull String s);
+
+  void onXss(@Nonnull char[] array);
+
+  void onXss(@Nonnull String format, @Nullable Object[] args);
+}
