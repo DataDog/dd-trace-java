924bbfe0ebf8b17710d675a5592aa299df901814
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2023-07-19 12:08:39 +0200

    Add a temporary workaround for integrationt tests shared state dependency

--- dd-java-agent/instrumentation/junit-5.3/src/main/java/datadog/trace/instrumentation/junit5/TestEventsHandlerHolder.java
+++ dd-java-agent/instrumentation/junit-5.3/src/main/java/datadog/trace/instrumentation/junit5/TestEventsHandlerHolder.java
@@ -6,8 +6,18 @@ import java.nio.file.Paths;
 
 public abstract class TestEventsHandlerHolder {
 
-  public static final TestEventsHandler TEST_EVENTS_HANDLER =
-      InstrumentationBridge.createTestEventsHandler("junit", Paths.get("").toAbsolutePath());
+  public static volatile TestEventsHandler TEST_EVENTS_HANDLER;
+
+  static {
+    reset();
+  }
 
   private TestEventsHandlerHolder() {}
+
+  // TODO a hack to work around shared state problems in integration tests;
+  //  to be removed later
+  public static void reset() {
+    TEST_EVENTS_HANDLER =
+        InstrumentationBridge.createTestEventsHandler("junit", Paths.get("").toAbsolutePath());
+  }
 }
