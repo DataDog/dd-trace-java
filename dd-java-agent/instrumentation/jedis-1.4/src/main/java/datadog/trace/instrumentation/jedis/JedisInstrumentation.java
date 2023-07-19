383618f6b7c70347e9ee163d0f1aa83f094e6f94
songlq <18332731561@163.com>
2023-07-19 16:17:56 +0800

    jedis

--- dd-java-agent/instrumentation/jedis-1.4/src/main/java/datadog/trace/instrumentation/jedis/JedisInstrumentation.java
+++ dd-java-agent/instrumentation/jedis-1.4/src/main/java/datadog/trace/instrumentation/jedis/JedisInstrumentation.java
@@ -2,12 +2,10 @@ package datadog.trace.instrumentation.jedis;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
-import datadog.trace.bootstrap.CallDepthThreadLocalMap;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatcher;
-import redis.clients.jedis.Connection;
 import redis.clients.jedis.Protocol.Command;
 
 import static datadog.trace.agent.tooling.bytebuddy.matcher.ClassLoaderMatchers.hasClassNamed;
@@ -15,11 +13,15 @@ import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
 import static datadog.trace.instrumentation.jedis.JedisClientDecorator.DECORATE;
+import static datadog.trace.instrumentation.jedis.JedisClientDecorator.REDIS_COMMAND;
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
 @AutoService(Instrumenter.class)
 public final class JedisInstrumentation extends Instrumenter.Tracing
     implements Instrumenter.ForSingleType {
+
+  private static final String SERVICE_NAME = "redis";
+  private static final String COMPONENT_NAME = SERVICE_NAME + "-command";
 
   public JedisInstrumentation() {
     super("jedis", "redis");
@@ -33,13 +35,13 @@ public final class JedisInstrumentation extends Instrumenter.Tracing
 
   @Override
   public String instrumentedType() {
-    return "redis.clients.jedis.Connection";
+    return "redis.clients.jedis.Protocol";
   }
 
   @Override
   public String[] helperClassNames() {
     return new String[] {
-      packageName + ".JedisClientDecorator",
+        packageName + ".JedisClientDecorator",
     };
   }
 
@@ -47,24 +49,20 @@ public final class JedisInstrumentation extends Instrumenter.Tracing
   public void adviceTransformations(AdviceTransformation transformation) {
     transformation.applyAdvice(
         isMethod()
+            .and(isPublic())
             .and(named("sendCommand"))
-            .and(takesArgument(0, named("redis.clients.jedis.Protocol$Command"))),
+            .and(takesArgument(1, named("redis.clients.jedis.Protocol$Command"))),
         JedisInstrumentation.class.getName() + "$JedisAdvice");
     // FIXME: This instrumentation only incorporates sending the command, not processing the result.
   }
 
   public static class JedisAdvice {
+
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static AgentScope onEnter(
-        @Advice.Argument(0) final Command command, @Advice.This final Connection thiz,@Advice.Argument(2)final byte[][] args) {
-      if (CallDepthThreadLocalMap.incrementCallDepth(Connection.class) > 0) {
-        return null;
-      }
-      final AgentSpan span = startSpan(JedisClientDecorator.OPERATION_NAME);
+    public static AgentScope onEnter(@Advice.Argument(1) final Command command, @Advice.Argument(2)final byte[][] args) {
+      final AgentSpan span = startSpan(REDIS_COMMAND);
       DECORATE.afterStart(span);
-      DECORATE.onConnection(span, thiz);
       DECORATE.onStatement(span, command.name());
-      //System.out.println("------- set raw "+ new String(command.raw));
       StringBuilder args1 = new StringBuilder();
       for(int i = 0; i < args.length; i++) {
         args1.append(new String(args[i]));
@@ -77,10 +75,6 @@ public final class JedisInstrumentation extends Instrumenter.Tracing
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
         @Advice.Enter final AgentScope scope, @Advice.Thrown final Throwable throwable) {
-      if (scope == null) {
-        return;
-      }
-      CallDepthThreadLocalMap.reset(Connection.class);
       DECORATE.onError(scope.span(), throwable);
       DECORATE.beforeFinish(scope.span());
       scope.close();
--- dd-java-agent/instrumentation/jedis-3.0/src/main/java/datadog/trace/instrumentation/jedis30/JedisInstrumentation.java
+++ dd-java-agent/instrumentation/jedis-3.0/src/main/java/datadog/trace/instrumentation/jedis30/JedisInstrumentation.java
@@ -1,21 +1,19 @@
 package datadog.trace.instrumentation.jedis30;
 
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.bootstrap.instrumentation.api.AgentScope;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import net.bytebuddy.asm.Advice;
+import redis.clients.jedis.Protocol;
+import redis.clients.jedis.commands.ProtocolCommand;
+
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
 import static datadog.trace.instrumentation.jedis30.JedisClientDecorator.DECORATE;
-import static net.bytebuddy.matcher.ElementMatchers.isMethod;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
-
-import com.google.auto.service.AutoService;
-import datadog.trace.agent.tooling.Instrumenter;
-import datadog.trace.bootstrap.CallDepthThreadLocalMap;
-import datadog.trace.bootstrap.instrumentation.api.AgentScope;
-import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
-import net.bytebuddy.asm.Advice;
-import redis.clients.jedis.Connection;
-import redis.clients.jedis.Protocol;
-import redis.clients.jedis.commands.ProtocolCommand;
+import static datadog.trace.instrumentation.jedis30.JedisClientDecorator.REDIS_COMMAND;
+import static net.bytebuddy.matcher.ElementMatchers.*;
 
 @AutoService(Instrumenter.class)
 public final class JedisInstrumentation extends Instrumenter.Tracing
@@ -26,23 +24,24 @@ public final class JedisInstrumentation extends Instrumenter.Tracing
   }
 
   @Override
-  public String[] helperClassNames() {
-    return new String[] {
-      packageName + ".JedisClientDecorator",
-    };
+  public String instrumentedType() {
+    return "redis.clients.jedis.Protocol";
   }
 
   @Override
-  public String instrumentedType() {
-    return "redis.clients.jedis.Connection";
+  public String[] helperClassNames() {
+    return new String[] {
+        packageName + ".JedisClientDecorator",
+    };
   }
 
   @Override
   public void adviceTransformations(AdviceTransformation transformation) {
     transformation.applyAdvice(
         isMethod()
+            .and(isPublic())
             .and(named("sendCommand"))
-            .and(takesArgument(0, named("redis.clients.jedis.commands.ProtocolCommand"))),
+            .and(takesArgument(1, named("redis.clients.jedis.commands.ProtocolCommand"))),
         JedisInstrumentation.class.getName() + "$JedisAdvice");
     // FIXME: This instrumentation only incorporates sending the command, not processing the result.
   }
@@ -50,17 +49,10 @@ public final class JedisInstrumentation extends Instrumenter.Tracing
   public static class JedisAdvice {
 
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static AgentScope onEnter(
-        @Advice.Argument(0) final ProtocolCommand command,
-        @Advice.This final Connection thiz,
-        @Advice.Argument(2)final byte[][] args) {
-      if (CallDepthThreadLocalMap.incrementCallDepth(Connection.class) > 0) {
-        return null;
-      }
-      final AgentSpan span = startSpan(JedisClientDecorator.OPERATION_NAME);
+    public static AgentScope onEnter(@Advice.Argument(1) final ProtocolCommand command,
+                                     @Advice.Argument(2)final byte[][] args ) {
+      final AgentSpan span = startSpan(REDIS_COMMAND);
       DECORATE.afterStart(span);
-      DECORATE.onConnection(span, thiz);
-
       if (command instanceof Protocol.Command) {
         DECORATE.onStatement(span, ((Protocol.Command) command).name());
       } else {
@@ -81,10 +73,6 @@ public final class JedisInstrumentation extends Instrumenter.Tracing
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
         @Advice.Enter final AgentScope scope, @Advice.Thrown final Throwable throwable) {
-      if (scope == null) {
-        return;
-      }
-      CallDepthThreadLocalMap.reset(Connection.class);
       DECORATE.onError(scope.span(), throwable);
       DECORATE.beforeFinish(scope.span());
       scope.close();
