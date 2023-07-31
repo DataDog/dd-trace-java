e1754c236db32e6aabc8f352293beca90e963c74
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2023-07-31 13:48:00 +0200

    Add initial version of tracer benchmarks

---
+++ benchmark/run.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -eu
+
+readonly SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+readonly INITIAL_DIR="$(pwd)"
+readonly USER_ID="$(id -u)"
+readonly GROUP_ID="$(id -g)"
+readonly TRACER="${SCRIPT_DIR}/tracer/dd-java-agent.jar"
+
+cd "${SCRIPT_DIR}"
+
+# Build container image
+echo "Building base image ..."
+docker build \
+  --build-arg="UID=${USER_ID}" \
+  --build-arg="GID=${GROUP_ID}" \
+  -t dd-trace-java/benchmark \
+  .
+
+# Find or rebuild tracer to be used in the benchmarks
+if [[ ! -f "${TRACER}" ]]; then
+  mkdir -p "${SCRIPT_DIR}/tracer"
+  cd "${SCRIPT_DIR}/.."
+  readonly TRACER_VERSION=$(./gradlew properties -q | grep "version:" | awk '{print $2}')
+  readonly TRACER_COMPILED="${SCRIPT_DIR}/../dd-java-agent/build/libs/dd-java-agent-${TRACER_VERSION}.jar"
+  if [ ! -f "${TRACER_COMPILED}" ]; then
+    echo "Tracer not found, starting gradle compile ..."
+    ./gradlew assemble
+  fi
+  cp "${TRACER_COMPILED}" "${TRACER}"
+  cd "${SCRIPT_DIR}"
+fi
+
+# Trigger benchmarks
+echo "Running benchmarks ..."
+docker run --rm \
+  -u "${USER_ID}:${GROUP_ID}" \
+  -v "${HOME}/.gradle":/home/benchmark/.gradle:delegated \
+  -v "${PWD}/..":/tracer:delegated \
+  -w /tracer/benchmark \
+  -e GRADLE_OPTS="-Dorg.gradle.daemon=false" \
+  --entrypoint=./benchmarks.sh \
+  --name dd-trace-java-benchmark \
+  --cap-add SYS_ADMIN \
+  dd-trace-java/benchmark \
+  "$@"
+
+cd "${INITIAL_DIR}"
