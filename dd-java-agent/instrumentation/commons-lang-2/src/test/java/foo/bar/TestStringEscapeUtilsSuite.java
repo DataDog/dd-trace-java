c5fdc6570ba047670abbce42f9d6c9e53ff96d84
Alejandro González García <alejandro.gonzalez@datadoghq.com>
2023-08-02 14:22:52 +0200

    Extend apache commons StringEscapeUtils tainting support (#5638)
    ##What Does This Do
    Extends org.apache.commons.lang.StringEscapeUtils tainting support
    Add org.apache.commons.lang3.StringEscapeUtils tainting support
    Add org.apache.commons.text.StringEscapeUtils tainting support

--- dd-java-agent/instrumentation/commons-lang-2/src/test/java/foo/bar/TestStringEscapeUtilsSuite.java
+++ dd-java-agent/instrumentation/commons-lang-2/src/test/java/foo/bar/TestStringEscapeUtilsSuite.java
@@ -7,4 +7,20 @@ public class TestStringEscapeUtilsSuite {
   public static String escapeHtml(String input) {
     return StringEscapeUtils.escapeHtml(input);
   }
+
+  public static String escapeJava(String input) {
+    return StringEscapeUtils.escapeJava(input);
+  }
+
+  public static String escapeJavaScript(String input) {
+    return StringEscapeUtils.escapeJavaScript(input);
+  }
+
+  public static String escapeXml(String input) {
+    return StringEscapeUtils.escapeXml(input);
+  }
+
+  public static String escapeSql(String input) {
+    return StringEscapeUtils.escapeSql(input);
+  }
 }
