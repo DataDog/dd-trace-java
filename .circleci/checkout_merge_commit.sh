0812a733086daf765356d2a66176d8c4ead93cb5
Santiago Mola <santiago.mola@datadoghq.com>
2023-08-08 12:58:48 +0200

    wip

--- .circleci/checkout_merge_commit.sh
+++ .circleci/checkout_merge_commit.sh
@@ -14,7 +14,7 @@ if [[ "$CIRCLE_BRANCH" != "master" && -n "${CCI_PR_NUMBER}" ]]; then
   FETCH_REFS="${FETCH_REFS:-} +refs/pull/${CCI_PR_NUMBER}/merge:refs/pull/${CCI_PR_NUMBER}/merge +refs/pull/${CCI_PR_NUMBER}/head:refs/pull/${CCI_PR_NUMBER}/head"
   git fetch -u origin ${FETCH_REFS}
 
-  if git merge-base --is-ancestor "$(git show-ref --hash refs/pull/${CCI_PR_NUMBER}/head) $(git show-ref --hash refs/pull/${CCI_PR_NUMBER}/merge)"; then
+  if git merge-base --is-ancestor "$(git show-ref --hash refs/pull/${CCI_PR_NUMBER}/head)" "$(git show-ref --hash refs/pull/${CCI_PR_NUMBER}/merge)"; then
     git checkout "pull/${CCI_PR_NUMBER}/merge"
   else
     echo "[WARN] There is a merge conflict between master and PR ${CCI_PR_NUMBER}, merge branch cannot be checked out."
