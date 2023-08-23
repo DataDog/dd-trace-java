b2481bc94dc771ef52acbef8bea49f35de256b26
Bruce Bujon <bruce.bujon@datadoghq.com>
2023-08-23 14:44:23 +0200

    feat(otel): Add RxJava async result type support

---
+++ dd-java-agent/instrumentation/rxjava-2/src/main/java/datadog/trace/instrumentation/rxjava2/RxJavaPluginsInstrumentation.java
@@ -0,0 +1,47 @@
+package datadog.trace.instrumentation.rxjava2;
+
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.api.InstrumenterConfig;
+import net.bytebuddy.asm.Advice;
+
+@AutoService(Instrumenter.class)
+public class RxJavaPluginsInstrumentation extends Instrumenter.Tracing
+    implements Instrumenter.ForSingleType {
+
+  public RxJavaPluginsInstrumentation() {
+    super("rxjava");
+  }
+
+  @Override
+  protected boolean defaultEnabled() {
+    // Only used with OpenTelemetry @WithSpan annotations
+    return InstrumenterConfig.get().isTraceOtelEnabled();
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "io.reactivex.plugins.RxJavaPlugins";
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      packageName + ".RxJavaAsyncResultSupportExtension",
+    };
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    transformation.applyAdvice(isMethod(), getClass().getName() + "$RxJavaPluginsAdvice");
+  }
+
+  public static class RxJavaPluginsAdvice {
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void init() {
+      RxJavaAsyncResultSupportExtension.initialize();
+    }
+  }
+}
