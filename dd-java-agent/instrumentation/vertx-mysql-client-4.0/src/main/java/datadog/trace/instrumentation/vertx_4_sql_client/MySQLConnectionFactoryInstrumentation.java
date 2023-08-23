e7f734cf23e010609d353563819c3f81599e959c
manuj.garg <manuj@fancode.com>
2023-08-23 19:36:51 +0530

    chore: add vertx4 mysql client instrumentation

---
+++ dd-java-agent/instrumentation/vertx-mysql-client-4.0/src/main/java/datadog/trace/instrumentation/vertx_4_sql_client/MySQLConnectionFactoryInstrumentation.java
@@ -0,0 +1,36 @@
+package datadog.trace.instrumentation.vertx_4_sql_client;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isConstructor;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.bootstrap.instrumentation.jdbc.DBInfo;
+import java.util.Map;
+
+@AutoService(Instrumenter.class)
+public class MySQLConnectionFactoryInstrumentation extends Instrumenter.Tracing
+    implements Instrumenter.ForSingleType {
+  public MySQLConnectionFactoryInstrumentation() {
+    super("vertx", "vertx-sql-client");
+  }
+
+  @Override
+  public Map<String, String> contextStore() {
+    return singletonMap("io.vertx.mysqlclient.impl.MySQLConnectionFactory", DBInfo.class.getName());
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "io.vertx.mysqlclient.impl.MySQLConnectionFactory";
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    transformation.applyAdvice(
+        isConstructor().and(takesArgument(2, named("io.vertx.mysqlclient.MySQLConnectOptions"))),
+        packageName + ".MySQLConnectionFactoryConstructorAdvice");
+  }
+}
