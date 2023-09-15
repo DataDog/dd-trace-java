f286bf3ff50c05eb70236cb6e7a84080052c119c
Douglas Q Hawkins <dougqh@gmail.com>
2023-09-15 16:47:14 -0400

    Resource name matching
    This changes adds resource name matching to the trace sampling rules.
    
    This change also changes matching on service and operation name to use glob matching -- which does introduce a small breaking change if someone used * or ? in their name previously
    
    In addition to the core change to trace sampling rules, this code also cleans up the handling of glob matching
    
    New util classes Matcher and Matchers have now been introduced that handle most of the glob related code
    
    This clean-up allowed me to eliminate some existing duplication in the single span code
    
    In a subsequent changeset, I may replace the pattern based glob matching with a proper glob matcher.

--- dd-trace-core/src/test/groovy/datadog/trace/common/sampling/TraceSamplingRulesTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/common/sampling/TraceSamplingRulesTest.groovy
@@ -18,6 +18,7 @@ class TraceSamplingRulesTest extends DDCoreSpecification {
       {\"service\": \"usersvc\", \"name\": \"healthcheck\", \"sample_rate\": 0.0}, 
       {\"service\": \"service-name\", \"sample_rate\": 0.5}, 
       {\"name\": \"operation-name\", \"sample_rate\": 0.75}, 
+      {\"resource\": \"resource-name\", \"sample_rate\": 1},
       {\"sample_rate\": 0.25}
     ]""")
 
@@ -26,10 +27,12 @@ class TraceSamplingRulesTest extends DDCoreSpecification {
 
     rules.rules[0].service == "usersvc"
     rules.rules[0].name == "healthcheck"
+    rules.rules[0].resource == null    
     rules.rules[0].sampleRate == 0.0d
 
     rules.rules[1].service == "service-name"
     rules.rules[1].name == null
+    rules.rules[1].resource == null
     rules.rules[1].sampleRate == 0.5d
 
     rules.rules[2].service == null
@@ -38,7 +41,13 @@ class TraceSamplingRulesTest extends DDCoreSpecification {
 
     rules.rules[3].service == null
     rules.rules[3].name == null
-    rules.rules[3].sampleRate == 0.25d
+    rules.rules[3].resource == "resource-name"
+    rules.rules[3].sampleRate == 1d
+    
+    rules.rules[4].service == null
+    rules.rules[4].name == null
+    rules.rules[4].resource == null
+    rules.rules[4].sampleRate == 0.25d
   }
 
   def "Skip Trace Sampling Rules with invalid rate values"() {
