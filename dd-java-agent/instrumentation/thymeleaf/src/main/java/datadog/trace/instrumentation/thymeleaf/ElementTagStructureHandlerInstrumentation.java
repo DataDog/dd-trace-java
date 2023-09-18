a03bc0213fb4d4a885dd799008aca9e2562d3e50
Alejandro Gonzalez <alejandro.gonzalez@datadoghq.com>
2023-09-18 12:33:39 +0200

    Instrumentation working

--- dd-java-agent/instrumentation/thymeleaf/src/main/java/datadog/trace/instrumentation/thymeleaf/ElementTagStructureHandlerInstrumentation.java
+++ dd-java-agent/instrumentation/thymeleaf/src/main/java/datadog/trace/instrumentation/thymeleaf/ElementTagStructureHandlerInstrumentation.java
@@ -7,18 +7,7 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
-import datadog.trace.api.iast.InstrumentationBridge;
-import datadog.trace.api.iast.Sink;
-import datadog.trace.api.iast.VulnerabilityTypes;
-import datadog.trace.api.iast.sink.XssModule;
-import datadog.trace.bootstrap.ContextStore;
-import datadog.trace.bootstrap.InstrumentationContext;
 import java.util.Map;
-import net.bytebuddy.asm.Advice;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.thymeleaf.engine.ElementTagStructureHandler;
-import org.thymeleaf.processor.element.IElementTagStructureHandler;
 
 @AutoService(Instrumenter.class)
 public class ElementTagStructureHandlerInstrumentation extends Instrumenter.Iast
@@ -36,37 +25,21 @@ public class ElementTagStructureHandlerInstrumentation extends Instrumenter.Iast
   public void adviceTransformations(AdviceTransformation transformation) {
     transformation.applyAdvice(
         isMethod().and(named("setBody")).and(takesArgument(0, CharSequence.class)),
-        ElementTagStructureHandlerInstrumentation.class.getName() + "$BodyAdvice");
+        packageName + ".BodyAdvice");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+        packageName + ".ThymeleafContext"
+    };
   }
 
   @Override
   public Map<String, String> contextStore() {
     return singletonMap(
         "org.thymeleaf.processor.element.IElementTagStructureHandler",
-        ThymeleafContext.class.getName());
+        "datadog.trace.instrumentation.thymeleaf.ThymeleafContext");
   }
 
-  public static class BodyAdvice {
-
-    public static final Logger log = LoggerFactory.getLogger(BodyAdvice.class);
-
-    @Advice.OnMethodExit(suppress = Throwable.class)
-    @Sink(VulnerabilityTypes.XSS)
-    public static void setBody(
-        @Advice.This ElementTagStructureHandler self, @Advice.Argument(0) final CharSequence text) {
-      final XssModule module = InstrumentationBridge.XSS;
-      if (module != null) {
-        ContextStore<IElementTagStructureHandler, ThymeleafContext> contextStore =
-            InstrumentationContext.get(IElementTagStructureHandler.class, ThymeleafContext.class);
-        log.debug("Get contextStore {}", contextStore);
-        log.debug("Try to get {} from contextStore", self);
-        ThymeleafContext ctx = contextStore.get(self);
-        log.debug("Get ThymeleafContext {}", ctx);
-        if (ctx != null) {
-          log.debug("Call module#onXss {} {} {}", text, ctx.getTemplateName(), ctx.getLine());
-          module.onXss(text, ctx.getTemplateName(), ctx.getLine());
-        }
-      }
-    }
-  }
 }
--- dd-java-agent/instrumentation/thymeleaf/src/main/java/datadog/trace/instrumentation/thymeleaf/StandardUtextTagProcessorInstrumentation.java
+++ dd-java-agent/instrumentation/thymeleaf/src/main/java/datadog/trace/instrumentation/thymeleaf/StandardUtextTagProcessorInstrumentation.java
@@ -3,22 +3,10 @@ package datadog.trace.instrumentation.thymeleaf;
 import static java.util.Collections.singletonMap;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
-import datadog.trace.api.iast.InstrumentationBridge;
-import datadog.trace.api.iast.Propagation;
-import datadog.trace.bootstrap.ContextStore;
-import datadog.trace.bootstrap.InstrumentationContext;
 import java.util.Map;
-import net.bytebuddy.asm.Advice;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.thymeleaf.context.ITemplateContext;
-import org.thymeleaf.engine.AttributeName;
-import org.thymeleaf.model.IProcessableElementTag;
-import org.thymeleaf.processor.element.IElementTagStructureHandler;
 
 @AutoService(Instrumenter.class)
 public class StandardUtextTagProcessorInstrumentation extends Instrumenter.Iast
@@ -32,20 +20,22 @@ public class StandardUtextTagProcessorInstrumentation extends Instrumenter.Iast
   public void adviceTransformations(AdviceTransformation transformation) {
     transformation.applyAdvice(
         isMethod()
-            .and(named("doProcess"))
-            .and(takesArgument(0, ITemplateContext.class))
-            .and(takesArgument(1, IProcessableElementTag.class))
-            .and(takesArgument(2, AttributeName.class))
-            .and(takesArgument(3, String.class))
-            .and(takesArgument(4, IElementTagStructureHandler.class)),
-        StandardUtextTagProcessorInstrumentation.class.getName() + "$ProcessAdvice");
+            .and(named("doProcess")),
+        packageName + ".ProcessAdvice");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+        packageName + ".ThymeleafContext"
+    };
   }
 
   @Override
   public Map<String, String> contextStore() {
     return singletonMap(
         "org.thymeleaf.processor.element.IElementTagStructureHandler",
-        ThymeleafContext.class.getName());
+        "datadog.trace.instrumentation.thymeleaf.ThymeleafContext");
   }
 
   @Override
@@ -53,27 +43,4 @@ public class StandardUtextTagProcessorInstrumentation extends Instrumenter.Iast
     return "org.thymeleaf.standard.processor.StandardUtextTagProcessor";
   }
 
-  public static class ProcessAdvice {
-
-    public static final Logger log = LoggerFactory.getLogger(ProcessAdvice.class);
-
-    @Advice.OnMethodEnter(suppress = Throwable.class)
-    @Propagation
-    public static void doProcess(
-        @Advice.Argument(1) final IProcessableElementTag tag,
-        @Advice.Argument(4) final IElementTagStructureHandler handler) {
-      if (InstrumentationBridge.XSS != null) {
-        ContextStore<IElementTagStructureHandler, ThymeleafContext> contextStore =
-            InstrumentationContext.get(IElementTagStructureHandler.class, ThymeleafContext.class);
-        log.debug("ContextStore {}", contextStore);
-        contextStore.put(handler, new ThymeleafContext(tag.getTemplateName(), tag.getLine()));
-        log.debug(
-            "{} {} {} added on contextStore {}",
-            handler,
-            tag.getTemplateName(),
-            tag.getLine(),
-            contextStore);
-      }
-    }
-  }
 }
