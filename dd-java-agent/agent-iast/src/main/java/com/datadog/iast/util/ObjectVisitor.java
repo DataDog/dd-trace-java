03aa729526c8a8eeb7564dcb178c3813602ddd65
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2023-09-18 12:34:09 +0200

    Move to a recursive solution for the visitor

--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/util/ObjectVisitor.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/util/ObjectVisitor.java
@@ -1,26 +1,27 @@
 package com.datadog.iast.util;
 
+import static com.datadog.iast.util.ObjectVisitor.State.CONTINUE;
+import static com.datadog.iast.util.ObjectVisitor.State.EXIT;
+
 import datadog.trace.api.Platform;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Predicate;
 import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("JavaReflectionMemberAccess")
 public class ObjectVisitor {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ObjectVisitor.class);
 
   private static final int MAX_VISITED_OBJECTS = 1000;
   private static final int MAX_DEPTH = 10;
-
   private static final Method TRY_SET_ACCESSIBLE;
 
   static {
@@ -44,125 +45,119 @@ public class ObjectVisitor {
       @Nonnull final Visitor visitor,
       final int maxDepth,
       final int maxObjects) {
-    visit(
-        object,
-        visitor,
-        maxDepth,
-        maxObjects,
-        ObjectVisitor::includedClasses,
-        ObjectVisitor::includedFields);
+    new ObjectVisitor(maxDepth, maxObjects, visitor).visit(0, "root", object);
   }
 
-  @SuppressWarnings("unchecked")
-  public static void visit(
-      @Nonnull final Object object,
-      @Nonnull final Visitor visitor,
-      final int maxDepth,
-      final int maxObjects,
-      @Nonnull final Predicate<Class<?>> classes,
-      @Nonnull final Predicate<Field> fields) {
-    final Set<Object> visited = new HashSet<>();
-    final BoundedContextQueue queue = new BoundedContextQueue(maxDepth, maxObjects);
-    queue.push(0, "root", object);
-    // BFS
-    while (!queue.isEmpty()) {
-      final Context<?> current = queue.pop();
-      final Object value = current.value;
-      if (!visited.add(value)) {
-        continue;
-      }
-      try {
-        if (value instanceof Object[]) {
-          visitArray(queue, (Context<Object[]>) current);
-        } else if (value instanceof Map) {
-          visitMap(queue, (Context<Map<?, ?>>) current);
-        } else if (value instanceof Iterable) {
-          visitIterable(queue, (Context<Iterable<?>>) current);
-        } else {
-          final State state =
-              visitObject(queue, (Context<Object>) current, visitor, classes, fields);
-          if (state == State.EXIT) {
-            return;
-          }
-        }
-      } catch (final Throwable e) {
-        LOGGER.warn("Failed to visit object of type {}", value.getClass(), e);
-      }
+  private int remaining;
+  private final int maxDepth;
+  private final Set<Object> visited;
+  private final Visitor visitor;
+
+  private ObjectVisitor(final int maxDepth, final int maxObjects, final Visitor visitor) {
+    this.maxDepth = maxDepth;
+    this.remaining = maxObjects;
+    this.visited = new HashSet<>();
+    this.visitor = visitor;
+  }
+
+  private State visit(final int depth, final String path, final Object value) {
+    if (remaining <= 0) {
+      return EXIT;
     }
+    remaining--;
+    if (depth > maxDepth) {
+      return CONTINUE;
+    }
+    if (!visited.add(value)) {
+      return CONTINUE;
+    }
+    State state = CONTINUE;
+    try {
+      if (value instanceof Object[]) {
+        state = visitArray(depth, path, (Object[]) value);
+      } else if (value instanceof Map) {
+        state = visitMap(depth, path, (Map<?, ?>) value);
+      } else if (value instanceof Iterable) {
+        state = visitIterable(depth, path, (Iterable<?>) value);
+      } else {
+        state = visitObject(depth, path, value);
+      }
+    } catch (final Throwable e) {
+      LOGGER.warn("Failed to visit object of type {}", value.getClass(), e);
+    }
+    return state;
   }
 
-  private static void visitArray(
-      @Nonnull final BoundedContextQueue queue, @Nonnull final Context<Object[]> current) {
-    final int depth = current.depth + 1;
-    for (int i = 0; i < current.value.length; i++) {
-      final Object item = current.value[i];
+  private State visitArray(final int depth, final String path, final Object[] array) {
+    final int arrayDepth = depth + 1;
+    for (int i = 0; i < array.length; i++) {
+      final Object item = array[i];
       if (item != null) {
-        final String itemPath = current.path + "[" + i + "]";
-        if (!queue.push(depth, itemPath, item)) {
-          break;
+        final String itemPath = path + "[" + i + "]";
+        final State state = visit(arrayDepth, itemPath, item);
+        if (state != CONTINUE) {
+          return state;
         }
       }
     }
+    return CONTINUE;
   }
 
-  private static void visitMap(
-      @Nonnull final BoundedContextQueue queue, @Nonnull final Context<Map<?, ?>> current) {
-    final int depth = current.depth + 1;
-    for (final Map.Entry<?, ?> entry : current.value.entrySet()) {
+  private State visitMap(final int depth, final String path, final Map<?, ?> map) {
+    final int mapDepth = depth + 1;
+    for (final Map.Entry<?, ?> entry : map.entrySet()) {
       final Object key = entry.getKey();
       if (key != null) {
-        final String keyPath = current.path + "[]";
-        if (!queue.push(depth, keyPath, entry.getKey())) {
-          break;
+        final String keyPath = path + "[]";
+        final ObjectVisitor.State state = visit(mapDepth, keyPath, key);
+        if (state != CONTINUE) {
+          return state;
         }
       }
       final Object item = entry.getValue();
       if (item != null) {
-        final String itemPath = current.path + "[" + key + "]";
-        if (!queue.push(depth, itemPath, item)) {
-          break;
+        final String itemPath = path + "[" + key + "]";
+        final State state = visit(mapDepth, itemPath, item);
+        if (state != CONTINUE) {
+          return state;
         }
       }
     }
+    return CONTINUE;
   }
 
-  private static void visitIterable(
-      @Nonnull final BoundedContextQueue queue, @Nonnull final Context<Iterable<?>> current) {
-    final int depth = current.depth + 1;
+  private State visitIterable(final int depth, final String path, final Iterable<?> iterable) {
+    final int iterableDepth = depth + 1;
     int index = 0;
-    for (final Object item : current.value) {
+    for (final Object item : iterable) {
       if (item != null) {
-        final String itemPath = current.path + "[" + (index++) + "]";
-        if (!queue.push(depth, itemPath, item)) {
-          break;
+        final String itemPath = path + "[" + (index++) + "]";
+        final State state = visit(iterableDepth, itemPath, item);
+        if (state != CONTINUE) {
+          return state;
         }
       }
     }
+    return CONTINUE;
   }
 
-  private static State visitObject(
-      @Nonnull final BoundedContextQueue queue,
-      @Nonnull final Context<Object> current,
-      @Nonnull final Visitor visitor,
-      @Nonnull final Predicate<Class<?>> classes,
-      @Nonnull final Predicate<Field> fields) {
-    final int childDepth = current.depth + 1;
-    final String path = current.path;
-    final Object value = current.value;
-    final State state = visitor.visit(path, value);
-    if (state != State.CONTINUE || !classes.test(value.getClass())) {
+  private State visitObject(final int depth, final String path, final Object value) {
+    final int childDepth = depth + 1;
+    State state = visitor.visit(path, value);
+    if (state != State.CONTINUE || !inspectClass(value.getClass())) {
       return state;
     }
-    Class<?> klass = current.value.getClass();
+    Class<?> klass = value.getClass();
     while (klass != Object.class) {
       for (final Field field : klass.getDeclaredFields()) {
         try {
-          if (fields.test(field) && setAccessible(field)) {
+          if (inspectField(field) && setAccessible(field)) {
             final Object fieldValue = field.get(value);
             if (fieldValue != null) {
               final String fieldPath = path + "." + field.getName();
-              if (!queue.push(childDepth, fieldPath, fieldValue)) {
-                return State.CONTINUE;
+              state = visit(childDepth, fieldPath, fieldValue);
+              if (state != CONTINUE) {
+                return state;
               }
             }
           }
@@ -172,10 +167,10 @@ public class ObjectVisitor {
       }
       klass = klass.getSuperclass();
     }
-    return State.CONTINUE;
+    return ObjectVisitor.State.CONTINUE;
   }
 
-  public static boolean includedClasses(final Class<?> cls) {
+  private static boolean inspectClass(final Class<?> cls) {
     if (cls.isPrimitive()) {
       return false; // skip primitives
     }
@@ -185,7 +180,7 @@ public class ObjectVisitor {
     return true;
   }
 
-  public static boolean includedFields(final Field field) {
+  private static boolean inspectField(final Field field) {
     final int modifiers = field.getModifiers();
     if (Modifier.isStatic(modifiers)) {
       return false;
@@ -215,57 +210,12 @@ public class ObjectVisitor {
   }
 
   public interface Visitor {
-    State visit(@Nonnull final String path, @Nonnull final Object value);
+    @Nonnull
+    State visit(@Nonnull String path, @Nonnull Object value);
   }
 
   public enum State {
     CONTINUE,
     EXIT
-  }
-
-  private static class Context<E> {
-    private final int depth;
-    @Nonnull private final String path;
-    @Nonnull private final E value;
-
-    private Context(final int depth, @Nonnull final String path, @Nonnull final E value) {
-      this.depth = depth;
-      this.path = path;
-      this.value = value;
-    }
-  }
-
-  /** Queue with a bounded number of pushes */
-  private static class BoundedContextQueue {
-
-    private final LinkedList<Context<?>> delegate = new LinkedList<>();
-    private final int maxDepth;
-    private int remainder;
-
-    public BoundedContextQueue(final int maxDepth, final int maxObjects) {
-      this.maxDepth = maxDepth;
-      remainder = maxObjects;
-    }
-
-    public Context<?> pop() {
-      return delegate.remove();
-    }
-
-    public <E> boolean push(final int depth, final String path, final E object) {
-      if (remainder == 0) {
-        return false; // ok no more pushes
-      }
-      remainder--;
-
-      // ignore if beyond max depth
-      if (depth <= maxDepth) {
-        delegate.add(new Context<>(depth, path, object));
-      }
-      return true;
-    }
-
-    public boolean isEmpty() {
-      return delegate.isEmpty();
-    }
   }
 }
