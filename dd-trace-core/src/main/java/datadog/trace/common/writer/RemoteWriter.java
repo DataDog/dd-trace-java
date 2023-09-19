1b9a4039b7914d02fb59bcab6df596653b31ee8f
keisku <keisuke.umegaki@datadoghq.com>
2023-09-19 18:32:02 +0900

    fix stacktrace print - 3

--- dd-trace-core/src/main/java/datadog/trace/common/writer/RemoteWriter.java
+++ dd-trace-core/src/main/java/datadog/trace/common/writer/RemoteWriter.java
@@ -73,7 +73,6 @@ public abstract class RemoteWriter implements Writer {
         switch (traceProcessingWorker.publish(root, samplingPriority, trace)) {
           case ENQUEUED_FOR_SERIALIZATION:
             log.debug("keisuke log - current stack trace: {}", (Object) Thread.currentThread().getStackTrace());
-            log.debug("keisuke log - current stack trace: {}", stackTraceElements);
             log.debug("Enqueued for serialization: {}", trace);
             healthMetrics.onPublish(trace, samplingPriority);
             break;
