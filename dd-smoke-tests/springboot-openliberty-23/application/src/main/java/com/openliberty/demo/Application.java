38072072aa59fbf8a944d977f4459316f8b2edf6
Nayeem Kamal <kamal.nayeem12@gmail.com>
2023-09-29 10:11:42 -0400

    [FRAPMS-3788] Openliberty jakarta integration (#5652)
    * added initial openliberty 23 instrumentation - WIP
    
    * adding jakarta servlets for openliberty
    
    * instrumenting liberty with jakarta- build changes - WIP
    
    * improved build.gradle to run tests
    
    * added liberty-23 smoke tests
    
    * reflected updates to liberty-20 from master
    
    * pulled in new advice helpers from master
    
    * resolved muzzle errors
    
    * added servlet-5.0 feature to server.xml
    
    * remove jsp and add servlet-5.0
    
    * fixed removal of jsp
    
    * disabled broken appsec tests
    
    * fixed smoke test version for openliberty 23
    
    * fixed logging in JakartaServletBlockingHelper

---
+++ dd-smoke-tests/springboot-openliberty-23/application/src/main/java/com/openliberty/demo/Application.java
@@ -0,0 +1,13 @@
+package com.openliberty.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
