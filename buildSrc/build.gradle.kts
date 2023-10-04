2a1a11a6b287c9942762caa7ace733f88294ad58
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2023-10-04 22:33:27 +0100

    ASM 9.6

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -38,8 +38,8 @@ dependencies {
   implementation("org.apache.maven", "maven-aether-provider", "3.3.9")
 
   implementation("com.google.guava", "guava", "20.0")
-  implementation("org.ow2.asm", "asm", "9.5")
-  implementation("org.ow2.asm", "asm-tree", "9.5")
+  implementation("org.ow2.asm", "asm", "9.6")
+  implementation("org.ow2.asm", "asm-tree", "9.6")
 
   testImplementation("org.spockframework", "spock-core", "2.2-groovy-3.0")
   testImplementation("org.codehaus.groovy", "groovy-all", "3.0.17")
