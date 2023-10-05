DataDog/dd-trace-java

commit 255a79929770236ece2f003d375dbb68f148cd19

Only invalidate current OTel context if we created it; otherwise when there's no active span leave the non-Datadog context in place. (#5880)

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/test/groovy/OpenTelemetry14ContextTest.groovy
@@ -172,6 +172,15 @@ class OpenTelemetry14ContextTest extends AgentTestRunner {
     !currentSpan.spanContext.isValid()
   }
 
+  def "test setting non-Datadog context"() {
+    when:
+    def rootScope = Context.root().makeCurrent()
+    then:
+    Context.current() == Context.root()
+    cleanup:
+    rootScope.close()
+  }
+
   def "test mixing manual and OTel instrumentation"() {
     setup:
     def otelParentSpan = tracer.spanBuilder("some-name").startSpan()

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/main/java/datadog/trace/instrumentation/opentelemetry14/OpenTelemetryContextInstrumentation.java
@@ -86,8 +86,17 @@ public class OpenTelemetryContextInstrumentation extends Instrumenter.Tracing
   public static class ContextCurrentAdvice {
     @Advice.OnMethodExit(suppress = Throwable.class)
     public static void current(@Advice.Return(readOnly = false) Context result) {
-      // Get OTel current span
       AgentSpan agentCurrentSpan = AgentTracer.activeSpan();
+      if (null == agentCurrentSpan) {
+        if (result instanceof OtelContext) {
+          // context was ours, but is now out of scope, so invalidate it
+          result = new OtelContext(OtelSpan.invalid(), OtelSpan.invalid());
+        } else {
+          // otherwise leave non-Datadog context unchanged
+        }
+        return;
+      }
+      // Get OTel current span
       Span otelCurrentSpan = null;
       if (agentCurrentSpan instanceof AttachableWrapper) {
         Object wrapper = ((AttachableWrapper) agentCurrentSpan).getWrapper();
@@ -96,23 +105,19 @@ public class OpenTelemetryContextInstrumentation extends Instrumenter.Tracing
         }
       }
       if (otelCurrentSpan == null) {
-        otelCurrentSpan =
-            agentCurrentSpan == null ? OtelSpan.invalid() : new OtelSpan(agentCurrentSpan);
+        otelCurrentSpan = new OtelSpan(agentCurrentSpan);
       }
       // Get OTel root span
       Span otelRootSpan = null;
-      AgentSpan agentRootSpan = null;
-      if (agentCurrentSpan != null) {
-        agentRootSpan = agentCurrentSpan.getLocalRootSpan();
-        if (agentRootSpan instanceof AttachableWrapper) {
-          Object wrapper = ((AttachableWrapper) agentRootSpan).getWrapper();
-          if (wrapper instanceof OtelSpan) {
-            otelRootSpan = (OtelSpan) wrapper;
-          }
+      AgentSpan agentRootSpan = agentCurrentSpan.getLocalRootSpan();
+      if (agentRootSpan instanceof AttachableWrapper) {
+        Object wrapper = ((AttachableWrapper) agentRootSpan).getWrapper();
+        if (wrapper instanceof OtelSpan) {
+          otelRootSpan = (OtelSpan) wrapper;
         }
       }
       if (otelRootSpan == null) {
-        otelRootSpan = agentRootSpan == null ? OtelSpan.invalid() : new OtelSpan(agentRootSpan);
+        otelRootSpan = new OtelSpan(agentRootSpan);
       }
       result = new OtelContext(otelCurrentSpan, otelRootSpan);
     }

commit 0b8fe59ab589dfc4d33648335cb741ef48f35227

Add support for OpenTelemetry root context manipulation (#5970)
Fix regression introduced in #5880

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/main/java/datadog/trace/instrumentation/opentelemetry14/OtelSpan.java
@@ -133,7 +133,7 @@ public class OtelSpan implements Span {
     return this.recording;
   }
 
-  AgentScope activate() {
+  public AgentScope activate() {
     return activateSpan(this.delegate);
   }
 

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/main/java/datadog/trace/instrumentation/opentelemetry14/OtelSpanContext.java
@@ -57,4 +57,18 @@ public class OtelSpanContext implements SpanContext {
   public boolean isRemote() {
     return this.remote;
   }
+
+  @Override
+  public String toString() {
+    return "OtelSpanContext{"
+        + "traceId='"
+        + getTraceId()
+        + "', spanId='"
+        + getSpanId()
+        + "', sampled="
+        + this.sampled
+        + ", remote="
+        + this.remote
+        + '}';
+  }
 }

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/main/java/datadog/trace/instrumentation/opentelemetry14/context/OpenTelemetryContextInstrumentation.java
@@ -1,7 +1,8 @@
-package datadog.trace.instrumentation.opentelemetry14;
+package datadog.trace.instrumentation.opentelemetry14.context;
 
 import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static datadog.trace.instrumentation.opentelemetry14.OpenTelemetryInstrumentation.ROOT_PACKAGE_NAME;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.returns;
 import static net.bytebuddy.matcher.ElementMatchers.takesNoArguments;
@@ -9,19 +10,16 @@ import static net.bytebuddy.matcher.ElementMatchers.takesNoArguments;
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.api.InstrumenterConfig;
-import datadog.trace.util.Strings;
-import io.opentelemetry.api.trace.TracerProvider;
+import io.opentelemetry.context.Context;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(Instrumenter.class)
-public class OpenTelemetryInstrumentation extends Instrumenter.Tracing
+public class OpenTelemetryContextInstrumentation extends Instrumenter.Tracing
     implements Instrumenter.CanShortcutTypeMatching {
-  public static final String ROOT_PACKAGE_NAME =
-      Strings.getPackageName(OpenTelemetryInstrumentation.class.getName());
 
-  public OpenTelemetryInstrumentation() {
+  public OpenTelemetryContextInstrumentation() {
     super("opentelemetry.experimental", "opentelemetry-1");
   }
 
@@ -32,7 +30,7 @@ public class OpenTelemetryInstrumentation extends Instrumenter.Tracing
 
   @Override
   public String hierarchyMarkerType() {
-    return "io.opentelemetry.api.OpenTelemetry";
+    return "io.opentelemetry.context.Context";
   }
 
   @Override
@@ -43,8 +41,7 @@ public class OpenTelemetryInstrumentation extends Instrumenter.Tracing
   @Override
   public String[] knownMatchingTypes() {
     return new String[] {
-      "io.opentelemetry.api.DefaultOpenTelemetry",
-      "io.opentelemetry.api.GlobalOpenTelemetry$ObfuscatedOpenTelemetry"
+      "io.opentelemetry.context.ArrayBasedContext",
     };
   }
 
@@ -56,37 +53,37 @@ public class OpenTelemetryInstrumentation extends Instrumenter.Tracing
   @Override
   public String[] helperClassNames() {
     return new String[] {
-      packageName + ".context.OtelContext",
-      packageName + ".OtelExtractedContext",
-      packageName + ".OtelScope",
-      packageName + ".OtelSpan",
-      packageName + ".OtelSpan$1",
-      packageName + ".OtelSpan$NoopSpan",
-      packageName + ".OtelSpan$NoopSpanContext",
-      packageName + ".OtelSpanBuilder",
-      packageName + ".OtelSpanBuilder$1",
-      packageName + ".OtelSpanContext",
-      packageName + ".OtelTracer",
-      packageName + ".OtelTracerBuilder",
-      packageName + ".OtelTracerProvider",
+      packageName + ".OtelContext",
+      ROOT_PACKAGE_NAME + ".OtelExtractedContext",
+      ROOT_PACKAGE_NAME + ".OtelScope",
+      ROOT_PACKAGE_NAME + ".OtelSpan",
+      ROOT_PACKAGE_NAME + ".OtelSpan$1",
+      ROOT_PACKAGE_NAME + ".OtelSpan$NoopSpan",
+      ROOT_PACKAGE_NAME + ".OtelSpan$NoopSpanContext",
+      ROOT_PACKAGE_NAME + ".OtelSpanBuilder",
+      ROOT_PACKAGE_NAME + ".OtelSpanBuilder$1",
+      ROOT_PACKAGE_NAME + ".OtelSpanContext",
+      ROOT_PACKAGE_NAME + ".OtelTracer",
+      ROOT_PACKAGE_NAME + ".OtelTracerBuilder",
+      ROOT_PACKAGE_NAME + ".OtelTracerProvider",
     };
   }
 
   @Override
   public void adviceTransformations(AdviceTransformation transformation) {
-    // TracerProvider.getTracerProvider()
+    // Context Context.root()
     transformation.applyAdvice(
         isMethod()
-            .and(named("getTracerProvider"))
+            .and(named("root"))
             .and(takesNoArguments())
-            .and(returns(named("io.opentelemetry.api.trace.TracerProvider"))),
-        OpenTelemetryInstrumentation.class.getName() + "$TracerProviderAdvice");
+            .and(returns(named("io.opentelemetry.context.Context"))),
+        OpenTelemetryContextInstrumentation.class.getName() + "$ContextRootAdvice");
   }
 
-  public static class TracerProviderAdvice {
+  public static class ContextRootAdvice {
     @Advice.OnMethodExit(suppress = Throwable.class)
-    public static void returnProvider(@Advice.Return(readOnly = false) TracerProvider result) {
-      result = OtelTracerProvider.INSTANCE;
+    public static void current(@Advice.Return(readOnly = false) Context result) {
+      result = OtelContext.ROOT;
     }
   }
 }

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/main/java/datadog/trace/instrumentation/opentelemetry14/context/OpenTelemetryContextStorageInstrumentation.java
@@ -1,7 +1,8 @@
-package datadog.trace.instrumentation.opentelemetry14;
+package datadog.trace.instrumentation.opentelemetry14.context;
 
 import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static datadog.trace.instrumentation.opentelemetry14.OpenTelemetryInstrumentation.ROOT_PACKAGE_NAME;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.returns;
 import static net.bytebuddy.matcher.ElementMatchers.takesNoArguments;
@@ -12,6 +13,7 @@ import datadog.trace.api.InstrumenterConfig;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
 import datadog.trace.bootstrap.instrumentation.api.AttachableWrapper;
+import datadog.trace.instrumentation.opentelemetry14.OtelSpan;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Context;
 import net.bytebuddy.asm.Advice;
@@ -19,10 +21,10 @@ import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(Instrumenter.class)
-public class OpenTelemetryContextInstrumentation extends Instrumenter.Tracing
+public class OpenTelemetryContextStorageInstrumentation extends Instrumenter.Tracing
     implements Instrumenter.CanShortcutTypeMatching {
 
-  public OpenTelemetryContextInstrumentation() {
+  public OpenTelemetryContextStorageInstrumentation() {
     super("opentelemetry.experimental", "opentelemetry-1");
   }
 
@@ -58,43 +60,40 @@ public class OpenTelemetryContextInstrumentation extends Instrumenter.Tracing
   public String[] helperClassNames() {
     return new String[] {
       packageName + ".OtelContext",
-      packageName + ".OtelExtractedContext",
-      packageName + ".OtelScope",
-      packageName + ".OtelSpan",
-      packageName + ".OtelSpan$1",
-      packageName + ".OtelSpan$NoopSpan",
-      packageName + ".OtelSpan$NoopSpanContext",
-      packageName + ".OtelSpanBuilder",
-      packageName + ".OtelSpanBuilder$1",
-      packageName + ".OtelSpanContext",
-      packageName + ".OtelTracer",
-      packageName + ".OtelTracerBuilder",
-      packageName + ".OtelTracerProvider",
+      ROOT_PACKAGE_NAME + ".OtelExtractedContext",
+      ROOT_PACKAGE_NAME + ".OtelScope",
+      ROOT_PACKAGE_NAME + ".OtelSpan",
+      ROOT_PACKAGE_NAME + ".OtelSpan$1",
+      ROOT_PACKAGE_NAME + ".OtelSpan$NoopSpan",
+      ROOT_PACKAGE_NAME + ".OtelSpan$NoopSpanContext",
+      ROOT_PACKAGE_NAME + ".OtelSpanBuilder",
+      ROOT_PACKAGE_NAME + ".OtelSpanBuilder$1",
+      ROOT_PACKAGE_NAME + ".OtelSpanContext",
+      ROOT_PACKAGE_NAME + ".OtelTracer",
+      ROOT_PACKAGE_NAME + ".OtelTracerBuilder",
+      ROOT_PACKAGE_NAME + ".OtelTracerProvider",
     };
   }
 
   @Override
   public void adviceTransformations(AdviceTransformation transformation) {
-    // Context.current()
+    // Context ContextStorage.current()
     transformation.applyAdvice(
         isMethod()
             .and(named("current"))
             .and(takesNoArguments())
             .and(returns(named("io.opentelemetry.context.Context"))),
-        OpenTelemetryContextInstrumentation.class.getName() + "$ContextCurrentAdvice");
+        OpenTelemetryContextStorageInstrumentation.class.getName()
+            + "$ContextStorageCurrentAdvice");
   }
 
-  public static class ContextCurrentAdvice {
+  public static class ContextStorageCurrentAdvice {
     @Advice.OnMethodExit(suppress = Throwable.class)
     public static void current(@Advice.Return(readOnly = false) Context result) {
+      // Check empty context
       AgentSpan agentCurrentSpan = AgentTracer.activeSpan();
       if (null == agentCurrentSpan) {
-        if (result instanceof OtelContext) {
-          // context was ours, but is now out of scope, so invalidate it
-          result = new OtelContext(OtelSpan.invalid(), OtelSpan.invalid());
-        } else {
-          // otherwise leave non-Datadog context unchanged
-        }
+        result = OtelContext.ROOT;
         return;
       }
       // Get OTel current span

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/main/java/datadog/trace/instrumentation/opentelemetry14/context/OtelContext.java
@@ -1,6 +1,8 @@
-package datadog.trace.instrumentation.opentelemetry14;
+package datadog.trace.instrumentation.opentelemetry14.context;
 
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
+import datadog.trace.instrumentation.opentelemetry14.OtelScope;
+import datadog.trace.instrumentation.opentelemetry14.OtelSpan;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.ContextKey;
@@ -10,6 +12,9 @@ import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 public class OtelContext implements Context {
+  /** Overridden root context. */
+  public static final OtelContext ROOT = new OtelContext(OtelSpan.invalid(), OtelSpan.invalid());
+
   private static final String OTEL_CONTEXT_SPAN_KEY = "opentelemetry-trace-span-key";
   private static final String DATADOG_CONTEXT_ROOT_SPAN_KEY = "datadog-root-span-key";
 
@@ -50,5 +55,15 @@ public class OtelContext implements Context {
       scope = new OtelScope(scope, agentScope);
     }
     return scope;
+  }
+
+  @Override
+  public String toString() {
+    return "OtelContext{"
+        + "currentSpan="
+        + this.currentSpan.getSpanContext()
+        + ", rootSpan="
+        + this.rootSpan.getSpanContext()
+        + '}';
   }
 }

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/test/groovy/OpenTelemetry14ContextTest.groovy
@@ -1,12 +1,13 @@
 import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.api.DDSpanId
 import datadog.trace.api.DDTraceId
-import datadog.trace.instrumentation.opentelemetry14.OtelContext
+import datadog.trace.instrumentation.opentelemetry14.context.OtelContext
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.trace.Span
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator
 import io.opentelemetry.context.Context
 import io.opentelemetry.context.ContextKey
+import io.opentelemetry.context.ThreadLocalContextStorage
 import io.opentelemetry.context.propagation.TextMapGetter
 import io.opentelemetry.context.propagation.TextMapSetter
 import spock.lang.Subject
@@ -24,6 +25,12 @@ class OpenTelemetry14ContextTest extends AgentTestRunner {
     super.configurePreAgent()
 
     injectSysConfig("dd.integration.opentelemetry.experimental.enabled", "true")
+  }
+
+  @Override
+  void setup() {
+    // Reset OTel context storage
+    ThreadLocalContextStorage.THREAD_LOCAL_STORAGE.remove()
   }
 
   def "test Span.current/makeCurrent()"() {
@@ -172,7 +179,7 @@ class OpenTelemetry14ContextTest extends AgentTestRunner {
     !currentSpan.spanContext.isValid()
   }
 
-  def "test setting non-Datadog context"() {
+  def "test clearing context"() {
     when:
     def rootScope = Context.root().makeCurrent()
     then:

dd-java-agent/instrumentation/opentelemetry/opentelemetry-1.4/src/test/groovy/OpenTelemetry14Test.groovy
@@ -5,6 +5,7 @@ import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.api.trace.SpanKind
 import io.opentelemetry.context.Context
+import io.opentelemetry.context.ThreadLocalContextStorage
 import spock.lang.Subject
 
 import java.security.InvalidParameterException
@@ -22,6 +23,12 @@ class OpenTelemetry14Test extends AgentTestRunner {
     super.configurePreAgent()
 
     injectSysConfig("dd.integration.opentelemetry.experimental.enabled", "true")
+  }
+
+  @Override
+  void setup() {
+    // Reset OTel context storage
+    ThreadLocalContextStorage.THREAD_LOCAL_STORAGE.remove()
   }
 
   def "test parent span using active span"() {