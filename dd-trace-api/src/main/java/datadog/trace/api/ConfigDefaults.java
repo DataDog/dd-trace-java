a7dd60866cf06755783d8fa6d786618b20b6fcc2
Valentin Zakharov <valentin.zakharov@datadoghq.com>
2023-10-16 14:18:53 +0200

    Stacktrace leak suppress - disabled by default

--- dd-trace-api/src/main/java/datadog/trace/api/ConfigDefaults.java
+++ dd-trace-api/src/main/java/datadog/trace/api/ConfigDefaults.java
@@ -105,7 +105,7 @@ public final class ConfigDefaults {
   static final String DEFAULT_IAST_REDACTION_VALUE_PATTERN =
       "(?:bearer\\s+[a-z0-9\\._\\-]+|glpat-[\\w\\-]{20}|gh[opsu]_[0-9a-zA-Z]{36}|ey[I-L][\\w=\\-]+\\.ey[I-L][\\w=\\-]+(?:\\.[\\w.+/=\\-]+)?|(?:[\\-]{5}BEGIN[a-z\\s]+PRIVATE\\sKEY[\\-]{5}[^\\-]+[\\-]{5}END[a-z\\s]+PRIVATE\\sKEY[\\-]{5}|ssh-rsa\\s*[a-z0-9/\\.+]{100,}))";
   public static final int DEFAULT_IAST_MAX_RANGE_COUNT = 10;
-  static final boolean DEFAULT_IAST_STACKTRACE_LEAK_SUPPRESS = true;
+  static final boolean DEFAULT_IAST_STACKTRACE_LEAK_SUPPRESS = false;
 
   static final int DEFAULT_IAST_TRUNCATION_MAX_VALUE_LENGTH = 250;
   public static final boolean DEFAULT_IAST_DEDUPLICATION_ENABLED = true;
