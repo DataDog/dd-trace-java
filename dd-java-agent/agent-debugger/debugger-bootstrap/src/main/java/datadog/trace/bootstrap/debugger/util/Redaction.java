20e4084f7a0bc357fc3a8ae87b18034f1b52fbdb
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2023-10-16 14:20:53 +0200

    Add config fo custom redacted identifiers
    Add property/env `dynamic.instrumentation.redacted.identifiers` to
    customize the redaction data based fields or key names

--- dd-java-agent/agent-debugger/debugger-bootstrap/src/main/java/datadog/trace/bootstrap/debugger/util/Redaction.java
+++ dd-java-agent/agent-debugger/debugger-bootstrap/src/main/java/datadog/trace/bootstrap/debugger/util/Redaction.java
@@ -1,8 +1,10 @@
 package datadog.trace.bootstrap.debugger.util;
 
+import datadog.trace.api.Config;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
 
 public class Redaction {
   // Need to be a unique instance (new String) for reference equality (==) and
@@ -12,33 +14,49 @@ public class Redaction {
   /*
    * based on sentry list: https://github.com/getsentry/sentry-python/blob/fefb454287b771ac31db4e30fa459d9be2f977b8/sentry_sdk/scrubber.py#L17-L58
    */
-  private static final Set<String> KEYWORDS =
-      new HashSet<>(
-          Arrays.asList(
-              "password",
-              "passwd",
-              "secret",
-              "apikey",
-              "auth",
-              "credentials",
-              "mysqlpwd",
-              "privatekey",
-              "token",
-              "ipaddress",
-              "session",
-              // django
-              "csrftoken",
-              "sessionid",
-              // wsgi
-              "remoteaddr",
-              "xcsrftoken",
-              "xforwardedfor",
-              "setcookie",
-              "cookie",
-              "authorization",
-              "xapikey",
-              "xforwardedfor",
-              "xrealip"));
+  private static final Set<String> KEYWORDS = ConcurrentHashMap.newKeySet();
+
+  static {
+    KEYWORDS.addAll(
+        Arrays.asList(
+            "password",
+            "passwd",
+            "secret",
+            "apikey",
+            "auth",
+            "credentials",
+            "mysqlpwd",
+            "privatekey",
+            "token",
+            "ipaddress",
+            "session",
+            // django
+            "csrftoken",
+            "sessionid",
+            // wsgi
+            "remoteaddr",
+            "xcsrftoken",
+            "xforwardedfor",
+            "setcookie",
+            "cookie",
+            "authorization",
+            "xapikey",
+            "xforwardedfor",
+            "xrealip"));
+  }
+
+  private static final Pattern COMMA_PATTERN = Pattern.compile(",");
+
+  public static void addUserDefinedKeywords(Config config) {
+    String redactedIdentifiers = config.getDebuggerRedactedIdentifiers();
+    if (redactedIdentifiers == null) {
+      return;
+    }
+    String[] identifiers = COMMA_PATTERN.split(redactedIdentifiers);
+    for (String identifier : identifiers) {
+      KEYWORDS.add(normalize(identifier));
+    }
+  }
 
   public static boolean isRedactedKeyword(String name) {
     if (name == null) {
--- dd-java-agent/agent-debugger/debugger-bootstrap/src/test/java/datadog/trace/bootstrap/debugger/util/RedactionTest.java
+++ dd-java-agent/agent-debugger/debugger-bootstrap/src/test/java/datadog/trace/bootstrap/debugger/util/RedactionTest.java
@@ -2,12 +2,13 @@ package datadog.trace.bootstrap.debugger.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import datadog.trace.api.Config;
 import org.junit.jupiter.api.Test;
 
 class RedactionTest {
 
   @Test
-  public void test() {
+  public void basic() {
     assertFalse(Redaction.isRedactedKeyword(null));
     assertFalse(Redaction.isRedactedKeyword(""));
     assertFalse(Redaction.isRedactedKeyword("foobar"));
@@ -18,5 +19,18 @@ class RedactionTest {
     assertTrue(Redaction.isRedactedKeyword("_Pass-Word_"));
     assertTrue(Redaction.isRedactedKeyword("$pass_worD"));
     assertTrue(Redaction.isRedactedKeyword("@passWord@"));
+  }
+
+  @Test
+  public void userDefinedKeywords() {
+    final String REDACTED_IDENTIFIERS = "dd.dynamic.instrumentation.redacted.identifiers";
+    System.setProperty(REDACTED_IDENTIFIERS, "_MotDePasse,$Passwort");
+    try {
+      Redaction.addUserDefinedKeywords(Config.get());
+      assertTrue(Redaction.isRedactedKeyword("mot-de-passe"));
+      assertTrue(Redaction.isRedactedKeyword("Passwort"));
+    } finally {
+      System.clearProperty(REDACTED_IDENTIFIERS);
+    }
   }
 }
