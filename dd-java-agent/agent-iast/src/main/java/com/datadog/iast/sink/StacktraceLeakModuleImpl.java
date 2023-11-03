48d6aecea171823f7d3d3de70c74f2086695e54e
ValentinZakharov <valentin.zakharov1@gmail.com>
2023-11-03 10:16:50 +0200

    Stacktrace leak protection (#5740)

---
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/StacktraceLeakModuleImpl.java
@@ -0,0 +1,37 @@
+package com.datadog.iast.sink;
+
+import com.datadog.iast.Dependencies;
+import com.datadog.iast.model.Evidence;
+import com.datadog.iast.model.Location;
+import com.datadog.iast.model.Vulnerability;
+import com.datadog.iast.model.VulnerabilityType;
+import datadog.trace.api.iast.sink.StacktraceLeakModule;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
+import org.jetbrains.annotations.NotNull;
+
+public class StacktraceLeakModuleImpl extends SinkModuleBase implements StacktraceLeakModule {
+
+  public StacktraceLeakModuleImpl(@NotNull Dependencies dependencies) {
+    super(dependencies);
+  }
+
+  @Override
+  public void onStacktraceLeak(
+      Throwable throwable, String moduleName, String className, String methodName) {
+    if (throwable != null) {
+      final AgentSpan span = AgentTracer.activeSpan();
+
+      Evidence evidence =
+          new Evidence(
+              "ExceptionHandler in "
+                  + moduleName
+                  + " \r\nthrown "
+                  + throwable.getClass().getName());
+      Location location = Location.forSpanAndClassAndMethod(span, className, methodName);
+
+      reporter.report(
+          span, new Vulnerability(VulnerabilityType.STACKTRACE_LEAK, location, evidence));
+    }
+  }
+}
