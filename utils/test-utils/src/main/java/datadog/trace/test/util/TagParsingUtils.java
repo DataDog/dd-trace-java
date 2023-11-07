b714cd1cbec0ea991d56fd42c8a9b84512cec666
Jaroslav Bachorik <j.bachorik@gmail.com>
2023-11-07 17:35:55 +0100

    A forbidden API?!

--- utils/test-utils/src/main/java/datadog/trace/test/util/TagParsingUtils.java
+++ utils/test-utils/src/main/java/datadog/trace/test/util/TagParsingUtils.java
@@ -1,10 +1,12 @@
 package datadog.trace.test.util;
 
+import de.thetaphi.forbiddenapis.SuppressForbidden;
 import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public final class TagParsingUtils {
+  @SuppressForbidden
   public static Map<String, String> parseTags(final Collection<Object> params) {
     return params.stream()
         .map(p -> ((String) p).split(":", 2))
