7ab6477ce539cd8075f0db8560be9357e6b0654b
am312 <andrew.munn@datadoghq.com>
2023-11-09 12:48:57 -0500

    add normalization in decorator

--- dd-java-agent/instrumentation/datastax-cassandra-4/src/main/java/datadog/trace/instrumentation/datastax/cassandra4/CassandraClientDecorator.java
+++ dd-java-agent/instrumentation/datastax-cassandra-4/src/main/java/datadog/trace/instrumentation/datastax/cassandra4/CassandraClientDecorator.java
@@ -6,6 +6,9 @@ import com.datastax.oss.driver.api.core.metadata.Node;
 import com.datastax.oss.driver.api.core.servererrors.CoordinatorException;
 import com.datastax.oss.driver.api.core.session.Session;
 import datadog.trace.api.naming.SpanNaming;
+import datadog.trace.api.cache.DDCache;
+import datadog.trace.api.cache.DDCaches;
+import datadog.trace.api.normalize.SQLNormalizer;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.InternalSpanTypes;
 import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
@@ -13,6 +16,8 @@ import datadog.trace.bootstrap.instrumentation.decorator.DBTypeProcessingDatabas
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
 
 public class CassandraClientDecorator extends DBTypeProcessingDatabaseClientDecorator<Session> {
   private static final String DB_TYPE = "cassandra";
@@ -23,6 +28,15 @@ public class CassandraClientDecorator extends DBTypeProcessingDatabaseClientDeco
   public static final CharSequence JAVA_CASSANDRA = UTF8BytesString.create("java-cassandra");
 
   public static final CassandraClientDecorator DECORATE = new CassandraClientDecorator();
+
+  private static final int COMBINED_STATEMENT_LIMIT = 2 * 1024 * 1024; // chars
+  private static final ToIntFunction<UTF8BytesString> STATEMENT_WEIGHER = UTF8BytesString::length;
+  private static final DDCache<CharSequence, UTF8BytesString> CACHED_STATEMENTS =
+      DDCaches.newFixedSizeWeightedCache(512, STATEMENT_WEIGHER, COMBINED_STATEMENT_LIMIT);
+
+  protected static UTF8BytesString normalizedQuery(CharSequence sql) {
+    return CACHED_STATEMENTS.computeIfAbsent(sql, SQLNormalizer::normalizeCharSequence);
+  }
 
   @Override
   protected String[] instrumentationNames() {
@@ -62,6 +76,11 @@ public class CassandraClientDecorator extends DBTypeProcessingDatabaseClientDeco
   @Override
   protected String dbHostname(Session session) {
     return null;
+  }
+
+  public AgentSpan onStatement(final AgentSpan span, final CharSequence statement) {
+    span.setResourceName(normalizedQuery(statement));
+    return span;
   }
 
   public AgentSpan onResponse(final AgentSpan span, final ResultSet result) {
