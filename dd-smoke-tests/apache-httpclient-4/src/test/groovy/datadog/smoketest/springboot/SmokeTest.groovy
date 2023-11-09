b16c6175e375cb0da24906f81844859fa1c5b962
Alejandro Gonzalez <alejandro.gonzalez@datadoghq.com>
2023-11-09 14:14:18 +0100

    clean test

--- dd-smoke-tests/apache-httpclient-4/src/test/groovy/datadog/smoketest/springboot/SmokeTest.groovy
+++ dd-smoke-tests/apache-httpclient-4/src/test/groovy/datadog/smoketest/springboot/SmokeTest.groovy
@@ -17,7 +17,6 @@ class SmokeTest extends AbstractIastServerSmokeTest {
 
     List<String> command = []
     command.add(javaPath())
-    //command.add("-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005")
     command.addAll(defaultJavaProperties)
     command.addAll([
       withSystemProperty(IAST_ENABLED, true),
