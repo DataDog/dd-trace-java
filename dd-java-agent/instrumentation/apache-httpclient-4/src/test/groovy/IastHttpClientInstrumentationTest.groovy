91bae1bb88523f3a562c0c00570a6870b632f0da
Alejandro Gonzalez <alejandro.gonzalez@datadoghq.com>
2023-11-09 14:07:44 +0100

    improve test

--- dd-java-agent/instrumentation/apache-httpclient-4/src/test/groovy/IastHttpClientInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/apache-httpclient-4/src/test/groovy/IastHttpClientInstrumentationTest.groovy
@@ -2,9 +2,11 @@ import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.api.iast.InstrumentationBridge
 import datadog.trace.api.iast.sink.SsrfModule
 import org.apache.http.HttpHost
+import org.apache.http.HttpRequest
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.BasicResponseHandler
 import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.message.BasicHttpRequest
 import org.apache.http.protocol.BasicHttpContext
 import spock.lang.AutoCleanup
 import spock.lang.Shared
@@ -29,7 +31,7 @@ class IastHttpClientInstrumentationTest extends AgentTestRunner {
     }
   }
 
-  void 'test ssrf'() {
+  void 'test ssrf for #httpClient execute method with args #args expecting call module with #classExpected arg'() {
     given:
     final ssrf = Mock(SsrfModule)
     InstrumentationBridge.registerIastModule(ssrf)
@@ -38,22 +40,34 @@ class IastHttpClientInstrumentationTest extends AgentTestRunner {
     httpClient.execute(*args)
 
     then:
-    1 * ssrf.onURLConnection(_)
+    if(classExpected == URI){
+      1 * ssrf.onURLConnection(_ as URI)
+    } else {
+      1 * ssrf.onURLConnection(_ as String)
+    }
 
     where:
-    httpClient | args
-    new DefaultHttpClient() | [getHttpUriRequest(server)]
-    new DefaultHttpClient() | [getHttpUriRequest(server), new BasicHttpContext()]
-    new DefaultHttpClient() | [getHttpHost(server), getHttpUriRequest(server)]
-    new DefaultHttpClient() | [getHttpHost(server), getHttpUriRequest(server), new BasicHttpContext()]
-    new DefaultHttpClient() | [getHttpUriRequest(server), new BasicResponseHandler()]
-    new DefaultHttpClient() | [getHttpUriRequest(server), new BasicResponseHandler(), new BasicHttpContext()]
-    new DefaultHttpClient() | [getHttpHost(server), getHttpUriRequest(server), new BasicResponseHandler()]
-    new DefaultHttpClient() | [getHttpHost(server), getHttpUriRequest(server), new BasicResponseHandler(), new BasicHttpContext()]
+    httpClient | args | classExpected
+    new DefaultHttpClient() | [getHttpUriRequest(server)] | URI
+    new DefaultHttpClient() | [getHttpUriRequest(server), new BasicHttpContext()] | URI
+    new DefaultHttpClient() | [getHttpUriRequest(server), new BasicResponseHandler()] | URI
+    new DefaultHttpClient() | [getHttpUriRequest(server), new BasicResponseHandler(), new BasicHttpContext()] | URI
+    new DefaultHttpClient() | [getHttpHost(server), getHttpUriRequest(server)] | URI
+    new DefaultHttpClient() | [getHttpHost(server), getHttpUriRequest(server), new BasicHttpContext()] | URI
+    new DefaultHttpClient() | [getHttpHost(server), getHttpUriRequest(server), new BasicResponseHandler()] | URI
+    new DefaultHttpClient() | [getHttpHost(server), getHttpUriRequest(server), new BasicResponseHandler(), new BasicHttpContext()] | URI
+    new DefaultHttpClient() | [getHttpHost(server), getHttpRequest(server)] | String
+    new DefaultHttpClient() | [getHttpHost(server), getHttpRequest(server), new BasicHttpContext()] | String
+    new DefaultHttpClient() | [getHttpHost(server), getHttpRequest(server), new BasicResponseHandler()] | String
+    new DefaultHttpClient() | [getHttpHost(server), getHttpRequest(server), new BasicResponseHandler(), new BasicHttpContext()] | String
   }
 
   private static org.apache.http.client.methods.HttpUriRequest getHttpUriRequest(final server){
     return new HttpGet(server.address.toString())
+  }
+
+  private static HttpRequest getHttpRequest(final server){
+    return new BasicHttpRequest("GET", server.address.toString())
   }
 
   private static HttpHost getHttpHost(final server){
