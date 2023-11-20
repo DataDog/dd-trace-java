ed2f74cb3ab2e8d901a381886f2e81c0341a692a
Javier Santos <javier.santos@datadoghq.com>
2023-11-20 12:11:30 +0100

    Added org.json package instrumentation

---
+++ dd-java-agent/instrumentation/org-json/src/main/java/datadog/trace/instrumentation/json/JSONArrayInstrumentation.java
@@ -0,0 +1,79 @@
+package datadog.trace.instrumentation.json;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.isConstructor;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.returns;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.api.iast.InstrumentationBridge;
+import datadog.trace.api.iast.Propagation;
+import datadog.trace.api.iast.propagation.PropagationModule;
+import net.bytebuddy.asm.Advice;
+
+@AutoService(Instrumenter.class)
+public class JSONArrayInstrumentation extends Instrumenter.Iast
+    implements Instrumenter.ForSingleType {
+
+  public JSONArrayInstrumentation() {
+    super("org-json");
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "org.json.JSONArray";
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    transformation.applyAdvice(
+        isConstructor().and(takesArguments(String.class)),
+        getClass().getName() + "$ConstructorAdvice");
+    transformation.applyAdvice(
+        isMethod()
+            .and(isPublic())
+            .and(returns(Object.class))
+            .and(named("get"))
+            .and(takesArguments(1)),
+        getClass().getName() + "$GetAdvice");
+    transformation.applyAdvice(
+        isMethod().and(isPublic()).and(returns(Object.class)).and(named("opt")),
+        getClass().getName() + "$OptAdvice");
+  }
+
+  public static class ConstructorAdvice {
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    @Propagation
+    public static void afterInit(@Advice.This Object self, @Advice.Argument(0) final Object input) {
+      final PropagationModule iastModule = InstrumentationBridge.PROPAGATION;
+      if (iastModule != null && input != null) {
+        iastModule.taintIfTainted(self, input);
+      }
+    }
+  }
+
+  public static class GetAdvice {
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    @Propagation
+    public static void afterMethod(@Advice.This Object self, @Advice.Return final Object result) {
+      final PropagationModule iastModule = InstrumentationBridge.PROPAGATION;
+      if (iastModule != null && result != null && result instanceof String) {
+        iastModule.taintIfTainted(result, self);
+      }
+    }
+  }
+
+  public static class OptAdvice {
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    @Propagation
+    public static void afterMethod(@Advice.This Object self, @Advice.Return final Object result) {
+      final PropagationModule iastModule = InstrumentationBridge.PROPAGATION;
+      if (iastModule != null && result != null) {
+        iastModule.taintIfTainted(result, self);
+      }
+    }
+  }
+}
