b41df3c012cbcb93ce895c57fae43f10701b0518
Zarir Hamza <zarir.hamza@datadoghq.com>
2023-11-23 22:42:41 -0500

    updates instrumentation to work without enabling threadcontext

---
+++ dd-java-agent/instrumentation/tinylog/src/main/java/datadog/trace/instrumentation/tinylog/LogEntryInstrumentation.java
@@ -0,0 +1,107 @@
+package datadog.trace.instrumentation.tinylog;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.log.UnionMap;
+import datadog.trace.api.Config;
+import datadog.trace.api.CorrelationIdentifier;
+import datadog.trace.api.DDSpanId;
+import datadog.trace.api.DDTraceId;
+import datadog.trace.api.InstrumenterConfig;
+import datadog.trace.bootstrap.InstrumentationContext;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
+import datadog.trace.bootstrap.instrumentation.api.Tags;
+import java.util.HashMap;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
+import org.tinylog.core.LogEntry;
+
+@AutoService(Instrumenter.class)
+public class LogEntryInstrumentation extends Instrumenter.Tracing
+    implements Instrumenter.ForSingleType {
+  public LogEntryInstrumentation() {
+    super("tinylog");
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "org.tinylog.core.LogEntry";
+  }
+
+  @Override
+  public Map<String, String> contextStore() {
+    return singletonMap("org.tinylog.core.LogEntry", AgentSpan.Context.class.getName());
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    transformation.applyAdvice(
+        isMethod().and(named("getContext")).and(takesArguments(0)),
+        LogEntryInstrumentation.class.getName() + "$GetContextAdvice");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      "datadog.trace.agent.tooling.log.UnionMap",
+      "datadog.trace.agent.tooling.log.UnionMap$1",
+      "datadog.trace.agent.tooling.log.UnionMap$1$1",
+    };
+  }
+
+  public static class GetContextAdvice {
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void onExit(
+        @Advice.This LogEntry event,
+        @Advice.Return(typing = Assigner.Typing.DYNAMIC, readOnly = false)
+            Map<String, String> mdc) {
+
+      if (mdc instanceof UnionMap) {
+        return;
+      }
+
+      AgentSpan.Context context =
+          InstrumentationContext.get(LogEntry.class, AgentSpan.Context.class).get(event);
+
+      // Nothing to add so return early
+      if (context == null && !AgentTracer.traceConfig().isLogsInjectionEnabled()) {
+        return;
+      }
+
+      Map<String, String> correlationValues = new HashMap<>(8);
+
+      if (context != null) {
+        DDTraceId traceId = context.getTraceId();
+        String traceIdValue =
+            InstrumenterConfig.get().isLogs128bTraceIdEnabled() && traceId.toHighOrderLong() != 0
+                ? traceId.toHexString()
+                : traceId.toString();
+        correlationValues.put(CorrelationIdentifier.getTraceIdKey(), traceIdValue);
+        correlationValues.put(
+            CorrelationIdentifier.getSpanIdKey(), DDSpanId.toString(context.getSpanId()));
+      }
+
+      String serviceName = Config.get().getServiceName();
+      if (null != serviceName && !serviceName.isEmpty()) {
+        correlationValues.put(Tags.DD_SERVICE, serviceName);
+      }
+      String env = Config.get().getEnv();
+      if (null != env && !env.isEmpty()) {
+        correlationValues.put(Tags.DD_ENV, env);
+      }
+      String version = Config.get().getVersion();
+      if (null != version && !version.isEmpty()) {
+        correlationValues.put(Tags.DD_VERSION, version);
+      }
+
+      mdc = null != mdc ? new UnionMap<>(mdc, correlationValues) : correlationValues;
+    }
+  }
+}
---
+++ dd-java-agent/instrumentation/tinylog/src/main/java/datadog/trace/instrumentation/tinylog/TinylogLoggingProviderInstrumentation.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package datadog.trace.instrumentation.tinylog;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeSpan;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.traceConfig;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isPrivate;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.bootstrap.InstrumentationContext;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import org.tinylog.core.LogEntry;
+
+@AutoService(Instrumenter.class)
+public class TinylogLoggingProviderInstrumentation extends Instrumenter.Tracing
+    implements Instrumenter.ForSingleType {
+  public TinylogLoggingProviderInstrumentation() {
+    super("tinylog");
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "org.tinylog.core.TinylogLoggingProvider";
+  }
+
+  @Override
+  public Map<String, String> contextStore() {
+    return singletonMap("org.tinylog.core.LogEntry", AgentSpan.Context.class.getName());
+  }
+
+  @Override
+  public void adviceTransformations(AdviceTransformation transformation) {
+    transformation.applyAdvice(
+        isMethod()
+            .and(isPrivate())
+            .and(named("output"))
+            .and(takesArguments(2))
+            .and(takesArgument(0, named("org.tinylog.core.LogEntry"))),
+        TinylogLoggingProviderInstrumentation.class.getName() + "$OutputAdvice");
+  }
+
+  public static class OutputAdvice {
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(@Advice.Argument(0) LogEntry event) {
+      AgentSpan span = activeSpan();
+
+      if (span != null && traceConfig(span).isLogsInjectionEnabled()) {
+        InstrumentationContext.get(LogEntry.class, AgentSpan.Context.class)
+            .put(event, span.context());
+      }
+    }
+  }
+}
