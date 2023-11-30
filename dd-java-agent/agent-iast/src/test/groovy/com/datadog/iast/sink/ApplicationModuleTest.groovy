b2b9fe918c643aaa26de4a3c598790403676d6bd
Alejandro Gonzalez <alejandro.gonzalez@datadoghq.com>
2023-11-30 13:01:27 +0100

    Fix flakyness

--- dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/sink/ApplicationModuleTest.groovy
+++ dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/sink/ApplicationModuleTest.groovy
@@ -2,14 +2,12 @@ package com.datadog.iast.sink
 
 import com.datadog.iast.IastModuleImplTestBase
 import com.datadog.iast.IastRequestContext
-import com.datadog.iast.model.Vulnerability
 import com.datadog.iast.model.VulnerabilityType
 import datadog.trace.api.gateway.RequestContext
 import datadog.trace.api.gateway.RequestContextSlot
 import datadog.trace.api.iast.sink.ApplicationModule
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan
 
-import static com.datadog.iast.taint.TaintUtils.taintFormat
 
 class ApplicationModuleTest extends IastModuleImplTestBase {
 
@@ -57,7 +55,7 @@ class ApplicationModuleTest extends IastModuleImplTestBase {
     where:
     path | expectedVulnType | expectedEvidence
     'application/insecurejsplayout/secure' | null | null
-    'application/insecurejsplayout/insecure' | VulnerabilityType.INSECURE_JSP_LAYOUT |  '/nestedinsecure/insecure2.jsp'+System.lineSeparator()+'/insecure.jsp'
+    'application/insecurejsplayout/insecure' | VulnerabilityType.INSECURE_JSP_LAYOUT |  ['/nestedinsecure/insecure2.jsp', '/insecure.jsp'] as String []
     'application/verbtampering/secure' | null | null
     'application/verbtampering/insecure' | VulnerabilityType.VERB_TAMPERING | 'http-method not defined in web.xml'
     'application/sessiontimeout/secure' | null | null
@@ -70,22 +68,22 @@ class ApplicationModuleTest extends IastModuleImplTestBase {
     'application/defaulthtmlescapeinvalid/insecure' | VulnerabilityType.DEFAULT_HTML_ESCAPE_INVALID | 'defaultHtmlEscape tag should be set'
   }
 
-  void 'in deep checkVulns methods test'(){
-  }
-
-
-
-  private static void assertVulnerability(final Vulnerability vuln, final VulnerabilityType expectedVulnType) {
+  private static void assertVulnerability(final  vuln, final  expectedVulnType) {
     assert vuln != null
     assert vuln.getType() == expectedVulnType
     assert vuln.getLocation() != null
   }
 
-  private static void assertEvidence(final Vulnerability vuln, final VulnerabilityType expectedVulnType, final String expectedEvidence) {
+  private static void assertEvidence(final  vuln, final  expectedVulnType, final  expectedEvidence) {
     assertVulnerability(vuln, expectedVulnType)
-    final evidence = vuln.getEvidence()
+    final evidence = vuln.evidence
     assert evidence != null
-    final formatted = taintFormat(evidence.getValue(), evidence.getRanges())
-    assert formatted == expectedEvidence
+    if(expectedEvidence instanceof String[]) {
+      for (String s : expectedEvidence) {
+        assert evidence.value.contains(s)
+      }
+    } else {
+      assert evidence.value == expectedEvidence
+    }
   }
 }
