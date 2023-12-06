485cb3cdd14dd38057d876c5aea2f579fbcb9c23
nayeem-kamal <nayeem.kamal@datadoghq.com>
2023-12-06 16:30:50 -0500

    wip dd-trace instrumentation

---
+++ dd-java-agent/instrumentation/dd-trace/src/main/java/datadog/trace/instrumentation/ddtrace/SpanInstrumentation.java
@@ -0,0 +1,7 @@
+package datadog.trace.instrumentation.ddtrace;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+
+@AutoService(Instrumenter.class)
+public class SpanInstrumentation {}
