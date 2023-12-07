ed2ac970fce36517fd5028eef9d81a42ac4293f4
Andrew Munn <am312@users.noreply.github.com>
2023-12-07 06:49:19 -0500

    correct interface

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/Instrumenter.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/Instrumenter.java
@@ -97,8 +97,8 @@ public interface Instrumenter {
   /**
    * Instrumentation that matches an optional type configured at runtime. This is used for last
    * minute additions in the field such as testing a new JDBC driver that is not yet in the allowed
-   * list and to provide a workaround until the next release. The ForKnownType interface is more
-   * appropriate for general use.
+   * list and to provide a workaround until the next release. The ForSingleType interface is more
+   * appropriate when you know the type at build-time.
    */
   interface ForConfiguredType extends ForConfiguredTypes {
     @Override
