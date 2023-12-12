1992bafe304c12f2d4a9296b69c3f1bfceb0f9ad
JoJo <jmeunier@bu.edu>
2023-12-12 09:37:53 -0500

    Append DBM trace context comments for SQL Server & Postgres (#6214)
    * Append DBM trace context comments for SQL Server & Postgres
    
    * update class comments

--- dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/MySQLConnectionInstrumentation.java
+++ dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/MySQLConnectionInstrumentation.java
@@ -24,24 +24,34 @@ import java.util.Map;
 import net.bytebuddy.asm.Advice;
 
 @AutoService(Instrumenter.class)
-public class SQLServerConnectionInstrumentation extends AbstractConnectionInstrumentation
+public class MySQLConnectionInstrumentation extends AbstractConnectionInstrumentation
     implements Instrumenter.ForKnownTypes {
 
   /**
-   * Instrumentation class for connections for SQL Server, which is a Database Monitoring supported
-   * DB *
+   * Instrumentation class for connections for MySQL, which is a Database Monitoring supported DB *
    */
-  public SQLServerConnectionInstrumentation() {
-    super("jdbc", "dbm-sqlserver");
+  public MySQLConnectionInstrumentation() {
+    super("jdbc", "dbm-mysql");
   }
 
   // Classes to cover all currently supported
   // db types for the Database Monitoring product
   static final String[] CONCRETE_TYPES = {
-    "com.microsoft.sqlserver.jdbc.SQLServerConnection",
-    // jtds (for SQL Server and Sybase)
-    "net.sourceforge.jtds.jdbc.ConnectionJDBC2", // 1.2
-    "net.sourceforge.jtds.jdbc.JtdsConnection", // 1.3
+    // should cover mysql
+    "com.mysql.jdbc.Connection",
+    "com.mysql.jdbc.jdbc1.Connection",
+    "com.mysql.jdbc.jdbc2.Connection",
+    "com.mysql.jdbc.ConnectionImpl",
+    "com.mysql.jdbc.JDBC4Connection",
+    "com.mysql.cj.jdbc.ConnectionImpl",
+    // complete
+    "org.mariadb.jdbc.MySQLConnection",
+    // MariaDB Connector/J v2.x
+    "org.mariadb.jdbc.MariaDbConnection",
+    // MariaDB Connector/J v3.x
+    "org.mariadb.jdbc.Connection",
+    // aws-mysql-jdbc
+    "software.aws.rds.jdbc.mysql.shading.com.mysql.cj.jdbc.ConnectionImpl",
   };
 
   @Override
@@ -63,7 +73,7 @@ public class SQLServerConnectionInstrumentation extends AbstractConnectionInstru
             .and(takesArgument(0, String.class))
             // Also include CallableStatement, which is a subtype of PreparedStatement
             .and(returns(hasInterface(named("java.sql.PreparedStatement")))),
-        SQLServerConnectionInstrumentation.class.getName() + "$ConnectionAdvice");
+        MySQLConnectionInstrumentation.class.getName() + "$ConnectionAdvice");
   }
 
   @Override
@@ -89,7 +99,7 @@ public class SQLServerConnectionInstrumentation extends AbstractConnectionInstru
         final DBInfo dbInfo =
             JDBCDecorator.parseDBInfo(
                 connection, InstrumentationContext.get(Connection.class, DBInfo.class));
-        sql = SQLCommenter.append(sql, DECORATE.getDbService(dbInfo));
+        sql = SQLCommenter.prepend(sql, DECORATE.getDbService(dbInfo));
         return inputSql;
       }
       return sql;
