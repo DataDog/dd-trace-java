6de4e82ae6863540f12947e6907c3d45fca92754
Santiago Mola <santiago.mola@datadoghq.com>
2023-12-12 14:40:34 +0100

    Add graphql.server.resolver to known WAF addresses

--- dd-java-agent/appsec/src/main/java/com/datadog/appsec/event/data/KnownAddresses.java
+++ dd-java-agent/appsec/src/main/java/com/datadog/appsec/event/data/KnownAddresses.java
@@ -101,6 +101,9 @@ public interface KnownAddresses {
   // XXX: Not really used yet, but it's a known address and we should not treat it as unknown.
   Address<Object> GRAPHQL_SERVER_ALL_RESOLVERS = new Address<>("graphql.server.all_resolvers");
 
+  // XXX: Not really used yet, but it's a known address and we should not treat it as unknown.
+  Address<Object> GRAPHQL_SERVER_RESOLVER = new Address<>("graphql.server.resolver");
+
   Address<String> USER_ID = new Address<>("usr.id");
 
   Address<Map<String, Object>> WAF_CONTEXT_PROCESSOR = new Address<>("waf.context.processor");
@@ -153,6 +156,8 @@ public interface KnownAddresses {
         return GRPC_SERVER_REQUEST_METADATA;
       case "graphql.server.all_resolvers":
         return GRAPHQL_SERVER_ALL_RESOLVERS;
+      case "graphql.server.resolver":
+        return GRAPHQL_SERVER_RESOLVER;
       case "usr.id":
         return USER_ID;
       case "waf.context.processor":
