b0cf14ddda76a395889616d990b0ac993cbe6a27
Manuel Álvarez Álvarez <manuel-alvarez-alvarez@users.noreply.github.com>
2023-12-13 13:16:33 +0100

    Remove ReferenceQueue from TaintedMap implementation and purge inline (#6241)
    Move to an inline purge map and drop the reference queue

--- dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/taint/TaintedMapTest.groovy
+++ dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/taint/TaintedMapTest.groovy
@@ -1,22 +1,34 @@
 package com.datadog.iast.taint
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
 import com.datadog.iast.model.Range
 import datadog.trace.test.util.CircularBuffer
 import datadog.trace.test.util.DDSpecification
 
-import java.lang.ref.Reference
-import java.lang.ref.ReferenceQueue
-import java.lang.ref.WeakReference
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 
 class TaintedMapTest extends DDSpecification {
 
+
+  private Logger logger
+  private Level defaultLevel
+
+  void setup() {
+    logger = TaintedMap.Debug.LOGGER as Logger
+    defaultLevel = logger.getLevel()
+  }
+
+  void cleanup() {
+    logger.setLevel(defaultLevel)
+  }
+
   def 'simple workflow'() {
     given:
-    def map = new TaintedMap()
+    final map = new TaintedMap.TaintedMapImpl()
     final o = new Object()
-    final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
+    final to = new TaintedObject(o, [] as Range[])
 
     expect:
     map.size() == 0
@@ -43,11 +55,10 @@ class TaintedMapTest extends DDSpecification {
 
   def 'get non-existent object'() {
     given:
-    def map = new TaintedMap()
+    final map = new TaintedMap.TaintedMapImpl()
     final o = new Object()
 
     expect:
-    !map.isFlat()
     map.get(o) == null
     map.size() == 0
     map.count() == 0
@@ -56,132 +67,15 @@ class TaintedMapTest extends DDSpecification {
   def 'last put always exists'() {
     given:
     int capacity = 256
-    int flatModeThreshold = (int) (capacity / 2)
-    def map = new TaintedMap(capacity, flatModeThreshold, new ReferenceQueue<>())
+    final map = new TaintedMap.TaintedMapImpl()
     int nTotalObjects = capacity * 10
 
     expect:
     (1..nTotalObjects).each { i ->
       final o = new Object()
-      final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
+      final to = new TaintedObject(o, [] as Range[])
       map.put(to)
       assert map.get(o) == to
-    }
-  }
-
-  def 'do not fail on extraneous reference'() {
-    given:
-    int capacity = 256
-    int flatModeThreshold = (int) (capacity / 2)
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
-    def gen = new ObjectGen(capacity)
-
-    when: 'extraneous reference in enqueued'
-    queue.free(new WeakReference<Object>(new Object()))
-
-    and: 'purge is triggered'
-    gen.genObjects(1, ObjectGen.TRIGGERS_PURGE).each { o ->
-      final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-      queue.hold(o, to)
-      map.put(to)
-    }
-
-    then:
-    !map.isFlat()
-    map.size() == 1
-    map.count() == 1
-  }
-
-  def 'do not fail on double free'() {
-    given:
-    int capacity = 256
-    int flatModeThreshold = (int) (capacity / 2)
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
-    def gen = new ObjectGen(capacity)
-
-    when: 'reference to non-present object in enqueued'
-    queue.free(new TaintedObject(new Object(), new Range[0] as Range[], queue))
-
-    and: 'purge is triggered'
-    gen.genObjects(1, ObjectGen.TRIGGERS_PURGE).each { o ->
-      final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-      queue.hold(o, to)
-      map.put(to)
-    }
-
-    then:
-    !map.isFlat()
-    map.size() == 1
-    map.count() == 1
-  }
-
-  def 'do not fail on double free with previous data'() {
-    given:
-    int capacity = 256
-    int flatModeThreshold = (int) (capacity / 2)
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
-    def gen = new ObjectGen(capacity)
-    def bucket = gen.genBucket(2, ObjectGen.TRIGGERS_PURGE)
-
-    when:
-    queue.free(new TaintedObject(bucket[0], new Range[0] as Range[], queue))
-    final to = new TaintedObject(bucket[1], [] as Range[], map.getReferenceQueue())
-    map.put(to)
-
-    then:
-    !map.isFlat()
-    map.size() == 1
-    map.count() == 1
-  }
-
-  def 'flat mode - last put wins'() {
-    given:
-    int capacity = 256
-    int flatModeThreshold = (int) (capacity / 2)
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
-    def gen = new ObjectGen(capacity)
-
-    when:
-    // Number of purges required to switch to flat mode (in the absence of garbage collection)
-    final int purgesToFlatMode = (int) (flatModeThreshold / TaintedMap.PURGE_COUNT) + 1
-    gen.genObjects(purgesToFlatMode, ObjectGen.TRIGGERS_PURGE).each { o ->
-      final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-      queue.hold(o, to)
-      map.put(to)
-    }
-
-    then:
-    map.isFlat()
-
-    when:
-    def lastPuts = []
-    def nonLastPuts = []
-    gen.genBuckets(capacity, 2).each { bucket ->
-      bucket.each { o ->
-        final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-        queue.hold(o, to)
-        map.put(to)
-      }
-      lastPuts.add(bucket[-1])
-      nonLastPuts.addAll(bucket[0..-2])
-    }
-
-    then:
-    map.size() == capacity
-    map.count() == capacity
-
-    and: 'last puts are present'
-    lastPuts.each { o ->
-      assert map.get(o).get() == o
-    }
-
-    and: 'non-last puts are not present'
-    nonLastPuts.each { o ->
-      assert map.get(o) == null
     }
   }
 
@@ -189,8 +83,7 @@ class TaintedMapTest extends DDSpecification {
     given:
     int capacity = 128
     int flatModeThreshold = 64
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
+    def map = new TaintedMap.TaintedMapImpl(capacity)
 
     int iters = 16
     int nObjectsPerIter = flatModeThreshold - 1
@@ -199,23 +92,26 @@ class TaintedMapTest extends DDSpecification {
 
     when:
     (1..iters).each {
-      // Insert objects that do not trigger purge
-      gen.genObjects(nObjectsPerIter, ObjectGen.DOES_NOT_TRIGGER_PURGE).each { o ->
-        final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-        queue.hold(o, to)
+      final queue = gen.genObjects(nObjectsPerIter, ObjectGen.TRUE).collect { o ->
+        final to = new TaintedObject(o, [] as Range[])
         map.put(to)
+        return to
       }
       // Clear previous objects
+      queue.each {
+        final referent = it.get()
+        it.clear()
+        map.get(referent)
+      }
       queue.clear()
       // Trigger purge
-      final o = gen.genObjects(1, ObjectGen.TRIGGERS_PURGE)[0]
-      final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
+      final o = gen.genObjects(1, ObjectGen.TRUE)[0]
+      final to = new TaintedObject(o, [] as Range[])
       objectBuffer.add(o)
       map.put(to)
     }
 
     then:
-    !map.isFlat()
     map.size() == iters
     map.count() == iters
     final entries = map.toList()
@@ -229,61 +125,10 @@ class TaintedMapTest extends DDSpecification {
     }
   }
 
-  def 'garbage-collected entries are purged in flat mode'() {
+  def 'multi-threaded with no collisions, no GC'() {
     given:
     int capacity = 128
-    int flatModeThreshold = 64
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
-
-    int iters = 1
-    def gen = new ObjectGen(capacity)
-    def objectBuffer = new CircularBuffer<Object>(iters)
-
-    when:
-    // Number of purges required to switch to flat mode (in the absence of garbage collection)
-    final int purgesToFlatMode = (int) (flatModeThreshold / TaintedMap.PURGE_COUNT) + 1
-    gen.genObjects(purgesToFlatMode, ObjectGen.TRIGGERS_PURGE).each { o ->
-      final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-      queue.hold(o, to)
-      map.put(to)
-    }
-
-    then:
-    map.isFlat()
-
-    when:
-    (1..iters).each {
-      // Clear previous objects
-      queue.clear()
-      // Trigger purge
-      final o = gen.genObjects(1, ObjectGen.TRIGGERS_PURGE)[0]
-      final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-      objectBuffer.add(o)
-      map.put(to)
-    }
-
-    then:
-    map.isFlat()
-    map.size() == iters
-    map.count() == iters
-    final entries = map.toList()
-    entries.findAll { it.get() != null }.size() == iters
-
-    and: 'all objects are as expected'
-    objectBuffer.each { o ->
-      final to = map.get(o)
-      assert to != null
-      assert to.get() == o
-    }
-  }
-
-  def 'multi-threaded with no collisions, no GC, non-flat mode'() {
-    given:
-    int capacity = 128
-    int flatModeThreshold = 64
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
+    def map = new TaintedMap.TaintedMapImpl(capacity)
 
     and:
     int nThreads = 16
@@ -291,16 +136,16 @@ class TaintedMapTest extends DDSpecification {
     def gen = new ObjectGen(capacity)
     def executorService = Executors.newFixedThreadPool(nThreads)
     def latch = new CountDownLatch(nThreads)
-    def buckets = gen.genBuckets(nThreads, nObjectsPerThread, ObjectGen.DOES_NOT_TRIGGER_PURGE)
+    def buckets = gen.genBuckets(nThreads, nObjectsPerThread, ObjectGen.TRUE)
 
     when: 'puts from different threads to different buckets'
-    def futures = (0..nThreads-1).collect { thread ->
+    def futures = (0..nThreads - 1).collect { thread ->
       executorService.submit({
         ->
         latch.countDown()
         latch.await()
         buckets[thread].each { o ->
-          final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
+          final to = new TaintedObject(o, [] as Range[])
           map.put(to)
         }
       } as Runnable)
@@ -310,7 +155,6 @@ class TaintedMapTest extends DDSpecification {
     })
 
     then:
-    !map.isFlat()
     nThreads == buckets.size()
 
     and: 'all objects are as expected'
@@ -325,72 +169,10 @@ class TaintedMapTest extends DDSpecification {
     executorService?.shutdown()
   }
 
-  def 'multi-threaded with no collisions, no GC, flat mode'() {
-    given:
-    int capacity = 128
-    int flatModeThreshold = 64
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
-
-    and:
-    int nThreads = 16
-    def gen = new ObjectGen(capacity)
-    def executorService = Executors.newFixedThreadPool(nThreads)
-    def latch = new CountDownLatch(nThreads)
-
-    when:
-    // Number of purges required to switch to flat mode (in the absence of garbage collection)
-    final int purgesToFlatMode = (int) (flatModeThreshold / TaintedMap.PURGE_COUNT) + 1
-    gen.genObjects(purgesToFlatMode, ObjectGen.TRIGGERS_PURGE).each { o ->
-      final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-      queue.hold(o, to)
-      map.put(to)
-    }
-
-    then:
-    map.isFlat()
-
-    when: 'puts from different threads to any buckets'
-    def futures = (0..nThreads-1).collect { thread ->
-      // Each thread has multiple objects for each bucket
-      def objects = gen.genBuckets(capacity, 10).flatten()
-      def taintedObjects = objects.collect {o ->
-        final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-        queue.hold(o, to)
-        return to
-      }
-      Collections.shuffle(taintedObjects)
-
-      executorService.submit({
-        ->
-        latch.countDown()
-        latch.await()
-        taintedObjects.each { to ->
-          map.put(to)
-        }
-      } as Runnable)
-    }
-    futures.collect({
-      it.get()
-    })
-
-    then:
-    map.isFlat()
-    map.size() == capacity
-    map.count() == capacity
-    map.toList().findAll({ it.get() != null }).size() == capacity
-    map.toList().collect({ it.get() }).toSet().size() == capacity
-
-    cleanup:
-    executorService?.shutdown()
-  }
-
   def 'clear is thread-safe (does not throw)'() {
     given:
     int capacity = 128
-    int flatModeThreshold = 64
-    def queue = new MockReferenceQueue()
-    def map = new TaintedMap(capacity, flatModeThreshold, queue)
+    def map = new TaintedMap.TaintedMapImpl(capacity)
 
     and:
     int nThreads = 16
@@ -399,13 +181,11 @@ class TaintedMapTest extends DDSpecification {
     def latch = new CountDownLatch(nThreads)
 
     when: 'puts from different threads to any buckets'
-    def futures = (0..nThreads-1).collect { thread ->
+    def futures = (0..nThreads - 1).collect { thread ->
       // Each thread has multiple objects for each bucket
       def objects = gen.genBuckets(capacity, 32).flatten()
-      def taintedObjects = objects.collect {o ->
-        final to = new TaintedObject(o, [] as Range[], map.getReferenceQueue())
-        queue.hold(o, to)
-        return to
+      def taintedObjects = objects.collect { o ->
+        return new TaintedObject(o, [] as Range[])
       }
       Collections.shuffle(taintedObjects)
 
@@ -429,51 +209,74 @@ class TaintedMapTest extends DDSpecification {
     then:
     map.size() == 0
     map.count() == 0
-    !map.isFlat()
 
     cleanup:
     executorService?.shutdown()
   }
 
-  private static class MockReferenceQueue extends ReferenceQueue<Object> {
-    private List<Reference<?>> queue = new ArrayList()
-    private Map<Object, Reference<?>> objects = new HashMap<>()
+  void 'map purges elements on put/get'() {
+    given:
+    final capacity = 1 // single bucket
+    final map = new TaintedMap.TaintedMapImpl(1)
+    final gen = new ObjectGen(capacity)
+    final to = gen.genObjects(5, ObjectGen.TRUE).collect { new TaintedObject(it, [] as Range[]) }
 
-    void hold(Object referent, Reference<?> reference) {
-      objects.put(referent, reference)
-    }
+    when: 'purging the head with put'
+    map.put(to[0])
+    to[0].clear()
+    map.put(to[1])
 
-    void free(Reference<?> ref) {
-      def referent = ref.get()
-      ref.clear()
-      queue.push(ref)
-      if (referent != null) {
-        objects.remove(referent)
-      }
-    }
+    then:
+    map.size() == 1
+    map.count() == 1
 
-    void clear() {
-      objects.values().toList().each {
-        free(it)
-      }
-    }
+    when: 'purging an element in the middle with put'
+    map.put(to[2])
+    map.put(to[3])
+    to[2].clear()
+    map.put(to[4])
 
-    @Override
-    Reference<?> poll() {
-      if (queue.isEmpty()) {
-        return null
-      }
-      return queue.pop()
-    }
+    then:
+    map.size() == 3
+    map.count() == 3
 
-    @Override
-    Reference<?> remove() throws InterruptedException {
-      throw new UnsupportedOperationException("NOT IMPLEMENTED")
-    }
+    when: 'purging the tail with get'
+    to[4].clear()
+    map.get('I am not in the map!!!')
 
-    @Override
-    Reference<?> remove(long timeout) throws IllegalArgumentException, InterruptedException {
-      throw new UnsupportedOperationException("NOT IMPLEMENTED")
-    }
+    then:
+    map.size() == 2
+    map.count() == 2
+  }
+
+  void 'test no op implementation'() {
+    setup:
+    final instance = TaintedMap.NoOp.INSTANCE
+    final toTaint = 'test'
+
+    when:
+    final tainted = instance.put(new TaintedObject(toTaint, [] as Range[]))
+
+    then:
+    tainted == null
+    instance.get(toTaint) == null
+    instance.count() == 0
+    instance.size() == 0
+    !instance.iterator().hasNext()
+  }
+
+  void 'test debug instance'() {
+    setup:
+    final map = new TaintedMap.Debug(new TaintedMap.TaintedMapImpl())
+    final capacity = TaintedMap.Debug.COMPUTE_STATISTICS_INTERVAL
+    final gen = new ObjectGen(capacity)
+    logger.setLevel(Level.ALL)
+
+    when:
+    gen.genObjects(capacity, ObjectGen.TRUE).each { map.put(new TaintedObject(it, [] as Range[])) }
+
+    then:
+    map.size() == capacity
+    noExceptionThrown()
   }
 }
