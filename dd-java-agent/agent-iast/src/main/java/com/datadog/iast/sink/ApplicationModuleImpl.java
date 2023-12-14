3522cd5e8f47ca31b32446a6bce594e75823f59a
Alejandro Gonzalez <alejandro.gonzalez@datadoghq.com>
2023-12-14 09:30:28 +0100

    replace all the Clue calc to avoid parse web.xml multiple times

--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/ApplicationModuleImpl.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/ApplicationModuleImpl.java
@@ -8,10 +8,8 @@ import com.datadog.iast.model.VulnerabilityType;
 import datadog.trace.api.iast.sink.ApplicationModule;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -37,26 +35,38 @@ public class ApplicationModuleImpl extends SinkModuleBase implements Application
   private static final String DISPATCHER_SERVLET_VALUE =
       "org.springframework.web.servlet.DispatcherServlet";
 
-  private static final String DEFAULT_HTML_ESCAPE_PATTERN = "defaultHtmlEscape";
+  private static final String DEFAULT_HTML_ESCAPE = "defaultHtmlEscape";
 
-  private static final String TOMCAT_MANAGER_APPLICATION_PATTERN = "Tomcat Manager Application";
+  private static final String TOMCAT_MANAGER_APPLICATION = "Tomcat Manager Application";
 
   private static final String LISTINGS_PATTERN = "<param-name>listings</param-name>";
 
-  private static final String SESSION_TIMEOUT_PATTERN = "<session-timeout>";
+  private static final String SESSION_TIMEOUT_START_TAG = "<session-timeout>";
 
-  private static final String SECURITY_CONSTRAINT_PATTERN = "<security-constraint>";
+  private static final String SESSION_TIMEOUT_END_TAG = "</session-timeout>";
+
+  private static final String SECURITY_CONSTRAINT_START_TAG = "<security-constraint>";
+
+  private static final String SECURITY_CONSTRAINT_END_TAG = "</security-constraint>";
+
+  public static final String PARAM_VALUE_START_TAG = "<param-value>";
+
+  public static final String PARAM_VALUE_END_TAG = "</param-value>";
+
+  public static final String WEB_INF = "WEB-INF";
+
+  public static final String WEB_XML = "web.xml";
 
   private static final String REGEX =
       String.join(
           "|",
           CONTEXT_LOADER_LISTENER_PATTERN,
           DISPATCHER_SERVLET_PATTERN,
-          DEFAULT_HTML_ESCAPE_PATTERN,
-          TOMCAT_MANAGER_APPLICATION_PATTERN,
+          DEFAULT_HTML_ESCAPE,
+          TOMCAT_MANAGER_APPLICATION,
           LISTINGS_PATTERN,
-          SESSION_TIMEOUT_PATTERN,
-          SECURITY_CONSTRAINT_PATTERN);
+          SESSION_TIMEOUT_START_TAG,
+          SECURITY_CONSTRAINT_START_TAG);
 
   private static final Pattern PATTERN = Pattern.compile(REGEX);
 
@@ -91,19 +101,19 @@ public class ApplicationModuleImpl extends SinkModuleBase implements Application
         case CONTEXT_LOADER_LISTENER_VALUE:
           isSpring = true;
           break;
-        case DEFAULT_HTML_ESCAPE_PATTERN:
+        case DEFAULT_HTML_ESCAPE:
           defaultHtmlEscapeIndex = matcher.start();
           break;
-        case TOMCAT_MANAGER_APPLICATION_PATTERN:
+        case TOMCAT_MANAGER_APPLICATION:
           reportAdminConsoleActive(span);
           break;
         case LISTINGS_PATTERN:
           checkDirectoryListingLeak(webXmlContent, matcher.start(), span);
           break;
-        case SESSION_TIMEOUT_PATTERN:
+        case SESSION_TIMEOUT_START_TAG:
           checkSessionTimeOut(webXmlContent, matcher.start(), span);
           break;
-        case SECURITY_CONSTRAINT_PATTERN:
+        case SECURITY_CONSTRAINT_START_TAG:
           checkVerbTampering(webXmlContent, matcher.start(), span);
           break;
       }
@@ -119,77 +129,70 @@ public class ApplicationModuleImpl extends SinkModuleBase implements Application
     String value = "false";
     if (defaultHtmlEscapeIndex != -1) {
       int start =
-          webXmlContent.indexOf("<param-value>", defaultHtmlEscapeIndex) + "<param-value>".length();
-      value = webXmlContent.substring(start, webXmlContent.indexOf("</param-value>", start));
+          webXmlContent.indexOf(PARAM_VALUE_START_TAG, defaultHtmlEscapeIndex)
+              + PARAM_VALUE_START_TAG.length();
+      value = webXmlContent.substring(start, webXmlContent.indexOf(PARAM_VALUE_END_TAG, start));
     }
     if (defaultHtmlEscapeIndex == -1 || !Boolean.parseBoolean(value)) {
-      reporter.report(
+      report(
           span,
-          new Vulnerability(
-              VulnerabilityType.DEFAULT_HTML_ESCAPE_INVALID,
-              Location.forSpanAndFileAndLine(span, "web.xml", -1),
-              new Evidence("defaultHtmlEscape tag should be set")));
+          VulnerabilityType.DEFAULT_HTML_ESCAPE_INVALID,
+          "defaultHtmlEscape tag should be set");
     }
   }
 
   private void reportAdminConsoleActive(AgentSpan span) {
-    reporter.report(
-        span,
-        new Vulnerability(
-            VulnerabilityType.ADMIN_CONSOLE_ACTIVE,
-            Location.forSpanAndFileAndLine(span, "web.xml", -1),
-            new Evidence("Tomcat Manager Application")));
+    report(span, VulnerabilityType.ADMIN_CONSOLE_ACTIVE, "Tomcat Manager Application");
   }
 
   private void checkDirectoryListingLeak(
       final String webXmlContent, int index, final AgentSpan span) {
-    int valueIndex = webXmlContent.indexOf("<param-value>", index);
-    int valueLast = webXmlContent.indexOf("</param-value>", valueIndex);
+    int valueIndex =
+        webXmlContent.indexOf(PARAM_VALUE_START_TAG, index) + PARAM_VALUE_START_TAG.length();
+    int valueLast = webXmlContent.indexOf(PARAM_VALUE_END_TAG, valueIndex);
     String data = webXmlContent.substring(valueIndex, valueLast);
-    if (data.trim().toLowerCase().contains("true")) {
-      reporter.report(
-          span,
-          new Vulnerability(
-              VulnerabilityType.DIRECTORY_LISTING_LEAK,
-              Location.forSpanAndFileAndLine(span, "web.xml", -1),
-              new Evidence("Directory listings configured")));
+    if (data.trim().toLowerCase().equals("true")) {
+      report(span, VulnerabilityType.DIRECTORY_LISTING_LEAK, "Directory listings configured");
     }
   }
 
   private void checkSessionTimeOut(final String webXmlContent, int index, final AgentSpan span) {
-    Clue clue =
-        getFirstClue(webXmlContent.substring(index), "<session-timeout>", "</session-timeout>");
-    String innerText = clue.getValue();
-    if (innerText != null) {
-      innerText = innerText.trim();
-      try {
-        int timeoutValue = Integer.parseInt(innerText);
-        if (timeoutValue > 30 || timeoutValue == -1) {
-          reporter.report(
-              span,
-              new Vulnerability(
-                  VulnerabilityType.SESSION_TIMEOUT,
-                  Location.forSpanAndFileAndLine(span, "web.xml", clue.getLine()),
-                  new Evidence("Found vulnerable timeout value: " + timeoutValue)));
-        }
-      } catch (NumberFormatException e) {
-        // Nothing to do
+    try {
+      String innerText =
+          webXmlContent
+              .substring(
+                  index + SESSION_TIMEOUT_START_TAG.length(),
+                  webXmlContent.indexOf(SESSION_TIMEOUT_END_TAG, index))
+              .trim();
+      int timeoutValue = Integer.parseInt(innerText);
+      if (timeoutValue > 30 || timeoutValue == -1) {
+        report(
+            span,
+            VulnerabilityType.SESSION_TIMEOUT,
+            "Found vulnerable timeout value: " + timeoutValue);
       }
+    } catch (NumberFormatException e) {
+      // Nothing to do
     }
   }
 
   private void checkVerbTampering(final String webXmlContent, int index, final AgentSpan span) {
-    Clue clue =
-        getFirstClue(
-            webXmlContent.substring(index), "<security-constraint>", "</security-constraint>");
-    if (clue.getValue() != null && !clue.getValue().contains("<http-method>")) {
-      reporter.report(
-          span,
-          new Vulnerability(
-              VulnerabilityType.VERB_TAMPERING,
-              Location.forSpanAndFileAndLine(span, "web.xml", clue.getLine()),
-              new Evidence("http-method not defined in web.xml")));
+    String innerText =
+        webXmlContent
+            .substring(
+                index + SECURITY_CONSTRAINT_START_TAG.length(),
+                webXmlContent.indexOf(SECURITY_CONSTRAINT_END_TAG, index))
+            .trim();
+    if (!innerText.contains("<http-method>")) {
+      report(span, VulnerabilityType.VERB_TAMPERING, "http-method not defined in web.xml");
     }
+  }
+
+  private void report(AgentSpan span, VulnerabilityType verbTampering, String value) {
+    reporter.report(
+        span,
+        new Vulnerability(
+            verbTampering, Location.forSpanAndFileAndLine(span, WEB_XML, -1), new Evidence(value)));
   }
 
   private void checkInsecureJSPLayout(String realPath, AgentSpan span) {
@@ -204,16 +207,13 @@ public class ApplicationModuleImpl extends SinkModuleBase implements Application
     reporter.report(
         span,
         new Vulnerability(
-            VulnerabilityType.INSECURE_JSP_LAYOUT,
-            Location.forSpanAndFileAndLine(span, "web.xml", -1),
-            new Evidence(result)));
+            VulnerabilityType.INSECURE_JSP_LAYOUT, Location.forSpan(span), new Evidence(result)));
   }
 
   @Nullable
   private String webXmlContent(final String realPath) {
     if (realPath != null) {
-      Path path =
-          Paths.get(realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "web.xml");
+      Path path = Paths.get(realPath + File.separatorChar + WEB_INF + File.separatorChar + WEB_XML);
       if (path.toFile().exists()) {
         try {
           return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
@@ -225,58 +225,13 @@ public class ApplicationModuleImpl extends SinkModuleBase implements Application
     return null;
   }
 
-  private Clue getFirstClue(final String text, final String startToken, final String endToken) {
-    Clue match = new Clue();
-    if (text == null) {
-      return match;
-    }
-
-    BufferedReader br = new BufferedReader(new StringReader(text));
-
-    try {
-      int startLine = -1;
-      int endLine = -1;
-      int currentLine = 0;
-      StringBuilder innerText = new StringBuilder();
-      String buff;
-      while ((buff = br.readLine()) != null) {
-        if (startLine == -1 && buff.contains(startToken)) {
-          startLine = currentLine;
-          int idx = buff.indexOf(startToken);
-
-          int endIdx = buff.indexOf(endToken);
-          if (endIdx != -1) {
-            innerText.append(buff.substring(idx + startToken.length(), endIdx));
-            endLine = currentLine;
-            match.setLine(startLine);
-            match.setInnerText(innerText.toString());
-            return match;
-          }
-        } else if (endLine == -1 && buff.contains(endToken)) {
-          int idx = buff.indexOf(endToken);
-          innerText.append(buff.substring(0, idx));
-          endLine = currentLine;
-          match.setLine(startLine);
-          match.setInnerText(innerText.toString());
-          return match;
-        } else if (startLine != -1 && endLine == -1) {
-          innerText.append(buff);
-        }
-        currentLine++;
-      }
-    } catch (IOException e) {
-      // Ignore
-    }
-    return match;
-  }
-
   private List<String> findRecursively(final File root) {
     List<String> jspPaths = new ArrayList<>();
     if (root.listFiles() != null) {
       for (File file : root.listFiles()) {
         if (file.isFile() && isJsp(file.getName().toLowerCase())) {
           jspPaths.add(file.getName());
-        } else if (file.isDirectory() && !"WEB-INF".equals(file.getName())) {
+        } else if (file.isDirectory() && !WEB_INF.equals(file.getName())) {
           addDirectoryJSPs("", file, jspPaths);
         }
       }
@@ -299,30 +254,5 @@ public class ApplicationModuleImpl extends SinkModuleBase implements Application
 
   private boolean isJsp(final String name) {
     return name.endsWith(".jsp") || name.endsWith(".jspx");
-  }
-
-  class Clue {
-    @Nullable private String innerText;
-
-    private int line;
-
-    Clue() {}
-
-    @Nullable
-    String getValue() {
-      return innerText;
-    }
-
-    void setInnerText(final String txt) {
-      innerText = txt;
-    }
-
-    int getLine() {
-      return line;
-    }
-
-    void setLine(final int line) {
-      this.line = line;
-    }
   }
 }
