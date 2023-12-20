5577c8d979685f002bead1879b2730cf09d9563c
Manuel Álvarez Álvarez <manuel-alvarez-alvarez@users.noreply.github.com>
2023-12-20 14:11:48 +0100

    Add time and bucket size boundaries to the tainted map (#6389)

--- internal-api/src/main/java/datadog/trace/api/iast/telemetry/IastMetric.java
+++ internal-api/src/main/java/datadog/trace/api/iast/telemetry/IastMetric.java
@@ -21,7 +21,8 @@ public enum IastMetric {
   EXECUTED_SINK(
       "executed.sink", true, Scope.REQUEST, Tag.VULNERABILITY_TYPE, Verbosity.INFORMATION),
   EXECUTED_TAINTED("executed.tainted", true, Scope.REQUEST, Verbosity.DEBUG),
-  REQUEST_TAINTED("request.tainted", true, Scope.REQUEST, Verbosity.INFORMATION);
+  REQUEST_TAINTED("request.tainted", true, Scope.REQUEST, Verbosity.INFORMATION),
+  TAINTED_FLAT_MODE("tainted.flat.mode", false, Scope.GLOBAL, Verbosity.INFORMATION);
 
   private static final int COUNT;
 
