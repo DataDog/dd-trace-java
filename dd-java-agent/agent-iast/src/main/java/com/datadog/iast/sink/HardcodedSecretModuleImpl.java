f13ab0a1871e9478b839fce8062a13929458ef06
Alejandro Gonzalez <alejandro.gonzalez@datadoghq.com>
2024-01-15 15:17:53 +0100

    use map instead of Set of Secret

--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/HardcodedSecretModuleImpl.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/HardcodedSecretModuleImpl.java
@@ -6,7 +6,8 @@ import com.datadog.iast.Dependencies;
 import com.datadog.iast.util.HardcodedSecretMatcher;
 import com.datadog.iast.util.IastClassVisitor;
 import datadog.trace.api.iast.sink.HardcodedSecretModule;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -24,50 +25,32 @@ public class HardcodedSecretModuleImpl extends SinkModuleBase implements Hardcod
       @Nonnull final String clazz,
       final @Nonnull byte[] classFile) {
 
-    Set<Secret> secrets = getSecrets(literals, clazz);
+    Map<String, String> secrets = getSecrets(literals);
     if (secrets != null) {
       reportVulnerability(secrets, clazz, classFile);
     }
   }
 
   private void reportVulnerability(
-      final Set<Secret> secrets, final String clazz, final @Nonnull byte[] classFile) {
+      final Map<String, String> secrets, final String clazz, final @Nonnull byte[] classFile) {
     ClassReader classReader = new ClassReader(classFile);
     IastClassVisitor classVisitor = new IastClassVisitor(secrets, clazz, reporter);
     classReader.accept(classVisitor, 0);
   }
 
   @Nullable
-  private Set<Secret> getSecrets(final Set<String> literals, final String clazz) {
-    Set<Secret> secrets = null;
+  private Map<String, String> getSecrets(final Set<String> literals) {
+    Map<String, String> secrets = null;
     for (String literal : literals) {
       for (HardcodedSecretMatcher secretMatcher : HARDCODED_SECRET_MATCHERS) {
         if (secretMatcher.matches(literal)) {
           if (secrets == null) {
-            secrets = new HashSet<>();
+            secrets = new HashMap<>();
           }
-          secrets.add(new Secret(literal, secretMatcher.getRedactedEvidence()));
+          secrets.put(literal, secretMatcher.getRedactedEvidence());
         }
       }
     }
     return secrets;
-  }
-
-  public static class Secret {
-    private final String value;
-    private final String redacted;
-
-    public Secret(final String value, final String redacted) {
-      this.value = value;
-      this.redacted = redacted;
-    }
-
-    public String getValue() {
-      return value;
-    }
-
-    public String getRedacted() {
-      return redacted;
-    }
   }
 }
--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/util/IastClassVisitor.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/util/IastClassVisitor.java
@@ -5,8 +5,7 @@ import com.datadog.iast.model.Evidence;
 import com.datadog.iast.model.Location;
 import com.datadog.iast.model.Vulnerability;
 import com.datadog.iast.model.VulnerabilityType;
-import com.datadog.iast.sink.HardcodedSecretModuleImpl.Secret;
-import java.util.Set;
+import java.util.Map;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
@@ -14,11 +13,12 @@ import org.objectweb.asm.Opcodes;
 
 public class IastClassVisitor extends ClassVisitor {
 
-  private final Set<Secret> secrets;
+  private final Map<String, String> secrets;
   private final String clazz;
   private final Reporter reporter;
 
-  public IastClassVisitor(final Set<Secret> secrets, final String clazz, final Reporter reporter) {
+  public IastClassVisitor(
+      final Map<String, String> secrets, final String clazz, final Reporter reporter) {
     super(Opcodes.ASM9);
     this.secrets = secrets;
     this.clazz = clazz;
@@ -33,7 +33,7 @@ public class IastClassVisitor extends ClassVisitor {
 
   static class IastMethodVisitor extends MethodVisitor {
 
-    private final Set<Secret> secrets;
+    private final Map<String, String> secrets;
     private final String clazz;
     private final Reporter reporter;
 
@@ -42,7 +42,7 @@ public class IastClassVisitor extends ClassVisitor {
     private int currentLine;
 
     public IastMethodVisitor(
-        final Set<Secret> secrets,
+        final Map<String, String> secrets,
         final String clazz,
         final String method,
         final Reporter reporter) {
@@ -62,17 +62,17 @@ public class IastClassVisitor extends ClassVisitor {
     public void visitLdcInsn(Object cst) {
       if (cst instanceof String) {
         String literal = ((String) cst);
-        for (Secret secret : secrets) {
-          if (secret.getValue().equals(literal)) {
-            reporter.report(
-                null,
-                new Vulnerability(
-                    VulnerabilityType.HARDCODED_SECRET,
-                    Location.forClassAndMethodAndLine(clazz, method, currentLine),
-                    new Evidence(secret.getRedacted())));
-            break;
-          }
-        }
+        secrets.forEach(
+            (key, value) -> {
+              if (key.equals(literal)) {
+                reporter.report(
+                    null,
+                    new Vulnerability(
+                        VulnerabilityType.HARDCODED_SECRET,
+                        Location.forClassAndMethodAndLine(clazz, method, currentLine),
+                        new Evidence(value)));
+              }
+            });
       }
     }
   }
