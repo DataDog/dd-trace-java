63e9d216e15c98f397171a5cdece98d32bf3cdc7
Bruce Bujon <bruce.bujon@datadoghq.com>
2024-01-22 11:35:30 +0100

    feat: Move configuration and tasks into plugin

--- buildSrc/src/main/groovy/otel/OtelConverterPlugin.groovy
+++ buildSrc/src/main/groovy/otel/OtelConverterPlugin.groovy
@@ -1,8 +1,10 @@
 package otel
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.TaskAction
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import org.slf4j.Logger
@@ -18,28 +20,62 @@ class OtelConverterPlugin implements Plugin<Project> {
 
   @Override
   void apply(Project project) {
+    // Declare "javaagent" configuration to declare dependencies to convert
+    project.configurations {
+      javaagent {
+        visible = false // Only accessible inside this project
+        transitive = false // Only fetch javaagent modules and not their dependencies
+      }
+    }
     // Create and initialize extension
     def extension = project.extensions.create('otelConverter', OtelConverterExtension)
     extension.sourceDirectory.convention(project.layout.buildDirectory.dir('javaagent'))
     extension.targetDirectory.convention(project.layout.buildDirectory.dir('classes'))
-    // Create task
-    project.task('convertOtelJavaAgent') {
-      doLast {
-        println '>>> Hello from plugin'
-        def sourceDirectory = extension.sourceDirectory.asFile.get()
-        def targetDirectory = extension.targetDirectory.asFile.get()
-        def converter = new Converter(sourceDirectory, targetDirectory)
-        sourceDirectory.traverse(
-          type: FILES,
-          nameFilter: ~/.*\.class$/
-        ) {
-          converter.convert(it)
+    // Register tasks
+    def fetchTask = project.tasks.register('fetchOtelJavaAgent', FetchJavaAgent).get()
+    def convertTask = project.tasks.register('convertOtelJavaAgent', ConvertJavaAgent).get()
+    def buildTask = project.tasks.named('build').get()
+    buildTask.dependsOn(convertTask)
+    convertTask.dependsOn(fetchTask)
+  }
+}
+
+/** Fetch and extract OpenTelemetry javaagent artifacts */
+class FetchJavaAgent extends DefaultTask {
+  @TaskAction
+  void fetch() {
+    println '>>> Running fetch task'
+    project.copy {
+      project.configurations.named('javaagent').get().resolve().each {
+        from(project.zipTree(it)) {
+          include '**/*.class'
+          // TODO Include META-INF like native-image configuration?
+          // TODO Include resource files?
         }
       }
+      into project.extensions.getByType(OtelConverterExtension).sourceDirectory
     }
   }
 }
 
+class ConvertJavaAgent extends DefaultTask {
+  @TaskAction
+  void convert() {
+    println '>>> Running convert task'
+    def extension = project.extensions.getByType(OtelConverterExtension)
+    def sourceDirectory = extension.sourceDirectory.asFile.get()
+    def targetDirectory = extension.targetDirectory.asFile.get()
+    def converter = new Converter(sourceDirectory, targetDirectory)
+    sourceDirectory.traverse(
+      type: FILES,
+      nameFilter: ~/.*\.class$/
+    ) {
+      converter.convert(it)
+    }
+  }
+}
+
+// TODO Merge with its task?
 class Converter {
   private static final Logger LOGGER = LoggerFactory.getLogger(OtelConverterPlugin.class)
   private final File sourceDirectory
