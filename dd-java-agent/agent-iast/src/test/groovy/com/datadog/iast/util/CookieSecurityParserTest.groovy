4711e083cb8f0a1b63e79dd497129285f47fa996
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2024-01-25 18:17:19 +0100

    Improve performance of the cookie parser by using an FSM

--- dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/util/CookieSecurityParserTest.groovy
+++ dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/util/CookieSecurityParserTest.groovy
@@ -29,22 +29,21 @@ class CookieSecurityParserTest extends Specification {
     sameSite == badCookie.getSameSite()
 
     where:
-    header                                                                                                                                           | cookieName | isSecure | isHttpOnly | sameSite
-    "user-id=7"                                                                                                                                      | "user-id"  | false    | false      | null
-    "user-id=7;Secure"                                                                                                                               | "user-id"  | true     | false      | null
-    "user-id=7;Secure;HttpOnly"                                                                                                                      | "user-id"  | true     | true       | null
-    "CUSTOMER=WILE_E_COYOTE; version='1'"                                                                                                            | "CUSTOMER" | false    | false      | null
-    "CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday, 09-Nov-99 23:12:40 GMT"                                                                      | "CUSTOMER" | false    | false      | null
-    "CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday, 09-Nov-99 23:12:40 GMT;SameSite=Lax;HttpOnly"                                                | "CUSTOMER" | false    | true       | 'Lax'
-    "PREF=ID=1eda537de48ac25d:CR=1:TM=1112868587:LM=1112868587:S=t3FPA-mT9lTR3bxU;expires=Sun, 17-Jan-2038 19:14:07 GMT; path=/; domain=.google.com" | "PREF"     | false    | false      | null
-    "CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday, 09-Nov-99 23:12:40 GMT; Secure"                                                              | "CUSTOMER" | true     | false      | null
-    "CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday, 09-Nov-99 23:12:40 GMT; path=\"/acme\";SameSite=Strict"                                      | "CUSTOMER" | false    | false      | 'Strict'
+    header                                                                                                                                                       | cookieName | isSecure | isHttpOnly | sameSite
+    "Set-Cookie: user-id=7"                                                                                                                                      | "user-id"  | false    | false      | null
+    'Set-Cookie: user-id="7"'                                                                                                                                    | "user-id"  | false    | false      | null
+    "Set-Cookie: user-id=7;Secure"                                                                                                                               | "user-id"  | true     | false      | null
+    "Set-Cookie: user-id=7;Secure;HttpOnly"                                                                                                                      | "user-id"  | true     | true       | null
+    "Set-Cookie: CUSTOMER=WILE_E_COYOTE; version='1'"                                                                                                            | "CUSTOMER" | false    | false      | null
+    "Set-Cookie: CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday, 09-Nov-99 23:12:40 GMT"                                                                      | "CUSTOMER" | false    | false      | null
+    "Set-Cookie: CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday, 09-Nov-99 23:12:40 GMT;SameSite=Lax;HttpOnly"                                                | "CUSTOMER" | false    | true       | 'Lax'
+    "Set-Cookie: PREF=ID=1eda537de48ac25d:CR=1:TM=1112868587:LM=1112868587:S=t3FPA-mT9lTR3bxU;expires=Sun, 17-Jan-2038 19:14:07 GMT; path=/; domain=.google.com" | "PREF"     | false    | false      | null
+    "Set-Cookie: CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday, 09-Nov-99 23:12:40 GMT; Secure"                                                              | "CUSTOMER" | true     | false      | null
+    "Set-Cookie: CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday, 09-Nov-99 23:12:40 GMT; path=\"/acme\";SameSite=Strict"                                      | "CUSTOMER" | false    | false      | 'Strict'
   }
 
 
   void 'parsing multi cookie header'() {
-    given:
-    String headerValue = "A=1;Secure;HttpOnly=true;SameSite=Strict;version='1',B=2;Secure;SameSite=Strict,C=3"
     when:
     final badCookies = CookieSecurityParser.parse(headerValue)
 
@@ -66,5 +65,35 @@ class CookieSecurityParserTest extends Specification {
     !badCookies.get(2).isSecure()
     !badCookies.get(2).isHttpOnly()
     badCookies.get(2).getSameSite() == null
+
+    where:
+    headerValue                                                                                       | _
+    "Set-Cookie: A=1;Secure;HttpOnly=true;SameSite=Strict;version='1',B=2;Secure;SameSite=Strict,C=3" | _
+    "Set-Cookie2: A=1;Secure;HttpOnly=true;SameSite=Strict,B=2;Secure;SameSite=Strict,C=3"            | _
+  }
+
+  void 'parsing wrong header'() {
+    when:
+    final cookies = CookieSecurityParser.parse('Un-Set-Cookie: user-id=7')
+
+    then:
+    cookies.empty
+  }
+
+  void 'parsing badly formatted cookies'() {
+    when:
+    final cookies = CookieSecurityParser.parse(header)
+
+    then:
+    cookies.size() == 1
+    final cookie = cookies.first()
+    cookie.cookieName == 'user-id'
+    cookie.secure
+
+
+    where:
+    header                              | _
+    'Set-Cookie: user-id=7;;;Secure;;'  | _
+    'Set-Cookie: user-id=7;Secure===;;' | _
   }
 }
