1a9b99a87b132d15a9f192d926fea9da4765b959
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2024-01-29 10:40:09 -0500

    Overhauling Matchers to support non-String types
    Previously TagsMatcher would raise a ClassCastException if a tag had a non-String value
    
    This change introduces matches methods for Object & primitive types.
    For numeric types, the matches routines only support integer values even for floating point types.
    This was done for consistency with languages that only support floating point, but to avoid matching complexities posed by floating point impression.
    The bulk of the handling of other types is done by the new BaseMatcher class from which PatternMatcher now extends.
    
    To handle the "*" case, a new AnyMatcher is introduced which accepts all values including non-integral floating point values.  This changeset also adds support for equivalent patterns like "**" and "***".
    
    New tests have been added to RuleSamplingTest & MatcherTests to cover a wide range of tag types.

--- dd-trace-core/src/test/groovy/datadog/trace/core/util/MatchersTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/core/util/MatchersTest.groovy
@@ -4,12 +4,12 @@ import datadog.trace.test.util.DDSpecification
 
 class MatchersTest extends DDSpecification {
 
-  def "match-all scenarios must return a null matcher"() {
+  def "match-all scenarios must return an any matcher"() {
     expect:
-    Matchers.compileGlob(glob) == null
+    Matchers.compileGlob(glob).isAny()
 
     where:
-    glob << [null, "*"]
+    glob << [null, "*", "**"]
   }
 
   def "pattern without * or ? must be an ExactMatcher"() {
@@ -41,14 +41,40 @@ class MatchersTest extends DDSpecification {
     def matcher = Matchers.compileGlob(pattern)
 
     then:
-    matcher.matches(matching)
-    !matcher.matches(nonMatching)
+    matcher.matches(value) == matches
 
     where:
-    pattern | matching | nonMatching
-    "Fo?"   | "Foo"    | "Fooo"
-    "Fo*"   | "Fo"     | "Fa"
-    "F*B?r" | "FooBar" | "FooFar"
-    ""      | ""       | "non-empty"
+    pattern | value                    | matches
+    "Fo?"   | "Foo"                    | true
+    "Fo?"   | new StringBuilder("Foo") | true
+    "Fo?"   | "Fooo"                   | false
+    "Fo*"   | "Fo"                     | true
+    "Fo*"   | "Fa"                     | false
+    "F*B?r" | "FooBar"                 | true
+    "F*B?r" | "FooFar"                 | false
+    "true"  | true                     | true
+    "false" | false                    | true
+    ""      | ""                       | true
+    ""      | "non-empty"              | false
+    "*"     | "foo"                    | true
+    "**"    | "foo"                    | true 
+    "???"   | "foo"                    | true 
+    "20"    | 20                       | true 
+    "-20"   | -20                      | true 
+    "20"    | (byte)(20)               | true
+    "20"    | (short)(20)              | true
+    "20"    | 20L                      | true 
+    "20"    | 20F                      | true 
+    "20"    | 20D                      | true
+    "20"    | new BigInteger("20")     | true
+    "20"    | new BigDecimal("20")     | true
+    "2*"    | 20.1F                    | false
+    "2*"    | 20.1D                    | false
+    "2*"    | new BigDecimal("20.1")   | false
+    "*"     | new Object() {}          | true
+    "**"    | new Object() {}          | true
+    "?"     | new Object() {}          | false
+    "*"     | null                     | true
+    "?"     | null                     | false
   }
 }
