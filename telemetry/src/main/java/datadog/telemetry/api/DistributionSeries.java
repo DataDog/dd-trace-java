0028f8569e96fc2b8215b505552fc6f3e3f80717
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2024-02-08 12:40:57 +0100

    Add support for periodic submission of telemetry distribution series

--- telemetry/src/main/java/datadog/telemetry/api/DistributionSeries.java
+++ telemetry/src/main/java/datadog/telemetry/api/DistributionSeries.java
@@ -28,6 +28,10 @@ public class DistributionSeries {
     return this;
   }
 
+  public void addPoint(Integer point) {
+    points.add(point);
+  }
+
   public List<String> getTags() {
     return tags;
   }
