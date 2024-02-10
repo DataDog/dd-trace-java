a973a165234562ef26ffefa25ffe772ba5665b46
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2024-02-10 18:54:07 +0100

    Add more test cases

--- telemetry/src/test/groovy/datadog/telemetry/TelemetryClientTest.groovy
+++ telemetry/src/test/groovy/datadog/telemetry/TelemetryClientTest.groovy
@@ -5,21 +5,27 @@ import spock.lang.Specification
 
 class TelemetryClientTest extends Specification {
 
-  def "Uses CI Visibility agentless URL if configured to do so"() {
+  def "Intake client uses CI Visibility agentless URL if configured to do so"() {
     setup:
     def config = Stub(Config)
     config.getApiKey() >> "dummy-key"
     config.getAgentTimeout() >> 123
-    config.isCiVisibilityEnabled() >> true
-    config.isCiVisibilityAgentlessEnabled() >> true
-
-    def agentlessUrl = "http://ci.visibility.agentless.url"
-    config.getCiVisibilityAgentlessUrl() >> agentlessUrl
+    config.getSite() >> "datad0g.com"
+    config.isCiVisibilityEnabled() >> ciVisEnabled
+    config.isCiVisibilityAgentlessEnabled() >> ciVisAgentlessEnabled
+    config.getCiVisibilityAgentlessUrl() >> ciVisAgentlessUrl
 
     when:
     def intakeClient = TelemetryClient.buildIntakeClient(config)
 
     then:
-    intakeClient.getUrl().toString() == agentlessUrl + "/api/v2/apmtelemetry"
+    intakeClient.getUrl().toString() == expectedUrl
+
+    where:
+    ciVisEnabled | ciVisAgentlessEnabled | ciVisAgentlessUrl                    | expectedUrl
+    true         | true                  | "http://ci.visibility.agentless.url" | "http://ci.visibility.agentless.url/api/v2/apmtelemetry"
+    false        | true                  | "http://ci.visibility.agentless.url" | "https://all-http-intake.logs.datad0g.com/api/v2/apmtelemetry"
+    true         | false                 | "http://ci.visibility.agentless.url" | "https://all-http-intake.logs.datad0g.com/api/v2/apmtelemetry"
+    true         | true                  | null                                 | "https://all-http-intake.logs.datad0g.com/api/v2/apmtelemetry"
   }
 }
