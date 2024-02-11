8e746069d27c4ec0ee2e6578d4a06db61f2af17c
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2024-02-11 20:21:35 +0000

    Simplify MuzzleGenerateTest

--- dd-java-agent/src/test/groovy/datadog/trace/agent/integration/muzzle/MuzzleGenerateTest.groovy
+++ dd-java-agent/src/test/groovy/datadog/trace/agent/integration/muzzle/MuzzleGenerateTest.groovy
@@ -8,16 +8,11 @@ class MuzzleGenerateTest extends Specification {
   def "muzzle references generated for all instrumentation"() {
     setup:
     List<Class> missingMatchers = []
-    ClassLoader agentClassLoader = IntegrationTestUtils.getAgentClassLoader()
-    Class<?> instrumenterClass = agentClassLoader.loadClass('datadog.trace.agent.tooling.Instrumenter')
+    ClassLoader agentClassLoader = IntegrationTestUtils.agentClassLoader
     Class<?> instrumenterModuleClass = agentClassLoader.loadClass('datadog.trace.agent.tooling.InstrumenterModule')
-    for (Object instrumenter : ServiceLoader.load(instrumenterClass, agentClassLoader)) {
-      if (!instrumenterModuleClass.isInstance(instrumenter)) {
-        // muzzle only applies to instrumenter modules
-        continue
-      }
-      if (instrumenter.instrumentationMuzzle == null) {
-        missingMatchers.add(instrumenter.class)
+    for (Object module : ServiceLoader.load(instrumenterModuleClass, agentClassLoader)) {
+      if (module.instrumentationMuzzle == null) {
+        missingMatchers.add(module.class)
       }
     }
     expect:
