b571f2ccab171bd46c618a2424ec409e3620fb78
Andrea Marziali <andrea.marziali@datadoghq.com>
2024-02-12 10:51:46 +0100

    update tests

--- dd-java-agent/instrumentation/spring-boot/src/test/groovy/SpringBootApplicationTest.groovy
+++ dd-java-agent/instrumentation/spring-boot/src/test/groovy/SpringBootApplicationTest.groovy
@@ -1,7 +1,7 @@
 import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
 
 import datadog.trace.agent.test.AgentTestRunner
-import datadog.trace.api.naming.SpanNaming
+import datadog.trace.api.Config
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.boot.SpringApplication
 import spock.lang.Unroll
@@ -16,13 +16,8 @@ class SpringBootApplicationTest extends AgentTestRunner {
     }
   }
 
-
   @Unroll
   def 'should change service name before bean factory initializes'() {
-    setup:
-    if (userService != null) {
-      injectSysConfig("service", userService)
-    }
     def context = SpringApplication.run(BeanWhoTraces, args)
     when:
     context.start()
@@ -38,16 +33,37 @@ class SpringBootApplicationTest extends AgentTestRunner {
     context != null
     cleanup:
     context?.stop()
-    SpanNaming.instance().localRoot().maybeOverrideServiceName(null)
     where:
-    userService   | expectedService | args
-    null          | "args"          | new String[]{
+    expectedService | args
+    "args"          | new String[]{
       "--spring.application.name=args"
     }
-    "my-service"  | "my-service"    | new String[]{
-      "--spring.application.name=args"
-    }
-    "my-service"  | "my-service"    | new String[0]
-    null          | "props"         | new String[0] // will load from properties
+    "props"         | new String[0] // will load from properties
+  }
+}
+
+class SpringBootApplicationNotAppliedForkedTest extends AgentTestRunner {
+  @Override
+  protected void configurePreAgent() {
+    super.configurePreAgent()
+    injectSysConfig("service", "myservice")
+  }
+
+  def 'should not service name when user inferred dd_service'() {
+    def context = SpringApplication.run(SpringBootApplicationTest.BeanWhoTraces, new String[0])
+    when:
+    context.start()
+    then:
+    assertTraces(1, {
+      trace(1) {
+        span {
+          serviceName "myservice"
+          operationName "spring"
+        }
+      }
+    })
+    context != null
+    cleanup:
+    context?.stop()
   }
 }
