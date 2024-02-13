45a9f8cd33c66202b873017cc551c2976bfb091e
Paul Laffon <paul.laffon@datadoghq.com>
2024-02-13 14:23:10 +0100

    Adapt for InstrumenterModule renaming

--- dd-java-agent/instrumentation/spark-executor/src/main/java/datadog/trace/instrumentation/spark/SparkExecutorInstrumentation.java
+++ dd-java-agent/instrumentation/spark-executor/src/main/java/datadog/trace/instrumentation/spark/SparkExecutorInstrumentation.java
@@ -10,13 +10,14 @@ import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.InstrumenterModule;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import net.bytebuddy.asm.Advice;
 import org.apache.spark.executor.Executor;
 
 @AutoService(Instrumenter.class)
-public class SparkExecutorInstrumentation extends Instrumenter.Tracing
+public class SparkExecutorInstrumentation extends InstrumenterModule.Tracing
     implements Instrumenter.ForSingleType {
 
   public SparkExecutorInstrumentation() {
