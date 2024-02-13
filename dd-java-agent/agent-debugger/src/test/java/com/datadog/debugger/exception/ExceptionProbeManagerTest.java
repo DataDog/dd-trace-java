77b49feebf2138c561162feb45778a5a0a3339fb
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2024-02-13 07:26:48 +0100

    split test and rename

--- dd-java-agent/agent-debugger/src/test/java/com/datadog/debugger/exception/ExceptionProbeManagerTest.java
+++ dd-java-agent/agent-debugger/src/test/java/com/datadog/debugger/exception/ExceptionProbeManagerTest.java
@@ -9,6 +9,8 @@ import java.util.Collections;
 import org.junit.jupiter.api.Test;
 
 class ExceptionProbeManagerTest {
+  private final RuntimeException exception = new RuntimeException("test");
+
   @Test
   public void instrumentStackTrace() {
     ClassNameFiltering classNameFiltering = new ClassNameFiltering(Collections.emptyList());
@@ -20,29 +22,32 @@ class ExceptionProbeManagerTest {
   }
 
   @Test
-  void frameFiltering() throws Exception {
+  void instrumentSingleFrame() {
     ClassNameFiltering classNameFiltering =
         new ClassNameFiltering(Arrays.asList("org.gradle.", "worker.org.gradle.", "org.junit."));
     ExceptionProbeManager exceptionProbeManager = new ExceptionProbeManager(classNameFiltering);
-    RuntimeException exception = new RuntimeException("test");
+
     String fingerprint = Fingerprinter.fingerprint(exception, classNameFiltering);
-    assertEquals("aff4c51e8660c93bae3343799a1c7d2d16309621144018c4d77b7e01a252e", fingerprint);
+    assertEquals("aa4a4dd768f6ef0fcc2b39a3bdedcbe44baff2e9dd0a779228db7bd8bf58", fingerprint);
     exceptionProbeManager.createProbesForException(fingerprint, exception.getStackTrace());
     assertEquals(1, exceptionProbeManager.getProbes().size());
     ExceptionProbe exceptionProbe = exceptionProbeManager.getProbes().iterator().next();
     assertEquals(
         "com.datadog.debugger.exception.ExceptionProbeManagerTest",
         exceptionProbe.getWhere().getTypeName());
-    // retry filtering the top frame
-    classNameFiltering =
+  }
+
+  @Test
+  void filterAllFrames() {
+    ClassNameFiltering classNameFiltering =
         new ClassNameFiltering(
             Arrays.asList(
                 "org.gradle.",
                 "worker.org.gradle.",
                 "org.junit.",
                 "com.datadog.debugger.exception.ExceptionProbeManagerTest"));
-    exceptionProbeManager = new ExceptionProbeManager(classNameFiltering);
-    fingerprint = Fingerprinter.fingerprint(exception, classNameFiltering);
+    ExceptionProbeManager exceptionProbeManager = new ExceptionProbeManager(classNameFiltering);
+    String fingerprint = Fingerprinter.fingerprint(exception, classNameFiltering);
     assertEquals("7a1e5e1bcc64ee26801d1471245eff6b6e8d7c61d0ea36fe85f3f75d79e42c", fingerprint);
     exceptionProbeManager.createProbesForException("", exception.getStackTrace());
     assertEquals(0, exceptionProbeManager.getProbes().size());
