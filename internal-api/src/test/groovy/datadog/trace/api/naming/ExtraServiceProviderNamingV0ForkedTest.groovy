eb24a3afd38eb944af15371b2acc08b8912eca5b
Alejandro González García <alejandro.gonzalez@datadoghq.com>
2024-02-14 10:31:01 +0100

    Update ExtraServiceProviderNamingV0ForkedTest.groovy

--- internal-api/src/test/groovy/datadog/trace/api/naming/ExtraServiceProviderNamingV0ForkedTest.groovy
+++ internal-api/src/test/groovy/datadog/trace/api/naming/ExtraServiceProviderNamingV0ForkedTest.groovy
@@ -12,7 +12,6 @@ class ExtraServiceProviderNamingV0ForkedTest extends DDSpecification {
     final extraServiceProvider = Mock(ExtraServicesProvider)
     ExtraServicesProvider.INSTANCE = extraServiceProvider
     def ddService = "testService"
-    injectSysConfig(TracerConfig.TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED, "false")
     injectSysConfig(GeneralConfig.SERVICE_NAME, ddService)
     final schema = SpanNaming.instance().namingSchema()
 
