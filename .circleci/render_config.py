4b295d66fe9b50debfe9709f774bc31d6ddea5c1
Santiago Mola <santiago.mola@datadoghq.com>
2024-02-16 08:48:58 +0100

    Fix setup job in CI

--- .circleci/render_config.py
+++ .circleci/render_config.py
@@ -67,6 +67,7 @@ if pr_url:
     pr_base_ref = data.get("base", {}).get("ref")
 else:
     labels = set()
+    pr_base_ref = ""
 
 branch = os.environ.get("CIRCLE_BRANCH", "")
 run_all = "all" in labels
