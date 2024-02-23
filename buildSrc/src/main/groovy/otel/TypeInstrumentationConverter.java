0a598325a64f87a7d81b85e7bbedacac9496ec56
Bruce Bujon <PerfectSlayer@users.noreply.github.com>
2024-02-23 15:49:24 +0100

    Add OpenTelemetry integrations support (#6526)
    * feat: Add otel instrumentation module
    * feat: Add otel converter build plugin
    * feat: Move configuration and tasks into plugin
    * feat: Add OTel API verifier POC
    * feat: Add muzzle converter POC
    * feat: Add forbidden api plugin configuration
    * feat: Add muzzle reference capture WIP
    * feat: Add muzzle flag converter tests
    * feat: Add WIP field and method muzzle ref capture
    * feat: Complete muzzle ref capture and add tests
    * feat: Add muzzle class generation
    * feat: Upgrade OTel converter plugin to avoid batch processing
    * feat: Add OTel muzzle clean up
    * feat: Add contextStore() method conversion
    * feat: Add instrumentation module conversion
    * feat: Add type instrumentation conversion
    * feat: Add service loader config generation
    * feat: Add missing frame computation
    * feat: Implement TypeInstrumentation conversion
    * feat: Disable module by default

---
+++ buildSrc/src/main/groovy/otel/TypeInstrumentationConverter.java
@@ -0,0 +1,79 @@
+package otel;
+
+import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
+import static org.objectweb.asm.Opcodes.ACONST_NULL;
+import static org.objectweb.asm.Opcodes.ARETURN;
+import static org.objectweb.asm.Opcodes.ASM9;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+/**
+ * This class visitor converts OpenTelemetry TypeInstrumentation into Datadog instrumenter.
+ */
+public class TypeInstrumentationConverter extends AbstractClassVisitor {
+  static final String TYPE_INSTRUMENTATION_CLASS_NAME = "io/opentelemetry/javaagent/extension/instrumentation/TypeInstrumentation";
+  private static final String FOR_TYPE_HIERARCHY_CLASS_NAME = "datadog/trace/agent/tooling/Instrumenter$ForTypeHierarchy";
+  private static final String TYPE_MATCHER_METHOD_NAME = "typeMatcher";
+  private static final String TYPE_MATCHER_DESC = "()Lnet/bytebuddy/matcher/ElementMatcher;";
+  private static final String HIERARCHY_MATCHER_METHOD_NAME = "hierarchyMatcher";
+  private static final String HAS_ADVICE_CLASS_NAME = "datadog/trace/instrumentation/opentelemetry/automatic/HasAdvice";
+
+  public TypeInstrumentationConverter(ClassVisitor classVisitor, String className) {
+    super(classVisitor, className);
+  }
+
+  @Override
+  public void visitEnd() {
+    if (inheritsTypeInstrumentation()) {
+      convertTypeInstrumentation();
+    }
+    if (this.next != null) {
+      ClassNode cn = (ClassNode) this.cv;
+      cn.accept(this.next);
+    }
+  }
+
+  private boolean inheritsTypeInstrumentation() {
+    ClassNode classNode = (ClassNode) this.cv;
+    return classNode.interfaces.stream().anyMatch(TYPE_INSTRUMENTATION_CLASS_NAME::equals);
+  }
+
+  private void convertTypeInstrumentation() {
+    ClassNode classNode = (ClassNode) this.cv;
+    // Remove OTel TypeInstrumentation interface
+    classNode.interfaces.remove(TYPE_INSTRUMENTATION_CLASS_NAME);
+    // Implement Instrumenter.ForTypeHierarchy
+    classNode.interfaces.add(FOR_TYPE_HIERARCHY_CLASS_NAME);
+    addHierarchyMarkerTypeMethod();
+    convertTypeMatcherMethod();
+    // Implement HasAdvice wrapper
+    classNode.interfaces.add(HAS_ADVICE_CLASS_NAME);
+  }
+
+  private void addHierarchyMarkerTypeMethod() {
+    ClassNode classNode = (ClassNode) this.cv;
+
+    MethodNode methodNode = new MethodNode(
+        ASM9,
+        ACC_PUBLIC,
+        "hierarchyMarkerType",
+        "()Ljava/lang/String;",
+        "()Ljava/lang/String;",
+        null
+    );
+    InsnList instructions = methodNode.instructions;
+    instructions.add(new InsnNode(ACONST_NULL));
+    instructions.add(new InsnNode(ARETURN));
+
+    classNode.methods.add(methodNode);
+  }
+
+  private void convertTypeMatcherMethod() {
+    MethodNode methodNode = findMethodNode(TYPE_MATCHER_METHOD_NAME, TYPE_MATCHER_DESC);
+    methodNode.name = HIERARCHY_MATCHER_METHOD_NAME;
+  }
+}
