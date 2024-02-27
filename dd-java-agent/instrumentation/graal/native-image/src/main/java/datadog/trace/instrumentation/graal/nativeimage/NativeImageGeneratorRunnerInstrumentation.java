86e8f6e6d1ea0a301feb28296344773e7b6db000
Richard Startin <richard.startin@datadoghq.com>
2024-02-27 11:29:51 +0000

    register events for native image

--- dd-java-agent/instrumentation/graal/native-image/src/main/java/datadog/trace/instrumentation/graal/nativeimage/NativeImageGeneratorRunnerInstrumentation.java
+++ dd-java-agent/instrumentation/graal/native-image/src/main/java/datadog/trace/instrumentation/graal/nativeimage/NativeImageGeneratorRunnerInstrumentation.java
@@ -62,6 +62,8 @@ public final class NativeImageGeneratorRunnerInstrumentation
               + "com.datadog.profiling.controller.openjdk.events.AvailableProcessorCoresEvent:build_time,"
               + "com.datadog.profiling.controller.openjdk.events.DeadlockEvent:build_time,"
               + "com.datadog.profiling.controller.openjdk.events.ProfilerSettingEvent:build_time,"
+              + "com.datadog.profiling.controller.openjdk.events.EndpointEvent:build_time,"
+              + "com.datadog.profiling.controller.openjdk.events.QueueTimeEvent:build_time,"
               + "com.datadog.profiling.controller.openjdk.events.TimelineEvent:build_time,"
               + "datadog.trace.api.Config:rerun,"
               + "datadog.trace.api.Platform:rerun,"
