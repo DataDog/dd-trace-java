f91e87af4b188a34cf3b1743f3871b9ab55cf7f5
Richard Startin <richard.startin@datadoghq.com>
2024-02-27 17:19:23 +0000

    collapse implementations of timer and checkpointer interfaces into profiling context (#6738)
    * unify checkpointer and contextintegration interfaces
    
    * factor queue timers into context integration interface
    
    * register events for native image

--- dd-java-agent/agent-profiling/profiling-controller-openjdk/src/main/java/com/datadog/profiling/controller/openjdk/JFREventContextIntegration.java
+++ dd-java-agent/agent-profiling/profiling-controller-openjdk/src/main/java/com/datadog/profiling/controller/openjdk/JFREventContextIntegration.java
@@ -1,16 +1,42 @@
 package com.datadog.profiling.controller.openjdk;
 
+import com.datadog.profiling.controller.openjdk.events.EndpointEvent;
+import com.datadog.profiling.controller.openjdk.events.QueueTimeEvent;
 import com.datadog.profiling.controller.openjdk.events.TimelineEvent;
 import com.datadog.profiling.utils.ExcludedVersions;
+import datadog.trace.api.EndpointTracker;
 import datadog.trace.api.Platform;
 import datadog.trace.api.Stateful;
+import datadog.trace.api.config.ProfilingConfig;
+import datadog.trace.api.profiling.Timing;
+import datadog.trace.bootstrap.config.provider.ConfigProvider;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.ProfilerContext;
 import datadog.trace.bootstrap.instrumentation.api.ProfilingContextIntegration;
+import jdk.jfr.EventType;
 
 public class JFREventContextIntegration implements ProfilingContextIntegration {
 
   public JFREventContextIntegration() {
     ExcludedVersions.checkVersionExclusion();
+    // Note: Loading CheckpointEvent when JFRCheckpointer is loaded is important because it also
+    // loads JFR classes - which may not be present on some JVMs
+    EventType.getEventType(EndpointEvent.class);
+    isEndpointCollectionEnabled =
+        ConfigProvider.getInstance()
+            .getBoolean(
+                ProfilingConfig.PROFILING_ENDPOINT_COLLECTION_ENABLED,
+                ProfilingConfig.PROFILING_ENDPOINT_COLLECTION_ENABLED_DEFAULT);
+    isTimelineEventsEnabled =
+        ConfigProvider.getInstance()
+            .getBoolean(
+                ProfilingConfig.PROFILING_TIMELINE_EVENTS_ENABLED,
+                ProfilingConfig.PROFILING_TIMELINE_EVENTS_ENABLED_DEFAULT);
+    isQueueTimeEnabled =
+        ConfigProvider.getInstance()
+            .getBoolean(
+                ProfilingConfig.PROFILING_QUEUEING_TIME_ENABLED,
+                ProfilingConfig.PROFILING_QUEUEING_TIME_ENABLED_DEFAULT);
   }
 
   // native image process will enable context integration immediately - the value will get
@@ -18,6 +44,9 @@ public class JFREventContextIntegration implements ProfilingContextIntegration {
   // java agent process will enable context integration when the agent is started and onStart is
   // called
   private volatile boolean isStarted = !Platform.isNativeImageBuilder();
+  private final boolean isEndpointCollectionEnabled;
+  private final boolean isTimelineEventsEnabled;
+  private final boolean isQueueTimeEnabled;
 
   @Override
   public void onStart() {
@@ -27,7 +56,7 @@ public class JFREventContextIntegration implements ProfilingContextIntegration {
 
   @Override
   public Stateful newScopeState(ProfilerContext profilerContext) {
-    if (!isStarted) {
+    if (!isTimelineEventsEnabled || !isStarted) {
       return Stateful.DEFAULT;
     }
     return new TimelineEvent(
@@ -39,5 +68,27 @@ public class JFREventContextIntegration implements ProfilingContextIntegration {
   @Override
   public String name() {
     return "jfr";
+  }
+
+  @Override
+  public void onRootSpanFinished(AgentSpan rootSpan, EndpointTracker tracker) {
+    if (isEndpointCollectionEnabled && tracker != null) {
+      tracker.endpointWritten(rootSpan);
+    }
+  }
+
+  @Override
+  public EndpointTracker onRootSpanStarted(AgentSpan rootSpan) {
+    return isEndpointCollectionEnabled
+        ? new EndpointEvent(rootSpan.getSpanId())
+        : EndpointTracker.NO_OP;
+  }
+
+  @Override
+  public Timing start(TimerType type) {
+    if (isQueueTimeEnabled && type == TimerType.QUEUEING) {
+      return new QueueTimeEvent();
+    }
+    return Timing.NoOp.INSTANCE;
   }
 }
