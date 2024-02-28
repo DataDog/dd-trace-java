9a13db1d0a91c27121f3772995f665ae490eada0
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2024-02-28 12:25:06 +0100

    Try to make kafka smoke tests non-flaky by using spring reply mechanism

--- dd-smoke-tests/kafka-2/src/main/java/datadog/smoketest/kafka/iast/IastController.java
+++ dd-smoke-tests/kafka-2/src/main/java/datadog/smoketest/kafka/iast/IastController.java
@@ -2,23 +2,22 @@ package datadog.smoketest.kafka.iast;
 
 import static datadog.smoketest.kafka.iast.IastConfiguration.BYTE_ARRAY_TOPIC;
 import static datadog.smoketest.kafka.iast.IastConfiguration.BYTE_BUFFER_TOPIC;
-import static datadog.smoketest.kafka.iast.IastConfiguration.GROUP_ID;
 import static datadog.smoketest.kafka.iast.IastConfiguration.JSON_TOPIC;
 import static datadog.smoketest.kafka.iast.IastConfiguration.STRING_TOPIC;
 
 import java.nio.ByteBuffer;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.requestreply.RequestReplyFuture;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,20 +27,20 @@ public class IastController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(IastController.class);
 
-  private final KafkaTemplate<String, String> stringTemplate;
-  private final KafkaTemplate<byte[], byte[]> byteArrayTemplate;
-  private final KafkaTemplate<ByteBuffer, ByteBuffer> byteBufferTemplate;
-  private final KafkaTemplate<IastMessage, IastMessage> jsonTemplate;
-  private final PriorityBlockingQueue<String> queue = new PriorityBlockingQueue<>();
+  private final ReplyingKafkaTemplate<String, String, String> stringTemplate;
+  private final ReplyingKafkaTemplate<byte[], byte[], String> byteArrayTemplate;
+  private final ReplyingKafkaTemplate<ByteBuffer, ByteBuffer, String> byteBufferTemplate;
+  private final ReplyingKafkaTemplate<IastMessage, IastMessage, String> jsonTemplate;
 
   public IastController(
-      @Qualifier("iastStringKafkaTemplate") final KafkaTemplate<String, String> stringTemplate,
-      @Qualifier("iastByteArrayKafkaTemplate")
-          final KafkaTemplate<byte[], byte[]> byteArrayTemplate,
-      @Qualifier("iastByteBufferKafkaTemplate")
-          final KafkaTemplate<ByteBuffer, ByteBuffer> byteBufferTemplate,
-      @Qualifier("iastJsonKafkaTemplate")
-          final KafkaTemplate<IastMessage, IastMessage> jsonTemplate) {
+      @Qualifier("iastStringTemplate")
+          final ReplyingKafkaTemplate<String, String, String> stringTemplate,
+      @Qualifier("iastByteArrayTemplate")
+          final ReplyingKafkaTemplate<byte[], byte[], String> byteArrayTemplate,
+      @Qualifier("iastByteBufferTemplate")
+          final ReplyingKafkaTemplate<ByteBuffer, ByteBuffer, String> byteBufferTemplate,
+      @Qualifier("iastJsonTemplate")
+          final ReplyingKafkaTemplate<IastMessage, IastMessage, String> jsonTemplate) {
     this.stringTemplate = stringTemplate;
     this.byteArrayTemplate = byteArrayTemplate;
     this.byteBufferTemplate = byteBufferTemplate;
@@ -50,114 +49,110 @@ public class IastController {
 
   @GetMapping("/iast/health")
   public ResponseEntity<String> health() {
-    stringTemplate.send(STRING_TOPIC, "health", null);
-    return ResponseEntity.ok("OK");
+    return sendAndReceive("health", null, stringTemplate, STRING_TOPIC);
   }
 
   @GetMapping("/iast/kafka/string")
-  public CompletableFuture<ResponseEntity<String>> string(@RequestParam("type") final String type) {
-    return sendAndWait(type, stringTemplate, STRING_TOPIC, Function.identity());
+  public ResponseEntity<String> string(@RequestParam("type") final String type) {
+    return sendAndReceive(type, stringTemplate, STRING_TOPIC, Function.identity());
   }
 
   @GetMapping("/iast/kafka/byteArray")
-  public CompletableFuture<ResponseEntity<String>> byteArray(
-      @RequestParam("type") final String type) {
-    return sendAndWait(type, byteArrayTemplate, BYTE_ARRAY_TOPIC, String::getBytes);
+  public ResponseEntity<String> byteArray(@RequestParam("type") final String type) {
+    return sendAndReceive(type, byteArrayTemplate, BYTE_ARRAY_TOPIC, String::getBytes);
   }
 
   @GetMapping("/iast/kafka/byteBuffer")
-  public CompletableFuture<ResponseEntity<String>> byteBuffer(
-      @RequestParam("type") final String type) {
-    return sendAndWait(
+  public ResponseEntity<String> byteBuffer(@RequestParam("type") final String type) {
+    return sendAndReceive(
         type, byteBufferTemplate, BYTE_BUFFER_TOPIC, it -> ByteBuffer.wrap(it.getBytes()));
   }
 
   @GetMapping("/iast/kafka/json")
-  public CompletableFuture<ResponseEntity<String>> json(@RequestParam("type") final String type) {
-    return sendAndWait(type, jsonTemplate, JSON_TOPIC, IastMessage::new);
+  public ResponseEntity<String> json(@RequestParam("type") final String type) {
+    return sendAndReceive(type, jsonTemplate, JSON_TOPIC, IastMessage::new);
   }
 
-  @KafkaListener(
-      groupId = GROUP_ID,
-      topics = STRING_TOPIC,
-      containerFactory = "iastStringListenerFactory")
-  public void listenString(final ConsumerRecord<String, String> record) {
+  @KafkaListener(topics = STRING_TOPIC, containerFactory = "iastStringListener")
+  @SendTo
+  public String listenString(final ConsumerRecord<String, String> record) {
     String key = record.key();
     String value = record.value();
-    handle(STRING_TOPIC, key, value);
+    return handle(STRING_TOPIC, key, value);
   }
 
-  @KafkaListener(
-      groupId = GROUP_ID,
-      topics = BYTE_ARRAY_TOPIC,
-      containerFactory = "iastByteArrayListenerFactory")
-  public void listenByteArray(final ConsumerRecord<byte[], byte[]> record) {
+  @KafkaListener(topics = BYTE_ARRAY_TOPIC, containerFactory = "iastByteArrayListener")
+  @SendTo
+  public String listenByteArray(final ConsumerRecord<byte[], byte[]> record) {
     byte[] key = record.key();
     byte[] value = record.value();
-    handle(BYTE_ARRAY_TOPIC, new String(key), value == null ? null : new String(value));
+    return handle(BYTE_ARRAY_TOPIC, new String(key), value == null ? null : new String(value));
   }
 
-  @KafkaListener(
-      groupId = GROUP_ID,
-      topics = BYTE_BUFFER_TOPIC,
-      containerFactory = "iastByteBufferListenerFactory")
-  public void listenByteBuffer(final ConsumerRecord<ByteBuffer, ByteBuffer> record) {
+  @KafkaListener(topics = BYTE_BUFFER_TOPIC, containerFactory = "iastByteBufferListener")
+  @SendTo
+  public String listenByteBuffer(final ConsumerRecord<ByteBuffer, ByteBuffer> record) {
     ByteBuffer key = record.key();
     ByteBuffer value = record.value();
-    handle(
+    return handle(
         BYTE_BUFFER_TOPIC,
         new String(key.array(), key.arrayOffset(), key.limit()),
         value == null ? null : new String(value.array(), value.arrayOffset(), value.limit()));
   }
 
-  @KafkaListener(
-      groupId = GROUP_ID,
-      topics = JSON_TOPIC,
-      containerFactory = "iastJsonListenerFactory")
-  public void listenJson(final ConsumerRecord<IastMessage, IastMessage> record) {
+  @KafkaListener(topics = JSON_TOPIC, containerFactory = "iastJsonListener")
+  @SendTo
+  public String listenJson(final ConsumerRecord<IastMessage, IastMessage> record) {
     final IastMessage key = record.key();
     final IastMessage value = record.value();
-    handle(JSON_TOPIC, key.getValue(), value == null ? null : value.getValue());
+    return handle(JSON_TOPIC, key.getValue(), value == null ? null : value.getValue());
   }
 
-  private <E> CompletableFuture<ResponseEntity<String>> sendAndWait(
+  private <E> ResponseEntity<String> sendAndReceive(
       final String type,
-      final KafkaTemplate<E, E> template,
+      final ReplyingKafkaTemplate<E, E, String> template,
       final String topic,
       final Function<String, E> mapper) {
-    LOGGER.info("Sending message to: " + topic);
     final boolean isKey = isKey(type);
     final String key = isKey ? type : "mock key";
     final String value = !isKey ? type : "mock value";
-    return template
-        .send(topic, mapper.apply(key), mapper.apply(value))
-        .completable()
-        .thenApply(result -> handleKafkaResponse(type, result));
+    LOGGER.info("Sending message to {}: {} {}", topic, key, value);
+    return sendAndReceive(mapper.apply(key), mapper.apply(value), template, topic);
   }
 
-  private void handle(final String topic, final String key, final String value) {
+  private <E> ResponseEntity<String> sendAndReceive(
+      final E key,
+      final E value,
+      final ReplyingKafkaTemplate<E, E, String> template,
+      final String topic) {
+    final ProducerRecord<E, E> record = new ProducerRecord<>(topic, key, value);
+    final RequestReplyFuture<E, E, String> future = template.sendAndReceive(record);
+    try {
+      future.getSendFuture().get(10, TimeUnit.SECONDS); // send ok
+      final ConsumerRecord<E, String> reply = future.get(10, TimeUnit.SECONDS); // reply
+      if (reply == null || !"OK".equals(reply.value())) {
+        return ResponseEntity.internalServerError()
+            .body(reply == null ? "REPLY_TIMEOUT" : reply.value());
+      } else {
+        return ResponseEntity.ok("OK");
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private String handle(final String topic, final String key, final String value) {
     LOGGER.info("Received message from {}: {} {}", topic, key, value);
     if (isKey(key)) {
       LOGGER.info("Kafka tainted key: " + key);
-      queue.add(key);
+      return "OK";
     } else if (isValue(value)) {
       LOGGER.info("Kafka tainted value: " + value);
-      queue.add(value);
+      return "OK";
+    } else if ("health".equals(key)) {
+      return "OK";
     }
-  }
-
-  private <E> ResponseEntity<String> handleKafkaResponse(
-      final String expected, final SendResult<E, E> result) {
-    try {
-      final String key = queue.poll(10, TimeUnit.SECONDS); // wait for Kafka to consume the message
-      if (expected.equals(key) && result.getRecordMetadata().hasOffset()) {
-        return ResponseEntity.ok("OK");
-      } else {
-        return ResponseEntity.internalServerError().body(key + " != " + expected);
-      }
-    } catch (InterruptedException e) {
-      return ResponseEntity.internalServerError().body(e.getMessage());
-    }
+    return "NO_OK";
   }
 
   private boolean isKey(final String key) {
--- dd-smoke-tests/kafka-3/application/src/main/java/datadog/smoketest/kafka/iast/IastConfiguration.java
+++ dd-smoke-tests/kafka-3/application/src/main/java/datadog/smoketest/kafka/iast/IastConfiguration.java
@@ -3,7 +3,6 @@ package datadog.smoketest.kafka.iast;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -24,13 +23,13 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class IastConfiguration {
-
-  public static final String GROUP_ID = "iast";
 
   public static final String STRING_TOPIC = "iast_string";
 
@@ -39,6 +38,14 @@ public class IastConfiguration {
   public static final String BYTE_BUFFER_TOPIC = "iast_byteBuffer";
 
   public static final String JSON_TOPIC = "iast_json";
+
+  public static final String REPLY_STRING_TOPIC = "iast_string_reply";
+
+  public static final String REPLY_BYTE_ARRAY_TOPIC = "iast_byteArray_reply";
+
+  public static final String REPLY_BYTE_BUFFER_TOPIC = "iast_byteBuffer_reply";
+
+  public static final String REPLY_JSON_TOPIC = "iast_json_reply";
 
   @Value("${spring.kafka.bootstrap-servers}")
   private String boostrapServers;
@@ -49,82 +56,277 @@ public class IastConfiguration {
         newTopic(STRING_TOPIC),
         newTopic(BYTE_ARRAY_TOPIC),
         newTopic(BYTE_BUFFER_TOPIC),
-        newTopic(JSON_TOPIC));
+        newTopic(JSON_TOPIC),
+        newTopic(REPLY_STRING_TOPIC),
+        newTopic(REPLY_BYTE_ARRAY_TOPIC),
+        newTopic(REPLY_BYTE_BUFFER_TOPIC),
+        newTopic(REPLY_JSON_TOPIC));
   }
 
   @Bean
-  public ConcurrentKafkaListenerContainerFactory<String, String> iastStringListenerFactory() {
-    return listenerFor(StringDeserializer.class, config -> {});
+  public DefaultKafkaConsumerFactory<String, String> iastStringConsumer() {
+    return consumerFor(STRING_TOPIC, StringDeserializer.class, StringDeserializer.class);
   }
 
   @Bean
-  public ConcurrentKafkaListenerContainerFactory<byte[], byte[]> iastByteArrayListenerFactory() {
-    return listenerFor(ByteArrayDeserializer.class, config -> {});
+  public DefaultKafkaConsumerFactory<byte[], byte[]> iastByteArrayConsumer() {
+    return consumerFor(BYTE_ARRAY_TOPIC, ByteArrayDeserializer.class, ByteArrayDeserializer.class);
   }
 
   @Bean
-  public ConcurrentKafkaListenerContainerFactory<ByteBuffer, ByteBuffer>
-      iastByteBufferListenerFactory() {
-    return listenerFor(ByteBufferDeserializer.class, config -> {});
+  public DefaultKafkaConsumerFactory<ByteBuffer, ByteBuffer> iastByteBufferConsumer() {
+    return consumerFor(
+        BYTE_BUFFER_TOPIC, ByteBufferDeserializer.class, ByteBufferDeserializer.class);
   }
 
   @Bean
-  public ConcurrentKafkaListenerContainerFactory<IastMessage, IastMessage> iastJsonListenerFactory() {
-    return listenerFor(
-        JsonDeserializer.class,
-        config -> config.put(JsonDeserializer.TRUSTED_PACKAGES, "datadog.*"));
+  public DefaultKafkaConsumerFactory<IastMessage, IastMessage> iastJsonConsumer() {
+    final Class<? extends Deserializer<IastMessage>> deserializer = jsonDeserializer();
+    return consumerFor(JSON_TOPIC, deserializer, deserializer);
   }
 
   @Bean
-  public KafkaTemplate<String, String> iastStringKafkaTemplate() {
-    return templateFor(StringSerializer.class);
+  public DefaultKafkaConsumerFactory<String, String> iastReplyStringConsumer() {
+    return consumerFor(REPLY_STRING_TOPIC, StringDeserializer.class, StringDeserializer.class);
   }
 
   @Bean
-  public KafkaTemplate<byte[], byte[]> iastByteArrayKafkaTemplate() {
-    return templateFor(ByteArraySerializer.class);
+  public DefaultKafkaConsumerFactory<byte[], String> iastReplyByteArrayConsumer() {
+    return consumerFor(
+        REPLY_BYTE_ARRAY_TOPIC, ByteArrayDeserializer.class, StringDeserializer.class);
   }
 
   @Bean
-  public KafkaTemplate<ByteBuffer, ByteBuffer> iastByteBufferKafkaTemplate() {
-    return templateFor(ByteBufferSerializer.class);
+  public DefaultKafkaConsumerFactory<ByteBuffer, String> iastReplyByteBufferConsumer() {
+    return consumerFor(
+        REPLY_BYTE_BUFFER_TOPIC, ByteBufferDeserializer.class, StringDeserializer.class);
   }
 
   @Bean
-  public KafkaTemplate<IastMessage, IastMessage> iastJsonKafkaTemplate() {
-    return templateFor(JsonSerializer.class);
+  public DefaultKafkaConsumerFactory<IastMessage, String> iastReplyJsonConsumer() {
+    return consumerFor(REPLY_JSON_TOPIC, jsonDeserializer(), StringDeserializer.class);
   }
 
-  @SuppressWarnings("rawtypes")
-  private <E> ConcurrentKafkaListenerContainerFactory<E, E> listenerFor(
-      final Class<? extends Deserializer> deserializer,
-      final Consumer<Map<String, Object>> consumer) {
-    final Map<String, Object> config = new HashMap<>();
-    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
-    config.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-    config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, deserializer);
-    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-    consumer.accept(config);
-    final DefaultKafkaConsumerFactory<E, E> consumerFactory =
-        new DefaultKafkaConsumerFactory<>(config);
-    ConcurrentKafkaListenerContainerFactory<E, E> factory =
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, String> iastStringListener() {
+    final ConcurrentKafkaListenerContainerFactory<String, String> factory =
         new ConcurrentKafkaListenerContainerFactory<>();
-    factory.setConsumerFactory(consumerFactory);
+    factory.setConsumerFactory(iastStringConsumer());
+    factory.setReplyTemplate(iastReplyStringTemplate());
     return factory;
   }
 
-  @SuppressWarnings("rawtypes")
-  private <E> KafkaTemplate<E, E> templateFor(final Class<? extends Serializer> serializer) {
-    Map<String, Object> configProps = new HashMap<>();
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<byte[], byte[]> iastByteArrayListener() {
+    final ConcurrentKafkaListenerContainerFactory<byte[], byte[]> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(iastByteArrayConsumer());
+    factory.setReplyTemplate(iastReplyByteArrayTemplate());
+    return factory;
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<ByteBuffer, ByteBuffer> iastByteBufferListener() {
+    final ConcurrentKafkaListenerContainerFactory<ByteBuffer, ByteBuffer> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(iastByteBufferConsumer());
+    factory.setReplyTemplate(iastReplyByteBufferTemplate());
+    return factory;
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<IastMessage, IastMessage> iastJsonListener() {
+    final ConcurrentKafkaListenerContainerFactory<IastMessage, IastMessage> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(iastJsonConsumer());
+    factory.setReplyTemplate(iastReplyJsonTemplate());
+    return factory;
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, String> iastReplyStringListener() {
+    final ConcurrentKafkaListenerContainerFactory<String, String> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(iastReplyStringConsumer());
+    return factory;
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<byte[], String> iastReplyByteArrayListener() {
+    final ConcurrentKafkaListenerContainerFactory<byte[], String> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(iastReplyByteArrayConsumer());
+    return factory;
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<ByteBuffer, String> iastReplyByteBufferListener() {
+    final ConcurrentKafkaListenerContainerFactory<ByteBuffer, String> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(iastReplyByteBufferConsumer());
+    return factory;
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<IastMessage, String> iastReplyJsonListener() {
+    final ConcurrentKafkaListenerContainerFactory<IastMessage, String> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(iastReplyJsonConsumer());
+    return factory;
+  }
+
+  @Bean
+  public ConcurrentMessageListenerContainer<String, String> iastReplyStringContainer() {
+    ConcurrentMessageListenerContainer<String, String> repliesContainer =
+        iastReplyStringListener().createContainer(REPLY_STRING_TOPIC);
+    repliesContainer.setAutoStartup(false);
+    return repliesContainer;
+  }
+
+  @Bean
+  public ConcurrentMessageListenerContainer<byte[], String> iastReplyByteArrayContainer() {
+    ConcurrentMessageListenerContainer<byte[], String> repliesContainer =
+        iastReplyByteArrayListener().createContainer(REPLY_BYTE_ARRAY_TOPIC);
+    repliesContainer.setAutoStartup(false);
+    return repliesContainer;
+  }
+
+  @Bean
+  public ConcurrentMessageListenerContainer<ByteBuffer, String> iastReplyByteBufferContainer() {
+    ConcurrentMessageListenerContainer<ByteBuffer, String> repliesContainer =
+        iastReplyByteBufferListener().createContainer(REPLY_BYTE_BUFFER_TOPIC);
+    repliesContainer.setAutoStartup(false);
+    return repliesContainer;
+  }
+
+  @Bean
+  public ConcurrentMessageListenerContainer<IastMessage, String> iastReplyJsonContainer() {
+    ConcurrentMessageListenerContainer<IastMessage, String> repliesContainer =
+        iastReplyJsonListener().createContainer(REPLY_JSON_TOPIC);
+    repliesContainer.setAutoStartup(false);
+    return repliesContainer;
+  }
+
+  @Bean
+  public DefaultKafkaProducerFactory<String, String> iastStringProducer() {
+    return producerFor(StringSerializer.class, StringSerializer.class);
+  }
+
+  @Bean
+  public DefaultKafkaProducerFactory<byte[], byte[]> iastByteArrayProducer() {
+    return producerFor(ByteArraySerializer.class, ByteArraySerializer.class);
+  }
+
+  @Bean
+  public DefaultKafkaProducerFactory<ByteBuffer, ByteBuffer> iastByteBufferProducer() {
+    return producerFor(ByteBufferSerializer.class, ByteBufferSerializer.class);
+  }
+
+  @Bean
+  public DefaultKafkaProducerFactory<IastMessage, IastMessage> iastJsonProducer() {
+    final Class<? extends Serializer<IastMessage>> serializer = jsonSerializer();
+    return producerFor(serializer, serializer);
+  }
+
+  @Bean
+  public DefaultKafkaProducerFactory<String, String> iastReplyStringProducer() {
+    return producerFor(StringSerializer.class, StringSerializer.class);
+  }
+
+  @Bean
+  public DefaultKafkaProducerFactory<byte[], String> iastReplyByteArrayProducer() {
+    return producerFor(ByteArraySerializer.class, StringSerializer.class);
+  }
+
+  @Bean
+  public DefaultKafkaProducerFactory<ByteBuffer, String> iastReplyByteBufferProducer() {
+    return producerFor(ByteBufferSerializer.class, StringSerializer.class);
+  }
+
+  @Bean
+  public DefaultKafkaProducerFactory<IastMessage, String> iastReplyJsonProducer() {
+    return producerFor(jsonSerializer(), StringSerializer.class);
+  }
+
+  @Bean
+  public ReplyingKafkaTemplate<String, String, String> iastStringTemplate() {
+    return new ReplyingKafkaTemplate<>(iastStringProducer(), iastReplyStringContainer());
+  }
+
+  @Bean
+  public ReplyingKafkaTemplate<byte[], byte[], String> iastByteArrayTemplate() {
+    return new ReplyingKafkaTemplate<>(iastByteArrayProducer(), iastReplyByteArrayContainer());
+  }
+
+  @Bean
+  public ReplyingKafkaTemplate<ByteBuffer, ByteBuffer, String> iastByteBufferTemplate() {
+    return new ReplyingKafkaTemplate<>(iastByteBufferProducer(), iastReplyByteBufferContainer());
+  }
+
+  @Bean
+  public ReplyingKafkaTemplate<IastMessage, IastMessage, String> iastJsonTemplate() {
+    return new ReplyingKafkaTemplate<>(iastJsonProducer(), iastReplyJsonContainer());
+  }
+
+  @Bean
+  public KafkaTemplate<String, String> iastReplyStringTemplate() {
+    return new KafkaTemplate<>(iastReplyStringProducer());
+  }
+
+  @Bean
+  public KafkaTemplate<byte[], String> iastReplyByteArrayTemplate() {
+    return new KafkaTemplate<>(iastReplyByteArrayProducer());
+  }
+
+  @Bean
+  public KafkaTemplate<ByteBuffer, String> iastReplyByteBufferTemplate() {
+    return new KafkaTemplate<>(iastReplyByteBufferProducer());
+  }
+
+  @Bean
+  public KafkaTemplate<IastMessage, String> iastReplyJsonTemplate() {
+    return new KafkaTemplate<>(iastReplyJsonProducer());
+  }
+
+  private <K, V> DefaultKafkaProducerFactory<K, V> producerFor(
+      final Class<? extends Serializer<K>> keySerializer,
+      final Class<? extends Serializer<V>> valueSerializer) {
+    final Map<String, Object> configProps = new HashMap<>();
     configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
-    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, serializer);
-    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, serializer);
-    final DefaultKafkaProducerFactory<E, E> factory =
-        new DefaultKafkaProducerFactory<>(configProps);
-    return new KafkaTemplate<>(factory);
+    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+    return new DefaultKafkaProducerFactory<>(configProps);
+  }
+
+  private <K, V> DefaultKafkaConsumerFactory<K, V> consumerFor(
+      final String topic,
+      final Class<? extends Deserializer<K>> keyDeserializer,
+      final Class<? extends Deserializer<V>> valueDeserializer) {
+    final Map<String, Object> config = new HashMap<>();
+    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+    config.put(ConsumerConfig.GROUP_ID_CONFIG, topic); // one group per topic
+    config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
+    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
+    if (JsonDeserializer.class.isAssignableFrom(keyDeserializer)
+        || JsonDeserializer.class.isAssignableFrom(valueDeserializer)) {
+      config.put(JsonDeserializer.TRUSTED_PACKAGES, "datadog.*");
+    }
+    return new DefaultKafkaConsumerFactory<>(config);
   }
 
   private NewTopic newTopic(final String name) {
-    return TopicBuilder.name(name).partitions(1).replicas(1).compact().build();
+    return TopicBuilder.name(name).partitions(1).replicas(1).build();
+  }
+
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  private Class<? extends Deserializer<IastMessage>> jsonDeserializer() {
+    final Class<? extends Deserializer> type = JsonDeserializer.class;
+    return (Class<? extends Deserializer<IastMessage>>) type;
+  }
+
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  private Class<? extends Serializer<IastMessage>> jsonSerializer() {
+    final Class<? extends Serializer> type = JsonSerializer.class;
+    return (Class<? extends Serializer<IastMessage>>) type;
   }
 }
