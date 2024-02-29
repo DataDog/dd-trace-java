487a9e05835a4409ea650a6c702ad255bf85b754
Nikita Tkachenko <121111529+nikita-tkachenko-datadog@users.noreply.github.com>
2024-02-29 13:27:04 +0100

    Update JUnit 4 method name extraction logic to properly handle Kotlin test names (#6751)

---
+++ dd-java-agent/instrumentation/junit-4.10/src/test/kotlin/org/example/TestSucceedKotlin.kt
@@ -0,0 +1,12 @@
+package org.example
+
+import org.junit.Assert
+import org.junit.Test
+
+class TestSucceedKotlin {
+
+  @Test
+  fun `single document (without provider) deserialized from json`() {
+    Assert.assertTrue(true)
+  }
+}
---
+++ dd-java-agent/instrumentation/junit-4.10/src/test/kotlin/org/example/TestSucceedParameterizedKotlin.kt
@@ -0,0 +1,30 @@
+package org.example
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class TestSucceedParameterizedKotlin(
+  private val param1: ParamObject,
+  private val param2: String,
+  private val param3: Int
+) {
+
+  companion object {
+    @JvmStatic
+    @Parameterized.Parameters(name = "{1}")
+    fun data(): Collection<Array<Any>> = listOf(
+      arrayOf(ParamObject(), "str1", 0),
+      arrayOf(ParamObject(), "str2", 1)
+    )
+  }
+
+  @Test
+  fun `single document (without provider) deserialized from json`() {
+    assertTrue(true)
+  }
+
+  class ParamObject
+}
