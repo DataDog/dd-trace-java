c9082c35bb5fbf30310a45e2ba054f43f6cec017
Nikita Tkachenko <121111529+nikita-tkachenko-datadog@users.noreply.github.com>
2024-03-06 15:49:55 +0100

    Fix source path calculation for classes in default package (#6781)

--- dd-java-agent/agent-ci-visibility/src/main/java/datadog/trace/civisibility/source/index/PackageResolver.java
+++ dd-java-agent/agent-ci-visibility/src/main/java/datadog/trace/civisibility/source/index/PackageResolver.java
@@ -2,7 +2,10 @@ package datadog.trace.civisibility.source.index;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import javax.annotation.Nullable;
 
 public interface PackageResolver {
+  /** @return the package path or <code>null</code> if the file is in the default package */
+  @Nullable
   Path getPackage(Path sourceFile) throws IOException;
 }
