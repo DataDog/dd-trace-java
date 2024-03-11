aee5f6f8c9be22baec07aeb1fefa3b9ff86f61ef
Santiago M. Mola <santiago.mola@datadoghq.com>
2024-03-11 11:18:52 +0100

    Fix logged exception for dependency URIs representing directories (#6792)

--- telemetry/src/main/java/datadog/telemetry/dependency/DependencyResolver.java
+++ telemetry/src/main/java/datadog/telemetry/dependency/DependencyResolver.java
@@ -2,6 +2,7 @@ package datadog.telemetry.dependency;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.Collections;
@@ -12,41 +13,48 @@ import org.slf4j.LoggerFactory;
 public class DependencyResolver {
 
   private static final Logger log = LoggerFactory.getLogger(DependencyResolver.class);
-  private static final String JAR_SUFFIX = ".jar";
 
   public static List<Dependency> resolve(URI uri) {
-    final String scheme = uri.getScheme();
     try {
-      JarReader.Extracted metadata = null;
-      String path = null;
-      if ("file".equals(scheme)) {
-        File f;
-        if (uri.isOpaque()) {
-          f = new File(uri.getSchemeSpecificPart());
-        } else {
-          f = new File(uri);
-        }
-        path = f.getAbsolutePath();
-        metadata = JarReader.readJarFile(path);
-      } else if ("jar".equals(scheme) && uri.getSchemeSpecificPart().startsWith("file:")) {
-        path = uri.getSchemeSpecificPart().substring("file:".length());
-        metadata = JarReader.readNestedJarFile(path);
-      } else {
-        log.debug("unsupported dependency type: {}", uri);
-        return Collections.emptyList();
-      }
-      final List<Dependency> dependencies =
-          Dependency.fromMavenPom(metadata.jarName, metadata.pomProperties);
-      if (!dependencies.isEmpty()) {
-        return dependencies;
-      }
-      try (final InputStream is = new FileInputStream(path)) {
-        return Collections.singletonList(
-            Dependency.guessFallbackNoPom(metadata.manifest, metadata.jarName, is));
-      }
+      return internalResolve(uri);
     } catch (Throwable t) {
       log.debug("Failed to determine dependency for uri {}", uri, t);
     }
     return Collections.emptyList();
+  }
+
+  static List<Dependency> internalResolve(final URI uri) throws IOException {
+    final String scheme = uri.getScheme();
+    JarReader.Extracted metadata;
+    String path;
+    if ("file".equals(scheme)) {
+      File f;
+      if (uri.isOpaque()) {
+        f = new File(uri.getSchemeSpecificPart());
+      } else {
+        f = new File(uri);
+      }
+      path = f.getAbsolutePath();
+      metadata = JarReader.readJarFile(path);
+    } else if ("jar".equals(scheme) && uri.getSchemeSpecificPart().startsWith("file:")) {
+      path = uri.getSchemeSpecificPart().substring("file:".length());
+      metadata = JarReader.readNestedJarFile(path);
+    } else {
+      log.debug("unsupported dependency type: {}", uri);
+      return Collections.emptyList();
+    }
+    if (metadata.isDirectory) {
+      log.debug("Extracting dependencies from directories is not supported: {}", uri);
+      return Collections.emptyList();
+    }
+    final List<Dependency> dependencies =
+        Dependency.fromMavenPom(metadata.jarName, metadata.pomProperties);
+    if (!dependencies.isEmpty()) {
+      return dependencies;
+    }
+    try (final InputStream is = new FileInputStream(path)) {
+      return Collections.singletonList(
+          Dependency.guessFallbackNoPom(metadata.manifest, metadata.jarName, is));
+    }
   }
 }
