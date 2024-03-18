dd6e3510105f49d4c6224c5da14456a2157fd2c2
Baptiste Foy <baptiste.foy@datadoghq.com>
2024-03-18 12:50:35 -0400

    fix(oci_script): Properly escape variables

--- .gitlab/build_java_package_oci.sh
+++ .gitlab/build_java_package_oci.sh
@@ -14,8 +14,8 @@ cp ../workspace/dd-java-agent/build/libs/*.jar sources/dd-java-agent.jar
 cp auto_inject-java.version sources/version
 
 datadog-package create \
-    --version=$VERSION \
+    --version="$VERSION" \
     --package="datadog-apm-library-java" \
     --archive=true \
-    --archive-path datadog-apm-library-java-$VERSION.tar \
+    --archive-path="datadog-apm-library-java-$VERSION.tar" \
     ./sources
