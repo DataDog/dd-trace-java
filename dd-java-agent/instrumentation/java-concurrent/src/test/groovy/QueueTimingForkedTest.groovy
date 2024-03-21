302c754358dfb4bd31d04a793f99c929bdbd37e6
Richard Startin <richard.startin@datadoghq.com>
2024-03-21 06:07:02 +0000

    ensure queue timer is started for RunnableFuture types (#6828)

---
+++ dd-java-agent/instrumentation/java-concurrent/src/test/groovy/QueueTimingForkedTest.groovy
@@ -0,0 +1,59 @@
+import datadog.trace.agent.test.AgentTestRunner
+import datadog.trace.agent.test.TestProfilingContextIntegration
+import datadog.trace.bootstrap.instrumentation.jfr.InstrumentationBasedProfiling
+
+import java.util.concurrent.Executors
+
+import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
+
+class QueueTimingForkedTest extends AgentTestRunner {
+
+  @Override
+  protected void configurePreAgent() {
+    // required for enabling the unwrapping instrumentation to get the relevant non-carrier class names
+    injectSysConfig("dd.profiling.enabled", "true")
+    injectSysConfig("dd.profiling.experimental.queueing.time.enabled", "true")
+    InstrumentationBasedProfiling.enableInstrumentationBasedProfiling()
+    super.configurePreAgent()
+  }
+
+  def "test queue timing with submit"() {
+    setup:
+    def executor = Executors.newSingleThreadExecutor()
+
+    when:
+    runUnderTrace("parent", {
+      executor.submit(new TestRunnable()).get()
+    })
+
+    then:
+    verify()
+
+    cleanup:
+    executor.shutdown()
+    TEST_PROFILING_CONTEXT_INTEGRATION.closedTimings.clear()
+  }
+
+  void verify() {
+    assert TEST_PROFILING_CONTEXT_INTEGRATION.isBalanced()
+    assert !TEST_PROFILING_CONTEXT_INTEGRATION.closedTimings.isEmpty()
+    int numAsserts = 0
+    while (!TEST_PROFILING_CONTEXT_INTEGRATION.closedTimings.isEmpty()) {
+      def timing = TEST_PROFILING_CONTEXT_INTEGRATION.closedTimings.takeFirst() as TestProfilingContextIntegration.TestQueueTiming
+      if (!(timing.task as Class).simpleName.isEmpty()) {
+        assert timing != null
+        assert timing.task == TestRunnable
+        assert timing.scheduler != null
+        assert timing.origin == Thread.currentThread()
+        numAsserts++
+      }
+    }
+    assert numAsserts > 0
+  }
+
+
+  class TestRunnable implements Runnable {
+    @Override
+    void run() {}
+  }
+}
