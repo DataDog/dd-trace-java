25e9b889923baf54d7162f2e76f98a6dda4f8054
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2024-03-27 10:53:41 +0100

    Add measures for cpu and rss via PS in load tests

--- benchmark/utils/run-k6-load-test.sh
+++ benchmark/utils/run-k6-load-test.sh
@@ -4,9 +4,12 @@ set -eu
 url=$1
 output=$2
 command=$3
+ps_pid=0
 exit_code=0
 
 cleanup() {
+  # stop ps commands
+  kill "${ps_pid}" 2>/dev/null
   # run the exit command
   bash -c "${command}"
   exit $exit_code
@@ -20,6 +23,9 @@ while true; do
     break
   fi
 done
+
+"${UTILS_DIR}/cpu_usage_ps.sh" java "${output}/cpu_usage.log" &
+ps_pid=$!
 
 # run the k6 benchmark and store the result as JSON
 k6 run k6.js --out "json=${output}/k6_$(date +%s).json" &>>"${output}/k6.log"
