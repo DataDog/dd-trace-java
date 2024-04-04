f4c80e539e6075d78d7036cf01a6365f9fb4e59e
Piotr Wolski <piotr.wolski42@gmail.com>
2024-04-04 16:17:53 -0400

    add test coverage

---
+++ dd-trace-core/src/test/groovy/datadog/trace/core/datastreams/SchemaBuilderTest.groovy
@@ -0,0 +1,23 @@
+package datadog.trace.core.datastreams
+
+import datadog.trace.core.test.DDCoreSpecification
+
+class SchemaBuilderTest extends DDCoreSpecification {
+
+  def "schema is converted correctly to JSON"() {
+    given:
+    SchemaBuilder builder = new SchemaBuilder()
+
+    when:
+    builder.addProperty("person", "name", false, "string", "name of the person", null, null, null)
+    builder.addProperty("person", "phone_numbers", true, "string", null, null, null, null)
+    builder.addProperty("person", "person_name", false, "string", null, null, null, null)
+    builder.addProperty("person", "address", false, "object", null, "#/components/schemas/address", null, null)
+    builder.addProperty("address", "zip", false, "number", null, null, "int", null)
+    builder.addProperty("address", "street", false, "string", null, null, null, null)
+    String schema = builder.build()
+
+    then:
+    "{\"components\":{\"schemas\":{\"person\":{\"properties\":{\"name\":{\"description\":\"name of the person\",\"type\":\"string\"},\"phone_numbers\":{\"items\":{\"type\":\"string\"},\"type\":\"array\"},\"person_name\":{\"type\":\"string\"},\"address\":{\"\$ref\":\"#/components/schemas/address\",\"type\":\"object\"}},\"type\":\"object\"},\"address\":{\"properties\":{\"zip\":{\"format\":\"int\",\"type\":\"number\"},\"street\":{\"type\":\"string\"}},\"type\":\"object\"}}},\"openapi\":\"3.0.0\"}" == schema
+  }
+}
