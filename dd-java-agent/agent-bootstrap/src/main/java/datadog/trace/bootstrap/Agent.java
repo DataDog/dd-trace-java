db9a81c973879b1f7552c28b9620ec7ff25afb51
Kristofer <kristofer@andes.is>
2024-04-23 12:23:28 +0000

    date format for automatic parsing

--- dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/Agent.java
+++ dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/Agent.java
@@ -70,7 +70,7 @@ public class Agent {
   private static final String SIMPLE_LOGGER_DATE_TIME_FORMAT_PROPERTY =
       "datadog.slf4j.simpleLogger.dateTimeFormat";
   private static final String SIMPLE_LOGGER_DATE_TIME_FORMAT_DEFAULT =
-      "'[dd.trace 'yyyy-MM-dd HH:mm:ss:SSS Z']'";
+      "yyyy-MM-dd HH:mm:ss.SSSS";
   private static final String SIMPLE_LOGGER_DEFAULT_LOG_LEVEL_PROPERTY =
       "datadog.slf4j.simpleLogger.defaultLogLevel";
 
