3029736e8f09154422f6d0339d11578d1f6d21e3
luneo7 <luneo7@gmail.com>
2024-05-07 15:15:58 -0500

    Relocate JCTools

--- dd-java-agent/instrumentation/graal/native-image/src/main/java/datadog/trace/instrumentation/graal/nativeimage/Target_datadog_jctools_counters_FixedSizeStripedLongCounterFields.java
+++ dd-java-agent/instrumentation/graal/native-image/src/main/java/datadog/trace/instrumentation/graal/nativeimage/Target_datadog_jctools_counters_FixedSizeStripedLongCounterFields.java
@@ -4,8 +4,8 @@ import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.TargetClass;
 
-@TargetClass(className = "org.jctools.counters.FixedSizeStripedLongCounterFields")
-public final class Target_org_jctools_counters_FixedSizeStripedLongCounterFields {
+@TargetClass(className = "datadog.jctools.counters.FixedSizeStripedLongCounterFields")
+public final class Target_datadog_jctools_counters_FixedSizeStripedLongCounterFields {
   @Alias
   @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayBaseOffset, declClass = long[].class)
   public static long COUNTER_ARRAY_BASE;
