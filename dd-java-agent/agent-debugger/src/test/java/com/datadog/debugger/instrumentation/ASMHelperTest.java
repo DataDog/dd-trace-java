283052bf2987d64a51376e13e810d6343dff4382
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2024-05-10 11:30:17 +0200

    Fix AIOOBE in adjustLocalVarsBasedOnArgs
    ArrayIndexOutOfBoundsException was thrown because size of the args
    and local vars were not correctly took into account.
    with Long/Double type it actually takes 2 slots into the local var
    tables.
    Add a protection against index out of bounds but also adjust
    local var array for long or double.

--- dd-java-agent/agent-debugger/src/test/java/com/datadog/debugger/instrumentation/ASMHelperTest.java
+++ dd-java-agent/agent-debugger/src/test/java/com/datadog/debugger/instrumentation/ASMHelperTest.java
@@ -1,12 +1,22 @@
 package com.datadog.debugger.instrumentation;
 
+import static com.datadog.debugger.instrumentation.ASMHelper.createLocalVarNodes;
 import static com.datadog.debugger.instrumentation.ASMHelper.ensureSafeClassLoad;
+import static com.datadog.debugger.instrumentation.ASMHelper.sortLocalVariables;
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.LocalVariableNode;
 
 public class ASMHelperTest {
+
+  public static final LocalVariableNode THIS =
+      createLocalVar(null, Types.OBJECT_TYPE.getDescriptor(), 0);
 
   @Test
   public void ensureSafeLoadClass() {
@@ -28,5 +38,103 @@ public class ASMHelperTest {
         ensureSafeClassLoad(
             ASMHelperTest.class.getTypeName(), "", ASMHelperTest.class.getClassLoader());
     assertEquals(ASMHelperTest.class, clazz);
+  }
+
+  @Test
+  public void adjustLocalVarsBasedOnArgs_empty() {
+    LocalVariableNode[] emptyLocalVariables = new LocalVariableNode[0];
+    Type[] emptyArgs = new Type[0];
+    ASMHelper.adjustLocalVarsBasedOnArgs(
+        false, emptyLocalVariables, emptyArgs, Collections.emptyList());
+    ASMHelper.adjustLocalVarsBasedOnArgs(
+        true, emptyLocalVariables, emptyArgs, Collections.emptyList());
+  }
+
+  @Test
+  public void adjustLocalVarsBasedOnArgs() {
+    doAdjustLocalVarsBasedOnArgs(
+        true, asList(Type.INT_TYPE), asList(createLocalVar("a", "I", 0)), asList("a"));
+    doAdjustLocalVarsBasedOnArgs(
+        false, asList(Type.INT_TYPE), asList(THIS, createLocalVar("a", "I", 1)), asList("a"));
+    doAdjustLocalVarsBasedOnArgs(
+        true,
+        asList(Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE),
+        asList(
+            createLocalVar("c", "I", 2), createLocalVar("b", "I", 1), createLocalVar("a", "I", 0)),
+        asList("a", "b", "c"));
+    doAdjustLocalVarsBasedOnArgs(
+        false,
+        asList(Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE),
+        asList(
+            THIS,
+            createLocalVar("c", "I", 3),
+            createLocalVar("b", "I", 2),
+            createLocalVar("a", "I", 1)),
+        asList("a", "b", "c"));
+    doAdjustLocalVarsBasedOnArgs(
+        true,
+        asList(Type.LONG_TYPE, Type.LONG_TYPE, Type.LONG_TYPE),
+        asList(
+            createLocalVar("c", "J", 4), createLocalVar("b", "J", 2), createLocalVar("a", "J", 0)),
+        asList("a", "b", "c"));
+    doAdjustLocalVarsBasedOnArgs(
+        false,
+        asList(Type.LONG_TYPE, Type.LONG_TYPE, Type.LONG_TYPE),
+        asList(
+            THIS,
+            createLocalVar("c", "J", 5),
+            createLocalVar("b", "J", 3),
+            createLocalVar("a", "J", 1)),
+        asList("a", "b", "c"));
+    doAdjustLocalVarsBasedOnArgs(
+        true,
+        asList(Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE),
+        asList(
+            createLocalVar("c", "I", 6), createLocalVar("b", "I", 5), createLocalVar("a", "I", 4)),
+        asList("a", "b", "c"));
+    doAdjustLocalVarsBasedOnArgs(
+        false,
+        asList(Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE),
+        asList(
+            THIS,
+            createLocalVar("c", "I", 6),
+            createLocalVar("b", "I", 5),
+            createLocalVar("a", "I", 4)),
+        asList("a", "b", "c"));
+    doAdjustLocalVarsBasedOnArgs(
+        true,
+        asList(Type.LONG_TYPE, Type.LONG_TYPE, Type.LONG_TYPE),
+        asList(
+            createLocalVar("c", "J", 10), createLocalVar("b", "J", 8), createLocalVar("a", "J", 6)),
+        asList("a", "b", "c"));
+    doAdjustLocalVarsBasedOnArgs(
+        false,
+        asList(Type.LONG_TYPE, Type.LONG_TYPE, Type.LONG_TYPE),
+        asList(
+            THIS,
+            createLocalVar("c", "J", 10),
+            createLocalVar("b", "J", 8),
+            createLocalVar("a", "J", 6)),
+        asList("a", "b", "c"));
+  }
+
+  private void doAdjustLocalVarsBasedOnArgs(
+      boolean isStatic,
+      List<Type> argTypes,
+      List<LocalVariableNode> localVarNodes,
+      List<String> expectedLocalVarNames) {
+    Type[] args = argTypes.toArray(new Type[0]);
+    List<LocalVariableNode> sortedLocalVariables = sortLocalVariables(localVarNodes);
+    LocalVariableNode[] localVars = createLocalVarNodes(sortedLocalVariables);
+    ASMHelper.adjustLocalVarsBasedOnArgs(isStatic, localVars, args, sortedLocalVariables);
+    int idx = isStatic ? 0 : 1;
+    for (String name : expectedLocalVarNames) {
+      assertEquals(name, localVars[idx].name);
+      idx += Type.getType(localVars[idx].desc).getSize();
+    }
+  }
+
+  private static LocalVariableNode createLocalVar(String name, String desc, int index) {
+    return new LocalVariableNode(name, desc, null, null, null, index);
   }
 }
