e1f24782e1b10a37bdc5d1981c2daeee03f60557
Piotr Wolski <piotr.wolski42@gmail.com>
2024-05-20 10:56:30 -0400

    data streams: Use backend parameters for histograms

--- dd-trace-core/src/main/java/datadog/trace/core/histogram/Histograms.java
+++ dd-trace-core/src/main/java/datadog/trace/core/histogram/Histograms.java
@@ -3,15 +3,23 @@ package datadog.trace.core.histogram;
 import com.datadoghq.sketch.ddsketch.DDSketch;
 import com.datadoghq.sketch.ddsketch.DDSketches;
 import com.datadoghq.sketch.ddsketch.mapping.BitwiseLinearlyInterpolatedMapping;
+import com.datadoghq.sketch.ddsketch.mapping.LogarithmicMapping;
 import com.datadoghq.sketch.ddsketch.store.CollapsingLowestDenseStore;
 
 public final class Histograms {
 
   private static final BitwiseLinearlyInterpolatedMapping INDEX_MAPPING =
       new BitwiseLinearlyInterpolatedMapping(1.0 / 128.0);
+  private static final LogarithmicMapping LOG_INDEX_MAPPING =
+      new LogarithmicMapping(1.015625, 1.8761281912861705);
 
   public static Histogram newHistogram() {
     DDSketch sketch = new DDSketch(INDEX_MAPPING, () -> new CollapsingLowestDenseStore(1024));
+    return new Histogram(sketch);
+  }
+
+  public static Histogram newLogHistogram() {
+    DDSketch sketch = new DDSketch(LOG_INDEX_MAPPING, () -> new CollapsingLowestDenseStore(1024));
     return new Histogram(sketch);
   }
 
