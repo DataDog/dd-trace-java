c4d23e30873ae270b18d63fb4ea5672594c993d3
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2024-05-22 11:38:03 +0200

    Fix short circuiting of boolean expressions
    Boolean expressions with AND (&&) or OR (||) should short circuit
    as expected for any languages. It helps to support correctly
    expressions like
    `isDefined(@exception) && contains(@exception.detailMessage, 'closed')`
    We are just delegating to the operator evaluation the evaluation of
    the left and right instead of evaluating operands before the operator.

--- dd-java-agent/agent-debugger/debugger-el/src/test/java/com/datadog/debugger/el/expressions/BinaryExpressionTest.java
+++ dd-java-agent/agent-debugger/debugger-el/src/test/java/com/datadog/debugger/el/expressions/BinaryExpressionTest.java
@@ -3,8 +3,10 @@ package com.datadog.debugger.el.expressions;
 import static com.datadog.debugger.el.PrettyPrintVisitor.print;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.datadog.debugger.el.RefResolverHelper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BinaryExpressionTest {
@@ -22,5 +24,27 @@ class BinaryExpressionTest {
         new BinaryExpression(BooleanExpression.TRUE, null, BinaryOperator.AND);
     assertFalse(expression.evaluate(RefResolverHelper.createResolver(this)));
     assertEquals("true && false", print(expression));
+  }
+
+  @Test
+  void testShortCircuitAnd() {
+    BinaryExpression expression =
+        new BinaryExpression(
+            BooleanExpression.FALSE,
+            valueRefResolver -> Assertions.fail("should not reach"),
+            BinaryOperator.AND);
+    assertFalse(expression.evaluate(RefResolverHelper.createResolver(this)));
+    assertEquals("false && null", print(expression));
+  }
+
+  @Test
+  void testShortCircuitOr() {
+    BinaryExpression expression =
+        new BinaryExpression(
+            BooleanExpression.TRUE,
+            valueRefResolver -> Assertions.fail("should not reach"),
+            BinaryOperator.OR);
+    assertTrue(expression.evaluate(RefResolverHelper.createResolver(this)));
+    assertEquals("true || null", print(expression));
   }
 }
