bf9fd09c0296956cea223908d27e8d77b0cb01fe
Santiago M. Mola <santiago.mola@datadoghq.com>
2024-06-03 17:14:48 +0200

    Fix IP denylist parsing when expiration date is long in the future (#7097)

---
+++ dd-java-agent/appsec/src/test/groovy/com/datadog/appsec/config/AppSecDataDeserializerSpecification.groovy
@@ -0,0 +1,63 @@
+package com.datadog.appsec.config
+
+import spock.lang.Specification
+
+import java.nio.charset.StandardCharsets
+
+class AppSecDataDeserializerSpecification extends Specification {
+
+  void "deserialize IP denylist"() {
+    given:
+    final deser = AppSecDataDeserializer.INSTANCE
+    final input = """
+{
+  "rules_data": [
+    {
+      "data": [
+        {
+          "expiration": 0,
+          "value": "1.1.1.1"
+        },
+        {
+          "expiration": 1717083300,
+          "value": "2.2.2.2"
+        },
+        {
+          "expiration": 9223372036854775807,
+          "value": "3.3.3.3"
+        }
+      ],
+      "id": "blocked_ips",
+      "type": "ip_with_expiration"
+    }
+  ]
+}
+    """
+
+    when:
+    def result = deser.deserialize(input.getBytes(StandardCharsets.UTF_8))
+
+    then:
+    result != null
+    result == [
+      [
+        data: [
+          [
+            expiration: 0.0,
+            value     : "1.1.1.1"
+          ],
+          [
+            expiration: 1717083300.0,
+            value     : "2.2.2.2"
+          ],
+          [
+            expiration: 9223372036854775807.0,
+            value     : "3.3.3.3"
+          ]
+        ],
+        id: "blocked_ips",
+        type: "ip_with_expiration"
+      ]
+    ]
+  }
+}
