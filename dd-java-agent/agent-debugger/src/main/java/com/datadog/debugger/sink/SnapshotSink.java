14cf31b484b85ce8aaa17c6b07f25a0bfcf1f925
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2024-06-03 22:17:28 +0200

    Move snapshot UUID generation at serialization
    UUID generation is not cheap, and therefore should be performed only
    when the snapshot is about to be sent.
    Moving UUID generation at serialization time makes it out of the
    critical path.

--- dd-java-agent/agent-debugger/src/main/java/com/datadog/debugger/sink/SnapshotSink.java
+++ dd-java-agent/agent-debugger/src/main/java/com/datadog/debugger/sink/SnapshotSink.java
@@ -63,6 +63,7 @@ public class SnapshotSink {
   }
 
   String serializeSnapshot(String serviceName, Snapshot snapshot) {
+    snapshot.getId(); // Ensure id is generated
     String str = DebuggerAgent.getSnapshotSerializer().serializeSnapshot(serviceName, snapshot);
     String prunedStr = SnapshotPruner.prune(str, MAX_SNAPSHOT_SIZE, 4);
     if (prunedStr.length() != str.length()) {
