9365715439bb90a1287c1bd0b635b2f8cd6dc530
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2024-06-11 13:45:33 +0200

    Add smoke test to validate propagation in different languages

---
+++ dd-smoke-tests/iast-propagation/src/main/kotlin/KotlinPropagation.kt
@@ -0,0 +1,10 @@
+import java.util.function.Supplier
+
+class KotlinPropagation : Supplier<List<String>> {
+
+  override fun get(): List<String> = listOf("plus", "interpolation")
+
+  fun plus(param: String): String = param + param
+
+  fun interpolation(param: String): String = "Interpolation $param"
+}
