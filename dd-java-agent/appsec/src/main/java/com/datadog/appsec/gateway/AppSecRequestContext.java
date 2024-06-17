49c5b75a40d7f30a50aca9163be0c6ef9e130cb7
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2024-06-17 08:03:16 +0200

    fix merge

--- dd-java-agent/appsec/src/main/java/com/datadog/appsec/gateway/AppSecRequestContext.java
+++ dd-java-agent/appsec/src/main/java/com/datadog/appsec/gateway/AppSecRequestContext.java
@@ -6,6 +6,7 @@ import com.datadog.appsec.report.AppSecEvent;
 import com.datadog.appsec.stack_trace.StackTraceCollection;
 import com.datadog.appsec.stack_trace.StackTraceEvent;
 import com.datadog.appsec.util.StandardizedLogging;
+import datadog.trace.api.Config;
 import datadog.trace.api.http.StoredBodySupplier;
 import datadog.trace.api.internal.TraceSegment;
 import io.sqreen.powerwaf.Additive;
@@ -417,14 +418,11 @@ public class AppSecRequestContext implements DataBundle, Closeable {
     for (AppSecEvent event : appSecEvents) {
       StandardizedLogging.attackDetected(log, event);
     }
-    synchronized (this) {
-      if (this.collectedEvents == null) {
-        this.collectedEvents = new ArrayList<>();
-      }
-      try {
-        this.collectedEvents.addAll(events);
-      } catch (UnsupportedOperationException e) {
-        throw new IllegalStateException("Events cannot be added anymore");
+    if (this.appSecEvents == null) {
+      synchronized (this) {
+        if (this.appSecEvents == null) {
+          this.appSecEvents = new ConcurrentLinkedQueue<>();
+        }
       }
     }
     this.appSecEvents.addAll(appSecEvents);
