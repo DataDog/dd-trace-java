174ea2816af004425ccf96369877a5646a7f128f
Piotr WOLSKI <piotr.wolski42@gmail.com>
2024-06-20 18:28:55 -0400

    Fix Protobuf schema sampling logic (#7197)
    * fix schema sampling logic
    * Capture serialization in DynamicMessages for Protobuf

--- dd-trace-core/src/test/groovy/datadog/trace/core/datastreams/SchemaSamplerTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/core/datastreams/SchemaSamplerTest.groovy
@@ -13,24 +13,18 @@ class SchemaSamplerTest  extends DDCoreSpecification {
     boolean canSample1 = sampler.canSample(currentTimeMillis)
     int weight1 = sampler.trySample(currentTimeMillis)
     boolean canSample2= sampler.canSample(currentTimeMillis + 1000)
-    int weight2 = sampler.trySample(currentTimeMillis + 1000)
     boolean canSample3 = sampler.canSample(currentTimeMillis + 2000)
-    int weight3 = sampler.trySample(currentTimeMillis + 2000)
     boolean canSample4 = sampler.canSample(currentTimeMillis + 30000)
     int weight4 = sampler.trySample(currentTimeMillis + 30000)
     boolean canSample5 = sampler.canSample(currentTimeMillis + 30001)
-    int weight5 = sampler.trySample(currentTimeMillis + 30001)
 
     then:
     canSample1
     weight1 == 1
     !canSample2
-    weight2 == 0
     !canSample3
-    weight3 == 0
     canSample4
     weight4 == 3
     !canSample5
-    weight5 == 0
   }
 }
