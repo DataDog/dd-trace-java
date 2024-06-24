ce271b630c57241333aa9e16a5f08b47e09251d3
Andrea Marziali <andrea.marziali@datadoghq.com>
2024-06-24 08:43:02 +0200

    [WIP]Upgrade JMX Mbean server when using custom one

---
+++ dd-java-agent/instrumentation/java-lang/src/main/java/datadog/trace/instrumentation/java/lang/management/CustomMBeanBuilderInstrumentation.java
@@ -0,0 +1,49 @@
+package datadog.trace.instrumentation.java.lang.management;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.returns;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.InstrumenterModule;
+import datadog.trace.bootstrap.InstanceStore;
+import javax.management.MBeanServer;
+import net.bytebuddy.asm.Advice;
+
+@AutoService(InstrumenterModule.class)
+public class CustomMBeanBuilderInstrumentation extends InstrumenterModule.Tracing
+    implements Instrumenter.ForConfiguredType {
+
+  public CustomMBeanBuilderInstrumentation() {
+    super("java-lang-management");
+  }
+
+  @Override
+  public boolean isEnabled() {
+    if (!super.isEnabled()) {
+      return false;
+    }
+    String customBuilder = System.getProperty("javax.management.builder.initial");
+    return customBuilder != null && !customBuilder.isEmpty();
+  }
+
+  @Override
+  public void methodAdvice(MethodTransformer transformer) {
+    transformer.applyAdvice(
+        isMethod().and(named("newMBeanServer")).and(returns(named("javax.management.MBeanServer"))),
+        this.getClass().getName() + "$StoreMBeanServerAdvice");
+  }
+
+  @Override
+  public String configuredMatchingType() {
+    return System.getProperty("javax.management.builder.initial");
+  }
+
+  public static class StoreMBeanServerAdvice {
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void afterCreation(@Advice.Return final MBeanServer mBeanServer) {
+      InstanceStore.of(MBeanServer.class).putIfAbsent("", mBeanServer);
+    }
+  }
+}
