a5570c9651e3526af8e2fcd7029c1260efd859c1
ValentinZakharov <valentin.zakharov1@gmail.com>
2024-07-01 18:18:01 +0300

    Update dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/InstrumentationLogger.java
    Co-authored-by: Santiago M. Mola <santiago.mola@datadoghq.com>

--- dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/InstrumentationLogger.java
+++ dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/InstrumentationLogger.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 public class InstrumentationLogger {
   public static void debug(
       String instrumentation, final Class<?> target, final Throwable throwable) {
-    // log exception
     LoggerFactory.getLogger(instrumentation)
         .debug("Failed to handle exception in instrumentation for " + target, throwable);
   }
