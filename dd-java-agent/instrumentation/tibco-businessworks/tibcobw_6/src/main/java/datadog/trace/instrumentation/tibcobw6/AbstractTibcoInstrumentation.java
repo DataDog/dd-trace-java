d70f30ad96e95464d8199aae7d5e4419a1a2876a
Andrea Marziali <andrea.marziali@datadoghq.com>
2024-07-02 16:35:40 +0200

    fix contextstore access

--- dd-java-agent/instrumentation/tibco-businessworks/tibcobw_6/src/main/java/datadog/trace/instrumentation/tibcobw6/AbstractTibcoInstrumentation.java
+++ dd-java-agent/instrumentation/tibco-businessworks/tibcobw_6/src/main/java/datadog/trace/instrumentation/tibcobw6/AbstractTibcoInstrumentation.java
@@ -2,7 +2,7 @@ package datadog.trace.instrumentation.tibcobw6;
 
 import datadog.trace.agent.tooling.InstrumenterModule;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractTibcoInstrumentation extends InstrumenterModule.Tracing {
@@ -17,7 +17,11 @@ public abstract class AbstractTibcoInstrumentation extends InstrumenterModule.Tr
 
   @Override
   public Map<String, String> contextStore() {
-    return Collections.singletonMap("com.tibco.pvm.api.PmWorkUnit", AgentSpan.class.getName());
+    Map<String, String> stores = new HashMap<>();
+    stores.put("com.tibco.pvm.api.PmWorkUnit", AgentSpan.class.getName());
+    stores.put(
+        "com.tibco.bw.jms.shared.api.receive.JMSMessageCallBackHandler", String.class.getName());
+    return stores;
   }
 
   @Override
