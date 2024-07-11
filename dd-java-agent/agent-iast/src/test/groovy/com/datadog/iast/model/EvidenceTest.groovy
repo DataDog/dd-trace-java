5de479f3ccc57598b142789e40a841a277220f5f
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2024-07-11 17:45:35 +0200

    Create new ranges for vulns to prevent GC issues

--- dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/model/EvidenceTest.groovy
+++ dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/model/EvidenceTest.groovy
@@ -1,6 +1,14 @@
 package com.datadog.iast.model
 
+import com.datadog.iast.model.json.VulnerabilityEncoding
+import datadog.trace.api.iast.SourceTypes
 import datadog.trace.test.util.DDSpecification
+import groovy.json.JsonSlurper
+
+import java.lang.ref.Reference
+import java.lang.ref.WeakReference
+
+import static datadog.trace.api.iast.VulnerabilityMarks.NOT_MARKED
 
 class EvidenceTest extends DDSpecification {
 
@@ -26,5 +34,37 @@ class EvidenceTest extends DDSpecification {
 
     then:
     override
+  }
+
+  void 'test that evidences are consolidated preventing the GC from clearing data'() {
+    given:
+    final String name = "Hello world!"
+    final Reference<?> ref = new WeakReference<>(name)
+
+    when:
+    final source = new Source(SourceTypes.REQUEST_PARAMETER_NAME, ref, "a random value")
+    final batch = vulnBatchForSource(source)
+
+    then:
+    source.getName() == name
+    final parsedBefore = new JsonSlurper().parseText(VulnerabilityEncoding.toJson(batch))
+    parsedBefore["sources"][0]["name"] == name
+
+    when:
+    ref.clear()
+
+    then:
+    source.getName() == Source.GARBAGE_COLLECTED_REF
+    final parsedAfter = new JsonSlurper().parseText(VulnerabilityEncoding.toJson(batch))
+    parsedAfter["sources"][0]["name"] == name
+  }
+
+  private static VulnerabilityBatch vulnBatchForSource(final Source source) {
+    final location = Location.forClassAndMethodAndLine(EvidenceTest.name, 'vulnBatchForSource', 69)
+    final evidence = new Evidence("test", [new Range(0, 4, source, NOT_MARKED)] as Range[])
+    final vuln = new Vulnerability(VulnerabilityType.INSECURE_COOKIE, location, evidence)
+    final batch = new VulnerabilityBatch()
+    batch.add(vuln)
+    return batch
   }
 }
