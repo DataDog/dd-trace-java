ce8069a8fd891b4d1111f9f1a37504d19fd57e04
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2024-07-19 07:57:51 +0200

    Update dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/StatementInstrumentation.java
    Co-authored-by: Raphaël Vandon <raphael.vandon@datadoghq.com>

--- dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/StatementInstrumentation.java
+++ dd-java-agent/instrumentation/jdbc/src/main/java/datadog/trace/instrumentation/jdbc/StatementInstrumentation.java
@@ -93,9 +93,10 @@ public final class StatementInstrumentation extends InstrumenterModule.Tracing
         final boolean isSqlServer = DECORATE.isSqlServer(dbInfo);
 
         if (isSqlServer && INJECT_COMMENT && injectTraceContext) {
+          // The span ID is pre-determined so that we can reference it when setting the context
           final long spanID = DECORATE.setContextInfo(connection, dbInfo);
+          // we then force that pre-determined span ID for the span covering the actual query
           span = AgentTracer.get().buildSpan(DATABASE_QUERY).withSpanId(spanID).start();
-
         } else {
           span = startSpan(DATABASE_QUERY);
         }
