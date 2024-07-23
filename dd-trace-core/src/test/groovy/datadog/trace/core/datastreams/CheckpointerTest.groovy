966492f9e49b8fbb19865743f9dbfa5ffd488108
Bruce Bujon <bruce.bujon@datadoghq.com>
2024-07-23 11:41:05 +0200

    feat(dsm): Add checkpointer test

---
+++ dd-trace-core/src/test/groovy/datadog/trace/core/datastreams/CheckpointerTest.groovy
@@ -0,0 +1,62 @@
+package datadog.trace.core.datastreams
+
+
+import datadog.trace.api.experimental.DataStreamsContextCarrier
+import datadog.trace.bootstrap.instrumentation.api.AgentTracer
+import datadog.trace.core.test.DDCoreSpecification
+
+import static datadog.trace.api.config.GeneralConfig.DATA_STREAMS_ENABLED
+
+class CheckpointerTest extends DDCoreSpecification {
+  void 'test setting produce checkpoint'() {
+    setup:
+    // Enable DSM
+    injectSysConfig(DATA_STREAMS_ENABLED, 'true')
+    // Create a test tracer
+    def tracer = tracerBuilder().build()
+    AgentTracer.forceRegister(tracer)
+    // Get the test checkpointer
+    def checkpointer = tracer.getDataStreamsCheckpointer()
+    // Declare the carrier to test injected data
+    def carrier = Mock(DataStreamsContextCarrier)
+
+    when:
+    // Start and activate a span
+    def span = tracer.buildSpan('test', 'dsm-checkpoint').start()
+    def scope = tracer.activateSpan(span)
+    // Trigger produce checkpoint
+    checkpointer.setProduceCheckpoint('kafka', 'testTopic', carrier)
+    // Clean up span
+    scope.close()
+    span.finish()
+
+    then:
+    1 * carrier.set('dd-pathway-ctx-base64', _)
+  }
+
+  void 'test setting consume checkpoint'() {
+    setup:
+    // Enable DSM
+    injectSysConfig(DATA_STREAMS_ENABLED, 'true')
+    // Create a test tracer
+    def tracer = tracerBuilder().build()
+    AgentTracer.forceRegister(tracer)
+    // Get the test checkpointer
+    def checkpointer = tracer.getDataStreamsCheckpointer()
+    // Declare the carrier to test injected data
+    def carrier = Mock(DataStreamsContextCarrier)
+
+    when:
+    // Start and activate a span
+    def span = tracer.buildSpan('test', 'dsm-checkpoint').start()
+    def scope = tracer.activateSpan(span)
+    // Trigger consume checkpoint
+    checkpointer.setProduceCheckpoint('kafka', 'testTopic', carrier)
+    // Clean up span
+    scope.close()
+    span.finish()
+
+    then:
+    1 * carrier.set('dd-pathway-ctx-base64', _)
+  }
+}
