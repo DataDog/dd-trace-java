70e4261cc54d9cf4ea82781f001979183b8a7126
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2024-08-02 12:14:09 +0200

    Extract rasp NetworkConnection logic to a module to be able to reuse it in different advices

--- dd-java-agent/instrumentation/appsec-utils/src/main/java/datadog/trace/instrumentation/appsec/rasp/modules/NetworkConnectionModule.java
+++ dd-java-agent/instrumentation/appsec-utils/src/main/java/datadog/trace/instrumentation/appsec/rasp/modules/NetworkConnectionModule.java
@@ -1,9 +1,8 @@
-package datadog.trace.instrumentation.appsec.rasp.advices;
+package datadog.trace.instrumentation.appsec.rasp.modules;
 
 import static datadog.trace.api.gateway.Events.EVENTS;
 
 import datadog.appsec.api.blocking.BlockingException;
-import datadog.trace.api.Config;
 import datadog.trace.api.gateway.BlockResponseFunction;
 import datadog.trace.api.gateway.Flow;
 import datadog.trace.api.gateway.RequestContext;
@@ -11,23 +10,20 @@ import datadog.trace.api.gateway.RequestContextSlot;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
 import datadog.trace.instrumentation.appsec.utils.InstrumentationLogger;
-import java.net.URL;
 import java.util.function.BiFunction;
-import net.bytebuddy.asm.Advice;
 
-public class NetworkConnectionRaspAdvice {
+public class NetworkConnectionModule {
 
-  @Advice.OnMethodExit()
-  public static void onExit(
-      @Advice.Argument(0) final Object input, @Advice.This final Object self) {
+  public static final NetworkConnectionModule INSTANCE = new NetworkConnectionModule();
 
-    if (!Config.get().isAppSecRaspEnabled()) {
+  private NetworkConnectionModule() {
+    // prevent instantiation
+  }
+
+  public void onNetworkConnection(final Object nc) {
+    if (nc == null) {
       return;
     }
-    if (input == null || !(input instanceof URL)) {
-      return;
-    }
-
     try {
       final BiFunction<RequestContext, String, Flow<Void>> networkConnectionCallback =
           AgentTracer.get()
@@ -48,7 +44,7 @@ public class NetworkConnectionRaspAdvice {
         return;
       }
 
-      Flow<Void> flow = networkConnectionCallback.apply(ctx, input.toString());
+      Flow<Void> flow = networkConnectionCallback.apply(ctx, nc.toString());
       Flow.Action action = flow.getAction();
       if (action instanceof Flow.Action.RequestBlockingAction) {
         BlockResponseFunction brf = ctx.getBlockResponseFunction();
@@ -67,7 +63,8 @@ public class NetworkConnectionRaspAdvice {
       throw e;
     } catch (final Throwable e) {
       // suppress anything else
-      InstrumentationLogger.debug("Exception during SSRF rasp advice", self.getClass(), e);
+      InstrumentationLogger.debug(
+          "Exception during SSRF rasp advice", NetworkConnectionModule.class, e);
     }
   }
 }
--- dd-java-agent/instrumentation/jackson-core/jackson-core-1/src/main/java/datadog/trace/instrumentation/jackson/codehouse/core/RaspJson1FactoryInstrumentation.java
+++ dd-java-agent/instrumentation/jackson-core/jackson-core-1/src/main/java/datadog/trace/instrumentation/jackson/codehouse/core/RaspJson1FactoryInstrumentation.java
@@ -7,10 +7,13 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
+import datadog.trace.api.Config;
+import datadog.trace.instrumentation.appsec.rasp.modules.NetworkConnectionModule;
 import datadog.trace.instrumentation.appsec.utils.InstrumentationLogger;
 import java.io.InputStream;
 import java.io.Reader;
 import java.net.URL;
+import net.bytebuddy.asm.Advice;
 
 @AutoService(InstrumenterModule.class)
 public class RaspJson1FactoryInstrumentation extends InstrumenterModule.AppSec
@@ -22,7 +25,9 @@ public class RaspJson1FactoryInstrumentation extends InstrumenterModule.AppSec
 
   @Override
   public String[] helperClassNames() {
-    return new String[] {InstrumentationLogger.class.getName()};
+    return new String[] {
+      InstrumentationLogger.class.getName(), NetworkConnectionModule.class.getName()
+    };
   }
 
   @Override
@@ -36,11 +41,26 @@ public class RaspJson1FactoryInstrumentation extends InstrumenterModule.AppSec
                     .or(takesArguments(Reader.class))
                     .or(takesArguments(URL.class))
                     .or(takesArguments(byte[].class))),
-        "datadog.trace.instrumentation.appsec.rasp.advices.NetworkConnectionRaspAdvice");
+        RaspJson1FactoryInstrumentation.class.getName() + "$NetworkConnectionRaspAdvice");
   }
 
   @Override
   public String instrumentedType() {
     return "org.codehaus.jackson.JsonFactory";
+  }
+
+  public static class NetworkConnectionRaspAdvice {
+
+    @Advice.OnMethodExit()
+    public static void onExit(@Advice.Argument(0) final Object input) {
+
+      if (!Config.get().isAppSecRaspEnabled()) {
+        return;
+      }
+      if (input == null || !(input instanceof URL)) {
+        return;
+      }
+      NetworkConnectionModule.INSTANCE.onNetworkConnection(input);
+    }
   }
 }
