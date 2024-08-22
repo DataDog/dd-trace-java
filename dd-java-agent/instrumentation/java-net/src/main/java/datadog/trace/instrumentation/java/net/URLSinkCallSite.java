e15fc47d0a5f1e2cec14e407ca2e76c01665f789
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2024-08-22 10:48:59 +0200

    fix test

--- dd-java-agent/instrumentation/java-net/src/main/java/datadog/trace/instrumentation/java/net/URLSinkCallSite.java
+++ dd-java-agent/instrumentation/java-net/src/main/java/datadog/trace/instrumentation/java/net/URLSinkCallSite.java
@@ -1,18 +1,32 @@
 package datadog.trace.instrumentation.java.net;
 
+import static datadog.trace.api.gateway.Events.EVENTS;
+
+import datadog.appsec.api.blocking.BlockingException;
 import datadog.trace.agent.tooling.csi.CallSite;
+import datadog.trace.api.Config;
 import datadog.trace.api.appsec.RaspCallSites;
+import datadog.trace.api.gateway.BlockResponseFunction;
+import datadog.trace.api.gateway.Flow;
+import datadog.trace.api.gateway.RequestContext;
+import datadog.trace.api.gateway.RequestContextSlot;
 import datadog.trace.api.iast.IastCallSites;
 import datadog.trace.api.iast.InstrumentationBridge;
 import datadog.trace.api.iast.Sink;
 import datadog.trace.api.iast.VulnerabilityTypes;
 import datadog.trace.api.iast.sink.SsrfModule;
-import datadog.trace.instrumentation.appsec.rasp.modules.NetworkConnectionModule;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
 import java.net.URL;
+import java.util.function.BiFunction;
 import javax.annotation.Nonnull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @CallSite(spi = {IastCallSites.class, RaspCallSites.class})
 public class URLSinkCallSite {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(URLSinkCallSite.class);
 
   @Sink(VulnerabilityTypes.SSRF)
   @CallSite.Before("java.net.URLConnection java.net.URL.openConnection()")
@@ -40,6 +54,47 @@ public class URLSinkCallSite {
   }
 
   private static void raspCallback(@Nonnull final URL url) {
-    NetworkConnectionModule.INSTANCE.onNetworkConnection(url.toString());
+    if (!Config.get().isAppSecRaspEnabled()) {
+      return;
+    }
+
+    try {
+      final BiFunction<RequestContext, String, Flow<Void>> networkConnectionCallback =
+          AgentTracer.get()
+              .getCallbackProvider(RequestContextSlot.APPSEC)
+              .getCallback(EVENTS.networkConnection());
+      if (networkConnectionCallback == null) {
+        return;
+      }
+
+      final AgentSpan span = AgentTracer.get().activeSpan();
+      if (span == null) {
+        return;
+      }
+
+      final RequestContext ctx = span.getRequestContext();
+      if (ctx == null) {
+        return;
+      }
+
+      Flow<Void> flow = networkConnectionCallback.apply(ctx, url.toString());
+      Flow.Action action = flow.getAction();
+      if (action instanceof Flow.Action.RequestBlockingAction) {
+        BlockResponseFunction brf = ctx.getBlockResponseFunction();
+        if (brf != null) {
+          Flow.Action.RequestBlockingAction rba = (Flow.Action.RequestBlockingAction) action;
+          brf.tryCommitBlockingResponse(
+              ctx.getTraceSegment(),
+              rba.getStatusCode(),
+              rba.getBlockingContentType(),
+              rba.getExtraHeaders());
+        }
+        throw new BlockingException("Blocked request (for SSRF attempt)");
+      }
+    } catch (final BlockingException e) {
+      throw e;
+    } catch (final Throwable e) {
+      LOGGER.debug("Exception during SSRF rasp callback", e);
+    }
   }
 }
