8e4c6dd1b68f3bcbe51c5e1d56c7805627f43ac8
Andrea Marziali <andrea.marziali@datadoghq.com>
2024-08-22 10:20:46 +0200

    Apache httpclient: do not copy all request headers on redirect

--- dd-java-agent/instrumentation/apache-httpasyncclient-4/src/main/java/datadog/trace/instrumentation/apachehttpasyncclient/ApacheHttpClientRedirectInstrumentation.java
+++ dd-java-agent/instrumentation/apache-httpasyncclient-4/src/main/java/datadog/trace/instrumentation/apachehttpasyncclient/ApacheHttpClientRedirectInstrumentation.java
@@ -8,6 +8,8 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
+import datadog.trace.util.PropagationUtils;
+import java.util.Collection;
 import java.util.Locale;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
@@ -15,6 +17,8 @@ import net.bytebuddy.implementation.bytecode.assign.Assigner;
 import net.bytebuddy.matcher.ElementMatcher;
 import org.apache.http.Header;
 import org.apache.http.HttpRequest;
+import org.apache.http.client.methods.HttpRequestWrapper;
+import org.apache.http.protocol.HttpContext;
 
 /**
  * Early versions don't copy headers over on redirect. This instrumentation copies our headers over
@@ -26,7 +30,7 @@ public class ApacheHttpClientRedirectInstrumentation extends InstrumenterModule.
     implements Instrumenter.ForTypeHierarchy {
 
   public ApacheHttpClientRedirectInstrumentation() {
-    super("httpasyncclient", "apache-httpasyncclient");
+    super("httpasyncclient", "apache-httpasyncclient", "httpclient-redirect");
   }
 
   @Override
@@ -44,32 +48,41 @@ public class ApacheHttpClientRedirectInstrumentation extends InstrumenterModule.
     transformer.applyAdvice(
         isMethod()
             .and(named("getRedirect"))
-            .and(takesArgument(0, named("org.apache.http.HttpRequest"))),
+            .and(takesArgument(2, named("org.apache.http.protocol.HttpContext"))),
         ApacheHttpClientRedirectInstrumentation.class.getName() + "$ClientRedirectAdvice");
   }
 
   public static class ClientRedirectAdvice {
     @Advice.OnMethodExit(suppress = Throwable.class)
     private static void onAfterExecute(
-        @Advice.Argument(value = 0) final HttpRequest original,
+        @Advice.Argument(value = 2) final HttpContext context,
         @Advice.Return(typing = Assigner.Typing.DYNAMIC) final HttpRequest redirect) {
       if (redirect == null) {
         return;
       }
+      Object originalRequest = context.getAttribute("http.request");
+      if (!(originalRequest instanceof HttpRequest)) {
+        return;
+      }
+      HttpRequest original = (HttpRequest) originalRequest;
+
       // Apache HttpClient 4.0.1+ copies headers from original to redirect only
       // if redirect headers are empty. Because we add headers
       // "x-datadog-" and "x-b3-" to redirect: it means redirect headers never
       // will be empty. So in case if not-instrumented redirect had no headers,
       // we just copy all not set headers from original to redirect (doing same
       // thing as apache httpclient does).
-      if (!redirect.headerIterator().hasNext()) {
+      if (!redirect.headerIterator().hasNext() && original instanceof HttpRequestWrapper) {
         // redirect didn't have other headers besides tracing, so we need to do copy
         // (same work as Apache HttpClient 4.0.1+ does w/o instrumentation)
-        redirect.setHeaders(original.getAllHeaders());
+        // We should use the initial request because the wrapped one might contain more headers
+        // (i.e.
+        // Host) we do not want to copy
+        redirect.setHeaders(((HttpRequestWrapper) original).getOriginal().getAllHeaders());
       } else {
+        final Collection<String> ddHeaders = PropagationUtils.getAllHeaders();
         for (final Header header : original.getAllHeaders()) {
-          final String name = header.getName().toLowerCase(Locale.ROOT);
-          if (name.startsWith("x-datadog-") || name.startsWith("x-b3-")) {
+          if (ddHeaders.contains(header.getName().toLowerCase(Locale.ROOT))) {
             if (!redirect.containsHeader(header.getName())) {
               redirect.setHeader(header.getName(), header.getValue());
             }
---
+++ internal-api/src/main/java/datadog/trace/util/PropagationUtils.java
@@ -0,0 +1,32 @@
+package datadog.trace.util;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+
+public class PropagationUtils {
+  private static final Collection<String> ALL_HEADERS =
+      Collections.unmodifiableCollection(
+          new LinkedHashSet<>(
+              Arrays.asList(
+                  // W3C headers
+                  "traceparent",
+                  "tracestate",
+                  // DD headers
+                  "x-datadog-trace-id",
+                  "x-datadog-parent-id",
+                  "x-datadog-sampling-priority",
+                  "x-datadog-origin",
+                  "x-datadog-tags",
+                  // B3 single headers
+                  "X-B3-TraceId",
+                  "X-B3-SpanId",
+                  "X-B3-Sampled",
+                  // B3 multi header
+                  "b3")));
+
+  public static Collection<String> getAllHeaders() {
+    return ALL_HEADERS;
+  }
+}
