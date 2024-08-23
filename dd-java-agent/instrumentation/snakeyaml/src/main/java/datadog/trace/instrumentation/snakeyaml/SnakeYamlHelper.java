c3f4563aad1ba78811042cbb8069d625440ceea1
Mario Vidal Domínguez <mario.vidal@datadoghq.com>
2024-08-23 11:09:25 +0200

    Add logger to the SnakeYamlHelper class

--- dd-java-agent/instrumentation/snakeyaml/src/main/java/datadog/trace/instrumentation/snakeyaml/SnakeYamlHelper.java
+++ dd-java-agent/instrumentation/snakeyaml/src/main/java/datadog/trace/instrumentation/snakeyaml/SnakeYamlHelper.java
@@ -2,11 +2,15 @@ package datadog.trace.instrumentation.snakeyaml;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.UndeclaredThrowableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.BaseConstructor;
 
 public final class SnakeYamlHelper {
   private SnakeYamlHelper() {}
+
+  private static final Logger log = LoggerFactory.getLogger(SnakeYamlHelper.class);
 
   private static final Field CONSTRUCTOR = prepareConstructor();
 
@@ -16,6 +20,7 @@ public final class SnakeYamlHelper {
       constructor = Yaml.class.getDeclaredField("constructor");
       constructor.setAccessible(true);
     } catch (Throwable e) {
+      log.debug("Failed to get Yaml constructor", e);
       return null;
     }
     return constructor;
