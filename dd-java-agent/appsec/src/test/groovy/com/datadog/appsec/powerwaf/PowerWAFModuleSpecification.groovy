128307945d94728e736e3346f0dea40ab31f9871
Santiago M. Mola <santiago.mola@datadoghq.com>
2024-08-28 16:58:22 +0200

    Enable WAF generate_stack action by default (#7518)

--- dd-java-agent/appsec/src/test/groovy/com/datadog/appsec/powerwaf/PowerWAFModuleSpecification.groovy
+++ dd-java-agent/appsec/src/test/groovy/com/datadog/appsec/powerwaf/PowerWAFModuleSpecification.groovy
@@ -657,7 +657,7 @@ class PowerWAFModuleSpecification extends DDSpecification {
 
     then:
     1 * segment.setTagTop('_dd.appsec.waf.version', _ as String)
-    1 * segment.setTagTop('_dd.appsec.event_rules.loaded', 115)
+    1 * segment.setTagTop('_dd.appsec.event_rules.loaded', 116)
     1 * segment.setTagTop('_dd.appsec.event_rules.error_count', 1)
     1 * segment.setTagTop('_dd.appsec.event_rules.errors', { it =~ /\{"[^"]+":\["bad rule"\]\}/})
     1 * segment.setTagTop('asm.keep', true)
@@ -777,11 +777,12 @@ class PowerWAFModuleSpecification extends DDSpecification {
     setupWithStubConfigService()
     AppSecEvent event
     StackTraceEvent stackTrace
-    injectSysConfig('appsec.stacktrace.enabled', 'true')
     pwafModule = new PowerWAFModule() // replace the one created too soon
+    def attackBundle = MapDataBundle.of(KnownAddresses.HEADERS_NO_COOKIES,
+      new CaseInsensitiveMap<List<String>>(['user-agent': 'Arachni/generate-stacktrace']))
 
     when:
-    dataListener.onDataAvailable(Stub(ChangeableFlow), ctx, ATTACK_BUNDLE, gwCtx)
+    dataListener.onDataAvailable(Stub(ChangeableFlow), ctx, attackBundle, gwCtx)
     ctx.closeAdditive()
 
     then:
@@ -791,16 +792,16 @@ class PowerWAFModuleSpecification extends DDSpecification {
     ctx.reportEvents(_ as Collection<AppSecEvent>) >> { event = it[0].iterator().next() }
     ctx.reportStackTrace(_ as StackTraceEvent) >> { stackTrace = it[0] }
 
-    event.rule.id == 'ua0-600-12x'
+    event.rule.id == 'generate-stacktrace-on-scanner'
     event.rule.name == 'Arachni'
     event.rule.tags == [type: 'security_scanner', category: 'attack_attempt']
 
     event.ruleMatches[0].operator == 'match_regex'
-    event.ruleMatches[0].operator_value == '^Arachni\\/v'
+    event.ruleMatches[0].operator_value == '^Arachni\\/generate-stacktrace'
     event.ruleMatches[0].parameters[0].address == 'server.request.headers.no_cookies'
-    event.ruleMatches[0].parameters[0].highlight == ['Arachni/v']
+    event.ruleMatches[0].parameters[0].highlight == ['Arachni/generate-stacktrace']
     event.ruleMatches[0].parameters[0].key_path == ['user-agent']
-    event.ruleMatches[0].parameters[0].value == 'Arachni/v0'
+    event.ruleMatches[0].parameters[0].value == 'Arachni/generate-stacktrace'
 
     event.spanId == 777
 
