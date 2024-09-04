43327605c7fd56af08e7c8d1efb3a2041e8d4535
Andrea Marziali <andrea.marziali@datadoghq.com>
2024-09-04 15:47:16 +0200

    Vertx: wrap internal routes to let the context propagate for blocking handlers

--- dd-java-agent/instrumentation/vertx-web-3.4/src/main/java/datadog/trace/instrumentation/vertx_3_4/server/RouteHandlerWrapper.java
+++ dd-java-agent/instrumentation/vertx-web-3.4/src/main/java/datadog/trace/instrumentation/vertx_3_4/server/RouteHandlerWrapper.java
@@ -9,43 +9,54 @@ import static datadog.trace.instrumentation.vertx_3_4.server.VertxDecorator.INST
 import datadog.trace.api.gateway.Flow;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
 import datadog.trace.bootstrap.instrumentation.api.Tags;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.vertx.ext.web.impl.RouteImpl;
+import io.vertx.ext.web.impl.RouterImpl;
 
 public class RouteHandlerWrapper implements Handler<RoutingContext> {
-  private static final Logger log = LoggerFactory.getLogger(RouteHandlerWrapper.class);
   static final String PARENT_SPAN_CONTEXT_KEY = AgentSpan.class.getName() + ".parent";
   static final String HANDLER_SPAN_CONTEXT_KEY = AgentSpan.class.getName() + ".handler";
   static final String ROUTE_CONTEXT_KEY = "dd." + Tags.HTTP_ROUTE;
 
   private final Handler<RoutingContext> actual;
+  private final boolean spanStarter;
 
   public RouteHandlerWrapper(final Handler<RoutingContext> handler) {
     actual = handler;
+    // When mounting a sub router, the handler is a lambda in either RouterImpl or RouteImpl, so
+    // this skips that. This prevents routers from creating a span during handling. In the event
+    // a route is not found, without this code, a span would be created for the router when it
+    // shouldn't
+    String name = handler.getClass().getName();
+    spanStarter =
+        !(name.startsWith(RouterImpl.class.getName())
+            || name.startsWith(RouteImpl.class.getName()));
   }
 
   @Override
   public void handle(final RoutingContext routingContext) {
     AgentSpan span = routingContext.get(HANDLER_SPAN_CONTEXT_KEY);
     Flow.Action.RequestBlockingAction rba = null;
-    if (span == null) {
-      AgentSpan parentSpan = activeSpan();
-      routingContext.put(PARENT_SPAN_CONTEXT_KEY, parentSpan);
+    if (spanStarter) {
+      if (span == null) {
+        AgentSpan parentSpan = activeSpan();
+        routingContext.put(PARENT_SPAN_CONTEXT_KEY, parentSpan);
 
-      span = startSpan(INSTRUMENTATION_NAME);
-      routingContext.put(HANDLER_SPAN_CONTEXT_KEY, span);
+        span = startSpan(INSTRUMENTATION_NAME);
+        routingContext.put(HANDLER_SPAN_CONTEXT_KEY, span);
 
-      routingContext.response().endHandler(new EndHandlerWrapper(routingContext));
-      DECORATE.afterStart(span);
-      span.setResourceName(DECORATE.className(actual.getClass()));
+        routingContext.response().endHandler(new EndHandlerWrapper(routingContext));
+        DECORATE.afterStart(span);
+        span.setResourceName(DECORATE.className(actual.getClass()));
+      }
+
+      updateRoutingContextWithRoute(routingContext);
     }
-
-    updateRoutingContextWithRoute(routingContext);
-
-    try (final AgentScope scope = activateSpan(span)) {
+    try (final AgentScope scope =
+        span != null ? activateSpan(span) : AgentTracer.NoopAgentScope.INSTANCE) {
       scope.setAsyncPropagation(true);
       try {
         actual.handle(routingContext);
