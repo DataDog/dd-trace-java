5bb90e70e61a21c1c0e87602963c174014a8b3de
Andrea Marziali <andrea.marziali@datadoghq.com>
2024-09-09 15:27:44 +0200

    Spring scheduling: ensure spans have no parent

--- dd-java-agent/instrumentation/spring-scheduling-3.1/src/main/java/datadog/trace/instrumentation/springscheduling/SpringSchedulingRunnableWrapper.java
+++ dd-java-agent/instrumentation/spring-scheduling-3.1/src/main/java/datadog/trace/instrumentation/springscheduling/SpringSchedulingRunnableWrapper.java
@@ -1,16 +1,20 @@
 package datadog.trace.instrumentation.springscheduling;
 
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.noopSpan;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
 import static datadog.trace.bootstrap.instrumentation.java.concurrent.ExcludeFilter.ExcludeType.RUNNABLE;
 import static datadog.trace.bootstrap.instrumentation.java.concurrent.ExcludeFilter.exclude;
 import static datadog.trace.instrumentation.springscheduling.SpringSchedulingDecorator.DECORATE;
 import static datadog.trace.instrumentation.springscheduling.SpringSchedulingDecorator.SCHEDULED_CALL;
 
+import datadog.trace.api.Config;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 
 public class SpringSchedulingRunnableWrapper implements Runnable {
+  private static final boolean LEGACY_TRACING =
+      Config.get().isLegacyTracingEnabled(false, "spring-scheduling");
   private final Runnable runnable;
 
   private SpringSchedulingRunnableWrapper(final Runnable runnable) {
@@ -19,22 +23,24 @@ public class SpringSchedulingRunnableWrapper implements Runnable {
 
   @Override
   public void run() {
-    final AgentSpan span = startSpan(SCHEDULED_CALL);
-    DECORATE.afterStart(span);
+    try (AgentScope closeMe = LEGACY_TRACING ? null : activateSpan(noopSpan())) {
+      final AgentSpan span = startSpan(SCHEDULED_CALL);
+      DECORATE.afterStart(span);
 
-    try (final AgentScope scope = activateSpan(span)) {
-      DECORATE.onRun(span, runnable);
-      scope.setAsyncPropagation(true);
+      try (final AgentScope scope = activateSpan(span)) {
+        DECORATE.onRun(span, runnable);
+        scope.setAsyncPropagation(true);
 
-      try {
-        runnable.run();
-      } catch (final Throwable throwable) {
-        DECORATE.onError(span, throwable);
-        throw throwable;
+        try {
+          runnable.run();
+        } catch (final Throwable throwable) {
+          DECORATE.onError(span, throwable);
+          throw throwable;
+        }
+      } finally {
+        DECORATE.beforeFinish(span);
+        span.finish();
       }
-    } finally {
-      DECORATE.beforeFinish(span);
-      span.finish();
     }
   }
 
