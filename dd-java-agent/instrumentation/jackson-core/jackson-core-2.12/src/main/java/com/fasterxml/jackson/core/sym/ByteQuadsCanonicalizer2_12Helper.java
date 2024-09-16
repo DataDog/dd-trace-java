622d2a63b63d1f0fba16a441f0e778999f8421b2
Mario Vidal Domínguez <mario.vidal@datadoghq.com>
2024-09-16 11:51:19 +0200

    Rename helpers, instrumentations, tests and refactor folders to specify versions

--- dd-java-agent/instrumentation/jackson-core/jackson-core-2.12/src/main/java/com/fasterxml/jackson/core/sym/ByteQuadsCanonicalizer2_12Helper.java
+++
@@ -1,9 +0,0 @@
-package com.fasterxml.jackson.core.sym;
-
-public final class ByteQuadsCanonicalizer2_12Helper {
-  private ByteQuadsCanonicalizer2_12Helper() {}
-
-  public static boolean fetchIntern(ByteQuadsCanonicalizer symbols) {
-    return symbols._intern;
-  }
-}
--- dd-java-agent/instrumentation/jackson-core/jackson-core-2.6/src/main/java/com/fasterxml/jackson/core/sym/ByteQuadsCanonicalizer2_6Helper.java
+++
@@ -1,9 +0,0 @@
-package com.fasterxml.jackson.core.sym;
-
-public final class ByteQuadsCanonicalizer2_6Helper {
-  private ByteQuadsCanonicalizer2_6Helper() {}
-
-  public static boolean fetchIntern(ByteQuadsCanonicalizer symbols) {
-    return symbols._intern;
-  }
-}
