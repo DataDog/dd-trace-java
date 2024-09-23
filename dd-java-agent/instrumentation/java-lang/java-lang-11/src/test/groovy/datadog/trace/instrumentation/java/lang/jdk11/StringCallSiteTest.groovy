5057f98da17bc12d5790ce1958a384745f76bbbb
Santiago M. Mola <santiago.mola@datadoghq.com>
2024-09-23 14:23:23 +0200

    Revert "Add propagation to String strip methods (#7651)" (#7664)
    This reverts commit 8da3e7aff152d2fe4fee43819ff600f1f0fed27b.

--- dd-java-agent/instrumentation/java-lang/java-lang-11/src/test/groovy/datadog/trace/instrumentation/java/lang/jdk11/StringCallSiteTest.groovy
+++ dd-java-agent/instrumentation/java-lang/java-lang-11/src/test/groovy/datadog/trace/instrumentation/java/lang/jdk11/StringCallSiteTest.groovy
@@ -1,5 +1,3 @@
-package datadog.trace.instrumentation.java.lang.jdk11
-
 import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.api.iast.InstrumentationBridge
 import datadog.trace.api.iast.propagation.StringModule
@@ -32,24 +30,5 @@ class StringCallSiteTest extends AgentTestRunner {
     result == expected
     1 * iastModule.onStringRepeat(self, count, expected)
     0 * _
-  }
-
-  def 'test string #method call site'() {
-    setup:
-    final module = Mock(StringModule)
-    InstrumentationBridge.registerIastModule(module)
-
-    when:
-    final result = TestStringJDK11Suite."$method"(input)
-
-    then:
-    result == output
-    1 * module.onStringStrip(input, output, trailing)
-
-    where:
-    method                | trailing | input     | output
-    "stringStrip"         | false    | ' hello ' | 'hello'
-    "stringStripLeading"  | false    | ' hello ' | 'hello '
-    "stringStripTrailing" | true     | ' hello ' | ' hello'
   }
 }
