c0a320e2928e595a804f637a4045abdfbec8f68c
Valentin Zakharov <valentin.zakharov1@gmail.com>
2024-09-24 17:11:33 +0200

    Added parallel endpoint to simulate span misalignment

--- dd-smoke-tests/appsec/springboot/src/main/java/datadog/smoketest/appsec/springboot/controller/WebController.java
+++ dd-smoke-tests/appsec/springboot/src/main/java/datadog/smoketest/appsec/springboot/controller/WebController.java
@@ -7,6 +7,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import datadog.smoketest.appsec.springboot.MyAsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +23,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
+
+  @Autowired
+  private MyAsyncService myAsyncService;
+
   @RequestMapping("/greeting")
   public String greeting() {
     return "Sup AppSec Dawg";
@@ -43,6 +50,12 @@ public class WebController {
   public String sqliQuery(@RequestParam("id") String id) throws Exception {
     Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
     conn.createStatement().execute("SELECT 1 FROM DUAL WHERE '1' = '" + id + "'");
+    return "EXECUTED";
+  }
+
+  @GetMapping("parallel/sqli/query")
+  public String sqliQueryParallel(@RequestParam("id") String id) {
+    myAsyncService.performAsyncTask(id);
     return "EXECUTED";
   }
 
