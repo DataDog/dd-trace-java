1b6a701b4d011fcedbb98ae444f9fdce5850c740
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2024-10-04 15:43:33 +0200

    Revert SCO changes

--- communication/src/main/java/datadog/communication/ddagent/SharedCommunicationObjects.java
+++ communication/src/main/java/datadog/communication/ddagent/SharedCommunicationObjects.java
@@ -38,19 +38,16 @@ public class SharedCommunicationObjects {
     if (okHttpClient == null) {
       String unixDomainSocket;
       String namedPipe;
-      HttpUrl url;
       if (!config.isCiVisibilityAgentlessEnabled()) {
         unixDomainSocket = SocketUtils.discoverApmSocket(config);
         namedPipe = config.getAgentNamedPipe();
-        url = agentUrl;
       } else {
         unixDomainSocket = null;
         namedPipe = null;
-        url = null;
       }
       okHttpClient =
           OkHttpUtils.buildHttpClient(
-              url, unixDomainSocket, namedPipe, getHttpClientTimeout(config));
+              agentUrl, unixDomainSocket, namedPipe, getHttpClientTimeout(config));
     }
   }
 
