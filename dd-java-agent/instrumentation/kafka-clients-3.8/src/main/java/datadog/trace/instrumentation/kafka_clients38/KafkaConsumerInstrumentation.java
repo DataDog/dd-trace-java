b53b6dc565d350da9a2c98fe903a8e8ad7d183fc
Nayeem Kamal <kamal.nayeem12@gmail.com>
2024-10-16 17:20:12 -0400

    Support Kafka-clients 3.8+ (#7626)

---
+++ dd-java-agent/instrumentation/kafka-clients-3.8/src/main/java/datadog/trace/instrumentation/kafka_clients38/KafkaConsumerInstrumentation.java
@@ -0,0 +1,91 @@
+package datadog.trace.instrumentation.kafka_clients38;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.returns;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.InstrumenterModule;
+import datadog.trace.api.Config;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@AutoService(InstrumenterModule.class)
+public final class KafkaConsumerInstrumentation extends InstrumenterModule.Tracing
+    implements Instrumenter.ForSingleType {
+
+  public KafkaConsumerInstrumentation() {
+    super("kafka");
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return super.isEnabled() && Config.get().isExperimentalKafkaEnabled();
+  }
+
+  @Override
+  public Map<String, String> contextStore() {
+    Map<String, String> contextStores = new HashMap<>(2);
+    contextStores.put("org.apache.kafka.clients.Metadata", "java.lang.String");
+    contextStores.put(
+        "org.apache.kafka.clients.consumer.ConsumerRecords",
+        "datadog.trace.instrumentation.kafka_clients38.KafkaConsumerInfo");
+    return Collections.unmodifiableMap(contextStores);
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "org.apache.kafka.clients.consumer.ConsumerRecords";
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      packageName + ".TextMapInjectAdapterInterface",
+      packageName + ".KafkaConsumerInfo",
+      packageName + ".KafkaConsumerInstrumentationHelper",
+      packageName + ".KafkaDecorator",
+      packageName + ".TextMapExtractAdapter",
+      packageName + ".TracingIterableDelegator",
+      packageName + ".TracingIterable",
+      packageName + ".TracingIterator",
+      packageName + ".TracingList",
+      packageName + ".TracingListIterator",
+      packageName + ".TextMapInjectAdapter",
+      "datadog.trace.instrumentation.kafka_common.Utils",
+      "datadog.trace.instrumentation.kafka_common.StreamingContext",
+    };
+  }
+
+  @Override
+  public void methodAdvice(MethodTransformer transformer) {
+    transformer.applyAdvice(
+        isMethod()
+            .and(isPublic())
+            .and(named("records"))
+            .and(takesArgument(0, String.class))
+            .and(returns(Iterable.class)),
+        packageName + ".IterableAdvice");
+    transformer.applyAdvice(
+        isMethod()
+            .and(isPublic())
+            .and(named("records"))
+            .and(takesArgument(0, named("org.apache.kafka.common.TopicPartition")))
+            .and(returns(List.class)),
+        packageName + ".ListAdvice");
+    transformer.applyAdvice(
+        isMethod()
+            .and(isPublic())
+            .and(named("iterator"))
+            .and(takesArguments(0))
+            .and(returns(Iterator.class)),
+        packageName + ".IteratorAdvice");
+  }
+}
---
+++ dd-java-agent/instrumentation/kafka-clients-3.8/src/main/java/datadog/trace/instrumentation/kafka_clients38/OffsetCommitCallbackInvokerInstrumentation.java
@@ -0,0 +1,34 @@
+package datadog.trace.instrumentation.kafka_clients38;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.InstrumenterModule;
+import datadog.trace.api.Config;
+
+// new - this instrumentation is completely new.
+// the purpose of this class is to provide us with information on consumer group and cluster ID
+public class OffsetCommitCallbackInvokerInstrumentation extends InstrumenterModule.Tracing
+    implements Instrumenter.ForSingleType {
+  public OffsetCommitCallbackInvokerInstrumentation() {
+    super("kafka");
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return super.isEnabled() && Config.get().isExperimentalKafkaEnabled();
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "org.apache.kafka.clients.consumer.internals.OffsetCommitCallbackInvoker";
+  }
+
+  @Override
+  public void methodAdvice(MethodTransformer transformer) {
+    transformer.applyAdvice(
+        isMethod().and(named("enqueueUserCallbackInvocation")),
+        packageName + ".OffsetCommitCallbackInvokerAdvice");
+  }
+}
