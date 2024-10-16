87e31bd031cc593b44f7430896c35812c87e4a12
Andrea Marziali <andrea.marziali@datadoghq.com>
2024-10-16 12:22:29 +0200

    Grizzly http client: finish spans for all handlers

--- dd-java-agent/instrumentation/grizzly-client-1.9/src/main/java/datadog/trace/instrumentation/grizzly/client/AsyncHandlerInstrumentation.java
+++ dd-java-agent/instrumentation/grizzly-client-1.9/src/main/java/datadog/trace/instrumentation/grizzly/client/AsyncHandlerInstrumentation.java
@@ -1,16 +1,16 @@
 package datadog.trace.instrumentation.grizzly.client;
 
-import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.extendsClass;
+import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.namedOneOf;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
 import static datadog.trace.instrumentation.grizzly.client.ClientDecorator.DECORATE;
 import static java.util.Collections.singletonMap;
 import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.returns;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import com.google.auto.service.AutoService;
-import com.ning.http.client.AsyncCompletionHandler;
 import com.ning.http.client.AsyncHandler;
 import com.ning.http.client.Response;
 import datadog.trace.agent.tooling.Instrumenter;
@@ -26,10 +26,10 @@ import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(InstrumenterModule.class)
-public final class AsyncCompletionHandlerInstrumentation extends InstrumenterModule.Tracing
+public final class AsyncHandlerInstrumentation extends InstrumenterModule.Tracing
     implements Instrumenter.ForTypeHierarchy {
 
-  public AsyncCompletionHandlerInstrumentation() {
+  public AsyncHandlerInstrumentation() {
     super("grizzly-client", "ning");
   }
 
@@ -45,12 +45,12 @@ public final class AsyncCompletionHandlerInstrumentation extends InstrumenterMod
 
   @Override
   public String hierarchyMarkerType() {
-    return "com.ning.http.client.AsyncCompletionHandler";
+    return "com.ning.http.client.AsyncHandler";
   }
 
   @Override
   public ElementMatcher<TypeDescription> hierarchyMatcher() {
-    return extendsClass(named(hierarchyMarkerType()));
+    return implementsInterface(named(hierarchyMarkerType()));
   }
 
   @Override
@@ -65,57 +65,63 @@ public final class AsyncCompletionHandlerInstrumentation extends InstrumenterMod
             .and(takesArgument(0, named("com.ning.http.client.HttpResponseBodyPart"))),
         getClass().getName() + "$OnActivity");
     transformer.applyAdvice(
-        named("onCompleted")
-            .and(takesArgument(0, named("com.ning.http.client.Response")))
-            .and(isPublic()),
+        named("onCompleted").and(returns(named("com.ning.http.client.Response"))).and(isPublic()),
         getClass().getName() + "$OnComplete");
   }
 
   @SuppressWarnings({"rawtypes", "unchecked"})
   public static class OnActivity {
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static AgentSpan resume(@Advice.This final AsyncCompletionHandler<?> handler) {
+    public static AgentScope resume(@Advice.This final AsyncHandler<?> handler) {
       ContextStore<AsyncHandler, Pair> contextStore =
           InstrumentationContext.get(AsyncHandler.class, Pair.class);
       Pair<AgentSpan, AgentSpan> pair = contextStore.get(handler);
-      if (null == pair) {
+      if (null == pair || !pair.hasRight()) {
         return null;
       }
-      return pair.getRight();
+      return activateSpan(pair.getRight());
+    }
+
+    @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
+    public static void onExit(@Advice.Enter final AgentScope scope) {
+      if (null != scope) {
+        scope.close();
+      }
     }
   }
 
   @SuppressWarnings({"unchecked", "rawtypes"})
   public static class OnComplete {
-
     @Advice.OnMethodEnter(suppress = Throwable.class)
     public static AgentScope onEnter(
-        @Advice.This final AsyncCompletionHandler<?> handler,
-        @Advice.Argument(0) final Response response) {
-      ContextStore<AsyncHandler, Pair> contextStore =
-          InstrumentationContext.get(AsyncHandler.class, Pair.class);
-      Pair<AgentSpan, AgentSpan> pair = contextStore.get(handler);
-      if (null == pair) {
+        @Advice.This final AsyncHandler<?> handler,
+        @Advice.Local("contextPair") Pair<AgentSpan, AgentSpan> pair) {
+      pair = InstrumentationContext.get(AsyncHandler.class, Pair.class).remove(handler);
+
+      if (null == pair || !pair.hasLeft()) {
         return null;
       }
-      contextStore.put(handler, null);
+      return activateSpan(pair.getLeft());
+    }
+
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void onExit(
+        @Advice.Enter final AgentScope scope,
+        @Advice.This final AsyncHandler<?> handler,
+        @Advice.Local("contextPair") Pair<AgentSpan, AgentSpan> pair,
+        @Advice.Return final Response response) {
+      if (null != scope) {
+        scope.close();
+      }
+
+      if (null == pair || !pair.hasRight()) {
+        return;
+      }
       AgentSpan requestSpan = pair.getRight();
       if (null != requestSpan) {
         DECORATE.onResponse(requestSpan, response);
         DECORATE.beforeFinish(requestSpan);
         requestSpan.finish();
-      }
-      AgentSpan parent = pair.getLeft();
-      if (null == parent) {
-        return null;
-      }
-      return activateSpan(parent);
-    }
-
-    @Advice.OnMethodExit(onThrowable = Throwable.class)
-    public static void onExit(@Advice.Enter final AgentScope scope) {
-      if (null != scope) {
-        scope.close();
       }
     }
   }
