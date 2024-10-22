c4496e07fe6ba714b6554ea11065d89df7bcdb35
Matthew Li <matthew.li@datadoghq.com>
2024-10-22 16:25:03 -0400

    spotless

--- dd-java-agent/instrumentation/aws-common/src/main/java/datadog/trace/instrumentation/aws/ExpectedQueryParams.java
+++ dd-java-agent/instrumentation/aws-common/src/main/java/datadog/trace/instrumentation/aws/ExpectedQueryParams.java
@@ -5,19 +5,18 @@ import java.util.List;
 
 public class ExpectedQueryParams {
   public static List<String> getExpectedQueryParams(String operation) {
-    switch(operation) {
+    switch (operation) {
       case "Publish":
         return Arrays.asList("Action", "Version", "TopicArn", "Message");
       case "PublishBatch":
         return Arrays.asList(
-      "Action",
-          "Version",
-          "TopicArn",
-          "PublishBatchRequestEntries.member.1.Id",
-          "PublishBatchRequestEntries.member.1.Message",
-          "PublishBatchRequestEntries.member.2.Id",
-          "PublishBatchRequestEntries.member.2.Message"
-        );
+            "Action",
+            "Version",
+            "TopicArn",
+            "PublishBatchRequestEntries.member.1.Id",
+            "PublishBatchRequestEntries.member.1.Message",
+            "PublishBatchRequestEntries.member.2.Id",
+            "PublishBatchRequestEntries.member.2.Message");
       case "AllocateAddress":
       case "DeleteOptionGroup":
         return Arrays.asList("Action", "Version");
@@ -32,28 +31,26 @@ public class ExpectedQueryParams {
         return Arrays.asList("Action", "Version", "QueueUrl", "AttributeName.1");
       case "SendMessageBatch":
         return Arrays.asList(
-      "Action",
-          "Version",
-          "QueueUrl",
-          "SendMessageBatchRequestEntry.1.Id",
-          "SendMessageBatchRequestEntry.1.MessageBody",
-          "SendMessageBatchRequestEntry.2.Id",
-          "SendMessageBatchRequestEntry.2.MessageBody",
-          "SendMessageBatchRequestEntry.3.Id",
-          "SendMessageBatchRequestEntry.3.MessageBody",
-          "SendMessageBatchRequestEntry.4.Id",
-          "SendMessageBatchRequestEntry.4.MessageBody",
-          "SendMessageBatchRequestEntry.5.Id",
-          "SendMessageBatchRequestEntry.5.MessageBody"
-        );
+            "Action",
+            "Version",
+            "QueueUrl",
+            "SendMessageBatchRequestEntry.1.Id",
+            "SendMessageBatchRequestEntry.1.MessageBody",
+            "SendMessageBatchRequestEntry.2.Id",
+            "SendMessageBatchRequestEntry.2.MessageBody",
+            "SendMessageBatchRequestEntry.3.Id",
+            "SendMessageBatchRequestEntry.3.MessageBody",
+            "SendMessageBatchRequestEntry.4.Id",
+            "SendMessageBatchRequestEntry.4.MessageBody",
+            "SendMessageBatchRequestEntry.5.Id",
+            "SendMessageBatchRequestEntry.5.MessageBody");
       case "DeleteMessageBatch":
         return Arrays.asList(
-      "Action",
-          "Version",
-          "QueueUrl",
-          "DeleteMessageBatchRequestEntry.1.Id",
-          "DeleteMessageBatchRequestEntry.1.ReceiptHandle"
-        );
+            "Action",
+            "Version",
+            "QueueUrl",
+            "DeleteMessageBatchRequestEntry.1.Id",
+            "DeleteMessageBatchRequestEntry.1.ReceiptHandle");
       default:
         return null;
     }
