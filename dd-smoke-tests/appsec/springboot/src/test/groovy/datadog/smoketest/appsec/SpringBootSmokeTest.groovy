8984d17965b8286f69b231b5335266a33ff42a28
ValentinZakharov <valentin.zakharov1@gmail.com>
2024-10-28 09:42:02 +0100

    Updated ASM rules to 1.13.1 (#7831)
    * Updated ASM rules to 1.13.1

--- dd-smoke-tests/appsec/springboot/src/test/groovy/datadog/smoketest/appsec/SpringBootSmokeTest.groovy
+++ dd-smoke-tests/appsec/springboot/src/test/groovy/datadog/smoketest/appsec/SpringBootSmokeTest.groovy
@@ -18,7 +18,7 @@ class SpringBootSmokeTest extends AbstractAppSecServerSmokeTest {
 
   def prepareCustomRules() {
     // Prepare ruleset with additional test rules
-    appendRules(
+    mergeRules(
       customRulesPath,
       [
         [
@@ -108,7 +108,7 @@ class SpringBootSmokeTest extends AbstractAppSecServerSmokeTest {
           on_match    : ['block']
         ],
         [
-          id          : '__test_lfi_block',
+          id          : 'rasp-930-100',     // to replace default rule
           name        : 'Local File Inclusion  exploit',
           enable      : 'true',
           tags        : [
@@ -439,7 +439,7 @@ class SpringBootSmokeTest extends AbstractAppSecServerSmokeTest {
     assert rootSpan.meta.get('_dd.appsec.json') != null, '_dd.appsec.json is not set'
     def trigger = null
     for (t in rootSpan.triggers) {
-      if (t['rule']['id'] == '__test_lfi_block') {
+      if (t['rule']['id'] == 'rasp-930-100') {
         trigger = t
         break
       }
