0ce3551ebd051864d6aa07b46eb2a5fb9992a44c
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2024-10-29 13:03:04 +0100

    Limit the visiting of objects for TBV

--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/TrustBoundaryViolationModuleImpl.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/TrustBoundaryViolationModuleImpl.java
@@ -4,10 +4,15 @@ import com.datadog.iast.Dependencies;
 import com.datadog.iast.model.VulnerabilityType;
 import datadog.trace.api.iast.IastContext;
 import datadog.trace.api.iast.sink.TrustBoundaryViolationModule;
+import java.util.Arrays;
+import java.util.List;
 import javax.annotation.Nonnull;
 
 public class TrustBoundaryViolationModuleImpl extends SinkModuleBase
     implements TrustBoundaryViolationModule {
+
+  private static final List<String> ALLOWED_COLLECTION_PKGS =
+      Arrays.asList("java.util", "org.apache.commons.collections", "com.google.common.collect");
 
   public TrustBoundaryViolationModuleImpl(final Dependencies dependencies) {
     super(dependencies);
@@ -21,7 +26,25 @@ public class TrustBoundaryViolationModuleImpl extends SinkModuleBase
     }
     checkInjection(VulnerabilityType.TRUST_BOUNDARY_VIOLATION, name);
     if (value != null) {
-      checkInjectionDeeply(VulnerabilityType.TRUST_BOUNDARY_VIOLATION, value);
+      checkInjectionDeeply(
+          VulnerabilityType.TRUST_BOUNDARY_VIOLATION,
+          value,
+          TrustBoundaryViolationModuleImpl::visitClass);
     }
+  }
+
+  /**
+   * Any potential object can be stored in the session (including lazy ones) so we must be very
+   * careful and ensure that we don't potentially cause any harm by limiting ourselves to well known
+   * types.
+   */
+  private static boolean visitClass(final Class<?> clazz) {
+    final String className = clazz.getName();
+    for (final String pkg : ALLOWED_COLLECTION_PKGS) {
+      if (className.startsWith(pkg)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
