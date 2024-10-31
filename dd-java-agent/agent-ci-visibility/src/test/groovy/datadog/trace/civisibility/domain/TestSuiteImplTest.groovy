43fd5f0f2b089b2d07d9c903116df566afb4a76a
Daniel Mohedano <daniel.mohedano@datadoghq.com>
2024-10-31 18:05:39 +0100

    Add codeowners tag to test suites (#7861)

--- dd-java-agent/agent-ci-visibility/src/test/groovy/datadog/trace/civisibility/domain/TestSuiteImplTest.groovy
+++ dd-java-agent/agent-ci-visibility/src/test/groovy/datadog/trace/civisibility/domain/TestSuiteImplTest.groovy
@@ -4,121 +4,78 @@ import datadog.trace.agent.test.asserts.ListWriterAssert
 import datadog.trace.api.Config
 import datadog.trace.api.DDSpanTypes
 import datadog.trace.api.DDTraceId
-import datadog.trace.api.civisibility.config.TestIdentifier
-import datadog.trace.api.civisibility.coverage.CoverageProbes
 import datadog.trace.api.civisibility.coverage.CoverageStore
 import datadog.trace.api.civisibility.coverage.NoOpCoverageStore
 import datadog.trace.api.civisibility.telemetry.tag.TestFrameworkInstrumentation
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan
-import datadog.trace.bootstrap.instrumentation.api.AgentTracer
+import datadog.trace.bootstrap.instrumentation.api.Tags
 import datadog.trace.civisibility.codeowners.NoCodeowners
 import datadog.trace.civisibility.decorator.TestDecoratorImpl
 import datadog.trace.civisibility.source.MethodLinesResolver
-import datadog.trace.civisibility.source.NoOpSourcePathResolver
+import datadog.trace.civisibility.source.SourcePathResolver
 import datadog.trace.civisibility.telemetry.CiVisibilityMetricCollectorImpl
 import datadog.trace.civisibility.utils.SpanUtils
 
-class TestImplTest extends SpanWriterTest {
-  def "test span is generated"() {
+class TestSuiteImplTest extends SpanWriterTest {
+  def "test suite span is generated and test.codeowners populated"() {
     setup:
-    def test = givenATest()
+    def testSuite = givenATestSuite()
 
     when:
-    test.end(null)
+    testSuite.end(null)
 
     then:
     ListWriterAssert.assertTraces(TEST_WRITER, 1, false, {
       trace(1) {
         span(0) {
-          parent()
-          spanType DDSpanTypes.TEST
+          spanType DDSpanTypes.TEST_SUITE_END
+          tags(false) {
+            "$Tags.TEST_CODEOWNERS" "[\"@global-owner1\",\"@global-owner2\"]"
+          }
         }
       }
     })
   }
 
-  def "test outstanding operation spans are closed"() {
-    setup:
-    def test = givenATest()
+  private static final class MyClass {}
 
-    // given operation that is started, but not closed
-    AgentTracer.activateSpan(AgentTracer.get().startSpan("instrumentation", "span"))
-
-    when:
-    test.end(null)
-
-    then:
-    ListWriterAssert.assertTraces(TEST_WRITER, 1, false, ListWriterAssert.SORT_TRACES_BY_START, {
-      trace(2) {
-        long testId
-        span(0) {
-          parent()
-          spanType DDSpanTypes.TEST
-          testId = getSpan().getSpanId()
-        }
-        span(1) {
-          parentSpanId(BigInteger.valueOf(testId))
-        }
-      }
-    })
-  }
-
-  def "test coverage is not reported if test was skipped"() {
-    setup:
-    def coverageProbes = Mock(CoverageProbes)
-
-    def coverageStore = Mock(CoverageStore)
-    coverageStore.getProbes() >> coverageProbes
-
-    def coveageStoreFactory = Stub(CoverageStore.Factory)
-    coveageStoreFactory.create((TestIdentifier) _) >> coverageStore
-
-    def test = givenATest(coveageStoreFactory)
-
-    when:
-    test.setSkipReason("skipped")
-    test.end(null)
-
-    then:
-    0 * coverageStore.report(_, _, _)
-  }
-
-  private TestImpl givenATest(
+  private TestSuiteImpl givenATestSuite(
     CoverageStore.Factory coverageStoreFactory = new NoOpCoverageStore.Factory()) {
-
     def traceId = Stub(DDTraceId)
     traceId.toLong() >> 123
 
     def moduleSpanContext = Stub(AgentSpan.Context)
     moduleSpanContext.getSpanId() >> 456
     moduleSpanContext.getTraceId() >> traceId
-    def suiteId = 789
 
     def testFramework = TestFrameworkInstrumentation.OTHER
     def config = Config.get()
     def metricCollector = Stub(CiVisibilityMetricCollectorImpl)
     def testDecorator = new TestDecoratorImpl("component", "session-name", "test-command", [:])
     def methodLinesResolver = { it -> MethodLinesResolver.MethodLines.EMPTY }
-    def codeowners = NoCodeowners.INSTANCE
-    new TestImpl(
+
+    def resolver = Stub(SourcePathResolver)
+    resolver.getSourcePath(MyClass) >> "MyClass.java"
+
+    def codeowners = Stub(NoCodeowners)
+    codeowners.getOwners("MyClass.java") >> ["@global-owner1", "@global-owner2"]
+
+    new TestSuiteImpl(
       moduleSpanContext,
-      suiteId,
       "moduleName",
       "suiteName",
-      "testName",
-      "testParameters",
+      "",
+      MyClass,
       null,
-      null,
-      null,
-      null,
+      false,
       InstrumentationType.BUILD,
       testFramework,
       config,
       metricCollector,
       testDecorator,
-      NoOpSourcePathResolver.INSTANCE,
-      methodLinesResolver,
+      resolver,
       codeowners,
+      methodLinesResolver,
       coverageStoreFactory,
       SpanUtils.DO_NOT_PROPAGATE_CI_VISIBILITY_TAGS
       )
