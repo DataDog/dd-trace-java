b4542f45e008704d425be6cee2c883840d1c9f49
Mario Vidal Domínguez <mario.vidal@datadoghq.com>
2024-10-31 10:20:37 +0100

    Increase coverage PropagationUtilsTest

--- internal-api/src/test/groovy/datadog/trace/api/iast/util/PropagationUtilsTest.groovy
+++ internal-api/src/test/groovy/datadog/trace/api/iast/util/PropagationUtilsTest.groovy
@@ -7,6 +7,51 @@ import datadog.trace.test.util.DDSpecification
 
 class PropagationUtilsTest extends DDSpecification {
 
+  void 'test onUriCreate null'() {
+    setup:
+    final uri = URI.create(value)
+
+    when:
+    final result = PropagationUtils.onUriCreate(value, uri)
+
+    then:
+    result == uri
+    0 * _
+
+    where:
+    value << ['http://test.com']
+  }
+
+  void 'test onStringBuilderToString null'() {
+    setup:
+    StringBuilder sb = new StringBuilder(value)
+
+    when:
+    final result = PropagationUtils.onStringBuilderToString(sb, sb.toString())
+
+    then:
+    result == sb.toString()
+    0 * _
+
+    where:
+    value << ['http://test.com']
+  }
+
+  void 'test onStringBuilderAppend null'() {
+    setup:
+    StringBuilder sb = new StringBuilder()
+
+    when:
+    final result = PropagationUtils.onStringBuilderAppend(value, sb.append(value))
+
+    then:
+    result == sb
+    0 * _
+
+    where:
+    value << ['http://test.com']
+  }
+
   void 'test onUriCreate'() {
     setup:
     final iastModule = Mock(CodecModule)
@@ -55,6 +100,63 @@ class PropagationUtilsTest extends DDSpecification {
     then:
     result == sb
     1 * iastModule.onStringBuilderAppend(_ as StringBuilder, _ as String)
+    0 * _
+
+    where:
+    value << ['http://test.com']
+  }
+
+  void 'test onUriCreate throw exception'() {
+    setup:
+    final iastModule = Mock(CodecModule)
+    InstrumentationBridge.registerIastModule(iastModule)
+    final uri = URI.create(value)
+
+    when:
+    final result = PropagationUtils.onUriCreate(value, uri)
+
+    then:
+    result == uri
+    1 * iastModule.onUriCreate(_ as URI, _ as String) >> { throw new Exception("test exception") }
+    1 * iastModule.onUnexpectedException("onUriCreate threw", _ as Exception)
+    0 * _
+
+    where:
+    value << ['http://test.com']
+  }
+
+  void 'test onStringBuilderToString throw exception'() {
+    setup:
+    final iastModule = Mock(StringModule)
+    InstrumentationBridge.registerIastModule(iastModule)
+    StringBuilder sb = new StringBuilder(value)
+
+    when:
+    final result = PropagationUtils.onStringBuilderToString(sb, sb.toString())
+
+    then:
+    result == sb.toString()
+    1 * iastModule.onStringBuilderToString(_ as StringBuilder, _ as String) >> { throw new Exception("test exception") }
+    1 * iastModule.onUnexpectedException("onStringBuilderToString threw", _ as Exception)
+    0 * _
+
+    where:
+    value << ['http://test.com']
+  }
+
+  void 'test onStringBuilderAppend throw exception'() {
+    setup:
+    final iastModule = Mock(StringModule)
+    InstrumentationBridge.registerIastModule(iastModule)
+    StringBuilder sb = new StringBuilder()
+
+    when:
+    final result = PropagationUtils.onStringBuilderAppend(value, sb.append(value))
+
+    then:
+    result == sb
+    1 * iastModule.onStringBuilderAppend(_ as StringBuilder, _ as String) >> { throw new Exception("test exception") }
+    1 * iastModule.onUnexpectedException("onStringBuilderAppend threw", _ as Exception)
     0 * _
 
     where:
