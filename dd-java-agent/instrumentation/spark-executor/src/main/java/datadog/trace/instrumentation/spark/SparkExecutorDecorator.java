02ce8d81a5efde4cc639aebfbf8caa6d41ab1387
Igor Kravchenko <21974069+kr-igor@users.noreply.github.com>
2024-11-06 10:16:01 -0600

    Use unreflected method to get properties

--- dd-java-agent/instrumentation/spark-executor/src/main/java/datadog/trace/instrumentation/spark/SparkExecutorDecorator.java
+++ dd-java-agent/instrumentation/spark-executor/src/main/java/datadog/trace/instrumentation/spark/SparkExecutorDecorator.java
@@ -4,16 +4,60 @@ import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
 import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
 import datadog.trace.bootstrap.instrumentation.decorator.BaseDecorator;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
 import java.util.Properties;
 import org.apache.spark.executor.Executor;
 import org.apache.spark.executor.TaskMetrics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SparkExecutorDecorator extends BaseDecorator {
+  private static final Logger log = LoggerFactory.getLogger(SparkExecutorDecorator.class);
+
   public static final CharSequence SPARK_TASK = UTF8BytesString.create("spark.task");
   public static final CharSequence SPARK = UTF8BytesString.create("spark");
   public static SparkExecutorDecorator DECORATE = new SparkExecutorDecorator();
   private final String propSparkAppName = "spark.app.name";
+  private static final String TASK_DESCRIPTION_CLASSNAME =
+      "org.apache.spark.scheduler.TaskDescription";
+  private static final MethodHandles.Lookup lookup = MethodHandles.lookup();
+  private static final MethodHandle propertiesField_mh;
+
+  private static Class<?> initClass() {
+    try {
+      return Class.forName(
+          SparkExecutorDecorator.TASK_DESCRIPTION_CLASSNAME,
+          false,
+          SparkExecutorDecorator.class.getClassLoader());
+    } catch (ClassNotFoundException e) {
+      log.debug("Can't find class '{}'", TASK_DESCRIPTION_CLASSNAME, e);
+    }
+    return null;
+  }
+
+  private static MethodHandle getFieldGetter() {
+    Class<?> cls = initClass();
+
+    try {
+      if (cls != null) {
+        Field field = cls.getDeclaredField("properties");
+        field.setAccessible(true);
+
+        return lookup.unreflectGetter(field);
+      }
+
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      log.debug("Can't find and unreflect declared field for '{}'", TASK_DESCRIPTION_CLASSNAME);
+    }
+
+    return null;
+  }
+
+  static {
+    propertiesField_mh = getFieldGetter();
+  }
 
   @Override
   protected String[] instrumentationNames() {
@@ -34,18 +78,18 @@ public class SparkExecutorDecorator extends BaseDecorator {
     span.setTag("task_id", taskRunner.taskId());
     span.setTag("task_thread_name", taskRunner.threadName());
 
-    if (taskDescription != null) {
+    if (taskDescription != null && propertiesField_mh != null) {
       try {
-        Field prop = taskDescription.getClass().getDeclaredField("properties");
-        prop.setAccessible(true);
-        Properties props = (Properties) prop.get(taskDescription);
-        String appName = props.getProperty(propSparkAppName);
-        if (appName != null) {
-          AgentTracer.get()
-              .getDataStreamsMonitoring()
-              .setThreadServiceName(taskRunner.getThreadId(), appName);
+        Properties props = (Properties) propertiesField_mh.invoke(taskDescription);
+        if (props != null) {
+          String appName = props.getProperty(propSparkAppName);
+          if (appName != null) {
+            AgentTracer.get()
+                .getDataStreamsMonitoring()
+                .setThreadServiceName(taskRunner.getThreadId(), appName);
+          }
         }
-      } catch (Exception ignored) {
+      } catch (Throwable ignored) {
       }
     }
   }
