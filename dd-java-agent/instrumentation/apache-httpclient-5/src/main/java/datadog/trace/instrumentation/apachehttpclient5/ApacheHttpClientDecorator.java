c4bd2fb9b2727b6d887c7bcce06bffe3d75e849c
Mario Vidal Domínguez <mario.vidal@datadoghq.com>
2024-11-11 19:44:32 +0100

    Undo apache decorator

--- dd-java-agent/instrumentation/apache-httpclient-5/src/main/java/datadog/trace/instrumentation/apachehttpclient5/ApacheHttpClientDecorator.java
+++ dd-java-agent/instrumentation/apache-httpclient-5/src/main/java/datadog/trace/instrumentation/apachehttpclient5/ApacheHttpClientDecorator.java
@@ -43,15 +43,6 @@ public class ApacheHttpClientDecorator extends HttpClientDecorator<HttpRequest, 
   }
 
   @Override
-  protected Object sourceUrl(final HttpRequest request) {
-    try {
-      return request.getUri();
-    } catch (URISyntaxException e) {
-      return null;
-    }
-  }
-
-  @Override
   protected int status(final HttpResponse httpResponse) {
     return httpResponse.getCode();
   }
