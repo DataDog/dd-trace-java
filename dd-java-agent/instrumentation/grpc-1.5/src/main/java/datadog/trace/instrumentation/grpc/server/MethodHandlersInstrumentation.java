7c2e3a1490700874bae4db8e5bad86ccc1c99e59
Justin Lee <justin.lee@datadoghq.com>
2024-11-11 16:36:10 -0500

    Add code origin support to the grpc server integration

---
+++ dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/server/MethodHandlersInstrumentation.java
@@ -0,0 +1,60 @@
+package datadog.trace.instrumentation.grpc.server;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.nameEndsWith;
+import static datadog.trace.bootstrap.debugger.spanorigin.CodeOriginInfo.entry;
+import static net.bytebuddy.matcher.ElementMatchers.isConstructor;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter.ForTypeHierarchy;
+import datadog.trace.agent.tooling.InstrumenterModule;
+import java.lang.reflect.Method;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+@AutoService(InstrumenterModule.class)
+public class MethodHandlersInstrumentation extends InstrumenterModule.Tracing
+    implements ForTypeHierarchy {
+  private static final ElementMatcher<TypeDescription> METHOD_HANDLERS =
+      nameEndsWith("$MethodHandlers");
+
+  public MethodHandlersInstrumentation() {
+    super("grpc-server-code-origin");
+  }
+
+  @Override
+  public String hierarchyMarkerType() {
+    return "io.grpc.MethodDescriptor";
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> hierarchyMatcher() {
+    return METHOD_HANDLERS;
+  }
+
+  @Override
+  public void methodAdvice(MethodTransformer transformer) {
+    transformer.applyAdvice(
+        isConstructor().and(takesArguments(2)),
+        MethodHandlersInstrumentation.class.getName() + "$BuildAdvice");
+  }
+
+  public static class BuildAdvice {
+
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(@Advice.Argument(0) Object serviceImpl) {
+      Class<?> serviceClass = serviceImpl.getClass();
+      Class<?> superclass = serviceClass.getSuperclass();
+      if (superclass != null) {
+        for (Method method : superclass.getDeclaredMethods()) {
+          try {
+            entry(serviceClass.getDeclaredMethod(method.getName(), method.getParameterTypes()));
+          } catch (NoSuchMethodException e) {
+            // service method not overridden on the impl.  skipping instrumentation.
+          }
+        }
+      }
+    }
+  }
+}
