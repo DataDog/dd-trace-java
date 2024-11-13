035deea2156c14947073fab30489183d58c84e14
Daniel Mohedano <daniel.mohedano@datadoghq.com>
2024-11-13 14:32:24 +0100

    add unit tests for `getClassLines` in `BestEffortLinesResolverTest`

--- dd-java-agent/agent-ci-visibility/src/test/groovy/datadog/trace/civisibility/source/BestEffortLinesResolverTest.groovy
+++ dd-java-agent/agent-ci-visibility/src/test/groovy/datadog/trace/civisibility/source/BestEffortLinesResolverTest.groovy
@@ -5,7 +5,7 @@ import spock.lang.Specification
 
 class BestEffortLinesResolverTest extends Specification {
 
-  def "test get source info from delegate"() {
+  def "test get method source info from delegate"() {
     setup:
     def testMethod = TestClass.getDeclaredMethod("testMethod")
     def expectedLines = new LinesResolver.Lines(42, 43)
@@ -16,19 +16,33 @@ class BestEffortLinesResolverTest extends Specification {
 
     delegate.getMethodLines(testMethod) >> expectedLines
     secondDelegate.getMethodLines(testMethod) >> LinesResolver.Lines.EMPTY
+
+    when:
+    def methodLines = resolver.getMethodLines(testMethod)
+
+    then:
+    methodLines == expectedLines
+  }
+
+  def "test get class source info from delegate"() {
+    setup:
+    def expectedLines = new LinesResolver.Lines(42, 43)
+
+    def delegate = Stub(LinesResolver)
+    def secondDelegate = Stub(LinesResolver)
+    def resolver = new BestEffortLinesResolver(delegate, secondDelegate)
+
     delegate.getClassLines(TestClass) >> expectedLines
     secondDelegate.getClassLines(TestClass) >> LinesResolver.Lines.EMPTY
 
     when:
-    def methodLines = resolver.getMethodLines(testMethod)
     def classLines = resolver.getClassLines(TestClass)
 
     then:
-    methodLines == expectedLines
     classLines == expectedLines
   }
 
-  def "test get source info from second delegate"() {
+  def "test get method source info from second delegate"() {
     setup:
     def testMethod = TestClass.getDeclaredMethod("testMethod")
     def expectedLines = new LinesResolver.Lines(42, 43)
@@ -39,19 +53,33 @@ class BestEffortLinesResolverTest extends Specification {
 
     delegate.getMethodLines(testMethod) >> LinesResolver.Lines.EMPTY
     secondDelegate.getMethodLines(testMethod) >> expectedLines
+
+    when:
+    def methodLines = resolver.getMethodLines(testMethod)
+
+    then:
+    methodLines == expectedLines
+  }
+
+  def "test get class source info from second delegate"() {
+    setup:
+    def expectedLines = new LinesResolver.Lines(42, 43)
+
+    def delegate = Stub(LinesResolver)
+    def secondDelegate = Stub(LinesResolver)
+    def resolver = new BestEffortLinesResolver(delegate, secondDelegate)
+
     delegate.getClassLines(TestClass) >> LinesResolver.Lines.EMPTY
     secondDelegate.getClassLines(TestClass) >> expectedLines
 
     when:
-    def methodLines = resolver.getMethodLines(testMethod)
     def classLines = resolver.getClassLines(TestClass)
 
     then:
-    methodLines == expectedLines
     classLines == expectedLines
   }
 
-  def "test failed to get info from both delegates"() {
+  def "test failed to get method info from both delegates"() {
     setup:
     def testMethod = TestClass.getDeclaredMethod("testMethod")
 
@@ -61,15 +89,27 @@ class BestEffortLinesResolverTest extends Specification {
 
     delegate.getMethodLines(testMethod) >> LinesResolver.Lines.EMPTY
     secondDelegate.getMethodLines(testMethod) >> LinesResolver.Lines.EMPTY
+
+    when:
+    def methodLines = resolver.getMethodLines(testMethod)
+
+    then:
+    methodLines == LinesResolver.Lines.EMPTY
+  }
+
+  def "test failed to get class info from both delegates"() {
+    setup:
+    def delegate = Stub(LinesResolver)
+    def secondDelegate = Stub(LinesResolver)
+    def resolver = new BestEffortLinesResolver(delegate, secondDelegate)
+
     delegate.getClassLines(TestClass) >> LinesResolver.Lines.EMPTY
     secondDelegate.getClassLines(TestClass) >> LinesResolver.Lines.EMPTY
 
     when:
-    def methodLines = resolver.getMethodLines(testMethod)
     def classLines = resolver.getClassLines(TestClass)
 
     then:
-    methodLines == LinesResolver.Lines.EMPTY
     classLines == LinesResolver.Lines.EMPTY
   }
 
