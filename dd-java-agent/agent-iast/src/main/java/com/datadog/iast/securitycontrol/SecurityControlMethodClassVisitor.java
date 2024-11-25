74a93b8a7d22790f725491100bde0ffd8a2afa17
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2024-11-25 10:11:42 +0100

    fix classVisitor

--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/securitycontrol/SecurityControlMethodClassVisitor.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/securitycontrol/SecurityControlMethodClassVisitor.java
@@ -3,6 +3,8 @@ package com.datadog.iast.securitycontrol;
 import static org.objectweb.asm.Opcodes.ASM8;
 
 import datadog.trace.api.iast.securitycontrol.SecurityControl;
+import java.util.List;
+import javax.annotation.Nullable;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
@@ -10,20 +12,31 @@ import org.objectweb.asm.Type;
 
 public class SecurityControlMethodClassVisitor extends ClassVisitor {
 
-  private final SecurityControl securityControl;
+  private final List<SecurityControl> securityControls;
 
   public SecurityControlMethodClassVisitor(
-      final ClassWriter cw, final SecurityControl securityControl) {
+      final ClassWriter cw, final List<SecurityControl> securityControls) {
     super(ASM8, cw);
-    this.securityControl = securityControl;
+    this.securityControls = securityControls;
   }
 
   @Override
+  @Nullable
   public MethodVisitor visitMethod(
       int access, String name, String desc, String signature, String[] exceptions) {
     MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
-    if (mv != null && shouldBeAdapted(name, desc)) {
-      mv = new SecurityControlMethodAdapter(mv, securityControl, desc);
+    if (mv == null) {
+      return null;
+    }
+    SecurityControl match = null;
+    for (SecurityControl securityControl : securityControls) {
+      if (shouldBeAdapted(securityControl, name, desc)) {
+        match = securityControl;
+        break;
+      }
+    }
+    if (match != null) {
+      return new SecurityControlMethodAdapter(mv, match, desc);
     }
     return mv;
   }
@@ -32,7 +45,7 @@ public class SecurityControlMethodClassVisitor extends ClassVisitor {
     cv.visitEnd();
   }
 
-  private boolean shouldBeAdapted(String name, String desc) {
+  private boolean shouldBeAdapted(SecurityControl securityControl, String name, String desc) {
 
     if (!securityControl.getMethod().equals(name)) {
       return false;
