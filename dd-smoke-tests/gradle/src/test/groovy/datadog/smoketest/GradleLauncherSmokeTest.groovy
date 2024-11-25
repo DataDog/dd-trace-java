7e874b05b1d97d2251f2c6a1e756e468c8dca62b
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2024-11-25 15:53:45 +0100

    Add a smoke test for Gradle Launcher instrumentation

---
+++ dd-smoke-tests/gradle/src/test/groovy/datadog/smoketest/GradleLauncherSmokeTest.groovy
@@ -0,0 +1,70 @@
+package datadog.smoketest
+
+import datadog.communication.util.IOUtils
+import datadog.trace.civisibility.utils.ShellCommandExecutor
+
+/**
+ * This test runs Gradle Launcher with the Java Tracer injected
+ * and verifies that the tracer is injected into the Gradle Daemon.
+ */
+class GradleLauncherSmokeTest extends AbstractGradleTest {
+
+  private static final int GRADLE_BUILD_TIMEOUT_MILLIS = 30_000
+
+  private static final String AGENT_JAR = System.getProperty("datadog.smoketest.agent.shadowJar.path")
+
+  def "test Gradle Launcher injects tracer into Gradle Daemon: v#gradleVersion, cmd line - #gradleDaemonCmdLineParams"() {
+    given:
+    givenGradleVersionIsCompatibleWithCurrentJvm(gradleVersion)
+    givenGradleProjectFiles("test-gradle-wrapper", ["gradle-wrapper.properties": ["gradle-version": gradleVersion]])
+
+    when:
+    def output = whenRunningGradleLauncherWithJavaTracerInjected(gradleDaemonCmdLineParams)
+
+    then:
+    gradleDaemonStartCommandContains(output,
+    // verify that the javaagent is injected into the Gradle Daemon start command
+    "-javaagent:${AGENT_JAR}",
+    // verify that existing Gradle Daemon JVM args are preserved:
+    // org.gradle.jvmargs provided on the command line, if present,
+    // otherwise org.gradle.jvmargs from gradle.properties file
+    // ("user.country" is used, as Gradle will filter out properties it is not aware of)
+    gradleDaemonCmdLineParams ? gradleDaemonCmdLineParams : "-Duser.country=VALUE_FROM_GRADLE_PROPERTIES_FILE")
+
+    where:
+    gradleVersion         | gradleDaemonCmdLineParams
+    "6.6.1"               | null
+    "6.6.1"               | "-Duser.country=VALUE_FROM_CMD_LINE"
+    "7.6.4"               | null
+    "7.6.4"               | "-Duser.country=VALUE_FROM_CMD_LINE"
+    "8.11.1"              | null
+    "8.11.1"              | "-Duser.country=VALUE_FROM_CMD_LINE"
+    LATEST_GRADLE_VERSION | null
+    LATEST_GRADLE_VERSION | "-Duser.country=VALUE_FROM_CMD_LINE"
+  }
+
+  private String whenRunningGradleLauncherWithJavaTracerInjected(String gradleDaemonCmdLineParams) {
+    def shellCommandExecutor = new ShellCommandExecutor(projectFolder.toFile(), GRADLE_BUILD_TIMEOUT_MILLIS, [
+      "GRADLE_OPTS"                      : "-javaagent:${AGENT_JAR}".toString(),
+      "DD_CIVISIBILITY_ENABLED"          : "true",
+      "DD_CIVISIBILITY_AGENTLESS_ENABLED": "true",
+      "DD_CIVISIBILITY_AGENTLESS_URL"    : "${mockBackend.intakeUrl}".toString(),
+      "DD_API_KEY"                       : "dummy"
+    ])
+    String[] command = ["./gradlew", "--no-daemon", "--info"]
+    if (gradleDaemonCmdLineParams) {
+      command += "-Dorg.gradle.jvmargs=$gradleDaemonCmdLineParams"
+    }
+    return shellCommandExecutor.executeCommand(IOUtils::readFully, command)
+  }
+
+  private static boolean gradleDaemonStartCommandContains(String buildOutput, String... tokens) {
+    def daemonStartCommandLog = buildOutput.split("\n").find { it.contains("Starting process 'Gradle build daemon'") }
+    for (String token : tokens) {
+      if (!daemonStartCommandLog.contains(token)) {
+        throw new org.opentest4j.AssertionFailedError("Gradle Daemon start command does not contain " + token, token, daemonStartCommandLog)
+      }
+    }
+    return true
+  }
+}
