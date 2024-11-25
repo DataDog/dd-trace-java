ec4f68de5c8a7f5186f532c9e01a5926cba2b0a0
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2024-11-25 12:45:57 +0100

    Add unit test for IastSecurityControlTransformer

---
+++ dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/securitycontrol/IastSecurityControlTransformerTest.groovy
@@ -0,0 +1,60 @@
+package com.datadog.iast.securitycontrol
+
+import datadog.trace.api.iast.InstrumentationBridge
+import datadog.trace.api.iast.VulnerabilityMarks
+import datadog.trace.api.iast.propagation.PropagationModule
+import datadog.trace.api.iast.securitycontrol.SecurityControl
+import datadog.trace.api.iast.securitycontrol.SecurityControlFormatter
+import datadog.trace.test.util.DDSpecification
+import foo.bar.securitycontrol.SecurityControlTestSuite
+import net.bytebuddy.agent.ByteBuddyAgent
+
+import java.lang.instrument.Instrumentation
+
+class IastSecurityControlTransformerTest extends DDSpecification{
+
+  def setupSpec() {
+    final config = 'SANITIZER:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:sanitize;INPUT_VALIDATOR:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:validateAll;INPUT_VALIDATOR:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:validate:1,2;java.lang.Object,java.lang.String,java.lang.String'
+    Instrumentation instrumentation =  ByteBuddyAgent.install()
+    List<SecurityControl> securityControls =
+      SecurityControlFormatter.format(config)
+    assert securityControls != null
+    instrumentation.addTransformer(new IastSecurityControlTransformer(securityControls), true)
+  }
+
+
+  void 'test sanitize'(){
+    given:
+    final iastModule = Mock(PropagationModule)
+    InstrumentationBridge.registerIastModule(iastModule)
+    final marks = VulnerabilityMarks.XSS_MARK
+
+    when:
+    final result = SecurityControlTestSuite.sanitize('test')
+
+    then:
+    1 * iastModule.markIfTainted('Sanitized test', marks)
+    0 * _
+  }
+
+  void 'test validate'(){
+    given:
+    final iastModule = Mock(PropagationModule)
+    InstrumentationBridge.registerIastModule(iastModule)
+    final marks = VulnerabilityMarks.XSS_MARK
+
+    when:
+    SecurityControlTestSuite.&"$method".call(args)
+
+    then:
+    expected * iastModule.markIfTainted(toValidate, marks)
+    0 * _
+
+    where:
+    method        | args                             | toValidate | expected
+    'validateAll' | ['test']                         | 'test'     | 1
+    'validateAll' | ['test1', "test2"]               | _          | 2
+    'validate'    | ['test']                         | 'test'     | 0
+    'validate'    | [new Object(), 'test1', "test2"] | _          | 2
+  }
+}
