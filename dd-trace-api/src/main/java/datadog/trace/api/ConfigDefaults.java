757caa40fb87a44684d5e05ebd94ad9df2375c67
veer <veer.bhatia@datadoghq.com>
2024-11-27 11:13:07 -0500

    fix the port

--- dd-trace-api/src/main/java/datadog/trace/api/ConfigDefaults.java
+++ dd-trace-api/src/main/java/datadog/trace/api/ConfigDefaults.java
@@ -34,7 +34,7 @@ public final class ConfigDefaults {
 
   /* These fields are made public because they're referenced elsewhere internally.  They're not intended as public API. */
   public static final String DEFAULT_AGENT_HOST = "localhost";
-  public static final int DEFAULT_TRACE_AGENT_PORT = 3333;
+  public static final int DEFAULT_TRACE_AGENT_PORT = 8126;
   public static final int DEFAULT_DOGSTATSD_PORT = 8125;
   public static final String DEFAULT_TRACE_AGENT_SOCKET_PATH = "/var/run/datadog/apm.socket";
   public static final String DEFAULT_DOGSTATSD_SOCKET_PATH = "/var/run/datadog/dsd.socket";
