9f4aaa89a371062d9a648f201c4b8edb19d1b5e4
Justin Lee <justin.lee@datadoghq.com>
2024-11-27 10:01:38 -0500

    PR feedback updates

--- dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/server/MethodHandlersInstrumentation.java
+++ dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/server/MethodHandlersInstrumentation.java
@@ -8,6 +8,7 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter.ForTypeHierarchy;
 import datadog.trace.agent.tooling.InstrumenterModule;
+import datadog.trace.api.Platform;
 import java.lang.reflect.Method;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
@@ -34,6 +35,11 @@ public class MethodHandlersInstrumentation extends InstrumenterModule.Tracing
   }
 
   @Override
+  public boolean isEnabled() {
+    return super.isEnabled() && !Platform.isGraalVM();
+  }
+
+  @Override
   public void methodAdvice(MethodTransformer transformer) {
     transformer.applyAdvice(
         isConstructor().and(takesArguments(2)),
@@ -50,7 +56,7 @@ public class MethodHandlersInstrumentation extends InstrumenterModule.Tracing
         for (Method method : superclass.getDeclaredMethods()) {
           try {
             entry(serviceClass.getDeclaredMethod(method.getName(), method.getParameterTypes()));
-          } catch (NoSuchMethodException e) {
+          } catch (Throwable e) {
             // service method not overridden on the impl.  skipping instrumentation.
           }
         }
