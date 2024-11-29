c1a79783e0ec687ee62a2f54e97532ee7e9468bf
Cecile Terpin <cecile.terpin@datadoghq.com>
2024-11-29 16:48:26 +0100

    Fix text

--- dd-trace-core/src/test/groovy/datadog/trace/core/traceinterceptor/LatencyTraceInterceptorTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/core/traceinterceptor/LatencyTraceInterceptorTest.groovy
@@ -13,34 +13,38 @@ class LatencyTraceInterceptorTest  extends DDCoreSpecification {
 
   def "test set sampling priority according to latency"() {
     setup:
+
+    injectSysConfig("trace.partial.flush.enabled", partialFlushEnabled)
+    injectSysConfig("trace.latency.interceptor.value", latencyThreshold)
+
+    when:
     def writer = new ListWriter()
-    def props = new Properties()
-    props.setProperty("trace.partial.flush.enabled", partialFlushEnabled)
-    props.setProperty("trace.latency.interceptor.value", latencyThreshold)
+    def tracer = tracerBuilder().writer(writer).build()
 
-    def tracer = tracerBuilder().withProperties(props).writer(writer).build()
-
-    def spanSetup = tracer.buildSpan("test","my_operation_name").withTag(tagname, true).start()
-    sleep(duration)
+    def spanSetup = tracer.buildSpan("test","my_operation_name").withTag(priorityTag, true).start()
+    sleep(minDuration)
     spanSetup.finish()
 
-    expect:
+    then:
     def trace = writer.firstTrace()
     trace.size() == 1
     def span = trace[0]
-
     span.context().getSamplingPriority() == expected
 
+    cleanup:
     tracer.close()
+
     where:
-    partialFlushEnabled | latencyThreshold | tagname                      | duration    | expected
-    "true"              |       "102"     | DDTags.MANUAL_KEEP            |   100       |  2
-    "true"              |       "102"    | DDTags.MANUAL_DROP             |   100       |  -1
-    "true"              |       "102"     | DDTags.MANUAL_KEEP            |   105       |  2
-    "true"              |       "102"     | DDTags.MANUAL_DROP            |   105       |  -1
-    // "false"             |       "102"     | DDTags.MANUAL_KEEP            |   100       |  2
-    // "false"             |       "102"     | DDTags.MANUAL_DROP            |   100       |  -1
-    // "false"             |       "102"     | DDTags.MANUAL_KEEP            |   105       |  2
-    // "false"             |       "102"     | DDTags.MANUAL_DROP            |   105       |  2
+    partialFlushEnabled | latencyThreshold | priorityTag                   | minDuration    | expected
+    "true"              |       "200"      | DDTags.MANUAL_KEEP            |   10           |   2
+    "true"              |       "200"      | DDTags.MANUAL_DROP            |   10           |  -1
+    "true"              |       "200"      | DDTags.MANUAL_KEEP            |   300          |   2
+    "true"              |       "200"      | DDTags.MANUAL_DROP            |   300          |  -1
+    "false"             |       "200"      | DDTags.MANUAL_KEEP            |   10           |   2
+    "false"             |       "200"      | DDTags.MANUAL_DROP            |   10           |  -1
+    "false"             |       "200"      | DDTags.MANUAL_KEEP            |   300          |   2
+    "false"             |       "200"      | DDTags.MANUAL_DROP            |   300          |   2
   }
+
+
 }
