174a934ca8e111658190ff16ad5ae49d5fabe703
Justin Lee <justin.lee@datadoghq.com>
2024-12-03 11:34:59 -0500

    mock the code origin recorder and simply check for invocations restrict the test dep to a narrower scope

--- dd-java-agent/instrumentation/grpc-1.5/src/test/groovy/GrpcCodeOriginTest.groovy
+++ dd-java-agent/instrumentation/grpc-1.5/src/test/groovy/GrpcCodeOriginTest.groovy
@@ -1,22 +1,9 @@
-import com.datadog.debugger.agent.ClassesToRetransformFinder
-import com.datadog.debugger.agent.Configuration
-import com.datadog.debugger.agent.ConfigurationUpdater
-import com.datadog.debugger.agent.DebuggerTransformer
-import com.datadog.debugger.agent.DenyListHelper
-import com.datadog.debugger.agent.JsonSnapshotSerializer
-import com.datadog.debugger.codeorigin.DefaultCodeOriginRecorder
-import com.datadog.debugger.instrumentation.InstrumentationResult
-import com.datadog.debugger.probe.ProbeDefinition
-import com.datadog.debugger.sink.DebuggerSink
-import com.datadog.debugger.sink.ProbeStatusSink
+import datadog.trace.bootstrap.debugger.DebuggerContext.CodeOriginRecorder
 import com.google.common.util.concurrent.MoreExecutors
 import datadog.trace.agent.test.naming.VersionedNamingTestBase
-import datadog.trace.api.Config
 import datadog.trace.api.DDSpanTypes
-import datadog.trace.api.DDTags
 import datadog.trace.bootstrap.debugger.DebuggerContext
 import datadog.trace.bootstrap.instrumentation.api.Tags
-import datadog.trace.util.AgentTaskScheduler
 import example.GreeterGrpc
 import example.Helloworld
 import io.grpc.BindableService
@@ -25,7 +12,7 @@ import io.grpc.Server
 import io.grpc.inprocess.InProcessChannelBuilder
 import io.grpc.inprocess.InProcessServerBuilder
 import io.grpc.stub.StreamObserver
-import net.bytebuddy.agent.ByteBuddyAgent
+import org.mockito.internal.util.MockUtil
 
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.Executors
@@ -33,12 +20,10 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicReference
 
 import static datadog.trace.api.config.TraceInstrumentationConfig.*
-import static datadog.trace.util.AgentThreadFactory.AgentThread.TASK_SCHEDULER
-import static java.lang.String.format
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.when
+import org.mockito.Mockito
 
 abstract class GrpcCodeOriginTest extends VersionedNamingTestBase {
+  private CodeOriginRecorder codeOriginRecorder
 
   @Override
   final String service() {
@@ -81,79 +66,78 @@ abstract class GrpcCodeOriginTest extends VersionedNamingTestBase {
     def error = new AtomicReference()
 
     BindableService greeter = new GreeterGrpc.GreeterImplBase() {
-      @Override
-      StreamObserver<Helloworld.Response> conversation(StreamObserver<Helloworld.Response> observer) {
-        return new StreamObserver<Helloworld.Response>() {
-          @Override
-          void onNext(Helloworld.Response value) {
+        @Override
+        StreamObserver<Helloworld.Response> conversation(StreamObserver<Helloworld.Response> observer) {
+          return new StreamObserver<Helloworld.Response>() {
+              @Override
+              void onNext(Helloworld.Response value) {
 
-            serverReceived << value.message
+                serverReceived << value.message
 
-            (1..msgCount).each {
-              if (TEST_TRACER.activeScope().isAsyncPropagating()) {
-                observer.onNext(value)
-              } else {
-                observer.onError(new IllegalStateException("not async propagating!"))
+                (1..msgCount).each {
+                  if (TEST_TRACER.activeScope().isAsyncPropagating()) {
+                    observer.onNext(value)
+                  } else {
+                    observer.onError(new IllegalStateException("not async propagating!"))
+                  }
+                }
+              }
+
+              @Override
+              void onError(Throwable t) {
+                if (TEST_TRACER.activeScope().isAsyncPropagating()) {
+                  error.set(t)
+                  observer.onError(t)
+                } else {
+                  observer.onError(new IllegalStateException("not async propagating!"))
+                }
+              }
+
+              @Override
+              void onCompleted() {
+                if (TEST_TRACER.activeScope().isAsyncPropagating()) {
+                  observer.onCompleted()
+                } else {
+                  observer.onError(new IllegalStateException("not async propagating!"))
+                }
               }
             }
-          }
-
-          @Override
-          void onError(Throwable t) {
-            if (TEST_TRACER.activeScope().isAsyncPropagating()) {
-              error.set(t)
-              observer.onError(t)
-            } else {
-              observer.onError(new IllegalStateException("not async propagating!"))
-            }
-          }
-
-          @Override
-          void onCompleted() {
-            if (TEST_TRACER.activeScope().isAsyncPropagating()) {
-              observer.onCompleted()
-            } else {
-              observer.onError(new IllegalStateException("not async propagating!"))
-            }
-          }
         }
       }
-    }
     Server server = InProcessServerBuilder.forName(getClass().name).addService(greeter)
-    .executor(directExecutor ? MoreExecutors.directExecutor() : Executors.newCachedThreadPool())
-    .build().start()
+      .executor(directExecutor ? MoreExecutors.directExecutor() : Executors.newCachedThreadPool())
+      .build().start()
 
-    Thread.sleep(1000)
     ManagedChannel channel = InProcessChannelBuilder.forName(getClass().name).build()
     GreeterGrpc.GreeterStub client = GreeterGrpc.newStub(channel).withWaitForReady()
 
     when:
     def streamObserver = client.conversation(new StreamObserver<Helloworld.Response>() {
-      @Override
-      void onNext(Helloworld.Response value) {
-        if (TEST_TRACER.activeScope().isAsyncPropagating()) {
-          clientReceived << value.message
-        } else {
-          error.set(new IllegalStateException("not async propagating!"))
+        @Override
+        void onNext(Helloworld.Response value) {
+          if (TEST_TRACER.activeScope().isAsyncPropagating()) {
+            clientReceived << value.message
+          } else {
+            error.set(new IllegalStateException("not async propagating!"))
+          }
         }
-      }
 
-      @Override
-      void onError(Throwable t) {
-        if (TEST_TRACER.activeScope().isAsyncPropagating()) {
-          error.set(t)
-        } else {
-          error.set(new IllegalStateException("not async propagating!"))
+        @Override
+        void onError(Throwable t) {
+          if (TEST_TRACER.activeScope().isAsyncPropagating()) {
+            error.set(t)
+          } else {
+            error.set(new IllegalStateException("not async propagating!"))
+          }
         }
-      }
 
-      @Override
-      void onCompleted() {
-        if (!TEST_TRACER.activeScope().isAsyncPropagating()) {
-          error.set(new IllegalStateException("not async propagating!"))
+        @Override
+        void onCompleted() {
+          if (!TEST_TRACER.activeScope().isAsyncPropagating()) {
+            error.set(new IllegalStateException("not async propagating!"))
+          }
         }
-      }
-    })
+      })
 
     clientRange.each {
       def message = Helloworld.Response.newBuilder().setMessage("call $it").build()
@@ -172,6 +156,9 @@ abstract class GrpcCodeOriginTest extends VersionedNamingTestBase {
       }
     }.flatten().sort()
 
+
+    def invocations = MockUtil.getInvocationContainer(codeOriginRecorder)
+    assert invocations.invocations.stream().anyMatch { it.method.name == "captureCodeOrigin" }
     assertTraces(2) {
       trace((hasClientMessageSpans() ? clientMessageCount * serverMessageCount : 0) + 1) {
         span {
@@ -220,11 +207,7 @@ abstract class GrpcCodeOriginTest extends VersionedNamingTestBase {
             "$Tags.COMPONENT" "grpc-server"
             "$Tags.SPAN_KIND" Tags.SPAN_KIND_SERVER
             "status.code" "OK"
-            isPresent(DDTags.DD_CODE_ORIGIN_TYPE)
 
-            for (label in ["file", "line", "method", "type", "signature"]) {
-              isPresent(format(DDTags.DD_CODE_ORIGIN_FRAME, 0, label))
-            }
             defaultTags(true)
           }
         }
@@ -270,35 +253,8 @@ abstract class GrpcCodeOriginTest extends VersionedNamingTestBase {
 
   void codeOriginSetup() {
     injectSysConfig(CODE_ORIGIN_FOR_SPANS_ENABLED, "true", true)
-
-    def configuration = Configuration.builder()
-    .setService("grpc code origin test")
-    .build()
-
-    def config = mock(Config.class)
-    when(config.isDebuggerEnabled()).thenReturn(true)
-    when(config.isDebuggerClassFileDumpEnabled()).thenReturn(true)
-    when(config.isDebuggerVerifyByteCode()).thenReturn(false)
-    when(config.getFinalDebuggerSnapshotUrl())
-    .thenReturn("http://localhost:8126/debugger/v1/input")
-    when(config.getFinalDebuggerSymDBUrl()).thenReturn("http://localhost:8126/symdb/v1/input")
-    when(config.getDebuggerCodeOriginMaxUserFrames()).thenReturn(8)
-
-    def probeStatusSink = mock(ProbeStatusSink.class)
-
-    def sink = new DebuggerSink(config, probeStatusSink)
-    def configurationUpdater = new ConfigurationUpdater(INSTRUMENTATION, DebuggerTransformer::new, config, sink, new ClassesToRetransformFinder())
-
-    def currentTransformer = new DebuggerTransformer(config, configuration, {
-      ProbeDefinition definition, InstrumentationResult result ->
-    }, sink)
-    INSTRUMENTATION.addTransformer(currentTransformer)
-
-    DebuggerContext.initProbeResolver(configurationUpdater)
-    DebuggerContext.initClassFilter(new DenyListHelper(null))
-    DebuggerContext.initValueSerializer(new JsonSnapshotSerializer())
-
-    DebuggerContext.initCodeOrigin(new DefaultCodeOriginRecorder(config, configurationUpdater))
+    codeOriginRecorder = Mockito.mock(CodeOriginRecorder)
+    DebuggerContext.initCodeOrigin(codeOriginRecorder)
   }
 }
 
