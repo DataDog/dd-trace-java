3397a5b0b1be13f4cbbb14935665cf9d3d3315f3
Mario Vidal Domínguez <mario.vidal@datadoghq.com>
2024-12-03 12:20:54 +0100

    First commit with Weak Hash vulnerability

---
+++ dd-smoke-tests/springboot-testing/src/main/java/datadog/smoketest/springboot/TestController.java
@@ -0,0 +1,20 @@
+package datadog.smoketest.springboot;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+  @GetMapping(value = "/weak-hash")
+  @ResponseBody
+  public String weakHash() throws NoSuchAlgorithmException {
+    MessageDigest hasher = MessageDigest.getInstance("MD5");
+    hasher.digest("Message body".getBytes(StandardCharsets.UTF_8));
+    return "Weak Hash page";
+  }
+}
---
+++ dd-smoke-tests/springboot-testing/src/test/groovy/datadog/smoketest/springboot/TestControllerTests.groovy
@@ -0,0 +1,26 @@
+package datadog.smoketest.springboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TestControllerTests {
+
+  @Autowired
+  private MockMvc mockMvc
+
+  @Test
+  void contextLoads() throws Exception {
+    this.mockMvc.perform(get("/weak-hash"))
+      .andExpect(status().isOk())
+      .andExpect(content().string("Weak Hash page"))
+  }
+}
