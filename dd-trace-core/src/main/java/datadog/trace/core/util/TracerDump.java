97e8bef3f632c73b12321c0fb7a867e3cc14bb9e
Matthew Li <matthew.li@datadoghq.com>
2024-12-11 11:24:03 -0500

    updating PR comments

--- dd-trace-core/src/main/java/datadog/trace/core/util/TracerDump.java
+++ dd-trace-core/src/main/java/datadog/trace/core/util/TracerDump.java
@@ -6,7 +6,9 @@ import datadog.trace.core.DDSpan;
 import datadog.trace.core.PendingTrace;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.zip.ZipOutputStream;
 
@@ -19,31 +21,38 @@ public final class TracerDump implements TracerFlare.Reporter {
   }
 
   private static final Set<WeakReference<DDSpan>> rootSpans = new HashSet<>();
+  private static final Map<String, WeakReference<DDSpan>> idToWeakRef = new HashMap<>();
 
-  public static void addActiveSpan(final AgentSpan rootSpan) {
+  public static void addUnfinishedRootSpan(final AgentSpan rootSpan) {
     DDSpan rootDDSpan = (DDSpan) rootSpan;
     WeakReference<DDSpan> weakRootSpan = new WeakReference<>(rootDDSpan);
+    idToWeakRef.put(getSpanIdString(rootSpan), weakRootSpan);
     rootSpans.add(weakRootSpan);
   }
 
   public static void suspendRootSpan(final AgentSpan rootSpan) {
-    DDSpan rootDDSpan = (DDSpan) rootSpan;
-    WeakReference<DDSpan> weakRootSpan = new WeakReference<>(rootDDSpan);
+    WeakReference<DDSpan> weakRootSpan = idToWeakRef.get(getSpanIdString(rootSpan));
     rootSpans.remove(weakRootSpan);
   }
 
   @Override
   public void addReportToFlare(ZipOutputStream zip) throws IOException {
+    StringBuilder dumpText = new StringBuilder();
     for (WeakReference<DDSpan> weakRootSpan : rootSpans) {
       DDSpan rootSpan = weakRootSpan.get();
       if (rootSpan != null) {
         PendingTrace trace = (PendingTrace) rootSpan.context().getTraceCollector();
         if (trace != null) {
           for (DDSpan span : trace.getSpans()) {
-            TracerFlare.addText(zip, "trace_dump.txt", span.toString());
+            dumpText.append(span.toString()).append("\n");
           }
         }
       }
     }
+    TracerFlare.addText(zip, "trace_dump.txt", dumpText.toString());
+  }
+
+  private static String getSpanIdString(final AgentSpan rootSpan) {
+    return String.valueOf(rootSpan.getSpanId());
   }
 }
