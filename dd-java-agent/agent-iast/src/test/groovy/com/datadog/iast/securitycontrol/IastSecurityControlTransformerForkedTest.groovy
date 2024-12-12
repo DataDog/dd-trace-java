69a34665827a275e029859678e49a353e0a68f08
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2024-12-12 19:15:23 +0100

    Add more tests

--- dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/securitycontrol/IastSecurityControlTransformerForkedTest.groovy
+++ dd-java-agent/agent-iast/src/test/groovy/com/datadog/iast/securitycontrol/IastSecurityControlTransformerForkedTest.groovy
@@ -13,8 +13,18 @@ import java.lang.instrument.Instrumentation
 
 class IastSecurityControlTransformerForkedTest extends DDSpecification{
 
+  private static final String SANITIZER = 'SANITIZER:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:sanitize'
+  private static final String SANITIZE_VALIDATE_OBJECT = 'SANITIZER:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:sanitizeObject'
+  private static final String SANITIZE_INPUTS= 'SANITIZER:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:sanitizeInputs'
+  private static final String SANITIZE_MANY_INPUTS= 'SANITIZER:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:sanitizeManyInputs'
+  private static final String INPUT_VALIDATOR_VALIDATE_ALL = 'INPUT_VALIDATOR:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:validateAll'
+  private static final String INPUT_VALIDATOR_VALIDATE_OVERLOADED = 'INPUT_VALIDATOR:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:validate:java.lang.Object,java.lang.String,java.lang.String:1,2'
+  private static final String INPUT_VALIDATOR_VALIDATE_RETURNING_INT = 'INPUT_VALIDATOR:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:validateReturningInt'
+  private static final String INPUT_VALIDATOR_VALIDATE_OBJECT = 'INPUT_VALIDATOR:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:validateObject'
+
+
   def setupSpec() {
-    final config = 'SANITIZER:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:sanitize;INPUT_VALIDATOR:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:validateAll;INPUT_VALIDATOR:XSS:foo.bar.securitycontrol.SecurityControlTestSuite:validate:java.lang.Object,java.lang.String,java.lang.String:1,2'
+    final config = "${SANITIZER};${SANITIZE_VALIDATE_OBJECT};${SANITIZE_INPUTS};${SANITIZE_MANY_INPUTS};${INPUT_VALIDATOR_VALIDATE_ALL};${INPUT_VALIDATOR_VALIDATE_OVERLOADED};${INPUT_VALIDATOR_VALIDATE_RETURNING_INT};${INPUT_VALIDATOR_VALIDATE_OBJECT}"
     Instrumentation instrumentation =  ByteBuddyAgent.install()
     List<SecurityControl> securityControls =
       SecurityControlFormatter.format(config)
@@ -30,11 +40,18 @@ class IastSecurityControlTransformerForkedTest extends DDSpecification{
     final marks = (VulnerabilityMarks.XSS_MARK | VulnerabilityMarks.CUSTOM_SECURITY_CONTROL_MARK)
 
     when:
-    SecurityControlTestSuite.sanitize('test')
+    SecurityControlTestSuite.&"$method".call(*args)
 
     then:
-    1 * iastModule.markIfTainted('Sanitized test', marks)
+    1 * iastModule.markIfTainted( 'Sanitized', marks)
     0 * _
+
+    where:
+    method        | args
+    'sanitize' | ['test']
+    'sanitizeObject' | ['test']
+    'sanitizeInputs' | ['test1', new Object(), 27i]
+    'sanitizeManyInputs' | ['test', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10']
   }
 
   void 'test validate'(){
@@ -44,7 +61,7 @@ class IastSecurityControlTransformerForkedTest extends DDSpecification{
     final marks = (VulnerabilityMarks.XSS_MARK | VulnerabilityMarks.CUSTOM_SECURITY_CONTROL_MARK)
 
     when:
-    SecurityControlTestSuite.&"$method".call(args)
+    SecurityControlTestSuite.&"$method".call(*args)
 
     then:
     for (final validate : toValidate){
@@ -54,9 +71,12 @@ class IastSecurityControlTransformerForkedTest extends DDSpecification{
 
     where:
     method        | args                             | toValidate         | expected
-    'validateAll' | ['test']                         | [args[0]]          | 1
-    'validateAll' | ['test1', 'test2']               | [args[0], args[1]] | 1
-    'validate'    | ['test']                         | args[0]            | 0
+    'validateAll' | ['test']                         | args          | 1
+    'validateAll' | ['test1', 'test2']               | args | 1
+    'validateAll' | ['test', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10'] | args | 1
+    'validate'    | ['test']                         | args           | 0
     'validate'    | [new Object(), 'test1', 'test2'] | [args[1], args[2]] | 1
+    'validateReturningInt'    | ['test'] | args | 1
+    'validateObject'    | [new Object()] | args | 1
   }
 }
--- dd-java-agent/agent-iast/src/test/java/foo/bar/securitycontrol/SecurityControlTestSuite.java
+++ dd-java-agent/agent-iast/src/test/java/foo/bar/securitycontrol/SecurityControlTestSuite.java
@@ -10,6 +10,20 @@ public class SecurityControlTestSuite {
     return true; // dummy implementation
   }
 
+  public static boolean validateAll(
+      String input,
+      String input2,
+      String input3,
+      String input4,
+      String input5,
+      String input6,
+      String input7,
+      String input8,
+      String input9,
+      String input10) {
+    return true; // dummy implementation
+  }
+
   public static boolean validate(String input) {
     return true; // dummy implementation
   }
@@ -18,7 +32,37 @@ public class SecurityControlTestSuite {
     return true; // dummy implementation
   }
 
+  public static int validateReturningInt(String input) {
+    return 1; // dummy implementation
+  }
+
+  public static int validateObject(Object input) {
+    return 1; // dummy implementation
+  }
+
   public static String sanitize(String input) {
-    return "Sanitized " + input;
+    return "Sanitized";
+  }
+
+  public static Object sanitizeObject(String input) {
+    return "Sanitized";
+  }
+
+  public static String sanitizeInputs(String input, Object input2, int input3) {
+    return "Sanitized";
+  }
+
+  public static String sanitizeManyInputs(
+      String input,
+      String input2,
+      String input3,
+      String input4,
+      String input5,
+      String input6,
+      String input7,
+      String input8,
+      String input9,
+      String input10) {
+    return "Sanitized";
   }
 }
