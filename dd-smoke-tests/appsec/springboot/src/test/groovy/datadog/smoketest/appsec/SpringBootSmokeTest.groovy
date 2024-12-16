431cba17d7eca01f6a4f9c54f0742b90a9593e28
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2024-12-16 12:10:48 +0100

    format test

--- dd-smoke-tests/appsec/springboot/src/test/groovy/datadog/smoketest/appsec/SpringBootSmokeTest.groovy
+++ dd-smoke-tests/appsec/springboot/src/test/groovy/datadog/smoketest/appsec/SpringBootSmokeTest.groovy
@@ -597,10 +597,10 @@ class SpringBootSmokeTest extends AbstractAppSecServerSmokeTest {
 
     where:
     endpoint                    | cmd                              | params
-    'arrayCmd'                  | ['/bin/../usr/bin/reboot', '-f']             | null
+    'arrayCmd'                  | ['/bin/../usr/bin/reboot', '-f'] | null
     'arrayCmdWithParams'        | ['/bin/../usr/bin/reboot', '-f'] | ['param']
     'arrayCmdWithParamsAndFile' | ['/bin/../usr/bin/reboot', '-f'] | ['param']
-    'processBuilder'            | ['/bin/../usr/bin/reboot', '-f']             | null
+    'processBuilder'            | ['/bin/../usr/bin/reboot', '-f'] | null
   }
 
   void 'rasp blocks on SHI'() {
