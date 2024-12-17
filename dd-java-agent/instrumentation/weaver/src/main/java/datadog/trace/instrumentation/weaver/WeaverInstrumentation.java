9aebeb1402c2935d4d402318623691c201450935
Daniel Mohedano <daniel.mohedano@datadoghq.com>
2024-12-17 12:09:01 +0100

    even simpler skeleton

--- dd-java-agent/instrumentation/weaver/src/main/java/datadog/trace/instrumentation/weaver/WeaverInstrumentation.java
+++ dd-java-agent/instrumentation/weaver/src/main/java/datadog/trace/instrumentation/weaver/WeaverInstrumentation.java
@@ -1,22 +1,18 @@
 package datadog.trace.instrumentation.weaver;
 
-import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
-import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
 import java.util.Set;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.matcher.ElementMatcher;
 import weaver.framework.SuiteEvent;
 
 @AutoService(InstrumenterModule.class)
 public class WeaverInstrumentation extends InstrumenterModule.CiVisibility
-    implements Instrumenter.ForTypeHierarchy {
+    implements Instrumenter.ForSingleType {
+  // implements Instrumenter.ForTypeHierarchy {
 
   public WeaverInstrumentation() {
     super("ci-visibility", "weaver");
@@ -27,14 +23,19 @@ public class WeaverInstrumentation extends InstrumenterModule.CiVisibility
     return super.isApplicable(enabledSystems);
   }
 
-  @Override
-  public String hierarchyMarkerType() {
-    return null;
-  }
+  // @Override
+  // public String hierarchyMarkerType() {
+  //  return null;
+  // }
+
+  // @Override
+  // public ElementMatcher<TypeDescription> hierarchyMatcher() {
+  //  return implementsInterface(named("weaver.framework.RunnerCompat.SuiteEventBroker"));
+  // }
 
   @Override
-  public ElementMatcher<TypeDescription> hierarchyMatcher() {
-    return implementsInterface(named("weaver.framework.RunnerCompat.SuiteEventBroker"));
+  public String instrumentedType() {
+    return "weaver.framework.RunnerCompat.ConcurrentQueueEventBroker";
   }
 
   @Override
@@ -47,10 +48,7 @@ public class WeaverInstrumentation extends InstrumenterModule.CiVisibility
   @Override
   public void methodAdvice(MethodTransformer transformer) {
     transformer.applyAdvice(
-        named("send")
-            .and(takesArguments(1))
-            .and(takesArgument(0, named("weaver.framework.SuiteEvent"))),
-        WeaverInstrumentation.class.getName() + "$SendEventAdvice");
+        named("send"), WeaverInstrumentation.class.getName() + "$SendEventAdvice");
   }
 
   public static class SendEventAdvice {
