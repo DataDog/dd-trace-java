428323f4e7aacba129a6353a18a70d3e002f197f
Andrea Marziali <andrea.marziali@datadoghq.com>
2024-12-18 12:20:20 +0100

    Improves ServiceNameCollector

--- internal-api/src/main/java/datadog/trace/api/remoteconfig/ServiceNameCollector.java
+++ internal-api/src/main/java/datadog/trace/api/remoteconfig/ServiceNameCollector.java
@@ -4,8 +4,10 @@ import static datadog.trace.api.telemetry.LogCollector.SEND_TELEMETRY;
 
 import datadog.trace.api.Config;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nullable;
 import org.slf4j.Logger;
@@ -16,6 +18,9 @@ public class ServiceNameCollector {
   private static final Logger log = LoggerFactory.getLogger(ServiceNameCollector.class);
 
   private static final int MAX_EXTRA_SERVICE = Config.get().getRemoteConfigMaxExtraServices();
+
+  private static final String SERVICE_NAME_LOWERCASE =
+      Config.get().getServiceName().toLowerCase(Locale.ROOT);
 
   // This is not final to allow mocking it on tests
   private static ServiceNameCollector INSTANCE = new ServiceNameCollector();
@@ -48,14 +53,30 @@ public class ServiceNameCollector {
       }
       return;
     }
-    if (!Config.get().getServiceName().equalsIgnoreCase(serviceName)) {
-      services.put(serviceName.toLowerCase(Locale.ROOT), serviceName);
-    }
+    services.put(serviceName, serviceName);
   }
 
+  /**
+   * Get the list of unique services deduplicated by case. There is no locking on the addService map
+   * so, the method is not thread safe.
+   *
+   * @return
+   */
   @Nullable
   public List<String> getServices() {
-    return services.isEmpty() ? null : new ArrayList<>(services.values());
+    if (services.isEmpty()) {
+      return null;
+    }
+    final Set<String> uniques = new HashSet<>(services.size());
+    // avoids reiterating again to convert a set to a list
+    final ArrayList<String> ret = new ArrayList<>(services.size());
+    for (String current : services.keySet()) {
+      String lowerCase = current.toLowerCase(Locale.ROOT);
+      if (!SERVICE_NAME_LOWERCASE.equals(lowerCase) && uniques.add(lowerCase)) {
+        ret.add(current);
+      }
+    }
+    return ret.isEmpty() ? null : ret;
   }
 
   public void clear() {
