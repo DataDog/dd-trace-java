d76cf046742b119b9a3f811e1ad7b1944fc37354
Matthew Li <matthew.li@datadoghq.com>
2024-12-19 21:55:22 -0600

    removing unused log

--- dd-trace-core/src/main/java/datadog/trace/core/PendingTraceBuffer.java
+++ dd-trace-core/src/main/java/datadog/trace/core/PendingTraceBuffer.java
@@ -49,7 +49,6 @@ public abstract class PendingTraceBuffer implements AutoCloseable {
   }
 
   private static class DelayingPendingTraceBuffer extends PendingTraceBuffer {
-    private static final Logger log = LoggerFactory.getLogger(DelayingPendingTraceBuffer.class);
     private static final long FORCE_SEND_DELAY_MS = TimeUnit.SECONDS.toMillis(5);
     private static final long SEND_DELAY_NS = TimeUnit.MILLISECONDS.toNanos(500);
     private static final long SLEEP_TIME_MS = 100;
