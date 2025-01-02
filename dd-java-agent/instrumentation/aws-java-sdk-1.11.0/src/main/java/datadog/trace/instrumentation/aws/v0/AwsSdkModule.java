ace20ff33fe502bcda98822a3e9a0c09ee072c55
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-01-02 14:58:59 +0000

    Example of how to group related instrumentations under a single module (AwsSdkModule) (#8141)

---
+++ dd-java-agent/instrumentation/aws-java-sdk-1.11.0/src/main/java/datadog/trace/instrumentation/aws/v0/AwsSdkModule.java
@@ -0,0 +1,47 @@
+package datadog.trace.instrumentation.aws.v0;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.InstrumenterModule;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/** Groups the instrumentations for AWS SDK 1.11.0+. */
+@AutoService(InstrumenterModule.class)
+public final class AwsSdkModule extends InstrumenterModule.Tracing {
+
+  public AwsSdkModule() {
+    super("aws-sdk");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      packageName + ".AwsSdkClientDecorator",
+      packageName + ".GetterAccess",
+      packageName + ".GetterAccess$1",
+      packageName + ".TracingRequestHandler",
+      packageName + ".AwsNameCache",
+      packageName + ".OnErrorDecorator",
+    };
+  }
+
+  @Override
+  public Map<String, String> contextStore() {
+    Map<String, String> map = new java.util.HashMap<>();
+    map.put("com.amazonaws.services.sqs.model.ReceiveMessageResult", "java.lang.String");
+    map.put(
+        "com.amazonaws.AmazonWebServiceRequest",
+        "datadog.trace.bootstrap.instrumentation.api.AgentSpan");
+    return map;
+  }
+
+  @Override
+  public List<Instrumenter> typeInstrumentations() {
+    return Arrays.asList(
+        new AWSHttpClientInstrumentation(),
+        new RequestExecutorInstrumentation(),
+        new HandlerChainFactoryInstrumentation());
+  }
+}
