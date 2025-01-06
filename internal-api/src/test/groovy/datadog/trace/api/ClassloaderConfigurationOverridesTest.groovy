7f3a1403efab39df11d7d742a80be7c28606bf87
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-01-06 09:15:17 +0100

    more coverage

--- internal-api/src/test/groovy/datadog/trace/api/ClassloaderConfigurationOverridesTest.groovy
+++ internal-api/src/test/groovy/datadog/trace/api/ClassloaderConfigurationOverridesTest.groovy
@@ -2,26 +2,27 @@ package datadog.trace.api
 
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan
 import datadog.trace.test.util.DDSpecification
+import spock.lang.Shared
 import spock.lang.Unroll
 
 class ClassloaderConfigurationOverridesTest extends DDSpecification {
 
+  @Shared
+  def ddService = Config.get().getServiceName()
+
+
   @Unroll
   def 'service name set = #expected when split-by-deployment enabled is #splitByDeploymentEnabled and service name manually set = #spanServiceName and contextual service name is #contextualServiceName'() {
     setup:
-    injectSysConfig("dd.service", "test")
     def span = Mock(AgentSpan)
     when:
     ClassloaderConfigurationOverrides.CAN_SPLIT_SERVICE_NAME_BY_DEPLOYMENT = splitByDeploymentEnabled
-    if (splitByDeploymentEnabled) {
-      injectSysConfig("dd.trace.experimental.jee.split-by-deployment", "true")
-    }
     ClassloaderConfigurationOverrides.addContextualInfo(Thread.currentThread().getContextClassLoader(),
       new ClassloaderConfigurationOverrides.ContextualInfo(contextualServiceName))
     ClassloaderConfigurationOverrides.maybeEnrichSpan(span)
     then:
     if (splitByDeploymentEnabled && contextualServiceName != null && !contextualServiceName.isEmpty()) {
-      (1.._) * span.getServiceName()
+      (1.._) * span.getServiceName() >> spanServiceName
     }
     if (expected) {
       1 * span.setServiceName(contextualServiceName)
@@ -34,11 +35,11 @@ class ClassloaderConfigurationOverridesTest extends DDSpecification {
     false                    | null                  | "test"          | false
     true                     | null                  | null            | false
     true                     | ""                    | null            | false
-    true                     | "cont"                | null            | true
+    true                     | "test"                | null            | true
     true                     | ""                    | "test"          | false
     true                     | null                  | "test"          | false
     true                     | "test"                | "test"          | false
-    true                     | "cont"                | "test"          | true
+    true                     | "test"                | ddService       | true
   }
 
   def "enrich should set tags when present"() {
@@ -49,5 +50,17 @@ class ClassloaderConfigurationOverridesTest extends DDSpecification {
     then:
     1 * span.setTag("key", "value")
     _ * span.getServiceName()
+  }
+
+  def "no enrichment when contextual info is absent"() {
+    def span = Mock(AgentSpan)
+    when:
+    ClassloaderConfigurationOverrides.addContextualInfo(Thread.currentThread().getContextClassLoader(), null)
+    then:
+    ClassloaderConfigurationOverrides.maybeGetContextualInfo() == null
+    when:
+    ClassloaderConfigurationOverrides.maybeEnrichSpan(span)
+    then:
+    _
   }
 }
