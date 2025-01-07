f050879f0a5650e807ade250ca665a7614ca471e
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-01-07 12:14:06 +0000

    Remove old context prototype

--- internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/ImplicitContextKeyed.java
+++
@@ -1,8 +0,0 @@
-package datadog.trace.bootstrap.instrumentation.api;
-
-/** A {@link ScopedContext} value that defines its own implicit {@link ScopedContextKey}. */
-public interface ImplicitContextKeyed {
-
-  /** Creates a new context based on the current context with this value. */
-  ScopedContext storeInto(ScopedContext context);
-}
