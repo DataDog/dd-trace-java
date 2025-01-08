a2125e2fbab826b8f6a5e142c621578decb9b0f0
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-01-08 10:31:15 +0100

    chore(core): Remove custom scope manager support

--- dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
+++ dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
@@ -51,7 +51,6 @@ import datadog.trace.bootstrap.instrumentation.api.AgentDataStreamsMonitoring;
 import datadog.trace.bootstrap.instrumentation.api.AgentHistogram;
 import datadog.trace.bootstrap.instrumentation.api.AgentPropagation;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
-import datadog.trace.bootstrap.instrumentation.api.AgentScopeManager;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpanLink;
 import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
@@ -165,7 +164,7 @@ public class CoreTracer implements AgentTracer.TracerAPI {
   /** Sampler defines the sampling policy in order to reduce the number of traces for instance */
   final Sampler initialSampler;
   /** Scope manager is in charge of managing the scopes from which spans are created */
-  final AgentScopeManager scopeManager;
+  final ContinuableScopeManager scopeManager;
 
   final MetricsAggregator metricsAggregator;
 
@@ -295,7 +294,7 @@ public class CoreTracer implements AgentTracer.TracerAPI {
     private SingleSpanSampler singleSpanSampler;
     private HttpCodec.Injector injector;
     private HttpCodec.Extractor extractor;
-    private AgentScopeManager scopeManager;
+    private ContinuableScopeManager scopeManager;
     private Map<String, ?> localRootSpanTags;
     private Map<String, ?> defaultSpanTags;
     private Map<String, String> serviceNameMappings;
@@ -353,11 +352,6 @@ public class CoreTracer implements AgentTracer.TracerAPI {
 
     public CoreTracerBuilder extractor(HttpCodec.Extractor extractor) {
       this.extractor = extractor;
-      return this;
-    }
-
-    public CoreTracerBuilder scopeManager(AgentScopeManager scopeManager) {
-      this.scopeManager = scopeManager;
       return this;
     }
 
@@ -1103,9 +1097,7 @@ public class CoreTracer implements AgentTracer.TracerAPI {
 
   @Override
   public void addScopeListener(final ScopeListener listener) {
-    if (scopeManager instanceof ContinuableScopeManager) {
-      ((ContinuableScopeManager) scopeManager).addScopeListener(listener);
-    }
+    this.scopeManager.addScopeListener(listener);
   }
 
   @Override
