4af59aacb517849f4fd8c8df7f891e67e459fbba
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-01-13 13:39:50 +0100

    muzzle

--- dd-java-agent/instrumentation/mule-4/src/main/java11/datadog/trace/instrumentation/mule4/JpmsAdvisingHelper.java
+++ dd-java-agent/instrumentation/mule-4/src/main/java11/datadog/trace/instrumentation/mule4/JpmsAdvisingHelper.java
@@ -3,10 +3,19 @@ package datadog.trace.instrumentation.mule4;
 import java.util.WeakHashMap;
 
 public class JpmsAdvisingHelper {
-  private static final WeakHashMap<Module, Boolean> ALREADY_PROCESSED_CACHE = new WeakHashMap<>();
+  private static final WeakHashMap<Class<?>, Boolean> ALREADY_PROCESSED_CACHE = new WeakHashMap<>();
 
-  public static boolean isModuleAlreadyProcessed(final Module module) {
-    return Boolean.TRUE.equals(ALREADY_PROCESSED_CACHE.putIfAbsent(module, Boolean.TRUE));
+  public static void allowAccessOnModuleClass(final Class<?> cls) {
+    if (Boolean.TRUE.equals(ALREADY_PROCESSED_CACHE.putIfAbsent(cls, Boolean.TRUE))) {
+      return;
+    }
+    final Module module = cls.getModule();
+    if (module != null) {
+      try {
+        module.addExports(cls.getPackageName(), module.getClassLoader().getUnnamedModule());
+      } catch (Throwable ignored) {
+      }
+    }
   }
 
   private JpmsAdvisingHelper() {}
