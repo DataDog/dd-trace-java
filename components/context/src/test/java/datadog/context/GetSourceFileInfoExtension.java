8088396064270b2ba883a707c2f6f68d8d1a5358
Sarah Chen <sarah.chen@datadoghq.com>
2025-01-13 17:09:52 -0500

    Add extension to get source file info.

---
+++ components/context/src/test/java/datadog/context/GetSourceFileInfoExtension.java
@@ -0,0 +1,41 @@
+package datadog.context;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+
+public class GetSourceFileInfoExtension implements TestInstancePostProcessor {
+  private static Map<String, String> sourceFiles = new HashMap<String, String>();
+
+  @Override
+  public void postProcessTestInstance(Object testInstance, ExtensionContext context)
+      throws IOException {
+    getTestData(context);
+  }
+
+  public static Map<String, String> getSourceFiles() {
+    return sourceFiles;
+  }
+
+  private static void getTestData(ExtensionContext context) throws IOException {
+    // get test classname and source file
+    String testClassName = context.getTestClass().get().getName();
+    String testClassPath = testClassName.replace(".", "/") + ".java";
+    String root = Paths.get("").toAbsolutePath().toString();
+    String absolutePath = root + "/src/test/java/" + testClassPath;
+    String subPath =
+        absolutePath.substring(absolutePath.indexOf("dd-trace-java") + "dd-trace-java".length());
+    // add info to sourceFiles map
+    sourceFiles.put(testClassName, subPath);
+    // print to sourceFiles.xml
+    BufferedWriter writer =
+        new BufferedWriter(new FileWriter(root + "/build/test-results/test/sourceFiles.xml"));
+    writer.write(sourceFiles.toString());
+    writer.close();
+  }
+}
