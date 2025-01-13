1c8c1b53809d6366bcb8d3263c220c0f3db1d8fa
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-01-13 17:52:41 +0000

    Delay creation of advice remapper+cache until needed
    (cherry picked from commit ede74e31902f08bca41aaf4793344928d7f95680)

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/AdviceShader.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/AdviceShader.java
@@ -10,89 +10,105 @@ import net.bytebuddy.jar.asm.commons.ClassRemapper;
 import net.bytebuddy.jar.asm.commons.Remapper;
 
 /** Shades advice bytecode by applying relocations to all references. */
-public final class AdviceShader extends Remapper {
-  private final DDCache<String, String> cache = DDCaches.newFixedSizeCache(64);
+public final class AdviceShader {
+  private final Map<String, String> relocations;
 
-  /** Flattened sequence of old-prefix, new-prefix relocations. */
-  private final String[] prefixes;
+  private volatile Remapper remapper;
 
   public static AdviceShader with(Map<String, String> relocations) {
-    return relocations != null ? new AdviceShader(relocations) : null;
+    if (relocations != null) {
+      return new AdviceShader(relocations);
+    }
+    return null;
   }
 
-  AdviceShader(Map<String, String> relocations) {
-    // convert relocations to a flattened sequence: old-prefix, new-prefix, etc.
-    this.prefixes = new String[relocations.size() * 2];
-    int i = 0;
-    for (Map.Entry<String, String> e : relocations.entrySet()) {
-      String oldPrefix = e.getKey();
-      String newPrefix = e.getValue();
-      if (oldPrefix.indexOf('.') > 0) {
-        // accept dotted prefixes, but store them in their internal form
-        this.prefixes[i++] = oldPrefix.replace('.', '/');
-        this.prefixes[i++] = newPrefix.replace('.', '/');
-      } else {
-        this.prefixes[i++] = oldPrefix;
-        this.prefixes[i++] = newPrefix;
-      }
-    }
+  private AdviceShader(Map<String, String> relocations) {
+    this.relocations = relocations;
   }
 
   /** Applies shading before calling the given {@link ClassVisitor}. */
-  public ClassVisitor shade(ClassVisitor cv) {
-    return new ClassRemapper(cv, this);
+  public ClassVisitor shadeClass(ClassVisitor cv) {
+    if (null == remapper) {
+      remapper = new AdviceMapper();
+    }
+    return new ClassRemapper(cv, remapper);
   }
 
   /** Returns the result of shading the given bytecode. */
-  public byte[] shade(byte[] bytecode) {
+  public byte[] shadeClass(byte[] bytecode) {
     ClassReader cr = new ClassReader(bytecode);
     ClassWriter cw = new ClassWriter(null, 0);
-    cr.accept(shade(cw), 0);
+    cr.accept(shadeClass(cw), 0);
     return cw.toByteArray();
   }
 
-  @Override
-  public String map(String internalName) {
-    if (internalName.startsWith("java/")
-        || internalName.startsWith("datadog/")
-        || internalName.startsWith("net/bytebuddy/")) {
-      return internalName; // never shade these references
-    }
-    return cache.computeIfAbsent(internalName, this::shade);
-  }
+  final class AdviceMapper extends Remapper {
+    private final DDCache<String, String> mappingCache = DDCaches.newFixedSizeCache(64);
 
-  @Override
-  public Object mapValue(Object value) {
-    if (value instanceof String) {
-      String text = (String) value;
-      if (text.isEmpty()) {
-        return text;
-      } else if (text.indexOf('.') > 0) {
-        return shadeDottedName(text);
+    /** Flattened sequence of old-prefix, new-prefix relocations. */
+    private final String[] prefixes;
+
+    AdviceMapper() {
+      // convert relocations to a flattened sequence: old-prefix, new-prefix, etc.
+      this.prefixes = new String[relocations.size() * 2];
+      int i = 0;
+      for (Map.Entry<String, String> e : relocations.entrySet()) {
+        String oldPrefix = e.getKey();
+        String newPrefix = e.getValue();
+        if (oldPrefix.indexOf('.') > 0) {
+          // accept dotted prefixes, but store them in their internal form
+          this.prefixes[i++] = oldPrefix.replace('.', '/');
+          this.prefixes[i++] = newPrefix.replace('.', '/');
+        } else {
+          this.prefixes[i++] = oldPrefix;
+          this.prefixes[i++] = newPrefix;
+        }
+      }
+    }
+
+    @Override
+    public String map(String internalName) {
+      if (internalName.startsWith("java/")
+          || internalName.startsWith("datadog/")
+          || internalName.startsWith("net/bytebuddy/")) {
+        return internalName; // never shade these references
+      }
+      return mappingCache.computeIfAbsent(internalName, this::shadeInternalName);
+    }
+
+    @Override
+    public Object mapValue(Object value) {
+      if (value instanceof String) {
+        String text = (String) value;
+        if (text.isEmpty()) {
+          return text;
+        } else if (text.indexOf('.') > 0) {
+          return shadeDottedName(text);
+        } else {
+          return shadeInternalName(text);
+        }
       } else {
-        return shade(text);
+        return super.mapValue(value);
       }
-    } else {
-      return super.mapValue(value);
     }
-  }
 
-  private String shade(String internalName) {
-    for (int i = 0; i < prefixes.length; i += 2) {
-      if (internalName.startsWith(prefixes[i])) {
-        return prefixes[i + 1] + internalName.substring(prefixes[i].length());
+    private String shadeInternalName(String internalName) {
+      for (int i = 0; i < prefixes.length; i += 2) {
+        if (internalName.startsWith(prefixes[i])) {
+          return prefixes[i + 1] + internalName.substring(prefixes[i].length());
+        }
       }
+      return internalName;
     }
-    return internalName;
-  }
 
-  private String shadeDottedName(String name) {
-    String internalName = name.replace('.', '/');
-    for (int i = 0; i < prefixes.length; i += 2) {
-      if (internalName.startsWith(prefixes[i])) {
-        return prefixes[i + 1].replace('/', '.') + name.substring(prefixes[i].length());
+    private String shadeDottedName(String name) {
+      String internalName = name.replace('.', '/');
+      for (int i = 0; i < prefixes.length; i += 2) {
+        if (internalName.startsWith(prefixes[i])) {
+          return prefixes[i + 1].replace('/', '.') + name.substring(prefixes[i].length());
+        }
       }
+      return name;
     }
-    return name;
   }
 }
