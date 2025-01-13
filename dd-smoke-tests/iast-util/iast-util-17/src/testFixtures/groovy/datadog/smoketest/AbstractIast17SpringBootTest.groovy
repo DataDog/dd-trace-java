fe5078c4b97bd7fb8c34754d7fc40fc618720093
sezen.leblay <sezen.leblay@datadoghq.com>
2025-01-13 11:15:12 +0100

    smoke test

---
+++ dd-smoke-tests/iast-util/iast-util-17/src/testFixtures/groovy/datadog/smoketest/AbstractIast17SpringBootTest.groovy
@@ -0,0 +1,56 @@
+package datadog.smoketest
+
+import okhttp3.FormBody
+import okhttp3.Request
+
+import static datadog.trace.api.config.IastConfig.IAST_DEBUG_ENABLED
+import static datadog.trace.api.config.IastConfig.IAST_DETECTION_MODE
+import static datadog.trace.api.config.IastConfig.IAST_ENABLED
+
+abstract class AbstractIast17SpringBootTest extends AbstractIastServerSmokeTest {
+
+  @Override
+  ProcessBuilder createProcessBuilder() {
+    String springBootShadowJar = System.getProperty('datadog.smoketest.springboot.shadowJar.path')
+
+    List<String> command = []
+    command.add(javaPath())
+    command.addAll(defaultJavaProperties)
+    command.addAll(iastJvmOpts())
+    command.addAll((String[]) ['-jar', springBootShadowJar, "--server.port=${httpPort}"])
+    ProcessBuilder processBuilder = new ProcessBuilder(command)
+    processBuilder.directory(new File(buildDirectory))
+    // Spring will print all environment variables to the log, which may pollute it and affect log assertions.
+    processBuilder.environment().clear()
+    return processBuilder
+  }
+
+  protected List<String> iastJvmOpts() {
+    return [
+      withSystemProperty(IAST_ENABLED, true),
+      withSystemProperty(IAST_DETECTION_MODE, 'FULL'),
+      withSystemProperty(IAST_DEBUG_ENABLED, true),
+    ]
+  }
+
+  void 'test String#translateEscapes'() {
+    setup:
+    final url = "http://localhost:${httpPort}/string/translateEscapes"
+    final body = new FormBody.Builder()
+      .add('paramater', value)
+      .build()
+    final request = new Request.Builder().url(url).post(body).build()
+
+    when:
+    client.newCall(request).execute()
+
+    then:
+    hasTainted { tainted ->
+      tainted.value == expected
+    }
+
+    where:
+    value       | expected
+    "withEscape/ttab" | "withEscape#0009tab"
+  }
+}
