b4225d20fda9088dadb011a52004c73adcb0b60b
sezen.leblay <sezen.leblay@datadoghq.com>
2025-01-15 15:11:22 +0100

    only focus on transport send

--- dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/EmailInjectionModuleImpl.java
+++ dd-java-agent/agent-iast/src/main/java/com/datadog/iast/sink/EmailInjectionModuleImpl.java
@@ -5,8 +5,6 @@ import com.datadog.iast.model.VulnerabilityType;
 import datadog.trace.api.iast.sink.EmailInjectionModule;
 import javax.annotation.Nullable;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMultipart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,20 +14,6 @@ public class EmailInjectionModuleImpl extends SinkModuleBase implements EmailInj
 
   public EmailInjectionModuleImpl(final Dependencies dependencies) {
     super(dependencies);
-  }
-
-  @Override
-  public void onSendEmail(@Nullable final MimeMultipart message) {
-    if (message == null) {
-      return;
-    }
-    try {
-      for (int i = 0; i < message.getCount(); i++) {
-        checkInjection(VulnerabilityType.EMAIL_HTML_INJECTION, message.getBodyPart(i));
-      }
-    } catch (MessagingException e) {
-      LOGGER.debug("Exception while checking injections of mime multipart message", e);
-    }
   }
 
   @Override
--- internal-api/src/main/java/datadog/trace/api/iast/sink/EmailInjectionModule.java
+++ internal-api/src/main/java/datadog/trace/api/iast/sink/EmailInjectionModule.java
@@ -3,10 +3,7 @@ package datadog.trace.api.iast.sink;
 import datadog.trace.api.iast.IastModule;
 import javax.annotation.Nullable;
 import javax.mail.Message;
-import javax.mail.internet.MimeMultipart;
 
 public interface EmailInjectionModule extends IastModule {
-  void onSendEmail(@Nullable MimeMultipart body);
-
   void onSendEmail(@Nullable Message message);
 }
