7f49f827d8783d845cc3a3e9085d084fd689790c
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-01-15 13:41:13 -0500

    update other implementations of TagsPostProcessor

--- dd-trace-core/src/main/java/datadog/trace/core/tagprocessor/PostProcessorChain.java
+++ dd-trace-core/src/main/java/datadog/trace/core/tagprocessor/PostProcessorChain.java
@@ -1,6 +1,8 @@
 package datadog.trace.core.tagprocessor;
 
+import datadog.trace.bootstrap.instrumentation.api.AgentSpanLink;
 import datadog.trace.core.DDSpanContext;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import javax.annotation.Nonnull;
@@ -14,10 +16,10 @@ public class PostProcessorChain implements TagsPostProcessor {
 
   @Override
   public Map<String, Object> processTags(
-      Map<String, Object> unsafeTags, DDSpanContext spanContext) {
+      Map<String, Object> unsafeTags, DDSpanContext spanContext, List<AgentSpanLink> spanLinks) {
     Map<String, Object> currentTags = unsafeTags;
     for (final TagsPostProcessor tagsPostProcessor : chain) {
-      currentTags = tagsPostProcessor.processTags(currentTags, spanContext);
+      currentTags = tagsPostProcessor.processTags(currentTags, spanContext, spanLinks);
     }
     return currentTags;
   }
