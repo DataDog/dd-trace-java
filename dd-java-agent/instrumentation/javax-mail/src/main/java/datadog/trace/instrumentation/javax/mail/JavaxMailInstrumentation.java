4182134842f10296e9b55e71ef773cb11151ad4e
sezen.leblay <sezen.leblay@datadoghq.com>
2025-01-16 15:50:54 +0100

    advice class added for easier debugging

--- dd-java-agent/instrumentation/javax-mail/src/main/java/datadog/trace/instrumentation/javax/mail/JavaxMailInstrumentation.java
+++ dd-java-agent/instrumentation/javax-mail/src/main/java/datadog/trace/instrumentation/javax/mail/JavaxMailInstrumentation.java
@@ -5,15 +5,9 @@ import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
-import datadog.trace.api.iast.InstrumentationBridge;
 import datadog.trace.api.iast.Sink;
 import datadog.trace.api.iast.VulnerabilityTypes;
-import datadog.trace.api.iast.sink.EmailInjectionModule;
-import java.io.IOException;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Part;
-import javax.mail.internet.MimeMultipart;
 import net.bytebuddy.asm.Advice;
 
 @AutoService(InstrumenterModule.class)
@@ -27,8 +21,7 @@ public class JavaxMailInstrumentation extends InstrumenterModule.Iast
   @Override
   public void methodAdvice(MethodTransformer transformer) {
     transformer.applyAdvice(
-        named("send"),
-        JavaxMailInstrumentation.class.getName() + "$MailInjectionAdvice");
+        named("send0"), JavaxMailInstrumentation.class.getName() + "$MailInjectionAdvice");
   }
 
   @Override
@@ -36,43 +29,18 @@ public class JavaxMailInstrumentation extends InstrumenterModule.Iast
     return "javax.mail.Transport";
   }
 
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      packageName + ".TransportSendAdvice",
+    };
+  }
+
   public static class MailInjectionAdvice {
     @Sink(VulnerabilityTypes.EMAIL_HTML_INJECTION)
     @Advice.OnMethodEnter(suppress = Throwable.class)
     private static void onSend(@Advice.Argument(0) final Message message) {
-      EmailInjectionModule emailInjectionModule = InstrumentationBridge.EMAIL_INJECTION;
-      if (message != null) {
-        try {
-          if (message.getContent() != null) {
-            if (message.isMimeType("text/html")) { // simple html
-              emailInjectionModule.onSendEmail(message.getContent().toString());
-            } else if (message.isMimeType(
-                "multipart/*")) { // needs to be converted into single string
-              if (message.getContent() instanceof MimeMultipart) {
-                emailInjectionModule.onSendEmail(
-                    convertPartToString((MimeMultipart) message.getContent()));
-              }
-            }
-          }
-        } catch (IOException | MessagingException e) {
-          throw new RuntimeException(e);
-        }
-      }
-    }
-
-    private static String convertPartToString(MimeMultipart content)
-        throws MessagingException, IOException {
-      if (content.getCount() == 0) {
-        return null;
-      }
-      StringBuilder sb = new StringBuilder();
-      for (int i = 0; i < content.getCount(); i++) {
-        Part part = content.getBodyPart(i);
-        if (part.isMimeType("text/html")) { // only concerned with html injection
-          sb.append(part.getContent().toString());
-        }
-      }
-      return sb.toString();
+      TransportSendAdvice.sendAdvice(message);
     }
   }
 }
