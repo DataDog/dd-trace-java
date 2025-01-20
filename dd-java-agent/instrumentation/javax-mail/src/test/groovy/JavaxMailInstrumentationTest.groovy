fba4788da7af48987f45d045e7b658ccf4d005b1
sezen.leblay <sezen.leblay@datadoghq.com>
2025-01-20 10:14:50 +0100

    define the tests I want

--- dd-java-agent/instrumentation/javax-mail/src/test/groovy/JavaxMailInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/javax-mail/src/test/groovy/JavaxMailInstrumentationTest.groovy
@@ -19,7 +19,88 @@ class JavaxMailInstrumentationTest  extends AgentTestRunner {
     System.setProperty("mail.smtp.class", MockTransport.getName())
   }
 
-  void 'test javax.mail.Message'() {
+  void 'test javax.mail.Message text'() {
+    given:
+    final module = Mock(EmailInjectionModule)
+    InstrumentationBridge.registerIastModule(module)
+    final session = Session.getInstance(new Properties())
+    final message = new MimeMessage(session)
+    message.setRecipient(Message.RecipientType.TO, new InternetAddress("mock@datadoghq.com"))
+    message.setContent(content, mimetype)
+
+    when:
+    Transport.send(message)
+
+    then:
+    1 * module.onSendEmail(message.getContent())
+
+    where:
+    mimetype | content
+    "text/html" | "<html><body>Hello, Content!</body></html>"
+  }
+
+  void 'test javax.mail.Message Content'() {
+    given:
+    final module = Mock(EmailInjectionModule)
+    InstrumentationBridge.registerIastModule(module)
+    final session = Session.getInstance(new Properties())
+    final message = new MimeMessage(session)
+    message.setRecipient(Message.RecipientType.TO, new InternetAddress("mock@datadoghq.com"))
+    message.setContent(content, mimetype)
+
+
+    when:
+    Transport.send(message)
+
+    then:
+    1 * module.onSendEmail(message.getContent())
+
+    where:
+    mimetype | content
+    "text/html" | "<html><body>Hello, Content!</body></html>"
+  }
+
+  void 'test javax.mail.Message simple text'() {
+    given:
+    final module = Mock(EmailInjectionModule)
+    InstrumentationBridge.registerIastModule(module)
+    final session = Session.getInstance(new Properties())
+    final message = new MimeMessage(session)
+    message.setRecipient(Message.RecipientType.TO, new InternetAddress("mock@datadoghq.com"))
+    message.setContent(content, mimetype)
+
+    when:
+    Transport.send(message)
+
+    then:
+    1 * module.onSendEmail(message.getContent())
+
+    where:
+    mimetype | content
+    "text/html" | "<html><body>Hello, Content!</body></html>"
+  }
+
+  void 'test javax.mail.Message sanitized Text'() {
+    given:
+    final module = Mock(EmailInjectionModule)
+    InstrumentationBridge.registerIastModule(module)
+    final session = Session.getInstance(new Properties())
+    final message = new MimeMessage(session)
+    message.setRecipient(Message.RecipientType.TO, new InternetAddress("mock@datadoghq.com"))
+    message.setContent(content, mimetype)
+
+    when:
+    Transport.send(message)
+
+    then:
+    1 * module.onSendEmail(message.getContent())
+
+    where:
+    mimetype | content
+    "text/html" | "<html><body>Hello, Content!</body></html>"
+  }
+
+  void 'test javax.mail.Message sanitized Object'() {
     given:
     final module = Mock(EmailInjectionModule)
     InstrumentationBridge.registerIastModule(module)
