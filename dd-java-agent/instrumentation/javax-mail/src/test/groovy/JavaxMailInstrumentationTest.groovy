be50dc3632f0d6dbaa0507ef30442d4566597f03
sezen.leblay <sezen.leblay@datadoghq.com>
2025-01-20 10:09:45 +0100

    test OK

--- dd-java-agent/instrumentation/javax-mail/src/test/groovy/JavaxMailInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/javax-mail/src/test/groovy/JavaxMailInstrumentationTest.groovy
@@ -1,12 +1,12 @@
 import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.api.iast.InstrumentationBridge
 import datadog.trace.api.iast.sink.EmailInjectionModule
-
-import javax.mail.Address
+import javax.mail.Transport
 import javax.mail.Message
 import javax.mail.Session
-import javax.mail.Transport
+import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
+import de.saly.javamail.mock2.MockTransport
 
 
 class JavaxMailInstrumentationTest  extends AgentTestRunner {
@@ -15,31 +15,27 @@ class JavaxMailInstrumentationTest  extends AgentTestRunner {
     injectSysConfig("dd.iast.enabled", "true")
   }
 
+  def setupSpec() {
+    System.setProperty("mail.smtp.class", MockTransport.getName())
+  }
 
-  void 'test javax.mail.Message'(MimeMessage value) {
+  void 'test javax.mail.Message'() {
     given:
     final module = Mock(EmailInjectionModule)
     InstrumentationBridge.registerIastModule(module)
-    final mockedTransport = Mock(Transport)
-    final message = value
-    value.setRecipients(MimeMessage.RecipientType.TO, "sezen.leblay@datadoghq.com")
-
-    // mock the actual sending process
-    mockedTransport.connect() >> {  }
-    mockedTransport.close() >> {  }
-    mockedTransport.sendMessage(_ as Message, _ as Address) >> {  }
+    final session = Session.getInstance(new Properties())
+    final message = new MimeMessage(session)
+    message.setRecipient(Message.RecipientType.TO, new InternetAddress("mock@datadoghq.com"))
+    message.setContent(content, mimetype)
 
     when:
-    mockedTransport.send(message)
+    Transport.send(message)
 
     then:
-    assert module.onSendEmail(message)
+    1 * module.onSendEmail(message.getContent())
 
     where:
-    value <<
-      new MimeMessage(Session.getDefaultInstance(new Properties())) { {
-          setContent("<html><body>Hello, Content!</body></html>", "text/html")
-        }
-      }
+    mimetype | content
+    "text/html" | "<html><body>Hello, Content!</body></html>"
   }
 }
