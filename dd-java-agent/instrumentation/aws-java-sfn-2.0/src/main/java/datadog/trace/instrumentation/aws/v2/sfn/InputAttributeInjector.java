fdb01a2c35dd6879b67e0066857193cd3301c43f
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-02-06 15:50:09 -0500

    update InputAttributeInjector to use `datadog.json` component https://github.com/DataDog/dd-trace-java/pull/7973/

--- dd-java-agent/instrumentation/aws-java-sfn-2.0/src/main/java/datadog/trace/instrumentation/aws/v2/sfn/InputAttributeInjector.java
+++ dd-java-agent/instrumentation/aws-java-sfn-2.0/src/main/java/datadog/trace/instrumentation/aws/v2/sfn/InputAttributeInjector.java
@@ -1,33 +1,32 @@
 package datadog.trace.instrumentation.aws.v2.sfn;
 
-import datadog.trace.bootstrap.JsonBuffer;
+import datadog.json.JsonMapper;
+import datadog.json.JsonWriter;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 
 public class InputAttributeInjector {
   public static String buildTraceContext(AgentSpan span) {
-    // Extract span tags
-    JsonBuffer spanTagsJSON = new JsonBuffer();
-    spanTagsJSON.beginObject();
-    span.getTags()
-        .forEach((tagKey, tagValue) -> spanTagsJSON.name(tagKey).value(tagValue.toString()));
-    spanTagsJSON.endObject();
+    String tagsJson = JsonMapper.toJson(span.getTags());
 
-    // Build DD trace context object
-    JsonBuffer ddTraceContextJSON = new JsonBuffer();
-    ddTraceContextJSON
-        .beginObject()
-        .name("_datadog")
-        .beginObject()
-        .name("x-datadog-trace-id")
-        .value(span.getTraceId().toString())
-        .name("x-datadog-parent-id")
-        .value(String.valueOf(span.getSpanId()))
-        .name("x-datadog-tags")
-        .value(spanTagsJSON)
-        .endObject()
-        .endObject();
+    try {
+      JsonWriter ddTraceContextJSON = new JsonWriter();
+      ddTraceContextJSON
+          .beginObject()
+          .name("_datadog")
+          .beginObject()
+          .name("x-datadog-trace-id")
+          .value(span.getTraceId().toString())
+          .name("x-datadog-parent-id")
+          .value(String.valueOf(span.getSpanId()))
+          .name("x-datadog-tags")
+          .jsonValue(tagsJson)
+          .endObject()
+          .endObject();
 
-    return ddTraceContextJSON.toString();
+      return ddTraceContextJSON.toString();
+    } catch (Exception e) {
+      return "{}";
+    }
   }
 
   public static String getModifiedInput(String request, String ddTraceContextJSON) {
@@ -38,8 +37,8 @@ public class InputAttributeInjector {
     if (inputContent.isEmpty()) {
       modifiedInput.insert(endPos, ddTraceContextJSON);
     } else {
-      modifiedInput.insert(
-          endPos, ",".concat(ddTraceContextJSON)); // prepend comma to existing input
+      // Prepend comma to separate from existing content
+      modifiedInput.insert(endPos, "," + ddTraceContextJSON);
     }
     return modifiedInput.toString();
   }
