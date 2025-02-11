7977695065076876e76de1c7e06c90efa951c35b
Valentin Zakharov <valentin.zakharov1@gmail.com>
2025-02-11 18:17:14 +0100

    Implemented time-based sampling per endpoint

--- dd-java-agent/appsec/src/main/java/com/datadog/appsec/api/security/ApiSecurityRequestSampler.java
+++ dd-java-agent/appsec/src/main/java/com/datadog/appsec/api/security/ApiSecurityRequestSampler.java
@@ -1,56 +1,38 @@
 package com.datadog.appsec.api.security;
 
+import com.datadog.appsec.gateway.AppSecRequestContext;
 import datadog.trace.api.Config;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class ApiSecurityRequestSampler {
 
-  private volatile int sampling;
-  private final AtomicLong cumulativeCounter = new AtomicLong();
+  private final ApiAccessTracker apiAccessTracker;
+  private final Config config;
 
   public ApiSecurityRequestSampler(final Config config) {
-    sampling = computeSamplingParameter(config.getApiSecurityRequestSampleRate());
+    this.apiAccessTracker = new ApiAccessTracker();
+    this.config = config;
   }
 
-  /**
-   * Sets the new sampling parameter
-   *
-   * @return {@code true} if the value changed
-   */
-  public boolean setSampling(final float newSamplingFloat) {
-    int newSampling = computeSamplingParameter(newSamplingFloat);
-    if (newSampling != sampling) {
-      sampling = newSampling;
-      cumulativeCounter.set(0); // Reset current sampling counter
-      return true;
+  public boolean sampleRequest(AppSecRequestContext ctx) {
+    if (!config.isApiSecurityEnabled() || ctx == null) {
+      return false;
     }
-    return false;
-  }
 
-  public int getSampling() {
-    return sampling;
-  }
+    String route = ctx.getRoute();
+    if (route == null) {
+      return false;
+    }
 
-  public boolean sampleRequest() {
-    long prevValue = cumulativeCounter.getAndAdd(sampling);
-    long newValue = prevValue + sampling;
-    if (newValue / 100 == prevValue / 100 + 1) {
-      // Sample request
-      return true;
+    String method = ctx.getMethod();
+    if (method == null) {
+      return false;
     }
-    // Skipped by sampling
-    return false;
-  }
 
-  static int computeSamplingParameter(final float pct) {
-    if (pct >= 1) {
-      return 100;
+    int statusCode = ctx.getResponseStatus();
+    if (statusCode == 0) {
+      return false;
     }
-    if (pct < 0) {
-      // Api security can only be disabled by setting the sampling to zero, so we set it to 100%.
-      // TODO: We probably want a warning here.
-      return 100;
-    }
-    return (int) (pct * 100);
+
+    return apiAccessTracker.updateApiAccessIfExpired(route, method, statusCode);
   }
 }
