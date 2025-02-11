50763f1ea864715407dac1725b7829b3ea6e2db8
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2025-02-11 08:20:29 +0100

    fix rebased compilation errors

--- dd-trace-core/src/main/java/datadog/trace/core/propagation/ApmTracionDisabledPropagator.java
+++ dd-trace-core/src/main/java/datadog/trace/core/propagation/ApmTracionDisabledPropagator.java
@@ -8,33 +8,34 @@ import datadog.context.propagation.CarrierSetter;
 import datadog.context.propagation.CarrierVisitor;
 import datadog.context.propagation.Propagator;
 import datadog.context.propagation.Propagators;
+import datadog.trace.api.ProductTraceSource;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpanContext;
 import datadog.trace.core.DDSpanContext;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class StandaloneAsmPropagator implements Propagator {
+public class ApmTracionDisabledPropagator implements Propagator {
   @Override
   public <C> void inject(Context context, C carrier, CarrierSetter<C> setter) {
-    // Stop propagation if appsec propagation is disabled (no ASM events), stop propagation
+    // Stop propagation if  no product events, stop propagation
     AgentSpan span;
     if ((span = fromContext(context)) != null) {
       AgentSpanContext spanContext = span.context();
       if (spanContext instanceof DDSpanContext) {
         DDSpanContext ddSpanContext = (DDSpanContext) spanContext;
-        if (!ddSpanContext.getPropagationTags().isAppsecPropagationEnabled()) {
+        if (ddSpanContext.getPropagationTags().getTraceSource() == ProductTraceSource.UNSET) {
           return;
         }
       }
-      // Only propagate tracing for appsec standalone product
+      // Only propagate tracing if trace source set
       Propagators.forConcern(TRACING_CONCERN).inject(span, carrier, setter);
     }
   }
 
   @Override
   public <C> Context extract(Context context, C carrier, CarrierVisitor<C> visitor) {
-    // Only propagate tracing for appsec standalone product
+    // Only propagate tracing if trace source set
     return Propagators.forConcern(TRACING_CONCERN).extract(context, carrier, visitor);
   }
 }
