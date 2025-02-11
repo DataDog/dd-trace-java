8149451dbb1fd317fdd1bb2a450c5cc955db2ac0
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2025-02-11 13:27:45 +0100

    Revert "fix codenarc"
    This reverts commit 289a048ffc9cfd97900d8389c0ade6565181b990.

--- dd-java-agent/instrumentation/org-json/src/test/groovy/JSONArrayInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/org-json/src/test/groovy/JSONArrayInstrumentationTest.groovy
@@ -3,6 +3,7 @@ import datadog.trace.api.iast.InstrumentationBridge
 import datadog.trace.api.iast.propagation.PropagationModule
 import org.json.JSONArray
 import org.json.JSONObject
+import org.json.JSONTokener
 
 class JSONArrayInstrumentationTest extends AgentTestRunner {
 
