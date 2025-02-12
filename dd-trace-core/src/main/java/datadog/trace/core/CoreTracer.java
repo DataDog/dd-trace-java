815385dba8c543c7b7882542550db2cf58ac0b71
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-02-12 13:13:27 +0000

    delegate

--- dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
+++ dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
@@ -932,8 +932,8 @@ public class CoreTracer implements AgentTracer.TracerAPI {
   @Override
   public AgentScope.Continuation captureActiveSpan() {
     AgentScope activeScope = this.scopeManager.active();
-    if (null != activeScope && activeScope.isAsyncPropagating()) {
-      return scopeManager.captureSpan(activeScope.span());
+    if (null != activeScope) {
+      return activeScope.capture();
     } else {
       return AgentTracer.noopContinuation();
     }
