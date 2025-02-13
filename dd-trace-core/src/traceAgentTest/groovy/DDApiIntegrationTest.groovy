a043cdb2317b21414ed252197803c7e0fd2346d1
Laplie Anderson <laplie.anderson@datadoghq.com>
2025-02-13 18:12:33 -0500

    Cleanup TraceAgentTests to always run

--- dd-trace-core/src/traceAgentTest/groovy/DDApiIntegrationTest.groovy
+++ dd-trace-core/src/traceAgentTest/groovy/DDApiIntegrationTest.groovy
@@ -3,6 +3,7 @@ import datadog.communication.http.OkHttpUtils
 import datadog.communication.serialization.ByteBufferConsumer
 import datadog.communication.serialization.FlushingBuffer
 import datadog.communication.serialization.msgpack.MsgPackWriter
+import datadog.trace.api.Config
 import datadog.trace.api.StatsDClient
 import datadog.trace.common.writer.ListWriter
 import datadog.trace.common.writer.Payload
@@ -15,43 +16,20 @@ import datadog.trace.common.writer.ddagent.TraceMapperV0_5
 import datadog.trace.core.CoreTracer
 import datadog.trace.core.DDSpan
 import datadog.trace.core.monitor.MonitoringImpl
-import datadog.trace.test.util.DDSpecification
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.containers.startupcheck.MinimumDurationRunningStartupCheckStrategy
-import spock.lang.Requires
-import spock.lang.Shared
 
 import java.nio.ByteBuffer
-import java.time.Duration
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicReference
 
-// It is fine to run on CI because CI provides agent externally, not through testcontainers
-@Requires({ "true" == System.getenv("CI") })
-class DDApiIntegrationTest extends DDSpecification {
-  def tracer = CoreTracer.builder().writer(new ListWriter()).build()
+class DDApiIntegrationTest extends AbstractTraceAgentTest {
+  def tracer
   DDSpan span
 
   // Looks like okHttp needs to resolve this, even for connection over socket
   static final SOMEHOST = "datadoghq.com"
   static final SOMEPORT = 123
-
-  /*
-   Note: type here has to stay undefined, otherwise tests will fail in CI in Java 7 because
-   'testcontainers' are built for Java 8 and Java 7 cannot load this class.
-   */
-  @Shared
-  def agentContainer
-  @Shared
-  def agentContainerHost = "localhost"
-  @Shared
-  def agentContainerPort = 8126
-  @Shared
-  Process process
-  @Shared
-  File socketPath
 
   def discovery
   def udsDiscovery
@@ -68,39 +46,8 @@ class DDApiIntegrationTest extends DDSpecification {
     agentResponse.set(responseJson)
   }
 
-  def setupSpec() {
-    /*
-     CI will provide us with agent container running along side our build.
-     When building locally, however, we need to take matters into our own hands
-     and we use 'testcontainers' for this.
-     */
-    if ("true" != System.getenv("CI")) {
-      agentContainer = new GenericContainer("datadog/agent:7.22.0")
-        .withEnv(["DD_APM_ENABLED": "true",
-          "DD_BIND_HOST"  : "0.0.0.0",
-          "DD_API_KEY"    : "invalid_key_but_this_is_fine",
-          "DD_LOGS_STDOUT": "yes"])
-        .withExposedPorts(datadog.trace.api.ConfigDefaults.DEFAULT_TRACE_AGENT_PORT)
-        .withStartupTimeout(Duration.ofSeconds(120))
-        // Apparently we need to sleep for a bit so agent's response `{"service:,env:":1}` in rate_by_service.
-        // This is clearly a race-condition and maybe we should avoid verifying complete response
-        .withStartupCheckStrategy(new MinimumDurationRunningStartupCheckStrategy(Duration.ofSeconds(10)))
-      //        .withLogConsumer { output ->
-      //        print output.utf8String
-      //      }
-      agentContainer.start()
-      agentContainerHost = agentContainer.getHost()
-      agentContainerPort = agentContainer.getMappedPort(datadog.trace.api.ConfigDefaults.DEFAULT_TRACE_AGENT_PORT)
-    }
-
-    File tmpDir = File.createTempDir()
-    tmpDir.deleteOnExit()
-    socketPath = new File(tmpDir, "socket")
-    println "!!!socat UNIX-LISEN:${socketPath},reuseaddr,fork TCP-CONNECT:${agentContainerHost}:${agentContainerPort}"
-    process = Runtime.getRuntime().exec("socat UNIX-LISTEN:${socketPath},reuseaddr,fork TCP-CONNECT:${agentContainerHost}:${agentContainerPort}")
-  }
-
   def setup() {
+    tracer = CoreTracer.builder().writer(new ListWriter()).build()
     span = tracer.buildSpan("fakeOperation").start()
     Thread.sleep(1)
     span.finish()
@@ -110,16 +57,9 @@ class DDApiIntegrationTest extends DDSpecification {
     tracer?.close()
   }
 
-  def cleanupSpec() {
-    if (agentContainer) {
-      agentContainer.stop()
-    }
-    process.destroy()
-  }
-
   def beforeTest(boolean enableV05) {
     MonitoringImpl monitoring = new MonitoringImpl(StatsDClient.NO_OP, 1, TimeUnit.SECONDS)
-    HttpUrl agentUrl = HttpUrl.get(String.format("http://%s:%d", agentContainerHost, agentContainerPort))
+    HttpUrl agentUrl = HttpUrl.get(Config.get().getAgentUrl())
     OkHttpClient httpClient = OkHttpUtils.buildHttpClient(agentUrl, 5000)
     discovery = new DDAgentFeaturesDiscovery(httpClient, monitoring, agentUrl, enableV05, true)
     api = new DDAgentApi(httpClient, agentUrl, discovery, monitoring, false)
@@ -143,7 +83,7 @@ class DDApiIntegrationTest extends DDSpecification {
     assert 200 == response.status()
     assert response.success()
     assert discovery.getTraceEndpoint() == "${version}/traces"
-    assert endpoint.get() == "http://${agentContainerHost}:${agentContainerPort}/${version}/traces"
+    assert endpoint.get() == "${Config.get().getAgentUrl()}/${version}/traces"
     assert agentResponse.get()["rate_by_service"] instanceof Map
 
     where:
@@ -166,7 +106,7 @@ class DDApiIntegrationTest extends DDSpecification {
     assert 200 == response.status()
     assert response.success()
     assert discovery.getTraceEndpoint() == "${version}/traces"
-    assert endpoint.get() == "http://${agentContainerHost}:${agentContainerPort}/${version}/traces"
+    assert endpoint.get() == "${Config.get().getAgentUrl()}/${version}/traces"
     assert agentResponse.get()["rate_by_service"] instanceof Map
 
     where:
