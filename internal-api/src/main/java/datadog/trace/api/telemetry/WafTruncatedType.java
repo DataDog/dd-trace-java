7539ffc1103ebaa14fd262decf1e6c78d81e1ee0
Mario Vidal Domínguez <mario.vidal@datadoghq.com>
2025-02-17 14:56:01 +0100

    Change enum name to be more specific

--- internal-api/src/main/java/datadog/trace/api/telemetry/WafTruncatedType.java
+++ internal-api/src/main/java/datadog/trace/api/telemetry/WafTruncatedType.java
@@ -1,13 +1,13 @@
 package datadog.trace.api.telemetry;
 
-public enum TruncatedType {
+public enum WafTruncatedType {
   STRING_TOO_LONG(1),
   LIST_MAP_TOO_LARGE(2),
   OBJECT_TOO_DEEP(4);
 
   private final int value;
 
-  TruncatedType(int value) {
+  WafTruncatedType(int value) {
     this.value = value;
   }
 
