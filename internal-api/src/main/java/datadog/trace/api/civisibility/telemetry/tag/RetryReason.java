9ecb70d2ac6a27609962d4bb0c31ba2de65d23ed
Daniel Mohedano <daniel.mohedano@datadoghq.com>
2025-02-17 15:24:15 +0100

    update retry reason tag value for attempt to fix

--- internal-api/src/main/java/datadog/trace/api/civisibility/telemetry/tag/RetryReason.java
+++ internal-api/src/main/java/datadog/trace/api/civisibility/telemetry/tag/RetryReason.java
@@ -5,14 +5,19 @@ import datadog.trace.api.civisibility.telemetry.TagValue;
 public enum RetryReason implements TagValue {
   atr("Auto Test Retries"),
   efd("Early Flakiness Detection"),
-  attemptToFix("Attempt to Fix");
+  attemptToFix("Attempt to Fix", "attempt_to_fix");
 
-  private final String s;
+  private final String value;
   private final String description;
 
   RetryReason(String description) {
     this.description = description;
-    this.s = "retry_reason:" + name();
+    this.value = name();
+  }
+
+  RetryReason(String description, String value) {
+    this.description = description;
+    this.value = value;
   }
 
   public String getDescription() {
@@ -20,7 +25,12 @@ public enum RetryReason implements TagValue {
   }
 
   @Override
+  public String toString() {
+    return value;
+  }
+
+  @Override
   public String asString() {
-    return s;
+    return "retry_reason:" + value;
   }
 }
