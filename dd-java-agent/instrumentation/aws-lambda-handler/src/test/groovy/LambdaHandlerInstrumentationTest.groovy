16db575faa7b85553011148e82e01a7cd1c1ddae
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-02-18 15:23:29 -0500

    add tests

--- dd-java-agent/instrumentation/aws-lambda-handler/src/test/groovy/LambdaHandlerInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/aws-lambda-handler/src/test/groovy/LambdaHandlerInstrumentationTest.groovy
@@ -24,6 +24,40 @@ abstract class LambdaHandlerInstrumentationTest extends VersionedNamingTestBase 
       }
     }
   }
+
+  def "test streaming handler with error"() {
+    when:
+    def input = new ByteArrayInputStream(StandardCharsets.UTF_8.encode("Hello").array())
+    def output = new ByteArrayOutputStream()
+    new HandlerStreamingWithError().handleRequest(input, output, null)
+
+    then:
+    thrown(Error)
+    assertTraces(1) {
+      trace(1) {
+        span {
+          operationName operation()
+          errored true
+          tags {
+            "error.type" "java.lang.Error"
+            "error.message" "Some error"
+            "error.stack" String
+            "language" "jvm"
+            "process_id" Long
+            "runtime-id" String
+            "thread.id" Long
+            "thread.name" String
+            "_dd.profiling.ctx" "test"
+            "_dd.profiling.enabled" 0
+            "_dd.agent_psr" 1.0
+            "_dd.tracer_host" String
+            "_sample_rate" 1
+            "_dd.trace_span_attribute_schema" 0
+          }
+        }
+      }
+    }
+  }
 }
 
 
