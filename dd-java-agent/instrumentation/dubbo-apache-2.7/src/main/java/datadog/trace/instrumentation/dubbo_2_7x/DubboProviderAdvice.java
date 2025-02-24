0980443385795db5c1a631a0b521f72126338bc1
liurui <liurui_wuhan@163.com>
2025-02-24 10:40:23 +0800

    修复dubbo response不生效的问题

--- dd-java-agent/instrumentation/dubbo-apache-2.7/src/main/java/datadog/trace/instrumentation/dubbo_2_7x/DubboProviderAdvice.java
+++ dd-java-agent/instrumentation/dubbo-apache-2.7/src/main/java/datadog/trace/instrumentation/dubbo_2_7x/DubboProviderAdvice.java
@@ -3,6 +3,7 @@ package datadog.trace.instrumentation.dubbo_2_7x;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import net.bytebuddy.asm.Advice;
 import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Result;
 import org.apache.dubbo.rpc.proxy.AbstractProxyInvoker;
 
 import static datadog.trace.instrumentation.dubbo_2_7x.DubboDecorator.DECORATE;
@@ -21,10 +22,11 @@ public class DubboProviderAdvice {
 
   @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
   public static void stopSpan(
-      @Advice.Enter final AgentScope scope, @Advice.Thrown final Throwable throwable) {
+      @Advice.Enter final AgentScope scope, @Advice.Return Result result, @Advice.Thrown final Throwable throwable) {
     if (scope == null) {
       return;
     }
+    DECORATE.buildResult(scope, result);
     DECORATE.onError(scope.span(), throwable);
     DECORATE.beforeFinish(scope.span());
 
