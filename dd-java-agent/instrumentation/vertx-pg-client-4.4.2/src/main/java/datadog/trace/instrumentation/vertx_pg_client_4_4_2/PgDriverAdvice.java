ccd206dd77b048440799e65c594514a10efe0ff9
Raphaël Vandon <raphael.vandon@datadoghq.com>
2025-03-03 16:10:59 +0100

    add instrumentation handling v4.4.2+

---
+++ dd-java-agent/instrumentation/vertx-pg-client-4.4.2/src/main/java/datadog/trace/instrumentation/vertx_pg_client_4_4_2/PgDriverAdvice.java
@@ -0,0 +1,40 @@
+package datadog.trace.instrumentation.vertx_pg_client_4_4_2;
+
+import datadog.trace.bootstrap.InstrumentationContext;
+import datadog.trace.bootstrap.instrumentation.jdbc.DBInfo;
+import io.vertx.core.Future;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.pgclient.PgConnection;
+import io.vertx.sqlclient.SqlClient;
+import io.vertx.sqlclient.SqlConnectOptions;
+import io.vertx.sqlclient.impl.SingletonSupplier;
+import java.util.function.Supplier;
+import net.bytebuddy.asm.Advice;
+
+public class PgDriverAdvice {
+
+  @Advice.OnMethodExit(suppress = Throwable.class)
+  public static void afterNewPoolImpl(
+      @Advice.Return final SqlClient zis,
+      @Advice.Argument(1) final Supplier<? extends Future<? extends SqlConnectOptions>> databases) {
+
+    if (databases instanceof SingletonSupplier) {
+      SqlConnectOptions options = (SqlConnectOptions) ((SingletonSupplier) databases).unwrap();
+      DBInfo.Builder builder = DBInfo.DEFAULT.toBuilder();
+      DBInfo info =
+          builder
+              .host(options.getHost())
+              .port(options.getPort())
+              .db(options.getDatabase())
+              .user(options.getUser())
+              .type("postgresql")
+              .build();
+      InstrumentationContext.get(SqlClient.class, DBInfo.class).put(zis, info);
+    }
+  }
+
+  // Limit ourselves to 4.4.2+ by using SingletonSupplier which was added in 4.4.2
+  private static void muzzleCheck(PgConnection connection) {
+    Supplier<Future<PgConnectOptions>> supplier = SingletonSupplier.wrap(new PgConnectOptions());
+  }
+}
