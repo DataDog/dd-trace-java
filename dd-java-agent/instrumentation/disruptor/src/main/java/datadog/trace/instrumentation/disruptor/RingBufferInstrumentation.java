ac60cb13d5540d03b8b498d49077491ea9e02cd2
liurui <liurui_wuhan@163.com>
2025-03-11 16:21:55 +0800

    调整 disruptor

--- dd-java-agent/instrumentation/disruptor/src/main/java/datadog/trace/instrumentation/disruptor/RingBufferInstrumentation.java
+++ dd-java-agent/instrumentation/disruptor/src/main/java/datadog/trace/instrumentation/disruptor/RingBufferInstrumentation.java
@@ -3,18 +3,17 @@ package datadog.trace.instrumentation.disruptor;
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
-import datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
-import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 import java.util.Collections;
 import java.util.Map;
 
-import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.extendsClass;
+import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.nameStartsWith;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.*;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
 
 @AutoService(InstrumenterModule.class)
 public class RingBufferInstrumentation extends InstrumenterModule.Tracing
@@ -24,7 +23,7 @@ public class RingBufferInstrumentation extends InstrumenterModule.Tracing
     super("disruptor");
   }
 
-  public static final String CLASS_NAME = "com.lmax.disruptor.RingBuffer";
+  public static final String CLASS_NAME = "com.lmax.disruptor.Sequencer";
 
   @Override
   public String hierarchyMarkerType() {
@@ -33,7 +32,7 @@ public class RingBufferInstrumentation extends InstrumenterModule.Tracing
 
   @Override
   public ElementMatcher hierarchyMatcher() {
-    return extendsClass(named(CLASS_NAME));
+    return implementsInterface(named(CLASS_NAME));
   }
 
   @Override
@@ -41,7 +40,7 @@ public class RingBufferInstrumentation extends InstrumenterModule.Tracing
     transformation.applyAdvice(
         isMethod()
             .and(isPublic())
-            .and(named("publish")),
+            .and(nameStartsWith("publish")),
         packageName + ".RingBufferPublishAdvice");
   }
 
