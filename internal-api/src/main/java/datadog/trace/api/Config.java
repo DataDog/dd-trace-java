cbdc4049588f4452607cb6281b6ecd9282e7fa6a
Sarah Chen <sarah.chen@datadoghq.com>
2025-03-17 09:27:33 -0400

    Add JDK built-in support for UDS on Java 16+ (#8314)
    * Use the JDK's built-in support for Unix Domain Sockets on Java 16+
    
    * First draft of timeout test.
    
    * Add server to test.
    
    * Fix build.gradle file.
    
    * Add debugging statements.
    
    * Second draft of timeout test.
    
    * Update getInputStream to use a selector.
    
    * Clean code.
    
    * Adjust dd-java-agent build.gradle.
    
    * Revert dd-java-agent build.gradle change
    
    * Revert another build.gradle change.
    
    * Try specifying setSrcDirs.
    
    * Try changing compatibility version.
    
    * Revert previous two changes.
    
    * Avoid implementation dependency for Java17.
    
    * Make gradle dependency more specific and add testImplementation to socket-utils.
    
    * Add synchronization to ensure that server starts before client connects.
    
    * Try this...
    
    * Add print statements.
    
    * Add catch statement.
    
    * Refactor getInputStream and getOutputStream.
    
    * Address PR comments.
    
    * Add test for when timeout is 0.
    
    * Add config option.
    
    ---------
    
    Co-authored-by: Stuart McCulloch <stuart.mcculloch@datadoghq.com>

--- internal-api/src/main/java/datadog/trace/api/Config.java
+++ internal-api/src/main/java/datadog/trace/api/Config.java
@@ -563,6 +563,8 @@ public class Config {
   private final boolean apmTracingEnabled;
   private final Set<String> experimentalFeaturesEnabled;
 
+  private final boolean jdkSocketEnabled;
+
   // Read order: System Properties -> Env Variables, [-> properties file], [-> default value]
   private Config() {
     this(ConfigProvider.createDefault());
@@ -1958,6 +1960,8 @@ public class Config {
     }
 
     this.apmTracingEnabled = configProvider.getBoolean(GeneralConfig.APM_TRACING_ENABLED, true);
+
+    this.jdkSocketEnabled = configProvider.getBoolean(JDK_SOCKET_ENABLED, true);
 
     log.debug("New instance: {}", this);
   }
@@ -3537,6 +3541,10 @@ public class Config {
     return apmTracingEnabled;
   }
 
+  public boolean isJdkSocketEnabled() {
+    return jdkSocketEnabled;
+  }
+
   /** @return A map of tags to be applied only to the local application root span. */
   public Map<String, Object> getLocalRootSpanTags() {
     final Map<String, String> runtimeTags = getRuntimeTags();
@@ -4784,6 +4792,8 @@ public class Config {
         + dataJobsCommandPattern
         + ", apmTracingEnabled="
         + apmTracingEnabled
+        + ", jdkSocketEnabled="
+        + jdkSocketEnabled
         + ", cloudRequestPayloadTagging="
         + cloudRequestPayloadTagging
         + ", cloudResponsePayloadTagging="
