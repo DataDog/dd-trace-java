0d1f2f6970ed498a5db21f456f3c6e94f961b7c5
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-03-28 12:20:00 +0000

    Temporary bridge between contexts and spans/scopes

--- internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/NoopScope.java
+++ internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/NoopScope.java
@@ -1,9 +1,16 @@
 package datadog.trace.bootstrap.instrumentation.api;
 
+import datadog.context.Context;
+
 final class NoopScope implements AgentScope {
   static final NoopScope INSTANCE = new NoopScope();
 
   private NoopScope() {}
+
+  @Override
+  public Context context() {
+    return Context.root();
+  }
 
   @Override
   public AgentSpan span() {
