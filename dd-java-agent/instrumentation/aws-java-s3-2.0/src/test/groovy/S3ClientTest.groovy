57b86e901d1a6e8f4124dd4e665477f83f6730ed
Laplie Anderson <laplie.anderson@datadoghq.com>
2025-04-01 20:04:02 -0400

    more fixes

--- dd-java-agent/instrumentation/aws-java-s3-2.0/src/test/groovy/S3ClientTest.groovy
+++ dd-java-agent/instrumentation/aws-java-s3-2.0/src/test/groovy/S3ClientTest.groovy
@@ -92,8 +92,8 @@ class S3ClientTest extends AgentTestRunner {
             tag "bucketname", bucketName
             tag "http.method", "PUT"
             tag "http.status_code", 200
-            tag "http.url", { it.startsWith("http://localhost") && it.contains("/$key") }
-            tag "peer.hostname", "localhost"
+            tag "http.url", { it.startsWith("http://" + LOCALSTACK.getHost()) && it.contains("/$key") }
+            tag "peer.hostname", LOCALSTACK.getHost()
             tag "peer.port", { it instanceof Integer }
             tag "span.kind", "client"
             tag "aws.requestId", { it != null }
@@ -151,8 +151,8 @@ class S3ClientTest extends AgentTestRunner {
             tag "bucketname", bucketName
             tag "http.method", "PUT"
             tag "http.status_code", 200
-            tag "http.url", { it.startsWith("http://localhost") && it.contains("/$sourceKey") }
-            tag "peer.hostname", "localhost"
+            tag "http.url", { it.startsWith("http://" + LOCALSTACK.getHost()) && it.contains("/$sourceKey") }
+            tag "peer.hostname", LOCALSTACK.getHost()
             tag "peer.port", { it instanceof Integer }
             tag "span.kind", "client"
             tag "aws.requestId", { it != null }
@@ -185,8 +185,8 @@ class S3ClientTest extends AgentTestRunner {
             tag "bucketname", bucketName
             tag "http.method", "PUT"
             tag "http.status_code", 200
-            tag "http.url", { it.startsWith("http://localhost") && it.contains("/$destKey") }
-            tag "peer.hostname", "localhost"
+            tag "http.url", { it.startsWith("http://" + LOCALSTACK.getHost()) && it.contains("/$destKey") }
+            tag "peer.hostname", LOCALSTACK.getHost()
             tag "peer.port", { it instanceof Integer }
             tag "span.kind", "client"
             tag "aws.requestId", { it != null }
