b7d542738dca647382f3bd5917eff10b168ba01e
Laplie Anderson <laplie.anderson@datadoghq.com>
2025-04-03 19:37:23 -0400

    set available processors for jetty

--- dd-java-agent/testing/src/main/groovy/datadog/trace/agent/test/server/http/TestHttpServer.groovy
+++ dd-java-agent/testing/src/main/groovy/datadog/trace/agent/test/server/http/TestHttpServer.groovy
@@ -125,12 +125,6 @@ class TestHttpServer implements AutoCloseable {
         internalServer.addConnector(https)
 
         customizer.call(internalServer)
-
-        // Jetty uses a heuristic to set reserved threads that breaks in CI
-        // It depends on Runtime.getRuntime().availableProcessors() which is not always correct in kubernetes
-        // Set explicitly to a reasonable number
-        (internalServer.getThreadPool() as QueuedThreadPool).setReservedThreads(10)
-
         internalServer.start()
         // set after starting, otherwise two callbacks get added.
         internalServer.stopAtShutdown = true
