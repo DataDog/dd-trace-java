aa6a29ded49f7b509857ecd9ad1328f196ff53a4
Laplie Anderson <laplie.anderson@datadoghq.com>
2025-04-03 11:43:06 -0400

    try fixed value of parallelism

--- dd-smoke-tests/log-injection/src/test/resources/SpockConfig.groovy
+++ dd-smoke-tests/log-injection/src/test/resources/SpockConfig.groovy
@@ -1,5 +1,11 @@
 runner {
   parallel {
     enabled true
+
+    // Runtime.getRuntime().availableProcessors() is used to scale the parallelism by default
+    // but it returns weird values in Gitlab/kubernetes so fix the parallelism to a specific value
+    if (System.getenv("GITLAB_CI") != null) {
+      fixed(4)
+    }
   }
 }
