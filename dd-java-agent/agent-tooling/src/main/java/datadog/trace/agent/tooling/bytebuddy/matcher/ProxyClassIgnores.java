6405de349c467a43e72f5fe8014f75096a586809
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-04-07 16:19:52 +0200

    Use prefix trie for proxy ignores

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/bytebuddy/matcher/ProxyClassIgnores.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/bytebuddy/matcher/ProxyClassIgnores.java
@@ -5,16 +5,25 @@ public class ProxyClassIgnores {
   private ProxyClassIgnores() {}
 
   public static boolean isIgnored(String name) {
-    if (name.indexOf('$') > -1) {
-      if (name.contains("$JaxbAccessor")
-          || name.contains("CGLIB$$")
-          || name.contains("$__sisu")
-          || name.contains("$$EnhancerByGuice$$")
-          || name.contains("$$EnhancerByProxool$$")
-          || name.contains("$$$view")
-          || name.contains("$$$endpoint") // jboss mdb proxies
-          || name.contains("$$_Weld")
-          || name.contains("_$$_jvst")) {
+    int last = -1;
+    int idx;
+    while (true) {
+      idx = name.indexOf('$', last + 1);
+      if (idx < 0) {
+        break;
+      }
+      if (last < 0 && name.contains("CGLIB$$")) {
+        // check this once
+        return true;
+      }
+      if (idx == last + 1) {
+        // skip the trie if consecutive $$ since, to be efficient, we can match prefixes from the
+        // first dollar
+        last = idx;
+        continue;
+      }
+      last = idx;
+      if (ProxyIgnoredClassNameTrie.apply(name, idx) == 1) {
         return true;
       }
     }
