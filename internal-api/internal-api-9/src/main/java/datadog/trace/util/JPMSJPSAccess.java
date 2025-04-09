2ead3b7ab62505c65adc3c40c14ae86324a5175b
Matt <matthew.alp@datadoghq.com>
2025-04-09 16:12:39 -0400

    Use system class loader for 17+ JDK support

--- internal-api/internal-api-9/src/main/java/datadog/trace/util/JPMSJPSAccess.java
+++ internal-api/internal-api-9/src/main/java/datadog/trace/util/JPMSJPSAccess.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class JPMSJPSAccess {
   public static void patchModuleAccess(Instrumentation inst) {
-    Module unnamedModule = ClassLoader.getPlatformClassLoader().getUnnamedModule();
+    Module unnamedModule = ClassLoader.getSystemClassLoader().getUnnamedModule();
     Module jvmstatModule = ModuleLayer.boot().findModule("jdk.internal.jvmstat").orElse(null);
 
     if (jvmstatModule != null) {
