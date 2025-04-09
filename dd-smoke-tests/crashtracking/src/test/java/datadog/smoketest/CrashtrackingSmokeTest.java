561f192644ffbd0e8bcb145f80ee9883c69694f1
Laplie Anderson <laplie.anderson@datadoghq.com>
2025-04-09 12:19:13 -0400

    Make crash tracking smoke test more resiliant

--- dd-smoke-tests/crashtracking/src/test/java/datadog/smoketest/CrashtrackingSmokeTest.java
+++ dd-smoke-tests/crashtracking/src/test/java/datadog/smoketest/CrashtrackingSmokeTest.java
@@ -1,24 +1,33 @@
 package datadog.smoketest;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 import datadog.trace.api.Platform;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,39 +38,80 @@ import org.junit.jupiter.api.Test;
  * that ships with OS X by default.
  */
 public class CrashtrackingSmokeTest {
+  private static final long DATA_TIMEOUT_MS = 10 * 1000;
+  private static Path LOG_FILE_DIR;
   private MockWebServer tracingServer;
+  private TestUDPServer udpServer;
+  private BlockingQueue<CrashTelemetryData> crashEvents = new LinkedBlockingQueue<>();
 
   @BeforeAll
   static void setupAll() {
     // Only Hotspot based implementation are supported
     assumeFalse(Platform.isJ9());
+
+    LOG_FILE_DIR = Paths.get(System.getProperty("datadog.smoketest.builddir"), "reports");
   }
 
   private Path tempDir;
+  private static OutputThreads outputThreads = new OutputThreads();
 
   @BeforeEach
   void setup() throws Exception {
     tempDir = Files.createTempDirectory("dd-smoketest-");
 
+    crashEvents.clear();
+
+    Moshi moshi = new Moshi.Builder().build();
     tracingServer = new MockWebServer();
     tracingServer.setDispatcher(
         new Dispatcher() {
           @Override
           public MockResponse dispatch(final RecordedRequest request) throws InterruptedException {
+            String data = request.getBody().readString(StandardCharsets.UTF_8);
+
+            if ("/telemetry/proxy/api/v2/apmtelemetry".equals(request.getPath())) {
+              try {
+                JsonAdapter<MinimalTelemetryData> adapter =
+                    moshi.adapter(MinimalTelemetryData.class);
+                MinimalTelemetryData minimal = adapter.fromJson(data);
+                if ("logs".equals(minimal.request_type)) {
+                  JsonAdapter<CrashTelemetryData> crashAdapter =
+                      moshi.adapter(CrashTelemetryData.class);
+                  crashEvents.add(crashAdapter.fromJson(data));
+                }
+              } catch (IOException e) {
+                System.out.println("Unable to parse " + e);
+              }
+            }
+            System.out.println("URL ====== " + request.getPath());
+            System.out.println(data);
+
             return new MockResponse().setResponseCode(200);
           }
         });
-    //    tracingServer.start(8126);
+
+    udpServer = new TestUDPServer();
+    udpServer.start();
+
+    synchronized (outputThreads.testLogMessages) {
+      outputThreads.testLogMessages.clear();
+    }
   }
 
   @AfterEach
   void teardown() throws Exception {
     tracingServer.shutdown();
+    udpServer.close();
 
     try (Stream<Path> fileStream = Files.walk(tempDir)) {
       fileStream.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
     }
     Files.deleteIfExists(tempDir);
+  }
+
+  @AfterAll
+  static void shutdown() {
+    outputThreads.close();
   }
 
   private static String javaPath() {
@@ -108,52 +158,13 @@ public class CrashtrackingSmokeTest {
                 appShadowJar(),
                 script.toString()));
     pb.environment().put("DD_TRACE_AGENT_PORT", String.valueOf(tracingServer.getPort()));
-    StringBuilder stdoutStr = new StringBuilder();
-    StringBuilder stderrStr = new StringBuilder();
 
     Process p = pb.start();
-    Thread stdout =
-        new Thread(
-            () -> {
-              try (BufferedReader br =
-                  new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-                br.lines()
-                    .forEach(
-                        l -> {
-                          System.out.println(l);
-                          stdoutStr.append(l).append('\n');
-                        });
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
-    Thread stderr =
-        new Thread(
-            () -> {
-              try (BufferedReader br =
-                  new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
-                br.lines()
-                    .forEach(
-                        l -> {
-                          System.err.println(l);
-                          stderrStr.append(l).append('\n');
-                        });
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
-    stdout.setDaemon(true);
-    stderr.setDaemon(true);
-    stdout.start();
-    stderr.start();
+    outputThreads.captureOutput(
+        p, LOG_FILE_DIR.resolve("testProcess.testCrashTracking.log").toFile());
 
     assertNotEquals(0, p.waitFor(), "Application should have crashed");
-
-    assertThat(stdoutStr.toString(), containsString(" was uploaded successfully"));
-    assertThat(
-        stderrStr.toString(),
-        containsString(
-            "com.datadog.crashtracking.CrashUploader - Successfully uploaded the crash files"));
+    assertCrashData();
   }
 
   /*
@@ -183,52 +194,14 @@ public class CrashtrackingSmokeTest {
                 appShadowJar(),
                 script.toString()));
     pb.environment().put("DD_TRACE_AGENT_PORT", String.valueOf(tracingServer.getPort()));
-    StringBuilder stdoutStr = new StringBuilder();
-    StringBuilder stderrStr = new StringBuilder();
 
     Process p = pb.start();
-    Thread stdout =
-        new Thread(
-            () -> {
-              try (BufferedReader br =
-                  new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-                br.lines()
-                    .forEach(
-                        l -> {
-                          System.out.println(l);
-                          stdoutStr.append(l).append('\n');
-                        });
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
-    Thread stderr =
-        new Thread(
-            () -> {
-              try (BufferedReader br =
-                  new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
-                br.lines()
-                    .forEach(
-                        l -> {
-                          System.err.println(l);
-                          stderrStr.append(l).append('\n');
-                        });
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
-    stdout.setDaemon(true);
-    stderr.setDaemon(true);
-    stdout.start();
-    stderr.start();
+    outputThreads.captureOutput(
+        p, LOG_FILE_DIR.resolve("testProcess.testCrashTrackingLegacy.log").toFile());
 
     assertNotEquals(0, p.waitFor(), "Application should have crashed");
 
-    assertThat(stdoutStr.toString(), containsString(" was uploaded successfully"));
-    assertThat(
-        stderrStr.toString(),
-        containsString(
-            "com.datadog.crashtracking.CrashUploader - Successfully uploaded the crash files"));
+    assertCrashData();
   }
 
   /*
@@ -255,51 +228,14 @@ public class CrashtrackingSmokeTest {
                 "-jar",
                 appShadowJar(),
                 script.toString()));
-    StringBuilder stdoutStr = new StringBuilder();
-    StringBuilder stderrStr = new StringBuilder();
 
     Process p = pb.start();
-    Thread stdout =
-        new Thread(
-            () -> {
-              try (BufferedReader br =
-                  new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-                br.lines()
-                    .forEach(
-                        l -> {
-                          System.out.println(l);
-                          stdoutStr.append(l).append('\n');
-                        });
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
-    Thread stderr =
-        new Thread(
-            () -> {
-              try (BufferedReader br =
-                  new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
-                br.lines()
-                    .forEach(
-                        l -> {
-                          System.err.println(l);
-                          stderrStr.append(l).append('\n');
-                        });
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
-    stdout.setDaemon(true);
-    stderr.setDaemon(true);
-    stdout.start();
-    stderr.start();
+    outputThreads.captureOutput(
+        p, LOG_FILE_DIR.resolve("testProcess.testOomeTracking.log").toFile());
+    pb.environment().put("DD_DOGSTATSD_PORT", String.valueOf(udpServer.getPort()));
 
     assertNotEquals(0, p.waitFor(), "Application should have crashed");
-
-    assertThat(
-        stderrStr.toString(),
-        containsString("com.datadog.crashtracking.OOMENotifier - OOME event sent"));
-    assertThat(stdoutStr.toString(), containsString("OOME Event generated successfully"));
+    assertOOMEvent();
   }
 
   @Test
@@ -326,58 +262,33 @@ public class CrashtrackingSmokeTest {
                 appShadowJar(),
                 oomeScript.toString()));
     pb.environment().put("DD_TRACE_AGENT_PORT", String.valueOf(tracingServer.getPort()));
-    StringBuilder stdoutStr = new StringBuilder();
-    StringBuilder stderrStr = new StringBuilder();
+    pb.environment().put("DD_DOGSTATSD_PORT", String.valueOf(udpServer.getPort()));
 
     Process p = pb.start();
-    Thread stdout =
-        new Thread(
-            () -> {
-              try (BufferedReader br =
-                  new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-                br.lines()
-                    .forEach(
-                        l -> {
-                          System.out.println(l);
-                          stdoutStr.append(l).append('\n');
-                        });
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
-    Thread stderr =
-        new Thread(
-            () -> {
-              try (BufferedReader br =
-                  new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
-                br.lines()
-                    .forEach(
-                        l -> {
-                          System.err.println(l);
-                          stderrStr.append(l).append('\n');
-                        });
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
-    stdout.setDaemon(true);
-    stderr.setDaemon(true);
-    stdout.start();
-    stderr.start();
+    outputThreads.captureOutput(
+        p, LOG_FILE_DIR.resolve("testProcess.testCombineTracking.log").toFile());
 
     assertNotEquals(0, p.waitFor(), "Application should have crashed");
 
-    // Crash uploader did get triggered
-    assertThat(stdoutStr.toString(), containsString(" was uploaded successfully"));
-    assertThat(
-        stderrStr.toString(),
-        containsString(
-            "com.datadog.crashtracking.CrashUploader - Successfully uploaded the crash files"));
+    assertCrashData();
+    assertOOMEvent();
+  }
 
-    // OOME notifier did get triggered
-    assertThat(
-        stderrStr.toString(),
-        containsString("com.datadog.crashtracking.OOMENotifier - OOME event sent"));
-    assertThat(stdoutStr.toString(), containsString("OOME Event generated successfully"));
+  private void assertCrashData() throws InterruptedException {
+    CrashTelemetryData crashData = crashEvents.poll(DATA_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+    assertNotNull(crashData, "Crash data not uploaded");
+    assertThat(crashData.payload.get(0).message, containsString("OutOfMemory"));
+    assertThat(crashData.payload.get(0).tags, containsString("severity:crash"));
+  }
+
+  private void assertOOMEvent() throws InterruptedException {
+    byte[] data = udpServer.getMessages().poll(DATA_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+    assertNotNull(data, "OOM Event not received");
+    String event = new String(data);
+
+    assertThat(event, startsWith("_e"));
+    assertThat(event, containsString(":OutOfMemoryError"));
+    assertThat(event, containsString("t:error"));
+    assertThat(event, containsString("s:java"));
   }
 }
