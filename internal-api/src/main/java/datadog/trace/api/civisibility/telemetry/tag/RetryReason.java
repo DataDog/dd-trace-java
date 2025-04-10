775b098931704096fc78c01e9fd23bf72ad85bbf
Daniel Mohedano <daniel.mohedano@datadoghq.com>
2025-04-10 11:00:38 +0200

    update test.retry_reason to use full name of the feature

--- internal-api/src/main/java/datadog/trace/api/civisibility/telemetry/tag/RetryReason.java
+++ internal-api/src/main/java/datadog/trace/api/civisibility/telemetry/tag/RetryReason.java
@@ -3,8 +3,8 @@ package datadog.trace.api.civisibility.telemetry.tag;
 import datadog.trace.api.civisibility.telemetry.TagValue;
 
 public enum RetryReason implements TagValue {
-  atr("Auto Test Retries"),
-  efd("Early Flakiness Detection"),
+  atr("Auto Test Retries", "auto_test_retry"),
+  efd("Early Flakiness Detection", "early_flake_detection"),
   attemptToFix("Attempt to Fix", "attempt_to_fix");
 
   private final String value;
