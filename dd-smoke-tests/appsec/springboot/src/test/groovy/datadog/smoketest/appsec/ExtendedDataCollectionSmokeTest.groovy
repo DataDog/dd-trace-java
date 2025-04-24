34bfd966d6666ed3b350669d1a7a1c10ff860e0a
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2025-04-24 08:34:31 +0200

    WIP

--- dd-smoke-tests/appsec/springboot/src/test/groovy/datadog/smoketest/appsec/ExtendedDataCollectionSmokeTest.groovy
+++ dd-smoke-tests/appsec/springboot/src/test/groovy/datadog/smoketest/appsec/ExtendedDataCollectionSmokeTest.groovy
@@ -36,7 +36,11 @@ class ExtendedDataCollectionSmokeTest extends AbstractAppSecServerSmokeTest {
     def client = OkHttpUtils.clientBuilder().build()
     def request = new Request.Builder()
       .url(url)
-      .addHeader('X-My-Header', "value")
+      .addHeader('X-My-Header-1', "value1")
+      .addHeader('X-My-Header-2', "value2")
+      .addHeader('X-My-Header-3', "value3")
+      .addHeader('X-My-Header-4', "value4")
+      .addHeader('Content-Type', "text/html")
       .get()
       .build()
 
@@ -53,6 +57,11 @@ class ExtendedDataCollectionSmokeTest extends AbstractAppSecServerSmokeTest {
     def rootSpans = this.rootSpans.toList()
     rootSpans.size() == 1
     def rootSpan = rootSpans[0]
-    rootSpan.meta.get('http.request.headers.x-my-header') == 'value'
+    rootSpan.meta.get('http.request.headers.x-my-header-1') == 'value1'
+    rootSpan.meta.get('http.request.headers.x-my-header-2') == 'value2'
+    rootSpan.meta.get('http.request.headers.x-my-header-3') == 'value3'
+    rootSpan.meta.get('http.request.headers.x-my-header-4') == 'value4'
+    rootSpan.meta.get('http.request.headers.content-type') == 'text/html'
+    !rootSpan.meta.containsKey('_dd.appsec.request.header_collection.discarded')
   }
 }
