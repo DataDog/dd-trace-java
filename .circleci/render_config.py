be86ea80edf58b4ded3349de163ea2209d342dd4
Sarah Chen <sarah.chen@datadoghq.com>
2025-04-24 11:36:46 -0400

    Add 24 to circleci render_config

--- .circleci/render_config.py
+++ .circleci/render_config.py
@@ -15,7 +15,7 @@ OUT_FILENAME = "config.continue.yml"
 GENERATED_CONFIG_PATH = os.path.join(SCRIPT_DIR, OUT_FILENAME)
 
 # JDKs that will run on every pipeline.
-ALWAYS_ON_JDKS = {"8", "17", "21"}
+ALWAYS_ON_JDKS = {"8", "17", "21", "24"}
 # And these will run only in master and release/ branches.
 MASTER_ONLY_JDKS = {
     "11",
