f9c53d2542599d40207525def12742a8136e5aa0
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-05-02 10:08:29 +0200

    feat(core): Add non ASCII character baggage extraction

--- dd-trace-core/src/test/groovy/datadog/trace/core/baggage/BaggagePropagatorTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/core/baggage/BaggagePropagatorTest.groovy
@@ -9,6 +9,8 @@ import datadog.trace.test.util.DDSpecification
 
 import java.util.function.BiConsumer
 
+import static datadog.trace.api.ConfigDefaults.DEFAULT_TRACE_BAGGAGE_MAX_BYTES
+import static datadog.trace.api.ConfigDefaults.DEFAULT_TRACE_BAGGAGE_MAX_ITEMS
 import static datadog.trace.core.baggage.BaggagePropagator.BAGGAGE_KEY
 
 class BaggagePropagatorTest extends DDSpecification {
@@ -33,7 +35,7 @@ class BaggagePropagatorTest extends DDSpecification {
   }
 
   def setup() {
-    this.propagator = new BaggagePropagator(true, true)
+    this.propagator = new BaggagePropagator(true, true, DEFAULT_TRACE_BAGGAGE_MAX_ITEMS, DEFAULT_TRACE_BAGGAGE_MAX_BYTES)
     this.setter = new MapCarrierAccessor()
     this.carrier = [:]
     this.context = Context.root()
@@ -63,8 +65,7 @@ class BaggagePropagatorTest extends DDSpecification {
 
   def "test baggage inject item limit"() {
     setup:
-    injectSysConfig("trace.baggage.max.items", '2')
-    propagator = new BaggagePropagator(true, true) //creating a new instance after injecting config
+    propagator = new BaggagePropagator(true, true, 2, DEFAULT_TRACE_BAGGAGE_MAX_BYTES) //creating a new instance after injecting config
     context = Baggage.create(baggage).storeInto(context)
 
     when:
@@ -81,8 +82,7 @@ class BaggagePropagatorTest extends DDSpecification {
 
   def "test baggage inject bytes limit"() {
     setup:
-    injectSysConfig("trace.baggage.max.bytes", '20')
-    propagator = new BaggagePropagator(true, true) //creating a new instance after injecting config
+    propagator = new BaggagePropagator(true, true, DEFAULT_TRACE_BAGGAGE_MAX_ITEMS, 20) //creating a new instance after injecting config
     context = Baggage.create(baggage).storeInto(context)
 
     when:
@@ -114,6 +114,30 @@ class BaggagePropagatorTest extends DDSpecification {
     baggageHeader                                                      | baggageMap
     "key1=val1,key2=val2,foo=bar"                                      | ["key1": "val1", "key2": "val2", "foo": "bar"]
     "%22%2C%3B%5C%28%29%2F%3A%3C%3D%3E%3F%40%5B%5D%7B%7D=%22%2C%3B%5C" | ['",;\\()/:<=>?@[]{}': '",;\\']
+  }
+
+  def "test extracting non ASCII headers"() {
+    setup:
+    def headers = [
+      (BAGGAGE_KEY) : "key1=vallée,clé2=value",
+    ]
+
+    when:
+    context = this.propagator.extract(context, headers, ContextVisitors.stringValuesMap())
+    def baggage = Baggage.fromContext(context)
+
+    then: 'non ASCII values data are still accessible as part of the API'
+    baggage != null
+    baggage.asMap().get('key1') == 'vallée'
+    baggage.asMap().get('clé2') == 'value'
+    baggage.w3cHeader == null
+
+
+    when:
+    this.propagator.inject(Context.root().with(baggage), carrier, setter)
+
+    then: 'baggage are URL encoded if not valid, even if not modified'
+    assert carrier[BAGGAGE_KEY] == 'key1=vall%C3%A9e,cl%C3%A92=value'
   }
 
   def "extract invalid baggage headers"() {
@@ -160,8 +184,7 @@ class BaggagePropagatorTest extends DDSpecification {
 
   def "test baggage cache items limit"(){
     setup:
-    injectSysConfig("trace.baggage.max.items", "2")
-    propagator = new BaggagePropagator(true, true) //creating a new instance after injecting config
+    propagator = new BaggagePropagator(true, true, 2, DEFAULT_TRACE_BAGGAGE_MAX_BYTES) //creating a new instance after injecting config
     def headers = [
       (BAGGAGE_KEY) : baggageHeader,
     ]
@@ -182,8 +205,7 @@ class BaggagePropagatorTest extends DDSpecification {
 
   def "test baggage cache bytes limit"(){
     setup:
-    injectSysConfig("trace.baggage.max.bytes", "20")
-    propagator = new BaggagePropagator(true, true) //creating a new instance after injecting config
+    propagator = new BaggagePropagator(true, true, DEFAULT_TRACE_BAGGAGE_MAX_ITEMS, 20) //creating a new instance after injecting config
     def headers = [
       (BAGGAGE_KEY) : baggageHeader,
     ]
