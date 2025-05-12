44ea4e64c997ced60e75fa6db5bbb8b6936762dd
Matt <matthew.alp@datadoghq.com>
2025-05-12 09:06:21 -0400

    Add a comment documenting how JVM processes are enumerated
    Co-authored-by: Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>

--- internal-api/src/main/java/datadog/trace/util/PidHelper.java
+++ internal-api/src/main/java/datadog/trace/util/PidHelper.java
@@ -99,6 +99,8 @@ public final class PidHelper {
     // Some JDKs don't have jvmstat available as a module, attempt to read from the hsperfdata
     // directory instead
     try (Stream<Path> stream =
+       // Emulating the hotspot way to enumerate the JVM processes using the perfdata file
+       // https://github.com/openjdk/jdk/blob/d7cb933b89839b692f5562aeeb92076cd25a99f6/src/hotspot/share/runtime/perfMemory.cpp#L244
         Files.list(Paths.get(getOSTempDir() + "/hsperfdata_" + System.getProperty("user.name")))) {
       return stream
           .filter(file -> !Files.isDirectory(file))
