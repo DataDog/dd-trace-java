d227938edfb4685353859df5f11aae706a67c10b
Matthew Li <matthew.li@datadoghq.com>
2025-05-14 15:16:02 -0400

    updating support for akka

--- dd-java-agent/instrumentation/akka-http/akka-http-10.0/src/main/java/datadog/trace/instrumentation/akkahttp/AkkaHttpSingleRequestInstrumentation.java
+++ dd-java-agent/instrumentation/akka-http/akka-http-10.0/src/main/java/datadog/trace/instrumentation/akkahttp/AkkaHttpSingleRequestInstrumentation.java
@@ -21,6 +21,7 @@ import datadog.trace.agent.tooling.InstrumenterModule;
 import datadog.trace.api.datastreams.DataStreamsContext;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.Java8BytecodeBridge;
 import net.bytebuddy.asm.Advice;
 import scala.concurrent.Future;
 
@@ -81,7 +82,7 @@ public final class AkkaHttpSingleRequestInstrumentation extends InstrumenterModu
 
       if (request != null) {
         DataStreamsContext dsmContext = DataStreamsContext.fromTags(CLIENT_PATHWAY_EDGE_TAGS);
-        defaultPropagator().inject(Context.current().with(span).with(dsmContext), request, headers);
+        defaultPropagator().inject(Java8BytecodeBridge.getCurrentContext().with(span).with(dsmContext), request, headers);
         // Request is immutable, so we have to assign new value once we update headers
         request = headers.getRequest();
       }
