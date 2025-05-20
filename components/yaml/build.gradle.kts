986c3d62b444933e22e64ffc40d32585e9a83c76
Brice Dutheil <brice.dutheil@gmail.com>
2025-05-20 14:56:29 +0200

    chore: remove jmh from projects that don't have any benchmarks

--- components/yaml/build.gradle.kts
+++ components/yaml/build.gradle.kts
@@ -1,12 +1,4 @@
-plugins {
-  id("me.champeau.jmh")
-}
-
 apply(from = "$rootDir/gradle/java.gradle")
-
-jmh {
-  version = "1.28"
-}
 
 // https://repo1.maven.org/maven2/org/yaml/snakeyaml/2.4/snakeyaml-2.4.pom
 dependencies {
