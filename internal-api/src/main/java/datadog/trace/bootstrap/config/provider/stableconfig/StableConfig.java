24f89bce264f5600585cbaede5fe401576fb336c
Yury Gribkov <yury.gribkov@gmail.com>
2025-05-21 14:59:03 +0200

    Fix broken tests

--- internal-api/src/main/java/datadog/trace/bootstrap/config/provider/stableconfig/StableConfig.java
+++ internal-api/src/main/java/datadog/trace/bootstrap/config/provider/stableconfig/StableConfig.java
@@ -1,10 +1,12 @@
 package datadog.trace.bootstrap.config.provider.stableconfig;
 
 import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
 import static java.util.Collections.unmodifiableList;
 import static java.util.Collections.unmodifiableMap;
 import static java.util.stream.Collectors.toList;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,11 +20,18 @@ public final class StableConfig {
     this.configId = String.valueOf(map.get("config_id"));
     this.apmConfigurationDefault =
         unmodifiableMap(
-            (Map<String, Object>) map.getOrDefault("apm_configuration_default", emptyList()));
+            (Map<String, Object>) map.getOrDefault("apm_configuration_default", emptyMap()));
     this.apmConfigurationRules =
         unmodifiableList(
             ((List<Object>) map.getOrDefault("apm_configuration_rules", emptyList()))
                 .stream().map(Rule::new).collect(toList()));
+  }
+
+  // test only
+  private StableConfig(String configId, Map<String, Object> apmConfigurationDefault) {
+    this.configId = configId;
+    this.apmConfigurationDefault = apmConfigurationDefault;
+    this.apmConfigurationRules = new ArrayList<>();
   }
 
   public String getConfigId() {
