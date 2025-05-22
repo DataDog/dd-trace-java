956674ce8c3ea02f5877be8edc2f2cc24e05018d
Laplie Anderson <laplie.anderson@datadoghq.com>
2025-05-22 10:49:59 +0200

    Remove CircleCI

--- .circleci/cancel_workflow.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-echo "Cancelling workflow ${CIRCLE_WORKFLOW_ID}"
-curl --request POST --url https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/cancel --header "Circle-Token: ${CIRCLE_TOKEN}"
--- .circleci/no_circleci_changes.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-git diff --name-only "$1" | grep --invert-match --quiet -E '^(.gitlab-ci.yml|.gitlab)'
