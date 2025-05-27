a1bba89acad3033ea3abd5bf69eaf0b957127034
Mikayla Toffler <tofflerm@gmail.com>
2025-05-27 12:26:51 -0400

    Remove noisy debug logs on UNDEFINED case

--- internal-api/src/main/java/datadog/trace/bootstrap/config/provider/StableConfigParser.java
+++ internal-api/src/main/java/datadog/trace/bootstrap/config/provider/StableConfigParser.java
@@ -244,8 +244,6 @@ public class StableConfigParser {
       }
       String value = System.getenv(envVar.toUpperCase());
       if (value == null || value.isEmpty()) {
-        log.debug(
-            "Environment variable specified in template {} has no value", envVar.toUpperCase());
         return UNDEFINED_VALUE;
       }
       return value;
@@ -263,7 +261,6 @@ public class StableConfigParser {
       }
       String value = System.getProperty(processArg.substring(2));
       if (value == null || value.isEmpty()) {
-        log.debug("Process argument specified in template {} has no value", processArg);
         return UNDEFINED_VALUE;
       }
       return value;
