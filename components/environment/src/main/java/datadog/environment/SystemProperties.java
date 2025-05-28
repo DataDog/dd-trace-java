b9dc925efa524cce4a8c61cd440994435fc9f380
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-05-28 07:05:35 +0200

    feat(env): Introduce environment component

--- components/environment/src/main/java/datadog/environment/SystemProperties.java
+++ components/environment/src/main/java/datadog/environment/SystemProperties.java
@@ -1,3 +1,27 @@
 package datadog.environment;
 
-public class SystemProperties {}
+/** Safely queries system properties. */
+public final class SystemProperties {
+  private SystemProperties() {}
+
+  public static String get(String property) {
+    return getOrDefault(property, null);
+  }
+
+  public static String getOrDefault(String property, String defaultValue) {
+    try {
+      return System.getProperty(property, defaultValue);
+    } catch (SecurityException ignored) {
+      return defaultValue;
+    }
+  }
+
+  public static boolean set(String property, String value) {
+    try {
+      System.setProperty(property, value);
+      return true;
+    }  catch (SecurityException ignored) {
+      return false;
+    }
+  }
+}
