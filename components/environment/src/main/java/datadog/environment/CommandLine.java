769f066589ad51cf29bdf8137450e0f5701dd263
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-05-30 08:37:21 +0200

    feat(env): Improve command line support and tests

--- components/environment/src/main/java/datadog/environment/CommandLine.java
+++ components/environment/src/main/java/datadog/environment/CommandLine.java
@@ -2,9 +2,11 @@ package datadog.environment;
 
 import static datadog.environment.OperatingSystem.isLinux;
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 
 import de.thetaphi.forbiddenapis.SuppressForbidden;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.reflect.Field;
 import java.nio.file.Files;
@@ -12,13 +14,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-/** Fetches and captures the JVM runtime CLI arguments. */
+/** Fetches and captures the JVM options and command arguments. */
 class CommandLine {
-  static final List<String> VM_ARGUMENTS = findVmArgs();
+  static final List<String> VM_OPTIONS = findVmOptions();
+  static final String[] FULL_CMD = findFullCommand();
+  static final String CMD = getCommand();
+  static final List<String> CMD_ARGUMENTS = getCommandArguments();
 
   @SuppressForbidden
-  private static List<String> findVmArgs() {
+  private static List<String> findVmOptions() {
     // Try ProcFS on Linux
     try {
       if (isLinux()) {
@@ -75,5 +82,46 @@ class CommandLine {
       System.err.println("WARNING: Unable to get VM args using managed beans");
     }
     return emptyList();
+  }
+
+  private static String[] findFullCommand() {
+    // Besides "sun.java.command" property is not an standard, all main JDKs has set this
+    // property.
+    // Tested on:
+    // - OracleJDK, OpenJDK, AdoptOpenJDK, IBM JDK, Azul Zulu JDK, Amazon Coretto JDK
+    String command = SystemProperties.getOrDefault("sun.java.command", "").trim();
+    if (command.isEmpty()) {
+      return new String[0];
+    }
+    return command.split(" ");
+  }
+
+  static String getCommand() {
+    return FULL_CMD.length == 0 ? null : FULL_CMD[0];
+  }
+
+  static List<String> getCommandArguments() {
+    if (FULL_CMD.length == 0) {
+      return emptyList();
+    } else {
+      return Arrays.asList(FULL_CMD).subList(1, FULL_CMD.length);
+    }
+  }
+
+  private static List<String> getArgumentsFromFile(String argFile) {
+    String filename = argFile.substring(1);
+    Path path = Paths.get(filename);
+    if (!Files.exists(path) || !Files.isReadable(path)) {
+      return singletonList(argFile);
+    }
+    try (Stream<String> lines = Files.lines(path)) {
+      return lines.flatMap(CommandLine::getArgumentFromLine).collect(Collectors.toList());
+    } catch (IOException e) {
+      return singletonList(argFile);
+    }
+  }
+
+  private static Stream<String> getArgumentFromLine(String line) {
+    return Stream.of(line.split("[ \\t\\n\\r\\f]"));
   }
 }
