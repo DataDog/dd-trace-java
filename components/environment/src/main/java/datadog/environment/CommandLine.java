4a5434015ea4c116fb0818c9c98a36dadafad586
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-05-30 10:54:29 +0200

    feat(env): Improve procfs support

--- components/environment/src/main/java/datadog/environment/CommandLine.java
+++ components/environment/src/main/java/datadog/environment/CommandLine.java
@@ -12,32 +12,57 @@ import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 /** Fetches and captures the JVM options and command arguments. */
 class CommandLine {
+  private static final String[] PROCFS_CMDLINE = readProcFsCmdLine();
+  private static final List<String> FULL_CMD = findFullCommand();
   static final List<String> VM_OPTIONS = findVmOptions();
-  static final String[] FULL_CMD = findFullCommand();
   static final String CMD = getCommand();
   static final List<String> CMD_ARGUMENTS = getCommandArguments();
 
-  @SuppressForbidden
-  private static List<String> findVmOptions() {
-    // Try ProcFS on Linux
-    try {
-      if (isLinux()) {
+  @SuppressForbidden // split on single-character uses fast path
+  private static String[] readProcFsCmdLine() {
+    if (!isLinux()) {
+      try {
         Path cmdlinePath = Paths.get("/proc/self/cmdline");
         if (Files.exists(cmdlinePath) && Files.isReadable(cmdlinePath)) {
           try (BufferedReader in = Files.newBufferedReader(cmdlinePath)) {
-            return Arrays.asList(in.readLine().split("\0"));
+            return in.readLine().split("\0");
           }
         }
+      } catch (Throwable ignored) {
       }
-    } catch (Throwable ignored) {
-      // Ignored exception
+    }
+    return null;
+  }
+
+  @SuppressForbidden // Class.forName() as backup
+  private static List<String> findVmOptions() {
+    // Try ProcFS on Linux
+    if (PROCFS_CMDLINE != null) {
+      // Start at 1 to skip "java" command itself
+      int index = 1;
+      // Look for main class or "-jar", end of VM options
+      for (; index < PROCFS_CMDLINE.length; index++) {
+        if (!PROCFS_CMDLINE[index].startsWith("-") || "-jar".equals(PROCFS_CMDLINE[index])) {
+          break;
+        }
+      }
+      String[] vmOptions = new String[index - 1];
+      System.arraycopy(PROCFS_CMDLINE, 1, vmOptions, 0, vmOptions.length);
+
+      // TODO JAVA_TOOLS_OPTIONS
+      //      String javaToolOptions = EnvironmentVariables.getOrDefault("JAVA_TOOL_OPTIONS", null);
+      //      if (javaToolOptions != null) {
+      //        parts.addAll(0, Arrays.asList(javaToolOptions.split(" ")));
+      //      }
+      // TODO ARGFILES
+      return Arrays.asList(vmOptions);
     }
 
     // Try Oracle-based
@@ -84,27 +109,25 @@ class CommandLine {
     return emptyList();
   }
 
-  private static String[] findFullCommand() {
+  @SuppressForbidden // split on single-character uses fast path
+  private static List<String> findFullCommand() {
     // Besides "sun.java.command" property is not an standard, all main JDKs has set this
     // property.
     // Tested on:
     // - OracleJDK, OpenJDK, AdoptOpenJDK, IBM JDK, Azul Zulu JDK, Amazon Coretto JDK
     String command = SystemProperties.getOrDefault("sun.java.command", "").trim();
-    if (command.isEmpty()) {
-      return new String[0];
-    }
-    return command.split(" ");
+    return command.isEmpty() ? emptyList() : Arrays.asList(command.split(" "));
   }
 
-  static String getCommand() {
-    return FULL_CMD.length == 0 ? null : FULL_CMD[0];
+  private static String getCommand() {
+    return FULL_CMD.isEmpty() ? null : FULL_CMD.get(0);
   }
 
-  static List<String> getCommandArguments() {
-    if (FULL_CMD.length == 0) {
-      return emptyList();
+  private static List<String> getCommandArguments() {
+    if (FULL_CMD.isEmpty()) {
+      return FULL_CMD;
     } else {
-      return Arrays.asList(FULL_CMD).subList(1, FULL_CMD.length);
+      return FULL_CMD.subList(1, FULL_CMD.size());
     }
   }
 
@@ -114,14 +137,27 @@ class CommandLine {
     if (!Files.exists(path) || !Files.isReadable(path)) {
       return singletonList(argFile);
     }
-    try (Stream<String> lines = Files.lines(path)) {
-      return lines.flatMap(CommandLine::getArgumentFromLine).collect(Collectors.toList());
+    List<String> args = new ArrayList<>();
+    try {
+      for (String line : Files.readAllLines(path)) {
+        // Use default delimiters that matches argfiles separator specification
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+          args.add(tokenizer.nextToken());
+        }
+      }
+      return args;
     } catch (IOException e) {
       return singletonList(argFile);
     }
   }
 
-  private static Stream<String> getArgumentFromLine(String line) {
-    return Stream.of(line.split("[ \\t\\n\\r\\f]"));
+  private static List<String> split(String str, String delimiter) {
+    List<String> parts = new ArrayList<>();
+    StringTokenizer tokenizer = new StringTokenizer(str, delimiter);
+    while (tokenizer.hasMoreTokens()) {
+      parts.add(tokenizer.nextToken());
+    }
+    return parts;
   }
 }
