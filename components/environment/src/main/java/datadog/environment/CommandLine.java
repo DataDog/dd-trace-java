6b2062577c4e8a204b77cf95526c3d68f8b40fa0
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-06-04 08:23:35 +0200

    feat(environment): Add more Javadoc

--- components/environment/src/main/java/datadog/environment/CommandLine.java
+++ components/environment/src/main/java/datadog/environment/CommandLine.java
@@ -6,31 +6,40 @@ import de.thetaphi.forbiddenapis.SuppressForbidden;
 import java.util.Arrays;
 import java.util.List;
 
-/** Fetches and captures the command line, both command and its arguments. */
+/**
+ * Fetches and captures the command line, both command and its arguments. It relies on a
+ * non-standard {@code sun.java.command} system property and was tested on:
+ *
+ * <ul>
+ *   <li>OracleJDK,
+ *   <li>OpenJDK,
+ *   <li>Temurin based JDK,
+ *   <li>IMB JDK,
+ *   <li>Azul Zulu,
+ *   <li>Amazon Coretto,
+ * </ul>
+ */
 class CommandLine {
-  final List<String> FULL_CMD = findFullCommand();
-  final String CMD = getCommand();
-  final List<String> CMD_ARGUMENTS = getCommandArguments();
+  private static final String SUN_JAVA_COMMAND_PROPERTY = "sun.java.command";
+  final List<String> fullCommand = findFullCommand();
+  final String command = getCommand();
+  final List<String> arguments = getCommandArguments();
 
   @SuppressForbidden // split on single-character uses fast path
   private List<String> findFullCommand() {
-    // Besides "sun.java.command" property is not an standard, all main JDKs has set this
-    // property.
-    // Tested on:
-    // - OracleJDK, OpenJDK, AdoptOpenJDK, IBM JDK, Azul Zulu JDK, Amazon Coretto JDK
-    String command = SystemProperties.getOrDefault("sun.java.command", "").trim();
+    String command = SystemProperties.getOrDefault(SUN_JAVA_COMMAND_PROPERTY, "").trim();
     return command.isEmpty() ? emptyList() : Arrays.asList(command.split(" "));
   }
 
   private String getCommand() {
-    return FULL_CMD.isEmpty() ? null : FULL_CMD.get(0);
+    return fullCommand.isEmpty() ? null : fullCommand.get(0);
   }
 
   private List<String> getCommandArguments() {
-    if (FULL_CMD.isEmpty()) {
-      return FULL_CMD;
+    if (fullCommand.isEmpty()) {
+      return fullCommand;
     } else {
-      return FULL_CMD.subList(1, FULL_CMD.size());
+      return fullCommand.subList(1, fullCommand.size());
     }
   }
 }
