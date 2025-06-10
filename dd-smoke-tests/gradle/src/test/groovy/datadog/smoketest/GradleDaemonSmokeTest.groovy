3853c65296a7a59cc6886e0c588d8d1ceb3d77c1
Nikita Tkachenko <121111529+nikita-tkachenko-datadog@users.noreply.github.com>
2025-06-10 18:37:04 +0200

    Fix flaky Gradle Daemon instrumentation smoke test (#8955)

--- dd-smoke-tests/gradle/src/test/groovy/datadog/smoketest/GradleDaemonSmokeTest.groovy
+++ dd-smoke-tests/gradle/src/test/groovy/datadog/smoketest/GradleDaemonSmokeTest.groovy
@@ -50,7 +50,7 @@ class GradleDaemonSmokeTest extends AbstractGradleTest {
 
     where:
     gradleVersion | projectName                                        | successExpected | expectedTraces | expectedCoverages
-    "3.0"         | "test-succeed-old-gradle"                          | true            | 5              | 1
+    "3.5"         | "test-succeed-old-gradle"                          | true            | 5              | 1
     "7.6.4"       | "test-succeed-legacy-instrumentation"              | true            | 5              | 1
     "7.6.4"       | "test-succeed-multi-module-legacy-instrumentation" | true            | 7              | 2
     "7.6.4"       | "test-succeed-multi-forks-legacy-instrumentation"  | true            | 6              | 2
@@ -244,6 +244,7 @@ class GradleDaemonSmokeTest extends AbstractGradleTest {
       .withProjectDir(projectFolder.toFile())
       .withGradleVersion(gradleVersion)
       .withArguments(arguments)
+      .withEnvironment(["GRADLE_VERSION": gradleVersion])
       .forwardOutput()
 
     println "${new Date()}: $specificationContext.currentIteration.displayName - Starting Gradle run"
