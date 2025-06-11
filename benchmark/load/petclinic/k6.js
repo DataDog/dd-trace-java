7245a767c0f142be5ff4d0bf1dad96209ec0de31
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2025-06-11 17:37:20 +0200

    wip: Force trigger Gitlab CI

--- benchmark/load/petclinic/k6.js
+++ benchmark/load/petclinic/k6.js
@@ -4,7 +4,7 @@ import {checkResponse, isOk} from "../../utils/k6.js";
 const baseUrl = 'http://localhost:8080';
 
 export const options = {
-  executor: 'shared-iterations', // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
+  executor: 'shared-iterations',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
   discardResponseBodies: true,
   vus: 5,
   iterations: 80000
