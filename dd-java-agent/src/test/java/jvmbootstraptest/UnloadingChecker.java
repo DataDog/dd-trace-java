6df1615e3ba32d9f16405af4ece35a3268b3b917
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-06-11 12:18:36 +0200

    Try to improve the class unloading test

--- dd-java-agent/src/test/java/jvmbootstraptest/UnloadingChecker.java
+++ dd-java-agent/src/test/java/jvmbootstraptest/UnloadingChecker.java
@@ -1,11 +1,22 @@
 package jvmbootstraptest;
 
-import datadog.trace.test.util.GCUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UnloadingChecker {
+
   public static void main(final String[] args) {
+    final Thread t =
+        new Thread(
+            () -> {
+              final List<byte[]> list = new ArrayList<>();
+              while (true) {
+                list.add(new byte[100 * 1024 * 1024]);
+              }
+            });
+    t.start();
     try {
-      GCUtils.awaitGC();
+      t.join(30_000);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
