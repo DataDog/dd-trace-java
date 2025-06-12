224d9dd33b4ccccc3f1a0d00666ac6c9a9faaeb1
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2025-06-12 15:49:42 +0200

    fix: Fix k6 config

--- benchmark/load/petclinic/k6.js
+++ benchmark/load/petclinic/k6.js
@@ -4,18 +4,19 @@ import {checkResponse, isOk} from "../../utils/k6.js";
 const baseUrl = 'http://localhost:8080';
 
 export const options = {
-  warmup: {
-    discardResponseBodies: true,
-    executor: 'constant-vus',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
-    vus: 5,
-    duration: '15s',
-  },
-  high_load: {
-    discardResponseBodies: true,
-    executor: 'constant-vus',
-    vus: 5,
-    duration: '20s',
-  },
+  discardResponseBodies: true,
+  scenarios: {
+    warmup: {
+      executor: 'constant-vus',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
+      vus: 5,
+      duration: '15s',
+    },
+    high_load: {
+      executor: 'constant-vus',
+      vus: 5,
+      duration: '20s',
+    },
+  }
 };
 
 export default function () {
