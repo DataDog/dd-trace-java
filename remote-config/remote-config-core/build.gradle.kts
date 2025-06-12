8bd7dd9f7b512da10de628bedd89bb0cf12921b6
Brice Dutheil <brice.dutheil@gmail.com>
2025-06-12 17:39:59 +0200

    chore(gradle): Convert :remote-config project to kotlin dsl

---
+++ remote-config/remote-config-core/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+  `java-library`
+}
+
+apply(from = "$rootDir/gradle/java.gradle")
+
+val minimumBranchCoverage by extra(0.6)
+val minimumInstructionCoverage by extra(0.8)
+val excludedClassesCoverage by extra(
+  listOf(
+    // not used yet
+    "datadog.remoteconfig.tuf.RemoteConfigRequest.ClientInfo.AgentInfo",
+    // only half the adapter interface used
+    "datadog.remoteconfig.tuf.InstantJsonAdapter",
+    // idem
+    "datadog.remoteconfig.tuf.RawJsonAdapter",
+    "datadog.remoteconfig.ExceptionHelper",
+  )
+)
+val excludedClassesBranchCoverage by extra(
+  listOf(
+    "datadog.remoteconfig.tuf.FeaturesConfig",
+    "datadog.remoteconfig.PollerRequestFactory",
+  )
+)
+val excludedClassesInstructionCoverage by extra(
+  listOf(
+    "datadog.remoteconfig.ConfigurationChangesListener.PollingHinterNoop",
+  )
+)
+
+dependencies {
+  api(project(":remote-config:remote-config-api"))
+
+  implementation(libs.slf4j)
+  implementation(libs.okhttp)
+  implementation(libs.moshi)
+  implementation(libs.bundles.cafe.crypto)
+
+  implementation(project(":internal-api"))
+
+  testImplementation(project(":utils:test-utils"))
+}
