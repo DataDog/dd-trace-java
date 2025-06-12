15ed68a7a74494e45022fa0882bd508aaab6d07e
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2025-06-12 12:34:24 +0200

    wip: Generalize affinities, export k6 results, change executor

--- benchmark/load/petclinic/k6.js
+++ benchmark/load/petclinic/k6.js
@@ -4,10 +4,10 @@ import {checkResponse, isOk} from "../../utils/k6.js";
 const baseUrl = 'http://localhost:8080';
 
 export const options = {
-  executor: 'shared-iterations',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
+  executor: 'constant-vus',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
   discardResponseBodies: true,
   vus: 5,
-  iterations: 80000
+  duration: '2m',
 };
 
 export default function () {
