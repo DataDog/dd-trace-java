4ec4ad74396a19e0807215725e1f40e4a1b6c3de
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2025-06-13 15:51:01 +0200

    fix: Fix local runs, reduce warmup duration

--- benchmark/load/insecure-bank/k6.js
+++ benchmark/load/insecure-bank/k6.js
@@ -9,15 +9,15 @@ export const options = {
     [`load--insecure-bank--${__ENV.VARIANT}--warmup`]: {
       executor: 'constant-vus',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
       vus: 5,
-      duration: '15s',
-      gracefulStop: '5s',
+      duration: '10s',
+      gracefulStop: '2s',
     },
     [`load--insecure-bank--${__ENV.VARIANT}--high_load`]: {
       executor: 'constant-vus',
       vus: 5,
-      startTime: '15s',
+      startTime: '12s',
       duration: '20s',
-      gracefulStop: '5s',
+      gracefulStop: '2s',
     },
   }
 };
