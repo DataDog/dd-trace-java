b1b0ab330eb8d296365c17a8290e6490b3d1b9ed
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-06-16 08:03:48 +0200

    Add DD_RUNTIME_METRICS_RUNTIME_ID_ENABLED alias for runtime id generation (#8981)

--- internal-api/src/test/groovy/datadog/trace/api/ConfigForkedTest.groovy
+++ internal-api/src/test/groovy/datadog/trace/api/ConfigForkedTest.groovy
@@ -3,18 +3,30 @@ package datadog.trace.api
 import spock.lang.Specification
 
 import static datadog.trace.api.config.GeneralConfig.RUNTIME_ID_ENABLED
+import static datadog.trace.api.config.GeneralConfig.RUNTIME_METRICS_RUNTIME_ID_ENABLED
 
 class ConfigForkedTest extends Specification {
 
   static final String PREFIX = "dd."
 
+  def getSetting() {
+    RUNTIME_ID_ENABLED
+  }
+
   def "test random runtime id generation can be turned off"(){
     setup:
-    System.setProperty(PREFIX + RUNTIME_ID_ENABLED, "false")
+    System.setProperty(PREFIX + getSetting(), "false")
 
     when:
     def config = new Config()
     then:
     config.runtimeId == ""
+  }
+}
+
+class RuntimeMetricsRuntimeIdAliasForkedTest extends ConfigForkedTest {
+  @Override
+  def getSetting() {
+    RUNTIME_METRICS_RUNTIME_ID_ENABLED
   }
 }
