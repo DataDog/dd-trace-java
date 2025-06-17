b137583e6b8158c79ae26b0168baf99d08816a33
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2025-06-17 17:28:44 +0200

    tweak: k6 config, multiple repetitions

--- benchmark/load/insecure-bank/k6.js
+++ benchmark/load/insecure-bank/k6.js
@@ -37,7 +37,7 @@ export const options = function (variants) {
     scenarios[`load--insecure-bank--${variant}--warmup`] = {
       executor: 'constant-vus',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
       vus: 5,
-      duration: '10s',
+      duration: '5s',
       gracefulStop: '2s',
       env: {
         "APP_URL": variants[variant]["APP_URL"]
@@ -47,8 +47,8 @@ export const options = function (variants) {
     scenarios[`load--insecure-bank--${variant}--high_load`] = {
       executor: 'constant-vus',
       vus: 5,
-      startTime: '12s',
-      duration: '20s',
+      startTime: '7s',
+      duration: '15s',
       gracefulStop: '2s',
       env: {
         "APP_URL": variants[variant]["APP_URL"]
