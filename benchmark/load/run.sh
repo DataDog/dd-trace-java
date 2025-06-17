cfa13c08d08cd6a52b2881fd52eacb53c3ddfc93
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2025-06-17 14:42:10 +0200

    wip: Fix bash error

--- benchmark/load/run.sh
+++ benchmark/load/run.sh
@@ -3,9 +3,6 @@
 set -ex
 
 type=$1
-app=$2
-
-export OUTPUT_DIR="${REPORTS_DIR}/${type}/${app}"
 
 if [ -n "$CI_JOB_TOKEN" ]; then
   # Inside BP, so we can assume 24 CPU cores on the second socket available and set CPU affinity
@@ -16,14 +13,18 @@ fi
 
 source "${UTILS_DIR}/update-java-version.sh" 17
 
-if [ "${app}" == "petclinic" ]; then
-  HEALTHCHECK_URL=http://localhost:8080
-elif [ "${app}" == "insecure-bank" ]; then
-  HEALTHCHECK_URL=http://localhost:8080/login
-else
-  echo "Unknown app ${app}"
-  exit 1
-fi
+for app in *; do
+  export OUTPUT_DIR="${REPORTS_DIR}/${type}/${app}"
 
-bash -c "${UTILS_DIR}/../${type}/${app}/start-servers.sh"
-bash -c "${CPU_AFFINITY_K6}${UTILS_DIR}/run-k6-load-test.sh ${HEALTHCHECK_URL} ${OUTPUT_DIR} 'pkill java'"
+  if [ "${app}" == "petclinic" ]; then
+    HEALTHCHECK_URL=http://localhost:8080
+  elif [ "${app}" == "insecure-bank" ]; then
+    HEALTHCHECK_URL=http://localhost:8080/login
+  else
+    echo "Unknown app ${app}"
+    exit 1
+  fi
+
+  bash -c "${UTILS_DIR}/../${type}/${app}/start-servers.sh"
+  bash -c "${CPU_AFFINITY_K6}${UTILS_DIR}/run-k6-load-test.sh ${HEALTHCHECK_URL} ${OUTPUT_DIR} 'pkill java'"
+done
