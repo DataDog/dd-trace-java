407b1a2cd9bafea7b4f1dea77c3e5b3a4d57c827
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2025-06-18 11:50:16 +0200

    tweak: Increase warmup time

--- benchmark/load/insecure-bank/k6.js
+++ benchmark/load/insecure-bank/k6.js
@@ -37,7 +37,7 @@ export const options = function (variants) {
     scenarios[`load--insecure-bank--${variant}--warmup`] = {
       executor: 'constant-vus',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
       vus: 5,
-      duration: '10s',
+      duration: '20s',
       gracefulStop: '2s',
       env: {
         "APP_URL": variants[variant]["APP_URL"]
@@ -47,7 +47,7 @@ export const options = function (variants) {
     scenarios[`load--insecure-bank--${variant}--high_load`] = {
       executor: 'constant-vus',
       vus: 5,
-      startTime: '12s',
+      startTime: '22s',
       duration: '15s',
       gracefulStop: '2s',
       env: {
