ead8d0adbce76fdbc44954582775121626ccf01f
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2025-06-20 11:54:53 +0200

    WIP

--- dd-smoke-tests/resteasy/src/main/java/smoketest/resteasy/App.java
+++ dd-smoke-tests/resteasy/src/main/java/smoketest/resteasy/App.java
@@ -1,5 +1,7 @@
 package smoketest.resteasy;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
 import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.core.Application;
@@ -10,6 +12,8 @@ public class App extends Application {
 
   public App() {
     singletons.add(new Resource());
+    singletons.add(org.jboss.resteasy.plugins.providers.StringTextStar.class); // Writer for String
+    singletons.add(new JacksonJsonProvider()); // Writer for json
   }
 
   @Override
