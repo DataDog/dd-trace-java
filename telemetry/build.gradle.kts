2f749a97c73704f16c75249cf99a3934c4050289
Brice Dutheil <brice.dutheil@gmail.com>
2025-06-25 15:17:12 +0200

    Convert some build file to kotlin dsl (#8743)
    * chore(gradle): Convert :telemetry build file to kotlin
    
    * chore(gradle): Convert :utils:socket-utils build file to kotlin
    
    * chore(gradle): Convert :utils:container-utils build file to kotlin
    
    * chore(gradle): Convert more :utils project to kotlin dsl
    
    * chore(gradle): Convert :remote-config project to kotlin dsl
    
    * chore(build): tweak compile configuration

---
+++ telemetry/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+  id("me.champeau.jmh")
+  id("java-library")
+}
+
+apply(from = "$rootDir/gradle/java.gradle")
+
+val minimumBranchCoverage by extra(0.6)
+val minimumInstructionCoverage by extra(0.8)
+val excludedClassesCoverage by extra(
+  listOf(
+    "datadog.telemetry.TelemetryRunnable.ThreadSleeperImpl",
+    "datadog.telemetry.HostInfo",
+    "datadog.telemetry.HostInfo.Os",
+    "datadog.telemetry.dependency.LocationsCollectingTransformer",
+    "datadog.telemetry.dependency.JbossVirtualFileHelper",
+    "datadog.telemetry.RequestBuilder.NumberJsonAdapter",
+    "datadog.telemetry.RequestBuilderSupplier",
+    "datadog.telemetry.TelemetrySystem",
+    "datadog.telemetry.api.*",
+    "datadog.telemetry.metric.CiVisibilityMetricPeriodicAction"
+  )
+)
+val excludedClassesBranchCoverage by extra(
+  listOf(
+    "datadog.telemetry.PolymorphicAdapterFactory.1",
+    "datadog.telemetry.HostInfo",
+    "datadog.telemetry.HostInfo.Os"
+  )
+)
+val excludedClassesInstructionCoverage by extra(emptyList<String>())
+
+dependencies {
+  implementation(libs.slf4j)
+
+  implementation(project(":internal-api"))
+
+  compileOnly(project(":dd-java-agent:agent-tooling"))
+  testImplementation(project(":dd-java-agent:agent-tooling"))
+  testImplementation(project(":dd-java-agent:agent-logging"))
+
+  compileOnly(project(":communication"))
+  testImplementation(project(":communication"))
+
+  compileOnly(project(":utils:container-utils"))
+  testImplementation(project(":utils:container-utils"))
+
+  api(libs.okhttp)
+  api(libs.moshi)
+
+  testImplementation(project(":utils:test-utils"))
+  testImplementation(group = "org.hamcrest", name = "hamcrest", version = "2.2")
+  testImplementation(group = "org.jboss", name = "jboss-vfs", version = "3.2.16.Final")
+}
+
+jmh {
+  jmhVersion = "1.28"
+  duplicateClassesStrategy = DuplicatesStrategy.EXCLUDE
+}
---
+++ utils/socket-utils/build.gradle.kts
@@ -0,0 +1,41 @@
+import de.thetaphi.forbiddenapis.gradle.CheckForbiddenApis
+import groovy.lang.Closure
+import org.gradle.kotlin.dsl.extra
+
+plugins {
+  `java-library`
+  idea
+}
+
+val minJavaVersionForTests by extra(JavaVersion.VERSION_17)
+
+apply(from = "$rootDir/gradle/java.gradle")
+
+dependencies {
+  implementation(libs.slf4j)
+  implementation(project(":internal-api"))
+  implementation(libs.jnr.unixsocket)
+  testImplementation(files(sourceSets["main_java17"].output))
+}
+
+tasks.named<CheckForbiddenApis>("forbiddenApisMain_java17") {
+  failOnMissingClasses = false
+}
+
+fun AbstractCompile.setJavaVersion(javaVersionInteger: Int) {
+  (project.extra.get("setJavaVersion") as Closure<*>).call(this, javaVersionInteger)
+}
+
+listOf("compileMain_java17Java", "compileTestJava").forEach {
+  tasks.named<JavaCompile>(it).configure {
+    setJavaVersion(17)
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+  }
+}
+
+idea {
+  module {
+    jdkName = "17"
+  }
+}
