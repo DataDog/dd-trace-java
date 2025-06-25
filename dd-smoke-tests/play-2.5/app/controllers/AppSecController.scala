e2710fff99bb0126bc70b8f2082c60439ce3c748
Manuel Álvarez Álvarez <manuel-alvarez-alvarez@users.noreply.github.com>
2025-06-25 12:55:18 +0200

    Extract Play json body response schemas (#8995)

--- dd-smoke-tests/play-2.5/app/controllers/AppSecController.scala
+++ dd-smoke-tests/play-2.5/app/controllers/AppSecController.scala
@@ -1,11 +1,18 @@
 package controllers
 
-import play.api.mvc.{Action, AnyContent, Controller}
+import play.api.mvc.{Action, AnyContent, AnyContentAsJson, Controller}
 
 class AppSecController extends Controller {
 
-  def apiSecuritySampling(statusCode: Int, test: String): Action[AnyContent] = Action {
+  def apiSecuritySampling(statusCode: Int, test: String) = Action {
     Status(statusCode)("EXECUTED")
+  }
+
+  def apiResponse(): Action[AnyContent] = Action { request =>
+    request.body match {
+      case AnyContentAsJson(data) => Ok(data).as("application/json")
+      case _                      => BadRequest("No JSON")
+    }
   }
 
 }
