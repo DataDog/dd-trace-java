1a14c5c414f511e25226c82dd33dcf0f70e77d06
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2025-07-02 14:00:06 +0200

    Keep tests separated

--- dd-java-agent/agent-tooling/src/test/java/datadog/trace/agent/tooling/csi/SuperInCtorExample.java
+++ dd-java-agent/agent-tooling/src/test/java/datadog/trace/agent/tooling/csi/SuperInCtorExample.java
@@ -5,8 +5,14 @@ import java.io.StringReader;
 public class SuperInCtorExample extends StringReader {
 
   public SuperInCtorExample(String s) {
+    // triggers APPSEC-55918
     super(s + new StringReader(s + "Test" + new StringBuilder("another test")));
-    if (s.isEmpty()) { // triggers APPSEC-58131
+  }
+
+  public SuperInCtorExample(StringBuilder s) {
+    super(s.toString());
+    // triggers APPSEC-58131
+    if (s.length() == 0) {
       throw new IllegalArgumentException();
     }
   }
