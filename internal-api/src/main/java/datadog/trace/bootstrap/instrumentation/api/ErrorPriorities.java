c26ef68db7a590e69cd0b2938ee38500a7333325
DJ Gregor <dj@gregor.com>
2025-07-03 14:00:48 -0700

    Add ErrorPriorities for manual instrumentation and tag interceptor

--- internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/ErrorPriorities.java
+++ internal-api/src/main/java/datadog/trace/bootstrap/instrumentation/api/ErrorPriorities.java
@@ -5,4 +5,6 @@ public class ErrorPriorities {
   public static final byte HTTP_SERVER_DECORATOR = -1;
 
   public static final byte DEFAULT = 0;
+  public static final byte TAG_INTERCEPTOR = 1;
+  public static final byte MANUAL_INSTRUMENTATION = 2;
 }
