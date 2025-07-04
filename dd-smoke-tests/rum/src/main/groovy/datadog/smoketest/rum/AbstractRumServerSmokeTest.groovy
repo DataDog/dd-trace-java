f941b4a69bc354486ac07d11e005d75620f4b2e9
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-07-04 11:29:02 +0200

    feat(rum): Add smoke tests

--- dd-smoke-tests/rum/src/main/groovy/datadog/smoketest/rum/AbstractRumServerSmokeTest.groovy
+++ dd-smoke-tests/rum/src/main/groovy/datadog/smoketest/rum/AbstractRumServerSmokeTest.groovy
@@ -5,19 +5,17 @@ import okhttp3.Response
 import spock.lang.Shared
 
 class AbstractRumServerSmokeTest extends AbstractServerSmokeTest {
-    @Shared
-    protected String[] defaultRumProperties = [
-            "-Ddd.rum.enabled=true",
-            "-Ddd.rum.application.id=appid",
-            "-Ddd.rum.client.token=token"
-    ]
+  @Shared
+  protected String[] defaultRumProperties = [
+    "-Ddd.rum.enabled=true",
+    "-Ddd.rum.application.id=appid",
+    "-Ddd.rum.client.token=token"
+  ]
 
 
-    static void assertRumInjected(Response response) {
-        assert response.header('x-datadog-rum-injected') == '1': 'RUM injected header missing'
-        def content = response.body().toString()
-        assert content.contains('https://www.datadoghq-browser-agent.com'): 'RUM script not injected'
-    }
-
-
+  static void assertRumInjected(Response response) {
+    assert response.header('x-datadog-rum-injected') == '1': 'RUM injected header missing'
+    def content = response.body().toString()
+    assert content.contains('https://www.datadoghq-browser-agent.com'): 'RUM script not injected'
+  }
 }
