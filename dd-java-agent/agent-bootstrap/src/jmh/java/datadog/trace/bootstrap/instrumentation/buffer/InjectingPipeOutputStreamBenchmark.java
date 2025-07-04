01f6aaeee17956416184ab513e27ad4417a6509f
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-07-04 13:53:30 +0200

    Add benchmark

---
+++ dd-java-agent/agent-bootstrap/src/jmh/java/datadog/trace/bootstrap/instrumentation/buffer/InjectingPipeOutputStreamBenchmark.java
@@ -0,0 +1,60 @@
+package datadog.trace.bootstrap.instrumentation.buffer;
+
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.apache.commons.io.IOUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 1, time = 30, timeUnit = SECONDS)
+@Measurement(iterations = 2, time = 30, timeUnit = SECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(MICROSECONDS)
+@Fork(value = 1)
+public class InjectingPipeOutputStreamBenchmark {
+  private static final List<String> htmlContent;
+  private static final byte[] marker;
+  private static final byte[] content;
+
+  static {
+    try (InputStream is = new URL("https://www.google.com").openStream()) {
+      htmlContent = IOUtils.readLines(is, StandardCharsets.UTF_8);
+    } catch (IOException ioe) {
+      throw new RuntimeException(ioe);
+    }
+    marker = "</head>".getBytes(StandardCharsets.UTF_8);
+    content = "<script/>".getBytes(StandardCharsets.UTF_8);
+  }
+
+  @Benchmark
+  public void withPipe() throws Exception {
+    try (final PrintWriter out =
+        new PrintWriter(
+            new InjectingPipeOutputStream(new ByteArrayOutputStream(), marker, content, null))) {
+      htmlContent.forEach(out::println);
+    }
+  }
+
+  @Benchmark
+  public void withoutPipe() throws Exception {
+    try (final PrintWriter out = new PrintWriter(new ByteArrayOutputStream())) {
+      htmlContent.forEach(out::println);
+    }
+  }
+}
