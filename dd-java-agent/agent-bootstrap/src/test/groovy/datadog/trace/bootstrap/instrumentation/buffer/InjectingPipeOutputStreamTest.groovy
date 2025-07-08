d4e5f6fcb7f6b37ae63ad88073b23e310cab9470
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-07-08 17:04:13 +0200

    bad commit

--- dd-java-agent/agent-bootstrap/src/test/groovy/datadog/trace/bootstrap/instrumentation/buffer/InjectingPipeOutputStreamTest.groovy
+++ dd-java-agent/agent-bootstrap/src/test/groovy/datadog/trace/bootstrap/instrumentation/buffer/InjectingPipeOutputStreamTest.groovy
@@ -1,7 +1,6 @@
 package datadog.trace.bootstrap.instrumentation.buffer
 
 import datadog.trace.test.util.DDSpecification
-import spock.lang.Specification
 
 class InjectingPipeOutputStreamTest extends DDSpecification {
 
@@ -38,32 +37,5 @@ class InjectingPipeOutputStreamTest extends DDSpecification {
     "<html><head><foo/></head><body/></html>" | "</head>"         | "<script>true</script>" | true  | "<html><head><foo/><script>true</script></head><body/></html>"
     "<html><body/></html>"                    | "</head>"         | "<something/>"          | false | "<html><body/></html>"
     "<foo/>"                                  | "<longerThanFoo>" | "<nothing>"             | false | "<foo/>"
-  }
-
-  def 'should maintain the state in case of IOException for epilogue #epilogue'() {
-    setup:
-    def baos = new ByteArrayOutputStream()
-    def downstream = new ExceptionControlledOutputStream(baos)
-    def piped = new OutputStreamWriter(new InjectingPipeOutputStream(downstream, "</head>".getBytes("UTF-8"), "<injected/>".getBytes("UTF-8"), null),
-    "UTF-8")
-    when:
-    piped.write(prologue)
-    piped.flush()
-    downstream.failWrite = true
-    piped.write(epilogue)
-    piped.flush()
-    then:
-    thrown IOException
-    when:
-    downstream.failWrite = false
-    piped.write(epilogue)
-    piped.close()
-    then:
-    def expected = prologue + epilogue
-    assert expected == new String(baos.toByteArray(), "UTF-8")
-    where:
-    prologue                | epilogue
-    "<html><head><script/>" | "</head></html>"
-    "<html><head><script/>" | "</html>"
   }
 }
