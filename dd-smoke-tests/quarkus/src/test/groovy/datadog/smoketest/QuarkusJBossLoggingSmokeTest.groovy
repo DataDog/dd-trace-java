923ac63ea1b0de526fb0a09c739945f6005b75a5
Sarah Chen <sarah.chen@datadoghq.com>
2025-07-10 14:10:14 -0400

    Ignore jboss and quarkus smoke tests for JDK 24+ (#9147)
    * Update jboss
    
    * Update quarkus

--- dd-smoke-tests/quarkus/src/test/groovy/datadog/smoketest/QuarkusJBossLoggingSmokeTest.groovy
+++ dd-smoke-tests/quarkus/src/test/groovy/datadog/smoketest/QuarkusJBossLoggingSmokeTest.groovy
@@ -1,11 +1,5 @@
 package datadog.smoketest
 
-import datadog.environment.JavaVirtualMachine
-import spock.lang.IgnoreIf
-
-@IgnoreIf(reason = "Failing on Java 24. Skip until we have a fix.", value = {
-  JavaVirtualMachine.isJavaVersionAtLeast(24)
-})
 class QuarkusJBossLoggingSmokeTest extends QuarkusSmokeTest {
   @Override
   String helloEndpointName() {
