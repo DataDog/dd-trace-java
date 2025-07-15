958550b54f65c7d2c0f1cec874967b5b23526d81
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2025-07-15 16:20:27 -0400

    spotless

--- dd-trace-core/src/main/java/datadog/trace/common/writer/ddagent/TraceMapper.java
+++ dd-trace-core/src/main/java/datadog/trace/common/writer/ddagent/TraceMapper.java
@@ -6,7 +6,7 @@ import datadog.trace.common.writer.RemoteMapper;
 import datadog.trace.core.DDSpanContext;
 
 public interface TraceMapper extends RemoteMapper {
-  static final  UTF8BytesString THREAD_NAME = UTF8BytesString.create(DDTags.THREAD_NAME);
+  static final UTF8BytesString THREAD_NAME = UTF8BytesString.create(DDTags.THREAD_NAME);
   static final UTF8BytesString THREAD_ID = UTF8BytesString.create(DDTags.THREAD_ID);
   static final UTF8BytesString SAMPLING_PRIORITY_KEY =
       UTF8BytesString.create(DDSpanContext.PRIORITY_SAMPLING_KEY);
