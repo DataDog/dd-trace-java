31909f32271f0425643a7c9f44d0ba56812341ae
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2025-07-15 14:40:29 -0400

    More static finals

--- dd-trace-core/src/main/java/datadog/trace/common/writer/ddagent/TraceMapper.java
+++ dd-trace-core/src/main/java/datadog/trace/common/writer/ddagent/TraceMapper.java
@@ -6,11 +6,10 @@ import datadog.trace.common.writer.RemoteMapper;
 import datadog.trace.core.DDSpanContext;
 
 public interface TraceMapper extends RemoteMapper {
-
-  UTF8BytesString THREAD_NAME = UTF8BytesString.create(DDTags.THREAD_NAME);
-  UTF8BytesString THREAD_ID = UTF8BytesString.create(DDTags.THREAD_ID);
-  UTF8BytesString SAMPLING_PRIORITY_KEY =
+  static final  UTF8BytesString THREAD_NAME = UTF8BytesString.create(DDTags.THREAD_NAME);
+  static final UTF8BytesString THREAD_ID = UTF8BytesString.create(DDTags.THREAD_ID);
+  static final UTF8BytesString SAMPLING_PRIORITY_KEY =
       UTF8BytesString.create(DDSpanContext.PRIORITY_SAMPLING_KEY);
-  UTF8BytesString ORIGIN_KEY = UTF8BytesString.create(DDTags.ORIGIN_KEY);
-  UTF8BytesString PROCESS_TAGS_KEY = UTF8BytesString.create(DDTags.PROCESS_TAGS);
+  static final UTF8BytesString ORIGIN_KEY = UTF8BytesString.create(DDTags.ORIGIN_KEY);
+  static final UTF8BytesString PROCESS_TAGS_KEY = UTF8BytesString.create(DDTags.PROCESS_TAGS);
 }
