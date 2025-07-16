1a8ee07e9a38f53af1aa690175434444c55e7ad7
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2025-07-16 12:11:27 +0200

    Fix failing remote config test

--- dd-smoke-tests/dynamic-config/src/test/groovy/datadog/smoketest/AppSecActivationSmokeTest.groovy
+++ dd-smoke-tests/dynamic-config/src/test/groovy/datadog/smoketest/AppSecActivationSmokeTest.groovy
@@ -12,6 +12,7 @@ class AppSecActivationSmokeTest extends AbstractSmokeTest {
       '-Ddd.remote_config.enabled=true',
       "-Ddd.remote_config.url=http://localhost:${server.address.port}/v0.7/config".toString(),
       '-Ddd.remote_config.poll_interval.seconds=1',
+      '-Ddd.profiling.enabled=false',
       '-cp',
       System.getProperty('datadog.smoketest.shadowJar.path'),
       AppSecApplication.name
@@ -30,10 +31,12 @@ class AppSecActivationSmokeTest extends AbstractSmokeTest {
       if (it['request_type'] != 'app-client-configuration-change') {
         return false
       }
-      final payload = (Map<String, Object>) it['payload']
-      final configurations = (List<Map<String, Object>>) payload['configuration']
-      final enabled = configurations.find { it['name'] == 'appsec_enabled' }
-      return enabled['value'] == 'true' && enabled['origin'] == 'remote_config'
+      final configurations = (List<Map<String, Object>>) it?.payload?.configuration ?: []
+      final enabledConfig = configurations.find { it.name == 'appsec_enabled' }
+      if (!enabledConfig) {
+        return false
+      }
+      return enabledConfig.value == 'true' && enabledConfig .origin == 'remote_config'
     }
   }
 }
