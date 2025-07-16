6316ab6f2230bbc4707b7d673cd0e740ab6998b0
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-07-16 14:28:00 +0200

    Make rum injector stream/writer more resilient to errors

--- dd-java-agent/agent-bootstrap/src/test/groovy/datadog/trace/bootstrap/instrumentation/buffer/InjectingPipeWriterTest.groovy
+++ dd-java-agent/agent-bootstrap/src/test/groovy/datadog/trace/bootstrap/instrumentation/buffer/InjectingPipeWriterTest.groovy
@@ -1,8 +1,37 @@
 package datadog.trace.bootstrap.instrumentation.buffer
 
 import datadog.trace.test.util.DDSpecification
+import org.apache.commons.io.IOUtils
 
 class InjectingPipeWriterTest extends DDSpecification {
+  static class GlitchedWriter extends FilterWriter {
+    int glitchesPos
+    int count
+
+    GlitchedWriter(Writer out, int glitchesPos) {
+      super(out)
+      this.glitchesPos = glitchesPos
+    }
+
+    @Override
+    void write(char[] c, int off, int len) throws IOException {
+      count += len
+      if (count >= glitchesPos) {
+        glitchesPos = Integer.MAX_VALUE
+        throw new IOException("Glitched after $count bytes")
+      }
+      out.write(c, off, len)
+    }
+
+    @Override
+    void write(int c) throws IOException {
+      if (++count == glitchesPos) {
+        throw new IOException("Glitched after $glitchesPos bytes")
+      }
+      out.write(c)
+    }
+  }
+
   def 'should filter a buffer and inject if found #found using write'() {
     setup:
     def downstream = new StringWriter()
@@ -35,5 +64,38 @@ class InjectingPipeWriterTest extends DDSpecification {
     "<html><head><foo/></head><body/></html>" | "</head>"         | "<script>true</script>" | true  | "<html><head><foo/><script>true</script></head><body/></html>"
     "<html><body/></html>"                    | "</head>"         | "<something/>"          | false | "<html><body/></html>"
     "<foo/>"                                  | "<longerThanFoo>" | "<nothing>"             | false | "<foo/>"
+  }
+
+  def 'should be resilient to exceptions when writing #body'() {
+    setup:
+    def writer = new StringWriter()
+    def downstream = new GlitchedWriter(writer, glichesAt)
+    def piped = new InjectingPipeWriter(downstream, marker.toCharArray(), contentToInject.toCharArray(), null)
+    when:
+    try {
+      for (String line : body) {
+        final chars = line.toCharArray()
+        try {
+          piped.write(chars)
+        } catch (IOException ioe) {
+          ioe.printStackTrace()
+          piped.write(chars)
+        }
+      }
+    } finally {
+      IOUtils.closeQuietly(piped) // it can throw when draining at close
+    }
+    then:
+    assert writer.toString() == expected
+    where:
+    body                                                            | marker            | contentToInject         | glichesAt | expected
+    // write fails after the content has been injected
+    ["<html>", "<head>", "<foo/>", "</head>", "<body/>", "</html>"] | "</head>"         | "<script>true</script>" | 60        | "<html><head><foo/><script>true</script></head><body/></html>"
+    // write fails before the content has been injected
+    ["<html>", "<head>", "<foo/>", "</head>", "<body/>", "</html>"] | "</head>"         | "<script>true</script>" | 20        | "<html><head><foo/></head><body/></html>"
+    // write fails after having filled the buffer. The last line is written twice
+    ["<html>", "<body/>", "</html>"]                                | "</head>"         | "<something/>"          | 10        | "<html><body/></h</html>"
+    // expected broken since the real write happens at close (drain) being the content smaller than the buffer. And retry on close is not a common practice. Hence, we suppose loosing content
+    ["<foo/>"]                                                      | "<longerThanFoo>" | "<nothing>"             | 3         | "<f"
   }
 }
