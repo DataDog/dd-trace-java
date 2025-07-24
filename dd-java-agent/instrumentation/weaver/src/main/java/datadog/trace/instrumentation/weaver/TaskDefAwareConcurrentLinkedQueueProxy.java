cbec227a6e715e32981554b110271d2e73c4150a
Daniel Mohedano <daniel.mohedano@datadoghq.com>
2025-07-24 11:45:44 +0200

    fix: update weaver instrumentation to handle both queue implementations

---
+++ dd-java-agent/instrumentation/weaver/src/main/java/datadog/trace/instrumentation/weaver/TaskDefAwareConcurrentLinkedQueueProxy.java
@@ -0,0 +1,24 @@
+package datadog.trace.instrumentation.weaver;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import sbt.testing.TaskDef;
+
+public final class TaskDefAwareConcurrentLinkedQueueProxy<T> extends ConcurrentLinkedQueue<T> {
+
+  private final TaskDef taskDef;
+  private final ConcurrentLinkedQueue<T> delegate;
+
+  public TaskDefAwareConcurrentLinkedQueueProxy(TaskDef taskDef, ConcurrentLinkedQueue<T> delegate) {
+    super();
+    this.taskDef = taskDef;
+    this.delegate = delegate;
+    DatadogWeaverReporter.start();
+  }
+
+  @Override
+  public T poll() {
+    T event = delegate.poll();
+    DatadogWeaverReporter.processEvent(event, taskDef);
+    return event;
+  }
+}
