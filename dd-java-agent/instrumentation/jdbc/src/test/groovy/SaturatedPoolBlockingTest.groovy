4498426e8685d4a4c5de5a0906f6e9e605bf0f8f
DJ Gregor <dj@gregor.com>
2025-07-24 09:46:36 -0700

    Apache DBCP2: Add a span when waiting on the pool

--- dd-java-agent/instrumentation/jdbc/src/test/groovy/SaturatedPoolBlockingTest.groovy
+++ dd-java-agent/instrumentation/jdbc/src/test/groovy/SaturatedPoolBlockingTest.groovy
@@ -1,25 +1,31 @@
 import datadog.trace.agent.test.AgentTestRunner
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import org.apache.commons.dbcp2.BasicDataSource
+import org.apache.commons.pool2.BaseObject
+import org.apache.commons.pool2.PooledObject
+import org.apache.commons.pool2.PooledObjectFactory
+import org.apache.commons.pool2.impl.DefaultPooledObject
+import org.apache.commons.pool2.impl.GenericObjectPool
 import test.TestDataSource
+import test.TestDriver
 
+import javax.sql.DataSource
+import java.sql.SQLException
 import java.sql.SQLTimeoutException
 import java.sql.SQLTransientConnectionException
+import java.time.Duration
 
 /**
  * Ideas taken from Hikari's com.zaxxer.hikari.pool.TestSaturatedPool830.
  */
 class SaturatedPoolBlockingTest extends AgentTestRunner {
-  def "saturated pool test"(int connectionTimeout, Long exhaustPoolForMillis, int expectedWaitingSpans, boolean expectedTimeout) {
+  public static final int CONNECTION_TIMEOUT = 1000
+
+  def "saturated pool test"(Closure<DataSource> createDataSource, Long exhaustPoolForMillis, int expectedWaitingSpans, boolean expectedTimeout) {
     setup:
     TEST_WRITER.setFilter((trace) -> trace.get(0).getOperationName() == "test.when")
-
-    final HikariConfig config = new HikariConfig()
-    config.setPoolName("testPool")
-    config.setMaximumPoolSize(1)
-    config.setConnectionTimeout(connectionTimeout)
-    config.setDataSourceClassName(TestDataSource.class.getName())
-    final HikariDataSource ds = new HikariDataSource(config)
+    final DataSource ds = createDataSource()
 
     when:
     if (exhaustPoolForMillis != null) {
@@ -45,6 +51,13 @@ class SaturatedPoolBlockingTest extends AgentTestRunner {
     } catch (SQLTimeoutException ignored) {
       // Hikari, older
       timedOut = true
+    } catch (SQLException e) {
+      if (e.getMessage().contains("pool error Timeout waiting for idle object")) {
+        // dbcp2
+        timedOut = true
+      } else {
+        throw e
+      }
     }
     span.finish()
 
@@ -62,10 +75,75 @@ class SaturatedPoolBlockingTest extends AgentTestRunner {
     }
 
     where:
-    connectionTimeout | exhaustPoolForMillis | expectedWaitingSpans | expectedTimeout
-    1000              | null                 | 0                    | false
-    1000              | null                 | 0                    | false
-    1000              | 500                  | 1                    | false
-    1000              | 1500                 | 1                    | true
+    createDataSource       | exhaustPoolForMillis | expectedWaitingSpans | expectedTimeout
+    this.&hikariDataSource | null                 | 0                    | false
+    this.&hikariDataSource | null                 | 0                    | false
+    this.&hikariDataSource | 500                  | 1                    | false
+    this.&hikariDataSource | 1500                 | 1                    | true
+    this.&dbcp2DataSource  | null                 | 0                    | false
+    this.&dbcp2DataSource  | null                 | 0                    | false
+    this.&dbcp2DataSource  | 500                  | 1                    | false
+    this.&dbcp2DataSource  | 1500                 | 1                    | true
+  }
+
+  // Make sure we don't create spans for pollFirst calls to LinkedBlockingDeque unless they are made by dbcp2
+  def "non-dbcp2 LinkedBlockingDeque"() {
+    setup:
+    def pool = new GenericObjectPool<>(new PooledObjectFactory() {
+
+      @Override
+      void activateObject(PooledObject p) throws Exception {
+      }
+
+      @Override
+      void destroyObject(PooledObject p) throws Exception {
+      }
+
+      @Override
+      PooledObject makeObject() throws Exception {
+        return new DefaultPooledObject(new Object())
+      }
+
+      @Override
+      void passivateObject(PooledObject p) throws Exception {
+      }
+
+      @Override
+      boolean validateObject(PooledObject p) {
+        return false
+      }
+    })
+    pool.setMaxTotal(1)
+
+    when:
+    def exhaustPoolForMillis = 500
+    def saturatedConnection = pool.borrowObject()
+    new Thread(() -> {
+      Thread.sleep(exhaustPoolForMillis)
+      pool.returnObject(saturatedConnection)
+    }, "saturated connection closer").start()
+
+    pool.borrowObject(1000)
+
+    then:
+    TEST_WRITER.size() == 0
+  }
+
+  private static DataSource hikariDataSource() {
+    final HikariConfig config = new HikariConfig()
+    config.setPoolName("testPool")
+    config.setMaximumPoolSize(1)
+    config.setConnectionTimeout(CONNECTION_TIMEOUT)
+    config.setDataSourceClassName(TestDataSource.class.getName())
+    return new HikariDataSource(config)
+  }
+
+  private static DataSource dbcp2DataSource() {
+    final BasicDataSource ds = new BasicDataSource()
+    ds.setMaxTotal(1)
+    ds.setMaxWait(Duration.ofMillis(CONNECTION_TIMEOUT))
+    ds.setDriverClassName(TestDriver.class.getName())
+    ds.start()
+    return ds
   }
 }
