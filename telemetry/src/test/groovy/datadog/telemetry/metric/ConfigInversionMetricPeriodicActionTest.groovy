8d2c687c66e9412a46ad17b2aee5ef9bb9f7bd1c
Matthew Li <matthew.li@datadoghq.com>
2025-07-25 14:52:59 -0400

    adding test coverage

---
+++ telemetry/src/test/groovy/datadog/telemetry/metric/ConfigInversionMetricPeriodicActionTest.groovy
@@ -0,0 +1,29 @@
+package datadog.telemetry.metric
+
+import datadog.telemetry.TelemetryService
+import datadog.telemetry.api.Metric
+import spock.lang.Specification
+
+class ConfigInversionMetricPeriodicActionTest extends Specification{
+
+  void 'test otel env var hiding metric'() {
+    setup:
+    final telemetryService = Mock(TelemetryService)
+    final action = new ConfigInversionMetricPeriodicAction()
+
+    when:
+    action.collector().setUndocumentedEnvVarMetric("DD_ENV_VAR")
+    action.collector().prepareMetrics()
+    action.doIteration(telemetryService)
+
+    then:
+    1 * telemetryService.addMetric({ Metric metric ->
+      metric.namespace == 'tracers' &&
+        metric.metric == 'untracked.config.detected' &&
+        metric.points[0][1] == 1 &&
+        metric.tags == ['config_name:DD_ENV_VAR'] &&
+        metric.type == Metric.TypeEnum.COUNT
+    })
+    0 * _._
+  }
+}
