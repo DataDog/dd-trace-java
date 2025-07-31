0687a050adac0410a265e3d10f2916350997ca80
Sarah Chen <sarah.chen@datadoghq.com>
2025-07-30 22:37:29 -0400

    Connect rum injector, telemetry collector, and statsdclient

--- dd-java-agent/instrumentation/servlet/request-5/src/main/java/datadog/trace/instrumentation/servlet5/RumHttpServletResponseWrapper.java
+++ dd-java-agent/instrumentation/servlet/request-5/src/main/java/datadog/trace/instrumentation/servlet5/RumHttpServletResponseWrapper.java
@@ -80,7 +80,7 @@ public class RumHttpServletResponseWrapper extends HttpServletResponseWrapper {
   }
 
   public void onInjected() {
-    RumInjector.reportInjectionSucceed();
+    RumInjector.getTelemetryCollector().onInjectionSucceed();
     try {
       setHeader("x-datadog-rum-injected", "1");
     } catch (Throwable ignored) {
