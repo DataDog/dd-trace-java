56f32103b262b49d768b8678bf6c1677b193442b
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2025-08-06 11:11:48 +0200

    Restore the check to avoid following symlinks that point inside the repo
    (cherry picked from commit c427509dfc41c5a04b7e24ecf52e04a9116d90bc)

--- dd-java-agent/agent-ci-visibility/src/main/java/datadog/trace/civisibility/source/index/RepoIndexBuilder.java
+++ dd-java-agent/agent-ci-visibility/src/main/java/datadog/trace/civisibility/source/index/RepoIndexBuilder.java
@@ -126,10 +126,29 @@ public class RepoIndexBuilder implements RepoIndexProvider {
 
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-      if (Files.isSymbolicLink(dir) && !followSymlinks) {
-        return FileVisitResult.SKIP_SUBTREE;
+      if (Files.isSymbolicLink(dir)) {
+        if (!followSymlinks) {
+          // Configured to skip symlinks
+          return FileVisitResult.SKIP_SUBTREE;
+        }
+        if (readSymbolicLink(dir).startsWith(repoRoot)) {
+          // The path is a symlink that points inside the repo.
+          // We'll visit the folder that it points to anyway,
+          // moreover, we don't want two different results for one file
+          // (one containing the symlink, the other - the actual folder).
+          return FileVisitResult.SKIP_SUBTREE;
+        }
       }
       return FileVisitResult.CONTINUE;
+    }
+
+    private static Path readSymbolicLink(Path path) {
+      try {
+        return Files.readSymbolicLink(path);
+      } catch (Exception e) {
+        log.debug("Could not read symbolic link {}", path, e);
+        return path;
+      }
     }
 
     @Override
