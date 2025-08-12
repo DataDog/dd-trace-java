90f536d8204727f7bfb2a2d33d823fec175bd12b
Matthew Li <matthew.li@datadoghq.com>
2025-08-12 11:26:56 -0400

    fixing buildtests

--- components/environment/build.gradle.kts
+++ components/environment/build.gradle.kts
@@ -37,6 +37,8 @@ val generateSupportedConfigurations by tasks.registering(JavaExec::class) {
     outputFile.get().asFile.parentFile.mkdirs()
   }
 }
+// Ensure Java compilation depends on the generated sources
+sourceSets["main"].java.srcDir(layout.buildDirectory.dir("generated/sources/supported"))
 
 tasks.named("compileJava") {
   dependsOn(generateSupportedConfigurations)
