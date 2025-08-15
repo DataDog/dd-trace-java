ba01e5b9d63098a22a053c2700e822a7bc3918d7
Mikayla Toffler <tofflerm@gmail.com>
2025-08-15 15:04:08 -0400

    Use origin CALCULATED for merged methods

--- internal-api/src/main/java/datadog/trace/bootstrap/config/provider/ConfigProvider.java
+++ internal-api/src/main/java/datadog/trace/bootstrap/config/provider/ConfigProvider.java
@@ -255,7 +255,13 @@ public final class ConfigProvider {
       String value = sources[i].get(key, aliases);
       Map<String, String> parsedMap = ConfigConverter.parseMap(value, key);
       if (!parsedMap.isEmpty()) {
-        origin = sources[i].origin();
+        if (origin != ConfigOrigin.DEFAULT) {
+          // If we have already found a non-default origin, the value is calculated from multiple
+          // sources.
+          origin = ConfigOrigin.CALCULATED;
+        } else {
+          origin = sources[i].origin();
+        }
       }
       merged.putAll(parsedMap);
     }
@@ -277,7 +283,13 @@ public final class ConfigProvider {
       Map<String, String> parsedMap =
           ConfigConverter.parseTraceTagsMap(value, ':', Arrays.asList(',', ' '));
       if (!parsedMap.isEmpty()) {
-        origin = sources[i].origin();
+        if (origin != ConfigOrigin.DEFAULT) {
+          // If we have already found a non-default origin, the value is calculated from multiple
+          // sources.
+          origin = ConfigOrigin.CALCULATED;
+        } else {
+          origin = sources[i].origin();
+        }
       }
       merged.putAll(parsedMap);
     }
@@ -298,7 +310,13 @@ public final class ConfigProvider {
       String value = sources[i].get(key);
       Map<String, String> parsedMap = ConfigConverter.parseOrderedMap(value, key);
       if (!parsedMap.isEmpty()) {
-        origin = sources[i].origin();
+        if (origin != ConfigOrigin.DEFAULT) {
+          // If we have already found a non-default origin, the value is calculated from multiple
+          // sources.
+          origin = ConfigOrigin.CALCULATED;
+        } else {
+          origin = sources[i].origin();
+        }
       }
       merged.putAll(parsedMap);
     }
@@ -322,7 +340,13 @@ public final class ConfigProvider {
         Map<String, String> parsedMap =
             ConfigConverter.parseMapWithOptionalMappings(value, key, defaultPrefix, lowercaseKeys);
         if (!parsedMap.isEmpty()) {
-          origin = sources[i].origin();
+          if (origin != ConfigOrigin.DEFAULT) {
+            // If we have already found a non-default origin, the value is calculated from multiple
+            // sources.
+            origin = ConfigOrigin.CALCULATED;
+          } else {
+            origin = sources[i].origin();
+          }
         }
         merged.putAll(parsedMap);
       }
