9f546b948b47f4a7416e54d45ebbfaabde804a6f
Bruce Bujon <PerfectSlayer@users.noreply.github.com>
2025-08-18 09:15:44 +0200

    Migrate instrumentations to Context API (#9358)
    * feat(bootstrap): Migrate Spray instrumentation to Context API
    * feat(bootstrap): Migrate Tomcat instrumentation to Context API
    * feat(bootstrap): Migrate Liberty instrumentations to Context API
    * feat(bootstrap): Migrate instrumentations to Context API
    * fix(bootstrap): Fix unexpected call to IG callback
    * feat(play): Simplify testing
    * fix(bootstrap): Fix import

--- dd-java-agent/agent-bootstrap/src/jmh/java/datadog/trace/bootstrap/instrumentation/decorator/HttpServerDecoratorBenchmark.java
+++ dd-java-agent/agent-bootstrap/src/jmh/java/datadog/trace/bootstrap/instrumentation/decorator/HttpServerDecoratorBenchmark.java
@@ -1,6 +1,8 @@
 package datadog.trace.bootstrap.instrumentation.decorator;
 
+import static datadog.context.Context.root;
 import static datadog.trace.bootstrap.instrumentation.api.AgentSpan.fromContext;
+import static java.util.Collections.emptyMap;
 import static java.util.concurrent.TimeUnit.MICROSECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -8,7 +10,6 @@ import datadog.context.Context;
 import datadog.trace.api.GlobalTracer;
 import datadog.trace.bootstrap.instrumentation.api.AgentPropagation;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
-import datadog.trace.bootstrap.instrumentation.api.AgentSpanContext;
 import datadog.trace.bootstrap.instrumentation.api.ContextVisitors;
 import datadog.trace.bootstrap.instrumentation.api.URIDataAdapter;
 import datadog.trace.bootstrap.instrumentation.api.URIDefaultDataAdapter;
@@ -17,7 +18,6 @@ import datadog.trace.common.writer.Writer;
 import datadog.trace.core.CoreTracer;
 import datadog.trace.core.DDSpan;
 import java.net.URI;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -59,13 +59,13 @@ public class HttpServerDecoratorBenchmark {
             .build();
     GlobalTracer.forceRegister(tracer);
     decorator = new BenchmarkHttpServerDecorator();
-    Context context = decorator.startSpan(Collections.emptyMap(), Context.root());
+    Context context = decorator.startSpan(emptyMap(), root());
     span = fromContext(context);
   }
 
   @Benchmark
   public AgentSpan onRequest() {
-    return decorator.onRequest(span, null, request, (AgentSpanContext.Extracted) null);
+    return decorator.onRequest(span, null, request, root());
   }
 
   public static class Request {
