23a0369883355ac552e6bca3de47fb01d31a3a7f
Sarah Chen <sarah.chen@datadoghq.com>
2025-08-28 10:25:52 -0400

    Propagate karate test skip for Java 24+

--- dd-java-agent/instrumentation/karate/src/test/java/org/example/TestFailedBuiltInRetryKarate.java
+++ dd-java-agent/instrumentation/karate/src/test/java/org/example/TestFailedBuiltInRetryKarate.java
@@ -7,7 +7,13 @@ import com.intuit.karate.core.ScenarioResult;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
 
+@DisabledForJreRange(
+    min = JRE.JAVA_24,
+    disabledReason =
+        "Karate does not support Java 24+ yet: https://github.com/karatelabs/karate/blob/master/.github/workflows/jdk-compat.yml#L18")
 public class TestFailedBuiltInRetryKarate {
 
   @Test
