561eb5e7affa9c961722c915ec42f3f7b3534e18
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-09-19 10:30:34 +0200

    Lazily obtain feature discovery when starting client metrics (#9548)
    * Lazily obtain feature discovery when starting client metrics
    
    * Add volatile to features
    
    * Defer discovery as much as possible
    
    * review and simplification

--- dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
+++ dd-trace-core/src/main/java/datadog/trace/core/CoreTracer.java
@@ -787,8 +787,10 @@ public class CoreTracer implements AgentTracer.TracerAPI {
     // Schedule the metrics aggregator to begin reporting after a random delay of 1 to 10 seconds
     // (using milliseconds granularity.) This avoids a fleet of traced applications starting at the
     // same time from sending metrics in sync.
-    AgentTaskScheduler.get()
-        .scheduleWithJitter(MetricsAggregator::start, metricsAggregator, 1, SECONDS);
+    sharedCommunicationObjects.whenReady(
+        () ->
+            AgentTaskScheduler.get()
+                .scheduleWithJitter(MetricsAggregator::start, metricsAggregator, 1, SECONDS));
 
     if (dataStreamsMonitoring == null) {
       this.dataStreamsMonitoring =
