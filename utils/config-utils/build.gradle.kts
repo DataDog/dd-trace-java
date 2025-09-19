16dcf77395b27866af35ec75a1626bdc558c586b
Matthew Li <matthew.li@datadoghq.com>
2025-09-19 14:46:47 -0700

    updating class coverage exclude

--- utils/config-utils/build.gradle.kts
+++ utils/config-utils/build.gradle.kts
@@ -32,8 +32,7 @@ val excludedClassesCoverage by extra(
     "datadog.trace.bootstrap.config.provider.stableconfig.Selector",
     // tested in internal-api
     "datadog.trace.bootstrap.config.provider.StableConfigParser",
-    "datadog.trace.bootstrap.config.provider.SystemPropertiesConfigSource",
-    "datadog.trace.config.inversion.ConfigInversionStrictStyle",
+    "datadog.trace.bootstrap.config.provider.SystemPropertiesConfigSource"
   )
 )
 
