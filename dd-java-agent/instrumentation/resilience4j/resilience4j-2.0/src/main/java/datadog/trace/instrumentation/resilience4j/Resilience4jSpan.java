b4b558fce6362a4d718cc0819abe6af3cc4d16ee
Yury Gribkov <yury.gribkov@gmail.com>
2025-09-25 12:07:18 -0700

    compare SPAN_NAME properly

--- dd-java-agent/instrumentation/resilience4j/resilience4j-2.0/src/main/java/datadog/trace/instrumentation/resilience4j/Resilience4jSpan.java
+++ dd-java-agent/instrumentation/resilience4j/resilience4j-2.0/src/main/java/datadog/trace/instrumentation/resilience4j/Resilience4jSpan.java
@@ -5,18 +5,18 @@ import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
 import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
 
 public class Resilience4jSpan {
-  public static final CharSequence SPAN_NAME = UTF8BytesString.create("resilience4j");
-  public static final String INSTRUMENTATION_NAME = "resilience4j";
+  public static final String RESILIENCE4J = "resilience4j";
+  public static final CharSequence SPAN_NAME = UTF8BytesString.create(RESILIENCE4J);
 
   public static AgentSpan current() {
     AgentSpan span = AgentTracer.activeSpan();
-    if (span == null || !SPAN_NAME.equals(span.getOperationName())) {
+    if (span == null || !RESILIENCE4J.contentEquals(span.getOperationName())) {
       return null;
     }
     return span;
   }
 
   public static AgentSpan start() {
-    return AgentTracer.startSpan(INSTRUMENTATION_NAME, SPAN_NAME);
+    return AgentTracer.startSpan(RESILIENCE4J, SPAN_NAME);
   }
 }
