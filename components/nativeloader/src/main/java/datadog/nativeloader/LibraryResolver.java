cfb4285f7ac3ba75046ab9af1b1b1d45fa452920
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2025-09-26 18:16:01 -0400

    Tweaking Javadoc

--- components/nativeloader/src/main/java/datadog/nativeloader/LibraryResolver.java
+++ components/nativeloader/src/main/java/datadog/nativeloader/LibraryResolver.java
@@ -5,7 +5,7 @@ import java.net.URL;
 /**
  * LibraryResolver encapsulates a library resolution strategy
  * 
- * The strategy should use the provided {@link PathLocator} to locate the desired resources.
+ * The LibraryResolver should use the provided {@link PathLocator} to locate the desired resources.
  * The LibraryResolver may try multiple locations to find the best possible library to use.
  */
 public interface LibraryResolver {
