2b899aa5d687391079c8ac2231a86b5887cfbff3
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2025-10-01 11:35:59 -0400

    Hooking into arch detection code relocated to :components:environment
    Also added javadoc to PlatformSpec

--- components/native-loader/src/main/java/datadog/nativeloader/PlatformSpec.java
+++ components/native-loader/src/main/java/datadog/nativeloader/PlatformSpec.java
@@ -1,6 +1,7 @@
 package datadog.nativeloader;
 
 import datadog.environment.OperatingSystem;
+import datadog.environment.OperatingSystem.Architecture;
 
 /**
  * PlatformSpec describes a native library "platform" -- including operating system, architecture,
@@ -11,28 +12,38 @@ public abstract class PlatformSpec {
     return DefaultPlatformSpec.INSTANCE;
   }
 
+  /** Is the target OS MacOS? */
   public abstract boolean isMac();
 
+  /** Is the target OS Windows? */
   public abstract boolean isWindows();
 
+  /** Is the target OS Linux? */
   public abstract boolean isLinux();
 
+  /** Is the target OS unknown / not handled by {@link NativeLoader}? */
   public final boolean isUnknownOs() {
     return !this.isLinux() && !this.isMac() && !this.isWindows();
   }
 
+  /** Is the target architecture x86-32 bit? */
   public abstract boolean isX86_32();
 
+  /** Is the target architecture x86-64 bit? */
   public abstract boolean isX86_64();
 
+  /** Is the target architecture ARM 32-bit? */
   public abstract boolean isArm32();
 
+  /** Is the target architecture ARM 64-bit? */
   public abstract boolean isAarch64();
 
+  /** Is the target architecture unknown / not handled by {@link NativeLoader}? */
   public final boolean isUnknownArch() {
     return !this.isX86_64() && !this.isAarch64() && !this.isX86_32() && !this.isArm32();
   }
 
+  /** Is the target using MUSL libc? */
   public abstract boolean isMusl();
 }
 
@@ -64,22 +75,26 @@ final class DefaultPlatformSpec extends PlatformSpec {
 
   @Override
   public boolean isAarch64() {
-    return OperatingSystem.isAarch64();
+    return isArch(Architecture.ARM64);
   }
 
   @Override
   public boolean isArm32() {
-    return false;
+    return isArch(Architecture.ARM);
   }
 
   @Override
   public boolean isX86_32() {
-    return false;
+    return isArch(Architecture.X86);
   }
 
   @Override
   public boolean isX86_64() {
-    return false;
+    return isArch(Architecture.X64);
+  }
+
+  static final boolean isArch(OperatingSystem.Architecture arch) {
+    return (OperatingSystem.architecture() == arch);
   }
 
   @Override
