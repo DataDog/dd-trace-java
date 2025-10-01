0e8c405f5c4b3b37ce6b5b28866dcc49096a8780
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2025-10-01 14:38:32 -0400

    Adding ability to record multiple locate requests to CapturingPathLocator

--- components/native-loader/src/test/java/datadog/nativeloader/CapturingPathLocator.java
+++ components/native-loader/src/test/java/datadog/nativeloader/CapturingPathLocator.java
@@ -1,16 +1,18 @@
 package datadog.nativeloader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public final class CapturingPathLocator implements PathLocator {
   final int simulateNotFoundCount;
   int numRequests;
 
-  String requestedComponent;
-  String requestedPath;
+  final Deque<LocateRequest> locateRequests = new LinkedList<>();
 
   public CapturingPathLocator() {
     this(0);
@@ -25,8 +27,7 @@ public final class CapturingPathLocator implements PathLocator {
   public URL locate(String component, String path) {
     if (this.numRequests++ < this.simulateNotFoundCount) return null;
 
-    this.requestedComponent = component;
-    this.requestedPath = path;
+    this.locateRequests.addLast(new LocateRequest(component, path));
 
     try {
       return new URL("http://localhost");
@@ -36,7 +37,25 @@ public final class CapturingPathLocator implements PathLocator {
   }
 
   void assertRequested(String expectedComponent, String expectedPath) {
-    assertEquals(expectedComponent, this.requestedComponent);
-    assertEquals(expectedPath, this.requestedPath);
+    this.locateRequests.removeFirst().assertRequested(expectedComponent, expectedPath);
+  }
+  
+  void assertDone() {
+	assertTrue(this.locateRequests.isEmpty());
+  }
+  
+  final class LocateRequest {
+	private final String requestedComponent;
+	private final String requestedPath;
+	
+	LocateRequest(String requestedComponent, String requestedPath) {
+	  this.requestedComponent = requestedComponent;
+	  this.requestedPath = requestedPath;
+	}
+	
+	void assertRequested(String expectedComponent, String expectedPath) {
+	   assertEquals(expectedComponent, this.requestedComponent);
+	   assertEquals(expectedPath, this.requestedPath);
+	}
   }
 }
