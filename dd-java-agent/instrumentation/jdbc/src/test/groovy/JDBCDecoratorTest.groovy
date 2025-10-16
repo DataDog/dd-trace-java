b776e20bc2e4620067aa60271627b93d3253de5d
Raphaël Vandon <raphael.vandon@datadoghq.com>
2025-10-16 16:19:49 +0200

    extract DBM specific test to a different class

--- dd-java-agent/instrumentation/jdbc/src/test/groovy/JDBCDecoratorTest.groovy
+++ dd-java-agent/instrumentation/jdbc/src/test/groovy/JDBCDecoratorTest.groovy
@@ -1,36 +1,11 @@
-import static datadog.trace.api.config.TraceInstrumentationConfig.DB_DBM_PROPAGATION_MODE_MODE
-import static datadog.trace.instrumentation.jdbc.JDBCDecorator.DECORATE
-
 import datadog.trace.agent.test.InstrumentationSpecification
-import datadog.trace.bootstrap.instrumentation.jdbc.DBInfo
 import datadog.trace.instrumentation.jdbc.JDBCDecorator
 import java.sql.Connection
 import java.sql.SQLException
 import test.TestConnection
 import test.TestDatabaseMetaData
 
-abstract class JDBCDecoratorTest extends InstrumentationSpecification {
-  @Override
-  protected void configurePreAgent() {
-    super.configurePreAgent()
-    setupPropagationMode()
-  }
-
-  def "Full integration disabled for unsupported DB types (#dbType)"() {
-    setup:
-    DBInfo dbInfo = new DBInfo.Builder().type(dbType).build()
-    expect:
-    // check that we get the signal to inject iff both the type and the config say yes
-    DECORATE.shouldInjectTraceContext(dbInfo) == (expectedByType && expectedFromConfig())
-
-    where:
-    dbType       | expectedByType
-    "oracle"     | true
-    "sqlserver"  | true
-    "mysql"      | true
-    "postgresql" | true
-  }
-
+class JDBCDecoratorTest extends InstrumentationSpecification {
   def "test dbInfo fetching"() {
     Connection connection = new TestConnection(false) {
         @Override
@@ -54,47 +29,5 @@ abstract class JDBCDecoratorTest extends InstrumentationSpecification {
     assert dbInfo.host == "myhost"
     assert dbInfo.port == 9999
     assert dbInfo.warehouse == "hello"
-  }
-
-  protected abstract void setupPropagationMode()
-
-  protected abstract boolean expectedFromConfig()
-}
-
-
-class JDBCDecoratorFullPropagationForkedTest extends JDBCDecoratorTest {
-  @Override
-  protected void setupPropagationMode() {
-    injectSysConfig(DB_DBM_PROPAGATION_MODE_MODE, "full")
-  }
-
-  @Override
-  protected boolean expectedFromConfig() {
-    // trace context should be injected only in full mode, this is the only "true".
-    return true
-  }
-}
-
-class JDBCDecoratorServicePropagationForkedTest extends JDBCDecoratorTest {
-  @Override
-  protected void setupPropagationMode() {
-    injectSysConfig(DB_DBM_PROPAGATION_MODE_MODE, "service")
-  }
-
-  @Override
-  protected boolean expectedFromConfig() {
-    return false
-  }
-}
-
-class JDBCDecoratorNoPropagationForkedTest extends JDBCDecoratorTest {
-  @Override
-  protected void setupPropagationMode() {
-    injectSysConfig(DB_DBM_PROPAGATION_MODE_MODE, "disabled")
-  }
-
-  @Override
-  protected boolean expectedFromConfig() {
-    return false
   }
 }
