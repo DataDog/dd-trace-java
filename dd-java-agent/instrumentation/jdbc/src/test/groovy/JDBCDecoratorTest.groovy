f992419eb7eb050f6baa49c1acd0635795c8cbad
Raphaël Vandon <raphael.vandon@datadoghq.com>
2025-10-16 15:42:05 +0200

    add test

--- dd-java-agent/instrumentation/jdbc/src/test/groovy/JDBCDecoratorTest.groovy
+++ dd-java-agent/instrumentation/jdbc/src/test/groovy/JDBCDecoratorTest.groovy
@@ -1,8 +1,13 @@
-import datadog.trace.agent.test.InstrumentationSpecification
-import datadog.trace.bootstrap.instrumentation.jdbc.DBInfo
-
 import static datadog.trace.api.config.TraceInstrumentationConfig.DB_DBM_PROPAGATION_MODE_MODE
 import static datadog.trace.instrumentation.jdbc.JDBCDecorator.DECORATE
+
+import datadog.trace.agent.test.InstrumentationSpecification
+import datadog.trace.bootstrap.instrumentation.jdbc.DBInfo
+import datadog.trace.instrumentation.jdbc.JDBCDecorator
+import java.sql.Connection
+import java.sql.SQLException
+import test.TestConnection
+import test.TestDatabaseMetaData
 
 abstract class JDBCDecoratorTest extends InstrumentationSpecification {
   @Override
@@ -24,6 +29,31 @@ abstract class JDBCDecoratorTest extends InstrumentationSpecification {
     "sqlserver"  | true
     "mysql"      | true
     "postgresql" | true
+  }
+
+  def "test dbInfo fetching"() {
+    Connection connection = new TestConnection(false) {
+        @Override
+        Properties getClientInfo() throws SQLException {
+          var prop = new Properties()
+          prop.setProperty("warehouse", "hello")
+          return prop
+        }
+      }
+    def metadata = new TestDatabaseMetaData()
+    metadata.setURL("jdbc:testdb://myhost:9999/testdatabase")
+    connection.setMetaData(metadata)
+
+    when:
+    def dbInfo = JDBCDecorator.parseDBInfoFromConnection(connection)
+
+    then:
+    dbInfo != null
+    assert dbInfo.type == "testdb"
+    assert dbInfo.db == "testdatabase"
+    assert dbInfo.host == "myhost"
+    assert dbInfo.port == 9999
+    assert dbInfo.warehouse == "hello"
   }
 
   protected abstract void setupPropagationMode()
