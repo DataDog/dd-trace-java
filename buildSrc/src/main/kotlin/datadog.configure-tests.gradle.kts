21940597db7a0ae4cbc8375fe6b926b677932ba5
Sarah Chen <sarah.chen@datadoghq.com>
2025-10-24 14:52:37 -0400

    Create configure-tests convention plugin

---
+++ buildSrc/src/main/kotlin/datadog.configure-tests.gradle.kts
@@ -0,0 +1,137 @@
+import org.gradle.api.tasks.testing.Test
+import org.gradle.api.tasks.testing.junitplatform.JUnitPlatformOptions
+import org.gradle.api.services.BuildService
+import org.gradle.api.services.BuildServiceParameters
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+
+fun isTestingInstrumentation(project: Project): Boolean {
+  return listOf(
+    "junit-4.10",
+    "cucumber",
+    "cucumber-junit-4",
+    "junit-4.13",
+    "munit-junit-4",
+    "junit-5.3",
+    "junit-5.8",
+    "cucumber-junit-5",
+    "spock-junit-5",
+    "testng-6",
+    "testng-7",
+    "karate",
+    "scalatest",
+    "selenium",
+    "weaver"
+  ).contains(project.name)
+}
+
+// Need concrete implementation of BuildService in Kotlin
+abstract class ForkedTestLimit : BuildService<BuildServiceParameters.None>
+
+val forkedTestLimit = gradle.sharedServices.registerIfAbsent("forkedTestLimit", ForkedTestLimit::class.java) {
+  maxParallelUsages.set(3)
+}
+
+// Go through the Test tasks and configure them
+tasks.withType(Test::class.java).configureEach {
+  // Disable all tests if skipTests property was specified
+  onlyIf { !project.rootProject.hasProperty("skipTests") }
+
+  // Enable force rerun of tests with -Prerun.tests.${project.name}
+  outputs.upToDateWhen {
+    !project.rootProject.hasProperty("rerun.tests.${project.name}")
+  }
+
+  // Avoid executing classes used to test testing frameworks instrumentation
+  if (isTestingInstrumentation(project)) {
+    exclude("**/TestSucceed*")
+    exclude("**/TestFailed*")
+    exclude("**/TestFailedWithSuccessPercentage*")
+    exclude("**/TestError*")
+    exclude("**/TestSkipped*")
+    exclude("**/TestSkippedClass*")
+    exclude("**/TestInheritance*", "**/BaseTestInheritance*")
+    exclude("**/TestFactory*")
+    exclude("**/TestParameterized*")
+    exclude("**/TestRepeated*")
+    exclude("**/TestTemplate*")
+    exclude("**/TestDisableTestTrace*")
+    exclude("**/TestAssumption*", "**/TestSuiteSetUpAssumption*")
+    exclude("**/TestUnskippable*")
+    exclude("**/TestWithSetup*")
+  }
+
+  // Split up tests that want to run forked in their own separate JVM for generated tasks
+  if (name.startsWith("forkedTest") || name.endsWith("ForkedTest")) {
+    setExcludes(emptyList())
+    setIncludes(listOf("**/*ForkedTest*"))
+    forkEvery = 1
+    // Limit the number of concurrent forked tests
+    usesService(forkedTestLimit)
+    onlyIf { !project.rootProject.hasProperty("skipForkedTests") }
+  } else {
+    exclude("**/*ForkedTest*")
+  }
+
+  // Set test timeout for 20 minutes. Default job timeout is 1h (configured on CI level).
+  timeout.set(Duration.of(20, ChronoUnit.MINUTES))
+}
+
+val allTestsTask = tasks.maybeCreate("allTests")
+val allLatestDepTestsTask = tasks.maybeCreate("allLatestDepTests")
+val checkTask = tasks.named("check")
+
+project.afterEvaluate {
+  tasks.withType(Test::class.java).forEach {
+    // Add test to appropriate aggregate task
+    if (it.name.contains("latest", ignoreCase = true)) {
+      allLatestDepTestsTask.dependsOn(it)
+    } else if (it.name != "traceAgentTest") {
+      allTestsTask.dependsOn(it)
+    }
+    // Make check depend on this test task
+    checkTask.configure {
+      dependsOn(it)
+    }
+  }
+}
+
+// Setup flaky tests jobs. Done in afterEvaluate so that it applies to latestDepTest.
+project.afterEvaluate {
+  tasks.withType(Test::class.java).configureEach {
+    // Flaky tests management for JUnit 5
+    if (testFramework is JUnitPlatformOptions) {
+      val junitPlatform = testFramework as JUnitPlatformOptions
+      if (project.rootProject.hasProperty("skipFlakyTests")) {
+        junitPlatform.excludeTags("flaky")
+      } else if (project.rootProject.hasProperty("runFlakyTests")) {
+        junitPlatform.includeTags("flaky")
+      }
+    }
+
+    // Flaky tests management for Spock
+    if (project.rootProject.hasProperty("skipFlakyTests")) {
+      jvmArgs("-Drun.flaky.tests=false")
+    } else if (project.rootProject.hasProperty("runFlakyTests")) {
+      jvmArgs("-Drun.flaky.tests=true")
+    }
+  }
+}
+
+if (!(project.property("activePartition") as Boolean)) {
+  project.afterEvaluate {
+    tasks.withType(Test::class.java).configureEach {
+      enabled = false
+    }
+  }
+}
+
+// tasks.withType(Test).configureEach {
+//   // https://docs.gradle.com/develocity/flaky-test-detection/
+//   // https://docs.gradle.com/develocity/gradle-plugin/current/#test_retry
+//   develocity.testRetry {
+//     if (providers.environmentVariable("CI").isPresent()) {
+//       maxRetries = 3
+//     }
+//   }
+// }
