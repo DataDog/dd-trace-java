248efa225db18aa38a14c540c84d00857b5ef4ea
DJ Gregor <dj@gregor.com>
2025-10-24 15:46:47 -0700

    Add long_running_traces.json to flare report
    Synchronized accesses to traceArray in LongRunningTracesTracker
    since the flare reporter can now access the array. This shouldn't
    be a concern for blocking because addTrace and flushAndCompact are
    the existing calls from PendingTraceBuffer's run() loop and
    getTracesAsJson is called by the reporter thread and will complete
    fairly quickly.

--- dd-trace-core/src/main/java/datadog/trace/core/LongRunningTracesTracker.java
+++ dd-trace-core/src/main/java/datadog/trace/core/LongRunningTracesTracker.java
@@ -3,12 +3,16 @@ package datadog.trace.core;
 import datadog.communication.ddagent.DDAgentFeaturesDiscovery;
 import datadog.communication.ddagent.SharedCommunicationObjects;
 import datadog.trace.api.Config;
+import datadog.trace.api.flare.TracerFlare;
+import datadog.trace.common.writer.TraceDumpJsonExporter;
 import datadog.trace.core.monitor.HealthMetrics;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipOutputStream;
 
-public class LongRunningTracesTracker {
+public class LongRunningTracesTracker implements TracerFlare.Reporter {
   private final DDAgentFeaturesDiscovery features;
   private final HealthMetrics healthMetrics;
   private long lastFlushMilli = 0;
@@ -41,6 +45,8 @@ public class LongRunningTracesTracker {
         (int) TimeUnit.SECONDS.toMillis(config.getLongRunningTraceFlushInterval());
     this.features = sharedCommunicationObjects.featuresDiscovery(config);
     this.healthMetrics = healthMetrics;
+
+    TracerFlare.addReporter(this);
   }
 
   public boolean add(PendingTraceBuffer.Element element) {
@@ -56,7 +62,7 @@ public class LongRunningTracesTracker {
     return true;
   }
 
-  private void addTrace(PendingTrace trace) {
+  private synchronized void addTrace(PendingTrace trace) {
     if (trace.empty()) {
       return;
     }
@@ -67,7 +73,7 @@ public class LongRunningTracesTracker {
     traceArray.add(trace);
   }
 
-  public void flushAndCompact(long nowMilli) {
+  public synchronized void flushAndCompact(long nowMilli) {
     if (nowMilli < lastFlushMilli + TimeUnit.SECONDS.toMillis(1)) {
       return;
     }
@@ -138,5 +144,19 @@ public class LongRunningTracesTracker {
     dropped = 0;
     write = 0;
     expired = 0;
+  }
+
+  public synchronized String getTracesAsJson() {
+    try (TraceDumpJsonExporter writer = new TraceDumpJsonExporter()) {
+      for (PendingTrace trace : traceArray) {
+        writer.write(trace.getSpans());
+      }
+      return writer.getDumpJson();
+    }
+  }
+
+  @Override
+  public void addReportToFlare(ZipOutputStream zip) throws IOException {
+    TracerFlare.addText(zip, "long_running_traces.txt", getTracesAsJson());
   }
 }
