e935d212539359f61c09d4c84f18a87df4572eb1
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2025-10-28 12:34:50 +0100

    Improve smoke test

--- dd-smoke-tests/springboot-java-11/src/test/groovy/datadog/smoketest/springboot/OpenFeatureProviderSmokeTest.groovy
+++ dd-smoke-tests/springboot-java-11/src/test/groovy/datadog/smoketest/springboot/OpenFeatureProviderSmokeTest.groovy
@@ -1,5 +1,7 @@
 package datadog.smoketest.springboot
 
+import datadog.remoteconfig.Capabilities
+import datadog.remoteconfig.Product
 import datadog.smoketest.AbstractServerSmokeTest
 import groovy.json.JsonSlurper
 import okhttp3.Request
@@ -19,6 +21,10 @@ class OpenFeatureProviderSmokeTest extends AbstractServerSmokeTest {
     return builder
   }
 
+  void setup() {
+    setRemoteConfig()
+  }
+
   void 'test open feature provider metadata'() {
     setup:
     final url = "http://localhost:${httpPort}/openfeature/provider-metadata"
@@ -33,6 +39,17 @@ class OpenFeatureProviderSmokeTest extends AbstractServerSmokeTest {
     responseBody['metadata'] == 'datadog-openfeature-provider'
     responseBody['providerClass'] == 'datadog.trace.api.openfeature.Provider'
     responseBody != null
+  }
+
+  void 'test remote config'() {
+    when:
+    final rcRequest = waitForRcClientRequest {req ->
+      decodeProducts(req).find { Product.FFE_FLAGS } != null
+    }
+
+    then:
+    final capabilities = decodeCapabilities(rcRequest)
+    hasCapability(capabilities, Capabilities.CAPABILITY_FFE_FLAG_CONFIGURATION_RULES)
   }
 
   void 'test open feature evaluation'() {
@@ -61,5 +78,22 @@ class OpenFeatureProviderSmokeTest extends AbstractServerSmokeTest {
     'user_1' | 'boolean' | 'boolean-flag' | true
     'user_1' | 'string'  | 'string-flag'  | 'defaultString'
     'user_1' | 'object'  | 'object-flag'  | [hello: 'World!']
+  }
+
+  private static Set<Product> decodeProducts(final Map<String, Object> request) {
+    return request.client.products.collect { Product.valueOf(it)}
+  }
+
+  private static long decodeCapabilities(final Map<String, Object> request) {
+    final clientCapabilities = request.client.capabilities as byte[]
+    long capabilities = 0l
+    for (int i = 0; i < clientCapabilities.length; i++) {
+      capabilities |= (clientCapabilities[i] & 0xFFL) << ((clientCapabilities.length - i - 1) * 8)
+    }
+    return capabilities
+  }
+
+  private static boolean hasCapability(final long capabilities, final long test) {
+    return (capabilities & test) > 0
   }
 }
