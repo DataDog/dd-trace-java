7beade4bca1c030af76303eba0f0b77d5092dc1f
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2025-10-29 13:26:16 +0100

    fix tests

--- internal-api/src/main/java/datadog/trace/api/Config.java
+++ internal-api/src/main/java/datadog/trace/api/Config.java
@@ -4342,7 +4342,7 @@ public class Config {
     } else if (isCiVisibilityAgentlessEnabled()) {
       return Intake.LOGS.getAgentlessUrl(this) + "logs";
     } else {
-      throw new IllegalArgumentException("Cannot find snapshot endpoint on datadog agent");
+      return getFinalDebuggerBaseUrl() + "/debugger/v1/diagnostics";
     }
   }
 
