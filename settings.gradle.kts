368a7c6b5fa83e424b903047b9856be2ef50327e
liurui <liurui_wuhan@163.com>
2025-11-03 15:46:58 +0800

    support apache dubbo stream version 3.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -488,6 +488,7 @@ include(
   ":dd-java-agent:instrumentation:rocketmq",
   ":dd-java-agent:instrumentation:rocketmq-5.0",
   ":dd-java-agent:instrumentation:dubbo-apache-2.7",
+  ":dd-java-agent:instrumentation:dubbo-apache:dubbo-apache-3.2",
   ":dd-java-agent:instrumentation:dubbo-alibaba",
   ":dd-java-agent:instrumentation:thrift",
   ":dd-java-agent:instrumentation:ons-client",
