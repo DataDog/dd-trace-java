fa9428e94716af76d27a281ae6db1485a544ac1e
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2025-11-05 10:42:40 -0500

    Adding listener tests
    - adding test of NativeLoader.Builder listener related methods

--- components/native-loader/src/test/java/datadog/nativeloader/NativeLoaderBuilderTest.java
+++ components/native-loader/src/test/java/datadog/nativeloader/NativeLoaderBuilderTest.java
@@ -25,6 +25,60 @@ public class NativeLoaderBuilderTest {
     NativeLoader.Builder builder = NativeLoader.builder().platformSpec(platformSpec);
     assertSame(platformSpec, builder.platformSpec());
   }
+  
+  @Test
+  public void defaultListeners() {
+	NativeLoader.Builder builder = NativeLoader.builder();
+	
+	assertTrue(builder.listeners().isNop());
+  }
+  
+  @Test
+  public void addListener() {
+	TestLibraryLoadingListener listener1 = new TestLibraryLoadingListener().
+	  expectLoad("foo");
+	TestLibraryLoadingListener listener2 = listener1.copy();
+	
+	NativeLoader.Builder builder = NativeLoader.builder().
+	  addListener(listener1).
+	  addListener(listener2);
+	
+	SafeLibraryLoadingListener listener = builder.listeners();
+	assertFalse(listener.isNop());
+	
+	listener.onLoad(
+	  builder.platformSpec(),
+	  null,
+	  "foo",
+	  false,
+	  Paths.get("/tmp/foo.dylib"));
+	
+	listener1.assertDone();
+	listener2.assertDone();
+  }
+  
+  @Test
+  public void addListeners() {
+	TestLibraryLoadingListener listener1 = new TestLibraryLoadingListener().
+	  expectLoad("foo");
+	TestLibraryLoadingListener listener2 = listener1.copy();
+	
+	NativeLoader.Builder builder = NativeLoader.builder().
+	  addListeners(listener1, listener2);
+	
+	SafeLibraryLoadingListener listener = builder.listeners();
+	assertFalse(listener.isNop());
+	
+	listener.onLoad(
+	  builder.platformSpec(),
+	  null,
+	  "foo",
+	  false,
+	  Paths.get("/tmp/foo.dylib"));
+	
+	listener1.assertDone();
+	listener2.assertDone();
+  }
 
   @Test
   public void defaultLibraryResolver() {
