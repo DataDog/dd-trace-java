df4602cd133fd908c2ad33de71adc9206adb72d8
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2025-11-10 15:07:05 -0500

    Fixed `:dd-smoke-tests:gradle` module tests.

--- dd-java-agent/testing/src/main/groovy/datadog/trace/agent/test/server/http/TestHttpServer.groovy
+++ dd-java-agent/testing/src/main/groovy/datadog/trace/agent/test/server/http/TestHttpServer.groovy
@@ -6,6 +6,7 @@ import datadog.trace.bootstrap.instrumentation.api.AgentSpanContext
 import datadog.trace.bootstrap.instrumentation.api.Tags
 import datadog.trace.core.DDSpan
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
+import groovy.transform.CompileStatic
 import org.eclipse.jetty.http.HttpMethod
 import org.eclipse.jetty.http.HttpVersion
 import org.eclipse.jetty.server.Handler
@@ -316,6 +317,7 @@ class TestHttpServer implements AutoCloseable {
       this.prefix = prefix.startsWith("/") ? prefix : "/" + prefix
     }
 
+    @CompileStatic // Workaround to avoid `groovy.lang.MissingMethodException`.
     @Override
     void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
       if (target.startsWith(prefix)) {
