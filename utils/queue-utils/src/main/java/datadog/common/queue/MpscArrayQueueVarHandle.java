f94c8d18c064916fee775a255954f3678c0d145a
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-11-10 13:40:56 +0100

    Final updates

--- utils/queue-utils/src/main/java/datadog/common/queue/MpscArrayQueueVarHandle.java
+++ utils/queue-utils/src/main/java/datadog/common/queue/MpscArrayQueueVarHandle.java
@@ -1,8 +1,5 @@
 package datadog.common.queue;
 
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.invoke.VarHandle;
 import java.util.Objects;
 import java.util.concurrent.locks.LockSupport;
 
@@ -15,51 +12,16 @@ import java.util.concurrent.locks.LockSupport;
  * @param <E> the type of elements stored
  */
 class MpscArrayQueueVarHandle<E> extends BaseQueue<E> {
-  private static final VarHandle ARRAY_HANDLE;
-  private static final VarHandle HEAD_HANDLE;
-  private static final VarHandle TAIL_HANDLE;
-  private static final VarHandle PRODUCER_LIMIT_HANDLE;
-
-  static {
-    try {
-      final Lookup lookup = MethodHandles.lookup();
-      TAIL_HANDLE = lookup.findVarHandle(MpscArrayQueueVarHandle.class, "tail", long.class);
-      HEAD_HANDLE = lookup.findVarHandle(MpscArrayQueueVarHandle.class, "head", long.class);
-      ARRAY_HANDLE = MethodHandles.arrayElementVarHandle(Object[].class);
-      PRODUCER_LIMIT_HANDLE =
-          lookup.findVarHandle(MpscArrayQueueVarHandle.class, "producerLimit", long.class);
-    } catch (Throwable t) {
-      throw new IllegalStateException(t);
-    }
-  }
-
-  /** The backing array (plain Java array for VarHandle access) */
-  private final Object[] buffer;
-
   // Padding to prevent false sharing
   @SuppressWarnings("unused")
   private long p0, p1, p2, p3, p4, p5, p6;
 
-  /** Next free slot for producers (multi-threaded) */
-  private volatile long tail = 0L;
-
-  // Padding around tail
-  @SuppressWarnings("unused")
-  private long q0, q1, q2, q3, q4, q5, q6;
-
   /** Cached producer limit to reduce volatile head reads */
-  private volatile long producerLimit = 0L;
+  protected volatile long producerLimit = 0L;
 
   // Padding around producerLimit
   @SuppressWarnings("unused")
-  private long r0, r1, r2, r3, r4, r5, r6;
-
-  /** Next slot to consume (single-threaded) */
-  private volatile long head = 0L;
-
-  // Padding around head
-  @SuppressWarnings("unused")
-  private long s0, s1, s2, s3, s4, s5, s6;
+  private long q0, q1, q2, q3, q4, q5, q6;
 
   /**
    * Creates a new MPSC queue.
@@ -68,7 +30,6 @@ class MpscArrayQueueVarHandle<E> extends BaseQueue<E> {
    */
   public MpscArrayQueueVarHandle(int requestedCapacity) {
     super(requestedCapacity);
-    this.buffer = new Object[capacity];
     this.producerLimit = capacity;
   }
 
@@ -79,7 +40,7 @@ class MpscArrayQueueVarHandle<E> extends BaseQueue<E> {
     // jctools does the same local copy to have the jitter optimise the accesses
     final Object[] localBuffer = this.buffer;
 
-    long localProducerLimit = (long) PRODUCER_LIMIT_HANDLE.getVolatile(this);
+    long localProducerLimit = producerLimit;
     long cachedHead = 0L; // Local cache of head to reduce volatile reads
 
     int spinCycles = 0;
@@ -99,7 +60,7 @@ class MpscArrayQueueVarHandle<E> extends BaseQueue<E> {
         }
 
         // Update producerLimit so other producers also benefit
-        PRODUCER_LIMIT_HANDLE.setVolatile(this, localProducerLimit);
+        producerLimit = localProducerLimit;
       }
 
       // Attempt to claim a slot
@@ -127,7 +88,7 @@ class MpscArrayQueueVarHandle<E> extends BaseQueue<E> {
 
   @Override
   @SuppressWarnings("unchecked")
-  public E poll() {
+  public final E poll() {
     final Object[] localBuffer = this.buffer;
 
     long currentHead = (long) HEAD_HANDLE.getOpaque(this);
@@ -150,15 +111,8 @@ class MpscArrayQueueVarHandle<E> extends BaseQueue<E> {
 
   @Override
   @SuppressWarnings("unchecked")
-  public E peek() {
+  public final E peek() {
     final int index = (int) ((long) HEAD_HANDLE.getOpaque(this) & mask);
     return (E) ARRAY_HANDLE.getVolatile(buffer, index);
-  }
-
-  @Override
-  public int size() {
-    long currentHead = (long) HEAD_HANDLE.getVolatile(this);
-    long currentTail = (long) TAIL_HANDLE.getVolatile(this);
-    return (int) (currentTail - currentHead);
   }
 }
