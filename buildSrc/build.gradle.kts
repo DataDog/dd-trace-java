e1b081ebb608724e824d6210825e5031c8293acb
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2025-11-12 08:13:51 -0500

    Code cleanup. (#9911)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -86,8 +86,8 @@ testing {
   suites {
     val test by getting(JvmTestSuite::class) {
       dependencies {
-        implementation(libs.spock.core)
         implementation(libs.groovy)
+        implementation(libs.spock.core)
       }
       targets.configureEach {
         testTask.configure {
