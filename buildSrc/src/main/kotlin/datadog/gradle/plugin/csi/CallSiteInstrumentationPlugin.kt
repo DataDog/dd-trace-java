0ebc011639e0c1f3e8b9895374da6e4c6eb706ac
Brice Dutheil <brice.dutheil@gmail.com>
2025-11-14 15:21:31 +0100

    chore: Move CSI plugin to it's own package

--- buildSrc/src/main/kotlin/datadog/gradle/plugin/csi/CallSiteInstrumentationPlugin.kt
+++ buildSrc/src/main/kotlin/datadog/gradle/plugin/csi/CallSiteInstrumentationPlugin.kt
@@ -1,15 +1,10 @@
-package datadog.gradle.plugin
+package datadog.gradle.plugin.csi
 
 import org.gradle.api.GradleException
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.file.ProjectLayout
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.api.provider.ListProperty
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
@@ -24,51 +19,6 @@ import java.nio.file.Paths
 import javax.inject.Inject
 
 private const val CALL_SITE_INSTRUMENTER_MAIN_CLASS = "datadog.trace.plugin.csi.PluginApplication"
-private const val CALL_SITE_CLASS_SUFFIX = "CallSite"
-private const val CALL_SITE_CONSOLE_REPORTER = "CONSOLE"
-private const val CALL_SITE_ERROR_CONSOLE_REPORTER = "ERROR_CONSOLE"
-
-/**
- * This extension allows to configure the Call Site Instrumenter plugin execution.
- */
-abstract class CallSiteInstrumentationExtension @Inject constructor(objectFactory: ObjectFactory, layout: ProjectLayout) {
-  /**
-   * The location of the source code to generate call site ({@code <project>/src/main/java} by default).
-   */
-  val srcFolder: DirectoryProperty = objectFactory.directoryProperty().convention(
-    layout.projectDirectory.dir("src").dir("main").dir("java")
-  )
-  /**
-   * The location to generate call site source code ({@code <project>/build/generated/sources/csi} by default).
-   */
-  val targetFolder: DirectoryProperty = objectFactory.directoryProperty().convention(
-    layout.buildDirectory.dir("generated${File.separatorChar}sources${File.separatorChar}csi")
-  )
-  /**
-   * The generated call site source file suffix (#CALL_SITE_CLASS_SUFFIX by default).
-   */
-  val suffix: Property<String> = objectFactory.property(String::class.java).convention(CALL_SITE_CLASS_SUFFIX)
-  /**
-   * The reporters to use after call site instrumenter run (only #CALL_SITE_CONSOLE_REPORTER and #CALL_SITE_ERROR_CONSOLE_REPORTER supported for now).
-   */
-  val reporters: ListProperty<String> = objectFactory.listProperty(String::class.java).convention(listOf(
-    CALL_SITE_ERROR_CONSOLE_REPORTER
-  ))
-  /**
-   * The location of the dd-trace-java project to look for the call site instrumenter (optional, current project root folder used if not set).
-   */
-  abstract val rootFolder: Property<File>
-
-  /**
-   * The JVM to use to run the call site instrumenter (optional, default JVM used if not set).
-   */
-  abstract val javaVersion: Property<JavaLanguageVersion>
-
-  /**
-   * The JVM arguments to run the call site instrumenter.
-   */
-  val jvmArgs: ListProperty<String> = objectFactory.listProperty(String::class.java).convention(listOf("-Xmx128m", "-Xms64m"))
-}
 
 abstract class CallSiteInstrumentationPlugin : Plugin<Project>{
   @get:Inject
