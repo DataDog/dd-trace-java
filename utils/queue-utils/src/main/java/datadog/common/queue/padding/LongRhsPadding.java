c0ae7a2817126761acc908cd608e8bbe199953c3
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-11-17 11:51:10 +0100

    Pad consumer and producer limits

--- utils/queue-utils/src/main/java/datadog/common/queue/padding/LongRhsPadding.java
+++ utils/queue-utils/src/main/java/datadog/common/queue/padding/LongRhsPadding.java
@@ -3,5 +3,6 @@ package datadog.common.queue.padding;
 /** Right-hand-side (RHS) padding to prevent false sharing. */
 class LongRhsPadding extends LongValue {
   /** 7 bytes fields to occupy space on the right side of the value. */
+  @SuppressWarnings("unused")
   private long p9, p10, p11, p12, p13, p14, p15;
 }
