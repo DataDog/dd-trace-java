62789f36f5cd6e70ade67ea40b06a9b20f253f40
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-11-17 11:19:03 +0100

    Use dedicated class hierarchy to ensure the padding is applied

---
+++ utils/queue-utils/src/main/java/datadog/common/queue/padding/LongValue.java
@@ -0,0 +1,10 @@
+package datadog.common.queue.padding;
+
+/**
+ * Holds the actual sequence value, padded on the left to prevent false sharing with preceding
+ * fields.
+ */
+class LongValue extends LhsPadding {
+  /** The volatile value being protected from false sharing. */
+  protected volatile long value;
+}
---
+++ utils/queue-utils/src/main/java/datadog/common/queue/padding/ThreadValue.java
@@ -0,0 +1,9 @@
+package datadog.common.queue.padding;
+
+/**
+ * Holds the actual thread value, padded on the left to prevent false sharing with preceding fields.
+ */
+class ThreadValue extends LhsPadding {
+  /** A left padded {@link Thread} */
+  protected volatile Thread value;
+}
