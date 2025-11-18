805f42d4eb2db743b318f9595e094f22a26a562c
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2025-11-18 14:28:56 +0100

    Remove equals

--- internal-api/src/main/java/datadog/trace/api/featureflag/exposure/Allocation.java
+++ internal-api/src/main/java/datadog/trace/api/featureflag/exposure/Allocation.java
@@ -1,23 +1,9 @@
 package datadog.trace.api.featureflag.exposure;
 
-import java.util.Objects;
-
 public class Allocation {
   public final String key;
 
   public Allocation(final String key) {
     this.key = key;
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Allocation that = (Allocation) o;
-    return Objects.equals(key, that.key);
   }
 }
