e425d4172e935f51b171301180e64df44120985a
Sarah Chen <sarah.chen@datadoghq.com>
2025-11-20 18:03:43 -0500

    iteration 1

--- benchmark/startup/run.sh
+++ benchmark/startup/run.sh
@@ -2,4 +2,37 @@
 set -eu
 
 source "${UTILS_DIR}/update-java-version.sh" 17
-"${UTILS_DIR}/run-sirun-benchmarks.sh" "$@"
+
+# from run-sirun-benchmarks.sh, matches dacapo and load benchmark run.sh scripts
+function message() {
+  echo "$(date +"%T"): $1"
+}
+
+run_benchmark() {
+  local type=$1
+  local app=$2
+  if [[ -d "${app}" ]] && [[ -f "${app}/benchmark.json" ]]; then
+
+    message "${type} benchmark: ${app} started"
+    cd "${app}"
+
+    # create output folder for the test
+    export OUTPUT_DIR="${REPORTS_DIR}/${type}/${app}"
+    mkdir -p "${OUTPUT_DIR}"
+
+    # run the bp-runner test
+    bp-runner bp-runner.yml &>"${OUTPUT_DIR}/${app}.json"
+
+    message "${type} benchmark: ${app} finished"
+
+    cd ..
+  fi
+}
+
+if [ "$#" == '2' ]; then
+  run_benchmark "$@"
+else
+  for folder in *; do
+    run_benchmark "$1" "${folder}"
+  done
+fi
