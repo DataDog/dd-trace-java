5612b44f2f9f91b01d90a1e49055c7c4195f8bf9
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-11-22 15:30:34 +0000

    Avoid expensive Class.getMethod call in code-origin advice

--- dd-java-agent/agent-debugger/src/test/java/com/datadog/debugger/origin/CodeOriginTestAdvice.java
+++ dd-java-agent/agent-debugger/src/test/java/com/datadog/debugger/origin/CodeOriginTestAdvice.java
@@ -3,15 +3,17 @@ package com.datadog.debugger.origin;
 import static datadog.trace.bootstrap.debugger.DebuggerContext.captureCodeOrigin;
 import static datadog.trace.bootstrap.debugger.DebuggerContext.marker;
 
-import java.lang.reflect.Method;
 import net.bytebuddy.asm.Advice;
 
 public class CodeOriginTestAdvice {
 
   @Advice.OnMethodEnter
   @SuppressWarnings("bytebuddy-exception-suppression")
-  public static void onEnter(@Advice.Origin final Method method) {
+  public static void onEnter(
+      @Advice.Origin("#t") String typeName,
+      @Advice.Origin("#m") String methodName,
+      @Advice.Origin("#d") String descriptor) {
     marker();
-    captureCodeOrigin(method, true);
+    captureCodeOrigin(typeName, methodName, descriptor, true);
   }
 }
