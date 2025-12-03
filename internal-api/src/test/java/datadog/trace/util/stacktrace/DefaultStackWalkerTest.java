b897cd68a24c4c062c2bfdbd29fa9fb2cbd5f769
Brice Dutheil <brice.dutheil@gmail.com>
2025-12-03 17:21:56 +0100

    fix: Ignore tests failing on Oracle JDK 1.8
    Those tests can be ignored as they are not specific to Oracle.

--- internal-api/src/test/java/datadog/trace/util/stacktrace/DefaultStackWalkerTest.java
+++ internal-api/src/test/java/datadog/trace/util/stacktrace/DefaultStackWalkerTest.java
@@ -5,13 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
+import datadog.environment.JavaVirtualMachine;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DefaultStackWalkerTest {
 
   private final StackWalker stackWalker = new DefaultStackWalker();
+
+  @BeforeEach
+  void setUp() {
+    assumeFalse(
+        JavaVirtualMachine.isOracleJDK8(),
+        "Oracle JDK 1.8 did not merge the fix in JDK-8058322, leading to the JVM failing to correctly "
+            + "extract method parameters without args, when the code is compiled on a later JDK (targeting 8). "
+            + "This can manifest when creating mocks.");
+  }
 
   @Test
   public void defaultStackWalker_must_be_enabled() {
