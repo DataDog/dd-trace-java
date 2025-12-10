fe77906c63aa279679ab86d37022df827340de68
Valentin Zakharov <valentin.zakharov@datadoghq.com>
2025-12-10 10:04:20 +0100

    Multi-tracing support for Couchbase 3.2+

---
+++ dd-java-agent/instrumentation/couchbase/couchbase-3.2/src/main/java/datadog/trace/instrumentation/couchbase_32/client/DelegatingRequestSpan.java
@@ -0,0 +1,72 @@
+package datadog.trace.instrumentation.couchbase_32.client;
+
+import com.couchbase.client.core.cnc.RequestSpan;
+import com.couchbase.client.core.msg.RequestContext;
+import java.time.Instant;
+
+/** RequestSpan, which delegates all calls to two other RequestSpans */
+public class DelegatingRequestSpan implements RequestSpan {
+
+  private final RequestSpan ddSpan;
+  private final RequestSpan cncSpan;
+
+  public DelegatingRequestSpan(RequestSpan ddSpan, RequestSpan cncSpan) {
+    this.ddSpan = ddSpan;
+    this.cncSpan = cncSpan;
+  }
+
+  public RequestSpan getDatadogSpan() {
+    return ddSpan;
+  }
+
+  public RequestSpan getCncSpan() {
+    return cncSpan;
+  }
+
+  @Override
+  public void attribute(String key, String value) {
+    // TODO: add null checks
+    ddSpan.attribute(key, value);
+    cncSpan.attribute(key, value);
+  }
+
+  @Override
+  public void attribute(String key, boolean value) {
+    ddSpan.attribute(key, value);
+    cncSpan.attribute(key, value);
+  }
+
+  @Override
+  public void attribute(String key, long value) {
+    ddSpan.attribute(key, value);
+    cncSpan.attribute(key, value);
+  }
+
+  @Override
+  public void event(String name, Instant timestamp) {
+    ddSpan.event(name, timestamp);
+    cncSpan.event(name, timestamp);
+  }
+
+  @Override
+  public void status(StatusCode status) {
+    ddSpan.status(status);
+    cncSpan.status(status);
+  }
+
+  @Override
+  public void end() {
+    try {
+      ddSpan.end();
+    } finally {
+      // guarantee cnc spans get ended even if ddSpan.end() throws exception
+      cncSpan.end();
+    }
+  }
+
+  @Override
+  public void requestContext(RequestContext requestContext) {
+    ddSpan.requestContext(requestContext);
+    cncSpan.requestContext(requestContext);
+  }
+}
