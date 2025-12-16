9b6a1e95a388916145b3dcb11d882816e23485e7
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-12-16 10:45:24 +0100

    feat(test): Add instrumentation test for JUnit

---
+++ dd-java-agent/instrumentation-testing/src/main/java/datadog/trace/agent/test/assertions/NonNull.java
@@ -0,0 +1,20 @@
+package datadog.trace.agent.test.assertions;
+
+import java.util.Optional;
+
+public class NonNull<T> implements Matcher<T> {
+  @Override
+  public Optional<T> expected() {
+    return Optional.empty();
+  }
+
+  @Override
+  public String message() {
+    return "Non-null value expected";
+  }
+
+  @Override
+  public boolean test(T t) {
+    return t != null;
+  }
+}
