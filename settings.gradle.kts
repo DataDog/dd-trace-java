5477fa0d27059903196d4cc9ea62416f7444f7f3
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2025-12-16 13:45:16 -0500

    Adjusted check for akka token to validate that it is not blank. (#10220)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -627,7 +627,7 @@ include(
 )
 
 // Optional `akka-http-10.6` instrumentation (see BUILDING.md for how to enable it):
-if (providers.gradleProperty("akkaRepositoryToken").isPresent) {
+if (providers.gradleProperty("akkaRepositoryToken").getOrElse("").isNotBlank()) {
   include(
     ":dd-java-agent:instrumentation:akka:akka-http:akka-http-10.6"
   )
