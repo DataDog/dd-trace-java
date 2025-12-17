5c4823ab686a8c4b3da2d7e130b0c3a5e589f21a
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2025-12-17 14:28:29 -0500

    Fixed gradle smoke tests.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -177,7 +177,7 @@ include(
   ":dd-smoke-tests:custom-systemloader",
   ":dd-smoke-tests:dynamic-config",
   ":dd-smoke-tests:field-injection",
-  // ":dd-smoke-tests:gradle",
+  ":dd-smoke-tests:gradle",
   ":dd-smoke-tests:grpc-1.5",
   ":dd-smoke-tests:java9-modules",
   ":dd-smoke-tests:jersey-2",
