65177e937dbbcefc9ce1f8176bd25825d9c06df1
Andrea Marziali <andrea.marziali@datadoghq.com>
2025-12-18 12:03:11 +0100

    rename kotlin-coroutines

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -442,7 +442,7 @@ include(
   ":dd-java-agent:instrumentation:kafka:kafka-streams-0.11",
   ":dd-java-agent:instrumentation:kafka:kafka-streams-1.0",
   ":dd-java-agent:instrumentation:karate-1.0",
-  ":dd-java-agent:instrumentation:kotlin-coroutines",
+  ":dd-java-agent:instrumentation:kotlin-coroutines-1.3",
   ":dd-java-agent:instrumentation:lettuce:lettuce-4.0",
   ":dd-java-agent:instrumentation:lettuce:lettuce-5.0",
   ":dd-java-agent:instrumentation:liberty:liberty-20.0",
