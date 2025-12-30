6fb0d63ac60a474b5f4bd0c3926d07b63b3131fc
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2025-12-30 14:24:19 +0000

    Update embedded copy of SerialVersionUIDAdder to pick up changes in ASM 9.9 (#10274)
    (specifically https://gitlab.ow2.org/asm/asm/-/issues/318032)

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/context/asm/SerialVersionUIDAdder.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/context/asm/SerialVersionUIDAdder.java
@@ -150,7 +150,7 @@ public class SerialVersionUIDAdder extends ClassVisitor {
    * @throws IllegalStateException If a subclass calls this constructor.
    */
   public SerialVersionUIDAdder(final ClassVisitor classVisitor) {
-    this(/* latest api = */ Opcodes.ASM8, classVisitor);
+    this(/* latest api = */ Opcodes.ASM9, classVisitor);
     if (getClass() != SerialVersionUIDAdder.class) {
       throw new IllegalStateException();
     }
@@ -159,9 +159,8 @@ public class SerialVersionUIDAdder extends ClassVisitor {
   /**
    * Constructs a new {@link SerialVersionUIDAdder}.
    *
-   * @param api the ASM API version implemented by this visitor. Must be one of {@link
-   *     Opcodes#ASM4}, {@link Opcodes#ASM5}, {@link Opcodes#ASM6}, {@link Opcodes#ASM7} or {@link
-   *     Opcodes#ASM8}.
+   * @param api the ASM API version implemented by this visitor. Must be one of the {@code
+   *     ASM}<i>x</i> values in {@link Opcodes}.
    * @param classVisitor a {@link ClassVisitor} to which this visitor will delegate calls.
    */
   protected SerialVersionUIDAdder(final int api, final ClassVisitor classVisitor) {
@@ -424,7 +423,7 @@ public class SerialVersionUIDAdder extends ClassVisitor {
   // DontCheck(AbbreviationAsWordInName): can't be renamed (for backward binary compatibility).
   protected byte[] computeSHAdigest(final byte[] value) {
     try {
-      return MessageDigest.getInstance("SHA").digest(value);
+      return MessageDigest.getInstance("SHA-1").digest(value);
     } catch (NoSuchAlgorithmException e) {
       throw new UnsupportedOperationException(e);
     }
