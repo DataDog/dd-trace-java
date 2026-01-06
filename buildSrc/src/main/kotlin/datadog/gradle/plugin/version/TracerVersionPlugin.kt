dc9952077ed4e14590008157b251784a5d23cb7b
songlq <18332731561@163.com>
2026-01-06 16:10:33 +0800

    version

--- buildSrc/src/main/kotlin/datadog/gradle/plugin/version/TracerVersionPlugin.kt
+++ buildSrc/src/main/kotlin/datadog/gradle/plugin/version/TracerVersionPlugin.kt
@@ -23,9 +23,9 @@ class TracerVersionPlugin @Inject constructor(
     val extension = targetProject.extensions.getByType(TracerVersionExtension::class.java)
 
     extension.detectDirty.set(
-       providerFactory.gradleProperty("tracerVersion.dirtiness")
-         .map { it.trim().toBoolean() }
-         .orElse(false)
+      providerFactory.gradleProperty("tracerVersion.dirtiness")
+        .map { it.trim().toBoolean() }
+        .orElse(false)
     )
 
     val versionProvider = versionProvider(targetProject, extension)
@@ -54,6 +54,7 @@ class TracerVersionPlugin @Inject constructor(
               logger.info("Incrementing patch because release branch : $currentBranch")
               nextPatchVersion()
             }
+
             else -> {
               logger.info("Incrementing minor")
               nextMinorVersion()
@@ -104,7 +105,11 @@ class TracerVersionPlugin @Inject constructor(
     }
   }
 
-  private fun toTracerVersion(describeString: String, extension: TracerVersionExtension, nextVersion: Version.() -> Version): String {
+  private fun toTracerVersion(
+    describeString: String,
+    extension: TracerVersionExtension,
+    nextVersion: Version.() -> Version
+  ): String {
     logger.info("Git describe output: {}", describeString)
 
     val tagPrefix = extension.tagVersionPrefix.get()
@@ -114,11 +119,12 @@ class TracerVersionPlugin @Inject constructor(
 
     val (lastTagVersion, describeTrailer) = matchResult.destructured
     val hasLaterCommits = describeTrailer.isNotBlank()
-    val version = Version.parse(lastTagVersion).let {
-      if (hasLaterCommits) {
-        it.nextVersion()
-      } else {
-        it
+    val version = if (describeString.contains("-ext")) {
+      // 如果包含 -ext，直接返回完整的 describeString，或者根据需求截取
+      return describeString.replace(tagPrefix, "")
+    } else {
+      Version.parse(lastTagVersion).let {
+        if (hasLaterCommits) it.nextVersion() else it
       }
     }
 
