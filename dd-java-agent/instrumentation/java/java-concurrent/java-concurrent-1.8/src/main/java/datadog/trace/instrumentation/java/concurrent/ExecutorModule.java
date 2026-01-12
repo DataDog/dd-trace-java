65d89d0ed6a2c09af46caf0a9c20a666f643a300
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-01-12 12:20:54 +0100

    finalize executor module

--- dd-java-agent/instrumentation/java/java-concurrent/java-concurrent-1.8/src/main/java/datadog/trace/instrumentation/java/concurrent/ExecutorModule.java
+++ dd-java-agent/instrumentation/java/java-concurrent/java-concurrent-1.8/src/main/java/datadog/trace/instrumentation/java/concurrent/ExecutorModule.java
@@ -1,14 +1,22 @@
 package datadog.trace.instrumentation.java.concurrent;
 
 import static datadog.trace.instrumentation.java.concurrent.ConcurrentInstrumentationNames.EXECUTOR_INSTRUMENTATION_NAME;
+import static datadog.trace.instrumentation.java.concurrent.executor.ThreadPoolExecutorInstrumentation.TPE;
 import static java.util.Collections.singleton;
 
 import com.google.auto.service.AutoService;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
 import datadog.trace.api.InstrumenterConfig;
+import datadog.trace.bootstrap.instrumentation.java.concurrent.State;
+import datadog.trace.instrumentation.java.concurrent.executor.JavaExecutorInstrumentation;
+import datadog.trace.instrumentation.java.concurrent.executor.NonStandardExecutorInstrumentation;
+import datadog.trace.instrumentation.java.concurrent.executor.RejectedExecutionHandlerInstrumentation;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.RunnableFuture;
 
 @AutoService(InstrumenterModule.class)
 public class ExecutorModule extends InstrumenterModule.Tracing {
@@ -17,12 +25,30 @@ public class ExecutorModule extends InstrumenterModule.Tracing {
   }
 
   @Override
+  public Map<String, String> contextStore() {
+    final Map<String, String> contextStore = new HashMap<>();
+    contextStore.put(Runnable.class.getName(), State.class.getName());
+    contextStore.put(TPE, Boolean.class.getName());
+    contextStore.put(RunnableFuture.class.getName(), State.class.getName());
+    return contextStore;
+  }
+
+  @Override
   public List<Instrumenter> typeInstrumentations() {
-    final List<Instrumenter> instrumenters = new ArrayList<>(2);
+    final List<Instrumenter> instrumenters = new ArrayList<>(5);
+    final InstrumenterConfig config = InstrumenterConfig.get();
     instrumenters.add(new AsyncPropagatingDisableInstrumentation());
     if (InstrumenterConfig.get().isIntegrationEnabled(singleton("new-task-for"), true)) {
       instrumenters.add(new WrapRunnableAsNewTaskInstrumentation());
     }
+    instrumenters.add(new JavaExecutorInstrumentation());
+    if (config.isIntegrationEnabled(singleton(EXECUTOR_INSTRUMENTATION_NAME + ".other"), true)) {
+      instrumenters.add(new NonStandardExecutorInstrumentation());
+    }
+    if (config.isIntegrationEnabled(singleton("rejected-execution-handler"), true)) {
+      instrumenters.add(new RejectedExecutionHandlerInstrumentation());
+    }
+
     return instrumenters;
   }
 }
