98d746e7621bdaec794182f2efb7b61eaad4bc85
Augusto de Oliveira <augusto.deoliveira@datadoghq.com>
2026-01-16 16:27:14 -0300

    N=100,M=1,warmup=160

--- benchmark/load/petclinic/k6.js
+++ benchmark/load/petclinic/k6.js
@@ -28,7 +28,7 @@ export const options = function (variants) {
     scenarios[`load--petclinic--${variant}--warmup`] = {
       executor: 'constant-vus',  // https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/#all-executors
       vus: 5,
-      duration: '20s',
+      duration: '160s',
       gracefulStop: '2s',
       env: {
         "APP_URL": variants[variant]["APP_URL"]
@@ -38,8 +38,8 @@ export const options = function (variants) {
     scenarios[`load--petclinic--${variant}--high_load`] = {
       executor: 'constant-vus',
       vus: 5,
-      startTime: '22s',
-      duration: '300s',
+      startTime: '162s',
+      duration: '100s',
       gracefulStop: '2s',
       env: {
         "APP_URL": variants[variant]["APP_URL"]
