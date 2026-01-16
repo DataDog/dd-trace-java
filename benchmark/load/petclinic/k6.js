fbe601b3f959bf5b3165e19766812235366c5401
Augusto de Oliveira <augusto.deoliveira@datadoghq.com>
2026-01-16 16:22:33 -0300

    N=300,M=1,warmup=20

--- benchmark/load/petclinic/k6.js
+++ benchmark/load/petclinic/k6.js
@@ -39,7 +39,7 @@ export const options = function (variants) {
       executor: 'constant-vus',
       vus: 5,
       startTime: '22s',
-      duration: '15s',
+      duration: '300s',
       gracefulStop: '2s',
       env: {
         "APP_URL": variants[variant]["APP_URL"]
