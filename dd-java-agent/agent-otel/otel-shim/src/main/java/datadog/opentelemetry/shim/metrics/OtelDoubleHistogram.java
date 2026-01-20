908f683e82ea20fbe22c22ca39a83363595ac9b6
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2026-01-20 22:51:30 +0000

    Validate and capture explicit bucket boundaries

--- dd-java-agent/agent-otel/otel-shim/src/main/java/datadog/opentelemetry/shim/metrics/OtelDoubleHistogram.java
+++ dd-java-agent/agent-otel/otel-shim/src/main/java/datadog/opentelemetry/shim/metrics/OtelDoubleHistogram.java
@@ -2,6 +2,8 @@ package datadog.opentelemetry.shim.metrics;
 
 import static datadog.opentelemetry.shim.metrics.OtelInstrumentBuilder.ofDoubles;
 import static datadog.opentelemetry.shim.metrics.OtelInstrumentType.HISTOGRAM;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
 
 import datadog.trace.relocate.api.RatelimitedLogger;
 import io.opentelemetry.api.common.Attributes;
@@ -9,8 +11,11 @@ import io.opentelemetry.api.metrics.DoubleHistogram;
 import io.opentelemetry.api.metrics.DoubleHistogramBuilder;
 import io.opentelemetry.api.metrics.LongHistogramBuilder;
 import io.opentelemetry.context.Context;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.slf4j.LoggerFactory;
 
@@ -20,8 +25,11 @@ final class OtelDoubleHistogram extends OtelInstrument implements DoubleHistogra
       new RatelimitedLogger(
           LoggerFactory.getLogger(OtelDoubleHistogram.class), 5, TimeUnit.MINUTES);
 
-  OtelDoubleHistogram(OtelInstrumentDescriptor descriptor) {
+  @Nullable private final List<Double> bucketBoundaries;
+
+  OtelDoubleHistogram(OtelInstrumentDescriptor descriptor, List<Double> bucketBoundaries) {
     super(descriptor);
+    this.bucketBoundaries = bucketBoundaries;
   }
 
   @Override
@@ -49,6 +57,8 @@ final class OtelDoubleHistogram extends OtelInstrument implements DoubleHistogra
   static final class Builder implements DoubleHistogramBuilder {
     private final OtelInstrumentBuilder instrumentBuilder;
 
+    private List<Double> bucketBoundaries;
+
     Builder(OtelMeter meter, String instrumentName) {
       this.instrumentBuilder = ofDoubles(meter, instrumentName, HISTOGRAM);
     }
@@ -67,18 +77,51 @@ final class OtelDoubleHistogram extends OtelInstrument implements DoubleHistogra
 
     @Override
     public DoubleHistogramBuilder setExplicitBucketBoundariesAdvice(List<Double> bucketBoundaries) {
-      // FIXME: implement boundary advice
+      try {
+        Objects.requireNonNull(bucketBoundaries, "bucketBoundaries must not be null");
+        this.bucketBoundaries =
+            validateBoundaries(unmodifiableList(new ArrayList<>(bucketBoundaries)));
+      } catch (NullPointerException | IllegalArgumentException e) {
+        log.warn("Error setting explicit bucket boundaries advice: " + e.getMessage());
+      }
       return this;
     }
 
     @Override
     public LongHistogramBuilder ofLongs() {
-      return new OtelLongHistogram.Builder(instrumentBuilder);
+      return new OtelLongHistogram.Builder(instrumentBuilder, bucketBoundaries);
     }
 
     @Override
     public DoubleHistogram build() {
-      return new OtelDoubleHistogram(instrumentBuilder.toDescriptor());
+      return new OtelDoubleHistogram(instrumentBuilder.toDescriptor(), bucketBoundaries);
+    }
+
+    static List<Double> validateBoundaries(List<Double> boundaries) {
+      if (boundaries.isEmpty()) {
+        return emptyList();
+      }
+      if (boundaries.get(0) == Double.NEGATIVE_INFINITY) {
+        throw new IllegalArgumentException("invalid bucket boundary: -Inf");
+      }
+      if (boundaries.get(boundaries.size() - 1) == Double.POSITIVE_INFINITY) {
+        throw new IllegalArgumentException("invalid bucket boundary: +Inf");
+      }
+      Double previousBoundary = null;
+      for (Double boundary : boundaries) {
+        if (boundary.isNaN()) {
+          throw new IllegalArgumentException("invalid bucket boundary: NaN");
+        }
+        if (previousBoundary != null && previousBoundary >= boundary) {
+          throw new IllegalArgumentException(
+              "Bucket boundaries must be in increasing order: "
+                  + previousBoundary
+                  + " >= "
+                  + boundary);
+        }
+        previousBoundary = boundary;
+      }
+      return boundaries;
     }
   }
 }
