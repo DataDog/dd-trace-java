3c0341ef7261cbc74d71f065ad87cd2929234792
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2026-01-20 22:51:30 +0000

    Validate and capture explicit bucket boundaries

--- dd-java-agent/agent-otel/otel-shim/src/main/java/datadog/opentelemetry/shim/metrics/OtelLongHistogram.java
+++ dd-java-agent/agent-otel/otel-shim/src/main/java/datadog/opentelemetry/shim/metrics/OtelLongHistogram.java
@@ -1,7 +1,10 @@
 package datadog.opentelemetry.shim.metrics;
 
+import static datadog.opentelemetry.shim.metrics.OtelDoubleHistogram.Builder.validateBoundaries;
 import static datadog.opentelemetry.shim.metrics.OtelInstrumentBuilder.ofLongs;
 import static datadog.opentelemetry.shim.metrics.OtelInstrumentType.HISTOGRAM;
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toList;
 
 import datadog.trace.relocate.api.RatelimitedLogger;
 import io.opentelemetry.api.common.Attributes;
@@ -9,7 +12,9 @@ import io.opentelemetry.api.metrics.LongHistogram;
 import io.opentelemetry.api.metrics.LongHistogramBuilder;
 import io.opentelemetry.context.Context;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.slf4j.LoggerFactory;
 
@@ -18,8 +23,11 @@ final class OtelLongHistogram extends OtelInstrument implements LongHistogram {
   private static final RatelimitedLogger log =
       new RatelimitedLogger(LoggerFactory.getLogger(OtelLongHistogram.class), 5, TimeUnit.MINUTES);
 
-  OtelLongHistogram(OtelInstrumentDescriptor descriptor) {
+  @Nullable private final List<Double> bucketBoundaries;
+
+  OtelLongHistogram(OtelInstrumentDescriptor descriptor, List<Double> bucketBoundaries) {
     super(descriptor);
+    this.bucketBoundaries = bucketBoundaries;
   }
 
   @Override
@@ -47,8 +55,11 @@ final class OtelLongHistogram extends OtelInstrument implements LongHistogram {
   static final class Builder implements LongHistogramBuilder {
     private final OtelInstrumentBuilder instrumentBuilder;
 
-    Builder(OtelInstrumentBuilder builder) {
+    private List<Double> bucketBoundaries;
+
+    Builder(OtelInstrumentBuilder builder, List<Double> bucketBoundaries) {
       this.instrumentBuilder = ofLongs(builder, HISTOGRAM);
+      this.bucketBoundaries = bucketBoundaries;
     }
 
     @Override
@@ -65,13 +76,21 @@ final class OtelLongHistogram extends OtelInstrument implements LongHistogram {
 
     @Override
     public LongHistogramBuilder setExplicitBucketBoundariesAdvice(List<Long> bucketBoundaries) {
-      // FIXME: implement boundary advice
+      try {
+        Objects.requireNonNull(bucketBoundaries, "bucketBoundaries must not be null");
+        this.bucketBoundaries =
+            validateBoundaries(
+                unmodifiableList(
+                    bucketBoundaries.stream().map(Long::doubleValue).collect(toList())));
+      } catch (NullPointerException | IllegalArgumentException e) {
+        log.warn("Error setting explicit bucket boundaries advice: " + e.getMessage());
+      }
       return this;
     }
 
     @Override
     public LongHistogram build() {
-      return new OtelLongHistogram(instrumentBuilder.toDescriptor());
+      return new OtelLongHistogram(instrumentBuilder.toDescriptor(), bucketBoundaries);
     }
   }
 }
