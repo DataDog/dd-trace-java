ad03cb59fd0b706078f25518f2e6b8e7ce025a6d
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2026-01-23 16:07:41 -0500

    Reverted changes to failed test and suppress IDEA warning with comment.

--- dd-smoke-tests/kafka-2/src/main/java/datadog/smoketest/kafka/iast/IastController.java
+++ dd-smoke-tests/kafka-2/src/main/java/datadog/smoketest/kafka/iast/IastController.java
@@ -141,13 +141,15 @@ public class IastController {
     }
   }
 
+  // Intentionally not using logger placeholders to exercise this test case.
+  @SuppressWarnings("StringConcatenationArgumentToLogCall")
   private String handle(final String topic, final String key, final String value) {
     LOGGER.info("Received message from {}: {} {}", topic, key, value);
     if (isKey(key)) {
-      LOGGER.info("Kafka tainted key: {}", key);
+      LOGGER.info("Kafka tainted key: " + key);
       return "OK";
     } else if (isValue(value)) {
-      LOGGER.info("Kafka tainted value: {}", value);
+      LOGGER.info("Kafka tainted value: " + value);
       return "OK";
     } else if ("health".equals(key)) {
       return "OK";
