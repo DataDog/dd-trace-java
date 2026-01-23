4c701715d920c2a35705b67ec5a9473835aef5d5
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-01-23 08:50:07 +0100

    leftover on timer instrumentation

--- dd-java-agent/instrumentation/java/java-concurrent/java-concurrent-1.8/src/main/java/datadog/trace/instrumentation/java/concurrent/timer/JavaTimerModule.java
+++ dd-java-agent/instrumentation/java/java-concurrent/java-concurrent-1.8/src/main/java/datadog/trace/instrumentation/java/concurrent/timer/JavaTimerModule.java
@@ -1,21 +1,16 @@
 package datadog.trace.instrumentation.java.concurrent.timer;
 
-import static datadog.trace.bootstrap.instrumentation.java.concurrent.AdviceUtils.cancelTask;
-import static datadog.trace.bootstrap.instrumentation.java.concurrent.AdviceUtils.capture;
-import static datadog.trace.bootstrap.instrumentation.java.concurrent.ExcludeFilter.ExcludeType.RUNNABLE;
-import static datadog.trace.bootstrap.instrumentation.java.concurrent.ExcludeFilter.exclude;
 import static datadog.trace.instrumentation.java.concurrent.ConcurrentInstrumentationNames.EXECUTOR_INSTRUMENTATION_NAME;
 import static datadog.trace.instrumentation.java.concurrent.ConcurrentInstrumentationNames.RUNNABLE_INSTRUMENTATION_NAME;
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonMap;
 
 import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
-import datadog.trace.bootstrap.ContextStore;
-import datadog.trace.bootstrap.InstrumentationContext;
 import datadog.trace.bootstrap.instrumentation.java.concurrent.State;
+import java.util.List;
 import java.util.Map;
-import java.util.TimerTask;
-import net.bytebuddy.asm.Advice;
 
 @AutoService(InstrumenterModule.class)
 public class JavaTimerModule extends InstrumenterModule.ContextTracking {
@@ -28,25 +23,8 @@ public class JavaTimerModule extends InstrumenterModule.ContextTracking {
     return singletonMap("java.lang.Runnable", State.class.getName());
   }
 
-  public static final class TimerScheduleAdvice {
-    @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void before(@Advice.Argument(0) TimerTask task, @Advice.Argument(2) long period) {
-      // don't propagate fixed time / rate executions
-      if (period != 0) {
-        return;
-      }
-      if (!exclude(RUNNABLE, task)) {
-        ContextStore<Runnable, State> contextStore =
-            InstrumentationContext.get(Runnable.class, State.class);
-        capture(contextStore, task);
-      }
-    }
-
-    @Advice.OnMethodExit(onThrowable = Throwable.class)
-    public static void after(@Advice.Argument(0) TimerTask task, @Advice.Thrown Throwable thrown) {
-      if (null != thrown && !exclude(RUNNABLE, task)) {
-        cancelTask(InstrumentationContext.get(Runnable.class, State.class), task);
-      }
-    }
+  @Override
+  public List<Instrumenter> typeInstrumentations() {
+    return asList(new JavaTimerInstrumentation(), new TimerTaskInstrumentation());
   }
 }
