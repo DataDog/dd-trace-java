af9d5cc86417297e717a69185d9cb81ad96bed61
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2026-01-28 08:47:09 -0500

    Bump Google Java Format.

--- buildSrc/call-site-instrumentation-plugin/build.gradle.kts
+++ buildSrc/call-site-instrumentation-plugin/build.gradle.kts
@@ -17,7 +17,7 @@ spotless {
     target("src/**/*.java")
     // ignore embedded test projects
     targetExclude("src/test/resources/**")
-    googleJavaFormat("1.32.0")
+    googleJavaFormat("1.33.0")
   }
 }
 
