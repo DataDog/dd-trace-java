33c7987196766c3891ea2becea9b2c6ce812c96d
Bruce Bujon <bruce.bujon@datadoghq.com>
2026-01-29 16:43:25 +0100

    feat(http): Migrate communication build script

--- communication/build.gradle.kts
+++ communication/build.gradle.kts
@@ -2,44 +2,19 @@ import groovy.lang.Closure
 
 plugins {
   `java-library`
-  idea
 }
 
 description = "communication"
 
 apply(from = rootDir.resolve("gradle/java.gradle"))
 
-// Manually configure Java 11 source set (like agent-bootstrap)
-// Cannot use tracerJava.addSourceSetFor because it creates circular dependencies
-// when Java 11 source set needs to see main source set classes
-sourceSets {
-  create("main_java11") {
-    java.srcDirs("${project.projectDir}/src/main/java11")
-  }
-}
-
-fun AbstractCompile.configureCompiler(javaVersionInteger: Int, compatibilityVersion: JavaVersion? = null, unsetReleaseFlagReason: String? = null) {
-  (project.extra["configureCompiler"] as Closure<*>).call(this, javaVersionInteger, compatibilityVersion, unsetReleaseFlagReason)
-}
-
-tasks.named<JavaCompile>("compileMain_java11Java") {
-  configureCompiler(11, JavaVersion.VERSION_1_8)
-}
-
-tasks.named<Jar>("jar") {
-  from(sourceSets["main_java11"].output)
-}
-
-idea {
-  module {
-    jdkName = "11"
-  }
-}
-
 dependencies {
   implementation(libs.slf4j)
 
+  api(project(":components:http:http-api"))
   api(project(":remote-config:remote-config-api"))
+  implementation(project(":components:http:http-compat"))
+  implementation(project(":components:http:http-lib"))
   implementation(project(":remote-config:remote-config-core"))
   implementation(project(":internal-api"))
   implementation(project(":utils:container-utils"))
@@ -47,18 +22,14 @@ dependencies {
   implementation(project(":utils:socket-utils"))
   implementation(project(":utils:version-utils"))
 
-  api(libs.okio)
-  api(libs.okhttp)
   api(libs.moshi)
   // metrics-lib is needed rather than metrics-api to change the default port of StatsD connection manager
   // TODO Could help decoupling it later to only depend on metrics-api
   implementation(project(":products:metrics:metrics-lib"))
 
-  // Java 11 source set needs access to main source set and dependencies
-  "main_java11CompileOnly"(project(":internal-api"))
-  "main_java11CompileOnly"(sourceSets["main"].output)
-
   testImplementation(project(":utils:test-utils"))
+  testImplementation(libs.okio)
+  testImplementation(libs.okhttp)
   testImplementation(libs.bundles.junit5)
   testImplementation(libs.bytebuddy)
   testImplementation("org.msgpack:msgpack-core:0.8.20")
