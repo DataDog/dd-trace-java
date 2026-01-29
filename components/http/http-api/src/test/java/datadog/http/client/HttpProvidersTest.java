5d0633e73b3b4a0c82636383a9d405125a8e351b
Bruce Bujon <bruce.bujon@datadoghq.com>
2026-01-29 10:50:17 +0100

    feat(http): Adding HttpProviders tests

--- components/http/http-api/src/test/java/datadog/http/client/HttpProvidersTest.java
+++ components/http/http-api/src/test/java/datadog/http/client/HttpProvidersTest.java
@@ -2,19 +2,49 @@ package datadog.http.client;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 abstract class HttpProvidersTest {
 
   abstract String getImplementationPackage();
 
+  @ParameterizedTest(name = "[{index}] {0} builder")
+  @MethodSource("builders")
+  void testNewBuilder(String type, Supplier<?> builderSupplier) {
+    Object builder = builderSupplier.get();
+    assertType(builder);
+  }
+
+  static Stream<Arguments> builders() {
+    return Stream.of(
+        arguments("client", (Supplier<?>) HttpProviders::newClientBuilder),
+        arguments("request", (Supplier<?>) HttpProviders::newClientBuilder),
+        arguments("url", (Supplier<?>) HttpProviders::newClientBuilder));
+  }
+
   @Test
-  void testNewClientBuilder() {
-    HttpRequest.Builder builder = HttpProviders.newRequestBuilder();
+  void testHttpUrlParse() {
+    HttpUrl url = HttpProviders.httpUrlParse("http://localhost");
+    assertType(url);
+  }
+
+  @Test
+  void testRequestBodyOfString() {
+    HttpRequestBody body = HttpProviders.requestBodyOfString("content");
+    assertType(body);
+  }
+
+  private  void assertType(Object builder) {
     assertNotNull(builder);
     assertTrue(builder.getClass().getName().startsWith(getImplementationPackage()));
   }
