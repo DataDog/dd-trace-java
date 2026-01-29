9991f871273f68c5cdd1d0daebe1d32ba8bfe6a3
Bruce Bujon <bruce.bujon@datadoghq.com>
2026-01-29 12:44:36 +0100

    feat(http): Add OkHttp compat implementation

--- components/http/http-compat/build.gradle.kts
+++ components/http/http-compat/build.gradle.kts
@@ -8,4 +8,6 @@ description = "HTTP Client Library - Java 8 Compatibility"
 
 dependencies {
   api(project(":components:http:http-api"))
+  implementation(libs.okhttp)
+  testImplementation(testFixtures(project(":components:http:http-api")))
 }
