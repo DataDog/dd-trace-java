531f63b5da0d1536eb3810ed9283aeffac57a03d
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2026-01-29 16:42:49 -0500

    Adding test for TagMap.Entry setters

---
+++ dd-trace-core/src/test/java/datadog/trace/core/SpanTest.java
@@ -0,0 +1,26 @@
+package datadog.trace.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import datadog.trace.api.TagMap;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+
+public class SpanTest {
+  static final CoreTracer TRACER = CoreTracer.builder().build();
+  
+  @Test
+  public void setTag_Entry() {
+	AgentSpan span = TRACER.startSpan("foo", "foo");
+	span.setTag(TagMap.Entry.create("message", "hello"));
+	
+	assertEquals("hello", span.getTag("message"));
+  }
+  
+  @Test
+  public void setMetric_Entry() {
+	AgentSpan span = TRACER.startSpan("foo", "foo");
+	span.setMetric(TagMap.Entry.create("metric", 20L));
+	
+	assertEquals(Long.valueOf(20L), span.getTag("metric"));
+  }
+}
