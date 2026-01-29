f5391f31f03687fc84f86778ea1c9453aebc061b
Bruce Bujon <bruce.bujon@datadoghq.com>
2026-01-29 09:03:56 +0100

    feat(http): Adding HttpProviders tests

--- components/http/http-api/build.gradle.kts
+++ components/http/http-api/build.gradle.kts
@@ -9,5 +9,9 @@ description = "HTTP Client API"
 
 
 dependencies {
+  // Add API implementations to test providers
+  testRuntimeOnly(project(":components:http:http-compat"))
+  testRuntimeOnly(project(":components:http:http-lib"))
+  // Add MockServer for test fixtures
   testFixturesImplementation("org.mock-server:mockserver-junit-jupiter-no-dependencies:5.14.0")
 }
