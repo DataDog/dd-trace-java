13ce25d05a0eaafb0a0a5638170fe503bc521511
Bruce Bujon <bruce.bujon@datadoghq.com>
2026-01-30 16:38:33 +0100

    feat(http): Migrate metrics

--- dd-trace-core/src/main/java/datadog/trace/common/metrics/ConflatingMetricsAggregator.java
+++ dd-trace-core/src/main/java/datadog/trace/common/metrics/ConflatingMetricsAggregator.java
@@ -115,7 +115,7 @@ public final class ConflatingMetricsAggregator implements MetricsAggregator, Eve
         config.getMetricsIgnoredResources(),
         sharedCommunicationObjects.featuresDiscovery(config),
         healthMetrics,
-        new OkHttpSink(
+        new HttpSink(
             sharedCommunicationObjects.agentHttpClient,
             sharedCommunicationObjects.agentUrl.toString(),
             V6_METRICS_ENDPOINT,
