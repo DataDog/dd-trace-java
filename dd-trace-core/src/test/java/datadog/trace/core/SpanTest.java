37d100914271ee66d4d9a0149e2239c5a4197be4
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2026-01-30 11:09:19 -0500

    spotless

--- dd-trace-core/src/test/java/datadog/trace/core/SpanTest.java
+++ dd-trace-core/src/test/java/datadog/trace/core/SpanTest.java
@@ -4,25 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import datadog.trace.api.TagMap;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
-
 import org.junit.jupiter.api.Test;
 
 public class SpanTest {
   static final CoreTracer TRACER = CoreTracer.builder().build();
-  
+
   @Test
   public void setTag_Entry() {
-	AgentSpan span = TRACER.startSpan("foo", "foo");
-	span.setTag(TagMap.Entry.create("message", "hello"));
-	
-	assertEquals("hello", span.getTag("message"));
+    AgentSpan span = TRACER.startSpan("foo", "foo");
+    span.setTag(TagMap.Entry.create("message", "hello"));
+
+    assertEquals("hello", span.getTag("message"));
   }
-  
+
   @Test
   public void setMetric_Entry() {
-	AgentSpan span = TRACER.startSpan("foo", "foo");
-	span.setMetric(TagMap.Entry.create("metric", 20L));
-	
-	assertEquals(Long.valueOf(20L), span.getTag("metric"));
+    AgentSpan span = TRACER.startSpan("foo", "foo");
+    span.setMetric(TagMap.Entry.create("metric", 20L));
+
+    assertEquals(Long.valueOf(20L), span.getTag("metric"));
   }
 }
