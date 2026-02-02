aa1f66bf39ef243a3727ed4fadea8aa5f8589ec3
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2026-02-02 13:58:05 -0500

    Caching & reusing spanKind entry to reduce allocation

--- dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/decorator/ClientDecorator.java
+++ dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/decorator/ClientDecorator.java
@@ -1,22 +1,34 @@
 package datadog.trace.bootstrap.instrumentation.decorator;
 
+import datadog.trace.api.TagMap;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.bootstrap.instrumentation.api.Tags;
 
 public abstract class ClientDecorator extends BaseDecorator {
+  // Deliberately not volatile, reading a stale null and creating an extra Entry is safe
+  private TagMap.Entry cachedSpanKindEntry = null;
 
   protected abstract String service();
 
+  /** Caches span kind entry to reduce allocation */
+  private final TagMap.Entry spanKindEntry() {
+	TagMap.Entry kindEntry = cachedSpanKindEntry;
+	if ( kindEntry == null ) {
+	  cachedSpanKindEntry = kindEntry = TagMap.Entry.create(Tags.SPAN_KIND, spanKind());
+	}
+	return kindEntry;
+  }
+  
   protected String spanKind() {
     return Tags.SPAN_KIND_CLIENT;
-  }
+  }  
 
   @Override
   public AgentSpan afterStart(final AgentSpan span) {
     if (service() != null) {
       span.setServiceName(service());
     }
-    span.setTag(Tags.SPAN_KIND, spanKind());
+    span.setTag(spanKindEntry());
 
     // Generate metrics for all client spans.
     span.setMeasured(true);
