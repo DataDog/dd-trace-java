6752a4b5689345186f7cc1024b3d36cb9189f0c7
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2026-02-05 11:44:27 -0500

    Adding null TagMap.Entry tests to SpanTest
    Fixing oversight of creating newAnyEntry rather newObjectEntry in create(tag, CharSequence)

--- dd-trace-core/src/test/java/datadog/trace/core/SpanTest.java
+++ dd-trace-core/src/test/java/datadog/trace/core/SpanTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import datadog.trace.api.TagMap;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class SpanTest {
   static final CoreTracer TRACER = CoreTracer.builder().build();
@@ -18,13 +18,35 @@ public class SpanTest {
 
     assertEquals("hello", span.getTag("message"));
   }
+  
+  @Test
+  @DisplayName("setTag: null")
+  public void setTagEntry() {
+    AgentSpan span = TRACER.startSpan("foo", "foo");
+    int initialSize = span.getTags().size();
+    
+    span.setTag(null);
+    
+    assertEquals(initialSize, span.getTags().size());
+  }
 
   @Test
-  @DisplayName("setMeric: TagMap.Entry")
+  @DisplayName("setMetric: TagMap.Entry")
   public void setMetricEntry() {
     AgentSpan span = TRACER.startSpan("foo", "foo");
     span.setMetric(TagMap.Entry.create("metric", 20L));
 
     assertEquals(Long.valueOf(20L), span.getTag("metric"));
+  }
+  
+  @Test
+  @DisplayName("setMetric: null")
+  public void setMetricEntry() {
+    AgentSpan span = TRACER.startSpan("foo", "foo");
+    int initialSize = span.getTags().size();
+    
+    span.setMetric(null);
+    
+    assertEquals(initialSize, span.getTags().size());
   }
 }
