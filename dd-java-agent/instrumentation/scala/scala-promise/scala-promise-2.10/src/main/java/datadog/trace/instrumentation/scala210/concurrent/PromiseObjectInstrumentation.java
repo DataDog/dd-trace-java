43f7f063961cc12b944a6f1db497a7389a35483a
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-02-06 10:59:16 +0100

    Move to single instrumenter module and flag for context tracking

--- dd-java-agent/instrumentation/scala/scala-promise/scala-promise-2.10/src/main/java/datadog/trace/instrumentation/scala210/concurrent/PromiseObjectInstrumentation.java
+++ dd-java-agent/instrumentation/scala/scala-promise/scala-promise-2.10/src/main/java/datadog/trace/instrumentation/scala210/concurrent/PromiseObjectInstrumentation.java
@@ -1,20 +1,16 @@
 package datadog.trace.instrumentation.scala210.concurrent;
 
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
-import static java.util.Collections.singletonMap;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 
-import com.google.auto.service.AutoService;
 import datadog.context.Context;
 import datadog.trace.agent.tooling.Instrumenter;
-import datadog.trace.agent.tooling.InstrumenterModule;
 import datadog.trace.api.InstrumenterConfig;
 import datadog.trace.bootstrap.ContextStore;
 import datadog.trace.bootstrap.InstrumentationContext;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import datadog.trace.instrumentation.scala.PromiseHelper;
 import java.util.Collections;
-import java.util.Map;
 import net.bytebuddy.asm.Advice;
 import scala.concurrent.impl.CallbackRunnable;
 import scala.util.Try;
@@ -24,23 +20,13 @@ import scala.util.Try;
  * to take priority over any spans captured while adding computations to a {@code Future} associated
  * with a {@code Promise}, then we capture the active span when the {@code Try} is resolved.
  */
-@AutoService(InstrumenterModule.class)
-public class PromiseObjectInstrumentation extends InstrumenterModule.Tracing
+public final class PromiseObjectInstrumentation
     implements Instrumenter.ForSingleType, Instrumenter.HasMethodAdvice {
-
-  public PromiseObjectInstrumentation() {
-    super("scala_promise_resolve", "scala_concurrent");
-  }
 
   @Override
   public String instrumentedType() {
     // The $ at the end is how Scala encodes a Scala object (as opposed to a class or trait)
     return "scala.concurrent.impl.Promise$";
-  }
-
-  @Override
-  public Map<String, String> contextStore() {
-    return singletonMap("scala.util.Try", Context.class.getName());
   }
 
   @Override
@@ -50,19 +36,12 @@ public class PromiseObjectInstrumentation extends InstrumenterModule.Tracing
         getClass().getName() + "$ResolveTry");
   }
 
-  @Override
-  public String[] helperClassNames() {
-    return new String[] {"datadog.trace.instrumentation.scala.PromiseHelper"};
-  }
-
-  @Override
   public boolean isEnabled() {
     // Only enable this if integrations have been enabled and the extra "integration"
     // scala_promise_completion_priority has been enabled specifically
-    return super.isEnabled()
-        && InstrumenterConfig.get()
-            .isIntegrationEnabled(
-                Collections.singletonList("scala_promise_completion_priority"), false);
+    return InstrumenterConfig.get()
+        .isIntegrationEnabled(
+            Collections.singletonList("scala_promise_completion_priority"), false);
   }
 
   public static final class ResolveTry {
