2d69111974888ba8320f3277ba999f6274dd0427
Sarah Chen <sarah.chen@datadoghq.com>
2026-02-12 16:29:57 -0500

    Remove final fields and add comment

--- dd-java-agent/agent-debugger/src/main/java/com/datadog/debugger/agent/ThirdPartyLibraries.java
+++ dd-java-agent/agent-debugger/src/main/java/com/datadog/debugger/agent/ThirdPartyLibraries.java
@@ -87,10 +87,8 @@ public class ThirdPartyLibraries {
   }
 
   private static class InternalConfig {
-    // TODO: JEP 500 - avoid mutating final fields
-    private final String version;
-    // TODO: JEP 500 - avoid mutating final fields
-    private final List<String> prefixes;
+    private String version;
+    private List<String> prefixes;
 
     public InternalConfig(String version, List<String> prefixes) {
       this.version = version;
