0535a727f08cd833b80d7cc2ec6ba05593b95cec
Yury Gribkov <yury.gribkov@gmail.com>
2026-02-18 10:29:36 -0800

    Add a test for a non-root span

--- dd-java-agent/agent-llmobs/src/test/groovy/datadog/trace/llmobs/domain/DDLLMObsSpanTest.groovy
+++ dd-java-agent/agent-llmobs/src/test/groovy/datadog/trace/llmobs/domain/DDLLMObsSpanTest.groovy
@@ -1,5 +1,7 @@
 package datadog.trace.llmobs.domain
 
+import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
+
 import datadog.trace.agent.tooling.TracerInstaller
 import datadog.trace.api.DDTags
 import datadog.trace.api.IdGenerationStrategy
@@ -62,7 +64,7 @@ class DDLLMObsSpanTest  extends DDSpecification{
 
   def "test span simple"() {
     setup:
-    def test = givenALLMObsSpan(Tags.LLMOBS_WORKFLOW_SPAN_KIND, "test-span")
+    def test = llmObsSpan(Tags.LLMOBS_WORKFLOW_SPAN_KIND, "test-span")
 
     when:
     def input = "test input"
@@ -136,7 +138,7 @@ class DDLLMObsSpanTest  extends DDSpecification{
 
   def "test span with overwrites"() {
     setup:
-    def test = givenALLMObsSpan(Tags.LLMOBS_AGENT_SPAN_KIND, "test-span")
+    def test = llmObsSpan(Tags.LLMOBS_AGENT_SPAN_KIND, "test-span")
 
     when:
     def input = "test input"
@@ -221,7 +223,7 @@ class DDLLMObsSpanTest  extends DDSpecification{
 
   def "test llm span string input formatted to messages"() {
     setup:
-    def test = givenALLMObsSpan(Tags.LLMOBS_LLM_SPAN_KIND, "test-span")
+    def test = llmObsSpan(Tags.LLMOBS_LLM_SPAN_KIND, "test-span")
 
     when:
     def input = "test input"
@@ -272,7 +274,7 @@ class DDLLMObsSpanTest  extends DDSpecification{
 
   def "test llm span with messages"() {
     setup:
-    def test = givenALLMObsSpan(Tags.LLMOBS_LLM_SPAN_KIND, "test-span")
+    def test = llmObsSpan(Tags.LLMOBS_LLM_SPAN_KIND, "test-span")
 
     when:
     def inputMsg =  LLMObs.LLMMessage.from("user", "input")
@@ -330,13 +332,12 @@ class DDLLMObsSpanTest  extends DDSpecification{
     setup:
     LLMObsMetricCollector collector = LLMObsMetricCollector.get()
     collector.drain()
-    def span = givenALLMObsSpan(Tags.LLMOBS_WORKFLOW_SPAN_KIND, "workflow-span")
 
     when:
-    span.finish()
-    def metrics = collector.drain()
+    llmObsSpan(Tags.LLMOBS_WORKFLOW_SPAN_KIND, "workflow-span").finish()
 
     then:
+    def metrics = collector.drain()
     metrics.size() == 1
 
     and:
@@ -348,9 +349,37 @@ class DDLLMObsSpanTest  extends DDSpecification{
     m.tags.contains('integration:llmobs')
     m.tags.contains('span_kind:workflow')
     m.tags.contains('autoinstrumented:0')
+    m.tags.contains('is_root_span:1')
   }
 
-  private LLMObsSpan givenALLMObsSpan(String kind, name){
-    new DDLLMObsSpan(kind, name, "test-ml-app", null, "test-svc", new WellKnownTags("test-runtime-1", "host-1", "test-env", "test-svc", "v1", "java"))
+  def "finish records span.finished telemetry for non-root span when LLMObs enabled"() {
+    setup:
+    LLMObsMetricCollector collector = LLMObsMetricCollector.get()
+    collector.drain()
+
+    when:
+    runUnderTrace("parent") {
+      llmObsSpan(Tags.LLMOBS_LLM_SPAN_KIND, "child-llm").finish()
+    }
+
+    then:
+    def metrics = collector.drain()
+    metrics.size() == 1
+
+    and:
+    def m = metrics[0]
+    m.namespace == 'mlobs'
+    m.metricName == 'span.finished'
+    m.type == 'count'
+    m.value == 1
+    m.tags.contains('integration:llmobs')
+    m.tags.contains('span_kind:llm')
+    m.tags.contains('autoinstrumented:0')
+    m.tags.contains('is_root_span:0')
+  }
+
+  private LLMObsSpan llmObsSpan(String kind, name) {
+    def tags = new WellKnownTags("test-runtime-1", "host-1", "test-env", "test-svc", "v1", "java")
+    new DDLLMObsSpan(kind, name, "test-ml-app", null, "test-svc", tags)
   }
 }
