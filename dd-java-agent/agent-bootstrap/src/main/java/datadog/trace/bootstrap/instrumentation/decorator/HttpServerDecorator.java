a1cb717c0e2e154866aa61bb5316db4d8fefc39e
Douglas Q Hawkins <douglas.hawkins@datadoghq.com>
2026-02-19 12:03:12 -0500

    Fixing visibility

--- dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/decorator/HttpServerDecorator.java
+++ dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/decorator/HttpServerDecorator.java
@@ -83,7 +83,7 @@ public abstract class HttpServerDecorator<REQUEST, CONNECTION, RESPONSE, REQUEST
 
   private final String primaryInstrumentationName;
 
-  HttpServerDecorator() {
+  protected HttpServerDecorator() {
     String[] instrumentationNames = instrumentationNames();
     this.primaryInstrumentationName =
         instrumentationNames != null && instrumentationNames.length > 0
