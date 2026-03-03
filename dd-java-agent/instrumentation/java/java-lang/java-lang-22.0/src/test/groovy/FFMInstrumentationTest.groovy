95c49cdb8e973030cab2ac4b92262c31d4001071
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-03-03 10:37:50 +0100

    try to add linux tests

--- dd-java-agent/instrumentation/java/java-lang/java-lang-22.0/src/test/groovy/FFMInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/java/java-lang/java-lang-22.0/src/test/groovy/FFMInstrumentationTest.groovy
@@ -4,8 +4,12 @@ import java.lang.foreign.Arena
 import java.lang.foreign.FunctionDescriptor
 import java.lang.foreign.Linker
 import java.lang.foreign.MemorySegment
+import java.lang.foreign.SymbolLookup
 import java.lang.foreign.ValueLayout
 import java.lang.invoke.MethodHandle
+import java.nio.file.Path
+import spock.lang.IgnoreIf
+import util.NativeLibraryResolver
 
 class FFMInstrumentationTest extends InstrumentationSpecification {
   @Override
@@ -15,10 +19,10 @@ class FFMInstrumentationTest extends InstrumentationSpecification {
     injectSysConfig("trace.native.methods", "unknown[*]")
   }
 
-  def "should trace ffm calls"() {
+  def "should trace ffm calls for #configured"() {
     setup:
     injectSysConfig("trace.native.methods", configured)
-    final MemorySegment strlenAddr = lookup.call().findOrThrow("strlen")
+    final MemorySegment strlenAddr = Linker.nativeLinker().defaultLookup().findOrThrow("strlen")
     final MethodHandle strlenHandle =
     Linker.nativeLinker().downcallHandle(
     strlenAddr, FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS))
@@ -47,16 +51,94 @@ class FFMInstrumentationTest extends InstrumentationSpecification {
     }
 
     where:
-    configured | traceExpected | resource | lookup
-    ""         | false         | _        | {
-      Linker.nativeLinker().defaultLookup()
+    configured | traceExpected | resource
+    ""         | false         | _
+    "[strlen]" | true          | "strlen"
+    "[*]"      | true          | "strlen"
+  }
+
+  @IgnoreIf({ !os.isLinux() })
+  def "should trace ffm calls using libraryLookup by name for #configured"() {
+    setup:
+    injectSysConfig("trace.native.methods", configured)
+
+    when:
+    long len
+    try (final Arena arena = Arena.ofConfined()) {
+      final SymbolLookup libLookup = SymbolLookup.libraryLookup("c", arena)
+      final MemorySegment strlenAddr = libLookup.findOrThrow("strlen")
+      final MethodHandle strlenHandle =
+      Linker.nativeLinker().downcallHandle(
+      strlenAddr, FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS))
+      len = (long) strlenHandle.invokeWithArguments(arena.allocateFrom("Hello world!"))
     }
-    "[strlen]" | true          | "strlen" | {
-      Linker.nativeLinker().defaultLookup()
+
+    then:
+    len == 12
+    assertTraces(traceExpected ? 1 : 0) {
+      if (traceExpected) {
+        trace(1) {
+          span {
+            operationName "trace.native"
+            resourceName "$resource"
+            tags {
+              "$Tags.COMPONENT" "trace-ffm"
+              defaultTags()
+            }
+          }
+        }
+      }
     }
-    "[*]"      | true          | "strlen" | {
-      Linker.nativeLinker().defaultLookup()
+
+    where:
+    configured       | traceExpected | resource
+    ""               | false         | _
+    "c[strlen]"      | true          | "c.strlen"
+    "c[*]"           | true          | "c.strlen"
+    "unknown_lib[*]" | false         | _
+  }
+
+  @IgnoreIf({ !os.isLinux() })
+  def "should trace ffm calls using libraryLookup by path for #configTemplate"() {
+    setup:
+    final MemorySegment strlenSym = Linker.nativeLinker().defaultLookup().findOrThrow("strlen")
+    final String libPath = NativeLibraryResolver.findLibraryPath(strlenSym)
+    final String libFileName = Path.of(libPath).getFileName().toString().toLowerCase(Locale.ROOT)
+    injectSysConfig("trace.native.methods", configTemplate.replace("{lib}", libFileName))
+
+    when:
+    long len
+    try (final Arena arena = Arena.ofConfined()) {
+      final SymbolLookup libLookup = SymbolLookup.libraryLookup(Path.of(libPath), arena)
+      final MemorySegment strlenAddr = libLookup.findOrThrow("strlen")
+      final MethodHandle strlenHandle =
+      Linker.nativeLinker().downcallHandle(
+      strlenAddr, FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS))
+      len = (long) strlenHandle.invokeWithArguments(arena.allocateFrom("Hello world!"))
     }
+
+    then:
+    len == 12
+    assertTraces(traceExpected ? 1 : 0) {
+      if (traceExpected) {
+        trace(1) {
+          span {
+            operationName "trace.native"
+            resourceName "${libFileName}.strlen"
+            tags {
+              "$Tags.COMPONENT" "trace-ffm"
+              defaultTags()
+            }
+          }
+        }
+      }
+    }
+
+    where:
+    configTemplate   | traceExpected
+    ""               | false
+    "{lib}[strlen]"  | true
+    "{lib}[*]"       | true
+    "unknown_lib[*]" | false
   }
 }
-
