2bbdce29bb153126947d58762e5d6ce2b6dcfe28
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-03-03 10:47:05 +0100

    cleanup

--- dd-java-agent/instrumentation/java/java-lang/java-lang-22.0/src/main/java/datadog/trace/instrumentation/java/lang/jdk22/SymbolLookupInstrumentation.java
+++ dd-java-agent/instrumentation/java/java-lang/java-lang-22.0/src/main/java/datadog/trace/instrumentation/java/lang/jdk22/SymbolLookupInstrumentation.java
@@ -3,7 +3,6 @@ package datadog.trace.instrumentation.java.lang.jdk22;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
-import static net.bytebuddy.matcher.ElementMatchers.isStatic;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import datadog.trace.agent.tooling.Instrumenter;
@@ -14,8 +13,6 @@ public class SymbolLookupInstrumentation
     implements Instrumenter.ForTypeHierarchy,
         Instrumenter.ForBootstrap,
         Instrumenter.HasMethodAdvice {
-  private static final String SYMBOL_LOOKUP = "java.lang.foreign.SymbolLookup";
-
   @Override
   public String hierarchyMarkerType() {
     return null; // bootstrap type
@@ -24,21 +21,11 @@ public class SymbolLookupInstrumentation
   @Override
   public ElementMatcher<TypeDescription> hierarchyMatcher() {
     // instrument both interface and sub-implementations
-    return implementsInterface(named(SYMBOL_LOOKUP)).or(named(SYMBOL_LOOKUP));
+    return implementsInterface(named("java.lang.foreign.SymbolLookup"));
   }
 
   @Override
   public void methodAdvice(MethodTransformer transformer) {
-    transformer.applyAdvice(
-        isMethod()
-            .and(isStatic())
-            .and(named("libraryLookup").and(takesArgument(0, named("java.lang.String")))),
-        "datadog.trace.instrumentation.java.lang.jdk22.SymbolLookupAdvices$CaptureLibraryName");
-    transformer.applyAdvice(
-        isMethod()
-            .and(isStatic())
-            .and(named("libraryLookup").and(takesArgument(0, named("java.nio.Path")))),
-        "datadog.trace.instrumentation.java.lang.jdk22.SymbolLookupAdvices$CaptureLibraryPath");
     transformer.applyAdvice(
         isMethod().and(named("find").and(takesArgument(0, named("java.lang.String")))),
         "datadog.trace.instrumentation.java.lang.jdk22.SymbolLookupAdvices$CaptureMemorySegment");
