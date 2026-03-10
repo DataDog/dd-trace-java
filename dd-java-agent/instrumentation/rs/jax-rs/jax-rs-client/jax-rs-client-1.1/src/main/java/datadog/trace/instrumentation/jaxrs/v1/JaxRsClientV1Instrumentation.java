ecd2ae2334c0993e18b0b81664a7e808aa6a07b2
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-03-10 11:52:35 +0100

    more clients

--- dd-java-agent/instrumentation/rs/jax-rs/jax-rs-client/jax-rs-client-1.1/src/main/java/datadog/trace/instrumentation/jaxrs/v1/JaxRsClientV1Instrumentation.java
+++ dd-java-agent/instrumentation/rs/jax-rs/jax-rs-client/jax-rs-client-1.1/src/main/java/datadog/trace/instrumentation/jaxrs/v1/JaxRsClientV1Instrumentation.java
@@ -1,9 +1,11 @@
 package datadog.trace.instrumentation.jaxrs.v1;
 
+import static datadog.trace.agent.tooling.InstrumenterModule.TargetSystem.CONTEXT_TRACKING;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.extendsClass;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.implementsInterface;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeSpan;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
 import static datadog.trace.bootstrap.instrumentation.api.Java8BytecodeBridge.getCurrentContext;
 import static datadog.trace.bootstrap.instrumentation.decorator.HttpServerDecorator.DD_CONTEXT_ATTRIBUTE;
@@ -19,6 +21,7 @@ import com.sun.jersey.api.client.ClientRequest;
 import com.sun.jersey.api.client.ClientResponse;
 import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
+import datadog.trace.agent.tooling.annotation.AppliesOn;
 import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import net.bytebuddy.asm.Advice;
@@ -52,11 +55,12 @@ public final class JaxRsClientV1Instrumentation extends InstrumenterModule.Traci
 
   @Override
   public void methodAdvice(MethodTransformer transformer) {
-    transformer.applyAdvice(
+    transformer.applyAdvices(
         named("handle")
             .and(takesArgument(0, extendsClass(named("com.sun.jersey.api.client.ClientRequest"))))
             .and(returns(extendsClass(named("com.sun.jersey.api.client.ClientResponse")))),
-        JaxRsClientV1Instrumentation.class.getName() + "$HandleAdvice");
+        JaxRsClientV1Instrumentation.class.getName() + "$HandleAdvice",
+        JaxRsClientV1Instrumentation.class.getName() + "$HandleContextPropagationAdvice");
   }
 
   public static class HandleAdvice {
@@ -73,7 +77,6 @@ public final class JaxRsClientV1Instrumentation extends InstrumenterModule.Traci
         DECORATE.afterStart(span);
         DECORATE.onRequest(span, request);
         request.getProperties().put(DD_CONTEXT_ATTRIBUTE, span);
-        DECORATE.injectContext(getCurrentContext().with(span), request.getHeaders(), SETTER);
         return activateSpan(span);
       }
       return null;
@@ -93,6 +96,18 @@ public final class JaxRsClientV1Instrumentation extends InstrumenterModule.Traci
       DECORATE.beforeFinish(span);
       scope.close();
       scope.span().finish();
+    }
+  }
+
+  @AppliesOn(CONTEXT_TRACKING)
+  public static class HandleContextPropagationAdvice {
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(@Advice.Argument(0) final ClientRequest request) {
+      AgentSpan span = activeSpan();
+      if (span == null) {
+        return;
+      }
+      DECORATE.injectContext(getCurrentContext().with(span), request.getHeaders(), SETTER);
     }
   }
 }
