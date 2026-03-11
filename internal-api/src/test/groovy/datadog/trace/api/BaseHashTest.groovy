dca92baba027c7a56eeec5bf6b623cfd2979423e
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-03-11 16:56:48 +0100

    Update BaseHash when process tags are updated

--- internal-api/src/test/groovy/datadog/trace/api/BaseHashTest.groovy
+++ internal-api/src/test/groovy/datadog/trace/api/BaseHashTest.groovy
@@ -1,13 +1,25 @@
 package datadog.trace.api
 
-import datadog.trace.test.util.DDSpecification
-
 import static datadog.trace.api.config.GeneralConfig.ENV
 import static datadog.trace.api.config.GeneralConfig.EXPERIMENTAL_PROPAGATE_PROCESS_TAGS_ENABLED
 import static datadog.trace.api.config.GeneralConfig.PRIMARY_TAG
 import static datadog.trace.api.config.GeneralConfig.SERVICE_NAME
 
+import datadog.trace.test.util.DDSpecification
+
 class BaseHashTest extends DDSpecification {
+
+
+  def setup() {
+    // start with fresh process tags
+    ProcessTags.reset()
+  }
+
+  def cleanup() {
+    // restore the default enablement
+    injectSysConfig(EXPERIMENTAL_PROPAGATE_PROCESS_TAGS_ENABLED, "true")
+    ProcessTags.reset()
+  }
 
   def "Service used in hash calculation"() {
     when:
@@ -46,17 +58,54 @@ class BaseHashTest extends DDSpecification {
     when:
     def firstBaseHash = BaseHash.calc(null)
 
-    injectSysConfig(EXPERIMENTAL_PROPAGATE_PROCESS_TAGS_ENABLED, "true")
-    ProcessTags.reset()
     ProcessTags.addTag("000", "first")
     def secondBaseHash = BaseHash.calc(null)
 
     then:
     firstBaseHash != secondBaseHash
     assert ProcessTags.getTagsForSerialization().startsWithAny("000:first,")
-    cleanup:
+  }
+
+  def "addTag triggers BaseHash recalculation"() {
+    given:
+    BaseHash.recalcBaseHash("container-hash-1")
+    def hashBefore = BaseHash.getBaseHash()
+
+    when:
+    ProcessTags.addTag("cluster.name", "new-cluster")
+
+    then:
+    BaseHash.getBaseHash() != hashBefore
+  }
+
+  def "recalcBaseHash preserves last containerTagsHash across ProcessTags changes"() {
+    given:
+    def containerHash = "my-container-hash"
+    BaseHash.recalcBaseHash(containerHash)
+    def hashWithContainerTag = BaseHash.getBaseHash()
+
+    when: "a process tag is added"
+    ProcessTags.addTag("cluster.name", "new-cluster")
+
+    then: "hash differs from before the tag was added"
+    BaseHash.getBaseHash() != hashWithContainerTag
+
+    and: "hash equals a fresh calc with the same container hash"
+    BaseHash.getBaseHash() == BaseHash.calc(containerHash)
+  }
+
+  def "addTag does not recalculate BaseHash when ProcessTags disabled"() {
+    setup:
     injectSysConfig(EXPERIMENTAL_PROPAGATE_PROCESS_TAGS_ENABLED, "false")
     ProcessTags.reset()
+    BaseHash.recalcBaseHash(null)
+    def hashBefore = BaseHash.getBaseHash()
+
+    when:
+    ProcessTags.addTag("cluster.name", "ignored")
+
+    then:
+    BaseHash.getBaseHash() == hashBefore
   }
 
   def "ContainerTagsHash used in hash calculation when provided"() {
@@ -75,10 +124,6 @@ class BaseHashTest extends DDSpecification {
     } else {
       assert secondBaseHash == firstBaseHash
     }
-
-    cleanup:
-    injectSysConfig(EXPERIMENTAL_PROPAGATE_PROCESS_TAGS_ENABLED, "false")
-    ProcessTags.reset()
 
     where:
     propagateTagsEnabled << [true, false]
