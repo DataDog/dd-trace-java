e2da5fb5d09d9bf6857de1d654d7a1a3ec4ee353
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-03-12 08:44:23 +0100

    fix jetty client

--- dd-java-agent/instrumentation/jetty/jetty-client/jetty-client-10.0/src/main/java11/datadog/trace/instrumentation/jetty_client10/SendContextPropagationAdvice.java
+++ dd-java-agent/instrumentation/jetty/jetty-client/jetty-client-10.0/src/main/java11/datadog/trace/instrumentation/jetty_client10/SendContextPropagationAdvice.java
@@ -5,7 +5,10 @@ import static datadog.trace.bootstrap.instrumentation.api.Java8BytecodeBridge.ge
 import static datadog.trace.instrumentation.jetty_client.HeadersInjectAdapter.SETTER;
 import static datadog.trace.instrumentation.jetty_client10.JettyClientDecorator.DECORATE;
 
+import datadog.context.Context;
 import datadog.trace.agent.tooling.annotation.AppliesOn;
+import datadog.trace.bootstrap.InstrumentationContext;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import net.bytebuddy.asm.Advice;
 import org.eclipse.jetty.client.api.Request;
 
@@ -13,6 +16,11 @@ import org.eclipse.jetty.client.api.Request;
 public class SendContextPropagationAdvice {
   @Advice.OnMethodEnter(suppress = Throwable.class)
   public static void methodEnter(@Advice.Argument(0) final Request request) {
-    DECORATE.injectContext(getCurrentContext(), request, SETTER);
+    final AgentSpan span = InstrumentationContext.get(Request.class, AgentSpan.class).get(request);
+    Context destination = getCurrentContext();
+    if (span != null) {
+      destination = destination.with(span);
+    }
+    DECORATE.injectContext(destination, request, SETTER);
   }
 }
