c1e9ac6389b670103bd91c572f7b1195b66b7887
Jean-Philippe Bempel <jean-philippe.bempel@datadoghq.com>
2026-03-12 08:28:50 +0100

    Prevent instrumenting debugger agent classes (#10803)
    Prevent instrumenting debugger agent classes
    
    Skip all classes from  debugger agent to avoid LinkageError
    attempted duplicate class definition error
    
    use array instead of list
    
    Co-authored-by: jean-philippe.bempel <jean-philippe.bempel@datadoghq.com>

--- dd-java-agent/agent-debugger/src/main/java/com/datadog/debugger/agent/DebuggerTransformer.java
+++ dd-java-agent/agent-debugger/src/main/java/com/datadog/debugger/agent/DebuggerTransformer.java
@@ -95,8 +95,19 @@ public class DebuggerTransformer implements ClassFileTransformer {
           SpanProbe.class);
   private static final String JAVA_IO_TMPDIR = "java.io.tmpdir";
   private static final boolean JAVA_AT_LEAST_19 = JavaVirtualMachine.isJavaVersionAtLeast(19);
-
   public static Path DUMP_PATH = Paths.get(SystemProperties.get(JAVA_IO_TMPDIR), "debugger");
+  private static final String[] SKIPPED_PACKAGES =
+      new String[] {
+        "com/datadog/debugger/agent/",
+        "com/datadog/debugger/codeorigin/",
+        "com/datadog/debugger/exception/",
+        "com/datadog/debugger/instrumentation/",
+        "com/datadog/debugger/probe/",
+        "com/datadog/debugger/sink/",
+        "com/datadog/debugger/symbol/",
+        "com/datadog/debugger/uploader/",
+        "com/datadog/debugger/util/"
+      };
 
   private final Config config;
   private final TransformerDefinitionMatcher definitionMatcher;
@@ -330,6 +341,16 @@ public class DebuggerTransformer implements ClassFileTransformer {
     if (classFilePath == null) {
       // in case of anonymous classes
       return true;
+    }
+    if (classFilePath.startsWith("com/datadog/debugger/")) {
+      // skip classes/packages that are part of debugger agent to avoid
+      // LinkageError: attempted duplicate class definition
+      // while retransforming a class used by instrumentation
+      for (int i = 0; i < SKIPPED_PACKAGES.length; i++) {
+        if (classFilePath.startsWith(SKIPPED_PACKAGES[i])) {
+          return true;
+        }
+      }
     }
     return false;
   }
