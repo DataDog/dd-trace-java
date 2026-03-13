6cdd956a78dc02046d8809eb859486b8c1fd2da3
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-03-13 14:39:27 +0100

    suggestions

--- communication/src/main/java/datadog/communication/ddagent/DDAgentFeaturesDiscovery.java
+++ communication/src/main/java/datadog/communication/ddagent/DDAgentFeaturesDiscovery.java
@@ -232,7 +232,7 @@ public class DDAgentFeaturesDiscovery implements DroppingPolicy {
     try {
       Map<String, Object> map = RESPONSE_ADAPTER.fromJson(response);
       final Object endpointObj = map.get("endpoints");
-      if (endpointObj == null) {
+      if (!(endpointObj instanceof List)) {
         log.debug("Bad response received from the agent. Ignoring it.");
         return false;
       }
