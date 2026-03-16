c3eda9755779368a66563c8bad0e2bdcad4cc008
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-03-16 10:48:44 +0100

    Narrow allowed packages to reduce startup time

--- dd-java-agent/instrumentation/java/java-lang/java-lang-22.0/src/main/java/datadog/trace/instrumentation/java/lang/jdk22/LinkerInstrumentation.java
+++ dd-java-agent/instrumentation/java/java-lang/java-lang-22.0/src/main/java/datadog/trace/instrumentation/java/lang/jdk22/LinkerInstrumentation.java
@@ -1,26 +1,16 @@
 package datadog.trace.instrumentation.java.lang.jdk22;
 
-import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.hasInterface;
 import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import datadog.trace.agent.tooling.Instrumenter;
-import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.matcher.ElementMatcher;
 
 public class LinkerInstrumentation
-    implements Instrumenter.ForTypeHierarchy,
-        Instrumenter.ForBootstrap,
-        Instrumenter.HasMethodAdvice {
+    implements Instrumenter.ForSingleType, Instrumenter.ForBootstrap, Instrumenter.HasMethodAdvice {
   @Override
-  public String hierarchyMarkerType() {
-    return null; // bootstrap type
-  }
-
-  @Override
-  public ElementMatcher<TypeDescription> hierarchyMatcher() {
-    return hasInterface(named("java.lang.foreign.Linker"));
+  public String instrumentedType() {
+    return "jdk.internal.foreign.abi.AbstractLinker";
   }
 
   @Override
