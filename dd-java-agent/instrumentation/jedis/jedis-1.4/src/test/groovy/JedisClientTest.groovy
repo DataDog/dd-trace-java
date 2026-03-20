5ce42231e29bc41b6f5f42ba085885fdc7ef00c2
Jordan Wong <jordan.wong@datadoghq.com>
2026-03-20 14:53:24 -0400

    eval: add generated jedis 1.4 instrumentation (blind test, config v1)
    Generated by apm-instrumentation-toolkit using add-apm-integrations skill v1.
    This is a blind test run - jedis was deleted from the repo before generation.
    
    Agent metrics: 423.2s, 96 turns, $3.29
    
    Layer 1 checks all pass:
    - compileJava, spotlessCheck, codenarcTest, muzzle, test, latestDepTest

---
+++ dd-java-agent/instrumentation/jedis/jedis-1.4/src/test/groovy/JedisClientTest.groovy
@@ -0,0 +1,215 @@
+import datadog.trace.agent.test.naming.VersionedNamingTestBase
+import datadog.trace.agent.test.utils.PortUtils
+import datadog.trace.api.Config
+import datadog.trace.api.DDSpanTypes
+import datadog.trace.bootstrap.instrumentation.api.Tags
+import redis.clients.jedis.Jedis
+import redis.embedded.RedisServer
+import spock.lang.Shared
+
+import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
+import static datadog.trace.api.config.TraceInstrumentationConfig.DB_CLIENT_HOST_SPLIT_BY_INSTANCE
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeSpan
+
+abstract class JedisClientTest extends VersionedNamingTestBase {
+
+  @Shared
+  int port = PortUtils.randomOpenPort()
+
+  @Shared
+  RedisServer redisServer = RedisServer.newRedisServer()
+  // bind to localhost to avoid firewall popup
+  .setting("bind 127.0.0.1")
+  // set max memory to avoid problems in CI
+  .setting("maxmemory 128M")
+  .port(port).build()
+
+  @Shared
+  Jedis jedis = new Jedis("localhost", port)
+
+  @Override
+  void configurePreAgent() {
+    super.configurePreAgent()
+
+    // This setting should have no effect since decorator returns null for the instance.
+    injectSysConfig(DB_CLIENT_HOST_SPLIT_BY_INSTANCE, "true")
+  }
+
+  def setupSpec() {
+    println "Using redis: $redisServer.@args"
+    redisServer.start()
+  }
+
+  def cleanupSpec() {
+    redisServer.stop()
+    //    jedis.close()  // not available in the early version we're using.
+  }
+
+  def setup() {
+    def cleanupSpan = runUnderTrace("cleanup") {
+      jedis.flushAll()
+      activeSpan()
+    }
+    TEST_WRITER.waitUntilReported(cleanupSpan)
+    TEST_WRITER.start()
+  }
+
+  def "set command"() {
+    when:
+    jedis.set("foo", "bar")
+
+    then:
+    assertTraces(1) {
+      trace(1) {
+        span {
+          serviceName service()
+          operationName operation()
+          resourceName "SET"
+          spanType DDSpanTypes.REDIS
+          measured true
+          tags {
+            "$Tags.COMPONENT" "redis-command"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.DB_TYPE" "redis"
+            "$Tags.PEER_HOSTNAME" "localhost"
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "get command"() {
+    when:
+    jedis.set("foo", "bar")
+    def value = jedis.get("foo")
+
+    then:
+    value == "bar"
+
+    assertTraces(2) {
+      trace(1) {
+        span {
+          serviceName service()
+          operationName operation()
+          resourceName "SET"
+          spanType DDSpanTypes.REDIS
+          tags {
+            "$Tags.COMPONENT" "redis-command"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.DB_TYPE" "redis"
+            "$Tags.PEER_HOSTNAME" "localhost"
+            peerServiceFrom(Tags.PEER_HOSTNAME)
+            defaultTags()
+          }
+        }
+      }
+      trace(1) {
+        span {
+          serviceName service()
+          operationName operation()
+          resourceName "GET"
+          spanType DDSpanTypes.REDIS
+          tags {
+            "$Tags.COMPONENT" "redis-command"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.DB_TYPE" "redis"
+            "$Tags.PEER_HOSTNAME" "localhost"
+            peerServiceFrom(Tags.PEER_HOSTNAME)
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "command with no arguments"() {
+    when:
+    jedis.set("foo", "bar")
+    def value = jedis.randomKey()
+
+    then:
+    value == "foo"
+
+    assertTraces(2) {
+      trace(1) {
+        span {
+          serviceName service()
+          operationName operation()
+          resourceName "SET"
+          spanType DDSpanTypes.REDIS
+          tags {
+            "$Tags.COMPONENT" "redis-command"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.DB_TYPE" "redis"
+            "$Tags.PEER_HOSTNAME" "localhost"
+            peerServiceFrom(Tags.PEER_HOSTNAME)
+            defaultTags()
+          }
+        }
+      }
+      trace(1) {
+        span {
+          serviceName service()
+          operationName operation()
+          resourceName "RANDOMKEY"
+          spanType DDSpanTypes.REDIS
+          tags {
+            "$Tags.COMPONENT" "redis-command"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
+            "$Tags.DB_TYPE" "redis"
+            "$Tags.PEER_HOSTNAME" "localhost"
+            peerServiceFrom(Tags.PEER_HOSTNAME)
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  String operation() {
+    return "redis.query"
+  }
+
+  @Override
+  String service() {
+    return Config.get().getServiceName()
+  }
+}
+
+class JedisClientV0Test extends JedisClientTest {
+
+  @Override
+  int version() {
+    return 0
+  }
+
+  @Override
+  String service() {
+    return "redis"
+  }
+
+  @Override
+  String operation() {
+    return "redis.query"
+  }
+}
+
+class JedisClientV1ForkedTest extends JedisClientTest {
+
+  @Override
+  int version() {
+    return 1
+  }
+
+  @Override
+  String service() {
+    return Config.get().getServiceName()
+  }
+
+  @Override
+  String operation() {
+    return "redis.query"
+  }
+}
