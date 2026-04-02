1a3083092bf174267938c381ef84bed91bc23397
Matthew Li <matthew.li@datadoghq.com>
2026-04-01 22:21:37 -0400

    updating test-utils to include config-utils dependency at runtime

--- utils/test-utils/build.gradle.kts
+++ utils/test-utils/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
   api(libs.bytebuddyagent)
 
   api(project(":components:environment"))
-  compileOnly(project(":utils:config-utils"))
+  api(project(":utils:config-utils"))
   api(group = "commons-fileupload", name = "commons-fileupload", version = "1.5")
 
   compileOnly(libs.junit.jupiter)
