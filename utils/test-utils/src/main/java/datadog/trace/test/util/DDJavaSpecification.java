d243e8131fca8bad1631fd527ea13bb403398f3c
Bruce Bujon <bruce.bujon@datadoghq.com>
2026-04-10 10:24:22 +0200

    feat(junit): Add context extension

--- utils/test-utils/src/main/java/datadog/trace/test/util/DDJavaSpecification.java
+++ utils/test-utils/src/main/java/datadog/trace/test/util/DDJavaSpecification.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import datadog.environment.EnvironmentVariables;
 import datadog.trace.junit.utils.config.WithConfigExtension;
+import datadog.trace.junit.utils.context.AllowContextTestingExtension;
 import de.thetaphi.forbiddenapis.SuppressForbidden;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -16,32 +16,23 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(WithConfigExtension.class)
+@ExtendWith({WithConfigExtension.class, AllowContextTestingExtension.class})
 @SuppressForbidden
 public class DDJavaSpecification {
 
   private static final long CHECK_TIMEOUT_MS = 3000;
-
-  static final String CONTEXT_BINDER = "datadog.context.ContextBinder";
-  static final String CONTEXT_MANAGER = "datadog.context.ContextManager";
-
-  private static Boolean contextTestingAllowed;
 
   protected boolean assertThreadsEachCleanup = true;
   private static volatile boolean ignoreThreadCleanup;
 
   @BeforeAll
   static void beforeAll() {
-    allowContextTesting();
     assertTrue(
         EnvironmentVariables.getAll().entrySet().stream()
             .noneMatch(e -> e.getKey().startsWith("DD_")));
     assertTrue(
         systemPropertiesExceptAllowed().entrySet().stream()
             .noneMatch(e -> e.getKey().toString().startsWith("dd.")));
-    assertTrue(
-        contextTestingAllowed,
-        "Context not ready for testing. Ensure all test classes extend DDJavaSpecification");
 
     if (getDDThreads().isEmpty()) {
       ignoreThreadCleanup = false;
@@ -49,27 +40,6 @@ public class DDJavaSpecification {
       System.out.println(
           "Found DD threads before test started. Ignoring thread cleanup for this test class");
       ignoreThreadCleanup = true;
-    }
-  }
-
-  static void allowContextTesting() {
-    if (contextTestingAllowed == null) {
-      try {
-        Class<?> binderClass = Class.forName(CONTEXT_BINDER);
-        Method binderAllowTesting = binderClass.getDeclaredMethod("allowTesting");
-        binderAllowTesting.setAccessible(true);
-        Class<?> managerClass = Class.forName(CONTEXT_MANAGER);
-        Method managerAllowTesting = managerClass.getDeclaredMethod("allowTesting");
-        managerAllowTesting.setAccessible(true);
-        contextTestingAllowed =
-            (Boolean) binderAllowTesting.invoke(null) && (Boolean) managerAllowTesting.invoke(null);
-      } catch (ClassNotFoundException e) {
-        // don't block testing if these types aren't found (project doesn't use context API)
-        contextTestingAllowed =
-            CONTEXT_BINDER.equals(e.getMessage()) || CONTEXT_MANAGER.equals(e.getMessage());
-      } catch (Throwable ignore) {
-        contextTestingAllowed = false;
-      }
     }
   }
 
@@ -135,5 +105,4 @@ public class DDJavaSpecification {
       System.out.println(names);
     }
   }
-
 }
