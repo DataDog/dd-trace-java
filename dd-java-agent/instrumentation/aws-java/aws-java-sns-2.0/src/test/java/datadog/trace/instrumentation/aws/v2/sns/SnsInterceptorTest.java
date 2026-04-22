f5bcaf771fba212a0d6cb1c630eaca1e07eef7e3
Yury Gribkov <yury.gribkov@gmail.com>
2026-04-22 13:56:20 -0700

    Move `trace details propagated when message attributes are readonly` to SnsInterceptorTest

--- dd-java-agent/instrumentation/aws-java/aws-java-sns-2.0/src/test/java/datadog/trace/instrumentation/aws/v2/sns/SnsInterceptorTest.java
+++ dd-java-agent/instrumentation/aws-java/aws-java-sns-2.0/src/test/java/datadog/trace/instrumentation/aws/v2/sns/SnsInterceptorTest.java
@@ -2,10 +2,13 @@ package datadog.trace.instrumentation.aws.v2.sns;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import datadog.context.Context;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,6 +17,7 @@ import software.amazon.awssdk.core.interceptor.ExecutionAttributes;
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue;
 import software.amazon.awssdk.services.sns.model.PublishBatchRequest;
 import software.amazon.awssdk.services.sns.model.PublishBatchRequestEntry;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
 
 public class SnsInterceptorTest {
 
@@ -48,6 +52,29 @@ public class SnsInterceptorTest {
         modified.publishBatchRequestEntries().get(0).messageAttributes().containsKey("_datadog"));
     assertTrue(
         modified.publishBatchRequestEntries().get(1).messageAttributes().containsKey("_datadog"));
+  }
+
+  @Test
+  void publishPreservesReadonlyAttributesWhileAddingDatadogContext() {
+    Map<String, MessageAttributeValue> headers = new HashMap<>();
+    headers.put(
+        "mykey", MessageAttributeValue.builder().dataType("String").stringValue("myvalue").build());
+    Map<String, MessageAttributeValue> readonlyHeaders = Collections.unmodifiableMap(headers);
+
+    PublishRequest request =
+        PublishRequest.builder()
+            .topicArn("arn:aws:sns:us-east-1:123456789012:test-topic")
+            .message("sometext")
+            .messageAttributes(readonlyHeaders)
+            .build();
+
+    PublishRequest modified =
+        (PublishRequest) new SnsInterceptor().modifyRequest(() -> request, executionAttributes());
+
+    assertNotSame(readonlyHeaders, modified.messageAttributes());
+    assertEquals("myvalue", modified.messageAttributes().get("mykey").stringValue());
+    assertTrue(modified.messageAttributes().containsKey("_datadog"));
+    assertFalse(readonlyHeaders.containsKey("_datadog"));
   }
 
   private static ExecutionAttributes executionAttributes() {
