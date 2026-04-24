e7e3a950c02d610fe7b47b1cee1a95b5f5db692d
Mikayla Toffler <tofflerm@gmail.com>
2026-04-24 13:50:45 -0400

    Implement OtlpPayloadDispatcher.java

---
+++ dd-trace-core/src/main/java/datadog/trace/common/writer/OtlpPayloadDispatcher.java
@@ -0,0 +1,67 @@
+package datadog.trace.common.writer;
+
+import datadog.trace.core.CoreSpan;
+import datadog.trace.core.DDSpanContext;
+import datadog.trace.core.otlp.common.OtlpPayload;
+import datadog.trace.core.otlp.common.OtlpSender;
+import datadog.trace.core.otlp.trace.OtlpTraceCollector;
+import datadog.trace.core.otlp.trace.OtlpTraceProtoCollector;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+final class OtlpPayloadDispatcher implements PayloadDispatcher {
+  private final OtlpTraceCollector collector;
+  private final OtlpSender sender;
+
+  OtlpPayloadDispatcher(OtlpSender sender) {
+    this(sender, OtlpTraceProtoCollector.INSTANCE);
+  }
+
+  OtlpPayloadDispatcher(OtlpSender sender, OtlpTraceCollector collector) {
+    this.sender = sender;
+    this.collector = collector;
+  }
+
+  @Override
+  public void addTrace(List<? extends CoreSpan<?>> trace) {
+    List<CoreSpan<?>> sampled = null;
+    for (CoreSpan<?> span : trace) {
+      if (shouldExport(span)) {
+        if (sampled == null) {
+          sampled = new ArrayList<>(trace.size());
+        }
+        sampled.add(span);
+      }
+    }
+    if (sampled != null) {
+      collector.addTrace(sampled);
+    }
+  }
+
+  @Override
+  public void flush() {
+    OtlpPayload payload = collector.collectTraces();
+    if (payload.getContentLength() > 0) {
+      sender.send(payload);
+    }
+  }
+
+  @Override
+  public void onDroppedTrace(int spanCount) {
+    // TODO: surface drop counts via HealthMetrics
+  }
+
+  @Override
+  public Collection<RemoteApi> getApis() {
+    return Collections.emptyList();
+  }
+
+  private static boolean shouldExport(CoreSpan<?> span) {
+    if (span.samplingPriority() > 0) {
+      return true;
+    }
+    return span.getTag(DDSpanContext.SPAN_SAMPLING_MECHANISM_TAG) != null;
+  }
+}
