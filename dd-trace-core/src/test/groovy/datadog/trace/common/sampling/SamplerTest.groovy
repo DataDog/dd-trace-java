145f8841f05ccc67a1d8244b81b825d531778b86
Mikayla Toffler <tofflerm@gmail.com>
2026-04-28 14:57:34 -0400

    Introduce isTraceOtlpExporterEnabled branching logic for Sampler.java, plus related tests

--- dd-trace-core/src/test/groovy/datadog/trace/common/sampling/SamplerTest.groovy
+++ dd-trace-core/src/test/groovy/datadog/trace/common/sampling/SamplerTest.groovy
@@ -66,4 +66,85 @@ class SamplerTest extends DDSpecification{
     then:
     !(sampler instanceof AsmStandaloneSampler)
   }
+
+  void "test that ParentBasedAlwaysOnSampler replaces AllSampler when OTLP traces export is enabled and priority sampling is disabled"() {
+    setup:
+    System.setProperty("dd.trace.otel.exporter", "otlp")
+    System.setProperty("dd.priority.sampling", "false")
+    Config config = new Config()
+
+    when:
+    Sampler sampler = Sampler.Builder.forConfig(config, null)
+
+    then:
+    sampler instanceof ParentBasedAlwaysOnSampler
+  }
+
+  void "test that AllSampler is selected when OTLP traces export is disabled and priority sampling is disabled"() {
+    setup:
+    System.setProperty("dd.priority.sampling", "false")
+    Config config = new Config()
+
+    when:
+    Sampler sampler = Sampler.Builder.forConfig(config, null)
+
+    then:
+    sampler instanceof AllSampler
+    !(sampler instanceof ParentBasedAlwaysOnSampler)
+  }
+
+  void "test that trace sampling rules are respected when OTLP traces export is enabled"() {
+    setup:
+    System.setProperty("dd.trace.otel.exporter", "otlp")
+    System.setProperty("dd.trace.sample.rate", "0.5")
+    Config config = new Config()
+
+    when:
+    Sampler sampler = Sampler.Builder.forConfig(config, null)
+
+    then:
+    sampler instanceof RuleBasedTraceSampler
+    !(sampler instanceof ParentBasedAlwaysOnSampler)
+  }
+
+  void "test that ParentBasedAlwaysOnSampler replaces RateByServiceTraceSampler when OTLP traces export is enabled with default priority sampling"() {
+    setup:
+    System.setProperty("dd.trace.otel.exporter", "otlp")
+    Config config = new Config()
+
+    when:
+    Sampler sampler = Sampler.Builder.forConfig(config, null)
+
+    then:
+    sampler instanceof ParentBasedAlwaysOnSampler
+    !(sampler instanceof RateByServiceTraceSampler)
+  }
+
+  void "test that ForcePrioritySampler is respected when OTLP traces export is enabled and priority sampling is forced to keep"() {
+    setup:
+    System.setProperty("dd.trace.otel.exporter", "otlp")
+    System.setProperty("dd.priority.sampling.force", "keep")
+    Config config = new Config()
+
+    when:
+    Sampler sampler = Sampler.Builder.forConfig(config, null)
+
+    then:
+    sampler instanceof ForcePrioritySampler
+    !(sampler instanceof ParentBasedAlwaysOnSampler)
+  }
+
+  void "test that ForcePrioritySampler is respected when OTLP traces export is enabled and priority sampling is forced to drop"() {
+    setup:
+    System.setProperty("dd.trace.otel.exporter", "otlp")
+    System.setProperty("dd.priority.sampling.force", "drop")
+    Config config = new Config()
+
+    when:
+    Sampler sampler = Sampler.Builder.forConfig(config, null)
+
+    then:
+    sampler instanceof ForcePrioritySampler
+    !(sampler instanceof ParentBasedAlwaysOnSampler)
+  }
 }
