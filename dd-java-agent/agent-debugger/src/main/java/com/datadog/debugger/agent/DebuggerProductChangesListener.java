298086ae690bb2e1e09bc5b5bcdcd303186c7566
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2026-05-04 18:16:05 +0200

    Remove support for full configuration as RC record
    In early days we started to have one big configuration per service
    with all probe definitions. we migrated for one probe per RC record
    since then. We do not have to support the big configuration record
    as this is no longer distributed by RemoteConfig.
    Remove also appliedConfiguration as this map is in practice useless

--- dd-java-agent/agent-debugger/src/main/java/com/datadog/debugger/agent/DebuggerProductChangesListener.java
+++ dd-java-agent/agent-debugger/src/main/java/com/datadog/debugger/agent/DebuggerProductChangesListener.java
@@ -1,7 +1,6 @@
 package com.datadog.debugger.agent;
 
 import static com.datadog.debugger.agent.ConfigurationAcceptor.Source.REMOTE_CONFIG;
-import static com.datadog.debugger.probe.ProbeDefinitionDeserializer.deserializeConfiguration;
 import static com.datadog.debugger.probe.ProbeDefinitionDeserializer.deserializeLogProbe;
 import static com.datadog.debugger.probe.ProbeDefinitionDeserializer.deserializeMetricProbe;
 import static com.datadog.debugger.probe.ProbeDefinitionDeserializer.deserializeSpanDecorationProbe;
@@ -17,14 +16,9 @@ import com.datadog.debugger.probe.TriggerProbe;
 import datadog.remoteconfig.PollingRateHinter;
 import datadog.remoteconfig.state.ConfigKey;
 import datadog.remoteconfig.state.ProductListener;
-import datadog.trace.api.Config;
-import datadog.trace.util.TagsHelper;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import org.slf4j.Logger;
@@ -44,12 +38,10 @@ public class DebuggerProductChangesListener implements ProductListener {
               "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
           .asPredicate();
 
-  private final String serviceName;
   private final ConfigurationAcceptor configurationAcceptor;
-  private final Map<String, Consumer<DefinitionBuilder>> configChunks = new HashMap<>();
+  private final Map<String, ProbeDefinition> probeByConfigId = new HashMap<>();
 
-  DebuggerProductChangesListener(Config config, ConfigurationAcceptor configurationAcceptor) {
-    this.serviceName = TagsHelper.sanitize(config.getServiceName());
+  DebuggerProductChangesListener(ConfigurationAcceptor configurationAcceptor) {
     this.configurationAcceptor = configurationAcceptor;
   }
 
@@ -60,31 +52,19 @@ public class DebuggerProductChangesListener implements ProductListener {
     try {
       if (configId.startsWith(METRIC_PROBE_PREFIX)) {
         MetricProbe metricProbe = deserializeMetricProbe(content);
-        configChunks.put(configId, definitions -> definitions.add(metricProbe));
+        probeByConfigId.put(configId, metricProbe);
       } else if (configId.startsWith(LOG_PROBE_PREFIX)) {
         LogProbe logProbe = deserializeLogProbe(content);
-        configChunks.put(configId, definitions -> definitions.add(logProbe));
+        probeByConfigId.put(configId, logProbe);
       } else if (configId.startsWith(SPAN_PROBE_PREFIX)) {
         SpanProbe spanProbe = deserializeSpanProbe(content);
-        configChunks.put(configId, definitions -> definitions.add(spanProbe));
+        probeByConfigId.put(configId, spanProbe);
       } else if (configId.startsWith(TRIGGER_PROBE_PREFIX)) {
         TriggerProbe triggerProbe = deserializeTriggerProbe(content);
-        configChunks.put(configId, definitions -> definitions.add(triggerProbe));
+        probeByConfigId.put(configId, triggerProbe);
       } else if (configId.startsWith(SPAN_DECORATION_PROBE_PREFIX)) {
         SpanDecorationProbe spanDecorationProbe = deserializeSpanDecorationProbe(content);
-        configChunks.put(configId, definitions -> definitions.add(spanDecorationProbe));
-      } else if (IS_UUID.test(configId)) {
-        Configuration newConfig = deserializeConfiguration(content);
-        if (newConfig.getService().equals(serviceName)) {
-          configChunks.put(
-              configId,
-              (builder) -> {
-                builder.addAll(newConfig.getDefinitions());
-              });
-        } else {
-          throw new IOException(
-              "got config.serviceName = " + newConfig.getService() + ", ignoring configuration");
-        }
+        probeByConfigId.put(configId, spanDecorationProbe);
       } else {
         LOGGER.debug("Unsupported configuration id: {}, ignoring configuration", configId);
       }
@@ -96,59 +76,11 @@ public class DebuggerProductChangesListener implements ProductListener {
 
   @Override
   public void remove(ConfigKey configKey, PollingRateHinter pollingRateHinter) throws IOException {
-    configChunks.remove(configKey.getConfigId());
+    probeByConfigId.remove(configKey.getConfigId());
   }
 
   @Override
   public void commit(PollingRateHinter pollingRateHinter) {
-    DefinitionBuilder builder = new DefinitionBuilder();
-    for (Consumer<DefinitionBuilder> chunk : configChunks.values()) {
-      chunk.accept(builder);
-    }
-    configurationAcceptor.accept(REMOTE_CONFIG, builder.build());
-  }
-
-  static class DefinitionBuilder {
-    private final Collection<ProbeDefinition> definitions = new ArrayList<>();
-
-    void add(MetricProbe probe) {
-      definitions.add(probe);
-    }
-
-    void add(LogProbe probe) {
-      definitions.add(probe);
-    }
-
-    void add(SpanProbe probe) {
-      definitions.add(probe);
-    }
-
-    void add(TriggerProbe probe) {
-      definitions.add(probe);
-    }
-
-    void add(SpanDecorationProbe probe) {
-      definitions.add(probe);
-    }
-
-    void addAll(Collection<ProbeDefinition> newDefinitions) {
-      for (ProbeDefinition definition : newDefinitions) {
-        if (definition instanceof MetricProbe) {
-          add((MetricProbe) definition);
-        } else if (definition instanceof LogProbe) {
-          add((LogProbe) definition);
-        } else if (definition instanceof SpanProbe) {
-          add((SpanProbe) definition);
-        } else if (definition instanceof TriggerProbe) {
-          add((TriggerProbe) definition);
-        } else if (definition instanceof SpanDecorationProbe) {
-          add((SpanDecorationProbe) definition);
-        }
-      }
-    }
-
-    Collection<ProbeDefinition> build() {
-      return definitions;
-    }
+    configurationAcceptor.accept(REMOTE_CONFIG, probeByConfigId.values());
   }
 }
