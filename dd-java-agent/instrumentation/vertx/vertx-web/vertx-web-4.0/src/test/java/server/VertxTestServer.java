62edb3556540d084b6c73ab7a2e4333c9e7aef72
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2026-05-04 15:47:30 +0200

    Add server.request.body.filenames AppSec address for Vert.x 3.4, 4.0 and 5.0
    Instruments RoutingContextImpl.fileUploads() in vertx-web-3.4, vertx-web-4.0
    and vertx-web-5.0 to fire EVENTS.requestFilesFilenames() with the list of
    uploaded filenames, enabling WAF-level blocking on malicious file names.

--- dd-java-agent/instrumentation/vertx/vertx-web/vertx-web-4.0/src/test/java/server/VertxTestServer.java
+++ dd-java-agent/instrumentation/vertx/vertx-web/vertx-web-4.0/src/test/java/server/VertxTestServer.java
@@ -101,6 +101,7 @@ public class VertxTestServer extends AbstractVerticle {
                       String res = convertFormAttributes(ctx);
                       ctx.response().setStatusCode(BODY_URLENCODED.getStatus()).end(res);
                     }));
+    router.route(BODY_MULTIPART.getPath()).handler(BodyHandler.create());
     router
         .route(BODY_MULTIPART.getPath())
         .handler(
@@ -109,13 +110,9 @@ public class VertxTestServer extends AbstractVerticle {
                     ctx,
                     BODY_MULTIPART,
                     () -> {
-                      ctx.request().setExpectMultipart(true);
-                      ctx.request()
-                          .endHandler(
-                              (_void) -> {
-                                String res = convertFormAttributes(ctx);
-                                ctx.response().setStatusCode(BODY_MULTIPART.getStatus()).end(res);
-                              });
+                      ctx.fileUploads();
+                      String res = convertFormAttributes(ctx);
+                      ctx.response().setStatusCode(BODY_MULTIPART.getStatus()).end(res);
                     }));
     router.route(BODY_JSON.getPath()).handler(BodyHandler.create());
     router
