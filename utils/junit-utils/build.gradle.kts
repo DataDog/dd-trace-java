1233e65714504c0403e433d57a230e3f5bf81fca
jean-philippe bempel <jean-philippe.bempel@datadoghq.com>
2026-05-13 11:17:12 +0200

    Migrate dd-trace-core groovy files to java part 7
    we migrate 9 tests:
     - BaggagePropagatorTest
     - ServiceDiscoveryTest
     - TagInterceptorTest
     - LatencyTraceInterceptorTest
     - GlobPatternTest
     - LRUCacheTest
     - MatchersTest
     - SimpleRateLimiterTest
     - SystemAccessTest

--- utils/junit-utils/build.gradle.kts
+++ utils/junit-utils/build.gradle.kts
@@ -10,6 +10,9 @@ dependencies {
   api(libs.forbiddenapis)
   api(project(":components:environment"))
 
+  implementation(project(":dd-trace-api"))
+  implementation(project(":internal-api"))
+
   compileOnly(libs.junit.jupiter)
   compileOnly(libs.tabletest)
 }
