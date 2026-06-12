0e807c1f9ad4b1c0a0903ffd8cf9db72c9a6f841
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-06-12 12:55:22 +0200

    Rework the implementation

--- dd-java-agent/instrumentation/lettuce/lettuce-5.0/src/main/java/datadog/trace/instrumentation/lettuce5/ConnectionFutureAdvice.java
+++ dd-java-agent/instrumentation/lettuce/lettuce-5.0/src/main/java/datadog/trace/instrumentation/lettuce5/ConnectionFutureAdvice.java
@@ -1,9 +1,11 @@
 package datadog.trace.instrumentation.lettuce5;
 
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
 import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
 import static datadog.trace.instrumentation.lettuce5.LettuceClientDecorator.DECORATE;
 
 import datadog.trace.bootstrap.InstrumentationContext;
+import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import io.lettuce.core.ConnectionFuture;
 import io.lettuce.core.RedisURI;
@@ -12,29 +14,28 @@ import net.bytebuddy.asm.Advice;
 
 public class ConnectionFutureAdvice {
   @Advice.OnMethodEnter(suppress = Throwable.class)
-  public static AgentSpan onEnter(@Advice.Argument(1) final RedisURI redisUri) {
+  public static AgentScope onEnter(@Advice.Argument(1) final RedisURI redisUri) {
     final AgentSpan span =
         startSpan(
             LettuceClientDecorator.REDIS_CLIENT.toString(), LettuceClientDecorator.OPERATION_NAME);
     DECORATE.afterStart(span);
     span.setResourceName(DECORATE.resourceNameForConnection(redisUri));
     DECORATE.onConnection(span, redisUri);
-    return span;
+    return activateSpan(span);
   }
 
   @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
   public static void stopSpan(
-      @Advice.Enter final AgentSpan span,
+      @Advice.Enter final AgentScope scope,
       @Advice.Thrown final Throwable throwable,
       @Advice.Argument(1) final RedisURI redisUri,
       @Advice.Return(readOnly = false)
           ConnectionFuture<? extends StatefulConnection> connectionFuture) {
-    if (span == null) {
-      return;
-    }
+    final AgentSpan span = scope.span();
     if (throwable != null) {
       DECORATE.onError(span, throwable);
       DECORATE.beforeFinish(span);
+      scope.close();
       span.finish();
       return;
     }
@@ -43,7 +44,7 @@ public class ConnectionFutureAdvice {
             new ConnectionContextBiConsumer(
                     redisUri, InstrumentationContext.get(StatefulConnection.class, RedisURI.class))
                 .andThen(new LettuceAsyncBiConsumer<>(span)));
-
+    scope.close();
     // span finished by LettuceAsyncBiConsumer
   }
 }
