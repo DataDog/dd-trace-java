5adf6e397af17277d153be7a7881e761916a0737
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2026-06-15 09:07:27 +0100

    Add ClassDefining tests and ignore extraneous observer calls. (#11644)
    Add ClassDefining tests and ignore extraneous observer calls.
    
    Co-authored-by: devflow.devflow-routing-intake <devflow.devflow-routing-intake@kubernetes.us1.ddbuild.io>

--- dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/classloading/ClassDefining.java
+++ dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/instrumentation/classloading/ClassDefining.java
@@ -1,12 +1,17 @@
 package datadog.trace.bootstrap.instrumentation.classloading;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /** Provides a way for a single optional observer to be notified before a class is defined. */
 public final class ClassDefining {
+  private static final AtomicBoolean HAS_OBSERVER = new AtomicBoolean();
   private static volatile Observer OBSERVER = (loader, bytecode, offset, length) -> {};
 
   /** Registers the given observer to get notifications about class definitions. */
   public static void observe(Observer observer) {
-    OBSERVER = observer;
+    if (HAS_OBSERVER.compareAndSet(false, true)) {
+      OBSERVER = observer; // set once in premain
+    }
   }
 
   /** Called from advice added to j.l.ClassLoader by DefineClassInstrumentation. */
