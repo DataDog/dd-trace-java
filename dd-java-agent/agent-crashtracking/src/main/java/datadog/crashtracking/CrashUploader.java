247f390fbe4b4ae8c053443e8b63b472de17d52f
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2026-06-18 17:46:23 -0400

    Cleanup of Spotbugs annotations.

--- dd-java-agent/agent-crashtracking/src/main/java/datadog/crashtracking/CrashUploader.java
+++ dd-java-agent/agent-crashtracking/src/main/java/datadog/crashtracking/CrashUploader.java
@@ -29,7 +29,6 @@ import datadog.trace.bootstrap.config.provider.ConfigProvider;
 import datadog.trace.util.AgentThreadFactory;
 import datadog.trace.util.PidHelper;
 import de.thetaphi.forbiddenapis.SuppressForbidden;
-import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -130,7 +129,7 @@ public final class CrashUploader {
   }
 
   CrashUploader(
-      @NonNull final Config config, @Nonnull final ConfigManager.StoredConfig storedConfig) {
+      @Nonnull final Config config, @Nonnull final ConfigManager.StoredConfig storedConfig) {
     this.config = config;
     this.storedConfig = storedConfig;
     this.uploaderSettings = storedConfig.toCrashUploaderSettings();
