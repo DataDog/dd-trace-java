eb4bec91a3642a53ded0b8b2896770ff67285578
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2026-06-23 21:31:29 +0100

    Make DDSpan, NoopSpan (and BlackHoleSpan) all SelfScopedContexts to further reduce allocations

--- components/context/src/main/java/datadog/context/SelfScopedContext.java
+++ components/context/src/main/java/datadog/context/SelfScopedContext.java
@@ -1,7 +1,7 @@
 package datadog.context;
 
 /** Context that acts as its own unattached scope. */
-interface SelfScopedContext extends Context, ContextScope {
+public interface SelfScopedContext extends Context, ContextScope {
   @Override
   default ContextScope asScope() {
     return this; // acts as no-op scope, avoiding allocation
