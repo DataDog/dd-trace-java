ffb48aeb95a05df3d20c27afe3a7b1c5d0ba59c4
Yury Gribkov <yury.gribkov@gmail.com>
2026-06-26 12:13:55 -0700

    Fix message handler spans appear disconnected from the incoming SQS trace (#11511)
    Add regression test for sync SQS receive response copy context loss
    
    Preserve SQS receive context across AWS SDK response rebuilding
    
    Store the receive queue URL before the SDK rebuilds
    ReceiveMessageResponse
    and propagate it through the response builder so messages() can still
    wrap
    the final list in TracingList.
    
    Also avoid wrapping messages during the SDK's internal
    MessageMD5ChecksumInterceptor pass to prevent creating consumer spans
    before user code actually consumes the messages.
    
    Merge branch 'master' into ygree/aws-sdk-sqs-bug
    
    Migrate SQS client tests to JUnit
    
    Document SQS checksum interceptor instrumentation
    
    add clarifying comments
    
    Merge branch 'master' into ygree/aws-sdk-sqs-bug
    
    Merge branch 'master' into ygree/aws-sdk-sqs-bug
    
    Co-authored-by: yury.gribkov <yury.gribkov@datadoghq.com>

--- dd-java-agent/instrumentation/aws-java/aws-java-sqs-2.0/src/main/java/datadog/trace/instrumentation/aws/v2/sqs/SqsModule.java
+++ dd-java-agent/instrumentation/aws-java/aws-java-sqs-2.0/src/main/java/datadog/trace/instrumentation/aws/v2/sqs/SqsModule.java
@@ -5,7 +5,6 @@ import datadog.trace.agent.tooling.Instrumenter;
 import datadog.trace.agent.tooling.InstrumenterModule;
 import datadog.trace.api.InstrumenterConfig;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +23,7 @@ public final class SqsModule extends InstrumenterModule.Tracing {
       "datadog.trace.instrumentation.aws.v2.sqs.MessageAttributeInjector",
       "datadog.trace.instrumentation.aws.v2.sqs.MessageExtractAdapter",
       "datadog.trace.instrumentation.aws.v2.sqs.SqsDecorator",
+      "datadog.trace.instrumentation.aws.v2.sqs.SqsReceiveResponseInternalAccess",
       "datadog.trace.instrumentation.aws.v2.sqs.TracingIterator",
       "datadog.trace.instrumentation.aws.v2.sqs.TracingList",
       "datadog.trace.instrumentation.aws.v2.sqs.TracingListIterator"
@@ -32,17 +32,25 @@ public final class SqsModule extends InstrumenterModule.Tracing {
 
   @Override
   public Map<String, String> contextStore() {
-    return Collections.singletonMap(
+    Map<String, String> contextStore = new java.util.HashMap<>();
+    contextStore.put(
         "software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse", "java.lang.String");
+    contextStore.put(
+        "software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse$BuilderImpl",
+        "java.lang.String");
+    return contextStore;
   }
 
   @Override
   public List<Instrumenter> typeInstrumentations() {
-    final List<Instrumenter> ret = new ArrayList<>(4);
+    final List<Instrumenter> ret = new ArrayList<>(6);
     ret.add(new SqsClientInstrumentation());
     ret.add(new SqsReceiveRequestInstrumentation());
     // we don't need to instrument messages when we're doing legacy AWS-SDK tracing
     if (!InstrumenterConfig.get().isLegacyInstrumentationEnabled(false, "aws-sdk")) {
+      ret.add(new SqsMd5ChecksumInterceptorInstrumentation());
+      ret.add(new SqsReceiveResponseBuilderInstrumentation());
+      ret.add(new SqsReceiveResponseBuilderImplInstrumentation());
       ret.add(new SqsReceiveResultInstrumentation());
     }
     return ret;
