b4c11ee0d7a97ea51d963127765b23017c0a7f67
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2026-06-29 10:26:49 +0100

    Cover continuation and listener behaviour when registering no-op manager during tests.

--- components/context/src/test/java/datadog/context/ContextProvidersForkedTest.java
+++ components/context/src/test/java/datadog/context/ContextProvidersForkedTest.java
@@ -2,10 +2,13 @@ package datadog.context;
 
 import static datadog.context.Context.root;
 import static datadog.context.ContextTest.STRING_KEY;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static datadog.context.ContextTestBase.trackingListener;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.Nonnull;
 import org.junit.jupiter.api.Test;
 
@@ -15,13 +18,15 @@ class ContextProvidersForkedTest {
     assertTrue(ContextBinder.allowTesting());
 
     Context context = root().with(STRING_KEY, "value");
+    assertNotEquals(root(), context);
+
     Object carrier = new Object();
 
     // should delegate to the default binder
     context.attachTo(carrier);
-    assertNotEquals(root(), Context.from(carrier));
-    assertEquals(context, Context.detachFrom(carrier));
-    assertEquals(root(), Context.from(carrier));
+    assertSame(context, Context.from(carrier));
+    assertSame(context, Context.detachFrom(carrier));
+    assertSame(root(), Context.from(carrier));
 
     // now register a NOOP context binder
     ContextBinder.register(
@@ -44,8 +49,9 @@ class ContextProvidersForkedTest {
 
     // NOOP binder, context will always be root
     context.attachTo(carrier);
-    assertEquals(root(), Context.from(carrier));
-    assertEquals(root(), Context.detachFrom(carrier));
+    assertSame(root(), Context.from(carrier));
+    assertSame(root(), Context.detachFrom(carrier));
+    assertSame(root(), Context.from(carrier));
   }
 
   @Test
@@ -53,15 +59,22 @@ class ContextProvidersForkedTest {
     assertTrue(ContextManager.allowTesting());
 
     Context context = root().with(STRING_KEY, "value");
+    assertNotEquals(root(), context);
 
     // should delegate to the default manager
-    try (ContextScope ignored = context.attach()) {
-      assertNotEquals(root(), Context.current());
+    try (ContextScope scope = context.attach()) {
+      assertSame(context, scope.context());
+      assertSame(context, Context.current());
+      ContextContinuation cont = context.capture();
+      assertSame(context, cont.context());
+      cont.release();
     }
 
     Context swapped = context.swap();
-    assertNotEquals(root(), Context.current());
-    swapped.swap();
+    assertSame(root(), swapped);
+    assertSame(context, Context.current());
+    assertSame(context, swapped.swap());
+    assertSame(root(), Context.current());
 
     // now register a NOOP context manager
     ContextManager.register(
@@ -90,14 +103,26 @@ class ContextProvidersForkedTest {
           public void addListener(ContextListener listener) {}
         });
 
+    List<String> events = new ArrayList<>();
+    ContextManager.register(trackingListener(events));
+
     // NOOP manager, context will always be root
-    try (ContextScope ignored = context.attach()) {
-      assertEquals(root(), Context.current());
+    try (ContextScope scope = context.attach()) {
+      assertSame(root(), scope.context());
+      assertSame(root(), Context.current());
+      ContextContinuation cont = context.capture();
+      assertSame(root(), cont.context());
+      cont.release();
     }
 
     // NOOP manager, context will always be root
     swapped = context.swap();
-    assertEquals(root(), Context.current());
-    swapped.swap();
+    assertSame(root(), swapped);
+    assertSame(root(), Context.current());
+    assertSame(root(), swapped.swap());
+    assertSame(root(), Context.current());
+
+    // NOOP manager, no events emitted
+    assertTrue(events.isEmpty());
   }
 }
