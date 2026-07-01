a3ef224d7ede6d28091b2c53006e8a34ee0105cd
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2026-07-01 14:14:17 -0400

    Bump develocity plugin to 4.5.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,7 @@ pluginManagement {
 }
 
 plugins {
-  id("com.gradle.develocity") version "4.4.2"
+  id("com.gradle.develocity") version "4.5.0"
   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
