edb264e7d8419dcaa161b4b8a077e61cbcc7ad29
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2026-07-01 23:49:30 -0400

    test(feature-flagging): cover flagevaluation writer lifecycle

--- products/feature-flagging/feature-flagging-agent/src/test/java/com/datadog/featureflag/FeatureFlaggingSystemTest.java
+++ products/feature-flagging/feature-flagging-agent/src/test/java/com/datadog/featureflag/FeatureFlaggingSystemTest.java
@@ -1,6 +1,8 @@
 package com.datadog.featureflag;
 
 import static datadog.trace.api.config.RemoteConfigConfig.REMOTE_CONFIGURATION_ENABLED;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -15,6 +17,8 @@ import datadog.remoteconfig.ConfigurationDeserializer;
 import datadog.remoteconfig.ConfigurationPoller;
 import datadog.remoteconfig.Product;
 import datadog.trace.api.Config;
+import datadog.trace.api.config.FeatureFlaggingConfig;
+import datadog.trace.api.featureflag.FeatureFlaggingGateway;
 import datadog.trace.junit.utils.config.WithConfig;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -23,6 +27,7 @@ import org.junit.jupiter.api.Test;
 class FeatureFlaggingSystemTest {
 
   @Test
+  @WithConfig(key = FeatureFlaggingConfig.FLAGGING_EVALUATION_COUNTS_ENABLED, value = "true")
   void testFeatureFlagSystemInitialization() {
     ConfigurationPoller poller = mock(ConfigurationPoller.class);
     DDAgentFeaturesDiscovery discovery = mock(DDAgentFeaturesDiscovery.class);
@@ -39,12 +44,35 @@ class FeatureFlaggingSystemTest {
     verify(poller).addCapabilities(Capabilities.CAPABILITY_FFE_FLAG_CONFIGURATION_RULES);
     verify(poller).addListener(eq(Product.FFE_FLAGS), any(ConfigurationDeserializer.class), any());
     verify(poller).start();
+    assertNotNull(FeatureFlaggingGateway.getFlagEvalWriter());
 
     FeatureFlaggingSystem.stop();
+    assertNull(FeatureFlaggingGateway.getFlagEvalWriter());
 
     verify(poller).removeCapabilities(Capabilities.CAPABILITY_FFE_FLAG_CONFIGURATION_RULES);
     verify(poller).removeListeners(Product.FFE_FLAGS);
     verify(poller).stop();
+  }
+
+  @Test
+  @WithConfig(key = FeatureFlaggingConfig.FLAGGING_EVALUATION_COUNTS_ENABLED, value = "false")
+  void testFlagEvaluationWriterCanBeDisabled() {
+    ConfigurationPoller poller = mock(ConfigurationPoller.class);
+    DDAgentFeaturesDiscovery discovery = mock(DDAgentFeaturesDiscovery.class);
+    SharedCommunicationObjects sharedCommunicationObjects = mock(SharedCommunicationObjects.class);
+    when(discovery.supportsEvpProxy()).thenReturn(true);
+    when(discovery.getEvpProxyEndpoint()).thenReturn("/evp_proxy/");
+    when(sharedCommunicationObjects.configurationPoller(any(Config.class))).thenReturn(poller);
+    when(sharedCommunicationObjects.featuresDiscovery(any(Config.class))).thenReturn(discovery);
+    sharedCommunicationObjects.agentUrl = HttpUrl.get("http://localhost");
+    sharedCommunicationObjects.agentHttpClient = new OkHttpClient.Builder().build();
+
+    try {
+      FeatureFlaggingSystem.start(sharedCommunicationObjects);
+      assertNull(FeatureFlaggingGateway.getFlagEvalWriter());
+    } finally {
+      FeatureFlaggingSystem.stop();
+    }
   }
 
   @Test
