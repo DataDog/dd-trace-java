96df04f1be66c6ebd237752e43c8f37b09ec8212
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2026-07-01 23:49:29 -0400

    feat(feature-flagging): wire flagevaluation writer lifecycle

--- products/feature-flagging/feature-flagging-agent/src/main/java/com/datadog/featureflag/FeatureFlaggingSystem.java
+++ products/feature-flagging/feature-flagging-agent/src/main/java/com/datadog/featureflag/FeatureFlaggingSystem.java
@@ -2,6 +2,8 @@ package com.datadog.featureflag;
 
 import datadog.communication.ddagent.SharedCommunicationObjects;
 import datadog.trace.api.Config;
+import datadog.trace.api.config.FeatureFlaggingConfig;
+import datadog.trace.api.featureflag.flagevaluation.FlagEvaluationWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class FeatureFlaggingSystem {
 
   private static volatile RemoteConfigService CONFIG_SERVICE;
   private static volatile ExposureWriter EXPOSURE_WRITER;
+  private static volatile FlagEvaluationWriter FLAG_EVAL_WRITER;
 
   private FeatureFlaggingSystem() {}
 
@@ -27,10 +30,29 @@ public class FeatureFlaggingSystem {
     EXPOSURE_WRITER = new ExposureWriterImpl(sco, config);
     EXPOSURE_WRITER.init();
 
+    final boolean evalCountsEnabled =
+        config
+            .configProvider()
+            .getBoolean(FeatureFlaggingConfig.FLAGGING_EVALUATION_COUNTS_ENABLED, true);
+    if (evalCountsEnabled) {
+      final FlagEvaluationWriterImpl evalWriter = new FlagEvaluationWriterImpl(sco, config);
+      evalWriter.start();
+      FLAG_EVAL_WRITER = evalWriter;
+      LOGGER.debug("Flag evaluation EVP writer started");
+    } else {
+      LOGGER.debug(
+          "Flag evaluation EVP writer disabled ({}=false)",
+          FeatureFlaggingConfig.FLAGGING_EVALUATION_COUNTS_ENABLED);
+    }
+
     LOGGER.debug("Feature Flagging system started");
   }
 
   public static void stop() {
+    if (FLAG_EVAL_WRITER != null) {
+      FLAG_EVAL_WRITER.close();
+      FLAG_EVAL_WRITER = null;
+    }
     if (EXPOSURE_WRITER != null) {
       EXPOSURE_WRITER.close();
       EXPOSURE_WRITER = null;
