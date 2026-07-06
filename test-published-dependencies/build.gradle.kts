ed441f404379073cc71c30bd87f4cd5aebf6dd34
Brice Dutheil <brice.dutheil@gmail.com>
2026-07-06 16:06:22 +0200

    Fix serialization error in spotless `test-published-artifacts` (#11865)
    * fix(ci): Dedicated spotless config on test-publihsed-artifacts
    
    The spotless _plugin script_, is being reused in
    test-published-artifacts, unfortunately spotless a serilization bug
    on the greclipse formatter that surfaces when no files mathing
    extension target. This causes the build to fail with a obscure error
    related to fingerprinting:
    
    ```
    java.lang.IllegalStateException: If the initializer was null, then one of
    roundtripStateInternal or equalityStateInternal should be non-null, and neither was
        at com.diffplug.spotless.FormatterStepSerializationRoundtrip.writeObject(...)
        at com.diffplug.spotless.ConfigurationCacheHackList.hashCode(...)
    ```
    
    This change applies a tailored configuration for this simple project.
    
    * fix(ci): Run `spotlessCheck` test-published-dependencies in spotless job
    
    * fix(ci): Comments

--- test-published-dependencies/build.gradle.kts
+++ test-published-dependencies/build.gradle.kts
@@ -13,5 +13,25 @@ allprojects {
   version = versionFromFile
 
   apply(from = "$sharedConfigDirectory/repositories.gradle")
-  apply(from = "$sharedConfigDirectory/spotless.gradle")
+  apply(plugin = "com.diffplug.spotless")
+
+  // Apply a simple spotless config here.
+  // Using the dd-trace-java's plugin scripts, adds a step with grecplipse that
+  // has a concurrency bug surfacing with gradle 9.6 when there's no matching file.
+  spotless {
+    kotlinGradle {
+      target("*.gradle.kts")
+      ktlint("1.8.0").editorConfigOverride(
+        mapOf(
+          // Disable trailing comma rules to minimize diff.
+          "ktlint_standard_trailing-comma-on-call-site" to "disabled",
+          "ktlint_standard_trailing-comma-on-declaration-site" to "disabled",
+        ),
+      )
+    }
+    java {
+      target("src/**/*.java")
+      googleJavaFormat("1.35.0")
+    }
+  }
 }
