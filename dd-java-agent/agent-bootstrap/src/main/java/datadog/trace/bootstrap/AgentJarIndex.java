794f5f296ef66ef08426bcd59a7fce44d7698899
Brice Dutheil <brice.dutheil@gmail.com>
2026-07-09 11:08:16 +0200

    refactor(agent): Remove unused `Comparator` import

--- dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/AgentJarIndex.java
+++ dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/AgentJarIndex.java
@@ -13,7 +13,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
