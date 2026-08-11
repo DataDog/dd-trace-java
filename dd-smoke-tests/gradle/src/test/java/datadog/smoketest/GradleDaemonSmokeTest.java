66dfc3a33dcdd7ef8fbec6ccac28a3eb02d46f85
Sarah Chen <sarah.chen@datadoghq.com>
2026-08-11 12:17:42 -0400

    🍒 Re-add Test task changes

--- dd-smoke-tests/gradle/src/test/java/datadog/smoketest/GradleDaemonSmokeTest.java
+++ dd-smoke-tests/gradle/src/test/java/datadog/smoketest/GradleDaemonSmokeTest.java
@@ -417,8 +417,7 @@ class GradleDaemonSmokeTest extends AbstractGradleTest {
 
   private BuildResult runGradle(
       String gradleVersion, List<String> arguments, boolean successExpected) throws IOException {
-    Map<String, String> buildEnv = new HashMap<>(System.getenv());
-    buildEnv.keySet().removeIf(name -> name.startsWith("CI_"));
+    Map<String, String> buildEnv = new HashMap<>();
     buildEnv.put("GRADLE_ARGS", "");
     buildEnv.put("GRADLE_OPTS", "");
     buildEnv.put("GRADLE_USER_HOME", testKitFolder.toString());
