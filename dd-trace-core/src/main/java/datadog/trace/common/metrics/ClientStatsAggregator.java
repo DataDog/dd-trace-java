d0990c3a0a86a0befb3a77cbd00104885de9840d
Jean-Philippe Bempel <jean-philippe.bempel@datadoghq.com>
2026-08-21 15:55:29 +0200

    address codex comments

--- dd-trace-core/src/main/java/datadog/trace/common/metrics/ClientStatsAggregator.java
+++ dd-trace-core/src/main/java/datadog/trace/common/metrics/ClientStatsAggregator.java
@@ -21,6 +21,7 @@ import datadog.communication.ddagent.DDAgentFeaturesDiscovery;
 import datadog.communication.ddagent.SharedCommunicationObjects;
 import datadog.trace.api.Config;
 import datadog.trace.api.WellKnownTags;
+import datadog.trace.api.internal.VisibleForTesting;
 import datadog.trace.bootstrap.instrumentation.api.InstrumentationTags;
 import datadog.trace.common.metrics.SignalItem.ReportSignal;
 import datadog.trace.common.writer.ddagent.DDAgentApi;
@@ -737,5 +738,10 @@ public final class ClientStatsAggregator implements MetricsAggregator, EventList
     public void run(ClientStatsAggregator target) {
       target.report();
     }
+  }
+
+  @VisibleForTesting
+  boolean isEmpty() {
+    return inbox.isEmpty();
   }
 }
