9459ad0bb2b0e5e9a98f1ad485f722dadadb6a45
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-08-26 11:05:47 +0200

    Correctly manage JPMS

---
+++ dd-smoke-tests/java9-modules/src/test/groovy/datadog/smoketest/Java9ModulesLambdaSmokeTest.groovy
@@ -0,0 +1,20 @@
+package datadog.smoketest
+
+import datadog.environment.JavaVirtualMachine
+import datadog.environment.OperatingSystem
+import spock.lang.IgnoreIf
+
+/**
+ * Runs the modular application with lambda field-injection enabled. The lambda's generated class
+ * belongs to the application's named module, which reaches the injected types only if the agent
+ * adds a read edge while transforming it.
+ */
+@IgnoreIf({
+  OperatingSystem.isLinux() && OperatingSystem.architecture().isArm64() && JavaVirtualMachine.isJ9()
+})
+class Java9ModulesLambdaSmokeTest extends Java9ModulesSmokeTest {
+  @Override
+  protected List<String> extraJvmArgs() {
+    return ["-Ddd.trace.lambda.enabled=true"]
+  }
+}
