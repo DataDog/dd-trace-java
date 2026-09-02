ff28215c34faf2857b8e858b43fa92ec39a76e84
Jordan Wong <jordan.wong@datadoghq.com>
2026-09-02 16:26:44 -0400

    workflow(grpc): integration_code

---
+++ dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/client/GrpcClientDecorator.java
@@ -0,0 +1,126 @@
+package datadog.trace.instrumentation.grpc.client;
+
+import static datadog.context.propagation.Propagators.defaultPropagator;
+import static datadog.trace.api.datastreams.DataStreamsContext.fromTags;
+import static datadog.trace.api.datastreams.DataStreamsTags.Direction.OUTBOUND;
+import static datadog.trace.api.datastreams.DataStreamsTags.create;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.startSpan;
+import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.traceConfig;
+
+import datadog.context.Context;
+import datadog.context.propagation.CarrierSetter;
+import datadog.trace.api.Config;
+import datadog.trace.api.GenericClassValue;
+import datadog.trace.api.cache.DDCache;
+import datadog.trace.api.cache.DDCaches;
+import datadog.trace.api.datastreams.DataStreamsContext;
+import datadog.trace.api.naming.SpanNaming;
+import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.bootstrap.instrumentation.api.AgentTracer;
+import datadog.trace.bootstrap.instrumentation.api.InstrumentationTags;
+import datadog.trace.bootstrap.instrumentation.api.InternalSpanTypes;
+import datadog.trace.bootstrap.instrumentation.api.Tags;
+import datadog.trace.bootstrap.instrumentation.api.UTF8BytesString;
+import datadog.trace.bootstrap.instrumentation.decorator.ClientDecorator;
+import io.grpc.MethodDescriptor;
+import io.grpc.Status;
+import java.util.BitSet;
+import java.util.Set;
+import java.util.function.Function;
+
+public class GrpcClientDecorator extends ClientDecorator {
+  public static final CharSequence OPERATION_NAME =
+      UTF8BytesString.create(
+          SpanNaming.instance().namingSchema().client().operationForProtocol("grpc"));
+  public static final CharSequence COMPONENT_NAME = UTF8BytesString.create("grpc-client");
+  public static final CharSequence GRPC_MESSAGE = UTF8BytesString.create("grpc.message");
+
+  private static DataStreamsContext createDsmContext() {
+    return fromTags(create("grpc", OUTBOUND));
+  }
+
+  public static final GrpcClientDecorator DECORATE = new GrpcClientDecorator();
+
+  private static final Set<String> IGNORED_METHODS = Config.get().getGrpcIgnoredOutboundMethods();
+  private static final BitSet CLIENT_ERROR_STATUSES = Config.get().getGrpcClientErrorStatuses();
+
+  private static final ClassValue<UTF8BytesString> MESSAGE_TYPES =
+      GenericClassValue.of(
+          new Function<Class<?>, UTF8BytesString>() {
+            @Override
+            public UTF8BytesString apply(Class<?> input) {
+              return UTF8BytesString.create(input.getName());
+            }
+          });
+
+  private static final DDCache<String, String> RPC_SERVICE_CACHE = DDCaches.newFixedSizeCache(64);
+
+  public UTF8BytesString requestMessageType(MethodDescriptor<?, ?> method) {
+    return messageType(method.getRequestMarshaller());
+  }
+
+  public UTF8BytesString responseMessageType(MethodDescriptor<?, ?> method) {
+    return messageType(method.getResponseMarshaller());
+  }
+
+  private UTF8BytesString messageType(MethodDescriptor.Marshaller<?> marshaller) {
+    return marshaller instanceof MethodDescriptor.ReflectableMarshaller
+        ? MESSAGE_TYPES.get(
+            ((MethodDescriptor.ReflectableMarshaller<?>) marshaller).getMessageClass())
+        : null;
+  }
+
+  @Override
+  protected String[] instrumentationNames() {
+    return new String[] {"grpc", "grpc-client"};
+  }
+
+  @Override
+  protected CharSequence component() {
+    return COMPONENT_NAME;
+  }
+
+  @Override
+  protected CharSequence spanType() {
+    return InternalSpanTypes.RPC;
+  }
+
+  @Override
+  protected String service() {
+    return null;
+  }
+
+  public <ReqT, RespT> AgentSpan startCall(MethodDescriptor<ReqT, RespT> method) {
+    if (IGNORED_METHODS.contains(method.getFullMethodName())) {
+      return AgentTracer.blackholeSpan();
+    }
+    AgentSpan span =
+        startSpan(COMPONENT_NAME.toString(), OPERATION_NAME)
+            .setTag("request.type", requestMessageType(method))
+            .setTag("response.type", responseMessageType(method))
+            .setTag(
+                Tags.RPC_SERVICE,
+                RPC_SERVICE_CACHE.computeIfAbsent(
+                    method.getFullMethodName(), MethodDescriptor::extractFullServiceName));
+    span.setResourceName(method.getFullMethodName());
+    afterStart(span);
+    return span;
+  }
+
+  public <C> void injectContext(Context context, final C request, CarrierSetter<C> setter) {
+    if (traceConfig().isDataStreamsEnabled()) {
+      context = context.with(createDsmContext());
+    }
+    defaultPropagator().inject(context, request, setter);
+  }
+
+  public void onClose(final AgentSpan span, final Status status) {
+    span.setTag("status.code", status.getCode().name());
+    span.setTag("grpc.status.code", status.getCode().name());
+    span.setTag(InstrumentationTags.GRPC_STATUS_CODE, status.getCode().value());
+    span.setTag("status.description", status.getDescription());
+
+    onError(span, status.getCause());
+    span.setError(CLIENT_ERROR_STATUSES.get(status.getCode().value()));
+  }
+}
---
+++ dd-java-agent/instrumentation/grpc-1.5/src/main/java/datadog/trace/instrumentation/grpc/server/GrpcServerBuilderInstrumentation.java
@@ -0,0 +1,80 @@
+package datadog.trace.instrumentation.grpc.server;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.HierarchyMatchers.extendsClass;
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static datadog.trace.bootstrap.CallDepthThreadLocalMap.incrementCallDepth;
+import static java.util.Arrays.asList;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.api.InstrumenterConfig;
+import datadog.trace.bootstrap.CallDepthThreadLocalMap;
+import datadog.trace.bootstrap.ContextStore;
+import datadog.trace.bootstrap.InstrumentationContext;
+import io.grpc.ServerBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+public class GrpcServerBuilderInstrumentation
+    implements Instrumenter.CanShortcutTypeMatching, Instrumenter.HasMethodAdvice {
+
+  @Override
+  public boolean onlyMatchKnownTypes() {
+    return InstrumenterConfig.get()
+        .isIntegrationShortcutMatchingEnabled(asList("grpc", "grpc-server"), true);
+  }
+
+  @Override
+  public String[] knownMatchingTypes() {
+    return new String[] {
+      "io.grpc.internal.AbstractServerImplBuilder",
+      "io.grpc.alts.AltsServerBuilder",
+      "io.grpc.ForwardingServerBuilder",
+      "io.grpc.inprocess.InProcessServerBuilder",
+      "io.grpc.netty.NettyServerBuilder",
+      "io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder",
+      "io.grpc.internal.ServerImplBuilder"
+    };
+  }
+
+  @Override
+  public String hierarchyMarkerType() {
+    return "io.grpc.ServerBuilder";
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> hierarchyMatcher() {
+    return extendsClass(named(hierarchyMarkerType()));
+  }
+
+  @Override
+  public void methodAdvice(MethodTransformer transformer) {
+    transformer.applyAdvice(
+        isMethod().and(isPublic()).and(named("build")).and(takesArguments(0)),
+        GrpcServerBuilderInstrumentation.class.getName() + "$BuildAdvice");
+  }
+
+  public static class BuildAdvice {
+
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(@Advice.This ServerBuilder<?> serverBuilder) {
+      int callDepth = incrementCallDepth(ServerBuilder.class);
+      if (callDepth == 0) {
+        ContextStore<ServerBuilder, Boolean> interceptedStore =
+            InstrumentationContext.get(ServerBuilder.class, Boolean.class);
+        if (interceptedStore.get(serverBuilder) == null) {
+          interceptedStore.put(serverBuilder, Boolean.TRUE);
+          serverBuilder.intercept(TracingServerInterceptor.INSTANCE);
+        }
+      }
+    }
+
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void onExit() {
+      CallDepthThreadLocalMap.decrementCallDepth(ServerBuilder.class);
+    }
+  }
+}
