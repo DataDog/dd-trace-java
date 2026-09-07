79c5653d457bc456e5fe6ee78015d16abe1bea1f
Brice Dutheil <brice.dutheil@gmail.com>
2026-09-07 14:24:36 +0200

    build: use Palantir Java Format

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ with(extensions["spotlessPredeclare"] as SpotlessExtension) {
     removeUnusedImports()
     forbidWildcardImports()
 
-    googleJavaFormat(libs.versions.google.java.format.get())
+    palantirJavaFormat(libs.versions.palantir.java.format.get())
     tableTestFormatter(libs.versions.tabletest.formatter.get())
   }
   groovyGradle {
--- buildSrc/modifiable-config-agent/build.gradle.kts
+++ buildSrc/modifiable-config-agent/build.gradle.kts
@@ -18,7 +18,7 @@ spotless {
     targetExclude("src/test/resources/**")
     removeUnusedImports()
     forbidWildcardImports()
-    googleJavaFormat(libs.versions.google.java.format.get())
+    palantirJavaFormat(libs.versions.palantir.java.format.get())
   }
 }
 
