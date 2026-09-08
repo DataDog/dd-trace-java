dc3deb999dd375bf3a95caecc1d62b3d3e43d4f9
Munir Abdinur <munir.abdinur@datadoghq.com>
2026-09-08 15:17:06 -0400

    test(otel): remove force flush API assertion

--- dd-trace-api/src/test/java/datadog/trace/api/metrics/DatadogMeterProviderTest.java
+++ dd-trace-api/src/test/java/datadog/trace/api/metrics/DatadogMeterProviderTest.java
@@ -3,7 +3,6 @@ package datadog.trace.api.metrics;
 import static java.lang.reflect.Modifier.isAbstract;
 import static java.lang.reflect.Modifier.isPublic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Method;
@@ -12,14 +11,12 @@ import org.junit.jupiter.api.Test;
 class DatadogMeterProviderTest {
 
   @Test
-  void exposesOnlyShutdownLifecycleMethod() throws Exception {
+  void exposesShutdownMethod() throws Exception {
     Method shutdown = DatadogMeterProvider.class.getMethod("shutdown");
 
     assertTrue(isPublic(shutdown.getModifiers()));
     assertTrue(isAbstract(shutdown.getModifiers()));
     assertEquals(CompletableResultCode.class, shutdown.getReturnType());
     assertEquals(0, shutdown.getParameterCount());
-    assertThrows(
-        NoSuchMethodException.class, () -> DatadogMeterProvider.class.getMethod("forceFlush"));
   }
 }
