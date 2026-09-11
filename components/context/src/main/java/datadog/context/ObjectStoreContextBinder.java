e9ae4b123d4153a9b080905b8c276eb7b2df31e8
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2026-09-11 13:53:41 +0100

    Use ObjectStore to track context bound to arbitrary objects.
    Replaces the placeholder synchronized WeakHashMap approach.

--- components/context/src/main/java/datadog/context/ObjectStoreContextBinder.java
+++ components/context/src/main/java/datadog/context/ObjectStoreContextBinder.java
@@ -1,22 +1,21 @@
 package datadog.context;
 
 import static datadog.context.Context.root;
-import static java.util.Collections.synchronizedMap;
 import static java.util.Objects.requireNonNull;
 
-import java.util.Map;
-import java.util.WeakHashMap;
+import datadog.instrument.fieldinject.ObjectStore;
 
-/** {@link ContextBinder} that uses a global weak map of carriers to contexts. */
-final class WeakMapContextBinder implements ContextBinder {
-  static final ContextBinder INSTANCE = new WeakMapContextBinder();
+/** {@link ContextBinder} that uses {@link ObjectStore} to track context carriers. */
+final class ObjectStoreContextBinder implements ContextBinder {
+  static final ContextBinder INSTANCE = new ObjectStoreContextBinder();
 
-  private static final Map<Object, Context> TRACKED = synchronizedMap(new WeakHashMap<>());
+  private static final ObjectStore<Object, Context> CONTEXT_STORE =
+      ObjectStore.of(Object.class, Context.class);
 
   @Override
   public Context from(Object carrier) {
     requireNonNull(carrier, "Context carrier cannot be null");
-    Context bound = TRACKED.get(carrier);
+    Context bound = CONTEXT_STORE.get(carrier);
     return null != bound ? bound : root();
   }
 
@@ -24,13 +23,13 @@ final class WeakMapContextBinder implements ContextBinder {
   public void attachTo(Object carrier, Context context) {
     requireNonNull(carrier, "Context carrier cannot be null");
     requireNonNull(context, "Context cannot be null. Use detachFrom() instead.");
-    TRACKED.put(carrier, context);
+    CONTEXT_STORE.put(carrier, context);
   }
 
   @Override
   public Context detachFrom(Object carrier) {
     requireNonNull(carrier, "Context key cannot be null");
-    Context previous = TRACKED.remove(carrier);
+    Context previous = CONTEXT_STORE.remove(carrier);
     return null != previous ? previous : root();
   }
 }
