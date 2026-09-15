d7613e8b41ba77a41ad704082c8d28db08a73f72
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-09-15 15:41:14 +0200

    Use context swap for Akka and Pekko HTTP flows

--- dd-java-agent/instrumentation/akka/akka-http/akka-http-10.0/src/main/java/datadog/trace/instrumentation/akkahttp/DatadogWrapperHelper.java
+++ dd-java-agent/instrumentation/akka/akka-http/akka-http-10.0/src/main/java/datadog/trace/instrumentation/akkahttp/DatadogWrapperHelper.java
@@ -7,17 +7,34 @@ import akka.http.scaladsl.model.HttpRequest;
 import akka.http.scaladsl.model.HttpResponse;
 import datadog.context.Context;
 import datadog.context.ContextScope;
+import datadog.trace.api.InstrumenterConfig;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 
 public class DatadogWrapperHelper {
+  private static final boolean LEGACY_CONTEXT_MANAGER_ENABLED =
+      InstrumenterConfig.get().isLegacyContextManagerEnabled();
+
   public static ContextScope createSpan(final HttpRequest request) {
+    return startSpan(request).attach();
+  }
+
+  public static ContextScope createSpanForFlow(final HttpRequest request) {
+    final Context context = startSpan(request);
+    if (LEGACY_CONTEXT_MANAGER_ENABLED) {
+      return context.attach();
+    }
+    context.swap();
+    return context.asScope();
+  }
+
+  private static Context startSpan(final HttpRequest request) {
     final Context parentContext = DECORATE.extract(request);
     final Context context = DECORATE.startSpan(request, parentContext);
     final AgentSpan span = fromContext(context);
     DECORATE.afterStart(span);
     DECORATE.onRequest(span, request, request, parentContext);
 
-    return context.attach();
+    return context;
   }
 
   public static void finishSpan(final Context context, final HttpResponse response) {
--- dd-java-agent/instrumentation/pekko/pekko-http-1.0/src/main/java/datadog/trace/instrumentation/pekkohttp/DatadogWrapperHelper.java
+++ dd-java-agent/instrumentation/pekko/pekko-http-1.0/src/main/java/datadog/trace/instrumentation/pekkohttp/DatadogWrapperHelper.java
@@ -5,19 +5,36 @@ import static datadog.trace.instrumentation.pekkohttp.PekkoHttpServerDecorator.D
 
 import datadog.context.Context;
 import datadog.context.ContextScope;
+import datadog.trace.api.InstrumenterConfig;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
 import org.apache.pekko.http.scaladsl.model.HttpRequest;
 import org.apache.pekko.http.scaladsl.model.HttpResponse;
 
 public class DatadogWrapperHelper {
+  private static final boolean LEGACY_CONTEXT_MANAGER_ENABLED =
+      InstrumenterConfig.get().isLegacyContextManagerEnabled();
+
   public static ContextScope createSpan(final HttpRequest request) {
+    return startSpan(request).attach();
+  }
+
+  public static ContextScope createSpanForFlow(final HttpRequest request) {
+    final Context context = startSpan(request);
+    if (LEGACY_CONTEXT_MANAGER_ENABLED) {
+      return context.attach();
+    }
+    context.swap();
+    return context.asScope();
+  }
+
+  private static Context startSpan(final HttpRequest request) {
     final Context parentContext = DECORATE.extract(request);
     final Context context = DECORATE.startSpan(request, parentContext);
     final AgentSpan span = fromContext(context);
     DECORATE.afterStart(span);
     DECORATE.onRequest(span, request, request, parentContext);
 
-    return context.attach();
+    return context;
   }
 
   public static void finishSpan(final Context context, final HttpResponse response) {
