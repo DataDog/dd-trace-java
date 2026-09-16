aab708fa43286d9b85a9702549c6efa6575e8950
Matthew Li <matthew.li@datadoghq.com>
2026-09-16 14:34:48 -0400

    update tests to include SDK_OTLP_EXPORT

--- dd-java-agent/instrumentation/spring/spring-webmvc/spring-webmvc-3.1/src/test/groovy/test/boot/SpringBootBasedTest.groovy
+++ dd-java-agent/instrumentation/spring/spring-webmvc/spring-webmvc-3.1/src/test/groovy/test/boot/SpringBootBasedTest.groovy
@@ -544,6 +544,7 @@ class SpringBootBasedTest extends HttpServerTest<ConfigurableApplicationContext>
             "runtime-id" String
             "thread.id" Number
             "thread.name" String
+            "_dd.sdk.otlp_export" "false"
             serviceNameSource "inferred_proxy"
           }
         }
