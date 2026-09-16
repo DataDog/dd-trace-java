5ca371e71ed5f1a6702e23ba5fe23b4133ab2795
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-09-16 12:40:04 +0200

    Prevent Couchbase Netty initialization from capturing context

--- dd-java-agent/instrumentation/couchbase/couchbase-2.0/src/test/groovy/springdata/CouchbaseSpringTemplateTest.groovy
+++ dd-java-agent/instrumentation/couchbase/couchbase-2.0/src/test/groovy/springdata/CouchbaseSpringTemplateTest.groovy
@@ -17,12 +17,6 @@ import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activeSpan
 @Flaky
 class CouchbaseSpringTemplateTest extends AbstractCouchbaseTest {
 
-  @Override
-  boolean useStrictTraceWrites() {
-    // TODO fix this by making sure that spans get closed properly
-    return false
-  }
-
   @Shared
   List<CouchbaseTemplate> templates
 
