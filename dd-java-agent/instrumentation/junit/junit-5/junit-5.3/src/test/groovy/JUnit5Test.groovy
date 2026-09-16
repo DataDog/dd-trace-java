bb92d653eb5a39b19b543a3dddba088283351427
Daniel Mohedano <daniel.mohedano@datadoghq.com>
2026-09-16 11:34:20 +0200

    test: clarify ATR eligibility coverage

--- dd-java-agent/instrumentation/junit/junit-5/junit-5.3/src/test/groovy/JUnit5Test.groovy
+++ dd-java-agent/instrumentation/junit/junit-5/junit-5.3/src/test/groovy/JUnit5Test.groovy
@@ -132,7 +132,8 @@ class JUnit5Test extends CiVisibilityInstrumentationTest {
 
     where:
     testcaseName                             | success | tests                          | retriedTests
-    "test-retry-failed"                      | false   | [TestFailed]                   | []
+    "test-failed"                            | false   | [TestFailed]                   | []
+    "test-retry-failed"                      | false   | [TestFailed]                   | [new TestFQN("org.example.TestFailed", "test_failed")]
     "test-failed-then-succeed"               | true    | [TestFailedThenSucceed]        | [new TestFQN("org.example.TestFailedThenSucceed", "test_failed_then_succeed")]
     "test-retry-template"                    | false   | [TestFailedTemplate]           | [new TestFQN("org.example.TestFailedTemplate", "test_template")]
     "test-retry-factory"                     | false   | [TestFailedFactory]            | [new TestFQN("org.example.TestFailedFactory", "test_factory")]
