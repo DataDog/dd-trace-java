9d31c51f2ddf8c9a86763281df66770152b63a64
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-09-21 15:39:03 +0200

    Remove flaky annotation

--- dd-java-agent/instrumentation/hystrix-1.4/src/test/groovy/HystrixObservableTest.groovy
+++ dd-java-agent/instrumentation/hystrix-1.4/src/test/groovy/HystrixObservableTest.groovy
@@ -3,7 +3,6 @@ import com.netflix.hystrix.HystrixObservableCommand
 import com.netflix.hystrix.exception.HystrixRuntimeException
 import datadog.trace.api.Trace
 import datadog.trace.bootstrap.instrumentation.api.Tags
-import datadog.trace.test.util.Flaky
 import rx.Observable
 import rx.Scheduler
 import rx.schedulers.Schedulers
@@ -246,7 +245,6 @@ class HystrixObservableTest extends HystrixTestRunner {
     }
   }
 
-  @Flaky("https://github.com/DataDog/dd-trace-java/issues/7725")
   def "test no fallback results in error for #action"() {
     setup:
     def observeOnFn = observeOn
