eb7603ff7f6dae20070feef8056dcf98927e0627
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-09-23 09:37:31 +0200

    Simplify tests

--- dd-java-agent/instrumentation/netty/netty-4.1/src/test/java/datadog/trace/instrumentation/netty41/Http2ContextTransferTest.java
+++ dd-java-agent/instrumentation/netty/netty-4.1/src/test/java/datadog/trace/instrumentation/netty41/Http2ContextTransferTest.java
@@ -9,7 +9,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import datadog.context.Context;
-import datadog.context.ContextKey;
 import io.netty.channel.Channel;
 import io.netty.util.Attribute;
 import io.netty.util.DefaultAttributeMap;
@@ -22,7 +21,7 @@ class Http2ContextTransferTest {
     Channel first = channel(parent);
     Channel second = channel(parent);
     Attribute<Context> attribute = parent.attr(CONTEXT_ATTRIBUTE_KEY);
-    Context context = Context.root().with(ContextKey.named("request"), "first");
+    Context context = Context.root();
     attribute.set(context);
 
     afterCreate(first);
@@ -32,39 +31,6 @@ class Http2ContextTransferTest {
     assertNull(attribute.get());
     assertSame(attribute, parent.attr(CONTEXT_ATTRIBUTE_KEY));
     assertFalse(second.hasAttr(CONTEXT_ATTRIBUTE_KEY));
-
-    Context nextContext = Context.root().with(ContextKey.named("request"), "second");
-    attribute.set(nextContext);
-    afterCreate(second);
-
-    assertSame(nextContext, second.attr(CONTEXT_ATTRIBUTE_KEY).get());
-    assertNull(attribute.get());
-  }
-
-  @Test
-  void doesNotCreateChildAttributeForClearedParentContext() {
-    Channel parent = channel(null);
-    Channel child = channel(parent);
-    parent.attr(CONTEXT_ATTRIBUTE_KEY).set(null);
-
-    afterCreate(child);
-
-    assertFalse(child.hasAttr(CONTEXT_ATTRIBUTE_KEY));
-  }
-
-  @Test
-  void preservesExistingChildContextAndDoesNotConsumeParent() {
-    Channel parent = channel(null);
-    Channel child = channel(parent);
-    Context parentContext = Context.root();
-    Context childContext = Context.root().with(ContextKey.named("child"), "existing");
-    parent.attr(CONTEXT_ATTRIBUTE_KEY).set(parentContext);
-    child.attr(CONTEXT_ATTRIBUTE_KEY).set(childContext);
-
-    afterCreate(child);
-
-    assertSame(childContext, child.attr(CONTEXT_ATTRIBUTE_KEY).get());
-    assertSame(parentContext, parent.attr(CONTEXT_ATTRIBUTE_KEY).get());
   }
 
   private static Channel channel(Channel parent) {
