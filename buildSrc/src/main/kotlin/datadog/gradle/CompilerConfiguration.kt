c841f607d7716b7df4509b966a59276a9a1c5cb6
Alexey Kuznetsov <alexey.kuznetsov@datadoghq.com>
2026-09-24 18:52:52 -0400

    Use Java 25 as the default compiler toolchain (#12305)
    Use Java 25 as the default compiler toolchain
    
    Merge branch 'master' into alexeyk/use-java-25
    
    Merge branch 'master' into alexeyk/use-java-25
    
    refactor: default the compiler to the Gradle daemon
    
    Merge branch 'master' into alexeyk/use-java-25
    
    fix: use default compiler for Tomcat builds
    
    Merge branch 'master' into alexeyk/use-java-25
    
    Merge branch 'master' into alexeyk/use-java-25
    
    Co-authored-by: bric3 <brice.dutheil@gmail.com>
    Co-authored-by: alexey.kuznetsov <alexey.kuznetsov@datadoghq.com>

---
+++ buildSrc/src/main/kotlin/datadog/gradle/CompilerConfiguration.kt
@@ -0,0 +1,26 @@
+package datadog.gradle
+
+import groovy.lang.Closure
+import org.gradle.api.JavaVersion
+import org.gradle.api.tasks.compile.AbstractCompile
+import org.gradle.kotlin.dsl.extra
+
+fun AbstractCompile.configureCompiler(
+  targetVersion: JavaVersion,
+  unsetReleaseFlagReason: String = "",
+) {
+  (project.extra["configureCompiler"] as Closure<*>).call(this, targetVersion, unsetReleaseFlagReason)
+}
+
+fun AbstractCompile.configureCompiler(
+  toolchainVersion: Int,
+  targetVersion: JavaVersion,
+  unsetReleaseFlagReason: String = "",
+) {
+  (project.extra["configureCompiler"] as Closure<*>).call(
+    this,
+    toolchainVersion,
+    targetVersion,
+    unsetReleaseFlagReason,
+  )
+}
