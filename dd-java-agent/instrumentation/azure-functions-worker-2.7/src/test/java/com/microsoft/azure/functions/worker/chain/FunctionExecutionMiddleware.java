675b1ca459e31fe5cd5c6ac5de3a69d0344d4fba
jcstorms1 <jordan.storms@datadoghq.com>
2026-09-24 14:35:40 -0400

    Align Azure worker test fixture with SPI

--- dd-java-agent/instrumentation/azure-functions-worker-2.7/src/test/java/com/microsoft/azure/functions/worker/chain/FunctionExecutionMiddleware.java
+++ dd-java-agent/instrumentation/azure-functions-worker-2.7/src/test/java/com/microsoft/azure/functions/worker/chain/FunctionExecutionMiddleware.java
@@ -1,9 +1,11 @@
 package com.microsoft.azure.functions.worker.chain;
 
+import com.microsoft.azure.functions.internal.spi.middleware.Middleware;
 import com.microsoft.azure.functions.internal.spi.middleware.MiddlewareChain;
 import com.microsoft.azure.functions.internal.spi.middleware.MiddlewareContext;
 
-public class FunctionExecutionMiddleware {
+public class FunctionExecutionMiddleware implements Middleware {
+  @Override
   public void invoke(MiddlewareContext context, MiddlewareChain chain) throws Exception {
     chain.doNext(context);
   }
