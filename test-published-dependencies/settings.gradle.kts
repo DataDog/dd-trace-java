ea37db51cb616d9d4963e24745138ef863894e12
Brice Dutheil <brice.dutheil@gmail.com>
2026-09-25 16:13:23 +0200

    fix: Route published dependency plugins through proxy

--- test-published-dependencies/settings.gradle.kts
+++ test-published-dependencies/settings.gradle.kts
@@ -1,3 +1,22 @@
+pluginManagement {
+  repositories {
+    mavenLocal()
+    if (settings.extra.has("gradlePluginProxy")) {
+      maven {
+        url = uri(settings.extra["gradlePluginProxy"] as String)
+        isAllowInsecureProtocol = true
+      }
+    }
+    if (settings.extra.has("mavenRepositoryProxy")) {
+      maven {
+        url = uri(settings.extra["mavenRepositoryProxy"] as String)
+        isAllowInsecureProtocol = true
+      }
+    }
+    gradlePluginPortal()
+  }
+}
+
 rootProject.name = "test-published-dependencies"
 
 dependencyResolutionManagement {
